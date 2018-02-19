101179c609439750938cac47103a60f77218ee79
Bruno Thomas <bruno@barreverte.fr>
2018-02-19 12:54:33 +0100

    [refactor] dead code + gitignore

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 import App from './App'
@@ -13,7 +11,7 @@ const i18n = new VueI18n({locale: 'en', messages})
 
 /* eslint-disable no-new */
 new Vue({
-  i18n: i18n,
+  i18n,
   el: '#app',
   router,
   components: { App },
