7784baf9886ac1d0e315151f7b74274f68c44f12
Bruno Thomas <bruno@barreverte.fr>
2018-02-20 18:57:08 +0100

    [ui] adds search results view

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -2,11 +2,13 @@
   <div class="search">
     <input v-model="searchQuery" type="search" :placeholder="$t('search.placeholder')" name="search">
     <button v-on:click="search">{{ $t('search.buttonlabel') }}</button>
+    <search-results v-bind:results="searchResults" />
   </div>
 </template>
 
 <script>
 import es from 'elasticsearch-browser'
+import SearchResults from './SearchResults'
 
 var esClient = new es.Client({
   host: 'elasticsearch:9200',
@@ -14,12 +16,14 @@ var esClient = new es.Client({
 })
 
 export default {
+  components: {SearchResults},
   name: 'search',
   data () {
-    return {searchQuery: ''}
+    return {searchQuery: '', searchResults: []}
   },
   methods: {
     search () {
+      var that = this
       esClient.search({
         index: 'datashare-local',
         type: 'doc',
@@ -30,6 +34,10 @@ export default {
             }
           }
         }
+      }).then(function (resp) {
+        that.searchResults = resp.hits.hits
+      }, function (err) {
+        console.trace(err.message)
       })
       this.searchQuery = ''
     }
---
+++ src/components/SearchResults.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="search-results list">
+    <div class="item" v-for="item in results" :key="item._id">
+      {{ item._source.path}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'search-results',
+  props: ['results']
+}
+</script>
