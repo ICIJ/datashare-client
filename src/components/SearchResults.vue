ccc3d6c6ab08662959537952cb5d7eaa8132135c
Bruno Thomas <bruno@barreverte.fr>
2018-02-22 10:27:51 +0100

    [ui] display entities first

--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -2,13 +2,13 @@
   <div class="search-results list">
     <div class="item" v-for="item in results" :key="item.score">
       document : {{item._source.path}}
-      <div class="fragments">
-        {{$t('search.results.match')}}:
-        <span class="fragment" v-for="fragment in item.highlight.content" v-html="fragment" :key="fragment"></span>
-      </div>
       <div class="named-entities">
         {{$t('search.results.entities')}}: <b>{{ item.inner_hits.NamedEntity.hits.total }}</b>
         <span class="named-entity" v-for="ne in item.inner_hits.NamedEntity.hits.hits" :key="ne._id">{{ne._source.mention}} ({{ne._source.category}}/{{ne._source.offset}})</span>
+      </div>
+      <div class="fragments">
+        {{$t('search.results.match')}}:
+        <span class="fragment" v-for="fragment in item.highlight.content" v-html="fragment" :key="fragment"></span>
       </div>
     </div>
   </div>
