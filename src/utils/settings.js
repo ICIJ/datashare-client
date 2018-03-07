258b358112310eae49a2496abb288867ff534e30
pirhoo <hello@pirhoo.com>
2018-03-07 10:43:31 +0100

    Added utils and sass resource loader

---
+++ src/utils/settings.js
@@ -0,0 +1,26 @@
+export const SETTINGS = {
+  indicators: {
+    'tax-treaty-status': [
+      'In effect',
+      'Awaits ratification',
+      'Awaits signature',
+      'In negotiation'
+    ]
+  },
+  variables: {
+    'dividends': {
+      label: 'Dividends'
+    },
+    'interest': {
+      label: 'Interest'
+    },
+    'royalties': {
+      label: 'Royalties'
+    },
+    'management-fee': {
+      label: 'Management Fee'
+    }
+  }
+}
+
+export default SETTINGS
