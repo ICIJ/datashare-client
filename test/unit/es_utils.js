0b3494a260e4f2d1bd06bad84589a50ab037412f
Bruno Thomas <bruno@barreverte.fr>
2018-03-14 14:00:50 +0100

    [refactor][test] extract es_utils module

---
+++ test/unit/es_utils.js
@@ -0,0 +1,68 @@
+function letData (index) {
+  return new IndexBuilder(index)
+}
+
+class IndexedNe {
+  constructor (mention, offset) {
+    this.mention = mention
+    this.offset = offset
+  }
+}
+
+class IndexedDocument {
+  constructor (path) {
+    this.path = path
+    this.join = {name: 'Document'}
+    this.type = 'Document'
+    this.metadata = {}
+    this.nerList = []
+  }
+  withContent (content) {
+    this.content = content
+    return this
+  }
+  withNer (mention, offset = 1) {
+    this.nerList.push(new IndexedNe(mention, offset))
+    return this
+  }
+}
+
+class IndexBuilder {
+  constructor (index) {
+    this.index = index
+  }
+  have (document) {
+    this.document = document
+    return this
+  }
+  async commit () {
+    var docId = this.document.path
+    await this.index.create({
+      index: process.env.CONFIG.es_index,
+      type: 'doc',
+      refresh: true,
+      id: docId,
+      body: this.document
+    })
+    for (var i = 0; i < this.document.nerList.length; i++) {
+      let ner = this.document.nerList[i]
+      await this.index.create({
+        index: process.env.CONFIG.es_index,
+        type: 'doc',
+        refresh: true,
+        id: ner.mention + ner.offset,
+        routing: docId,
+        body: {
+          mention: ner.mention,
+          mentionNorm: ner.mention,
+          documentId: docId,
+          offset: ner.offset,
+          type: 'NamedEntity',
+          join: {name: 'NamedEntity', parent: docId}
+        }
+      })
+    }
+  }
+}
+
+export {letData, IndexBuilder, IndexedDocument, IndexedNe}
--- test/unit/specs/Search.spec.js
+++ test/unit/specs/Search.spec.js
@@ -9,6 +9,7 @@ import router from '@/router'
 
 import FontAwesomeIcon from '@/components/FontAwesomeIcon'
 import Search from '@/components/Search'
+import {IndexedDocument, letData} from '../es_utils'
 
 Vue.use(VueI18n)
 
@@ -98,70 +99,3 @@ describe('Search.vue', () => {
     expect(wrapped.vm.$el.querySelector('span.aggregation').textContent).to.equal('3 occurrences in 2 documents')
   })
 })
-
-function letData (index) {
-  return new IndexBuilder(index)
-}
-
-class IndexedNe {
-  constructor (mention, offset) {
-    this.mention = mention
-    this.offset = offset
-  }
-}
-
-class IndexedDocument {
-  constructor (path) {
-    this.path = path
-    this.join = {name: 'Document'}
-    this.type = 'Document'
-    this.metadata = {}
-    this.nerList = []
-  }
-  withContent (content) {
-    this.content = content
-    return this
-  }
-  withNer (mention, offset = 1) {
-    this.nerList.push(new IndexedNe(mention, offset))
-    return this
-  }
-}
-
-class IndexBuilder {
-  constructor (index) {
-    this.index = index
-  }
-  have (document) {
-    this.document = document
-    return this
-  }
-  async commit () {
-    var docId = this.document.path
-    await this.index.create({
-      index: process.env.CONFIG.es_index,
-      type: 'doc',
-      refresh: true,
-      id: docId,
-      body: this.document
-    })
-    for (var i = 0; i < this.document.nerList.length; i++) {
-      let ner = this.document.nerList[i]
-      await this.index.create({
-        index: process.env.CONFIG.es_index,
-        type: 'doc',
-        refresh: true,
-        id: ner.mention + ner.offset,
-        routing: docId,
-        body: {
-          mention: ner.mention,
-          mentionNorm: ner.mention,
-          documentId: docId,
-          offset: ner.offset,
-          type: 'NamedEntity',
-          join: {name: 'NamedEntity', parent: docId}
-        }
-      })
-    }
-  }
-}
