120a0a32e78900f2126ed75ea88ebd04d30d976f
pirhoo <hello@pirhoo.com>
2018-04-20 11:40:37 -0400

    Started to integrate tasks list

--- src/components/Indexing.vue
+++ src/components/Indexing.vue
@@ -1,26 +1,33 @@
 <template>
-  <div class="indexing">
-    <form class="indexing__form input-group" @submit.prevent="submit">
-      <div class="form-group">
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" id="index" v-model="index">
-          <label class="form-check-label" for="index">
-            {{ $t('indexing.index_stage_label') }}
-          </label>
-        </div>
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" id="extract" v-model="extract">
-          <label class="form-check-label" for="extract">
-            {{ $t('indexing.extract_stage_label') }}
-          </label>
-        </div>
-      </div>
-      <div class="form-group">
-        <label for="path">Path</label>
-        <input id="path" v-model="path"/>
-      </div>
-      <div class="form-group">
-        <label for="pipeline">NLP Pipeline</label>
+  <div class="indexing container-fluid">
+    <div class="row">
+      <form class="indexing__form mt-4 col-lg-3 col-md-4 order-last" @submit.prevent="submit">
+        <div class="card">
+          <div class="card-header">
+            <h3 class="h5 m-0">
+              {{ $t('indexing.new') }}
+            </h3>
+          </div>
+          <div class="form-group card-body my-0">
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" id="index" v-model="index">
+              <label class="form-check-label" for="index">
+                {{ $t('indexing.index_stage_label') }}
+              </label>
+            </div>
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" id="extract" v-model="extract">
+              <label class="form-check-label" for="extract">
+                {{ $t('indexing.extract_stage_label') }}
+              </label>
+            </div>
+          </div>
+          <div class="form-group card-body my-0">
+            <label for="path">Path</label>
+            <input id="path" v-model="path" class="form-control "/>
+          </div>
+          <div class="form-group card-body my-0">
+            <label for="pipeline">NLP Pipeline</label>
             <select class="form-control" id="pipeline" v-model="pipeline">
               <option value="CORENLP">Core NLP</option>
               <option value="OPENNLP">Open NLP</option>
@@ -28,19 +35,42 @@
               <option value="IXAPIPE">Ixa Pipe</option>
               <option value="GATENLP">Gate NLP</option>
             </select>
-      </div>
-      <div class="form-group">
-        <button class="btn btn-primary" type="submit">{{ $t('indexing.btnLabel') }}</button>
-      </div>
-    </form>
-    <template v-if="tasks.length">
-      <div v-for="task in tasks" :key="task.name" class="indexing__tasks">
-        <div class="indexing__tasks__progress progress">
-          <div class="progress-bar" role="progressbar" :style="'width: ' + task.progress * 100 + '%'" :aria-valuenow="task.progress * 100" aria-valuemin="0" aria-valuemax="100">{{task.name}} ({{task.state}})</div>
+          </div>
+          <div class="form-group card-footer mb-0">
+            <button class="btn btn-icij" type="submit">
+              {{ $t('indexing.btnLabel') }}
+            </button>
+          </div>
+        </div>
+      </form>
+      <div class="col-lg-9 col-md-8 mt-4" v-if="tasks.length">
+        <div class="card">
+          <div class="card-header">
+            <h3 class="h5 m-0">
+              {{ $t('indexing.ongoing') }}
+            </h3>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li v-for="task in tasks" :key="task.name" class="indexing__tasks list-group-item">
+              {{ task.name }}
+              <span class="badge badge-pill small float-right">
+                {{ task.state }}
+              </span>
+              <div class="indexing__tasks__progress progress">
+                <div class="progress-bar" :class="taskStateToClass(task.state)" role="progressbar" :style="'width: ' + task.progress * 100 + '%'" :aria-valuenow="task.progress * 100" aria-valuemin="0" aria-valuemax="100">
+                  {{ Math.round(task.progress * 100) }}%
+                </div>
+              </div>
+            </li>
+          </ul>
+          <div class="card-footer text-right border-0">
+            <button class="btn btn-icij" type="button" @click="cleanTasks">
+              {{ $t('indexing.purge') }}
+            </button>
+          </div>
         </div>
       </div>
-      <button class="btn btn-secondary" type="button" @click="cleanTasks">Clean</button>
-    </template>
+    </div>
   </div>
 </template>
 
@@ -75,6 +105,13 @@ export default {
     },
     cleanTasks () {
       this.$store.dispatch('indexing/cleanTasks')
+    },
+    taskStateToClass (state) {
+      switch (state) {
+        case ('DONE'): return 'bg-success'
+        case ('EXECPT'): return 'bg-danger'
+        case ('RUNNING'): return 'bg-info'
+      }
     }
   }
 }
