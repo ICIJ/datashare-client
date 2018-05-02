4205a643a409edb6c23953a09660693e4d9240c9
Bruno Thomas <bruno@barreverte.fr>
2018-05-02 14:29:46 +0200

    [docviewers] adds pdf viewer

---
+++ src/components/PdfViewer.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="pdf-viewer">
+    <div class="pdf-viewer__header" v-if="document.pages.length > 1">
+      Page <select class="form-control input-sm" v-model.number="document.active">
+      <option v-for="page in document.pages.length" v-bind:key="page.address">
+        {{ page }}
+      </option>
+    </select> of {{ document.pages.length }}
+    </div>
+    <div v-if="page(document.active)">
+      <img class="pdf-viewer__canvas img-responsive img-thumbnail" :src="page(document.active)"/>
+    </div>
+    <div v-else class="alert">
+      <i class="fa fa fa-cog fa-spin"></i>
+      Generating preview...
+    </div>
+  </div>
+</template>
+
+<script>
+import PDFJS from 'pdfjs-dist'
+
+export default {
+  name: 'pdf-viewer',
+  props: ['url', 'workerSrc'],
+  data () {
+    return {
+      document: {
+        promise: null,
+        active: 1,
+        pages: []
+      }
+    }
+  },
+  methods: {
+    page (p) {
+      this.$set(this.document, 'active', p)
+      // Did we fetch this page already?
+      if (this.document.pages[p - 1]) {
+        return this.document.pages[p - 1]
+      } else {
+        this.pdf().then(pdf => {
+          return this.render(pdf, p).then(canvas => {
+            if (this.document.pages.length === 0) {
+              this.$set(this.document, 'pages', new Array(pdf.pdfInfo.numPages))
+            }
+            this.$set(this.document.pages, p - 1, canvas.toDataURL())
+          })
+        })
+      }
+    },
+    pdf () {
+      return new Promise((resolve, reject) => {
+        if (this.document.promise) {
+          this.document.promise.then(resolve)
+        } else {
+          PDFJS.workerSrc = this.workerSrc
+          this.document.promise = PDFJS.getDocument(this.url).promise
+          this.document.promise.then(resolve)
+        }
+      })
+    },
+    render (pdf, p) {
+      return pdf.getPage(p).then(page => {
+        const scale = 1.5
+        const viewport = page.getViewport(scale)
+        const canvas = document.createElement('canvas')
+        const canvasContext = canvas.getContext('2d')
+
+        canvas.height = viewport.height
+        canvas.width = viewport.width
+
+        const renderContext = {canvasContext, viewport}
+        return page.render(renderContext).then(() => canvas)
+      })
+    }
+  }
+}
+</script>
