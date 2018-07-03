e705d1b034a3bc1d276bd32c4f6e33e224ac80eb
annelhote <anne.lhote@gmail.com>
2018-07-03 19:20:13 +0200

    Delete index if exists, before creating it into esConnector

--- test/unit/specs/utils/esConnectionHelper.js
+++ test/unit/specs/utils/esConnectionHelper.js
@@ -2,19 +2,23 @@ import elasticsearch from 'elasticsearch-browser'
 import esMapping from '@/datashare_index_mappings.json'
 
 let es = new elasticsearch.Client({host: process.env.CONFIG.es_host})
+let esIndex = process.env.CONFIG.es_index
 
 const esConnectionHelper = () => {
   before(async () => {
-    await es.indices.create({index: process.env.CONFIG.es_index})
-    await es.indices.putMapping({index: process.env.CONFIG.es_index, type: 'doc', body: esMapping})
+    if (es.indices.exists({index: esIndex})) {
+      await es.indices.delete({index: esIndex})
+    }
+    await es.indices.create({index: esIndex})
+    await es.indices.putMapping({index: esIndex, type: 'doc', body: esMapping})
   })
 
   after(async () => {
-    await es.indices.delete({index: process.env.CONFIG.es_index})
+    await es.indices.delete({index: esIndex})
   })
 
   beforeEach(async () => {
-    await es.deleteByQuery({index: process.env.CONFIG.es_index, conflicts: 'proceed', refresh: true, body: {query: {match_all: {}}}})
+    await es.deleteByQuery({index: esIndex, conflicts: 'proceed', refresh: true, body: {query: {match_all: {}}}})
   })
 }
 
