98a1c7987e0f13f404b432bf5f497b22f703f4da
Pierre Romera <hello@pirhoo.com>
2018-07-05 18:13:34 +0200

    Prevented unecessary route change

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -24,6 +24,8 @@ export default {
   },
   methods: {
     submit () {
+      // Did the route change? If not, do nothing
+      if (this.$route.name === 'search' && this.$route.query.q === this.query) return false
       // Change the route after update the store with the new query
       this.$store.commit('search/query', this.query)
       this.$router.push({ name: 'search', query: this.$store.getters['search/toRouteQuery'] })
