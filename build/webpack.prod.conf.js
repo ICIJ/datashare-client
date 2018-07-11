e3ee5dad2e18405c698829c8079755f8a40c4eb9
Bruno Thomas <bruno@barreverte.fr>
2018-07-11 16:49:13 +0200

    [build] revert hardsource plugin
    this lead to erroneous runtime build (weird errors like 'w is undefined' or 'void(0) is not a constructor')
    furthermore, the build is 2min 30sec approx, maybe there has been a circular dependency issue or
    something like this that has been resolved

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -10,7 +10,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const HardSourceWebpackPlugin = require('hard-source-webpack-plugin')
 
 const env = process.env.NODE_ENV === 'testing'
   ? require('../config/test.env')
@@ -36,8 +35,6 @@ const webpackConfig = merge(baseWebpackConfig, {
     chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
   },
   plugins: [
-    // Speeds up build with incremental cache
-    new HardSourceWebpackPlugin(),
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
     new webpack.DefinePlugin({
       'process.env': env
