95ffcf271fb3e8203bf919a5450e522e4ecc628e
Bruno Thomas <bruno@barreverte.fr>
2018-07-18 16:41:56 +0200

    [web] adds git version plugin

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,6 @@
 'use strict'
+const GitRevisionPlugin = require('git-revision-webpack-plugin')
+const gitRevisionPlugin = new GitRevisionPlugin()
 
 const CONFIG = {
   es_index: '"datashare"',
@@ -6,7 +8,10 @@ const CONFIG = {
   data_prefix: '"/data/"',
   ds_auth_signin: '"/auth/xemx/signin"',
   ds_cookie_name: '"_ds_session_id"',
-  ds_auth_signout: '"/auth/signout"'
+  ds_auth_signout: '"/auth/signout"',
+  git_version: JSON.stringify(gitRevisionPlugin.version()),
+  git_hash: JSON.stringify(gitRevisionPlugin.commithash()),
+  git_branch: JSON.stringify(gitRevisionPlugin.branch()),
 }
 
 module.exports = {
