4942d24da5c3c695007a12d4991d07ffc00260d4
Pierre Romera <hello@pirhoo.com>
2018-07-31 12:53:09 +0400

    Refactor expect

--- tests/unit/specs/api/Document.spec.js
+++ tests/unit/specs/api/Document.spec.js
@@ -1,44 +1,43 @@
 import Document from '@/api/Document'
-import { expect } from 'chai'
 
 describe('Document', () => {
   it('should display human readable size for -1', () => {
     let doc = new Document({_source: {contentLength: -1}})
-    expect(doc.humanSize).to.equal('unknown')
+    expect(doc.humanSize).toEqual('unknown')
   })
 
   it('should display human readable size for 0', () => {
     let doc = new Document({_source: {contentLength: 0}})
-    expect(doc.humanSize).to.equal('0 B')
+    expect(doc.humanSize).toEqual('0 B')
   })
 
   it('should display human readable size for Bytes', () => {
     let doc = new Document({_source: {contentLength: 12}})
-    expect(doc.humanSize).to.equal('12 B')
+    expect(doc.humanSize).toEqual('12 B')
   })
 
   it('should display human readable size for Kilobytes', () => {
     let doc = new Document({_source: {contentLength: 10342}})
-    expect(doc.humanSize).to.equal('10.10 kB (10342 B)')
+    expect(doc.humanSize).toEqual('10.10 kB (10342 B)')
   })
 
   it('should display human readable size for Megabytes', () => {
     let doc = new Document({_source: {contentLength: 12345678}})
-    expect(doc.humanSize).to.equal('11.77 MB (12345678 B)')
+    expect(doc.humanSize).toEqual('11.77 MB (12345678 B)')
   })
 
   it('should give relative path of document from data prefix', () => {
     let docFile = new Document({_source: {path: '/a/path/to/data/sub/dir/file.txt'}})
-    expect(docFile.relativePath).to.equal('/api/data/sub/dir/file.txt')
+    expect(docFile.relativePath).toEqual('/api/data/sub/dir/file.txt')
   })
 
   it('should give the original path of document from data prefix if no data string in path', () => {
     let docFile = new Document({_source: {path: '/a/datapath/to/sub/dir/file.txt'}})
-    expect(docFile.relativePath).to.equal('/a/datapath/to/sub/dir/file.txt')
+    expect(docFile.relativePath).toEqual('/a/datapath/to/sub/dir/file.txt')
   })
 
   it('should give relative path when data_prefix is not a directory', () => {
     let docFile = new Document({_source: {path: '/datashare/data/file.txt'}})
-    expect(docFile.relativePath).to.equal('/api/data/file.txt')
+    expect(docFile.relativePath).toEqual('/api/data/file.txt')
   })
 })
