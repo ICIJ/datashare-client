71241de184851e23151d673e65c91cda96323a1c
Pierre Romera <hello@pirhoo.com>
2018-08-04 01:08:02 +0400

    Refactor mixing to split code with Facet component

--- src/mixins/facets.js
+++ src/mixins/facets.js
@@ -1,58 +1,18 @@
-import filter from 'lodash/filter'
-import includes from 'lodash/includes'
-import slice from 'lodash/slice'
-import toLower from 'lodash/toLower'
-import toString from 'lodash/toString'
-import each from 'lodash/each'
-import pick from 'lodash/pick'
-
-import bModal from 'bootstrap-vue/es/components/modal/modal'
-
-import Response from '@/api/Response'
-import ContentPlaceholder from '@/components/ContentPlaceholder'
-import { removeDiacritics } from '@/utils/strings.js'
-import FacetSearch from '@/components/FacetSearch'
-
-const initialNumberOfFilesDisplayed = 5
+import get from 'lodash/get'
 
 export const mixin = {
   props: ['facet'],
-  components: {
-    ContentPlaceholder,
-    FacetSearch,
-    bModal
-  },
   data () {
     return {
-      facetQuery: '',
-      display: {
-        icon: 'angle-down',
-        label: 'More',
-        size: initialNumberOfFilesDisplayed
-      },
-      response: Response.none(),
-      collapseItems: false,
       isReady: false
     }
   },
-  created () {
-    this.aggregate()
-    // Watch change on the facet store the restart aggregation
-    this.$store.watch(this.watchedForUpdate, this.aggregate, { deep: true })
-  },
   computed: {
-    items () {
-      return this.response.get(`aggregations.${this.facet.key}.buckets`, [])
+    root () {
+      return get(this, '$refs.facet', {})
     },
-    filteredItems () {
-      return filter(this.items, item => {
-        return (filter(Object.keys(item), attribute => {
-          return includes(this.normalize(item[attribute]), this.normalize(this.facetQuery))
-        }).length > 0) || (this.facet.itemLabel && includes(this.normalize(this.facet.itemLabel(item)), this.normalize(this.facetQuery)))
-      }, this)
-    },
-    headerIcon () {
-      return this.collapseItems ? 'caret-right' : 'caret-down'
+    isGlobal () {
+      return this.$store.state.aggregation.globalSearch
     },
     facetFilter () {
       return this.$store.getters['search/findFacet'](this.facet.name)
@@ -64,46 +24,9 @@ export const mixin = {
           boxes: [[0, '70%'], ['20%', '10%']]
         }
       ]
-    },
-    isGlobal () {
-      return this.$store.state.aggregation.globalSearch
-    },
-    hasResults () {
-      return this.isReady && this.items.length > 0
-    },
-    noResults () {
-      return this.isReady && this.items.length === 0
-    },
-    noMatches () {
-      return this.isReady && this.filteredItems.length === 0
     }
   },
   methods: {
-    asyncFacetSearch () {
-      if (this.$refs.asyncFacetSearch) {
-        this.$refs.asyncFacetSearch.show()
-        // Only once the modal has been rendered
-        this.$nextTick(() => { this.facetQuery = '' })
-      }
-    },
-    aggregate () {
-      if (this.facet) {
-        this.isReady = false
-        this.response = Response.none()
-        return this.$store.dispatch('aggregation/query', { name: this.facet.name }).then(r => {
-          this.response = this.addInvertedFacets(r)
-          this.isReady = true
-        })
-      }
-    },
-    addInvertedFacets (response) {
-      if (!this.isGlobal && this.facetFilter && this.facetFilter.reverse) {
-        each(this.facetFilter.values, key => {
-          response.push(`aggregations.${this.facet.key}.buckets`, { key })
-        })
-      }
-      return response
-    },
     addValue (item) {
       this.$store.commit('search/addFacetValue', this.facet.itemParam(item))
       this.refreshRoute()
@@ -128,35 +51,11 @@ export const mixin = {
     isReversed () {
       return this.$store.getters['search/isFacetReversed'](this.facet.name)
     },
-    toggleItems () {
-      this.collapseItems = !this.collapseItems
-    },
-    watchedForUpdate (state) {
-      if (!state.aggregation.globalSearch) {
-        // This will allow to watch change on the search only when
-        // the aggregation is not global (ie. relative to the search).
-        return pick(state.search, ['query', 'facets'])
-      }
-    },
     refreshRoute () {
       this.$router.push({
         name: 'search',
         query: this.$store.getters['search/toRouteQuery']
       })
-    },
-    displayedFilteredItems () {
-      return slice(this.filteredItems, 0, this.display.size)
-    },
-    normalize (str) {
-      return removeDiacritics(toLower(toString(str)))
-    },
-    shouldDisplayShowMoreAction () {
-      return this.filteredItems.length > initialNumberOfFilesDisplayed
-    },
-    toogleDisplay () {
-      this.display.icon = this.display.icon === 'angle-down' ? 'angle-up' : 'angle-down'
-      this.display.label = this.display.label === 'More' ? 'Less' : 'More'
-      this.display.size = this.display.size === initialNumberOfFilesDisplayed ? this.filteredItems.length : initialNumberOfFilesDisplayed
     }
   }
 }
