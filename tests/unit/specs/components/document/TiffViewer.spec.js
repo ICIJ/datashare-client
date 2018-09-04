b12d5973509e3fe047ec49146c7ed4ef07d678f5
Bruno Thomas <bruno@barreverte.fr>
2018-09-04 13:59:45 +0000

    [test] migrate tiffviewer test with jest

--- tests/unit/specs/components/document/TiffViewer.spec.js
+++ tests/unit/specs/components/document/TiffViewer.spec.js
@@ -1,26 +1,40 @@
-import { mount } from '@vue/test-utils'
-
+import Vuex from 'vuex'
+import VueI18n from 'vue-i18n'
+import BootstrapVue from 'bootstrap-vue'
+import FontAwesomeIcon from '@/components/FontAwesomeIcon'
 import TiffViewer from '@/components/document/TiffViewer'
-import noop from 'lodash/noop'
+import { mount, createLocalVue } from '@vue/test-utils'
+import {createServer} from 'http-server'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+localVue.use(VueI18n)
+localVue.use(BootstrapVue)
+localVue.component('font-awesome-icon', FontAwesomeIcon)
 
 describe.skip('TiffViewer.vue', () => {
-  beforeEach(async () => {
-    TiffViewer.mounted = noop
+  let httpServer = null
+  beforeAll(() => {
+    httpServer = createServer({root: 'tests/unit'})
+    httpServer.listen(9876)
+  })
+  afterAll(() => {
+    httpServer.close()
   })
 
   it('should display error when tiff is not found', async () => {
-    var wrapped = mount(TiffViewer, {propsData: {'url': 'invalid.url'}})
+    var wrapped = mount(TiffViewer, {localVue, propsData: {'url': 'http://localhost:9876/invalid.url'}})
     await wrapped.vm.page(1)
-    wrapped.update()
+    await localVue.nextTick()
 
-    expect(wrapped.vm.$el.querySelector('.alert').textContent).to.contain('404 Not Found')
+    expect(wrapped.vm.$el.querySelector('.alert').textContent).toContain('404 Not Found')
   })
 
   it('should display canvas when tiff is found', async () => {
-    var wrapped = mount(TiffViewer, {propsData: {'url': 'base/resources/image.tiff'}})
+    var wrapped = mount(TiffViewer, {localVue, propsData: {'url': 'http://localhost:9876/resources/image.tiff'}})
     await wrapped.vm.page(1)
-    wrapped.update()
+    await localVue.nextTick()
 
-    expect(wrapped.vm.$el.querySelector('img.tiff-viewer__canvas')).to.not.equal(null)
+    expect(wrapped.vm.$el.querySelector('img.tiff-viewer__canvas')).not.toEqual(null)
   })
 })
