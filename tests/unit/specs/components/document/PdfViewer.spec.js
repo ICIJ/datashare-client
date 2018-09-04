917f846a56bdfa563140d35afbc59b3a6fd586ee
Bruno Thomas <bruno@barreverte.fr>
2018-09-04 14:11:42 +0000

    [test] pdfviewer green with http server

--- tests/unit/specs/components/document/PdfViewer.spec.js
+++ tests/unit/specs/components/document/PdfViewer.spec.js
@@ -1,30 +1,50 @@
-import { mount } from '@vue/test-utils'
+import Vuex from 'vuex'
+import VueI18n from 'vue-i18n'
+import BootstrapVue from 'bootstrap-vue'
+import FontAwesomeIcon from '@/components/FontAwesomeIcon'
+import { mount, createLocalVue } from '@vue/test-utils'
 import PdfViewer from '@/components/document/PdfViewer'
 import noop from 'lodash/noop'
 import PDFJS from 'pdfjs-dist'
+import {createServer} from 'http-server'
 
-describe.skip('PdfViewer.vue', () => {
+const localVue = createLocalVue()
+localVue.use(Vuex)
+localVue.use(VueI18n)
+localVue.use(BootstrapVue)
+localVue.component('font-awesome-icon', FontAwesomeIcon)
+
+describe('PdfViewer.vue', () => {
+  let httpServer = null
+  beforeAll(() => {
+    httpServer = createServer({root: 'tests/unit'})
+    httpServer.listen(9876)
+  })
+  afterAll(() => {
+    httpServer.close()
+  })
   beforeEach(async () => {
     PdfViewer.mounted = noop
   })
 
-  it('should display canvas when PDF is found', async () => {
-    var wrapped = mount(PdfViewer, {propsData: {'url': '/document.pdf'}})
+  it('should display canvas when PDF is not found', async () => {
+    var wrapped = mount(PdfViewer, {localVue, propsData: {'url': 'http://localhost:9876/missingDocument.pdf'}})
     await wrapped.vm.loadPage(1)
     expect(wrapped.vm.$el.querySelector('img.pdf-viewer__canvas')).toEqual(null)
+    expect(wrapped.vm.$el.querySelector('.alert').textContent).toContain('stream must have data')
   }, 10000)
 
   it('should cache pdf data', async () => {
     jest.spyOn(PDFJS, 'getDocument')
-    var wrapped = mount(PdfViewer, {propsData: {'url': '/document.pdf'}})
+    var wrapped = mount(PdfViewer, {localVue, propsData: {'url': 'http://localhost:9876/resources/document.pdf'}})
     await wrapped.vm.loadPage(1)
     await wrapped.vm.loadPage(2)
-    wrapped.update()
+    await localVue.nextTick()
     expect(PDFJS.getDocument).toHaveBeenCalled()
   }, 10000)
 
   it('should display pdf page', async () => {
-    var wrapped = mount(PdfViewer, {propsData: {'url': '/document.pdf'}})
+    var wrapped = mount(PdfViewer, {localVue, propsData: {'url': 'http://localhost:9876/resources/document.pdf'}})
     expect(wrapped.vm.page(1)).toBeUndefined(undefined)
     await wrapped.vm.loadPage(1)
     expect(wrapped.vm.page(1)).not.toBeUndefined(undefined)
