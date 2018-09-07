8b7a1af13e52804057abdf65cca68266fdfe9809
Anne L'Hôte <anne.lhote@gmail.com>
2018-09-07 16:37:55 +0200

    [issue] Change feedback while loading document, close #3

--- src/components/document/DocumentView.vue
+++ src/components/document/DocumentView.vue
@@ -1,119 +1,124 @@
 <template>
-  <div class="document" v-if="document">
-    <div class="document__header">
-      <h3>
-        <span>{{ document.basename }}</span>
-        <a class="btn btn-link float-right" :href="getFullUrl" target="_blank" title="Download source file">
-          <font-awesome-icon icon="download" />
-          <span class="sr-only">{{ $t('document.download_button') }}</span>
-        </a>
-      </h3>
-      <nav class="document__header__nav">
-        <ul class="list-inline">
-          <li class="document__header__nav__item list-inline-item">
-            <a @click="tab = 'details'" :class="{active: tab === 'details'}">
-              {{ $t('document.details') }}
-            </a>
-          </li>
-          <li class="document__header__nav__item list-inline-item">
-            <a @click="tab = 'named_entities'" :class="{active: tab === 'named_entities'}">
-              {{ $t('document.named_entities') }}
-            </a>
-          </li>
-          <li class="document__header__nav__item list-inline-item">
-            <a @click="tab = 'text'" :class="{active: tab === 'text'}">
-              {{ $t('document.extracted_text') }}
-            </a>
-          </li>
-          <li class="document__header__nav__item list-inline-item">
-            <a @click="tab = 'preview'" :class="{active: tab === 'preview'}">
-              {{ $t('document.preview') }}
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </div>
-    <div class="tab-content document__content">
-      <div class="tab-pane" v-bind:class="{active: tab === 'details'}">
-        <dl class="row">
-          <dt class="col-sm-3">{{ $t('document.name') }}</dt>
-          <dd class="col-sm-9">{{ document.basename }}</dd>
-          <dt class="col-sm-3">{{ $t('document.path') }}</dt>
-          <dd class="col-sm-9">{{ document.source.path }}</dd>
-          <dt class="col-sm-3">{{ $t('document.id') }}</dt>
-          <dd class="col-sm-9">{{ document.id }}</dd>
-          <template v-if="document.source.metadata.tika_metadata_creation_date">
-            <dt class="col-sm-3">{{ $t('document.creation_date') }}</dt>
-            <dd class="col-sm-9">{{ document.creationDate }}</dd>
-          </template>
-          <template v-if="document.source.contentLength !== -1">
-            <dt class="col-sm-3">{{ $t('document.size') }}</dt>
-            <dd class="col-sm-9">{{ document.humanSize }}</dd>
-          </template>
-          <template v-if="document.source.language !== 'UNKNOWN'">
-            <dt class="col-sm-3">{{ $t('document.content_language') }}</dt>
-            <dd class="col-sm-9">{{ document.source.language }}</dd>
-          </template>
-          <template v-if="document.source.contentType !== 'unknown'">
-            <dt class="col-sm-3">{{ $t('document.content_type') }}</dt>
-            <dd class="col-sm-9">{{ document.source.contentType }}</dd>
-          </template>
-          <template v-if="document.source.contentEncoding !== 'unknown'">
-            <dt class="col-sm-3">{{ $t('document.content_encoding') }}</dt>
-            <dd class="col-sm-9">{{ document.source.contentEncoding }}</dd>
-          </template>
-          <template v-if="document.source.extractionLevel > 0">
-            <dt class="col-sm-3">{{ $t('document.tree_level') }}</dt>
-            <dd class="col-sm-9">{{ document.source.extractionLevel }}</dd>
-          </template>
-          <template v-if="document.source.extractionLevel > 0 && parentDocument">
-            <dt class="col-sm-3">{{ $t('document.parent_document') }}</dt>
-            <dd class="col-sm-9">
-              <router-link :to="{ name: 'document', params: { id: document.source.parentDocument, routing: document.routing } }">
-                {{ parentDocument.basename }}
-              </router-link>
-            </dd>
-          </template>
-        </dl>
+  <div v-if="!isReady">
+    <content-placeholder class="list-group-item py-2 px-3" />
+  </div>
+  <div v-else>
+    <div class="document" v-if="document">
+      <div class="document__header">
+        <h3>
+          <span>{{ document.basename }}</span>
+          <a class="btn btn-link float-right" :href="getFullUrl" target="_blank" title="Download source file">
+            <font-awesome-icon icon="download" />
+            <span class="sr-only">{{ $t('document.download_button') }}</span>
+          </a>
+        </h3>
+        <nav class="document__header__nav">
+          <ul class="list-inline">
+            <li class="document__header__nav__item list-inline-item">
+              <a @click="tab = 'details'" :class="{active: tab === 'details'}">
+                {{ $t('document.details') }}
+              </a>
+            </li>
+            <li class="document__header__nav__item list-inline-item">
+              <a @click="tab = 'named_entities'" :class="{active: tab === 'named_entities'}">
+                {{ $t('document.named_entities') }}
+              </a>
+            </li>
+            <li class="document__header__nav__item list-inline-item">
+              <a @click="tab = 'text'" :class="{active: tab === 'text'}">
+                {{ $t('document.extracted_text') }}
+              </a>
+            </li>
+            <li class="document__header__nav__item list-inline-item">
+              <a @click="tab = 'preview'" :class="{active: tab === 'preview'}">
+                {{ $t('document.preview') }}
+              </a>
+            </li>
+          </ul>
+        </nav>
       </div>
-      <div class="tab-pane document__named-entities" v-bind:class="{active: tab === 'named_entities'}">
-        <div v-for="(results, index) in groupByCategories(namedEntities)" :key="index" class="mb-4">
-          <div class="mb-2" :class="getCategoryClass(results[0].source.category, 'text-')">
-            <font-awesome-icon :icon="getCategoryIcon(results[0].source.category)" />
-            {{ capitalize(results[0].source.category) }} <i>({{ results.length }})</i>
-          </div>
-          <span v-for="(result, index) in groupByMentionNorm(results)" :key="index" class="d-inline mr-2">
-            <span class="badge badge-pill p-0 badge-light text-uppercase text-black border" :class="getCategoryClass(result[0].source.category, 'border-')">
-              <span class="p-1 d-inline-block" :title="capitalize(result[0].source.mentionNorm)"  v-b-tooltip.hover>
-                {{ result[0].source.mentionNorm }}
-              </span>
-              <span class="bg-darkest text-light p-1 px-2 d-inline-block" :title="$tc('aggregations.mentions.occurrence', result.length, { count: result.length })" :class="getCategoryClass(result[0].source.category, 'bg-')" v-b-tooltip.hover>
-                {{ result.length }}
+      <div class="tab-content document__content">
+        <div class="tab-pane" v-bind:class="{active: tab === 'details'}">
+          <dl class="row">
+            <dt class="col-sm-3">{{ $t('document.name') }}</dt>
+            <dd class="col-sm-9">{{ document.basename }}</dd>
+            <dt class="col-sm-3">{{ $t('document.path') }}</dt>
+            <dd class="col-sm-9">{{ document.source.path }}</dd>
+            <dt class="col-sm-3">{{ $t('document.id') }}</dt>
+            <dd class="col-sm-9">{{ document.id }}</dd>
+            <template v-if="document.source.metadata.tika_metadata_creation_date">
+              <dt class="col-sm-3">{{ $t('document.creation_date') }}</dt>
+              <dd class="col-sm-9">{{ document.creationDate }}</dd>
+            </template>
+            <template v-if="document.source.contentLength !== -1">
+              <dt class="col-sm-3">{{ $t('document.size') }}</dt>
+              <dd class="col-sm-9">{{ document.humanSize }}</dd>
+            </template>
+            <template v-if="document.source.language !== 'UNKNOWN'">
+              <dt class="col-sm-3">{{ $t('document.content_language') }}</dt>
+              <dd class="col-sm-9">{{ document.source.language }}</dd>
+            </template>
+            <template v-if="document.source.contentType !== 'unknown'">
+              <dt class="col-sm-3">{{ $t('document.content_type') }}</dt>
+              <dd class="col-sm-9">{{ document.source.contentType }}</dd>
+            </template>
+            <template v-if="document.source.contentEncoding !== 'unknown'">
+              <dt class="col-sm-3">{{ $t('document.content_encoding') }}</dt>
+              <dd class="col-sm-9">{{ document.source.contentEncoding }}</dd>
+            </template>
+            <template v-if="document.source.extractionLevel > 0">
+              <dt class="col-sm-3">{{ $t('document.tree_level') }}</dt>
+              <dd class="col-sm-9">{{ document.source.extractionLevel }}</dd>
+            </template>
+            <template v-if="document.source.extractionLevel > 0 && parentDocument">
+              <dt class="col-sm-3">{{ $t('document.parent_document') }}</dt>
+              <dd class="col-sm-9">
+                <router-link :to="{ name: 'document', params: { id: document.source.parentDocument, routing: document.routing } }">
+                  {{ parentDocument.basename }}
+                </router-link>
+              </dd>
+            </template>
+          </dl>
+        </div>
+        <div class="tab-pane document__named-entities" v-bind:class="{active: tab === 'named_entities'}">
+          <div v-for="(results, index) in groupByCategories(namedEntities)" :key="index" class="mb-4">
+            <div class="mb-2" :class="getCategoryClass(results[0].source.category, 'text-')">
+              <font-awesome-icon :icon="getCategoryIcon(results[0].source.category)" />
+              {{ capitalize(results[0].source.category) }} <i>({{ results.length }})</i>
+            </div>
+            <span v-for="(result, index) in groupByMentionNorm(results)" :key="index" class="d-inline mr-2">
+              <span class="badge badge-pill p-0 badge-light text-uppercase text-black border" :class="getCategoryClass(result[0].source.category, 'border-')">
+                <span class="p-1 d-inline-block" :title="capitalize(result[0].source.mentionNorm)"  v-b-tooltip.hover>
+                  {{ result[0].source.mentionNorm }}
+                </span>
+                <span class="bg-darkest text-light p-1 px-2 d-inline-block" :title="$tc('aggregations.mentions.occurrence', result.length, { count: result.length })" :class="getCategoryClass(result[0].source.category, 'bg-')" v-b-tooltip.hover>
+                  {{ result.length }}
+                </span>
               </span>
             </span>
-          </span>
+          </div>
+        </div>
+        <div class="tab-pane text-pre-wrap" v-bind:class="{active: tab === 'text'}" v-html="markedSourceContent()"></div>
+        <div class="tab-pane" v-bind:class="{active: tab === 'preview'}">
+          <template v-if="document.contentType === 'application/pdf'">
+            <pdf-viewer :url="document.relativePath" />
+          </template>
+          <template v-else-if="document.contentType === 'image/tiff'">
+            <tiff-viewer :url="document.relativePath" />
+          </template>
+          <template v-else-if="document.contentType === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' || document.contentType === 'text/csv'">
+            <spreadsheet-viewer :url="document.relativePath" :type="document.contentType"/>
+          </template>
+          <template v-else>
+            Not available
+          </template>
         </div>
       </div>
-      <div class="tab-pane text-pre-wrap" v-bind:class="{active: tab === 'text'}" v-html="markedSourceContent()"></div>
-      <div class="tab-pane" v-bind:class="{active: tab === 'preview'}">
-        <template v-if="document.contentType === 'application/pdf'">
-          <pdf-viewer :url="document.relativePath" />
-        </template>
-        <template v-else-if="document.contentType === 'image/tiff'">
-          <tiff-viewer :url="document.relativePath" />
-        </template>
-        <template v-else-if="document.contentType === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' || document.contentType === 'text/csv'">
-          <spreadsheet-viewer :url="document.relativePath" :type="document.contentType"/>
-        </template>
-        <template v-else>
-          Not available
-        </template>
-      </div>
     </div>
-  </div>
-  <div v-else class="nodocument">
-    <font-awesome-icon icon="exclamation-triangle" />
-    <span>{{ $t('document.not_found') }}</span>
+    <div v-else class="nodocument">
+      <font-awesome-icon icon="exclamation-triangle" />
+      <span>{{ $t('document.not_found') }}</span>
+    </div>
   </div>
 </template>
 
@@ -129,11 +134,13 @@ import PdfViewer from './PdfViewer'
 import sortedUniqBy from 'lodash/sortedUniqBy'
 import SpreadsheetViewer from './SpreadsheetViewer'
 import TiffViewer from './TiffViewer'
+import ContentPlaceholder from '@/components/ContentPlaceholder'
 
 export default {
   name: 'document-view',
   mixins: [ner],
   components: {
+    ContentPlaceholder,
     PdfViewer,
     SpreadsheetViewer,
     TiffViewer
@@ -141,12 +148,16 @@ export default {
   props: ['id', 'routing'],
   data () {
     return {
-      tab: 'details'
+      tab: 'details',
+      isReady: false
     }
   },
   methods: {
     getDoc (params = { id: this.id, routing: this.routing }) {
-      return this.$store.dispatch('document/get', params).then(() => this.$store.dispatch('document/getParent')).then(() => this.$store.dispatch('document/getNamedEntities'))
+      this.isReady = false
+      return this.$store.dispatch('document/get', params).then(() => this.$store.dispatch('document/getParent')).then(() => this.$store.dispatch('document/getNamedEntities')).then(() => {
+        this.isReady = true
+      })
     },
     groupByCategories (array) {
       return orderBy(groupBy(array, m => m.source.category), ['length', m => m[0].source.category], ['desc', 'asc'])
