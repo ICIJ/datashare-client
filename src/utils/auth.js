7d11d08b24ca31ee31c1eb8a4056d0ff3a8a0d32
Anne L'Hôte <anne.lhote@gmail.com>
2019-01-17 17:22:26 +0000

    [refactor) simplifies auth.js

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,14 +1,10 @@
 import { getCookie } from 'tiny-cookie'
 import get from 'lodash/get'
 
-export const isAuthenticated = function () {
+export function isAuthenticated () {
   // Skip authentication in development
-  if (process.env.NODE_ENV === 'development') {
-    return true
-  }
+  if (process.env.NODE_ENV === 'development') return true
   // Find the cookie created by the backend
-  const cookieName = process.env.VUE_APP_DS_COOKIE_NAME
-  const cookie = getCookie(cookieName, JSON.parse)
-
+  const cookie = getCookie(process.env.VUE_APP_DS_COOKIE_NAME, JSON.parse)
   return get(cookie, 'login', null) !== null
 }
---
+++ src/utils/user.js
@@ -0,0 +1,14 @@
+import { getCookie } from 'tiny-cookie'
+import get from 'lodash/get'
+
+export const getOS = function () {
+  // Skip authentication in development
+  if (process.env.NODE_ENV === 'development') {
+    return true
+  }
+  // Find the cookie created by the backend
+  const cookieName = process.env.VUE_APP_DS_COOKIE_NAME
+  const cookie = getCookie(cookieName, JSON.parse)
+
+  return get(cookie, 'login', null) !== null
+}
