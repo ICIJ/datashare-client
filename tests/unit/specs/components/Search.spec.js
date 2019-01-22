65e53028b311921da252f46f55d065be1e766ed0
Anne L'Hôte <anne.lhote@gmail.com>
2019-01-22 13:57:12 +0000

    [refactor][ci]

--- tests/unit/specs/components/Search.spec.js
+++ tests/unit/specs/components/Search.spec.js
@@ -4,20 +4,17 @@ import VueI18n from 'vue-i18n'
 import VueProgressBar from 'vue-progressbar'
 import BootstrapVue from 'bootstrap-vue'
 import { mount, createLocalVue } from '@vue/test-utils'
-
 import noop from 'lodash/noop'
-import trim from 'lodash/trim'
-
 import esConnectionHelper from 'tests/unit/specs/utils/esConnectionHelper'
 import messages from '@/messages'
 import router from '@/router'
 import store from '@/store'
-
 import FontAwesomeIcon from '@/components/FontAwesomeIcon'
 import Search from '@/components/Search'
 import { IndexedDocuments, IndexedDocument, letData } from 'tests/unit/es_utils'
-import fetchPonyfill from 'fetch-ponyfill'
 import DatashareClient from '@/api/DatashareClient'
+import fetchPonyfill from 'fetch-ponyfill'
+const { Response } = fetchPonyfill()
 
 const localVue = createLocalVue()
 localVue.use(VueI18n)
@@ -26,7 +23,6 @@ localVue.use(VueProgressBar, { color: '#852308' })
 localVue.use(BootstrapVue)
 localVue.component('font-awesome-icon', FontAwesomeIcon)
 const i18n = new VueI18n({ locale: 'en', messages })
-const { Response } = fetchPonyfill()
 
 jest.mock('@/api/DatashareClient', () => jest.fn())
 DatashareClient.mockImplementation(() => {
@@ -38,10 +34,10 @@ DatashareClient.mockImplementation(() => {
   }
 })
 
-describe('Search.vue', function () {
+describe('Search.vue', () => {
   esConnectionHelper()
-  var es = esConnectionHelper.es
-  var wrapped = null
+  const es = esConnectionHelper.es
+  let wrapper
   // High timeout because multiple searches can be heavy for the Elasticsearch
   jest.setTimeout(1e4)
 
@@ -53,138 +49,138 @@ describe('Search.vue', function () {
   beforeEach(async () => {
     Search.created = noop
     Vue.prototype.config = { dataDir: '/home/user/data' }
-    wrapped = mount(Search, { localVue, i18n, router, store })
+    wrapper = mount(Search, { localVue, i18n, router, store })
     store.commit('search/clear')
   })
 
   it('should display no documents found', async () => {
-    await wrapped.vm.search('foo')
-    await wrapped.vm.$nextTick()
+    await wrapper.vm.search('foo')
+    await wrapper.vm.$nextTick()
 
-    expect(trim(wrapped.vm.$el.querySelector('.search-results__header__number-of-results').textContent)).toEqual('No documents found.')
+    expect(wrapper.find('.search-results__header__number-of-results').text()).toEqual('No documents found.')
   })
 
   it('should display one document found', async () => {
     await letData(es).have(new IndexedDocument('docs/bar.txt').withContent('this is bar document')).commit()
 
-    await wrapped.vm.search('bar')
-    await wrapped.vm.$nextTick()
+    await wrapper.vm.search('bar')
+    await wrapper.vm.$nextTick()
 
-    expect(trim(wrapped.vm.$el.querySelector('.search-results__header__progress__pagination').textContent)).toEqual('1 - 1')
-    expect(trim(wrapped.vm.$el.querySelector('.search-results__header__progress_number-of-results').textContent)).toEqual('on 1 document found')
-    expect(trim(wrapped.vm.$el.querySelector('.search-results-item__fragments').innerHTML)).toEqual('this is <mark>bar</mark> document')
+    expect(wrapper.find('.search-results__header__progress__pagination').text()).toEqual('1 - 1')
+    expect(wrapper.find('.search-results__header__progress_number-of-results').text()).toEqual('on 1 document found')
+    expect(wrapper.find('.search-results-item__fragments').html()).toEqual('<div class="search-results-item__fragments">this is <mark>bar</mark> document</div>')
   })
 
   it('should display 2 documents found', async () => {
     await letData(es).have(new IndexedDocument('docs/bar1.txt').withContent('this is bar 1 document')).commit()
     await letData(es).have(new IndexedDocument('docs/bar2.txt').withContent('this is bar 2 document')).commit()
 
-    await wrapped.vm.search('bar')
-    await wrapped.vm.$nextTick()
+    await wrapper.vm.search('bar')
+    await wrapper.vm.$nextTick()
 
-    expect(trim(wrapped.vm.$el.querySelector('.search-results__header__progress__pagination').textContent)).toEqual('1 - 2')
-    expect(trim(wrapped.vm.$el.querySelector('.search-results__header__progress_number-of-results').textContent)).toEqual('on 2 documents found')
-    expect(wrapped.vm.$el.querySelectorAll('.search-results-item').length).toEqual(2)
+    expect(wrapper.find('.search-results__header__progress__pagination').text()).toEqual('1 - 2')
+    expect(wrapper.find('.search-results__header__progress_number-of-results').text()).toEqual('on 2 documents found')
+    expect(wrapper.findAll('.search-results-item').length).toEqual(2)
   })
 
   it('should make a link without routing for a document', async () => {
     await letData(es).have(new IndexedDocument('doc.txt').withContent('this is a document')).commit()
 
-    await wrapped.vm.search('document')
-    await wrapped.vm.$nextTick()
+    await wrapper.vm.search('document')
+    await wrapper.vm.$nextTick()
 
-    expect(wrapped.vm.$el.querySelector('.search-results-item__basename a').href).toMatch(/doc.txt$/)
+    expect(wrapper.find('.search-results-item__basename a').attributes().href).toMatch(/doc.txt$/)
   })
 
   it('should make a link with routing for a child document', async () => {
     await letData(es).have(new IndexedDocument('parent.txt').withContent('this is a parent document')).commit()
     await letData(es).have(new IndexedDocument('child.txt').withContent('this is a children document').withParent('parent.txt')).commit()
 
-    await wrapped.vm.search('children')
-    await wrapped.vm.$nextTick()
+    await wrapper.vm.search('children')
+    await wrapper.vm.$nextTick()
 
-    expect(wrapped.vm.$el.querySelector('.search-results-item__basename a').href).toMatch(/child.txt\/parent.txt/)
+    expect(wrapper.find('.search-results-item__basename a').attributes().href).toMatch(/child.txt\/parent.txt/)
   })
 
   it('should return 2 documents', async () => {
     await letData(es).have(new IndexedDocuments().setBaseName('doc').withContent('this is a document').count(4)).commit()
 
-    await wrapped.vm.search({ query: 'document', from: 0, size: 2 })
-    await wrapped.vm.$nextTick()
+    await wrapper.vm.search({ query: 'document', from: 0, size: 2 })
+    await wrapper.vm.$nextTick()
 
-    expect(wrapped.vm.$el.querySelectorAll('.search-results-item').length).toEqual(2)
+    expect(wrapper.findAll('.search-results-item').length).toEqual(2)
   })
 
   it('should return 3 documents', async () => {
     await letData(es).have(new IndexedDocuments().setBaseName('doc').withContent('this is a document').count(4)).commit()
 
-    await wrapped.vm.search({ query: 'document', from: 0, size: 3 })
-    await wrapped.vm.$nextTick()
+    await wrapper.vm.search({ query: 'document', from: 0, size: 3 })
+    await wrapper.vm.$nextTick()
 
-    expect(wrapped.vm.$el.querySelectorAll('.search-results-item').length).toEqual(3)
+    expect(wrapper.findAll('.search-results-item').length).toEqual(3)
   })
 
   it('should not display the pagination (1/2)', async () => {
-    await wrapped.vm.search('foo')
-    await wrapped.vm.$nextTick()
+    await wrapper.vm.search('foo')
+    await wrapper.vm.$nextTick()
 
-    expect(wrapped.vm.$el.querySelectorAll('.search-results__header__first-page').length).toEqual(0)
-    expect(wrapped.vm.$el.querySelectorAll('.search-results__header__previous-page').length).toEqual(0)
-    expect(wrapped.vm.$el.querySelectorAll('.search-results__header__next-page').length).toEqual(0)
-    expect(wrapped.vm.$el.querySelectorAll('.search-results__header__last-page').length).toEqual(0)
+    expect(wrapper.findAll('.search-results__header__first-page').length).toEqual(0)
+    expect(wrapper.findAll('.search-results__header__previous-page').length).toEqual(0)
+    expect(wrapper.findAll('.search-results__header__next-page').length).toEqual(0)
+    expect(wrapper.findAll('.search-results__header__last-page').length).toEqual(0)
   })
 
   it('should not display the pagination (2/2)', async () => {
     await letData(es).have(new IndexedDocument('doc_01.txt').withContent('this is the first document')).commit()
 
-    await wrapped.vm.search('document')
-    await wrapped.vm.$nextTick()
+    await wrapper.vm.search('document')
+    await wrapper.vm.$nextTick()
 
-    expect(wrapped.vm.$el.querySelectorAll('.search-results__header__first-page').length).toEqual(0)
-    expect(wrapped.vm.$el.querySelectorAll('.search-results__header__previous-page').length).toEqual(0)
-    expect(wrapped.vm.$el.querySelectorAll('.search-results__header__next-page').length).toEqual(0)
-    expect(wrapped.vm.$el.querySelectorAll('.search-results__header__last-page').length).toEqual(0)
+    expect(wrapper.findAll('.search-results__header__first-page').length).toEqual(0)
+    expect(wrapper.findAll('.search-results__header__previous-page').length).toEqual(0)
+    expect(wrapper.findAll('.search-results__header__next-page').length).toEqual(0)
+    expect(wrapper.findAll('.search-results__header__last-page').length).toEqual(0)
   })
 
   it('should display the pagination', async () => {
     await letData(es).have(new IndexedDocuments().setBaseName('doc').withContent('this is a document').count(4)).commit()
 
-    await wrapped.vm.search({ query: 'document', from: 0, size: 3 })
-    await wrapped.vm.$nextTick()
+    await wrapper.vm.search({ query: 'document', from: 0, size: 3 })
+    await wrapper.vm.$nextTick()
 
-    expect(wrapped.vm.$el.querySelectorAll('.search-results__header__first-page').length).toEqual(2)
-    expect(wrapped.vm.$el.querySelectorAll('.search-results__header__previous-page').length).toEqual(2)
-    expect(wrapped.vm.$el.querySelectorAll('.search-results__header__next-page').length).toEqual(2)
-    expect(wrapped.vm.$el.querySelectorAll('.search-results__header__last-page').length).toEqual(2)
-    expect(trim(wrapped.vm.$el.querySelector('.search-results__header__progress__pagination').textContent)).toEqual('1 - 3')
-    expect(trim(wrapped.vm.$el.querySelector('.search-results__header__progress_number-of-results').textContent)).toEqual('on 4 documents found')
-    expect(wrapped.vm.$el.querySelectorAll('.search-results-item').length).toEqual(3)
+    expect(wrapper.findAll('.search-results__header__first-page').length).toEqual(2)
+    expect(wrapper.findAll('.search-results__header__previous-page').length).toEqual(2)
+    expect(wrapper.findAll('.search-results__header__next-page').length).toEqual(2)
+    expect(wrapper.findAll('.search-results__header__last-page').length).toEqual(2)
+    expect(wrapper.find('.search-results__header__progress__pagination').text()).toEqual('1 - 3')
+    expect(wrapper.find('.search-results__header__progress_number-of-results').text()).toEqual('on 4 documents found')
+    expect(wrapper.findAll('.search-results-item').length).toEqual(3)
   })
 
   it('should display the first and the previous page as unavailable', async () => {
     await letData(es).have(new IndexedDocuments().setBaseName('doc').withContent('this is a document').count(4)).commit()
 
-    await wrapped.vm.search({ query: 'document', from: 0, size: 3 })
-    await wrapped.vm.$nextTick()
+    await wrapper.vm.search({ query: 'document', from: 0, size: 3 })
+    await wrapper.vm.$nextTick()
 
-    expect(wrapped.vm.$el.querySelectorAll('.search-results__header__first-page.disabled').length).toEqual(2)
-    expect(wrapped.vm.$el.querySelectorAll('.search-results__header__previous-page.disabled').length).toEqual(2)
-    expect(wrapped.vm.$el.querySelectorAll('.search-results__header__next-page.disabled').length).toEqual(0)
-    expect(wrapped.vm.$el.querySelectorAll('.search-results__header__last-page.disabled').length).toEqual(0)
+    expect(wrapper.findAll('.search-results__header__first-page.disabled').length).toEqual(2)
+    expect(wrapper.findAll('.search-results__header__previous-page.disabled').length).toEqual(2)
+    expect(wrapper.findAll('.search-results__header__next-page.disabled').length).toEqual(0)
+    expect(wrapper.findAll('.search-results__header__last-page.disabled').length).toEqual(0)
   })
 
   it('should display the next and the last page as unavailable', async () => {
     await letData(es).have(new IndexedDocuments().setBaseName('doc').withContent('this is a document').count(4)).commit()
 
-    await wrapped.vm.search({ query: 'document', from: 3, size: 3 })
-    await wrapped.vm.$nextTick()
+    await wrapper.vm.search({ query: 'document', from: 3, size: 3 })
+    await wrapper.vm.$nextTick()
 
-    expect(wrapped.vm.$el.querySelectorAll('.search-results__header__first-page.disabled').length).toEqual(0)
-    expect(wrapped.vm.$el.querySelectorAll('.search-results__header__previous-page.disabled').length).toEqual(0)
-    expect(wrapped.vm.$el.querySelectorAll('.search-results__header__next-page.disabled').length).toEqual(2)
-    expect(wrapped.vm.$el.querySelectorAll('.search-results__header__last-page.disabled').length).toEqual(2)
-    expect(trim(wrapped.vm.$el.querySelector('.search-results__header__progress__pagination').textContent)).toEqual('4 - 4')
-    expect(trim(wrapped.vm.$el.querySelector('.search-results__header__progress_number-of-results').textContent)).toEqual('on 4 documents found')
-    expect(wrapped.vm.$el.querySelectorAll('.search-results-item').length).toEqual(1)
+    expect(wrapper.findAll('.search-results__header__first-page.disabled').length).toEqual(0)
+    expect(wrapper.findAll('.search-results__header__previous-page.disabled').length).toEqual(0)
+    expect(wrapper.findAll('.search-results__header__next-page.disabled').length).toEqual(2)
+    expect(wrapper.findAll('.search-results__header__last-page.disabled').length).toEqual(2)
+    expect(wrapper.find('.search-results__header__progress__pagination').text()).toEqual('4 - 4')
+    expect(wrapper.find('.search-results__header__progress_number-of-results').text()).toEqual('on 4 documents found')
+    expect(wrapper.findAll('.search-results-item').length).toEqual(1)
   })
 })
--- tests/unit/specs/components/SearchSettings.spec.js
+++ tests/unit/specs/components/SearchSettings.spec.js
@@ -1,80 +1,67 @@
-import Vuex from 'vuex'
+import Vue from 'vue'
 import VueI18n from 'vue-i18n'
-import VueProgressBar from 'vue-progressbar'
-import BootstrapVue from 'bootstrap-vue'
-import { mount, createLocalVue } from '@vue/test-utils'
-
+import { createLocalVue, shallowMount } from '@vue/test-utils'
 import esConnectionHelper from 'tests/unit/specs/utils/esConnectionHelper'
 import messages from '@/messages'
 import router from '@/router'
 import store from '@/store'
-
-import FontAwesomeIcon from '@/components/FontAwesomeIcon'
 import SearchSettings from '@/components/SearchSettings'
+import vBTooltip from 'bootstrap-vue/es/components/tooltip/tooltip'
 
 const localVue = createLocalVue()
 localVue.use(VueI18n)
-localVue.use(Vuex)
-localVue.use(VueProgressBar, { color: '#852308' })
-localVue.use(BootstrapVue)
-localVue.component('font-awesome-icon', FontAwesomeIcon)
-
+localVue.directive('b-tooltip', vBTooltip)
+Vue.config.ignoredElements = ['font-awesome-icon']
 const i18n = new VueI18n({ locale: 'en', messages })
 
-describe('SearchSettings.vue', function () {
+describe('SearchSettings.vue', () => {
   esConnectionHelper()
-  let wrapped = null
+  let wrapper
   // High timeout because multiple searches can be heavy for the Elasticsearch
   jest.setTimeout(1e4)
 
-  beforeAll(() => {
-    // Remove all facets to avoid unecessary request
-    store.commit('search/clear')
-    let settingsButton = document.createElement('button')
-    settingsButton.setAttribute('id', 'toggleSettings')
-    document.body.appendChild(settingsButton)
+  beforeEach(() => {
+    wrapper = shallowMount(SearchSettings, { localVue, i18n, store })
+    store.commit('search/reset')
   })
 
   afterAll(() => {
-    // And restore all facets!
     store.commit('search/reset')
   })
 
   it('should display the dropdown to choose the number of results per page', async () => {
     await store.commit('search/size', 10)
-    wrapped = mount(SearchSettings, {localVue, i18n, router, store})
+    wrapper = shallowMount(SearchSettings, { localVue, i18n, store })
 
-    let e = wrapped.vm.$el.querySelector('#input-page-size')
+    let e = wrapper.vm.$el.querySelector('#input-page-size')
     expect(e.options[e.selectedIndex].value).toEqual('10')
   })
 
   it('should display the dropdown to choose the order', async () => {
     await store.commit('search/sort', 'dateOldest')
-    wrapped = mount(SearchSettings, {localVue, i18n, router, store})
+    wrapper = shallowMount(SearchSettings, { localVue, i18n, store })
 
-    let e = wrapped.vm.$el.querySelector('#input-sort')
+    let e = wrapper.vm.$el.querySelector('#input-sort')
     expect(e.options[e.selectedIndex].value).toEqual('dateOldest')
   })
 
   it('should have an input unchecked for globalSearch', async () => {
-    wrapped = mount(SearchSettings, {localVue, i18n, router, store})
-    wrapped.vm.$store.commit('search/reset')
-    await wrapped.vm.$nextTick()
-    expect(wrapped.vm.$el.querySelector('#input-global').checked).toEqual(true)
+    await wrapper.vm.$nextTick()
+    expect(wrapper.vm.$el.querySelector('#input-global').checked).toEqual(true)
   })
 
   it('should have an input checked for globalSearch', async () => {
-    wrapped = mount(SearchSettings, {localVue, i18n, router, store})
-    wrapped.vm.$store.commit('search/setGlobalSearch', true)
-    await wrapped.vm.$nextTick()
-    expect(wrapped.vm.$el.querySelector('#input-global').checked).toEqual(true)
+    wrapper.vm.$store.commit('search/setGlobalSearch', true)
+    await wrapper.vm.$nextTick()
+    expect(wrapper.vm.$el.querySelector('#input-global').checked).toEqual(true)
   })
 
   it('should call router push on facets reset', async () => {
     jest.spyOn(router, 'push')
-    wrapped = mount(SearchSettings, { localVue, i18n, router, store })
+    wrapper = shallowMount(SearchSettings, { localVue, i18n, router, store })
+
     expect(router.push).not.toHaveBeenCalled()
-    wrapped.vm.resetFacets()
+    wrapper.vm.resetFacets()
     expect(router.push).toHaveBeenCalled()
   })
 })
