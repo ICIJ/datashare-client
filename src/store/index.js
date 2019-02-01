1ccda97424dede94958e1d63aa2e0db7eb6a24b8
Pierre Romera <hello@pirhoo.com>
2019-02-01 15:25:47 +0100

    Remove permanent state on search store

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import createPersistedState from 'vuex-persistedstate'
 
 import document from './modules/document'
 import search from './modules/search'
@@ -10,14 +9,5 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: { document, search, indexing },
-  plugins: [
-    createPersistedState({
-      paths: ['search'],
-      filter: (mutation) => {
-        // Only for search mutations
-        return mutation.type.indexOf('search/') === 0
-      }
-    })
-  ],
   strict: process.env.NODE_ENV !== 'production'
 })
