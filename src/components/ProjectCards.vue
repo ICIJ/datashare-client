e97619cbc1112386262959dca896c3ec3ebe676b
Pierre Romera <hello@pirhoo.com>
2019-03-22 23:49:59 +0100

    Fixed col

--- src/components/ProjectCards.vue
+++ src/components/ProjectCards.vue
@@ -1,32 +1,32 @@
 <template>
-  <div class="project-cards d-flex flex-wrap justify-content-start">
-    <router-link :to="{ name: 'search', query: { index, q: '*' } }" class="project-cards__item d-flex" v-for="(index, i) in indices" :key="i">
-      <div class="project-cards__item__header py-2 px-3 bg-white text-secondary">
-        <fa icon="database" />
+  <div class="project-cards container-fluid p-0">
+    <div class="row">
+      <div class="col-4 mb-4"  v-for="(index, i) in indices" :key="i">
+        <router-link :to="{ name: 'search', query: { index, q: '*' } }" class="project-cards__item d-flex justify-content-start text-nowrap">
+          <div class="project-cards__item__header py-2 px-3 bg-white text-secondary">
+            <fa icon="database" />
+          </div>
+          <div class="project-cards__item__body py-2 px-3 font-weight-bold">
+            {{ index.split('-').map(capitalize).join(' ') }}
+          </div>
+        </router-link>
       </div>
-      <div class="project-cards__item__body py-2 px-3 font-weight-bold">
-        {{ index.split('-').map(capitalize).join(' ') }}
-      </div>
-    </router-link>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
   .project-cards {
-    margin-top: $spacer;
-    margin-left: -$spacer;
 
     &__item {
-      width: 100% / 3%;
-      min-width: 200px;
+      display: block;
       color: white;
       background: $primary;
       border: darken($primary, 10%) 1px solid;
-      margin: 0 0 $spacer $spacer;
 
       &:hover {
-          background: lighten($primary, 5);
-          color: white;
+        background: darken($primary, 5);
+        color: white;
       }
     }
   }
