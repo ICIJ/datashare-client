fadab27ad7a219fc777f522ab734a98b3f445e2e
Pierre Romera <hello@pirhoo.com>
2019-04-26 16:24:39 +0200

    Fixed size test

--- tests/unit/specs/components/SearchSettings.spec.js
+++ tests/unit/specs/components/SearchSettings.spec.js
@@ -50,7 +50,7 @@ describe('SearchSettings.vue', () => {
     jest.spyOn(router, 'push')
     wrapper = shallowMount(SearchSettings, { localVue, i18n, store, router })
     const rootWrapper = createWrapper(wrapper.vm.$root)
-    wrapper.findAll('#input-page-size option').at(0).setSelected()
+    wrapper.findAll('#input-page-size option').at(3).setSelected()
 
     expect(wrapper.vm.selectedSize).toEqual('100')
     expect(router.push).toHaveBeenCalled()
