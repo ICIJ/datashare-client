2a9a2b44f545857783b76599ae0ca724135b624e
Anne L'Hôte <anne.lhote@gmail.com>
2019-05-15 10:31:33 +0000

    [fix] corrects the check of the FacetDate constructor name
    See ICIJ/datashare#140

--- src/components/SearchResultsAppliedFilters.vue
+++ src/components/SearchResultsAppliedFilters.vue
@@ -6,6 +6,7 @@
 
 <script>
 import SearchResultsAppliedFilter from '@/components/SearchResultsAppliedFilter'
+import { FacetDate } from '@/store/facetsStore'
 import concat from 'lodash/concat'
 import map from 'lodash/map'
 
@@ -24,7 +25,7 @@ export default {
         map(facet.values, value => {
           let label = facet.itemLabel ? facet.itemLabel({ key: value, key_as_string: value }) : value
           label = this.$te(label) ? this.$t(label) : label
-          if (facet.constructor.name === 'FacetDate') {
+          if (facet.constructor.name === new FacetDate().constructor.name) {
             const date = new Date(parseInt(label))
             label = date.getFullYear() + '-' + ('0' + (date.getMonth() + 1)).slice(-2)
           }
