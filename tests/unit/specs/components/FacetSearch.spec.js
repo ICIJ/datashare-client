0b74a2915c1156090428c0fc9abaafcf16d204e6
Anne L'Hôte <anne.lhote@gmail.com>
2019-05-29 14:45:12 +0000

    [refactor]

--- tests/unit/specs/components/FacetSearch.spec.js
+++ tests/unit/specs/components/FacetSearch.spec.js
@@ -39,161 +39,192 @@ describe('FacetSearch.vue', () => {
     wrapper = mount(FacetSearch, { localVue, i18n, store, router, propsData: { infiniteScroll: false, throttle: 0, facet } })
   })
 
-  it('should display 2 items', async () => {
-    await letData(es).have(new IndexedDocument('index.js').withContentType('text/javascript')).commit()
-    await letData(es).have(new IndexedDocument('list.js').withContentType('text/javascript')).commit()
-    await letData(es).have(new IndexedDocument('show.js').withContentType('text/javascript')).commit()
-    await letData(es).have(new IndexedDocument('index.html').withContentType('text/html')).commit()
-    await letData(es).have(new IndexedDocument('list.html').withContentType('text/html')).commit()
+  describe('pagination', () => {
+    it('should display 2 items', async () => {
+      await letData(es).have(new IndexedDocument('doc_01')
+        .withContentType('type_01')).commit()
+      await letData(es).have(new IndexedDocument('doc_02')
+        .withContentType('type_02')).commit()
 
-    await wrapper.vm.startOver()
+      await wrapper.vm.startOver()
 
-    expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(2)
+      expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(2)
+    })
+
+    it('should paginate 4 items on 2 pages', async () => {
+      wrapper.vm.pageSize = 2
+      await letData(es).have(new IndexedDocument('doc_01')
+        .withContentType('type_01')).commit()
+      await letData(es).have(new IndexedDocument('doc_02')
+        .withContentType('type_02')).commit()
+      await letData(es).have(new IndexedDocument('doc_03')
+        .withContentType('type_03')).commit()
+      await letData(es).have(new IndexedDocument('doc_04')
+        .withContentType('type_04')).commit()
+
+      await wrapper.vm.startOver()
+      expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(2)
+
+      await wrapper.vm.next()
+      expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(4)
+    })
+
+    it('should paginate 10 items on 10 pages', async () => {
+      wrapper.vm.pageSize = 1
+      await letData(es).have(new IndexedDocument('doc_01')
+        .withContentType('type_01')).commit()
+      await letData(es).have(new IndexedDocument('doc_02')
+        .withContentType('type_02')).commit()
+      await letData(es).have(new IndexedDocument('doc_03')
+        .withContentType('type_03')).commit()
+      await letData(es).have(new IndexedDocument('doc_04')
+        .withContentType('type_04')).commit()
+      await letData(es).have(new IndexedDocument('doc_05')
+        .withContentType('type_05')).commit()
+      await letData(es).have(new IndexedDocument('doc_06')
+        .withContentType('type_06')).commit()
+      await letData(es).have(new IndexedDocument('doc_07')
+        .withContentType('type_07')).commit()
+      await letData(es).have(new IndexedDocument('doc_08')
+        .withContentType('type_08')).commit()
+      await letData(es).have(new IndexedDocument('doc_09')
+        .withContentType('type_09')).commit()
+      await letData(es).have(new IndexedDocument('doc_10')
+        .withContentType('type_10')).commit()
+
+      await wrapper.vm.startOver()
+      expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(1)
+      await wrapper.vm.next()
+      expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(2)
+      await wrapper.vm.next()
+      expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(3)
+      await wrapper.vm.next()
+      expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(4)
+      await wrapper.vm.next()
+      expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(5)
+      await wrapper.vm.next()
+      expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(6)
+      await wrapper.vm.next()
+      expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(7)
+      await wrapper.vm.next()
+      expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(8)
+      await wrapper.vm.next()
+      expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(9)
+      await wrapper.vm.next()
+      expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(10)
+    })
+
+    it('should paginate 10 items on 2 pages, and start over', async () => {
+      wrapper.vm.pageSize = 5
+      await letData(es).have(new IndexedDocument('doc_01')
+        .withContentType('type_01')).commit()
+      await letData(es).have(new IndexedDocument('doc_02')
+        .withContentType('type_02')).commit()
+      await letData(es).have(new IndexedDocument('doc_03')
+        .withContentType('type_03')).commit()
+      await letData(es).have(new IndexedDocument('doc_04')
+        .withContentType('type_04')).commit()
+      await letData(es).have(new IndexedDocument('doc_05')
+        .withContentType('type_05')).commit()
+      await letData(es).have(new IndexedDocument('doc_06')
+        .withContentType('type_06')).commit()
+      await letData(es).have(new IndexedDocument('doc_07')
+        .withContentType('type_07')).commit()
+      await letData(es).have(new IndexedDocument('doc_08')
+        .withContentType('type_08')).commit()
+      await letData(es).have(new IndexedDocument('doc_09')
+        .withContentType('type_09')).commit()
+      await letData(es).have(new IndexedDocument('doc_10')
+        .withContentType('type_10')).commit()
+
+      await wrapper.vm.startOver()
+      expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(5)
+      await wrapper.vm.next()
+      expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(10)
+
+      await wrapper.vm.startOver()
+      expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(5)
+      await wrapper.vm.next()
+      expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(10)
+    })
   })
 
-  it('should paginate 4 items on 2 pages', async () => {
-    wrapper.vm.pageSize = 2
-    await letData(es).have(new IndexedDocument('index.js').withContentType('text/javascript')).commit()
-    await letData(es).have(new IndexedDocument('index.html').withContentType('text/html')).commit()
-    await letData(es).have(new IndexedDocument('index.css').withContentType('text/css')).commit()
-    await letData(es).have(new IndexedDocument('index.php').withContentType('text/php')).commit()
+  describe('search', () => {
+    it('should create query tokens', async () => {
+      wrapper.setData({ facetQuery: 'iCij' })
+      await wrapper.vm.startOver()
 
-    await wrapper.vm.startOver()
+      expect(wrapper.vm.queryTokens).toContain('iCij')
+      expect(wrapper.vm.queryTokens).toContain('icij')
+      expect(wrapper.vm.queryTokens).toContain('ICIJ')
+      expect(wrapper.vm.queryTokens).toContain('ICij')
+    })
 
-    expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(2)
+    it('should filter the list according to facetQuery', async () => {
+      await letData(es).have(new IndexedDocument('doc_01')
+        .withContentType('type_01')).commit()
+      await letData(es).have(new IndexedDocument('doc_02')
+        .withContentType('type_02')).commit()
+      await letData(es).have(new IndexedDocument('doc_03')
+        .withContentType('type_03')).commit()
+      await letData(es).have(new IndexedDocument('doc_04')
+        .withContentType('type_04')).commit()
+      await letData(es).have(new IndexedDocument('doc_05')
+        .withContentType('type_05')).commit()
+      await letData(es).have(new IndexedDocument('doc_06')
+        .withContentType('type_06')).commit()
+      await letData(es).have(new IndexedDocument('doc_07')
+        .withContentType('type_07')).commit()
+      await letData(es).have(new IndexedDocument('doc_08')
+        .withContentType('type_08')).commit()
+      await letData(es).have(new IndexedDocument('doc_09')
+        .withContentType('type_09')).commit()
+      await letData(es).have(new IndexedDocument('doc_10')
+        .withContentType('type_10')).commit()
 
-    await wrapper.vm.next()
+      wrapper.setData({ facetQuery: '' })
+      await wrapper.vm.startOver()
+      expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(8)
 
-    expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(4)
-  })
+      wrapper.setData({ facetQuery: 'type_1' })
+      await wrapper.vm.startOver()
+      expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(1)
+    })
 
-  it('should paginate 10 items on 10 pages', async () => {
-    wrapper.vm.pageSize = 1
+    it('should trigger a search when value of facetQuery changes', () => {
+      jest.spyOn(wrapper.vm, 'search').mockImplementation(jest.fn)
+      expect(wrapper.vm.search).not.toHaveBeenCalled()
 
-    await letData(es).have(new IndexedDocument('index.pdf').withContentType('pdf')).commit()
-    await letData(es).have(new IndexedDocument('index.doc').withContentType('doc')).commit()
-    await letData(es).have(new IndexedDocument('index.docx').withContentType('docx')).commit()
-    await letData(es).have(new IndexedDocument('index.html').withContentType('html')).commit()
-    await letData(es).have(new IndexedDocument('index.css').withContentType('css')).commit()
-    await letData(es).have(new IndexedDocument('index.js').withContentType('js')).commit()
-    await letData(es).have(new IndexedDocument('index.tx').withContentType('tx')).commit()
-    await letData(es).have(new IndexedDocument('index.vue').withContentType('vue')).commit()
-    await letData(es).have(new IndexedDocument('index.txt').withContentType('txt')).commit()
-    await letData(es).have(new IndexedDocument('index.xls').withContentType('xls')).commit()
+      wrapper.setData({ facetQuery: 'pdf' })
+      expect(wrapper.vm.search).toHaveBeenCalled()
+    })
 
-    await wrapper.vm.startOver()
-    expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(1)
-    await wrapper.vm.next()
-    expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(2)
-    await wrapper.vm.next()
-    expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(3)
-    await wrapper.vm.next()
-    expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(4)
-    await wrapper.vm.next()
-    expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(5)
-    await wrapper.vm.next()
-    expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(6)
-    await wrapper.vm.next()
-    expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(7)
-    await wrapper.vm.next()
-    expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(8)
-    await wrapper.vm.next()
-    expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(9)
-    await wrapper.vm.next()
-    expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(10)
-  })
+    it('should emit a facet::hide::named-entities event on click to delete named entity', async () => {
+      wrapper = mount(FacetSearch, { localVue, i18n, store, router, propsData: { infiniteScroll: false, throttle: 0, facet: find(store.state.search.facets, { name: 'named-entity-person' }) } })
+      await letData(es).have(new IndexedDocument('doc_01').withNer('ner')).commit()
+      await wrapper.vm.startOver()
 
-  it('should paginate 10 items on 2 pages, and start over', async () => {
-    wrapper.vm.pageSize = 5
+      const mockCallback = jest.fn()
+      EventBus.$on('facet::hide::named-entities', mockCallback)
 
-    await letData(es).have(new IndexedDocument('index.pdf').withContentType('pdf')).commit()
-    await letData(es).have(new IndexedDocument('index.doc').withContentType('doc')).commit()
-    await letData(es).have(new IndexedDocument('index.docx').withContentType('docx')).commit()
-    await letData(es).have(new IndexedDocument('index.html').withContentType('html')).commit()
-    await letData(es).have(new IndexedDocument('index.css').withContentType('css')).commit()
-    await letData(es).have(new IndexedDocument('index.js').withContentType('js')).commit()
-    await letData(es).have(new IndexedDocument('index.tx').withContentType('tx')).commit()
-    await letData(es).have(new IndexedDocument('index.vue').withContentType('vue')).commit()
-    await letData(es).have(new IndexedDocument('index.txt').withContentType('txt')).commit()
-    await letData(es).have(new IndexedDocument('index.xls').withContentType('xls')).commit()
+      await wrapper.find('.facet__items__item__menu .dropdown-item:first-child').trigger('click')
 
-    await wrapper.vm.startOver()
-    expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(5)
-    await wrapper.vm.next()
-    expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(10)
+      expect(mockCallback.mock.calls).toHaveLength(1)
+    })
 
-    await wrapper.vm.startOver()
-    expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(5)
-    await wrapper.vm.next()
-    expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(10)
-  })
+    it('should call the search function after a named entity deletion', async () => {
+      wrapper = mount(FacetSearch, { localVue, i18n, store, router, propsData: { infiniteScroll: false, throttle: 0, facet: find(store.state.search.facets, { name: 'named-entity-person' }) } })
+      await letData(es).have(new IndexedDocument('doc_01').withNer('ner')).commit()
+      await wrapper.vm.startOver()
 
-  it('should create query tokens', async () => {
-    wrapper.vm.facetQuery = 'iCij'
-    await wrapper.vm.startOver()
-    expect(wrapper.vm.queryTokens).toContain('iCij')
-    expect(wrapper.vm.queryTokens).toContain('icij')
-    expect(wrapper.vm.queryTokens).toContain('ICIJ')
-    expect(wrapper.vm.queryTokens).toContain('ICij')
-  })
+      const spySearch = jest.spyOn(wrapper.vm, 'search')
+      expect(spySearch).not.toBeCalled()
 
-  it('should filter the list according to facetQuery', async () => {
-    await letData(es).have(new IndexedDocument('index.jade').withContentType('jade')).commit()
-    await letData(es).have(new IndexedDocument('index.doc').withContentType('doc')).commit()
-    await letData(es).have(new IndexedDocument('index.docx').withContentType('docx')).commit()
-    await letData(es).have(new IndexedDocument('index.html').withContentType('html')).commit()
-    await letData(es).have(new IndexedDocument('index.css').withContentType('css')).commit()
-    await letData(es).have(new IndexedDocument('index.js').withContentType('js')).commit()
-    await letData(es).have(new IndexedDocument('index.tx').withContentType('tx')).commit()
-    await letData(es).have(new IndexedDocument('index.vue').withContentType('vue')).commit()
-    await letData(es).have(new IndexedDocument('index.txt').withContentType('txt')).commit()
-    await letData(es).have(new IndexedDocument('index.xls').withContentType('xls')).commit()
+      await wrapper.find('.facet__items__item__menu .dropdown-item:first-child').trigger('click')
 
-    wrapper.setData({ facetQuery: '' })
-    await wrapper.vm.startOver()
-    expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(8)
-
-    wrapper.setData({ facetQuery: 'doc' })
-    await wrapper.vm.startOver()
-    expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(2)
-
-    wrapper.setData({ facetQuery: 'jade' })
-    await wrapper.vm.startOver()
-    expect(wrapper.findAll('.facet__items__item .custom-checkbox')).toHaveLength(1)
-  })
-
-  it('should trigger a search when value of facetQuery changes', async () => {
-    jest.spyOn(wrapper.vm, 'search').mockImplementation(jest.fn)
-    wrapper.vm.facetQuery = 'pdf'
-    await wrapper.vm.$nextTick()
-    expect(wrapper.vm.search).toHaveBeenCalled()
-  })
-
-  it('should emit a facet::hide::named-entities event on click to delete named entity', async () => {
-    wrapper = mount(FacetSearch, { localVue, i18n, store, router, propsData: { infiniteScroll: false, throttle: 0, facet: find(store.state.search.facets, { name: 'named-entity-person' }) } })
-    await letData(es).have(new IndexedDocument('doc_01.txt').withContent('this is a naz document').withNer('naz')).commit()
-    await wrapper.vm.startOver()
-
-    const mockCallback = jest.fn()
-    EventBus.$on('facet::hide::named-entities', mockCallback)
-
-    await wrapper.find('.facet__items__item__menu .dropdown-item:first-child').trigger('click')
-
-    expect(mockCallback.mock.calls).toHaveLength(1)
-  })
-
-  it('should call the search function after a named entity deletion', async () => {
-    wrapper = mount(FacetSearch, { localVue, i18n, store, router, propsData: { infiniteScroll: false, throttle: 0, facet: find(store.state.search.facets, { name: 'named-entity-person' }) } })
-    await letData(es).have(new IndexedDocument('doc_01.txt').withContent('this is a naz document').withNer('naz')).commit()
-    await wrapper.vm.startOver()
-
-    const spySearch = jest.spyOn(wrapper.vm, 'search')
-    expect(spySearch).not.toBeCalled()
-
-    await wrapper.find('.facet__items__item__menu .dropdown-item:first-child').trigger('click')
-
-    expect(spySearch).toBeCalled()
-    expect(spySearch).toBeCalledTimes(1)
+      expect(spySearch).toBeCalled()
+      expect(spySearch).toBeCalledTimes(1)
+    })
   })
 
   it('should display all the indexing dates', async () => {
@@ -215,9 +246,12 @@ describe('FacetSearch.vue', () => {
   })
 
   it('should display the total count of content type', async () => {
-    await letData(es).have(new IndexedDocument('doc_01').withContentType('type_01')).commit()
-    await letData(es).have(new IndexedDocument('doc_02').withContentType('type_02')).commit()
-    await letData(es).have(new IndexedDocument('doc_03').withContentType('type_03')).commit()
+    await letData(es).have(new IndexedDocument('doc_01')
+      .withContentType('type_01')).commit()
+    await letData(es).have(new IndexedDocument('doc_02')
+      .withContentType('type_02')).commit()
+    await letData(es).have(new IndexedDocument('doc_03')
+      .withContentType('type_03')).commit()
 
     await wrapper.vm.startOver()
 
