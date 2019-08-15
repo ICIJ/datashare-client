04c67f272fec0be766fa707e13c51ce35b3aac75
Pierre Romera <hello@pirhoo.com>
2019-08-15 09:46:10 +0200

    Added missing babel plugin

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   presets: [
     '@vue/app'
+  ],
+  plugins: [
+    // This is needed so require.context (from Webpack) also works in Jest
+    'transform-require-context'
   ]
 }
