d2e42f07f28425b0033e08ea2ec2181284d6fd3f
Pierre Romera <hello@pirhoo.com>
2019-09-27 14:06:09 +0200

    Mock matchMedia globally

--- tests/unit/setup.js
+++ tests/unit/setup.js
@@ -2,3 +2,13 @@ global.console = Object.assign(global.console, {
   warn: jest.fn(),
   info: jest.fn()
 })
+
+window.matchMedia = jest.fn().mockImplementation(query => {
+  return {
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn()
+  }
+})
