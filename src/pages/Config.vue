f8765ad1382e08a89d8f567714ae595de50101f4
Anne L'Hôte <anne.lhote@gmail.com>
2019-11-27 10:31:54 +0000

    [ICIJ/datashare#310] Add type on config form

--- src/pages/Config.vue
+++ src/pages/Config.vue
@@ -8,12 +8,20 @@
     <div class="container mt-4">
       <b-form>
         <b-form-group
-          :label="$t(`config.${field}`)"
+          :label="$t(`config.${field.name}`)"
           v-for="field in configFields"
-          :key="field"
+          :key="field.name"
         >
+          <b-form-radio-group
+            v-if="field.type === 'boolean'"
+            v-model="$config.values[field.name]"
+            :options="[{ text: 'Yes', value: true }, { text: 'No', value: false }]"
+            disabled
+          ></b-form-radio-group>
           <b-form-input
-            v-model="$config.values[field]"
+            v-else
+            v-model="$config.values[field.name]"
+            :type="field.type"
             disabled
           ></b-form-input>
         </b-form-group>
@@ -27,7 +35,37 @@ export default {
   name: 'Config',
   data () {
     return {
-      configFields: ['oauthAuthorizeUrl', 'dataSourceUrl', 'oauthTokenUrl', 'defaultUserName', 'dataDir', 'tcpListenPort', 'oauthApiUrl', 'clusterName', 'defaultProject', 'ocr']
+      configFields: [{
+        name: 'oauthAuthorizeUrl',
+        type: 'text'
+      }, {
+        name: 'dataSourceUrl',
+        type: 'text'
+      }, {
+        name: 'oauthTokenUrl',
+        type: 'text'
+      }, {
+        name: 'defaultUserName',
+        type: 'text'
+      }, {
+        name: 'dataDir',
+        type: 'text'
+      }, {
+        name: 'tcpListenPort',
+        type: 'number'
+      }, {
+        name: 'oauthApiUrl',
+        type: 'text'
+      }, {
+        name: 'clusterName',
+        type: 'text'
+      }, {
+        name: 'defaultProject',
+        type: 'text'
+      }, {
+        name: 'ocr',
+        type: 'boolean'
+      }]
     }
   }
 }
--- tests/unit/specs/pages/Config.spec.js
+++ tests/unit/specs/pages/Config.spec.js
@@ -11,4 +11,16 @@ describe('Config.vue', () => {
 
     expect(wrapper.find('h3').text()).toBe('config.title')
   })
+
+  it('should display a radio button', () => {
+    const wrapper = shallowMount(Config, { localVue, mocks: { $t: msg => msg }, data: function () { return { configFields: [{ name: 'testBoolean', type: 'boolean' }] } } })
+
+    expect(wrapper.findAll('b-form-radio-group-stub')).toHaveLength(1)
+  })
+
+  it('should display a text input', () => {
+    const wrapper = shallowMount(Config, { localVue, mocks: { $t: msg => msg }, data: function () { return { configFields: [{ name: 'testText', type: 'text' }] } } })
+
+    expect(wrapper.findAll('b-form-input-stub')).toHaveLength(1)
+  })
 })
