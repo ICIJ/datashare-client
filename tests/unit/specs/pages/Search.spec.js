637ac3907d6bebed57b8f2c3d43258291c6ed579
Anne L'Hôte <anne.lhote@gmail.com>
2019-12-04 09:57:51 +0000

    [ci] fix tests

--- tests/unit/specs/pages/Search.spec.js
+++ tests/unit/specs/pages/Search.spec.js
@@ -1,53 +1,52 @@
-import Vuex from 'vuex'
-import { App } from '@/main'
-import VueRouter from 'vue-router'
-import Search from '@/pages/Search'
-import { actions, getters, state, mutations } from '@/store/modules/search'
+import cloneDeep from 'lodash/cloneDeep'
 import { createLocalVue, shallowMount } from '@vue/test-utils'
+import VueRouter from 'vue-router'
+import Vuex from 'vuex'
+
+import { App } from '@/main'
+import Search from '@/pages/Search'
+import { state, getters, mutations, actions } from '@/store/modules/search'
 
 const { localVue } = App.init(createLocalVue()).useAll()
 const router = new VueRouter()
 
 describe('Search.vue', () => {
-  let wrapper, localStore
+  let wrapper, store
+  const actionsStore = Object.assign(cloneDeep(actions), { query: jest.fn(), refresh: jest.fn(), updateFromRouteQuery: jest.fn() })
 
   beforeEach(() => {
-    localStore = new Vuex.Store({
+    store = new Vuex.Store({
       modules: {
         search: {
-          getters,
-          state,
-          mutations,
           namespaced: true,
-          actions: Object.assign(actions, {
-            query: jest.fn(),
-            refresh: jest.fn(),
-            updateFromRouteQuery: jest.fn()
-          })
+          state: cloneDeep(state),
+          getters: cloneDeep(getters),
+          mutations: cloneDeep(mutations),
+          actions: actionsStore
         },
         document: {
           namespaced: true
         }
       }
     })
-    wrapper = shallowMount(Search, { localVue, router, store: localStore })
+    wrapper = shallowMount(Search, { localVue, router, store })
   })
 
   it('should refresh the view on custom event', () => {
     wrapper.vm.$root.$emit('index::delete::all')
 
-    expect(actions.query).toBeCalledTimes(2)
+    expect(actionsStore.query).toBeCalledTimes(2)
   })
 
   it('should execute a new search on event "facet::starred::refresh"', () => {
     wrapper.vm.$root.$emit('facet::starred::refresh')
 
-    expect(actions.refresh).toBeCalledTimes(1)
+    expect(actionsStore.refresh).toBeCalledTimes(1)
   })
 
   it('should redirect to the complete query', () => {
     const query = 'this is a query'
-    localStore.commit('search/query', query)
+    store.commit('search/query', query)
 
     expect(wrapper.find('.search__body__backdrop').props('to')).toMatchObject({ name: 'search', query: { q: query } })
   })
