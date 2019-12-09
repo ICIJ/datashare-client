bd72d9a4f79771bb69e58b2744dac15b26b9ba99
Anne L'Hôte <anne.lhote@gmail.com>
2019-12-09 09:12:23 +0000

    [ci] refactor

--- tests/unit/specs/components/SearchResultsAppliedFilters.spec.js
+++ tests/unit/specs/components/SearchResultsAppliedFilters.spec.js
@@ -6,19 +6,19 @@ import { datashare } from '@/store/modules/search'
 import { jsonResp } from 'tests/unit/tests_utils'
 import SearchResultsAppliedFilters from '@/components/SearchResultsAppliedFilters'
 
-const { localVue, i18n, store } = App.init(createLocalVue()).useAll()
+const { localVue, store } = App.init(createLocalVue()).useAll()
 const router = new VueRouter()
 
 describe('SearchResultsAppliedFilters.vue', () => {
   let wrapper
 
   beforeEach(() => {
-    wrapper = shallowMount(SearchResultsAppliedFilters, { localVue, i18n, store })
+    wrapper = shallowMount(SearchResultsAppliedFilters, { localVue, store, mocks: { $t: msg => msg, $te: msg => msg } })
     jest.spyOn(datashare, 'fetch')
     datashare.fetch.mockReturnValue(jsonResp())
   })
 
-  afterEach(async () => store.dispatch('search/reset'))
+  afterEach(() => store.dispatch('search/reset'))
 
   afterAll(() => datashare.fetch.mockRestore())
 
@@ -38,34 +38,34 @@ describe('SearchResultsAppliedFilters.vue', () => {
     it('should translate the label of a facet', () => {
       store.commit('search/addFacetValue', { name: 'contentType', value: 'text/plain' })
 
-      expect(wrapper.vm.filters[0].label).toEqual('Plain text document')
+      expect(wrapper.vm.filters[0].label).toBe('Plain text document')
     })
 
     it('should translate the label of a facet date', () => {
       store.commit('search/addFacetValue', { name: 'indexingDate', value: 1556668800000 })
 
-      expect(wrapper.vm.filters[0].label).toEqual('2019-05')
+      expect(wrapper.vm.filters[0].label).toBe('2019-05')
     })
 
     it('should translate the label of a facet date range', () => {
       store.commit('search/addFacetValue', { name: 'creationDate', value: [1556668800000, 1566908357980] })
 
-      expect(wrapper.vm.filters[0].label).toEqual('05/01/2019')
-      expect(wrapper.vm.filters[1].label).toEqual('08/27/2019')
+      expect(wrapper.vm.filters[0].label).toBe('05/01/2019')
+      expect(wrapper.vm.filters[1].label).toBe('08/27/2019')
     })
 
     it('should translate in French the label of a facet date range', () => {
       localStorage.setItem('locale', 'fr')
       store.commit('search/addFacetValue', { name: 'creationDate', value: [1556668800000, 1566908357980] })
 
-      expect(wrapper.vm.filters[0].label).toEqual('01/05/2019')
-      expect(wrapper.vm.filters[1].label).toEqual('27/08/2019')
+      expect(wrapper.vm.filters[0].label).toBe('01/05/2019')
+      expect(wrapper.vm.filters[1].label).toBe('27/08/2019')
     })
 
     it('should translate the label of a facet yes no', () => {
       store.commit('search/addFacetValue', { name: 'starred', value: true })
 
-      expect(wrapper.vm.filters[0].label).toEqual('Starred')
+      expect(wrapper.vm.filters[0].label).toBe('facet.starred')
     })
 
     it('should set facet as positive applied facet', () => {
@@ -84,7 +84,7 @@ describe('SearchResultsAppliedFilters.vue', () => {
 
   describe('deletes applied filters', () => {
     beforeEach(() => {
-      wrapper = mount(SearchResultsAppliedFilters, { localVue, i18n, router, store })
+      wrapper = mount(SearchResultsAppliedFilters, { localVue, router, store, mocks: { $t: msg => msg } })
     })
 
     it('should remove the "AND" on last applied filter deletion', async () => {
