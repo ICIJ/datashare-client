71b211cf967c1ccadf8cf75716929415c8d8d1b6
Pierre Romera <hello@pirhoo.com>
2020-01-23 17:56:10 +0000

    Longer size for mounting location

--- src/components/MountedDataLocation.vue
+++ src/components/MountedDataLocation.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="mounted-data-location d-flex align-items-center px-1">
     <fa icon="folder" class="mx-1 text-muted mounted-data-location__icon" />
-    <span class="flex-grow-1 text-monospace p-0 text-truncate mounted-data-location__value" :title="$t('footer.homedir')" v-b-tooltip>
+    <div class="flex-grow-1 text-monospace px-0 py-1 text-truncate mounted-data-location__value" :id="valueId">
       {{ $config.get('mountedDataDir') || $config.get('dataDir') }}
-    </span>
+    </div>
+    <b-popover :target="valueId" triggers="hover" placement="top" :boundary-padding="16 * 1.5">
+      <template v-slot:title>{{ $t('footer.homedir') }}</template>
+      <div class="text-monospace">
+        {{ $config.get('mountedDataDir') || $config.get('dataDir') }}
+      </div>
+    </b-popover>
     <confirm-button class="btn btn-sm text-secondary mounted-data-location__delete-index" :confirmed="deleteAll" :title="$t('indexing.delete_index_label')" v-b-tooltip :label="$t('indexing.delete_index_label')" :description="$t('indexing.delete_index_description')">
       <fa icon="trash-alt" />
       <span class="sr-only">
@@ -14,6 +20,8 @@
 </template>
 
 <script>
+import uniqueId from 'lodash/uniqueId'
+
 export default {
   name: 'MountedDataLocation',
   methods: {
@@ -22,6 +30,11 @@ export default {
       await this.$store.dispatch('batchSearch/deleteBatchSearches')
       this.$store.commit('userHistory/clear')
       this.$root.$emit('index::delete::all')
+    }
+  },
+  computed: {
+    valueId () {
+      return uniqueId('mounted-data-location__value--')
     }
   }
 }
