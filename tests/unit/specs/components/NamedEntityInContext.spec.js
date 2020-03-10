a1c48c7baaee83129fa274f70ecd0be61589cc0f
Anne L'Hôte <anne.lhote@gmail.com>
2020-03-10 17:44:02 +0000

    [fix][ci] Fix tests because of commit 9bbc3be

--- tests/unit/specs/components/NamedEntityInContext.spec.js
+++ tests/unit/specs/components/NamedEntityInContext.spec.js
@@ -6,7 +6,7 @@ import NamedEntityInContext from '@/components/NamedEntityInContext'
 import { IndexedDocument, letData } from 'tests/unit/es_utils'
 import { Core } from '@/core'
 
-const { localVue, store } = Core.init(createLocalVue()).useAll()
+const { i18n, localVue, store } = Core.init(createLocalVue()).useAll()
 
 describe('NamedEntityInContext.vue', () => {
   const index = toLower('NamedEntityInContext')
@@ -15,16 +15,18 @@ describe('NamedEntityInContext.vue', () => {
   const es = esConnectionHelper.es
   // A quick function to generate the default propsData for most tests
   const defaultPropsData = async function ({ namedEntityIndex = 0, extractLength = 16 } = {}) {
+    const category = 'PERSON'
     const id = 'document'
     await letData(es).have(new IndexedDocument(id, index)
       .withContent('Lorem Lea ipsum dolor Yassine sit amet')
-      .withNer('Lea', 6, 'PERSON')
-      .withNer('Yassine', 22, 'PERSON')
-      .withNer('contact@icij.org', -1, 'PERSON'))
+      .withNer('Lea', 6, category)
+      .withNer('Yassine', 22, category)
+      .withNer('contact@icij.org', -1, category))
       .commit()
     const document = await store.dispatch('document/get', { id, index })
-    const namedEntities = await store.dispatch('document/getFirstPageForNamedEntityInCategory', 'PERSON')
-    const namedEntity = namedEntities.hits[namedEntityIndex]
+    await store.dispatch('document/getFirstPageForNamedEntityInCategory', category)
+    const namedEntities = store.state.document.namedEntitiesPaginatedByCategories
+    const namedEntity = namedEntities[category][0].hits[namedEntityIndex]
     return { document, namedEntity, extractLength }
   }
 
@@ -34,31 +36,31 @@ describe('NamedEntityInContext.vue', () => {
 
   it('should be a Vue instance', async () => {
     const propsData = await defaultPropsData()
-    const wrapper = shallowMount(NamedEntityInContext, { localVue, propsData })
+    const wrapper = shallowMount(NamedEntityInContext, { i18n, localVue, propsData })
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 
   it('should display the named entity at the beginning of the text', async () => {
     const propsData = await defaultPropsData()
-    const wrapper = shallowMount(NamedEntityInContext, { localVue, propsData })
+    const wrapper = shallowMount(NamedEntityInContext, { i18n, localVue, propsData })
     expect(wrapper.find('.named-entity-in-context__extract').text()).toBe('Lorem Lea ipsum dolor...')
   })
 
   it('should display the named entity at the end of the text', async () => {
     const propsData = await defaultPropsData({ namedEntityIndex: 1 })
-    const wrapper = shallowMount(NamedEntityInContext, { localVue, propsData })
+    const wrapper = shallowMount(NamedEntityInContext, { i18n, localVue, propsData })
     expect(wrapper.find('.named-entity-in-context__extract').text()).toBe('...m dolor Yassine...')
   })
 
   it('should display the named entity at the end of the text with a larger chunk', async () => {
     const propsData = await defaultPropsData({ namedEntityIndex: 1, extractLength: 40 })
-    const wrapper = shallowMount(NamedEntityInContext, { localVue, propsData })
+    const wrapper = shallowMount(NamedEntityInContext, { i18n, localVue, propsData })
     expect(wrapper.find('.named-entity-in-context__extract').text()).toBe('...rem Lea ipsum dolor Yassine sit amet')
   })
 
   it('should not display the extract if the named entity comes from the medata', async () => {
     const propsData = await defaultPropsData({ namedEntityIndex: 2 })
-    const wrapper = shallowMount(NamedEntityInContext, { localVue, propsData, mocks: { $t: msg => msg } })
+    const wrapper = shallowMount(NamedEntityInContext, { i18n, localVue, propsData })
     expect(wrapper.find('.named-entity-in-context__extract').exists()).toBeFalsy()
     expect(wrapper.find('.named-entity-in-context__meta').exists()).toBeTruthy()
   })
