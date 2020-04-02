f11fe80d2bc1b16e2bfe0629e981053583d78985
Anne L'Hôte <anne.lhote@gmail.com>
2020-04-02 11:05:36 +0000

    [ICIJ/datashare#418] Sticky page selector in Preview

--- src/components/document/viewers/PaginatedViewer.vue
+++ src/components/document/viewers/PaginatedViewer.vue
@@ -1,12 +1,12 @@
 <template>
-  <v-wait for="load_data">
+  <v-wait for="load data paginated viewer">
     <div class="p-3 w-100 text-muted" slot="waiting">
       {{ $t('document.fetching') }}
     </div>
-    <VuePerfectScrollbar class="paginated-viewer d-flex" v-if="meta.previewable">
-      <div id="paginated-viewer__header" class="bg-light px-3 py-2 paginated-viewer__header">
+    <div class="paginated-viewer d-flex" v-if="meta.previewable">
+      <div id="paginated-viewer__header" class="bg-light py-2 paginated-viewer__header">
         <div id="paginated-viewer__thumbnails" class="paginated-viewer__thumbnails">
-          <div class="text-center mt-2 mb-4 d-flex align-items-center viewer__thumbnails__header">
+          <div class="text-center m-2 d-flex align-items-center viewer__thumbnails__header">
             <select class="form-control form-control-sm" v-model.number="active">
               <option v-for="page in pagesRange" :key="page" :value="page">
                 {{ page + 1 }}
@@ -16,18 +16,20 @@
               / {{ meta.pages }}
             </span>
           </div>
-          <div v-for="page in pagesRange" :key="page" @click="active = page" class="my-2 paginated-viewer__thumbnails__item" :class="{ 'paginated-viewer__thumbnails__item--active': active === page }">
-            <document-thumbnail :document="document" size="150" :page="page" lazy class="w-100 border-0" />
-            <span class="paginated-viewer__thumbnails__item__page">
-              {{ page + 1 }}
-            </span>
+          <div class="paginated-viewer__thumbnails__items bg-light">
+            <div v-for="page in pagesRange" :key="page" @click="active = page" class="m-2 paginated-viewer__thumbnails__items__item" :class="{ 'paginated-viewer__thumbnails__item--active': active === page }">
+              <document-thumbnail :document="document" size="150" :page="page" lazy class="w-100 border-0" />
+              <span class="paginated-viewer__thumbnails__items__item__page">
+                {{ page + 1 }}
+              </span>
+            </div>
           </div>
         </div>
       </div>
       <div class="paginated-viewer__preview p-3 text-center">
         <document-thumbnail :document="document" size="1200" :page="active" :key="active" class="w-auto d-inline-block" />
       </div>
-    </VuePerfectScrollbar>
+    </div>
     <div class="p-3" v-else>
       {{ $t('document.not_available') }}
     </div>
@@ -40,7 +42,6 @@ import range from 'lodash/range'
 import startCase from 'lodash/startCase'
 import axios from 'axios'
 import { getCookie } from 'tiny-cookie'
-import VuePerfectScrollbar from 'vue-perfect-scrollbar'
 
 import DocumentThumbnail from '@/components/DocumentThumbnail.vue'
 
@@ -52,8 +53,7 @@ export default {
     }
   },
   components: {
-    DocumentThumbnail,
-    VuePerfectScrollbar
+    DocumentThumbnail
   },
   data () {
     return {
@@ -64,11 +64,11 @@ export default {
     }
   },
   async mounted () {
-    this.$wait.start('load_data')
+    this.$wait.start('load data paginated viewer')
     this.$Progress.start()
     this.meta = (await axios({ url: this.metaUrl, ...this.metaOptions })).data
     this.$Progress.finish()
-    this.$wait.end('load_data')
+    this.$wait.end('load data paginated viewer')
   },
   computed: {
     pagesRange () {
@@ -100,59 +100,60 @@ export default {
 
 <style lang="scss">
   .paginated-viewer {
-    min-height: 100%;
-    min-width: 100%;
-    position: relative;
-
     &__header {
-      height: 100%;
-      overflow-y: auto;
-      position: absolute;
-      width: 150px;
+      width: 200px;
     }
 
     &__thumbnails {
-      &__item {
-        border: 1px solid $border-color;
-        cursor: pointer;
-        position: relative;
+      min-height: 100%;
+      position: relative;
 
-        img {
-          width: 100%;
-        }
+      &__items {
+        height: calc(100% - 35px);
+        overflow-y: auto;
+        position: absolute;
+        width: 100%;
 
-        &:hover {
-          border-color: $primary;
-          box-shadow: 0 0 0 0.1em rgba($primary, .2);
-        }
-
-        &--active, &--active:hover {
-          border-color: $secondary;
-        }
-
-        &--active &__page {
-          background: $secondary;
-          color: white;
-        }
-
-        &__page {
-          background: $light;
+        &__item {
           border: 1px solid $border-color;
-          border-bottom: 0;
-          border-right: 0;
-          bottom: 0;
-          font-size: 0.8em;
-          font-weight: bold;
-          padding: 0.2em 0.4em;
-          position: absolute;
-          right: 0;
+          cursor: pointer;
+          position: relative;
+
+          img {
+            width: 100%;
+          }
+
+          &:hover {
+            border-color: $primary;
+            box-shadow: 0 0 0 0.1em rgba($primary, .2);
+          }
+
+          &--active, &--active:hover {
+            border-color: $secondary;
+          }
+
+          &--active &__page {
+            background: $secondary;
+            color: white;
+          }
+
+          &__page {
+            background: $light;
+            border: 1px solid $border-color;
+            border-bottom: 0;
+            border-right: 0;
+            bottom: 0;
+            font-size: 0.8em;
+            font-weight: bold;
+            padding: 0.2em 0.4em;
+            position: absolute;
+            right: 0;
+          }
         }
       }
     }
 
     &__preview {
-      margin-left: 150px;
-
       img {
         max-width: 100%;
       }
