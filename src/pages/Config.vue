7572dd03cde488179b2305c4403c46fc801cb93f
Pierre Romera <hello@pirhoo.com>
2020-04-06 12:36:00 +0200

    Changed input layout

--- src/pages/Config.vue
+++ src/pages/Config.vue
@@ -10,18 +10,16 @@
         <v-wait for="load_data">
           <fa icon="circle-notch" spin size="2x" class="d-flex mx-auto mt-5" slot="waiting" />
           <b-form @submit.prevent="onSubmit">
-            <b-form-group
-              :label="name"
-              v-for="(value, name) in config"
-              :key="name"
-            >
-              <b-form-input
-                v-model="config[name]"
-              ></b-form-input>
+            <b-form-group :label="name" v-for="(value, name) in config" :key="name" label-cols-xs="12" label-cols-sm="4" label-cols-lg="3">
+              <b-form-input v-model="config[name]" />
             </b-form-group>
-            <b-button type="submit" variant="primary">
-              {{ $t('config.submit') }}
-            </b-button>
+            <b-row>
+              <b-col offset-xs="0" offset-sm="4" offset-lg="3">
+                <b-button type="submit" variant="primary">
+                  {{ $t('config.submit') }}
+                </b-button>
+              </b-col>
+            </b-row>
           </b-form>
         </v-wait>
       </div>
