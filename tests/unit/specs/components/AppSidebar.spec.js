95a04721550ae45316c266f467bc0a8b4d035842
Anne L'Hôte <anne.lhote@gmail.com>
2020-04-08 15:26:47 +0000

    [ci][fix] Fix tests

--- tests/unit/specs/components/AppSidebar.spec.js
+++ tests/unit/specs/components/AppSidebar.spec.js
@@ -27,67 +27,67 @@ describe('AppSidebar.vue', () => {
 
   it('should display the link to analyze my documents', () => {
     Murmur.config.set('manageDocuments', true)
-    const wrapper = shallowMount(AppSidebar, { i18n, localVue, store, router })
+    const wrapper = shallowMount(AppSidebar, { i18n, localVue, router, store })
     expect(wrapper.findAll('.app-sidebar__container__menu__item--documents').exists()).toBeTruthy()
   })
 
   it('should NOT display the link to analyze my documents according to config', () => {
     Murmur.config.set('manageDocuments', false)
-    const wrapper = shallowMount(AppSidebar, { i18n, localVue, store, router })
+    const wrapper = shallowMount(AppSidebar, { i18n, localVue, router, store })
     expect(wrapper.findAll('.app-sidebar__container__menu__item--documents').exists()).toBeFalsy()
   })
 
   it('should NOT display the link to the analyse page in SERVER mode', () => {
     Murmur.config.merge(mode('SERVER'))
-    const wrapper = shallowMount(AppSidebar, { i18n, localVue, store, router })
+    const wrapper = shallowMount(AppSidebar, { i18n, localVue, router, store })
     expect(wrapper.findAll('.app-sidebar__container__menu__item--documents').exists()).toBeFalsy()
   })
 
   it('should display the github help link', () => {
     Murmur.config.merge(mode('LOCAL'))
-    const wrapper = shallowMount(AppSidebar, { i18n, localVue, store, router })
+    const wrapper = shallowMount(AppSidebar, { i18n, localVue, router, store })
     expect(wrapper.find('.app-sidebar__container__menu__item--help a').attributes('href')).toEqual(expect.stringContaining('github.com'))
   })
 
   it('should display the help link in SERVER mode', () => {
     Murmur.config.merge(mode('SERVER'))
-    const wrapper = shallowMount(AppSidebar, { i18n, localVue, store, router })
-    expect(wrapper.find('.app-sidebar__container__menu__item--help a').attributes('href')).toBe('https://support.cloud.icij.org/')
+    const wrapper = shallowMount(AppSidebar, { i18n, localVue, router, store })
+    expect(wrapper.find('.app-sidebar__container__menu__item--help a').attributes('href')).toBe('https://github.com/ICIJ/datashare/wiki/Datashare-Support')
   })
 
   it('should NOT display a logout link', () => {
     Murmur.config.set('mode', 'LOCAL')
-    const wrapper = shallowMount(AppSidebar, { i18n, localVue, store, router })
+    const wrapper = shallowMount(AppSidebar, { i18n, localVue, router, store })
     expect(wrapper.findAll('.app-sidebar__container__menu__item--logout').exists()).toBeFalsy()
   })
 
   it('should display a logout link in server mode', () => {
     Murmur.config.set('mode', 'SERVER')
-    const wrapper = shallowMount(AppSidebar, { i18n, localVue, store, router })
+    const wrapper = shallowMount(AppSidebar, { i18n, localVue, router, store })
     expect(wrapper.findAll('.app-sidebar__container__menu__item--logout').exists()).toBeTruthy()
   })
 
   it('should NOT display the Mounted location component if not in manageDocuments mode', () => {
     Murmur.config.set('manageDocuments', false)
-    const wrapper = shallowMount(AppSidebar, { i18n, localVue, store, router })
+    const wrapper = shallowMount(AppSidebar, { i18n, localVue, router, store })
     expect(wrapper.findAll('.app-sidebar__data-location').exists()).toBeFalsy()
   })
 
   it('should display the Mounted location component if in manageDocuments mode', () => {
     Murmur.config.set('manageDocuments', true)
-    const wrapper = shallowMount(AppSidebar, { i18n, localVue, store, router })
+    const wrapper = shallowMount(AppSidebar, { i18n, localVue, router, store })
     expect(wrapper.findAll('.app-sidebar__data-location').exists()).toBeTruthy()
   })
 
   it('should disable 4 menu links if no projects', () => {
     Murmur.config.set('datashare_projects', [])
-    const wrapper = shallowMount(AppSidebar, { i18n, localVue, store, router })
+    const wrapper = shallowMount(AppSidebar, { i18n, localVue, router, store })
     expect(wrapper.findAll('.app-sidebar__container__menu__item__link--disabled')).toHaveLength(4)
   })
 
   it('should enable menu links if projects', () => {
     Murmur.config.set('datashare_projects', ['project'])
-    const wrapper = shallowMount(AppSidebar, { i18n, localVue, store, router })
+    const wrapper = shallowMount(AppSidebar, { i18n, localVue, router, store })
     expect(wrapper.findAll('.app-sidebar__container__menu__item__link--disabled').exists()).toBeFalsy()
   })
 })
