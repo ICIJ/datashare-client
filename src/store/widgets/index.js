e3b8bc08a46db941598f530fbacd4dd42acb320c
Pierre Romera <hello@pirhoo.com>
2020-06-17 17:57:13 +0200

    Add order to each widget

--- src/store/widgets/index.js
+++ src/store/widgets/index.js
@@ -7,12 +7,39 @@ export { default as WidgetListGroup } from './WidgetListGroup'
 export { default as WidgetText } from './WidgetText'
 
 const widgets = [
-  { name: 'default-text', card: true, cols: 6, type: 'WidgetText', title: 'Insights' },
-  { name: 'file-barometer', card: true, cols: 3, type: 'WidgetFileBarometer' },
-  { name: 'disk-usage', card: true, cols: 3, type: 'WidgetDiskUsage' },
-  { name: 'documents-by-creation-date', card: true, cols: 12, type: 'WidgetDocumentsByCreationDateByPath', title: 'Number of documents by creation date' },
+  {
+    name: 'default-text',
+    order: 10,
+    card: true,
+    cols: 6,
+    type: 'WidgetText',
+    title: 'Insights'
+  },
+  {
+    name: 'file-barometer',
+    order: 20,
+    card: true,
+    cols: 3,
+    type: 'WidgetFileBarometer'
+  },
+  {
+    name: 'disk-usage',
+    order: 30,
+    card: true,
+    cols: 3,
+    type: 'WidgetDiskUsage'
+  },
+  {
+    name: 'documents-by-creation-date',
+    order: 40,
+    card: true,
+    cols: 12,
+    type: 'WidgetDocumentsByCreationDateByPath',
+    title: 'Number of documents by creation date'
+  },
   {
     name: 'default-list',
+    order: 50,
     card: true,
     cols: 12,
     type: 'WidgetListGroup',
