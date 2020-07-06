3464923104627e424b21ec227f601a112c0e9885
Anne L'Hôte <anne.lhote@gmail.com>
2020-07-06 08:36:50 +0000

    [ICIJ/datashare#545] Replace file type descriptions

--- src/components/DocumentTypeCard.vue
+++ src/components/DocumentTypeCard.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="document-type-card d-flex">
-    <document-thumbnail :document="document" class="mr-2 mb-2" crop />
+    <document-thumbnail :document="document" class="mr-2 mb-2" crop></document-thumbnail>
     <div>
       <p class="m-0">
         {{ localizedDescription }}
         <strong v-if="!document.hasStandardExtension" class="font-weight-bold" v-html="$t('search.nav.document.extensionWarning', { extension: document.standardExtension })"></strong>
       </p>
-      <p class="bg-warning mb-0 mt-2 p-2" v-if="document.hasContentTypeWarning">
-        <fa icon="exclamation-triangle" class="mr-1" />
+      <p class="bg-warning mb-0 mt-2 p-2 text-dark" v-if="document.hasContentTypeWarning">
+        <fa icon="exclamation-triangle" class="mr-1"></fa>
         {{ localizedContentTypeWarning }}
       </p>
     </div>
@@ -15,7 +15,7 @@
 </template>
 
 <script>
-import DocumentThumbnail from './DocumentThumbnail.vue'
+import DocumentThumbnail from '@/components/DocumentThumbnail'
 
 /**
  * A small card to display information about the content-type of a document.
@@ -36,11 +36,11 @@ export default {
   computed: {
     localizedDescription () {
       const descriptions = this.document.contentTypeDescription
-      return descriptions[this.$i18n.locale] || descriptions['en']
+      return descriptions[this.$i18n.locale] || descriptions.en
     },
     localizedContentTypeWarning () {
       const warnings = this.document.contentTypeWarning
-      return warnings[this.$i18n.locale] || warnings['en']
+      return warnings[this.$i18n.locale] || warnings.en
     }
   }
 }
