573bd8929fd7e590e9f2c8a11749568d7b722e55
Anne L'Hôte <anne.lhote@gmail.com>
2020-07-22 15:06:01 +0000

    [ICIJ/datashare#558][ci] Fix tests

--- tests/unit/specs/pages/Api.spec.js
+++ tests/unit/specs/pages/Api.spec.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { createLocalVue, mount, shallowMount } from '@vue/test-utils'
+import { removeCookie, setCookie } from 'tiny-cookie'
 
 import Api from '@/api'
 import { Core } from '@/core'
@@ -17,11 +18,16 @@ describe('Api.vue', () => {
   const { i18n, localVue, router, store } = Core.init(createLocalVue()).useAll()
   let wrapper = null
 
+  beforeAll(() => setCookie(process.env.VUE_APP_DS_COOKIE_NAME, { login: 'doe' }, JSON.stringify))
+
   beforeEach(() => {
     wrapper = shallowMount(ApiPage, { i18n, localVue, router, store })
   })
 
-  afterAll(() => jest.unmock('axios'))
+  afterAll(() => {
+    jest.unmock('axios')
+    removeCookie(process.env.VUE_APP_DS_COOKIE_NAME)
+  })
 
   it('should display a button to generate the API key but default', () => {
     expect(wrapper.findAll('.api__explanation b-button-stub')).toHaveLength(1)
@@ -44,15 +50,16 @@ describe('Api.vue', () => {
     expect(wrapper.find('.row .col-6').text()).toBe('123456abcdef')
   })
 
-  it('should request the API key', () => {
+  it('should request the API key', async () => {
     wrapper = mount(ApiPage, { i18n, localVue, router, store })
     axios.request.mockClear()
 
-    wrapper.find('.api__explanation .btn').trigger('click')
+    await wrapper.find('.api__explanation .btn').trigger('click')
+    await wrapper.vm.$nextTick()
 
     expect(axios.request).toBeCalledTimes(1)
     expect(axios.request).toBeCalledWith(expect.objectContaining({
-      url: Api.getFullUrl('/api/key/create'),
+      url: Api.getFullUrl('/api/key/doe'),
       method: 'PUT'
     }))
   })
