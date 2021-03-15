443bd6085bc66e9c42d1f8299866a43d3e06393a
Maxime Vanza <maximevanza@live.fr>
2021-03-15 08:29:24 +0000

    [ICIJ/datashare#761] revert previous commit and merge new one

--- tests/unit/specs/store/filters/FilterPath.spec.js
+++ tests/unit/specs/store/filters/FilterPath.spec.js
@@ -5,12 +5,12 @@ describe('FilterPath.js', () => {
   it('should add slash to path in json if path doesnt have one', () => {
     const filter = new FilterPath({})
     const q = filter.queryBuilder(bodybuilder(), { name: 'path', values: ['/home/foo/bar'] }, 'query')
-    expect(JSON.stringify(q.build())).toContain('{\"query\":{\"bool\":{\"query\":{\"prefix\":{\"path\":\"/home/foo/bar/\"}}}}}')
+    expect(JSON.stringify(q.build())).toContain('{\"query\":{\"bool\":{\"query\":{\"prefix\":{\"dirname.tree\":\"/home/foo/bar/\"}}}}}')
   })
 
   it('should NOT add slash to path in json if path already have one', () => {
     const filter = new FilterPath({})
     const q = filter.queryBuilder(bodybuilder(), { name: 'path', values: ['/home/foo/bar/'] }, 'query')
-    expect(JSON.stringify(q.build())).toContain('{\"query\":{\"bool\":{\"query\":{\"prefix\":{\"path\":\"/home/foo/bar/\"}}}}}')
+    expect(JSON.stringify(q.build())).toContain('{\"query\":{\"bool\":{\"query\":{\"prefix\":{\"dirname.tree\":\"/home/foo/bar/\"}}}}}')
   })
 })
