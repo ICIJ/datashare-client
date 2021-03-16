5955373d38f91d8d7ad0533735b8c439589ba348
Anne L'Hôte <anne.lhote@gmail.com>
2021-03-16 11:51:53 +0000

    [typo] .

--- src/router/guards.js
+++ src/router/guards.js
@@ -34,7 +34,7 @@ export default ({ router, auth, store, config, i18n, setPageTitle }) => {
   }
 
   function checkUserProjects (to, from, next) {
-    // @depracated this load the list from a depracated list of project for retro-compatibility
+    // @deprecated this load the list from a deprecated list of project for retro-compatibility
     const legacyProjects = config.get('datashare_projects', [])
     const projects = config.get('groups_by_applications.datashare', [])
     const allProjects = [...projects, ...legacyProjects]
