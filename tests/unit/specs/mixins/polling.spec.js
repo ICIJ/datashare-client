4725fb8cdbada61ffcf51ef9ec9504dfc5be4319
Pierre Romera <hello@pirhoo.com>
2021-07-21 08:46:30 +0000

    refactor: use helper functions

--- tests/unit/specs/mixins/polling.spec.js
+++ tests/unit/specs/mixins/polling.spec.js
@@ -1,7 +1,11 @@
 import { createLocalVue, shallowMount } from '@vue/test-utils'
 import polling from '@/mixins/polling'
 
+// A few helper functions
 const flushPromises = () => new Promise(resolve => setImmediate(resolve))
+const flushPromisesAndPendingTimers = async () => jest.runOnlyPendingTimers() && await flushPromises()
+const flushPromisesAndAdvanceTimers = async time => jest.advanceTimersByTime(time) && await flushPromises()
+
 // Use fake timers to control times!
 // @see https://jestjs.io/fr/docs/timer-mocks
 jest.useFakeTimers()
@@ -18,19 +22,16 @@ describe('polling mixin', () => {
   it('should register a polling function repeatedly called', async () => {
     const fn = jest.fn().mockReturnValue(Promise.resolve(true))
     wrapper.vm.registerPoll({ fn, timeout: 1000 })
-    jest.runOnlyPendingTimers()
-    await flushPromises()
+    await flushPromisesAndPendingTimers()
     expect(fn).toBeCalledTimes(1)
-    jest.runOnlyPendingTimers()
-    await flushPromises()
+    await flushPromisesAndPendingTimers()
     expect(fn).toBeCalledTimes(2)
   })
 
   it('should register a polling function with function to calculate its timeout', async () => {
     const fn = jest.fn().mockReturnValue(Promise.resolve(true))
     wrapper.vm.registerPoll({ fn, timeout: () => 1000 })
-    jest.runOnlyPendingTimers()
-    await flushPromises()
+    await flushPromisesAndPendingTimers()
     expect(fn).toBeCalledTimes(1)
   })
 
@@ -39,8 +40,7 @@ describe('polling mixin', () => {
     const fnFoo = jest.fn().mockReturnValue(Promise.resolve(true))
     wrapper.vm.registerPoll({ fn: fnBar, timeout: 1000 })
     wrapper.vm.registerPoll({ fn: fnFoo, timeout: 1000 })
-    jest.runOnlyPendingTimers()
-    await flushPromises()
+    await flushPromisesAndPendingTimers()
     expect(fnBar).toBeCalledTimes(1)
     expect(fnFoo).toBeCalledTimes(1)
   })
@@ -48,22 +48,18 @@ describe('polling mixin', () => {
   it('should register one polling function called once when the promise returns false', async () => {
     const fn = jest.fn().mockReturnValue(Promise.resolve(false))
     wrapper.vm.registerPoll({ fn, timeout: 1000 })
-    jest.runOnlyPendingTimers()
-    await flushPromises()
+    await flushPromisesAndPendingTimers()
     expect(fn).toBeCalledTimes(1)
-    jest.runOnlyPendingTimers()
-    await flushPromises()
+    await flushPromisesAndPendingTimers()
     expect(fn).toBeCalledTimes(1)
   })
 
   it('should register one polling function called once when the promise is rejected', async () => {
     const fn = jest.fn().mockReturnValue(Promise.reject(new Error()))
     wrapper.vm.registerPoll({ fn, timeout: 1000 })
-    jest.runOnlyPendingTimers()
-    await flushPromises()
+    await flushPromisesAndPendingTimers()
     expect(fn).toBeCalledTimes(1)
-    jest.runOnlyPendingTimers()
-    await flushPromises()
+    await flushPromisesAndPendingTimers()
     expect(fn).toBeCalledTimes(1)
   })
 
@@ -71,26 +67,21 @@ describe('polling mixin', () => {
     let count = 0
     const fn = jest.fn().mockImplementation(async () => ++count < 2)
     wrapper.vm.registerPoll({ fn, timeout: 1000 })
-    jest.runOnlyPendingTimers()
-    await flushPromises()
+    await flushPromisesAndPendingTimers()
     expect(fn).toBeCalledTimes(1)
-    jest.runOnlyPendingTimers()
-    await flushPromises()
+    await flushPromisesAndPendingTimers()
     expect(fn).toBeCalledTimes(2)
-    jest.runOnlyPendingTimers()
-    await flushPromises()
+    await flushPromisesAndPendingTimers()
     expect(fn).toBeCalledTimes(2)
   })
 
   it('should not call a polling function after the component is destroy', async () => {
     const fn = jest.fn().mockReturnValue(Promise.resolve(true))
     wrapper.vm.registerPoll({ fn, timeout: 1000 })
-    jest.runOnlyPendingTimers()
-    await flushPromises()
+    await flushPromisesAndPendingTimers()
     expect(fn).toBeCalledTimes(1)
     wrapper.destroy()
-    jest.runOnlyPendingTimers()
-    await flushPromises()
+    await flushPromisesAndPendingTimers()
     expect(fn).toBeCalledTimes(1)
   })
 
@@ -113,25 +104,20 @@ describe('polling mixin', () => {
   it('should call a polling function 2 times in 2000ms', async () => {
     const fn = jest.fn().mockReturnValue(Promise.resolve(true))
     wrapper.vm.registerPoll({ fn, timeout: 1000 })
-    jest.advanceTimersByTime(500)
-    await flushPromises()
+    await flushPromisesAndAdvanceTimers(500)
     expect(fn).toBeCalledTimes(0)
-    jest.advanceTimersByTime(500)
-    await flushPromises()
+    await flushPromisesAndAdvanceTimers(500)
     expect(fn).toBeCalledTimes(1)
-    jest.advanceTimersByTime(500)
-    await flushPromises()
+    await flushPromisesAndAdvanceTimers(500)
     expect(fn).toBeCalledTimes(1)
-    jest.advanceTimersByTime(500)
-    await flushPromises()
+    await flushPromisesAndAdvanceTimers(500)
     expect(fn).toBeCalledTimes(2)
   })
 
   it('should call a polling immediatly even before its timeout', async () => {
     const fn = jest.fn().mockReturnValue(Promise.resolve(true))
     wrapper.vm.registerPoll({ fn, timeout: 1000, immediate: true })
-    jest.advanceTimersByTime(500)
-    await flushPromises()
+    await flushPromisesAndAdvanceTimers(500)
     expect(fn).toBeCalledTimes(1)
   })
 
@@ -139,8 +125,7 @@ describe('polling mixin', () => {
     const fn = jest.fn().mockReturnValue(Promise.resolve(true))
     wrapper.vm.registerPollOnce({ fn, timeout: 1000, immediate: true })
     wrapper.vm.registerPollOnce({ fn, timeout: 1000, immediate: true })
-    jest.runOnlyPendingTimers()
-    await flushPromises()
+    await flushPromisesAndPendingTimers()
     expect(fn).toBeCalledTimes(1)
   })
 
@@ -148,8 +133,7 @@ describe('polling mixin', () => {
     const fn = jest.fn().mockReturnValue(Promise.resolve(true))
     wrapper.vm.registerPoll({ fn, timeout: 1000, immediate: true })
     wrapper.vm.registerPoll({ fn, timeout: 1000, immediate: true })
-    jest.runOnlyPendingTimers()
-    await flushPromises()
+    await flushPromisesAndPendingTimers()
     expect(fn).toBeCalledTimes(2)
   })
 })
