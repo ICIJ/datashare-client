4d7f23388c4897199214ac6576ae394f3864ddc3
Maxime Vanza <maximevanza@live.fr>
2021-10-04 17:41:08 +0000

    [ICIJ/datashare#846] adds tooltip with document name in grid view

--- src/components/SearchResultsGrid.vue
+++ src/components/SearchResultsGrid.vue
@@ -5,10 +5,12 @@
       <div class="search-results-grid__items">
         <div v-for="document in response.hits" :key="document.id" class="search-results-grid__items__item d-flex flex-column border rounded">
           <document-actions :document="document" class="search-results-grid__items__item__actions m-2" :is-download-allowed="isDownloadAllowed" />
-          <router-link class="flex-grow-1 search-results-grid__items__item__thumbnail" :to="{ name: 'document', params: document.routerParams, query: { q: query } }">
+          <router-link class="flex-grow-1 search-results-grid__items__item__thumbnail"
+                       :to="{ name: 'document', params: document.routerParams, query: { q: query } }" :title="document.slicedNameToString">
             <document-thumbnail :document="document" size="md" />
           </router-link>
-          <router-link class="search-results-grid__items__item__title py-2 px-3 small" :to="{ name: 'document', params: document.routerParams }">
+          <router-link class="search-results-grid__items__item__title py-2 px-3 small" :to="{ name: 'document', params: document.routerParams }"
+                       :title="document.slicedNameToString">
             <document-sliced-name :document="document" active-text-truncate text-truncate-rtl-attachments/>
           </router-link>
         </div>
