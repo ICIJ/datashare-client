fb853a4ec2ca4b0a3b1a28ce3732a045260dd721
Pierre Romera <hello@pirhoo.com>
2022-03-08 15:24:26 +0100

    feat: add search to several indices

--- src/components/EmailString.vue
+++ src/components/EmailString.vue
@@ -8,11 +8,11 @@
           <div class="text-muted small font-weight-normal py-2">{{ emailWithoutName || email }}</div>
         </template>
         <div class="text-right email-string__popover__content">
-          <router-link :to="{ name: 'search', query: { q: qReceived, index } }" class="btn btn-sm btn-primary">
+          <router-link :to="{ name: 'search', query: { q: qReceived, indices } }" class="btn btn-sm btn-primary">
             <fa mask="square" icon="arrow-down" transform="shrink-3 up-2" class="email-string__popover__content__icon"></fa>
             {{ $t('email.receivedLink') }}
           </router-link>
-          <router-link :to="{ name: 'search', query: { q: qSent, index } }" class="btn btn-sm btn-primary ml-1">
+          <router-link :to="{ name: 'search', query: { q: qSent, indices } }" class="btn btn-sm btn-primary ml-1">
             <fa mask="square" icon="arrow-up" transform="shrink-3 down-2" class="email-string__popover__content__icon"></fa>
             {{ $t('email.sentLink') }}
           </router-link>
@@ -25,7 +25,7 @@
 <script>
 import trim from 'lodash/trim'
 
-const EMAIL_REGEX = new RegExp('(.+)\<(.+)\>', 'i')
+const EMAIL_REGEX = /(.+)\<(.+)\>/i
 
 /**
  * Rich email display with shortcuts to the global search.
@@ -67,8 +67,8 @@ export default {
       const matches = String(this.email).match(EMAIL_REGEX)
       return matches ? trim(matches[2]) : null
     },
-    index () {
-      return this.$store.state.search.index
+    indices () {
+      return this.$store.state.search.indices
     },
     qReceived () {
       const field = 'metadata.tika_metadata_message_to'
