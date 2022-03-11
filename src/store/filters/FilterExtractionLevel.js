2c17811d1860db150c0230d58d780b7db832f164
Pierre Romera <hello@pirhoo.com>
2022-03-11 16:30:36 +0000

    feat: sort extraction level alphabeticaly by default

--- src/store/filters/FilterExtractionLevel.js
+++ src/store/filters/FilterExtractionLevel.js
@@ -2,6 +2,11 @@ import FilterText from './FilterText'
 import { getExtractionLevelTranslationKey } from '@/utils/utils'
 
 export default class FilterExtractionLevel extends FilterText {
+  constructor (options) {
+    super(options)
+    this.sort = '_key'
+    this.sortByOrder = 'asc'
+  }
   itemLabel (item) {
     return getExtractionLevelTranslationKey(item.key)
   }
