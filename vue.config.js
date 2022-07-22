62386d4a2c665e90a593e1f3b664fd0230e7a67a
Pierre Romera <hello@pirhoo.com>
2022-07-22 19:00:56 +0200

    build: deactivate parralel build under 7 CPUs

--- vue.config.js
+++ vue.config.js
@@ -13,6 +13,7 @@ process.env.VUE_APP_GIT_BRANCH = gitRevisionPlugin.branch()
 module.exports = {
   lintOnSave: false,
   runtimeCompiler: true,
+  parallel: require('os').cpus().length > 6,
   chainWebpack: config => {
     // Resource loader configuration:
     // 4 named rules must include this loader
