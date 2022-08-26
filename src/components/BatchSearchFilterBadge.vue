0ffa24f408dcfee2d08c9c149c941bc5ced18803
Caroline Desprat <cdesprat@icij.org>
2022-08-26 10:42:57 +0000

    refactor: create batch search filter badge component

---
+++ src/components/BatchSearchFilterBadge.vue
@@ -0,0 +1,11 @@
+<template>
+  <b-badge variant="secondary" class="position-absolute p-2 rounded-circle" >
+    {{ }}
+  </b-badge>
+</template>
+
+<script>
+export default {
+  name: 'BatchSearchFiltersBadge'
+}
+</script>
