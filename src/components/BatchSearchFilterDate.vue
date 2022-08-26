46c291af681f539c22859861ce22bc66e6cf8265
Caroline Desprat <cdesprat@icij.org>
2022-08-26 12:09:14 +0000

    feat: create batch search filter date

---
+++ src/components/BatchSearchFilterDate.vue
@@ -0,0 +1,63 @@
+<template>
+  <batch-search-filter :id="id" :label="label" :active="isActive">
+    <keep-alive>
+      <date-picker
+        is-range
+        color="gray"
+        v-model="selectedDateRange"
+        :model-config="{ type: 'number' }"
+        :locale="locale"
+        :key="locale">
+      </date-picker>
+    </keep-alive>
+  </batch-search-filter>
+</template>
+
+<script>
+import BatchSearchFilter from '@/components/BatchSearchFilter'
+import DatePicker from 'v-calendar/lib/components/date-picker.umd'
+export default {
+  name: 'BatchSearchFilterDate',
+  components: {
+    BatchSearchFilter,
+    DatePicker
+  },
+  model: {
+    prop: 'selectedDateRange',
+    event: 'update'
+  },
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    label: {
+      type: String,
+      required: true
+    },
+    date: {
+      type: Object
+    }
+  },
+  methods: {
+
+  },
+  computed: {
+    isActive () {
+      return this.date != null
+    },
+    locale () {
+      return this.$i18n.locale
+    },
+    selectedDateRange: {
+      get () {
+        return this.date
+      },
+      set (values) {
+        this.$emit('update', values)
+      }
+    }
+
+  }
+}
+</script>
