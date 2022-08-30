5f1a1960bdc6eb4b32adba8d93671fca4dcb6754
Caroline Desprat <cdesprat@icij.org>
2022-08-30 16:19:08 +0000

    refactor: create component for batch search query

---
+++ src/components/BatchSearchFilterQuery.vue
@@ -0,0 +1,57 @@
+<template>
+  <form class="batch-search-filter-query col-md-6 px-0" @submit.prevent="filterByQuery">
+    <search-bar-input v-model="search"
+                      :placeholder="$t('batchSearch.placeholder')"
+                      class="batch-search__search-bar__input"
+                      hide-tips
+    >
+      <template #fields>
+        <search-bar-input-dropdown
+          v-model="field"
+          :fieldOptions="fieldOptions"
+          :fieldOptionsPath="fieldOptionsPath"
+          class="batch-search-filter-query__field"
+        />
+      </template>
+    </search-bar-input>
+  </form>
+</template>
+
+<script>
+import utils from '@/mixins/utils'
+import SearchBarInput from '@/components/SearchBarInput'
+import SearchBarInputDropdown from '@/components/SearchBarInputDropdown'
+
+export default {
+  name: 'BatchSearchFilterQuery',
+  components: { SearchBarInputDropdown, SearchBarInput },
+  mixins: [utils],
+  data () {
+    return {
+      field: 'all',
+      search: this.$route.query?.query ?? ''
+    }
+  },
+  methods: {
+    filterByQuery () {
+      const params = this.$route.query
+      return this.$router.push({
+        name: 'batch-search',
+        query: { ...params, page: 1, query: this.search }
+      })
+    }
+  },
+  computed: {
+    fieldOptions () {
+      const options = ['all', 'name', 'description']
+      if (this.isServer) {
+        options.push('user_id')
+      }
+      return options
+    },
+    fieldOptionsPath () {
+      return ['batchSearch', 'field']
+    }
+  }
+}
+</script>
