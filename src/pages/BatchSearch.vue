89bfe5173246c148377b11612fedc6ffabcf73a7
Caroline Desprat <cdesprat@icij.org>
2022-08-31 15:03:40 +0000

    style: use card for empty batch search

--- src/pages/BatchSearch.vue
+++ src/pages/BatchSearch.vue
@@ -1,31 +1,31 @@
 <template>
   <div class="batch-search container h-100 pt-4">
-    <v-wait for="load haveBatchSearch" class="batch-search__wait">
-      <fa icon="circle-notch" spin size="2x" class="d-flex mx-auto mt-5" slot="waiting"></fa>
+    <v-wait class="batch-search__wait" for="load haveBatchSearch">
+      <fa slot="waiting" class="d-flex mx-auto mt-5" icon="circle-notch" size="2x" spin></fa>
       <template v-if="hasBatchSearch">
-      <div class="d-flex flex-wrap align-items-center">
-        <batch-search-filter-query class="batch-search__search-bar my-1"/>
-        <batch-search-clear-filters class="batch-search__clear-filter-btn my-1"/>
-        <b-btn class="ml-auto my-1" variant="primary" @click="$refs['batch-search-form'].show()">
-          <fa class="mr-1" icon="plus"></fa>
-          <span class="text-nowrap">{{ $t('batchSearch.heading') }}</span>
-        </b-btn>
-      </div>
-      <batch-search-table/>
-    </template>
-    <template v-else>
-      <div class="text-center">
-        <div v-html="noBatchSearch" class="py-2"/>
-        <b-btn class="ml-auto my-1" variant="primary" @click="$refs['batch-search-form'].show()">
-          <fa class="mr-1" icon="plus"></fa>
-          <span class="text-nowrap">{{ $t('batchSearch.heading') }}</span>
-        </b-btn>
-      </div>
-    </template>
-    <b-modal ref="batch-search-form" :title="$t('batchSearch.heading')" body-class="p-0" hide-footer size="md">
-      <batch-search-form hide-border hide-title @submit="$refs['batch-search-form'].hide()"></batch-search-form>
-    </b-modal>
-      </v-wait>
+        <div class="d-flex flex-wrap align-items-center">
+          <batch-search-filter-query class="batch-search__search-bar my-1"/>
+          <batch-search-clear-filters class="batch-search__clear-filter-btn m-1"/>
+          <b-btn class="ml-auto my-1" variant="primary" @click="$refs['batch-search-form'].show()">
+            <fa class="mr-1" icon="plus"></fa>
+            <span class="text-nowrap">{{ $t('batchSearch.heading') }}</span>
+          </b-btn>
+        </div>
+        <batch-search-table/>
+      </template>
+      <template v-else>
+        <div class="card text-center">
+          <div class="py-2" v-html="noBatchSearch"/>
+          <b-btn class="ml-auto my-1" variant="primary" @click="$refs['batch-search-form'].show()">
+            <fa class="mr-1" icon="plus"></fa>
+            <span class="text-nowrap">{{ $t('batchSearch.heading') }}</span>
+          </b-btn>
+        </div>
+      </template>
+      <b-modal ref="batch-search-form" :title="$t('batchSearch.heading')" body-class="p-0" hide-footer size="md">
+        <batch-search-form hide-border hide-title @submit="$refs['batch-search-form'].hide()"></batch-search-form>
+      </b-modal>
+    </v-wait>
   </div>
 </template>
 
@@ -52,7 +52,10 @@ export default {
   methods: {
     async getBatchSearch () {
       this.$wait.start('load haveBatchSearch')
-      await this.$store.dispatch('batchSearch/getBatchSearches', { page: 1, size: 1 })
+      await this.$store.dispatch('batchSearch/getBatchSearches', {
+        page: 1,
+        size: 1
+      })
       this.$wait.end('load haveBatchSearch')
     }
   },
