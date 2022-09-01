bb727ac6ba7742da56d80b54f61d0a0f599bbce2
Caroline Desprat <cdesprat@icij.org>
2022-09-01 13:59:15 +0000

    test: submit query once

--- tests/unit/specs/components/BatchSearchFilterQuery.spec.js
+++ tests/unit/specs/components/BatchSearchFilterQuery.spec.js
@@ -1,4 +1,4 @@
-import { createLocalVue, shallowMount } from '@vue/test-utils'
+import { createLocalVue, shallowMount, mount } from '@vue/test-utils'
 
 import BatchSearchFilterQuery from '@/components/BatchSearchFilterQuery'
 import { Core } from '@/core'
@@ -45,6 +45,21 @@ describe('BatchSearchFilterQuery.vue', () => {
       await wrapper.vm.filterByQuery()
       expect(wrapper.vm.$route.query.query).toBe('')
       expect(wrapper.vm.$route.query.page).toBe(1)
+    })
+
+    it('should execute "filterByQuery" on submit one time', async () => {
+      await router.push({
+        name: 'batch-search',
+        query: { query: 'search_from_url' }
+      })
+      const wrapper = mount(BatchSearchFilterQuery, { i18n, localVue, router })
+
+      const searchSpy = jest.spyOn(wrapper.vm, 'filterByQuery')
+      expect(searchSpy).not.toBeCalled()
+
+      await wrapper.find('.btn-dark').trigger('submit')
+
+      expect(searchSpy).toBeCalledTimes(1)
     })
   })
 })
