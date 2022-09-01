3a7706b58ce89438f4ce0c73950337470068552d
Caroline Desprat <cdesprat@icij.org>
2022-09-01 18:09:47 +0000

    fix: remove useless test

--- tests/unit/specs/components/SearchBarInputDropdown.spec.js
+++ tests/unit/specs/components/SearchBarInputDropdown.spec.js
@@ -8,22 +8,10 @@ describe('SearchBarInputDropdown.vue', function () {
   const { i18n, localVue, store } = Core.init(createLocalVue()).useAll()
   const router = new VueRouter()
 
-  let wrapper = null
-  const shallowMountFactory = (propsData = {}) => {
-    return shallowMount(SearchBarInputDropdown, { i18n, localVue, router, store, propsData })
-  }
-
   it('should display a dropdown with 8 options fields with All fields selected by default', () => {
-    wrapper = shallowMountFactory()
-    expect(wrapper.find('.search-bar-input-dropdown').element).toBeTruthy()
-    expect(wrapper.find('.search-bar-input-dropdown').element.getAttribute('text')).toBe('All fields')
+    const wrapper = shallowMount(SearchBarInputDropdown, { i18n, localVue, router, store })
+    expect(wrapper.find('.search-bar-input-fields').element).toBeTruthy()
+    expect(wrapper.find('.search-bar-input-fields').element.getAttribute('text')).toBe('All fields')
     expect(wrapper.findAll('b-dropdown-item-stub')).toHaveLength(8)
-  })
-
-  it('should show tips when there is a query', () => {
-    wrapper = shallowMountFactory()
-    expect(wrapper.find('.search-bar-input__tips-addon--active').element).toBeFalsy()
-    wrapper = shallowMountFactory({ query: 'query' })
-    expect(wrapper.find('.search-bar-input__tips-addon--active').element).toBeTruthy()
   })
 })
