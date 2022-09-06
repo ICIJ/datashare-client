42bf0bb87999bb636f510fa9f463188c8af3ca4d
Pierre Romera <hello@pirhoo.com>
2022-09-06 09:07:33 +0000

    refactor: reduce code

--- src/pages/BatchSearch.vue
+++ src/pages/BatchSearch.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="batch-search container h-100 pt-4">
     <v-wait class="batch-search__wait" for="load haveBatchSearch">
-      <fa slot="waiting" class="d-flex mx-auto mt-5" icon="circle-notch" size="2x" spin></fa>
+      <fa slot="waiting" class="d-flex mx-auto mt-5" icon="circle-notch" size="2x" spin />
       <template v-if="hasBatchSearch">
         <div class="d-flex flex-wrap align-items-center">
           <batch-search-filter-query class="batch-search__search-bar my-1"/>
           <batch-search-clear-filters class="batch-search__clear-filter-btn m-1"/>
-          <b-btn class="ml-auto my-1" variant="primary" @click="$refs['batch-search-form'].show()">
-            <fa class="mr-1" icon="plus"></fa>
-            <span class="text-nowrap">{{ $t('batchSearch.heading') }}</span>
+          <b-btn class="ml-auto my-1 text-nowrap" variant="primary" @click="$refs['batch-search-form'].show()">
+            <fa class="mr-1" icon="plus" />
+            {{ $t('batchSearch.heading') }}
           </b-btn>
         </div>
         <batch-search-table/>
@@ -17,7 +17,7 @@
         <div class="batch-search__none  text-center">
           <div class="batch-search__none__message b-table-empty-row" v-html="noBatchSearch"/>
           <b-btn class="mx-auto my-2" variant="primary" @click="$refs['batch-search-form'].show()">
-            <fa class="mr-1" icon="plus"></fa>
+            <fa class="mr-1" icon="plus" />
             <span class="text-nowrap">{{ $t('batchSearch.heading') }}</span>
           </b-btn>
         </div>
