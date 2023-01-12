25724f6249e69e551efbcf82a09b47e50cc605e8
Pierre Romera <hello@pirhoo.com>
2023-01-12 15:16:06 +0000

    refactor: create a I18nMixin to handle locale settings

---
+++ src/core/I18nMixin.js
@@ -0,0 +1,60 @@
+import { isEmpty } from 'lodash'
+import settings from '@/utils/settings'
+
+/**
+  Mixin class extending the core to add helpers for i18n.
+  @mixin I18nMixin
+  @typicalname datashare
+*/
+const I18nMixin = (superclass) =>
+  class extends superclass {
+    /**
+     * Initialize i18N using the local storage and load
+     * the necessary locale's messages
+     * @memberof I18nMixin.prototype
+     * @returns {Promise}
+     */
+    initializeI18n() {
+      const locale = localStorage.getItem('locale') ?? this.i18n.locale
+      return this.loadI18Locale(locale)
+    }
+
+    /**
+     * Set the active locale both in local stoage and VueI18n.
+     * @memberof I18nMixin.prototype
+     * @param {String} locale - Key of the local (fr, de, en, ja, ...)
+     * @returns {String}
+     */
+    setI18nLocale(locale = settings.defaultLocale) {
+      localStorage.setItem('locale', locale)
+      this.i18n.locale = locale
+      return locale
+    }
+
+    /**
+     * Check the given locale storage was loaded.
+     * @memberof I18nMixin.prototype
+     * @param {String} locale - Key of the local (fr, de, en, ja, ...)
+     * @returns {Boolean}
+     */
+    hasI18Locale(locale) {
+      return !isEmpty(this.i18n.getLocaleMessage(locale))
+    }
+
+    /**
+     * Load i18n messages for the given locale (if needed)
+     * and set it as the current locale.
+     * @memberof I18nMixin.prototype
+     * @returns {Promise}
+     */
+    async loadI18Locale(locale) {
+      if (!this.hasI18Locale(locale)) {
+        const messages = await import(/* webpackChunkName: "[request]" */ '@/lang/' + locale + '.json')
+        this.i18n.setLocaleMessage(locale, messages.default)
+        return this.setI18nLocale(locale)
+      }
+      return this.setI18nLocale(locale)
+    }
+  }
+
+export default I18nMixin
---
+++ tests/unit/specs/core/I18nMixin.spec.js
@@ -0,0 +1,84 @@
+import { createLocalVue } from '@vue/test-utils'
+import { Core } from '@/core'
+
+describe('I18nMixin', () => {
+  let core
+
+  describe('without language in local storage', () => {
+    beforeEach(async () => {
+      localStorage.removeItem('locale')
+      core = Core.init(createLocalVue()).useAll()
+      await core.initializeI18n()
+    })
+
+    afterEach(async () => {
+      await core.loadI18Locale('en')
+    })
+
+    it('should use "en" as default language', () => {
+      expect(core.i18n.locale).toBe('en')
+    })
+
+    it('should have "en" messages', () => {
+      expect(core.hasI18Locale('en')).toBeTruthy()
+    })
+
+    it('should save the "en" local in local storage by default', async () => {
+      expect(localStorage.getItem('locale')).toBe('en')
+    })
+
+    it('should not have "fr" messages', () => {
+      expect(core.hasI18Locale('fr')).toBeFalsy()
+    })
+
+    it('should switch to Spanish (es)', async () => {
+      await core.loadI18Locale('es')
+      expect(core.i18n.locale).toBe('es')
+    })
+
+    it('should have "es" message', async () => {
+      await core.loadI18Locale('es')
+      expect(core.hasI18Locale('es')).toBeTruthy()
+    })
+
+    it('should save the "es" local in local storage', async () => {
+      await core.loadI18Locale('es')
+      expect(localStorage.getItem('locale')).toBe('es')
+    })
+  })
+
+  describe('with german language in local storage', () => {
+    beforeEach(async () => {
+      localStorage.setItem('locale', 'de')
+      core = Core.init(createLocalVue()).useAll()
+      await core.initializeI18n()
+    })
+
+    afterEach(async () => {
+      await core.loadI18Locale('de')
+    })
+
+    it('should use "de" as default language', () => {
+      expect(core.i18n.locale).toBe('de')
+    })
+
+    it('should have "de" messages', () => {
+      expect(core.hasI18Locale('de')).toBeTruthy()
+    })
+
+    it('should switch to Japanese (ja)', async () => {
+      await core.loadI18Locale('ja')
+      expect(core.i18n.locale).toBe('ja')
+    })
+
+    it('should have "ja" mjasage', async () => {
+      await core.loadI18Locale('ja')
+      expect(core.hasI18Locale('ja')).toBeTruthy()
+    })
+
+    it('should save the "ja" local in local storage', async () => {
+      await core.loadI18Locale('ja')
+      expect(localStorage.getItem('locale')).toBe('ja')
+    })
+  })
+})
