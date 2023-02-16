82ba8b4c964a69f2079cc355ece6ee7ef4f862c4
mvanzalu <maximevanza@live.fr>
2023-02-16 15:12:02 +0000

    test: adds tests for TreeBreadcrumb on Windows ICIJ/datashare#1012

--- tests/unit/specs/components/TreeBreadcrumb.spec.js
+++ tests/unit/specs/components/TreeBreadcrumb.spec.js
@@ -40,4 +40,27 @@ describe('TreeBreadcrumb.vue', () => {
     expect(wrapper.find('.tree-breadcrumb__item--root').exists()).toBeTruthy()
     expect(wrapper.find('.tree-breadcrumb__item--abbr').exists()).toBeTruthy()
   })
+
+  it('should display 3 directories with Windows pathSeparator', async () => {
+    config.set('dataDir', 'C:\\home\\foo')
+    config.set('pathSeparator', '\\')
+    const propsData = { path: 'C:\\home\\foo\\bar\\baz', noDatadir: true }
+    const wrapper = shallowMount(TreeBreadcrumb, { i18n, localVue, propsData })
+    expect(wrapper.findAll('.tree-breadcrumb__item')).toHaveLength(3)
+    // One for the root link
+    expect(wrapper.find('.tree-breadcrumb__item--root').exists()).toBeTruthy()
+    // But no abbreviation
+    expect(wrapper.find('.tree-breadcrumb__item--abbr').exists()).toBeFalsy()
+  })
+
+  it('should display 4 directories (including the root and the abbreviation) with Windows pathSeparator', async () => {
+    config.set('dataDir', 'C:\\home\\foo')
+    config.set('pathSeparator', '\\')
+    const propsData = { path: 'C:\\home\\foo\\bar\\baz\\lo\\rem', maxDirectories: 2, noDatadir: true }
+    const wrapper = shallowMount(TreeBreadcrumb, { i18n, localVue, propsData })
+    expect(wrapper.findAll('.tree-breadcrumb__item')).toHaveLength(4)
+    // One for the root link and one for the abbreviation
+    expect(wrapper.find('.tree-breadcrumb__item--root').exists()).toBeTruthy()
+    expect(wrapper.find('.tree-breadcrumb__item--abbr').exists()).toBeTruthy()
+  })
 })
