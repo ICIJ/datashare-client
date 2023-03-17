019044355eb807bb385a888c9800e8df9de5e35a
mvanzalu <maximevanza@live.fr>
2023-03-17 15:11:52 +0000

    refactor: retrieve text/ocr languages in ExtractingForm

--- tests/unit/specs/components/ExtractingFormOcrControl.spec.js
+++ tests/unit/specs/components/ExtractingFormOcrControl.spec.js
@@ -1,7 +1,6 @@
 import { createLocalVue, mount } from '@vue/test-utils'
 import { flushPromises } from 'tests/unit/tests_utils'
 
-import { Api } from '@/api'
 import { Core } from '@/core'
 import ExtractingFormOcrControl from '@/components/ExtractingFormOcrControl'
 
@@ -21,37 +20,37 @@ const TEXT_LANGUAGES = [
 
 describe('ExtractingFormOcrControl.vue', () => {
   let wrapper
-  let wait, store, i18n, api, localVue
-  let spyText, spyOcr
+  let wait, store, i18n, localVue
   beforeAll(async () => {
-    api = new Api()
     localVue = createLocalVue()
-    const core = Core.init(localVue, api).useAll()
+    const core = Core.init(localVue).useAll()
     wait = core.wait
     store = core.store
     i18n = core.i18n
   })
-  afterEach(() => {
-    spyText?.mockClear()
-    spyOcr?.mockClear()
-  })
 
   describe('OCR for italian language is installed', () => {
-    beforeEach(async () => {
-      // Mock textLanguages method
-      spyText = jest.spyOn(Api.prototype, 'textLanguages').mockImplementation(() => TEXT_LANGUAGES)
-      spyOcr = jest.spyOn(Api.prototype, 'ocrLanguages').mockImplementation(() => OCR_LANGUAGES)
-    })
     it('should display an alert indicating that no ocr are installed', async () => {
-      wrapper = mount(ExtractingFormOcrControl, { wait, store, i18n, localVue, propsData: { isoLang: 'ita' } })
+      wrapper = mount(ExtractingFormOcrControl, {
+        wait,
+        store,
+        i18n,
+        localVue,
+        propsData: { isoLang: 'ita', textLanguages: TEXT_LANGUAGES, ocrLanguages: OCR_LANGUAGES, hasTesseract: true }
+      })
       await flushPromises()
       expect(wrapper.find('.extracting_language_form_control__install_ocr_language').exists()).toBe(true)
       expect(wrapper.find('.extracting_language_form_control').text()).not.toContain('Tesseract OCR is not installed.')
     })
 
     it('should display an alert indicating that tesseract is not installed', async () => {
-      wrapper = mount(ExtractingFormOcrControl, { wait, store, i18n, localVue, propsData: { isoLang: 'ita' } })
-      wrapper.setData({ hasTesseract: false })
+      wrapper = mount(ExtractingFormOcrControl, {
+        wait,
+        store,
+        i18n,
+        localVue,
+        propsData: { isoLang: 'ita', textLanguages: TEXT_LANGUAGES, ocrLanguages: OCR_LANGUAGES, hasTesseract: false }
+      })
       await flushPromises()
       expect(wrapper.find('.extracting_language_form_control').text()).toContain('Tesseract OCR is not installed.')
     })
