d124ea0f7706bed1da173428106bfd8865ca69d8
Caroline Desprat <cdesprat@icij.org>
2023-03-30 15:28:52 +0000

    feat: add highlight on cut words
    Co-authored-by: Pierre Romera <pirhoo@users.noreply.github.com>

--- src/utils/strings.js
+++ src/utils/strings.js
@@ -13,8 +13,8 @@ export function addLocalSearchMarksClassByOffsets({ content = '', term = '', off
   const chunks = content.split('')
   // Add mark tag to the corresponding letters
   offsets.forEach((offset) => {
-    const start = offset - delta
-    const end = start + term.length - 1
+    const start = offset - Number(delta)
+    const end = Math.min(start + term.length - 1, chunks.length - 1)
     // Only replace by offset in existing chunk
     if (chunks[start] && chunks[end]) {
       chunks[start] = `<mark class="local-search-term" data-offset="${offset}">${chunks[start]}`
@@ -23,32 +23,6 @@ export function addLocalSearchMarksClassByOffsets({ content = '', term = '', off
   })
   // Then merge letters again
   return chunks.join('')
-}
-
-export function addLocalSearchMarksClassSensitive(content = '<div></div>', localSearchTerm = { label: '' }) {
-  const escapedLocalSearchTerm = localSearchTerm.regex ? localSearchTerm.label : escapeRegExp(localSearchTerm.label)
-  // In case the searched term is split on 2 lines in the content
-  const escapedLocalSearchTermAsRegex = escapedLocalSearchTerm.replace(' ', '( |  |.|..| .)')
-  const regex = new RegExp(`(?![^<]*>)${escapedLocalSearchTermAsRegex}`, 'gms')
-  const localSearchOccurrences = (content.match(regex) || []).length
-  const localSearchIndex = Number(!!localSearchOccurrences)
-  try {
-    if (localSearchOccurrences === 0) throw new Error()
-    const needle = new RegExp(`(${escapedLocalSearchTermAsRegex})`, 'gms')
-    const replacedContent = content.replace(needle, (m) => {
-      const term = m.replace(/(\r\n|\n|\r)/gm, ' ').replace('  ', ' ')
-      return `<mark class="local-search-term">${term}</mark>`
-    })
-
-    return {
-      content: replacedContent,
-      localSearchIndex,
-      localSearchOccurrences
-    }
-    // Silently fails
-  } catch (error) {
-    return { content, localSearchIndex, localSearchOccurrences }
-  }
 }
 
 export function addLocalSearchMarksClass(content = '<div></div>', localSearchTerm = { label: '' }) {
