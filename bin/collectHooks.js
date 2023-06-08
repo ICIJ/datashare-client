90c4319d21aa3425a90c5011fd9f3452ec2420b4
Pierre Romera <hello@pirhoo.com>
2023-06-08 17:34:19 +0000

    fix: correct hook paths

--- bin/collectHooks.js
+++ bin/collectHooks.js
@@ -2,18 +2,19 @@ const Handlebars = require('handlebars')
 const { execSync } = require('child_process')
 const { readFileSync, writeFileSync } = require('fs')
 const { kebabCase, compact } = require('lodash')
-const { basename, join } = require('path')
+const { basename, join, relative } = require('path')
 
 const { repository } = require('../package.json')
 
 const DOC_PATH = join('dist', 'docs')
+const SRC_PATH = join('src')
 
 const build = Handlebars.compile(readFileSync('bin/DOCS.HOOKS.hbs', 'UTF-8'))
 const joinToDoc = (path) => join(DOC_PATH, path)
 
 function srcToDocumentationPath(src) {
   const name = basename(src, '.vue')
-  const path = src.split('/').slice(1, -1).map(kebabCase).join('/')
+  const path = src.split('/').slice(0, -1).map(kebabCase).join('/')
   return `${path}/${name}.md`
 }
 
@@ -21,13 +22,13 @@ function srcToDocumentationPath(src) {
 const occurrences = execSync('git grep --line --no-color --extended-regexp \'<hook name="(.*:\\w*)"\'').toString()
 // Each line is an occurrence, with 3 columns separated by :
 const hooks = compact(occurrences.split('\n')).map((occurrence) => {
-  const component = occurrence.split(':')[0]
+  const component = occurrence.split(':')[0].split('/').slice(1).join('/')
   const line = occurrence.split(':')[1]
   const match = occurrence.split(':').slice(2).join(':')
   const hook = (match.match(/\"(.*:\w*)\"/) || [])[1]
   const path = srcToDocumentationPath(component)
-  const href = new URL(`blob/master/${component}#L${line}`, repository.url)
-  return { component, href, line, path, hook }
+  const source = new URL(`blob/master/src/${component}#L${line}`, repository.url)
+  return { component, source, line, path, hook }
 })
 
 // Build templates using components collections
