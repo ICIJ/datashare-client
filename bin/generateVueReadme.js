e178f748f1c1eeb635919e6bb518c2379f9703ef
Pierre Romera <hello@pirhoo.com>
2023-06-09 19:01:33 +0200

    refactor: make code functional and add JSdoc

--- bin/generateVueReadme.js
+++ bin/generateVueReadme.js
@@ -1,19 +1,47 @@
-const glob = require('glob')
-const { writeFileSync } = require('fs')
-const { capitalize } = require('lodash')
+const glob = require('glob');
+const { writeFileSync } = require('fs');
+const { capitalize } = require('lodash');
 
-const components = {
-  generateReadme() {
-    this.directories.forEach((filepath) => {
-      const title = filepath.split('/').slice(3, -1).map(capitalize).join(' - ')
-      const readmepath = `${filepath}README.md`
-      writeFileSync(readmepath, `# ${title}`)
-    })
-  },
-  get directories() {
-    const mark = true
-    return glob.sync('dist/docs/vue/**/*/', { mark })
-  }
+/**
+ * Generates README files for each directory.
+ */
+function generateReadme() {
+  const directories = getDirectories();
+  directories.forEach((filepath) => {
+    const title = generateTitle(filepath);
+    const readmepath = `${filepath}README.md`;
+    writeReadmeFile(readmepath, title);
+  });
 }
 
-components.generateReadme()
+/**
+ * Retrieves the directories using glob.
+ * @returns {string[]} An array of directory paths.
+ */
+function getDirectories() {
+  const mark = true;
+  return glob.sync('dist/docs/vue/**/*/', { mark });
+}
+
+/**
+ * Generates the title for a directory based on the filepath.
+ * @param {string} filepath - The directory filepath.
+ * @returns {string} The generated title.
+ */
+function generateTitle(filepath) {
+  const pathSegments = filepath.split('/').slice(3, -1);
+  const capitalizedSegments = pathSegments.map(capitalize);
+  return capitalizedSegments.join(' - ');
+}
+
+/**
+ * Writes the README file with the given title.
+ * @param {string} readmepath - The path to the README file.
+ * @param {string} title - The title to be included in the README.
+ */
+function writeReadmeFile(readmepath, title) {
+  const content = `# ${title}`;
+  writeFileSync(readmepath, content);
+}
+
+generateReadme();
