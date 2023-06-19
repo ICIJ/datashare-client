c3674da2a77d02b5ddbc980d63a3cb4e0fc56fa0
Pierre Romera <hello@pirhoo.com>
2023-06-19 10:06:45 +0200

    refactor: lint

--- bin/dmd/helpers/anchorName.js
+++ bin/dmd/helpers/anchorName.js
@@ -6,7 +6,8 @@ const { anchorName: dmdAnchorName } = require('dmd/helpers/ddata')
  * @returns {string} The lowercase anchor name.
  */
 exports.anchorName = function (options) {
-  return dmdAnchorName.call(this, options)
+  return dmdAnchorName
+    .call(this, options)
     .toLowerCase()
     .replace(/[ -_]+/g, '-')
     .replace(/[^a-z0-9_ -]/g, '')
