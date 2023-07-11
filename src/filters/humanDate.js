c5b8c13d3f5f3d65c51dd107fb471b8d1518069c
Caroline Desprat <cdesprat@icij.org>
2023-07-11 10:35:27 +0000

    refactor: use date filters

--- src/filters/humanDate.js
+++ src/filters/humanDate.js
@@ -3,3 +3,10 @@ import moment from 'moment'
 export function humanDate(date, locale) {
   return moment(date).locale(locale).format('Y/MM/DD')
 }
+export function humanLongDate(date, locale) {
+  return moment(date).locale(locale).format('LLL')
+}
+
+export function humanShortDate(date, locale) {
+  return moment(date).locale(locale).format('LLL')
+}
