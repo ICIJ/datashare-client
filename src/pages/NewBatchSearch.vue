c01d660c7d5591fc712a83ed1d4a2f59cde2b7e3
Pierre Romera <hello@pirhoo.com>
2023-07-17 18:54:02 +0200

    feat: create a page to hold the BS form

---
+++ src/pages/NewBatchSearch.vue
@@ -0,0 +1,35 @@
+<script>
+import BatchSearchForm from '@/components/BatchSearchForm'
+
+export default {
+  name: 'NewBatchSearch',
+  components: {
+    BatchSearchForm
+  },
+  computed: {
+    batchSearchRoute() {
+      return { name: 'batch-search-list' }
+    }
+  },
+  methods: {
+    submit() {
+      this.$router.push(this.batchSearchRoute)
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container py-4">
+    <div class="mx-1 mb-2 d-flex">
+      <router-link :to="batchSearchRoute">
+        <fa icon="angle-left" class="mr-1" fixed-width />
+        {{ $t('batchSearch.title') }}
+      </router-link>
+      <span class="border-left border-dark pl-3 ml-3 font-weight-bold">
+        {{ $t('newBatchSearch.title') }}
+      </span>
+    </div>
+    <batch-search-form hide-title @submit="submit"></batch-search-form>
+  </div>
+</template>
