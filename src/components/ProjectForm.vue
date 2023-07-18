d3b8e62f119c620abd1358858954686a456c54d0
Pierre Romera <hello@pirhoo.com>
2023-07-18 16:50:47 +0200

    feat: add ProjectForm component

---
+++ src/components/ProjectForm.vue
@@ -0,0 +1,200 @@
+<script>
+import { every, cloneDeep, kebabCase } from 'lodash'
+
+import { slugger, isUrl } from '@/utils/strings'
+
+export default {
+  name: 'ProjectForm',
+  props: {
+    card: {
+      type: Boolean
+    },
+    disabled: {
+      type: Boolean
+    }
+  },
+  data() {
+    return {
+      form: {
+        name: null,
+        label: null,
+        description: null,
+        logoUrl: null,
+        sourceUrl: null,
+        publisherName: null,
+        maintainerName: null
+      }
+    }
+  },
+  computed: {
+    bodyComponent() {
+      return this.card ? 'b-card-body' : 'div'
+    },
+    footerComponent() {
+      return this.card ? 'b-card-footer' : 'div'
+    },
+    classList() {
+      return this.card ? ['card'] : []
+    },
+    valid() {
+      return every([
+        !this.disabled,
+        this.isPresent(this.form.name),
+        this.isPresent(this.form.label) && !this.isReservedWord(this.form.label),
+        !this.isPresent(this.form.sourceUrl) || this.isUrl(this.form.sourceUrl),
+        !this.isPresent(this.form.logoUrl) || this.isUrl(this.form.logoUrl)
+      ])
+    }
+  },
+  watch: {
+    'form.label': function (label) {
+      // Transform to kebab case first to insert a "minus" when input is using camel case
+      this.form.name = slugger(kebabCase(label)).toLowerCase()
+    }
+  },
+  methods: {
+    isReservedWord(value) {
+      const reserved = ['new', 'edit', 'delete']
+      return reserved.includes(value.toLowerCase())
+    },
+    isUrl(value) {
+      return isUrl(value)
+    },
+    isPresent(value) {
+      return value !== null && value !== ''
+    },
+    isBlank(value) {
+      return !this.isPresent(value)
+    },
+    submit() {
+      if (this.valid) {
+        this.$emit('submit', cloneDeep(this.form))
+      }
+    },
+    reset() {
+      this.$set(this, 'form', {
+        name: null,
+        label: null,
+        description: null,
+        logoUrl: null,
+        sourceUrl: null,
+        publisherName: null,
+        maintainerName: null
+      })
+    }
+  }
+}
+</script>
+
+<template>
+  <b-form class="project-form" :class="classList" novalidate @submit.stop.prevent="submit">
+    <component :is="bodyComponent">
+      <b-form-row>
+        <b-col>
+          <b-form-group
+            class="project-form__group project-form__group--label"
+            :invalid-feedback="$t('projectForm.form.label.invalidFeedback', { label: form.label })"
+            :label="$t('projectForm.form.label.label')"
+            :description="$t('projectForm.form.label.description')"
+            :disabled="disabled"
+            :validated="isPresent(form.label) && !isReservedWord(form.label)"
+          >
+            <b-form-input
+              v-model="form.label"
+              name="label"
+              type="text"
+              placeholder=""
+              required
+              :state="isBlank(form.label) ? null : !isReservedWord(form.label)"
+            />
+          </b-form-group>
+        </b-col>
+        <b-col>
+          <b-form-group
+            :label="$t('projectForm.form.name.label')"
+            :description="$t('projectForm.form.name.description')"
+            :disabled="disabled"
+          >
+            <b-form-input v-model="form.name" name="name" type="text" placeholder="" required readonly />
+          </b-form-group>
+        </b-col>
+      </b-form-row>
+      <b-form-group
+        class="project-form__group project-form__group--name"
+        :label="$t('projectForm.form.description.label')"
+        :description="$t('projectForm.form.description.description')"
+        :disabled="disabled"
+        :validated="isPresent(form.description)"
+      >
+        <b-form-textarea v-model="form.description" placeholder="" rows="3" max-rows="8" />
+      </b-form-group>
+      <b-form-group
+        class="project-form__group project-form__group--logo-url"
+        :label="$t('projectForm.form.logoUrl.label')"
+        :description="$t('projectForm.form.logoUrl.description')"
+        :disabled="disabled"
+        :validated="isUrl(form.logoUrl)"
+      >
+        <b-form-input
+          v-model="form.logoUrl"
+          :state="isBlank(form.logoUrl) ? null : isUrl(form.logoUrl)"
+          name="logoUrl"
+          type="url"
+          placeholder="https://..."
+        />
+      </b-form-group>
+      <b-form-group
+        class="project-form__group project-form__group--maintainer-name"
+        :label="$t('projectForm.form.maintainerName.label')"
+        :description="$t('projectForm.form.maintainerName.description')"
+        :disabled="disabled"
+        :validated="isPresent(form.maintainerName)"
+      >
+        <b-form-input v-model="form.maintainerName" name="maintainerName" type="text" placeholder="" />
+      </b-form-group>
+      <b-form-row>
+        <b-col>
+          <b-form-group
+            class="project-form__group project-form__group--publisher-name"
+            :label="$t('projectForm.form.publisherName.label')"
+            :description="$t('projectForm.form.publisherName.description')"
+            :disabled="disabled"
+            :validated="isPresent(form.publisherName)"
+          >
+            <b-form-input v-model="form.publisherName" name="publisherName" type="text" placeholder="" />
+          </b-form-group>
+        </b-col>
+        <b-col>
+          <b-form-group
+            class="project-form__group project-form__group--source-url"
+            :label="$t('projectForm.form.sourceUrl.label')"
+            :description="$t('projectForm.form.sourceUrl.description')"
+            :disabled="disabled"
+            :validated="isUrl(form.sourceUrl)"
+          >
+            <b-form-input
+              v-model="form.sourceUrl"
+              name="sourceUrl"
+              type="url"
+              placeholder="https://..."
+              :state="isBlank(form.sourceUrl) ? null : isUrl(form.sourceUrl)"
+            />
+          </b-form-group>
+        </b-col>
+      </b-form-row>
+    </component>
+    <component :is="footerComponent" class="d-flex">
+      <confirm-button
+        type="button"
+        class="btn btn-outline-primary mr-3"
+        :confirmed="reset"
+        :label="$t('projectForm.resetConfirmation')"
+      >
+        <slot name="reset-text">{{ $t('projectForm.reset') }}</slot>
+      </confirm-button>
+      <b-button type="submit" variant="primary" class="ml-auto" :disabled="!valid">
+        <slot name="submit-text">{{ $t('projectForm.submit') }}</slot>
+      </b-button>
+    </component>
+  </b-form>
+</template>
---
+++ tests/unit/specs/components/ProjectForm.spec.js
@@ -0,0 +1,133 @@
+import { createLocalVue, mount } from '@vue/test-utils'
+
+import ProjectForm from '@/components/ProjectForm'
+import { Core } from '@/core'
+
+const { localVue, i18n } = Core.init(createLocalVue()).useAll()
+
+describe('ProjectForm.vue', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = mount(ProjectForm, { localVue, i18n })
+  })
+
+  it('should not use b-card-* components by default', () => {
+    expect(wrapper.findComponent({ name: 'b-card-body' }).exists()).toBeFalsy()
+    expect(wrapper.findComponent({ name: 'b-card-footer' }).exists()).toBeFalsy()
+  })
+
+  it('should not have the .card class by default', async () => {
+    expect(wrapper.classes('card')).toBeFalsy()
+  })
+
+  it('should use b-card-* components when `card` prop is set', async () => {
+    await wrapper.setProps({ card: true })
+    expect(wrapper.findComponent({ name: 'b-card-body' }).exists()).toBeTruthy()
+    expect(wrapper.findComponent({ name: 'b-card-footer' }).exists()).toBeTruthy()
+  })
+
+  it('should have the .card class when `card` prop is set', async () => {
+    await wrapper.setProps({ card: true })
+    expect(wrapper.classes('card')).toBeTruthy()
+  })
+
+  it('should generate a slugified name when setting the label', async () => {
+    await wrapper.find('input[name=label]').setValue('Pandora Papers')
+    expect(wrapper.find('input[name=name]').element.value).toBe('pandora-papers')
+  })
+
+  it('should generate a slugified name when setting the label, respecting case', async () => {
+    await wrapper.find('input[name=label]').setValue('PandoraPapers')
+    expect(wrapper.find('input[name=name]').element.value).toBe('pandora-papers')
+  })
+
+  it('should not validate the form group if a reserved name is used as label', async () => {
+    await wrapper.find('input[name=label]').setValue('new')
+    expect(wrapper.find('.project-form__group--label').classes('was-validated')).toBeFalsy()
+    expect(wrapper.find('.project-form__group--label .invalid-feedback').exists()).toBeTruthy()
+  })
+
+  it('should not validate the form group if an invalid URL for sourceUrl', async () => {
+    await wrapper.find('input[name=sourceUrl]').setValue('sftp://foo.bar')
+    expect(wrapper.find('.project-form__group--source-url').classes('was-validated')).toBeFalsy()
+  })
+
+  it('should validate the form group if a valid URL for sourceUrl', async () => {
+    await wrapper.find('input[name=sourceUrl]').setValue('https://foo.bar')
+    expect(wrapper.find('.project-form__group--source-url').classes('was-validated')).toBeTruthy()
+  })
+
+  it('should not validate the form group if an invalid URL for logoUrl', async () => {
+    await wrapper.find('input[name=logoUrl]').setValue('sftp://foo.bar')
+    expect(wrapper.find('.project-form__group--logo-url').classes('was-validated')).toBeFalsy()
+  })
+
+  it('should validate the form group if a valid URL for logoUrl', async () => {
+    await wrapper.find('input[name=logoUrl]').setValue('https://foo.bar')
+    expect(wrapper.find('.project-form__group--logo-url').classes('was-validated')).toBeTruthy()
+  })
+
+  it('should not submit an invalid form without name', async () => {
+    await wrapper.trigger('submit')
+    expect(wrapper.emitted().submit).toBeFalsy()
+  })
+
+  it('should not submit an invalid form with a name but an invalid logoUrl', async () => {
+    await wrapper.find('input[name=label]').setValue('foo')
+    await wrapper.find('input[name=logoUrl]').setValue('sftp://foo.bar')
+    await wrapper.trigger('submit')
+    expect(wrapper.emitted().submit).toBeFalsy()
+  })
+
+  it('should not submit an invalid form with a name but an invalid sourceUrl', async () => {
+    await wrapper.find('input[name=label]').setValue('foo')
+    await wrapper.find('input[name=sourceUrl]').setValue('sftp://foo.bar')
+    await wrapper.trigger('submit')
+    expect(wrapper.emitted().submit).toBeFalsy()
+  })
+
+  it('should submit a valid form with a label', async () => {
+    await wrapper.find('input[name=label]').setValue('foo')
+    await wrapper.trigger('submit')
+    expect(wrapper.emitted().submit).toBeTruthy()
+  })
+
+  it('should submit a valid form with a label and a sourceUrl', async () => {
+    await wrapper.find('input[name=label]').setValue('foo')
+    await wrapper.find('input[name=sourceUrl]').setValue('https://foo.bar')
+    await wrapper.trigger('submit')
+    expect(wrapper.emitted().submit).toBeTruthy()
+  })
+
+  it('should submit a valid form with a label and a logoUrl', async () => {
+    await wrapper.find('input[name=label]').setValue('foo')
+    await wrapper.find('input[name=logoUrl]').setValue('https://foo.bar')
+    await wrapper.trigger('submit')
+    expect(wrapper.emitted().submit).toBeTruthy()
+  })
+
+  it('should not submit the form with a reserved word as label', async () => {
+    await wrapper.find('input[name=label]').setValue('new')
+    await wrapper.trigger('submit')
+    expect(wrapper.emitted().submit).toBeFalsy()
+  })
+
+  it('should not disabled any form group by default', () => {
+    expect(wrapper.find('fieldset[disabled]').exists()).toBeFalsy()
+    expect(wrapper.find('fieldset:not([disabled])').exists()).toBeTruthy()
+  })
+
+  it('should disabled all form groups when `disabled` is set', async () => {
+    await wrapper.setProps({ disabled: true })
+    expect(wrapper.find('fieldset[disabled]').exists()).toBeTruthy()
+    expect(wrapper.find('fieldset:not([disabled])').exists()).toBeFalsy()
+  })
+
+  it('should not be able to submit the form when `disabled` is set', async () => {
+    await wrapper.setProps({ disabled: true })
+    await wrapper.find('input[name=label]').setValue('foo')
+    await wrapper.trigger('submit')
+    expect(wrapper.emitted().submit).toBeFalsy()
+  })
+})
