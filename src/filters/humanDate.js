185d909f2ddd8bc172c9efe4b220a5bafd2c22de
Pierre Romera <hello@pirhoo.com>
2023-07-26 08:15:59 +0000

    refactor: indent

--- src/filters/humanDate.js
+++ src/filters/humanDate.js
@@ -3,12 +3,15 @@ import moment from 'moment'
 export function humanDate(date, locale) {
   return moment(date).locale(locale).format('Y/MM/DD')
 }
+
 export function humanLongDate(date, locale) {
   return moment(date).locale(locale).format('LLL')
 }
+
 export function humanShortDate(date, locale) {
   return moment(date).locale(locale).format('LL')
 }
+
 export function isDateValid(date) {
   return moment(date).isValid()
 }
