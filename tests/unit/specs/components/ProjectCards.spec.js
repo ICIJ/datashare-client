5f87a1d3a60fab61507160885c850c6c1d46e388
Caroline Desprat <cdesprat@icij.org>
2023-08-02 08:48:50 +0000

    feat: add search button on project card and add active project border ICIJ/datashare#1109
    Co-authored-by: Pierre Romera Zhang <pirhoo@users.noreply.github.com>

--- tests/unit/specs/components/ProjectCards.spec.js
+++ tests/unit/specs/components/ProjectCards.spec.js
@@ -4,38 +4,40 @@ import { createLocalVue, shallowMount } from '@vue/test-utils'
 import ProjectCards from '@/components/ProjectCards'
 import { Core } from '@/core'
 
-const { localVue, store } = Core.init(createLocalVue()).useAll()
+const { localVue, store, i18n } = Core.init(createLocalVue()).useAll()
 
 describe('ProjectCards.vue', () => {
   let wrapper
 
   it('should display one card for first-index', () => {
     Murmur.config.set('projects', [{ name: 'foo' }])
-    wrapper = shallowMount(ProjectCards, { localVue, store })
+    wrapper = shallowMount(ProjectCards, { localVue, store, i18n })
 
     expect(wrapper.findAll('.project-cards__item')).toHaveLength(1)
   })
 
   it('should display one card for with the index name in titlecase', () => {
-    Murmur.config.set('projects', [{ name: 'first-index' }])
-    wrapper = shallowMount(ProjectCards, { localVue, store })
+    Murmur.config.set('projects', [{ name: 'first-index', label: 'First Index' }])
+    wrapper = shallowMount(ProjectCards, { localVue, store, i18n })
 
-    expect(wrapper.find('.project-cards__item__body').text()).toBe('First Index')
+    expect(wrapper.find('.project-cards__item__heading').text()).toBe('First Index')
   })
 
   it('should display two cards', () => {
     Murmur.config.set('projects', [{ name: 'first-index' }, { name: 'second-index' }])
-    wrapper = shallowMount(ProjectCards, { localVue, store })
+    wrapper = shallowMount(ProjectCards, { localVue, store, i18n })
 
     expect(wrapper.findAll('.project-cards__item')).toHaveLength(2)
   })
 
   it('should display one active card among the two cards', () => {
-    Murmur.config.set('projects', [{ name: 'first-index' }, { name: 'second-index' }])
+    Murmur.config.set('projects', [{ name: 'first-index' }, { name: 'second-index', label: 'Second Index' }])
     store.commit('search/index', 'second-index')
-    wrapper = shallowMount(ProjectCards, { localVue, store })
+    wrapper = shallowMount(ProjectCards, { localVue, store, i18n })
 
     expect(wrapper.findAll('.project-cards__item--active')).toHaveLength(1)
-    expect(wrapper.find('.project-cards__item--active').text()).toBe('Second Index')
+    expect(wrapper.find('.project-cards__item--active .project-cards__item__heading').text().trim()).toBe(
+      'Second Index'
+    )
   })
 })
