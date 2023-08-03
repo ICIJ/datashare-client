6ae2039364a68702ec75b73e1dedcaf14b51e70e
Caroline Desprat <cdesprat@icij.org>
2023-08-03 15:12:24 +0000

    test: remove axios mock from Core.spec.js

--- tests/unit/specs/core/Core.spec.js
+++ tests/unit/specs/core/Core.spec.js
@@ -5,133 +5,126 @@ import VueI18n from 'vue-i18n'
 import VueRouter from 'vue-router'
 import Vuex from 'vuex'
 
-import { Api } from '@/api'
 import { Core } from '@/core'
 
 describe('Core', () => {
-  let localVue
-  let mockAxios
-  let api
+  let api, core
 
   beforeAll(() => {
-    mockAxios = jest.fn()
-    mockAxios.get = jest.fn().mockResolvedValue({ data: {} })
-    mockAxios.request = jest.fn().mockResolvedValue({ data: { userProjects: ['first-index'], mode: 'LOCAL' } })
-    api = new Api(mockAxios, { $emit: jest.fn() })
+    api = {
+      createProject: jest.fn(),
+      isDownloadAllowed: jest.fn(),
+      getUser: jest.fn(),
+      getSettings: jest.fn(),
+      getProject: jest.fn()
+    }
   })
 
   beforeEach(() => {
-    localVue = createLocalVue()
-  })
+    jest.clearAllMocks()
+    const localVue = createLocalVue()
+    core = Core.init(localVue, api)
 
-  beforeEach(() => {
     const app = document.createElement('div')
     app.setAttribute('id', 'core')
     document.body.appendChild(app)
   })
 
-  afterEach(() => mockAxios.request.mockClear())
-
   it('should instantiate the Core class using a static method', () => {
-    const core = Core.init(localVue)
     expect(Core.isInstanceOfCore(core)).toBe(true)
   })
-
-  it('should expose the router', () => {
-    const { router } = Core.init(localVue).useAll()
-    expect(router).toBeInstanceOf(VueRouter)
-  })
-
-  it('should expose the VueI18n', () => {
-    const { i18n } = Core.init(localVue).useAll()
-    expect(i18n).toBeInstanceOf(VueI18n)
-  })
-
-  it('should expose the store', () => {
-    const { store } = Core.init(localVue).useAll()
-    expect(store).toBeInstanceOf(Vuex.Store)
-  })
-
   it('should not expose the router if it is not installed', () => {
-    const { router } = Core.init(localVue)
-    expect(router).not.toBeInstanceOf(VueRouter)
+    expect(core.router).not.toBeInstanceOf(VueRouter)
   })
 
   it('should not expose the VueI18n if it is not installed', () => {
-    const { i18n } = Core.init(localVue)
-    expect(i18n).not.toBeInstanceOf(VueI18n)
+    expect(core.i18n).not.toBeInstanceOf(VueI18n)
   })
 
-  it('should expose the config from Murmur', () => {
-    const { config } = Core.init(localVue).useAll()
-    expect(config).toBe(Murmur.config)
-  })
+  describe('Call useAll on Core', () => {
+    beforeEach(() => {
+      core.useAll()
+      // empty config for projects to avoid side effects
+      core.config.set('projects', undefined)
+      core.config.set('defaultProject', undefined)
+    })
 
-  it('should mount the app on a specific element', () => {
-    const core = Core.init(localVue).useAll()
-    const vm = core.mount('#core')
-    expect(vm).toBeInstanceOf(Vue)
-  })
+    it('should expose the router', () => {
+      expect(core.router).toBeInstanceOf(VueRouter)
+    })
 
-  it('should call a global event "datashare:ready" after the core is configured', (done) => {
-    document.addEventListener(
-      'datashare:ready',
-      ({ detail }) => {
-        expect(Core.isInstanceOfCore(detail.core)).toBe(true)
-        done()
-      },
-      { once: true }
-    )
-    // Create and configure the core
-    Core.init(localVue, api).useAll().configure()
-  })
+    it('should expose the VueI18n', () => {
+      expect(core.i18n).toBeInstanceOf(VueI18n)
+    })
 
-  it('should resolve the `ready` promise after the core was configured', async () => {
-    // Create and configure the core
-    const core = Core.init(localVue, api).useAll()
-    // For `ready` to be resolved, the core must configure
-    await core.configure()
-    await expect(core.ready).resolves.toBe(core)
-  })
+    it('should expose the store', () => {
+      expect(core.store).toBeInstanceOf(Vuex.Store)
+    })
 
-  it('should call isDownloadAllowed API endpoint', async () => {
-    const project = 'my-project'
-    const core = Core.init(localVue, api).useAll()
-    core.store.commit('search/index', project)
-    await core.configure()
+    it('should expose the config from Murmur', () => {
+      expect(core.config).toBe(Murmur.config)
+    })
 
-    expect(mockAxios.request).toBeCalledTimes(4)
-    expect(mockAxios.request).toBeCalledWith(
-      expect.objectContaining({
-        url: Api.getFullUrl(`/api/project/isDownloadAllowed/${project}`)
-      })
-    )
-  })
+    it('should mount the app on a specific element', () => {
+      const vm = core.mount('#core')
+      expect(vm).toBeInstanceOf(Vue)
+    })
 
-  it('should install the internal `VueCore` plugin', () => {
-    // Create and configure the core
-    const core = Core.init(localVue).useAll()
-    const vm = core.mount('#core')
-    expect(Core.isInstanceOfCore(vm.$core)).toBe(true)
-  })
+    it('should call a global event "datashare:ready" after the core is configured', (done) => {
+      api.getSettings.mockResolvedValueOnce({})
+      api.getProject.mockResolvedValueOnce({})
 
-  it('should return empty string if user has no projects', () => {
-    const core = Core.init(localVue).useAll()
-    core.config.set('defaultProject', 'my_project')
-    expect(core.getDefaultProject()).toEqual('')
-  })
+      document.addEventListener(
+        'datashare:ready',
+        ({ detail }) => {
+          expect(Core.isInstanceOfCore(detail.core)).toBe(true)
+          done()
+        },
+        { once: true }
+      )
+      // Create and configure the core
+      return core.configure()
+    })
 
-  it('should getDefaultProject when user has it', () => {
-    const core = Core.init(localVue).useAll()
-    core.config.set('projects', [{ name: 'my_project' }])
-    core.config.set('defaultProject', 'my_project')
-    expect(core.getDefaultProject()).toEqual('my_project')
-  })
+    it('should resolve the `ready` promise after the core was configured', async () => {
+      api.getSettings.mockResolvedValueOnce({})
+      api.getProject.mockResolvedValueOnce({ userProjects: ['first-index'] })
+      // For `ready` to be resolved, the core must configure
+      await core.configure()
+      await expect(core.ready).resolves.toBe(core)
+    })
 
-  it("should return first user project when user doesn't have the default project", () => {
-    const core = Core.init(localVue).useAll()
-    core.config.set('projects', [{ name: 'user_project' }])
-    core.config.set('defaultProject', 'default_project')
-    expect(core.getDefaultProject()).toEqual('user_project')
+    it('should call isDownloadAllowed API endpoint', async () => {
+      const project = 'my-project'
+      api.getSettings.mockResolvedValueOnce({})
+      api.createProject.mockResolvedValueOnce({ name: project, label: 'MyProject' })
+      core.store.commit('search/index', project)
+      await core.configure()
+
+      expect(api.isDownloadAllowed).toBeCalledTimes(1)
+      expect(api.isDownloadAllowed).toBeCalledWith(project)
+    })
+
+    it('should return empty string if user has no projects', () => {
+      core.config.set('defaultProject', 'my_project')
+      expect(core.getDefaultProject()).toEqual('')
+    })
+    it('should install the internal `VueCore` plugin', () => {
+      // Create and configure the core
+      const vm = core.mount('#core')
+      expect(Core.isInstanceOfCore(vm.$core)).toBe(true)
+    })
+
+    it('should getDefaultProject when user has it', () => {
+      core.config.set('projects', [{ name: 'my_project' }])
+      core.config.set('defaultProject', 'my_project')
+      expect(core.getDefaultProject()).toEqual('my_project')
+    })
+
+    it("should return first user project when user doesn't have the default project", () => {
+      core.config.set('projects', [{ name: 'user_project' }])
+      core.config.set('defaultProject', 'default_project')
+      expect(core.getDefaultProject()).toEqual('user_project')
+    })
   })
 })
