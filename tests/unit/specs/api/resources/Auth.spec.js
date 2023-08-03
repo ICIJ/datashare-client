983ebe5ec0ae054e5c9763e4db9d34767a9a1569
Caroline Desprat <cdesprat@icij.org>
2023-08-03 16:41:35 +0000

    refactor: replace axios in Auth.spec.js

--- tests/unit/specs/api/resources/Auth.spec.js
+++ tests/unit/specs/api/resources/Auth.spec.js
@@ -1,46 +1,47 @@
 import { createLocalVue } from '@vue/test-utils'
 import { removeCookie, setCookie } from 'tiny-cookie'
 
-import { Api } from '@/api'
 import { Core } from '@/core'
 import { getMode } from '@/mode'
 
 describe('auth backend client', () => {
   let auth
-  let mockAxios
+  let api
 
   beforeAll(() => {
-    mockAxios = { request: jest.fn() }
-    const api = new Api(mockAxios, null)
+    api = { getUser: jest.fn(), setSettings: jest.fn() }
     const core = Core.init(createLocalVue(), api, getMode()).useAll()
     auth = core.auth
   })
   beforeEach(() => {
-    mockAxios.request.mockClear()
-    mockAxios.request.mockRejectedValue({ response: { status: 401 } })
+    jest.clearAllMocks()
   })
 
   afterEach(() => auth.reset())
 
   describe('getUsername', () => {
     it('should return user name if user is authenticated with basic auth', async () => {
-      mockAxios.request.mockResolvedValue({ data: { uid: 'john' } })
+      api.getUser.mockResolvedValue({ uid: 'john' })
       expect(await auth.getUsername()).toBe('john')
-      expect(mockAxios.request).toBeCalledWith({ url: 'http://localhost:9009/api/users/me' })
+      expect(api.getUser).toBeCalledTimes(1)
     })
 
     it('should return null if user is not authenticated with basic auth', async () => {
+      api.getUser.mockRejectedValue({ response: { status: 401 } })
       expect(await auth.getUsername()).toBeNull()
-      expect(mockAxios.request).toBeCalledWith({ url: 'http://localhost:9009/api/users/me' })
+      expect(api.getUser).toBeCalledTimes(1)
     })
 
     it('should throw error when testing basic auth and response is other than 200 or 401', async () => {
-      mockAxios.request.mockRejectedValue({ response: { status: 500, statusText: 'message' } })
+      api.getUser.mockRejectedValue({ response: { status: 500, statusText: 'message' } })
       try {
-        await auth.getUsername()
-      } catch (error) {
-        expect(error).toEqual(new Error('500 message'))
+        await auth._getBasicAuthUserName()
+      } catch (e) {
+        expect(e).toEqual(new Error('500 message'))
+        return
       }
+      // helps to check if we pass in the catch
+      expect(true).toBe(false)
     })
   })
 
