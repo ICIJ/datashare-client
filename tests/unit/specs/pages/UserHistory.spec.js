477cc366d3bb037a71981ab1446fde04a833d9f1
Caroline Desprat <cdesprat@icij.org>
2023-08-03 15:58:57 +0000

    refactor: mock getUserHistory and deleteUserHistory in UserHistory.spec.js

--- tests/unit/specs/pages/UserHistory.spec.js
+++ tests/unit/specs/pages/UserHistory.spec.js
@@ -2,19 +2,16 @@ import { createLocalVue, shallowMount, mount } from '@vue/test-utils'
 import VueRouter from 'vue-router'
 import { flushPromises } from 'tests/unit/tests_utils'
 
-import { Api } from '@/api'
 import { Core } from '@/core'
 import UserHistory from '@/pages/UserHistory'
 
 describe('UserHistory.vue', () => {
   let i18n, localVue, store, wait, router
+  let api
   let wrapper = null
-  let mockAxios
 
   beforeAll(() => {
-    mockAxios = { request: jest.fn() }
-
-    const api = new Api(mockAxios, null)
+    api = {}
     const core = Core.init(createLocalVue(), api).useAll()
     i18n = core.i18n
     localVue = core.localVue
@@ -24,39 +21,6 @@ describe('UserHistory.vue', () => {
 
   beforeEach(() => {
     jest.clearAllMocks()
-    mockAxios.request.mockResolvedValue({
-      data: [
-        {
-          id: 'id_01',
-          user: {
-            id: 'user',
-            name: null,
-            email: null,
-            provider: 'local'
-          },
-          creationDate: 'creation_date_01',
-          modificationDate: 'modification_date_01',
-          type: 'DOCUMENT',
-          name: 'name_01',
-          uri: 'uri_01'
-        },
-        {
-          id: 'id_02',
-          user: {
-            id: 'user',
-            name: null,
-            email: null,
-            provider: 'local'
-          },
-          creationDate: 'creation_date_02',
-          modificationDate: 'modification_date_02',
-          type: 'SEARCH',
-          name: 'name_02',
-          uri: 'uri_02'
-        }
-      ]
-    })
-
     router = new VueRouter({
       routes: [
         {
@@ -89,41 +53,23 @@ describe('UserHistory.vue', () => {
   })
 
   it('should call get user history when page is loaded', async () => {
+    api.getUserHistory = jest.fn()
     await router.replace({ name: 'user-history.document.list' })
     wrapper = shallowMount(UserHistory, { i18n, localVue, router, store, wait })
     await wrapper.vm.$nextTick()
-    expect(mockAxios.request).toBeCalledTimes(1)
-    expect(mockAxios.request).toBeCalledWith(
-      expect.objectContaining({
-        url: Api.getFullUrl('/api/users/me/history'),
-        method: 'GET',
-        params: {
-          from: 0,
-          size: 100,
-          type: 'document',
-          project: undefined,
-          desc: true,
-          sort: 'modification_date'
-        }
-      })
-    )
+    expect(api.getUserHistory).toBeCalledTimes(1)
+    expect(api.getUserHistory).toBeCalledWith('document', 0, 100, undefined, undefined, undefined)
   })
 
   it('should call delete user history api function is called', async () => {
+    api.deleteUserHistory = jest.fn()
+
     await router.replace({ name: 'user-history.document.list' })
     wrapper = shallowMount(UserHistory, { i18n, localVue, router, store, wait })
     await wrapper.vm.deleteUserHistory()
     await wrapper.vm.$nextTick()
 
-    expect(mockAxios.request).toBeCalledTimes(2)
-    expect(mockAxios.request).toBeCalledWith(
-      expect.objectContaining({
-        url: Api.getFullUrl('/api/users/me/history'),
-        method: 'DELETE',
-        params: {
-          type: 'document'
-        }
-      })
-    )
+    expect(api.deleteUserHistory).toBeCalledTimes(1)
+    expect(api.deleteUserHistory).toBeCalledWith('document')
   })
 })
