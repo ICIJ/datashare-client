cb0ee59194a8c9c24071439b7227f1f446fc2b1e
Caroline Desprat <cdesprat@icij.org>
2023-08-04 11:11:01 +0000

    refactor: replace axios in recommended.spec.js

--- tests/unit/specs/store/modules/recommended.spec.js
+++ tests/unit/specs/store/modules/recommended.spec.js
@@ -6,16 +6,18 @@ import { storeBuilder } from '@/store/storeBuilder'
 describe('RecommendedStore', () => {
   const { index } = esConnectionHelper.build()
 
-  let mockAxios, api, store
+  let api, store
   beforeAll(() => {
-    mockAxios = { request: jest.fn() }
-    api = new Api(mockAxios)
+    api = {
+      getRecommendationsByProject: jest.fn(),
+      getDocumentsRecommendedBy: jest.fn()
+    }
     store = storeBuilder(api)
     store.commit('search/index', index)
   })
 
   beforeEach(() => {
-    mockAxios.request.mockClear()
+    jest.clearAllMocks()
   })
 
   it('should define a store module', () => {
@@ -34,30 +36,19 @@ describe('RecommendedStore', () => {
   })
 
   it('should set the list of documents recommended by a list of users', async () => {
-    mockAxios.request.mockResolvedValue({ data: ['document_01', 'document_02', 'document_03'] })
-    mockAxios.request.mockClear()
-
+    const documents = ['document_01', 'document_02', 'document_03']
+    api.getDocumentsRecommendedBy.mockResolvedValue(documents)
     await store.dispatch('recommended/getDocumentsRecommendedBy', ['user_01', 'user_02'])
 
-    expect(mockAxios.request).toBeCalledTimes(1)
-    expect(mockAxios.request).toBeCalledWith(
-      expect.objectContaining({
-        url: Api.getFullUrl(`/api/${index}/documents/recommendations`),
-        method: 'GET',
-        params: {
-          userids: 'user_01,user_02'
-        }
-      })
-    )
+    expect(api.getDocumentsRecommendedBy).toBeCalledTimes(1)
+    expect(api.getDocumentsRecommendedBy).toBeCalledWith(index, ['user_01', 'user_02'])
     expect(store.state.recommended.documents).toEqual(['document_01', 'document_02', 'document_03'])
   })
 
   it('should reset the list of documents recommended if no users', async () => {
-    mockAxios.request.mockClear()
-
     await store.dispatch('recommended/getDocumentsRecommendedBy', [])
 
-    expect(mockAxios.request).toBeCalledTimes(0)
+    expect(api.getDocumentsRecommendedBy).toBeCalledTimes(0)
     expect(store.state.recommended.documents).toEqual([])
   })
 
@@ -72,25 +63,17 @@ describe('RecommendedStore', () => {
   })
 
   it('should return users who recommended documents from this project', async () => {
-    mockAxios.request.mockResolvedValue({
-      data: {
-        aggregates: [
-          { item: { id: 'user_01' }, count: 1 },
-          { item: { id: 'user_02' }, count: 1 }
-        ]
-      }
+    api.getRecommendationsByProject.mockResolvedValue({
+      aggregates: [
+        { item: { id: 'user_01' }, count: 1 },
+        { item: { id: 'user_02' }, count: 1 }
+      ]
     })
-    mockAxios.request.mockClear()
     await store.dispatch('recommended/fetchIndicesRecommendations')
 
-    expect(mockAxios.request).toBeCalledTimes(1)
-    expect(mockAxios.request).toBeCalledWith(
-      expect.objectContaining({
-        url: Api.getFullUrl('/api/users/recommendations'),
-        method: 'GET',
-        params: { project: index }
-      })
-    )
+    expect(api.getRecommendationsByProject).toBeCalledTimes(1)
+    expect(api.getRecommendationsByProject).toBeCalledWith(index)
+
     expect(store.state.recommended.byUsers).toEqual([
       { user: 'user_01', count: 1 },
       { user: 'user_02', count: 1 }
@@ -98,18 +81,11 @@ describe('RecommendedStore', () => {
   })
 
   it('should return the total of documents recommended for this project', async () => {
-    mockAxios.request.mockResolvedValue({ data: { totalCount: 42, aggregates: [] } })
-    mockAxios.request.mockClear()
+    api.getRecommendationsByProject.mockResolvedValue({ totalCount: 42, aggregates: [] })
     await store.dispatch('recommended/fetchIndicesRecommendations')
 
-    expect(mockAxios.request).toBeCalledTimes(1)
-    expect(mockAxios.request).toBeCalledWith(
-      expect.objectContaining({
-        url: Api.getFullUrl('/api/users/recommendations'),
-        method: 'GET',
-        params: { project: index }
-      })
-    )
+    expect(api.getRecommendationsByProject).toBeCalledTimes(1)
+    expect(api.getRecommendationsByProject).toBeCalledWith(index)
     expect(store.state.recommended.total).toBe(42)
   })
 })
