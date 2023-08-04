9d5e38772ee9848410bc775a8a4567c96bbda1e7
Caroline Desprat <cdesprat@icij.org>
2023-08-04 14:51:27 +0000

    refactor: remove mockAxios from JsonViewer.spec.js

--- tests/unit/specs/components/document/viewers/JsonViewer.spec.js
+++ tests/unit/specs/components/document/viewers/JsonViewer.spec.js
@@ -4,27 +4,20 @@ import { flushPromises } from 'tests/unit/tests_utils'
 import JsonViewer from '@/components/document/viewers/JsonViewer'
 import JsonFormatter from '@/components/JsonFormatter'
 import { Core } from '@/core'
-import { Api } from '@/api'
 
 const documentJson = require('tests/unit/resources/document.json')
 
 describe('JsonViewer.vue', () => {
-  let localVue, mockAxios, api, wrapper
+  let localVue, api, wrapper
   beforeAll(() => {
-    mockAxios = { request: jest.fn() }
-    api = new Api(mockAxios, null)
+    api = { getSource: jest.fn().mockResolvedValue({ data: documentJson }) }
     const core = Core.init(createLocalVue(), api).useAll()
     localVue = core.localVue
   })
 
-  beforeEach(async () => {
-    mockAxios.request.mockResolvedValue({ data: documentJson })
+  it('should render the JSON in a JsonFormatter component', async () => {
     wrapper = shallowMount(JsonViewer, { localVue, propsData: { document: { url: 'document.json' } } })
     await flushPromises()
-  })
-  afterEach(() => mockAxios.request.mockClear())
-
-  it('should render the JSON in a JsonFormatter component', () => {
     expect(wrapper.findComponent(JsonFormatter).exists()).toBeTruthy()
   })
 })
