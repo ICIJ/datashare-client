c1b5980646be5f39688c823d8e8d0cddc3c50d17
Caroline Desprat <cdesprat@icij.org>
2023-08-11 15:36:58 +0000

    feat: create page for find named entities

--- src/pages/ProjectViewFindNamedEntities.vue
+++ src/pages/ProjectViewFindNamedEntities.vue
@@ -1,15 +1,15 @@
 <script>
 import { get } from 'lodash'
 
-import ExtractingForm from '@/components/ExtractingForm'
+import FindNamedEntitiesForm from '@/components/FindNamedEntitiesForm'
 
 /**
  * This page display a form to create a new project.
  */
 export default {
-  name: 'ProjectDocumentExtract',
+  name: 'ProjectViewFindNamedEntities',
   components: {
-    ExtractingForm
+    FindNamedEntitiesForm
   },
   computed: {
     projectRoute() {
@@ -52,8 +52,20 @@ export default {
 
 <template>
   <div class="project-new">
-    <div class="container">
-      <extracting-form id="extracting-form" class="my-4 px-3" />
+    <div class="container p-4">
+      <div class="card">
+        <h4 class="card-header">Find named entities (people, locations, organisations)</h4>
+        <find-named-entities-form id="find-named-entities-form p-4" class="card-body" @submit="() => {}">
+          <template #footer="{ disabled }">
+            <div class="col text-right">
+              <b-btn variant="outline-primary" type="reset" :disabled="disabled"> Reset </b-btn>
+              <b-btn variant="primary" class="ml-2" type="submit" :disabled="disabled">
+                {{ $t('indexing.go') }}
+              </b-btn>
+            </div>
+          </template>
+        </find-named-entities-form>
+      </div>
     </div>
   </div>
 </template>
