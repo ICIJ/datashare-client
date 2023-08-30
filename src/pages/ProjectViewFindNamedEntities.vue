f1df20799d5a9072a5cff6c35b7b9c3f327aea79
Caroline Desprat <cdesprat@icij.org>
2023-08-30 13:04:36 +0000

    feat: add find named entities and add document tasks notifications

--- src/pages/ProjectViewFindNamedEntities.vue
+++ src/pages/ProjectViewFindNamedEntities.vue
@@ -4,7 +4,7 @@ import { get } from 'lodash'
 import FindNamedEntitiesForm from '@/components/FindNamedEntitiesForm'
 
 /**
- * This page display a form to create a new project.
+ * This page displays a form to create a new project.
  */
 export default {
   name: 'ProjectViewFindNamedEntities',
@@ -14,37 +14,35 @@ export default {
   computed: {
     projectRoute() {
       return { name: 'project.document-extract' }
+    },
+    projectName() {
+      return this.$route.params?.name
     }
   },
   methods: {
-    async submit(project) {
-      try {
-        this.$wait.start('creating')
-        await this.$core.api.createProject(project)
-        await this.$core.setProject(project)
-        this.notifyCreationSucceed()
-        this.redirectToProject(project)
-      } catch (error) {
+    async submit({ error }) {
+      if (error) {
         this.notifyCreationFailed(error)
-      } finally {
-        this.$wait.end('creating')
+      } else {
+        this.notifyCreationSucceed()
       }
     },
     notifyCreationSucceed() {
-      const title = this.$t('projectNew.notify.succeed')
+      const title = this.$t('projectViewFindNamedEntities.notify.succeed')
       const variant = 'success'
-      const body = this.$t('projectNew.notify.succeedBody')
+      const message = this.$t('projectViewFindNamedEntities.notify.succeedBody')
+      const linkText = this.$t('projectViewFindNamedEntities.notify.seeTasks')
+      const body = this.$createElement('div', {}, [
+        this.$createElement('p', {}, message),
+        this.$createElement('router-link', { props: { to: { name: 'task.analysis.list' } } }, linkText)
+      ])
       this.$root.$bvToast.toast(body, { variant, title })
     },
     notifyCreationFailed(error) {
-      const title = this.$t('projectNew.notify.failed')
+      const title = this.$t('projectViewFindNamedEntities.notify.failed')
       const variant = 'danger'
-      const body = get(error, 'response.data.error') ?? this.$t('projectNew.notify.failedBody')
+      const body = get(error, 'response.data.error') ?? this.$t('projectViewFindNamedEntities.notify.failedBody')
       this.$root.$bvToast.toast(body, { variant, title })
-    },
-    redirectToProject({ name }) {
-      const params = { name }
-      return this.$router.push({ name: 'project.view', params })
     }
   }
 }
@@ -55,10 +53,15 @@ export default {
     <div class="container p-4">
       <div class="card">
         <h4 class="card-header">Find named entities (people, locations, organisations)</h4>
-        <find-named-entities-form id="find-named-entities-form p-4" class="card-body" @submit="() => {}">
+        <find-named-entities-form
+          id="find-named-entities-form p-4"
+          class="card-body"
+          :project-name="projectName"
+          disable-project-selection
+          @submit="submit"
+        >
           <template #footer="{ disabled }">
             <div class="col text-right">
-              <b-btn variant="outline-primary" type="reset" :disabled="disabled"> Reset </b-btn>
               <b-btn variant="primary" class="ml-2" type="submit" :disabled="disabled">
                 {{ $t('indexing.go') }}
               </b-btn>
