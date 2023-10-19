cfc6c33ea44dc9d7aad5143aa92d8c1b7b0f4866
Pierre Romera <hello@pirhoo.com>
2023-10-19 12:08:38 +0200

    feat: break columns on md screen

--- src/pages/ProjectViewInsights.vue
+++ src/pages/ProjectViewInsights.vue
@@ -2,7 +2,7 @@
   <div class="project-view-insights">
     <div class="container project-view-insights__container">
       <b-row class="align-items-stretch">
-        <b-col v-for="(widget, index) in instantiatedWidgets" :key="index" :md="widget.cols">
+        <b-col v-for="(widget, index) in instantiatedWidgets" :key="index" :lg="widget.cols">
           <div class="project-view-insights__container__widget" :class="{ card: widget.card }">
             <component :is="widget.component" :widget="widget" class="flex-grow-1" />
           </div>
