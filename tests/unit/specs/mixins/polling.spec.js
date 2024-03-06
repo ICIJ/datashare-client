88f0720ae279fd8a7b5df24f7d2ce215c022a462
Pierre Romera <hello@pirhoo.com>
2024-03-06 19:14:19 +0100

    test: use nextTick to flush promises

--- tests/unit/specs/mixins/polling.spec.js
+++ tests/unit/specs/mixins/polling.spec.js
@@ -5,14 +5,14 @@ import polling from '@/mixins/polling'
 describe('polling mixin', () => {
   let wrapper
 
-  const flushPromisesAndPendingTimers = async () => {
+  const flushPromisesAndPendingTimers = async ({ vm }) => {
+    await vm.$nextTick()
     await vi.runOnlyPendingTimersAsync()
-    await vi.runAllTimers()
   }
 
-  const flushPromisesAndAdvanceTimers = async (time) => {
+  const flushPromisesAndAdvanceTimers = async ({ vm }, time) => {
+    await vm.$nextTick()
     await vi.advanceTimersByTimeAsync(time)
-    await vi.runAllTimers()
   }
 
   beforeEach(() => {
@@ -29,16 +29,16 @@ describe('polling mixin', () => {
   it('should register a polling function repeatedly called', async () => {
     const fn = vi.fn().mockReturnValue(Promise.resolve(true))
     wrapper.vm.registerPoll({ fn, timeout: 1000 })
-    await flushPromisesAndPendingTimers()
+    await flushPromisesAndPendingTimers(wrapper)
     expect(fn).toBeCalledTimes(1)
-    await flushPromisesAndPendingTimers()
+    await flushPromisesAndPendingTimers(wrapper)
     expect(fn).toBeCalledTimes(2)
   })
 
   it('should register a polling function with function to calculate its timeout', async () => {
     const fn = vi.fn().mockReturnValue(Promise.resolve(true))
     wrapper.vm.registerPoll({ fn, timeout: () => 1000 })
-    await flushPromisesAndPendingTimers()
+    await flushPromisesAndPendingTimers(wrapper)
     expect(fn).toBeCalledTimes(1)
   })
 
@@ -47,7 +47,7 @@ describe('polling mixin', () => {
     const fnFoo = vi.fn().mockReturnValue(Promise.resolve(true))
     wrapper.vm.registerPoll({ fn: fnBar, timeout: 1000 })
     wrapper.vm.registerPoll({ fn: fnFoo, timeout: 1000 })
-    await flushPromisesAndPendingTimers()
+    await flushPromisesAndPendingTimers(wrapper)
     expect(fnBar).toBeCalledTimes(1)
     expect(fnFoo).toBeCalledTimes(1)
   })
@@ -55,18 +55,18 @@ describe('polling mixin', () => {
   it('should register one polling function called once when the promise returns false', async () => {
     const fn = vi.fn().mockReturnValue(Promise.resolve(false))
     wrapper.vm.registerPoll({ fn, timeout: 1000 })
-    await flushPromisesAndPendingTimers()
+    await flushPromisesAndPendingTimers(wrapper)
     expect(fn).toBeCalledTimes(1)
-    await flushPromisesAndPendingTimers()
+    await flushPromisesAndPendingTimers(wrapper)
     expect(fn).toBeCalledTimes(1)
   })
 
   it('should register one polling function called once when the promise is rejected', async () => {
     const fn = vi.fn().mockImplementation(() => Promise.reject(new Error()))
     wrapper.vm.registerPoll({ fn, timeout: 1000 })
-    await flushPromisesAndPendingTimers()
+    await flushPromisesAndPendingTimers(wrapper)
     expect(fn).toBeCalledTimes(1)
-    await flushPromisesAndPendingTimers()
+    await flushPromisesAndPendingTimers(wrapper)
     expect(fn).toBeCalledTimes(1)
   })
 
@@ -74,30 +74,22 @@ describe('polling mixin', () => {
     let count = 0
     const fn = vi.fn().mockImplementation(async () => ++count < 2)
     wrapper.vm.registerPoll({ fn, timeout: 1000 })
-    await flushPromisesAndPendingTimers()
+    await flushPromisesAndPendingTimers(wrapper)
     expect(fn).toBeCalledTimes(1)
-    await flushPromisesAndPendingTimers()
+    await flushPromisesAndPendingTimers(wrapper)
     expect(fn).toBeCalledTimes(2)
-    await flushPromisesAndPendingTimers()
+    await flushPromisesAndPendingTimers(wrapper)
     expect(fn).toBeCalledTimes(2)
   })
 
   it('should not call a polling function after the component is destroy', async () => {
     const fn = vi.fn().mockReturnValue(Promise.resolve(true))
     wrapper.vm.registerPoll({ fn, timeout: 1000 })
-    await flushPromisesAndPendingTimers()
+    await flushPromisesAndPendingTimers(wrapper)
     expect(fn).toBeCalledTimes(1)
     wrapper.destroy()
-    await flushPromisesAndPendingTimers()
+    await flushPromisesAndPendingTimers(wrapper)
     expect(fn).toBeCalledTimes(1)
-  })
-
-  it('should unregister a polling function after the component is destroy', async () => {
-    const fn = vi.fn().mockReturnValue(Promise.resolve(true))
-    wrapper.vm.registerPoll({ fn, timeout: 1000 })
-    expect(wrapper.vm.registeredPolls).toHaveLength(1)
-    wrapper.destroy()
-    expect(wrapper.vm.registeredPolls).toHaveLength(0)
   })
 
   it('should unregister a polling function after the component is destroy', async () => {
@@ -111,20 +103,20 @@ describe('polling mixin', () => {
   it('should call a polling function 2 times in 2000ms', async () => {
     const fn = vi.fn().mockReturnValue(Promise.resolve(true))
     wrapper.vm.registerPoll({ fn, timeout: 1000 })
-    await flushPromisesAndAdvanceTimers(500)
+    await flushPromisesAndAdvanceTimers(wrapper, 500)
     expect(fn).toBeCalledTimes(0)
-    await flushPromisesAndAdvanceTimers(500)
+    await flushPromisesAndAdvanceTimers(wrapper, 500)
     expect(fn).toBeCalledTimes(1)
-    await flushPromisesAndAdvanceTimers(500)
+    await flushPromisesAndAdvanceTimers(wrapper, 500)
     expect(fn).toBeCalledTimes(1)
-    await flushPromisesAndAdvanceTimers(500)
+    await flushPromisesAndAdvanceTimers(wrapper, 500)
     expect(fn).toBeCalledTimes(2)
   })
 
   it('should call a polling immediatly even before its timeout', async () => {
     const fn = vi.fn().mockReturnValue(Promise.resolve(true))
     wrapper.vm.registerPoll({ fn, timeout: 1000, immediate: true })
-    await flushPromisesAndAdvanceTimers(500)
+    await flushPromisesAndAdvanceTimers(wrapper, 500)
     expect(fn).toBeCalledTimes(1)
   })
 
@@ -132,7 +124,7 @@ describe('polling mixin', () => {
     const fn = vi.fn().mockReturnValue(Promise.resolve(true))
     wrapper.vm.registerPollOnce({ fn, timeout: 1000, immediate: true })
     wrapper.vm.registerPollOnce({ fn, timeout: 1000, immediate: true })
-    await flushPromisesAndPendingTimers()
+    await flushPromisesAndPendingTimers(wrapper)
     expect(fn).toBeCalledTimes(1)
   })
 
@@ -140,7 +132,7 @@ describe('polling mixin', () => {
     const fn = vi.fn().mockReturnValue(Promise.resolve(true))
     wrapper.vm.registerPoll({ fn, timeout: 1000, immediate: true })
     wrapper.vm.registerPoll({ fn, timeout: 1000, immediate: true })
-    await flushPromisesAndPendingTimers()
+    await flushPromisesAndPendingTimers(wrapper)
     expect(fn).toBeCalledTimes(2)
   })
 })
