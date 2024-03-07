6184a3ab38dfcdad2f1f172032618016f60ef62f
Pierre Romera <hello@pirhoo.com>
2024-03-07 13:45:06 +0000

    refactor: lint

--- tests/unit/specs/components/widget/WidgetNested.spec.js
+++ tests/unit/specs/components/widget/WidgetNested.spec.js
@@ -1,6 +1,6 @@
 import { createLocalVue, mount } from '@vue/test-utils'
-import esConnectionHelper from '~tests/unit/specs/utils/esConnectionHelper'
 
+import esConnectionHelper from '~tests/unit/specs/utils/esConnectionHelper'
 import * as widgets from '@/store/widgets'
 import WidgetNested from '@/components/widget/WidgetNested'
 import { Core } from '@/core'
