43a330bf79e4acaa05c4c56a9e184a6e3ce99cf8
Pierre Romera <hello@pirhoo.com>
2024-03-07 16:34:13 +0000

    fix: use correct icon import

--- src/utils/font-awesome-files.js
+++ src/utils/font-awesome-files.js
@@ -1,18 +1,20 @@
 import find from 'lodash/find'
 import get from 'lodash/get'
 import trim from 'lodash/trim'
-import faFileArchive from '@fortawesome/free-solid-svg-icons/faFileArchive'
-import faFileAudio from '@fortawesome/free-solid-svg-icons/faFileAudio'
-import faFileCsv from '@fortawesome/free-solid-svg-icons/faFileCsv'
-import faFileExcel from '@fortawesome/free-solid-svg-icons/faFileExcel'
-import faFileImage from '@fortawesome/free-solid-svg-icons/faFileImage'
-import faFile from '@fortawesome/free-solid-svg-icons/faFile'
-import faFilePdf from '@fortawesome/free-solid-svg-icons/faFilePdf'
-import faFilePowerpoint from '@fortawesome/free-solid-svg-icons/faFilePowerpoint'
-import faFileText from '@fortawesome/free-solid-svg-icons/faFileText'
-import faFileVideo from '@fortawesome/free-solid-svg-icons/faFileVideo'
-import faFileWord from '@fortawesome/free-solid-svg-icons/faFileWord'
-import faFileZipper from '@fortawesome/free-solid-svg-icons/faFileZipper'
+import {
+  faFileArchive,
+  faFileAudio,
+  faFileCsv,
+  faFileExcel,
+  faFileImage,
+  faFile,
+  faFilePdf,
+  faFilePowerpoint,
+  faFileText,
+  faFileVideo,
+  faFileWord,
+  faFileZipper
+} from '@fortawesome/free-solid-svg-icons'
 
 import types from '@/utils/types.json'
 
