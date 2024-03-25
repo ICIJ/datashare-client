5e2393e4ad2b3963a9bf1cb61a9d9fc77d82f1b7
Caroline Desprat <cdesprat@icij.org>
2024-03-25 09:56:16 +0000

    refactor: batch search error display

--- src/components/BatchSearchStatus.vue
+++ src/components/BatchSearchStatus.vue
@@ -1,29 +1,17 @@
 <template>
-  <div class="batch-search-status">
-    <ellipse-status :status="batchSearch.state" :no-badge="noLabel" horizontal>
-      <template v-if="isFailed" #error>
-        <div v-if="batchSearch.errorQuery" class="batch-search-status__modal__error-query mb-2 fw-bolder">
-          <span v-html="$t('batchSearch.errorQuery', { query: batchSearch.errorQuery })"></span>
-        </div>
-        <div v-if="batchSearch.errorMessage">
-          <template v-if="errorMessageAsJson">
-            <json-formatter
-              class="batch-search-status__modal__error-message"
-              :json="errorMessageAsJson"
-              :open="4"
-              :config="{ theme: 'dark' }"
-            />
-          </template>
-          <template v-else>
-            <pre
-              class="batch-search-status__modal__error-message mt-3 mb-0"
-            ><code>{{ batchSearch.errorMessage }}</code></pre>
-          </template>
-          <div class="mt-2" v-html="$t('batchSearch.errorMessage')"></div>
-        </div>
-      </template>
-    </ellipse-status>
-  </div>
+  <task-item-status
+    :no-label='noLabel'
+    :task-item="{
+      ...batchSearch,
+      key: 'batchSearch'
+    }"
+  >
+    <template #title>
+      <div v-if="batchSearch.errorQuery" class="task-item-status__modal__error-query mb-2 font-weight-bolder">
+        <span v-html="$t('batchSearch.errorQuery', { query: batchSearch.errorQuery })"></span>
+      </div>
+    </template>
+  </task-item-status>
 </template>
 
 <script>
