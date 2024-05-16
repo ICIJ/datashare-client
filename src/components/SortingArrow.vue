525d7e9cffda8d87479bf7c21704919ebeeff63d
Caroline Desprat <cdesprat@icij.org>
2024-05-16 14:55:44 +0000

    feat: add sorting arrow to batchsearch table

--- src/components/SortingArrow.vue
+++ src/components/SortingArrow.vue
@@ -1,8 +1,6 @@
 <template>
   <span>
-      <fa v-if="isAsc" icon="arrow-up" />
-      <fa v-else-if="isDesc" icon="arrow-down" />
-      <fa v-else icon="arrow-up" class="disabled-arrow" />
+      <fa :icon="icon" fixed-width :class="{'disabled-arrow':disabled}" />
   </span>
 </template>
 <script setup>
@@ -10,6 +8,8 @@
   const  props = defineProps(['sortBy', 'fieldKey'])
   const isAsc = computed(() => props.sortBy.key === props.fieldKey && props.sortBy.order.toLowerCase() === 'asc')
   const isDesc = computed(() => props.sortBy.key === props.fieldKey && props.sortBy.order.toLowerCase() === 'desc')
+  const icon = computed(() => isDesc.value ?  'arrow-down' : 'arrow-up')
+  const disabled = computed(() => !(isAsc.value || isDesc.value))
 </script>
 
 <style scoped lang="scss">
