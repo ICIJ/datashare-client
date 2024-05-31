586954402391c541af016afe4e5ebd20224b6a19
Pierre Romera <hello@pirhoo.com>
2024-05-31 22:25:22 +0200

    build: load Murmur without i18n and bootstrapvue

--- vite.config.mjs
+++ vite.config.mjs
@@ -24,6 +24,7 @@ export default ({ mode }) => {
       extensions: ['.mjs', '.js', '.mts', '.ts', '.jsx', '.tsx', '.json', '.vue'],
       alias: {
         path: 'path-browserify',
+        vue: resolve(__dirname, 'node_modules/vue/dist/vue.esm-bundler.js'),
         '@': resolve(__dirname, './src'),
         '~node_modules': resolve('node_modules'),
         '~mixins': resolve(__dirname, './src/mixins'),
@@ -43,6 +44,9 @@ export default ({ mode }) => {
     server: {
       port: 9009,
       host: '0.0.0.0',
+      watch: {
+        usePolling: true
+      },
       proxy: {
         '^/(version|settings|me|api).?': {
           target: process.env.VITE_DEV_PROXY,
