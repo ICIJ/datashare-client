d7b3f2f11503b68b8ae0e7bf109485dee8802b61
Pierre Romera <hello@pirhoo.com>
2024-06-03 16:27:15 +0200

    build: load preview host from env

--- src/mode/index.js
+++ src/mode/index.js
@@ -8,17 +8,20 @@ const MODES = Object.freeze({
   [MODE_NAME.LOCAL]: {
     modeName: 'local',
     multipleProjects: true,
-    manageDocuments: true
+    manageDocuments: true,
+    previewHost: import.meta.env?.VITE_DS_PREVIEW_HOST
   },
   [MODE_NAME.EMBEDDED]: {
     modeName: 'embedded',
     multipleProjects: true,
-    manageDocuments: true
+    manageDocuments: true,
+    previewHost: import.meta.env?.VITE_DS_PREVIEW_HOST
   },
   [MODE_NAME.SERVER]: {
     modeName: 'server',
     multipleProjects: true,
-    manageDocuments: false
+    manageDocuments: false,
+    previewHost: import.meta.env?.VITE_DS_PREVIEW_HOST
   }
 })
 
