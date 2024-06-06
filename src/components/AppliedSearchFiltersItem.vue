e95d71e7a321a6bb3491b4aa4e6456d0c5dd6579
Caroline Desprat <cdesprat@icij.org>
2024-06-06 14:29:27 +0000

    fix: remove console.log

--- src/components/AppliedSearchFiltersItem.vue
+++ src/components/AppliedSearchFiltersItem.vue
@@ -114,7 +114,6 @@ export default {
         await this.$store.dispatch('search/deleteQueryTerm', this.filter.value)
       } else {
         await this.$store.dispatch('search/removeFilterValue', this.filter)
-        console.log("############################# HEY")
         EventBus.emit('filter::search::update', { name: this.filter.name })
       }
 
