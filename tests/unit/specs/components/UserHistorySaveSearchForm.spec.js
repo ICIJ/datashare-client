72979d7d18fda89727e1028b20cf5b8ac64f6f74
Caroline Desprat <cdesprat@icij.org>
2024-06-06 13:24:10 +0000

    fix(test): UserHistorySaveSearchForm.spec.js Co-authored-by: Pierre Romera Zhang <promera@icij.org>

--- tests/unit/specs/components/UserHistorySaveSearchForm.spec.js
+++ tests/unit/specs/components/UserHistorySaveSearchForm.spec.js
@@ -1,32 +1,25 @@
-import Murmur from '@icij/murmur-next'
-import { createLocalVue, shallowMount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 
-import { Core } from '@/core'
+import CoreSetup from '~tests/unit/CoreSetup'
 import UserHistorySaveSearchForm from '@/components/UserHistorySaveSearchForm'
 
 describe('UserHistorySaveSearchForm.vue', () => {
-  let wrapper
-  let i18n, localVue, router, store, wait
-  const propsData = {
-    index: 'project',
-    uri: 'uri'
-  }
-
-  beforeAll(() => {
-    Murmur.config.merge({ mode: 'SERVER' })
-    const core = Core.init(createLocalVue()).useAll()
-    i18n = core.i18n
-    localVue = core.localVue
-    router = core.router
-    store = core.store
-    wait = core.wait
-  })
 
   it('should call "saveSearch" method on click on submit button', async () => {
-    wrapper = shallowMount(UserHistorySaveSearchForm, { i18n, localVue, propsData, router, store, wait })
+    const { plugins, config } = CoreSetup.init().useAll().useRouter()
+    const propsData = {
+      index: 'project',
+      uri: 'uri'
+    }
+    config.merge({ mode: 'SERVER' })
+    const wrapper = shallowMount(UserHistorySaveSearchForm, {
+      propsData,
+      global: { plugins },
+      renderStubDefaultSlot: true
+    })
     const saveSearchMock = vi.spyOn(wrapper.vm, 'saveSearch')
     wrapper.setData({ name: 'Test' })
-    wrapper.find('.card-footer .d-flex b-btn-stub').trigger('submit')
+    wrapper.find('.card-footer .d-flex b-button-stub').trigger('submit')
     expect(saveSearchMock).toBeCalledTimes(1)
   })
 })
