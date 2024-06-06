9ac7785d9373abb393d29bc20a8dd7c0cc89bb24
Caroline Desprat <cdesprat@icij.org>
2024-06-06 11:00:23 +0000

    feat: create core plugin to be used as plugin in tests Co-authored-by: Pierre Romera Zhang <promera@icij.org>

--- tests/unit/specs/pages/Error.spec.js
+++ tests/unit/specs/pages/Error.spec.js
@@ -1,55 +1,50 @@
-import { createLocalVue, shallowMount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import { setCookie, removeCookie } from 'tiny-cookie'
 
 import Auth from '@/api/resources/Auth'
-import { Core } from '@/core'
+import CoreSetup from '~tests/unit/CoreSetup'
 import { getMode } from '@/mode'
 import Error from '@/pages/Error'
 
 describe('Error.vue local mode', () => {
   let wrapper
-  let i18n
-  let localVue
+  let plugins
+  let core
 
   beforeEach(() => {
-    const core = Core.init(createLocalVue()).useAll()
-    i18n = core.i18n
-    localVue = core.localVue
+    core = CoreSetup.init().useAll()
+    plugins = core.plugins
   })
 
   it('should display a custom title', () => {
     const propsData = { title: 'This is wrong!' }
-    wrapper = shallowMount(Error, { i18n, localVue, propsData })
+    wrapper = shallowMount(Error, {
+      propsData,
+      global: { plugins }
+    })
     expect(wrapper.find('.error__container__heading').text()).toBe('This is wrong!')
   })
 
   it('should display a header when user is logged in in server mode', async () => {
     // Mock the user session
     setCookie(process.env.VITE_DS_COOKIE_NAME, { login: 'foo' }, JSON.stringify)
-    // Mock the isServer property
-    const computed = { isServer: () => true }
-    wrapper = shallowMount(Error, { i18n, localVue, computed })
+    core.config.set('mode', 'SERVER')
+    wrapper = shallowMount(Error, {
+      global: { plugins }
+    })
     // Flush promises
     await new Promise((resolve) => setImmediate(resolve))
-    // Render again
     expect(wrapper.find('.error__header').exists()).toBeTruthy()
   })
 })
 
 describe('Error.vue server mode', () => {
-  let wrapper
-  let i18n
-  let localVue
-  beforeEach(() => {
-    const core = Core.init(createLocalVue(), new Auth(getMode('server'))).useAll()
-    i18n = core.i18n
-    localVue = core.localVue
-  })
-
   it('should not display a header when user is logged out in server mode', async () => {
+    const core = CoreSetup.init(new Auth(getMode('server'))).useAll()
+
     removeCookie(process.env.VITE_DS_COOKIE_NAME)
     // Mock the isServer property
-    wrapper = shallowMount(Error, { i18n, localVue })
+    const wrapper = shallowMount(Error, { global: { plugins: core.plugins } })
     // Flush promises
     await new Promise((resolve) => setImmediate(resolve))
     // Render again
