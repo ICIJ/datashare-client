e65131d4df985e66d5b4645e43526c35cf0dc81d
Pierre Romera <hello@pirhoo.com>
2024-06-07 16:51:58 +0000

    fix: use CoreSetup

--- tests/unit/specs/components/NamedEntityInContext.spec.js
+++ tests/unit/specs/components/NamedEntityInContext.spec.js
@@ -1,16 +1,17 @@
-import { createLocalVue, shallowMount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
+import { beforeEach } from 'vitest'
 
+import CoreSetup from '~tests/unit/CoreSetup'
 import { IndexedDocument, letData } from '~tests/unit/es_utils'
 import esConnectionHelper from '~tests/unit/specs/utils/esConnectionHelper'
 import NamedEntityInContext from '@/components/NamedEntityInContext'
-import { Core } from '@/core'
 
 describe('NamedEntityInContext.vue', () => {
   const { index, es } = esConnectionHelper.build()
   const api = { elasticsearch: es }
-  const { i18n, localVue, store, wait } = Core.init(createLocalVue(), api).useAll()
+  let core
 
-  // A quick function to generate the default propsData for most tests
+  // A quick function to generate the default props for most tests
   const defaultPropsData = async function ({ namedEntityIndex = 0, extractLength = 16 } = {}) {
     const category = 'PERSON'
     const id = 'document'
@@ -23,45 +24,53 @@ describe('NamedEntityInContext.vue', () => {
           .withNer('contact@icij.org', -1, category)
       )
       .commit()
-    const document = await store.dispatch('document/get', { id, index })
-    await store.dispatch('document/getContent')
-    await store.dispatch('document/getFirstPageForNamedEntityInCategory', { category })
-    const namedEntities = store.state.document.namedEntitiesPaginatedByCategories
+    const document = await core.store.dispatch('document/get', { id, index })
+    await core.store.dispatch('document/getContent')
+    await core.store.dispatch('document/getFirstPageForNamedEntityInCategory', { category })
+    const namedEntities = core.store.state.document.namedEntitiesPaginatedByCategories
     const namedEntity = namedEntities[category][0].hits[namedEntityIndex]
     return { document, namedEntity, extractLength }
   }
 
-  beforeAll(() => store.commit('search/index', index))
+  beforeEach(() => {
+    core = CoreSetup.init(api).useAll()
+    core.store.commit('search/index', index)
+  })
 
-  afterEach(() => store.commit('document/reset'))
+  afterEach(() => core.store.commit('document/reset'))
 
   it('should be a Vue instance', async () => {
-    const propsData = await defaultPropsData()
-    const wrapper = shallowMount(NamedEntityInContext, { i18n, localVue, propsData, store, wait })
+    const props = await defaultPropsData()
+    const global = { plugins: core.plugins, renderStubDefaultSlot: true }
+    const wrapper = shallowMount(NamedEntityInContext, { props, global })
     expect(wrapper).toBeTruthy()
   })
 
   it('should display the named entity at the beginning of the text', async () => {
-    const propsData = await defaultPropsData()
-    const wrapper = shallowMount(NamedEntityInContext, { i18n, localVue, propsData, store, wait })
+    const props = await defaultPropsData()
+    const global = { plugins: core.plugins, renderStubDefaultSlot: true }
+    const wrapper = shallowMount(NamedEntityInContext, { props, global })
     expect(wrapper.find('.named-entity-in-context__extract').text()).toBe('Lorem Lea ipsum dolor...')
   })
 
   it('should display the named entity at the end of the text', async () => {
-    const propsData = await defaultPropsData({ namedEntityIndex: 1 })
-    const wrapper = shallowMount(NamedEntityInContext, { i18n, localVue, propsData, store, wait })
+    const props = await defaultPropsData({ namedEntityIndex: 1 })
+    const global = { plugins: core.plugins, renderStubDefaultSlot: true }
+    const wrapper = shallowMount(NamedEntityInContext, { props, global })
     expect(wrapper.find('.named-entity-in-context__extract').text()).toBe('...m dolor Yassine...')
   })
 
   it('should display the named entity at the end of the text with a larger chunk', async () => {
-    const propsData = await defaultPropsData({ namedEntityIndex: 1, extractLength: 40 })
-    const wrapper = shallowMount(NamedEntityInContext, { i18n, localVue, propsData, store, wait })
+    const props = await defaultPropsData({ namedEntityIndex: 1, extractLength: 40 })
+    const global = { plugins: core.plugins, renderStubDefaultSlot: true }
+    const wrapper = shallowMount(NamedEntityInContext, { props, global })
     expect(wrapper.find('.named-entity-in-context__extract').text()).toBe('...rem Lea ipsum dolor Yassine sit amet')
   })
 
   it('should not display the extract if the named entity comes from the medata', async () => {
-    const propsData = await defaultPropsData({ namedEntityIndex: 2 })
-    const wrapper = shallowMount(NamedEntityInContext, { i18n, localVue, propsData, store, wait })
+    const props = await defaultPropsData({ namedEntityIndex: 2 })
+    const global = { plugins: core.plugins, renderStubDefaultSlot: true }
+    const wrapper = shallowMount(NamedEntityInContext, { props, global })
     expect(wrapper.find('.named-entity-in-context__extract').exists()).toBeFalsy()
     expect(wrapper.find('.named-entity-in-context__meta').exists()).toBeTruthy()
   })
