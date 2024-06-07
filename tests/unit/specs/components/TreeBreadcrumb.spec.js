029bbbf421d60c900b43ba2c16c9c6b48d7eba21
Pierre Romera <hello@pirhoo.com>
2024-06-07 11:27:18 +0200

    fix: use new CoreSetup

--- tests/unit/specs/components/TreeBreadcrumb.spec.js
+++ tests/unit/specs/components/TreeBreadcrumb.spec.js
@@ -1,30 +1,30 @@
-import { createLocalVue, shallowMount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 
 import TreeBreadcrumb from '@/components/TreeBreadcrumb'
-import { Core } from '@/core'
+import CoreSetup from '~tests/unit/CoreSetup'
 
 describe('TreeBreadcrumb.vue', () => {
-  const { i18n, localVue, config } = Core.init(createLocalVue()).useAll()
+  const { plugins, config } = CoreSetup.init().useAll()
 
   beforeAll(() => {
     config.set('dataDir', '/home/foo')
   })
 
   it('should be a Vue instance', () => {
-    const propsData = { path: '/home/foo/bar', noDatadir: true }
-    const wrapper = shallowMount(TreeBreadcrumb, { i18n, localVue, propsData })
+    const props = { path: '/home/foo/bar', noDatadir: true }
+    const wrapper = shallowMount(TreeBreadcrumb, { global: { plugins }, props })
     expect(wrapper).toBeTruthy()
   })
 
   it('should display 2 directories', async () => {
-    const propsData = { path: '/home/foo/bar', noDatadir: true }
-    const wrapper = shallowMount(TreeBreadcrumb, { i18n, localVue, propsData })
+    const props = { path: '/home/foo/bar', noDatadir: true }
+    const wrapper = shallowMount(TreeBreadcrumb, { global: { plugins }, props })
     expect(wrapper.findAll('.tree-breadcrumb__item')).toHaveLength(2)
   })
 
   it('should display 3 directories', async () => {
-    const propsData = { path: '/home/foo/bar/baz', noDatadir: true }
-    const wrapper = shallowMount(TreeBreadcrumb, { i18n, localVue, propsData })
+    const props = { path: '/home/foo/bar/baz', noDatadir: true }
+    const wrapper = shallowMount(TreeBreadcrumb, { global: { plugins }, props })
     expect(wrapper.findAll('.tree-breadcrumb__item')).toHaveLength(3)
     // One for the root link
     expect(wrapper.find('.tree-breadcrumb__item--root').exists()).toBeTruthy()
@@ -33,8 +33,8 @@ describe('TreeBreadcrumb.vue', () => {
   })
 
   it('should display 4 directories (including the root and the abbreviation)', async () => {
-    const propsData = { path: '/home/foo/bar/baz/lo/rem', maxDirectories: 2, noDatadir: true }
-    const wrapper = shallowMount(TreeBreadcrumb, { i18n, localVue, propsData })
+    const props = { path: '/home/foo/bar/baz/lo/rem', maxDirectories: 2, noDatadir: true }
+    const wrapper = shallowMount(TreeBreadcrumb, { global: { plugins }, props })
     expect(wrapper.findAll('.tree-breadcrumb__item')).toHaveLength(4)
     // One for the root link and one for the abbreviation
     expect(wrapper.find('.tree-breadcrumb__item--root').exists()).toBeTruthy()
@@ -44,8 +44,8 @@ describe('TreeBreadcrumb.vue', () => {
   it('should display 3 directories with Windows pathSeparator', async () => {
     config.set('dataDir', 'C:\\home\\foo')
     config.set('pathSeparator', '\\')
-    const propsData = { path: 'C:\\home\\foo\\bar\\baz', noDatadir: true }
-    const wrapper = shallowMount(TreeBreadcrumb, { i18n, localVue, propsData })
+    const props = { path: 'C:\\home\\foo\\bar\\baz', noDatadir: true }
+    const wrapper = shallowMount(TreeBreadcrumb, { global: { plugins }, props })
     expect(wrapper.findAll('.tree-breadcrumb__item')).toHaveLength(3)
     // One for the root link
     expect(wrapper.find('.tree-breadcrumb__item--root').exists()).toBeTruthy()
@@ -56,8 +56,8 @@ describe('TreeBreadcrumb.vue', () => {
   it('should display 4 directories (including the root and the abbreviation) with Windows pathSeparator', async () => {
     config.set('dataDir', 'C:\\home\\foo')
     config.set('pathSeparator', '\\')
-    const propsData = { path: 'C:\\home\\foo\\bar\\baz\\lo\\rem', maxDirectories: 2, noDatadir: true }
-    const wrapper = shallowMount(TreeBreadcrumb, { i18n, localVue, propsData })
+    const props = { path: 'C:\\home\\foo\\bar\\baz\\lo\\rem', maxDirectories: 2, noDatadir: true }
+    const wrapper = shallowMount(TreeBreadcrumb, { global: { plugins }, props })
     expect(wrapper.findAll('.tree-breadcrumb__item')).toHaveLength(4)
     // One for the root link and one for the abbreviation
     expect(wrapper.find('.tree-breadcrumb__item--root').exists()).toBeTruthy()
