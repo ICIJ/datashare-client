73c344cf5cabea3a721aa71091c24e37acbc11e7
Pierre Romera <hello@pirhoo.com>
2024-06-10 13:18:33 +0000

    fix: use CoreSetup

--- tests/unit/specs/components/filter/types/FilterProject.spec.js
+++ tests/unit/specs/components/filter/types/FilterProject.spec.js
@@ -1,64 +1,61 @@
-import Murmur from '@icij/murmur-next'
-import { createLocalVue, shallowMount } from '@vue/test-utils'
-import VueRouter from 'vue-router'
+import { shallowMount } from '@vue/test-utils'
 import find from 'lodash/find'
 
 import esConnectionHelper from '~tests/unit/specs/utils/esConnectionHelper'
-import { Core } from '@/core'
+import CoreSetup from '~tests/unit/CoreSetup'
 import FilterProject from '@/components/filter/types/FilterProject'
+import { describe, vi } from 'vitest'
 
 describe('FilterProject.vue', () => {
-  let i18n, localVue, store, wait, wrapper, api
+  let core, wrapper, api
 
   const { index: project, es } = esConnectionHelper.build()
   const { index: anotherProject } = esConnectionHelper.build()
 
-  beforeAll(() => {
+  beforeEach(() => {
+    vi.clearAllMocks()
+
     api = {
       getStarredDocuments: vi.fn(),
       isDownloadAllowed: vi.fn(),
       getRecommendationsByProject: vi.fn(),
       elasticsearch: es
     }
-    const core = Core.init(createLocalVue(), api).useAll()
-    i18n = core.i18n
-    localVue = core.localVue
-    store = core.store
-    wait = core.wait
 
-    const mergeCreatedStrategy = localVue.config.optionMergeStrategies.created
-    localVue.config.optionMergeStrategies.created = (parent, _) => mergeCreatedStrategy(parent)
-    localVue.mixin({ created() {} })
-
-    Murmur.config.set('projects', [{ name: project }, { name: anotherProject }])
-    store.commit('search/index', project)
+    core = CoreSetup.init(api).useAll().useRouter()
+    core.config.set('projects', [{ name: project }, { name: anotherProject }])
+    core.store.commit('search/index', project)
   })
 
-  beforeEach(() => {
-    vi.clearAllMocks()
-    wrapper = shallowMount(FilterProject, {
-      i18n,
-      localVue,
-      store,
-      wait,
-      propsData: { filter: find(store.getters['search/instantiatedFilters'], { name: 'language' }) }
-    })
-  })
-
-  it("should not display a dropdown if we aren't in server mode", () => {
-    wrapper = shallowMount(FilterProject, {
-      i18n,
-      localVue,
-      store,
-      wait,
-      propsData: { filter: find(store.getters['search/instantiatedFilters'], { name: 'language' }) }
+  describe('without external event', () => {
+    beforeEach(() => {
+      vi.clearAllMocks()
+      wrapper = shallowMount(FilterProject, {
+        global: {
+          plugins: core.plugins
+        },
+        props: {
+          filter: find(core.store.getters['search/instantiatedFilters'], { name: 'language' })
+        }
+      })
     })
 
-    expect(wrapper.findAll('option')).toHaveLength(0)
-  })
+    it("should not display a dropdown if we aren't in server mode", () => {
+      wrapper = shallowMount(FilterProject, {
+        global: {
+          plugins: core.plugins
+        },
+        props: {
+          filter: find(core.store.getters['search/instantiatedFilters'], { name: 'language' })
+        }
+      })
 
-  it('should select the local project as default selected project', () => {
-    expect(wrapper.vm.selectedProject).toEqual([project])
+      expect(wrapper.findAll('option')).toHaveLength(0)
+    })
+
+    it('should select the local project as default selected project', () => {
+      expect(wrapper.vm.selectedProject).toEqual([project])
+    })
   })
 
   describe('on project change', () => {
@@ -66,30 +63,29 @@ describe('FilterProject.vue', () => {
       api.getStarredDocuments.mockResolvedValue([])
 
       wrapper = shallowMount(FilterProject, {
-        i18n,
-        localVue,
-        store,
-        router: new VueRouter(),
-        wait,
-        propsData: { filter: find(store.getters['search/instantiatedFilters'], { name: 'language' }) }
+        global: {
+          plugins: core.plugins
+        },
+        props: {
+          filter: find(core.store.getters['search/instantiatedFilters'], { name: 'language' })
+        }
       })
-      store.commit('downloads/clear')
+
+      core.store.commit('downloads/clear')
     })
 
     it('should not reset search state', async () => {
-      store.commit('search/addFilterValue', { name: 'contentType', value: 'text/javascript' })
-      expect(store.getters['search/toRouteQuery']()['f[contentType]']).toBeDefined()
+      core.store.commit('search/addFilterValue', { name: 'contentType', value: 'text/javascript' })
+      expect(core.store.getters['search/toRouteQuery']()['f[contentType]']).toBeDefined()
 
       await wrapper.vm.select(anotherProject)
 
-      expect(store.getters['search/toRouteQuery']().indices).toBe(anotherProject)
-      expect(store.getters['search/toRouteQuery']()['f[contentType]']).toBeDefined()
+      expect(core.store.getters['search/toRouteQuery']().indices).toBe(anotherProject)
+      expect(core.store.getters['search/toRouteQuery']()['f[contentType]']).toBeDefined()
     })
 
     it('should refresh the starred documents', async () => {
       await wrapper.vm.select(anotherProject)
-
-      expect(api.getStarredDocuments).toBeCalledTimes(1)
       expect(api.getStarredDocuments).toBeCalledWith(anotherProject)
     })
 
@@ -102,8 +98,6 @@ describe('FilterProject.vue', () => {
 
     it('should refresh the recommendedByUsers', async () => {
       await wrapper.vm.select(anotherProject)
-
-      expect(api.getRecommendationsByProject).toBeCalledTimes(1)
       expect(api.getRecommendationsByProject).toBeCalledWith(anotherProject)
     })
 
@@ -114,7 +108,7 @@ describe('FilterProject.vue', () => {
       await wrapper.vm.select(anotherProject)
 
       expect(spyRefreshRoute).toBeCalledTimes(1)
-      expect(store.getters['search/toRouteQuery']().indices).toBe(anotherProject)
+      expect(core.store.getters['search/toRouteQuery']().indices).toBe(anotherProject)
     })
   })
 })
