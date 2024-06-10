2f2a7f21d5bda3f90b0ccab49f9d7f8ce45539a3
Pierre Romera <hello@pirhoo.com>
2024-06-10 08:30:10 +0000

    fix: use CoreSetup and store mutation directly

--- tests/unit/specs/components/FindNamedEntitiesForm.spec.js
+++ tests/unit/specs/components/FindNamedEntitiesForm.spec.js
@@ -1,79 +1,87 @@
-import { createLocalVue, shallowMount } from '@vue/test-utils'
+import { mount, shallowMount } from '@vue/test-utils'
 
-import { Core } from '@/core'
+import CoreSetup from '~tests/unit/CoreSetup'
 import FindNamedEntitiesForm from '@/components/FindNamedEntitiesForm'
 
 describe('FindNamedEntitiesForm.vue', () => {
-  let wrapper, i18n, localVue, store, wait, api, config
+  let api
 
-  beforeAll(() => {
+  beforeEach(() => {
     api = { getNerPipelines: vi.fn(), findNames: vi.fn() }
   })
 
-  beforeEach(() => {
-    const core = Core.init(createLocalVue(), api).useAll()
-    config = core.config
-    i18n = core.i18n
-    localVue = core.localVue
-    store = core.store
-    wait = core.wait
-    config.set('defaultProject', 'local-datashare')
-    config.set('projects', [{ name: 'local-datashare' }])
-    store.commit('indexing/reset')
-    wrapper = shallowMount(FindNamedEntitiesForm, { i18n, localVue, store, wait })
+  describe('on default project', () => {
+    let wrapper
+
+    beforeEach(() => {
+      const { config, plugins, store } = CoreSetup.init(api).useAll()
+      config.set('defaultProject', 'local-datashare')
+      config.set('projects', [{ name: 'local-datashare' }])
+      store.commit('indexing/reset')
+      wrapper = shallowMount(FindNamedEntitiesForm, { global: { plugins, renderStubDefaultSlot: true } })
+    })
+
+    it('should load NER pipelines on component mounted', () => {
+      expect(api.getNerPipelines).toBeCalledTimes(1)
+    })
+
+    it('should call findNames action with CORENLP pipeline, by default', async () => {
+      await wrapper.vm.submitFindNamedEntities()
+      await expect(api.findNames).toBeCalledWith(
+        'CORENLP',
+        expect.objectContaining({
+          syncModels: true
+        })
+      )
+    })
+
+    it('should call findNames action with ANOTHERNLP pipeline', async () => {
+      await wrapper.vm.$store.commit('indexing/formPipeline', 'ANOTHERNLP')
+      await wrapper.vm.submitFindNamedEntities()
+      expect(api.findNames).toBeCalledWith(
+        'ANOTHERNLP',
+        expect.objectContaining({
+          syncModels: true
+        })
+      )
+    })
+
+    it('should call findNames action with no models synchronization', async () => {
+      await wrapper.vm.$store.commit('indexing/formPipeline', 'CORENLP')
+      await wrapper.vm.$store.commit('indexing/formOffline', true)
+      await wrapper.vm.submitFindNamedEntities()
+      expect(api.findNames).toBeCalledWith(
+        'CORENLP',
+        expect.objectContaining({
+          syncModels: false
+        })
+      )
+    })
+
+    it('should reset the modal params on submitting the form', async () => {
+      await wrapper.setData({ pipeline: 'ANOTHERNLP' })
+      await wrapper.vm.submitFindNamedEntities()
+      expect(wrapper.vm.pipeline).toBe('CORENLP')
+    })
+
+    it('should show the project selector when there is only one project', () => {
+      expect(wrapper.findComponent({ name: 'ProjectSelector' }).exists()).toBeFalsy()
+    })
   })
 
-  it('should load NER pipelines on component mounted', () => {
-    expect(api.getNerPipelines).toBeCalledTimes(1)
-  })
+  describe('on another project', () => {
+    let wrapper
 
-  it('should call findNames action with CORENLP pipeline, by default', () => {
-    wrapper.vm.submitFindNamedEntities()
-    expect(api.findNames).toBeCalledWith(
-      'CORENLP',
-      expect.objectContaining({
-        syncModels: true
-      })
-    )
-  })
+    beforeEach(() => {
+      const { config, plugins, store } = CoreSetup.init(api).useAll()
+      config.set('defaultProject', 'foo')
+      config.set('projects', [{ name: 'bar' }, { name: 'foo' }])
+      store.commit('indexing/reset')
+      wrapper = mount(FindNamedEntitiesForm, { global: { plugins, renderStubDefaultSlot: true } })
+    })
 
-  it('should call findNames action with ANOTHERNLP pipeline', () => {
-    wrapper.vm.$set(wrapper.vm, 'pipeline', 'ANOTHERNLP')
-    wrapper.vm.submitFindNamedEntities()
-    expect(api.findNames).toBeCalledWith(
-      'ANOTHERNLP',
-      expect.objectContaining({
-        syncModels: true
-      })
-    )
-  })
-
-  it('should call findNames action with no models synchronization', () => {
-    wrapper.vm.$set(wrapper.vm, 'pipeline', 'CORENLP')
-    wrapper.vm.$set(wrapper.vm, 'offline', true)
-    wrapper.vm.submitFindNamedEntities()
-    expect(api.findNames).toBeCalledWith(
-      'CORENLP',
-      expect.objectContaining({
-        syncModels: false
-      })
-    )
-  })
-
-  it('should reset the modal params on submitting the form', async () => {
-    wrapper.vm.$set(wrapper.vm, 'pipeline', 'ANOTHERNLP')
-    await wrapper.vm.submitFindNamedEntities()
-    expect(wrapper.vm.pipeline).toBe('CORENLP')
-  })
-
-  it('should show the project selector when there is several projects', async () => {
-    await config.set('defaultProject', 'foo')
-    await config.set('projects', [{ name: 'bar' }, { name: 'foo' }])
-    await wrapper.setData({ defaultProject: 'foo' })
-    expect(wrapper.findComponent({ name: 'ProjectSelector' }).exists()).toBeTruthy()
-  })
-
-  it('should show the project selector when there is only one project', async () => {
-    expect(wrapper.findComponent({ name: 'ProjectSelector' }).exists()).toBeFalsy()
+    it('should show the project selector when there is several projects', async () => {
+      expect(wrapper.findComponent({ name: 'ProjectSelector' }).exists()).toBeTruthy()
+    })
   })
 })
