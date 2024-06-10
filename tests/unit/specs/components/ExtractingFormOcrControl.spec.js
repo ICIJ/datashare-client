4db65e1215b2de0f4cfa06c30fc15aeb8a8963f9
Pierre Romera <hello@pirhoo.com>
2024-06-10 10:06:44 +0000

    fix: use CoreSetup

--- tests/unit/specs/components/ExtractingFormOcrControl.spec.js
+++ tests/unit/specs/components/ExtractingFormOcrControl.spec.js
@@ -1,7 +1,7 @@
-import { createLocalVue, mount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 
 import { flushPromises } from '~tests/unit/tests_utils'
-import { Core } from '@/core'
+import CoreSetup from '~tests/unit/CoreSetup'
 import ExtractingFormOcrControl from '@/components/ExtractingFormOcrControl'
 
 const OCR_LANGUAGES = [
@@ -19,24 +19,21 @@ const TEXT_LANGUAGES = [
 ]
 
 describe('ExtractingFormOcrControl.vue', () => {
+  const { plugins } = CoreSetup.init().useAll()
   let wrapper
-  let wait, store, i18n, localVue
-  beforeAll(async () => {
-    localVue = createLocalVue()
-    const core = Core.init(localVue).useAll()
-    wait = core.wait
-    store = core.store
-    i18n = core.i18n
-  })
 
   describe('OCR for italian language is installed', () => {
     it('should display an alert indicating that no ocr are installed', async () => {
       wrapper = mount(ExtractingFormOcrControl, {
-        wait,
-        store,
-        i18n,
-        localVue,
-        propsData: { isoLang: 'ita', textLanguages: TEXT_LANGUAGES, ocrLanguages: OCR_LANGUAGES, hasTesseract: true }
+        global: {
+          plugins
+        },
+        props: {
+          isoLang: 'ita',
+          textLanguages: TEXT_LANGUAGES,
+          ocrLanguages: OCR_LANGUAGES,
+          hasTesseract: true
+        }
       })
       await flushPromises()
       expect(wrapper.find('.extracting_language_form_control__install_ocr_language').exists()).toBe(true)
@@ -45,11 +42,15 @@ describe('ExtractingFormOcrControl.vue', () => {
 
     it('should display an alert indicating that tesseract is not installed', async () => {
       wrapper = mount(ExtractingFormOcrControl, {
-        wait,
-        store,
-        i18n,
-        localVue,
-        propsData: { isoLang: 'ita', textLanguages: TEXT_LANGUAGES, ocrLanguages: OCR_LANGUAGES, hasTesseract: false }
+        global: {
+          plugins
+        },
+        props: {
+          isoLang: 'ita',
+          textLanguages: TEXT_LANGUAGES,
+          ocrLanguages: OCR_LANGUAGES,
+          hasTesseract: false
+        }
       })
       await flushPromises()
       expect(wrapper.find('.extracting_language_form_control').text()).toContain('Tesseract OCR is not installed.')
