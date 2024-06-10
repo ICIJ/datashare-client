0c3551d0b2309fedc72955933f885eef994607e1
Pierre Romera <hello@pirhoo.com>
2024-06-10 08:38:08 +0000

    fix: use CoreSetup and store from  vm

--- tests/unit/specs/components/FiltersPanel.spec.js
+++ tests/unit/specs/components/FiltersPanel.spec.js
@@ -1,15 +1,15 @@
-import { createLocalVue, shallowMount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 
 import FiltersPanel from '@/components/FiltersPanel'
-import { Core } from '@/core'
+import CoreSetup from '~tests/unit/CoreSetup'
 
 describe('FiltersPanel.vue', () => {
   let wrapper = null
-  const api = { tree: vi.fn() }
-  const { i18n, localVue, router, store, wait } = Core.init(createLocalVue(), api).useAll()
 
   beforeEach(() => {
-    wrapper = shallowMount(FiltersPanel, { i18n, localVue, router, store, wait })
+    const api = { tree: vi.fn() }
+    const { plugins } = CoreSetup.init(api).useAll()
+    wrapper = shallowMount(FiltersPanel, { global: { plugins } })
   })
 
   it('should display the aggregation panel by default', () => {
@@ -18,15 +18,12 @@ describe('FiltersPanel.vue', () => {
 
   it('should hide the aggregation panel on click on `Hide filters`', async () => {
     await wrapper.find('.filters-panel__sticky__toolbar__toggler').trigger('click')
-
     expect(wrapper.find('.filters-panel').isVisible()).toBeFalsy()
   })
 
   it('should not reset the starredDocuments on filters reset', () => {
-    store.commit('starred/documents', [{ id: 'doc_01' }, { id: 'doc_02' }])
-
+    wrapper.vm.$store.commit('starred/documents', [{ id: 'doc_01' }, { id: 'doc_02' }])
     wrapper.vm.$root.$emit('filter::search::reset-filters')
-
-    expect(store.state.starred.documents).toEqual([{ id: 'doc_01' }, { id: 'doc_02' }])
+    expect(wrapper.vm.$store.state.starred.documents).toEqual([{ id: 'doc_01' }, { id: 'doc_02' }])
   })
 })
