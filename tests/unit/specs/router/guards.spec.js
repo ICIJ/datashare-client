ff2fecbe56b81c652f800fe124f355cde7dfcdcb
Caroline Desprat <cdesprat@icij.org>
2024-06-10 09:39:26 +0000

    fix: guards.spec.js

--- tests/unit/specs/router/guards.spec.js
+++ tests/unit/specs/router/guards.spec.js
@@ -1,49 +1,47 @@
-import { createLocalVue, shallowMount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import { setCookie, removeCookie } from 'tiny-cookie'
 import { expect } from 'vitest'
 
-import { Core } from '@/core'
 import { flushPromises } from '~tests/unit/tests_utils'
+import CoreSetup from '~tests/unit/CoreSetup'
 
 describe('guards', () => {
+  const { router, plugins, config } = CoreSetup.init().useAll().useRouter()
+
   describe('checkUserAuthentication', () => {
-    let wrapper = null
+    let wrapper
 
     beforeEach(async () => {
-      const { localVue, router, config } = Core.init(createLocalVue()).useAll()
       config.set('mode', 'SERVER')
       config.set('projects', ['local-datashare'])
       router.addRoute({ path: '/pub', name: 'pub', meta: { skipsAuth: true } })
-      wrapper = shallowMount({ template: '<router-view />' }, { localVue, router })
+      wrapper = shallowMount({ template: '<router-view />' }, { global: { plugins } })
       await flushPromises()
-      await wrapper.vm.$router.replace({ name: 'pub' }).catch(vi.fn())
+      await router.replace({ name: 'pub' }).catch(vi.fn())
     })
 
     it('should redirect to /login if no cookie', async () => {
       removeCookie(process.env.VITE_DS_COOKIE_NAME)
-      await expect(() => {
-        return wrapper.vm.$router.push({ name: 'settings' })
-      }).rejects.toThrowError()
+      await router.push({ name: 'settings' })
+      expect(router.currentRoute.value.name).toBe('login')
     })
 
     it('should redirect to /login if cookie is null', async () => {
       setCookie(process.env.VITE_DS_COOKIE_NAME, null)
-      await expect(() => {
-        return wrapper.vm.$router.push({ name: 'settings' })
-      }).rejects.toThrowError()
+      await router.push({ name: 'settings' })
+      expect(router.currentRoute.value.name).toBe('login')
     })
 
     it('should redirect to /login if cookie has no login property', async () => {
       setCookie(process.env.VITE_DS_COOKIE_NAME, 'yolo', JSON.stringify)
-      await expect(() => {
-        return wrapper.vm.$router.push({ name: 'settings' })
-      }).rejects.toThrowError()
+      await router.push({ name: 'settings' })
+      expect(router.currentRoute.value.name).toBe('login')
     })
 
     it('should not redirect to /login when we have the right cookie', async () => {
       setCookie(process.env.VITE_DS_COOKIE_NAME, { login: 'yolo' }, JSON.stringify)
-      await wrapper.vm.$router.push({ name: 'settings' })
-      expect(wrapper.vm.$route.name).toBe('settings')
+      await router.push({ name: 'settings' })
+      expect(router.currentRoute.value.name).toBe('settings')
     })
   })
 
@@ -52,45 +50,42 @@ describe('guards', () => {
 
     beforeEach(async () => {
       setCookie(process.env.VITE_DS_COOKIE_NAME, { login: 'yolo' }, JSON.stringify)
-      const { localVue, router, config } = Core.init(createLocalVue()).useAll()
       config.set('mode', 'SERVER')
       config.set('projects', ['local-datashare'])
       router.addRoute({ path: '/pub', name: 'pub', meta: { skipsAuth: true } })
-      wrapper = shallowMount({ template: '<router-view />' }, { localVue, router })
+      wrapper = shallowMount({ template: '<router-view />' }, { global: { plugins } })
       await flushPromises()
-      await wrapper.vm.$router.replace({ name: 'pub' }).catch(vi.fn())
+      await router.replace({ name: 'pub' })
     })
 
     it('should redirect task.analysis.list to error in SERVER mode', async () => {
-      wrapper.vm.$config.set('mode', 'SERVER')
-      await expect(() => {
-        return wrapper.vm.$router.push({ name: 'task.analysis.list' })
-      }).rejects.toThrowError()
+      config.set('mode', 'SERVER')
+      await router.push({ name: 'task.analysis.list' })
+      expect(router.currentRoute.value.name).toBe('error')
     })
 
     it('should not redirect task.analysis.list to error in LOCAL mode', async () => {
-      wrapper.vm.$config.set('mode', 'LOCAL')
-      await wrapper.vm.$router.push({ name: '' }).catch(vi.fn())
-      expect(wrapper.vm.$route.name).not.toBe('error')
+      config.set('mode', 'LOCAL')
+      await router.push({ name: 'task.analysis.list' })
+      expect(router.currentRoute.value.name).not.toBe('error')
     })
 
     it('should redirect project.new to error in SERVER mode', async () => {
-      wrapper.vm.$config.set('mode', 'SERVER')
-      await expect(() => {
-        return wrapper.vm.$router.push({ name: 'project.new' })
-      }).rejects.toThrowError()
+      config.set('mode', 'SERVER')
+      await router.push({ name: 'project.new' })
+      expect(router.currentRoute.value.name).toBe('error')
     })
 
     it('should not redirect project.new to error in LOCAL mode', async () => {
       wrapper.vm.$config.set('mode', 'LOCAL')
-      await wrapper.vm.$router.push({ name: 'project.new' }).catch(vi.fn())
-      expect(wrapper.vm.$route.name).not.toBe('error')
+      await router.push({ name: 'project.new' })
+      expect(router.currentRoute.value.name).not.toBe('error')
     })
 
     it('should not redirect project.new to error in EMBEDDED mode', async () => {
       wrapper.vm.$config.set('mode', 'EMBEDDED')
-      await wrapper.vm.$router.push({ name: 'project.new' }).catch(vi.fn())
-      expect(wrapper.vm.$route.name).not.toBe('error')
+      await router.push({ name: 'project.new' })
+      expect(router.currentRoute.value.name).not.toBe('error')
     })
   })
 })
