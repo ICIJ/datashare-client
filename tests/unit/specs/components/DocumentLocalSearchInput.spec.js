5bbf6245210e427d236599ef388c8ce91f6f2a3c
Pierre Romera <hello@pirhoo.com>
2024-06-10 17:38:49 +0000

    fix: use CoreSetup

--- tests/unit/specs/components/DocumentLocalSearchInput.spec.js
+++ tests/unit/specs/components/DocumentLocalSearchInput.spec.js
@@ -1,19 +1,19 @@
-import { createLocalVue, shallowMount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 
-import { Core } from '@/core'
+import CoreSetup from '~tests/unit/CoreSetup'
 import DocumentLocalSearchInput from '@/components/DocumentLocalSearchInput'
 
-const { localVue, store } = Core.init(createLocalVue()).useAll()
+const { plugins } = CoreSetup.init().useAll()
 
 describe('DocumentLocalSearchInput.vue', () => {
   it('should display display search bar, searched term and count of occurrences', () => {
     const wrapper = shallowMount(DocumentLocalSearchInput, {
-      localVue,
-      store,
-      propsData: { searchTerm: 'document' },
-      mocks: { $t: (msg) => msg }
+      global: {
+        plugins,
+        renderStubDefaultSlot: true
+      },
+      props: { modelValue: 'document' }
     })
-
     expect(wrapper.find('.document-local-search-input').classes('document-local-search-input--pristine')).toBeTruthy()
     expect(wrapper.find('.document-local-search-input__term').exists()).toBeTruthy()
     expect(wrapper.find('.document-local-search-input__count').exists()).toBeTruthy()
@@ -21,13 +21,13 @@ describe('DocumentLocalSearchInput.vue', () => {
 
   it('should disable input when disabled props is true', async () => {
     const wrapper = shallowMount(DocumentLocalSearchInput, {
-      localVue,
-      store,
-      propsData: { disabled: false },
-      mocks: { $t: (msg) => msg }
+      global: {
+        plugins
+      },
+      props: { disabled: false }
     })
     expect(wrapper.find('.document-local-search-input__term').attributes('disabled')).toBeUndefined()
     await wrapper.setProps({ disabled: true })
-    expect(wrapper.find('.document-local-search-input__term').attributes('disabled')).toBe('disabled')
+    expect(wrapper.find('.document-local-search-input__term').attributes('disabled')).toBeDefined()
   })
 })
