6ea13c7e51bbb392e88eef419fead7b82beecabe
Pierre Romera <hello@pirhoo.com>
2024-06-10 18:23:46 +0000

    fix: use CoreSetup

--- tests/unit/specs/components/document/DocumentTabDetails.spec.js
+++ tests/unit/specs/components/document/DocumentTabDetails.spec.js
@@ -1,90 +1,96 @@
-import { createLocalVue, mount } from '@vue/test-utils'
-import Murmur from '@icij/murmur-next'
+import { mount } from '@vue/test-utils'
 
 import esConnectionHelper from '~tests/unit/specs/utils/esConnectionHelper'
+import CoreSetup from '~tests/unit/CoreSetup'
 import { IndexedDocument, letData } from '~tests/unit/es_utils'
-import { Core } from '@/core'
 import DocumentTabDetails from '@/components/document/DocumentTabDetails'
 
 describe('DocumentTabDetails.vue', () => {
   const { index, es } = esConnectionHelper.build()
   const id = 'document'
-  let wrapper, i18n, localVue, store, router
+  let wrapper, core
+
   beforeAll(() => {
     const api = { elasticsearch: es }
-    const core = Core.init(createLocalVue(), api).useAll()
-    i18n = core.i18n
-    localVue = core.localVue
-    router = core.router
-    store = core.store
+    core = CoreSetup.init(api).useAll()
   })
 
   afterEach(() => {
-    store.commit('document/reset')
-    Murmur.config.merge({ dataDir: null, mountedDataDir: null })
+    core.store.commit('document/reset')
+    core.config.merge({ dataDir: null, mountedDataDir: null })
   })
 
   it('should display document path with config.mountedDataDir', async () => {
-    Murmur.config.merge({ dataDir: '/home/datashare/data', mountedDataDir: 'C:/Users/ds/docs' })
+    core.config.merge({ dataDir: '/home/datashare/data', mountedDataDir: 'C:/Users/ds/docs' })
     const id = '/home/datashare/data/foo.txt'
     await letData(es).have(new IndexedDocument(id, index)).commit()
-    await store.dispatch('document/get', { id, index })
+    await core.store.dispatch('document/get', { id, index })
     wrapper = mount(DocumentTabDetails, {
-      i18n,
-      localVue,
-      store,
-      router,
-      propsData: { document: store.state.document.doc }
+      global: {
+        plugins: core.plugins
+      },
+      props: {
+        document: core.store.state.document.doc
+      }
     })
 
-    expect(wrapper.find('.document__content__path input[type=text]').vm.value).toBe('C:/Users/ds/docs/foo.txt')
+    const input = wrapper.find('.document__content__path input[type=text]')
+    expect(input.element.value).toBe('C:/Users/ds/docs/foo.txt')
   })
 
   it('should display the document type', async () => {
     await letData(es).have(new IndexedDocument(id, index).withContentType('application/pdf')).commit()
-    await store.dispatch('document/get', { id, index })
+    await core.store.dispatch('document/get', { id, index })
     wrapper = mount(DocumentTabDetails, {
-      i18n,
-      localVue,
-      store,
-      router,
-      propsData: { document: store.state.document.doc }
+      global: {
+        plugins: core.plugins
+      },
+      props: {
+        document: core.store.state.document.doc
+      }
     })
 
-    expect(wrapper.find('.document__content__content-type input[type=text]').vm.value).toBe(
-      'Portable Document Format (PDF)'
-    )
+    const input = wrapper.find('.document__content__content-type input[type=text]')
+    expect(input.element.value).toBe('Portable Document Format (PDF)')
   })
 
   it('should display a child document', async () => {
     const parentDocument = 'parentDocument'
     await letData(es).have(new IndexedDocument(parentDocument, index).withResourceName('parentDocument')).commit()
     await letData(es).have(new IndexedDocument(id, index).withParent(parentDocument)).commit()
-    await store
+    await core.store
       .dispatch('document/get', { index, id, routing: parentDocument })
-      .then(() => store.dispatch('document/getParent'))
+      .then(() => core.store.dispatch('document/getParent'))
     wrapper = mount(DocumentTabDetails, {
-      i18n,
-      localVue,
-      store,
-      router,
-      propsData: { document: store.state.document.doc, parentDocument: store.state.document.parentDocument }
+      global: {
+        plugins: core.plugins
+      },
+      props: {
+        document: core.store.state.document.doc,
+        parentDocument: core.store.state.document.parentDocument
+      }
     })
 
-    expect(wrapper.find('.document__content__basename input[type=text]').vm.value).toBe(id)
-    expect(wrapper.find('.document__content__tree-level input[type=text]').vm.value).toBe('1st')
-    expect(wrapper.find('.document__content__parent input[type=text]').vm.value).toBe(parentDocument)
+    const inputBasename = wrapper.find('.document__content__basename input[type=text]')
+    expect(inputBasename.element.value).toBe(id)
+
+    const inputTreeLevel = wrapper.find('.document__content__tree-level input[type=text]')
+    expect(inputTreeLevel.element.value).toBe('1st')
+
+    const inputParent = wrapper.find('.document__content__parent input[type=text]')
+    expect(inputParent.element.value).toBe(parentDocument)
   })
 
   it('should not display the creation date if it is missing', async () => {
     await letData(es).have(new IndexedDocument(id, index)).commit()
-    await store.dispatch('document/get', { id, index })
+    await core.store.dispatch('document/get', { id, index })
     wrapper = mount(DocumentTabDetails, {
-      i18n,
-      localVue,
-      store,
-      router,
-      propsData: { document: store.state.document.doc }
+      global: {
+        plugins: core.plugins
+      },
+      props: {
+        document: core.store.state.document.doc
+      }
     })
 
     expect(wrapper.find('.document__content__creation-date').exists()).toBeFalsy()
@@ -92,13 +98,14 @@ describe('DocumentTabDetails.vue', () => {
 
   it('should display the creation date if it is defined', async () => {
     await letData(es).have(new IndexedDocument(id, index).withCreationDate('2020-12-04T00:00:01Z')).commit()
-    await store.dispatch('document/get', { id, index })
+    await core.store.dispatch('document/get', { id, index })
     wrapper = mount(DocumentTabDetails, {
-      i18n,
-      localVue,
-      store,
-      router,
-      propsData: { document: store.state.document.doc }
+      global: {
+        plugins: core.plugins
+      },
+      props: {
+        document: core.store.state.document.doc
+      }
     })
 
     expect(wrapper.find('.document__content__creation-date').exists()).toBeTruthy()
@@ -106,13 +113,14 @@ describe('DocumentTabDetails.vue', () => {
 
   it('should not display the author if it is missing', async () => {
     await letData(es).have(new IndexedDocument(id, index)).commit()
-    await store.dispatch('document/get', { id, index })
+    await core.store.dispatch('document/get', { id, index })
     wrapper = mount(DocumentTabDetails, {
-      i18n,
-      localVue,
-      store,
-      router,
-      propsData: { document: store.state.document.doc }
+      global: {
+        plugins: core.plugins
+      },
+      props: {
+        document: core.store.state.document.doc
+      }
     })
 
     expect(wrapper.find('.document__content__author').exists()).toBeFalsy()
@@ -120,13 +128,14 @@ describe('DocumentTabDetails.vue', () => {
 
   it('should display the author date if it is defined', async () => {
     await letData(es).have(new IndexedDocument(id, index).withAuthor('local')).commit()
-    await store.dispatch('document/get', { id, index })
+    await core.store.dispatch('document/get', { id, index })
     wrapper = mount(DocumentTabDetails, {
-      i18n,
-      localVue,
-      store,
-      router,
-      propsData: { document: store.state.document.doc }
+      global: {
+        plugins: core.plugins
+      },
+      props: {
+        document: core.store.state.document.doc
+      }
     })
 
     expect(wrapper.find('.document__content__author').exists()).toBeTruthy()
@@ -134,13 +143,14 @@ describe('DocumentTabDetails.vue', () => {
 
   it('should display a link to the list of children documents', async () => {
     await letData(es).have(new IndexedDocument(id, index)).commit()
-    await store.dispatch('document/get', { id, index })
+    await core.store.dispatch('document/get', { id, index })
     wrapper = mount(DocumentTabDetails, {
-      i18n,
-      localVue,
-      store,
-      router,
-      propsData: { document: store.state.document.doc }
+      global: {
+        plugins: core.plugins
+      },
+      props: {
+        document: core.store.state.document.doc
+      }
     })
 
     expect(wrapper.find('.document__content__shortcuts__children').exists()).toBeTruthy()
@@ -148,13 +158,14 @@ describe('DocumentTabDetails.vue', () => {
 
   it('should display a link to the search in the folder of the document', async () => {
     await letData(es).have(new IndexedDocument(id, index)).commit()
-    await store.dispatch('document/get', { id, index })
+    await core.store.dispatch('document/get', { id, index })
     wrapper = mount(DocumentTabDetails, {
-      i18n,
-      localVue,
-      store,
-      router,
-      propsData: { document: store.state.document.doc }
+      global: {
+        plugins: core.plugins
+      },
+      props: {
+        document: core.store.state.document.doc
+      }
     })
 
     expect(wrapper.find('.document__content__shortcuts__folder').exists()).toBeTruthy()
@@ -162,13 +173,14 @@ describe('DocumentTabDetails.vue', () => {
 
   it('should display an "Unknown" file size', async () => {
     await letData(es).have(new IndexedDocument(id, index)).commit()
-    await store.dispatch('document/get', { id, index })
+    await core.store.dispatch('document/get', { id, index })
     wrapper = mount(DocumentTabDetails, {
-      i18n,
-      localVue,
-      store,
-      router,
-      propsData: { document: store.state.document.doc }
+      global: {
+        plugins: core.plugins
+      },
+      props: {
+        document: core.store.state.document.doc
+      }
     })
 
     expect(wrapper.find('.document__content__content-length').exists()).toBeTruthy()
@@ -177,28 +189,32 @@ describe('DocumentTabDetails.vue', () => {
 
   it('should display an file size', async () => {
     await letData(es).have(new IndexedDocument(id, index).withContentLength('123456')).commit()
-    await store.dispatch('document/get', { id, index })
+    await core.store.dispatch('document/get', { id, index })
     wrapper = mount(DocumentTabDetails, {
-      i18n,
-      localVue,
-      store,
-      router,
-      propsData: { document: store.state.document.doc }
+      global: {
+        plugins: core.plugins,
+        renderStubDefaultSlot: true
+      },
+      props: {
+        document: core.store.state.document.doc
+      }
     })
 
-    expect(wrapper.find('.document__content__content-length').exists()).toBeTruthy()
-    expect(wrapper.find('.document__content__content-length input[type=text]').vm.value).toBe('120.56 KB (123456 B)')
+    const input = wrapper.find('.document__content__content-length input[type=text]')
+    expect(input.exists()).toBeTruthy()
+    expect(input.element.value).toBe('120.56 KB (123456 B)')
   })
 
   it('should display the title and the normalized title', async () => {
     await letData(es).have(new IndexedDocument(id, index)).commit()
-    await store.dispatch('document/get', { id, index })
+    await core.store.dispatch('document/get', { id, index })
     wrapper = mount(DocumentTabDetails, {
-      i18n,
-      localVue,
-      store,
-      router,
-      propsData: { document: store.state.document.doc }
+      global: {
+        plugins: core.plugins
+      },
+      props: {
+        document: core.store.state.document.doc
+      }
     })
 
     expect(wrapper.find('.document__content__title').exists()).toBeTruthy()
