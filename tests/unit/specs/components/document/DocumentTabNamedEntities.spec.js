52f98b9df036d1db9db09d66fd19fa976fa76a2e
Pierre Romera <hello@pirhoo.com>
2024-06-11 08:29:43 +0000

    fix: use CoreSetup

--- tests/unit/specs/components/document/DocumentTabNamedEntities.spec.js
+++ tests/unit/specs/components/document/DocumentTabNamedEntities.spec.js
@@ -1,23 +1,19 @@
-import Murmur from '@icij/murmur-next'
-import { createLocalVue, shallowMount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 
 import { IndexedDocument, letData } from '~tests/unit/es_utils'
 import esConnectionHelper from '~tests/unit/specs/utils/esConnectionHelper'
+import CoreSetup from '~tests/unit/CoreSetup'
 import DocumentTabNamedEntities from '@/components/document/DocumentTabNamedEntities'
-import { Core } from '@/core'
 
 describe('DocumentTabNamedEntities.vue', () => {
   const { index, es } = esConnectionHelper.build()
-  const api = { elasticsearch: es }
-  const { i18n, localVue, store, wait } = Core.init(createLocalVue(), api).useAll()
   const id = 'document'
-  let document = null
-  let wrapper = null
-
-  beforeAll(() => Murmur.config.set('manageDocuments', true))
+  let core
 
   beforeEach(() => {
-    store.commit('document/reset')
+    core = CoreSetup.init({ elasticsearch: es }).useAll()
+    core.config.set('manageDocuments', true)
+    core.store.commit('document/reset')
   })
 
   it('should display named entities in the dedicated tab', async () => {
@@ -30,24 +26,24 @@ describe('DocumentTabNamedEntities.vue', () => {
           .withNer('mention_03', 0, 'LOCATION')
       )
       .commit()
-    document = await store.dispatch('document/get', { id, index })
-    await store.dispatch('document/getFirstPageForNamedEntityInAllCategories')
-    wrapper = shallowMount(DocumentTabNamedEntities, {
-      i18n,
-      localVue,
-      store,
-      wait,
-      propsData: { document },
-      sync: false
+
+    const document = await core.store.dispatch('document/get', { id, index })
+    await core.store.dispatch('document/getFirstPageForNamedEntityInAllCategories')
+    const wrapper = shallowMount(DocumentTabNamedEntities, {
+      global: {
+        plugins: core.plugins,
+        renderStubDefaultSlot: true
+      },
+      props: { document }
     })
 
     const pills = wrapper.findAll('b-badge-stub')
     expect(pills).toHaveLength(3)
-    expect(pills.at(0).find('b-badge-stub').text()).toBe('mention_01')
+    expect(pills.at(0).text()).toBe('mention_01')
     expect(pills.at(0).classes()).toContain('border-category-person')
-    expect(pills.at(1).find('b-badge-stub').text()).toBe('mention_02')
+    expect(pills.at(1).text()).toBe('mention_02')
     expect(pills.at(1).classes()).toContain('border-category-organization')
-    expect(pills.at(2).find('b-badge-stub').text()).toBe('mention_03')
+    expect(pills.at(2).text()).toBe('mention_03')
     expect(pills.at(2).classes()).toContain('border-category-location')
   })
 
@@ -61,36 +57,38 @@ describe('DocumentTabNamedEntities.vue', () => {
           .withNer('loulou', 0, 'PERSON')
       )
       .commit()
-    document = await store.dispatch('document/get', { id, index })
-    await store.dispatch('document/getFirstPageForNamedEntityInAllCategories')
-    wrapper = shallowMount(DocumentTabNamedEntities, {
-      i18n,
-      localVue,
-      store,
-      wait,
-      propsData: { document },
-      sync: false
+
+    const document = await core.store.dispatch('document/get', { id, index })
+    await core.store.dispatch('document/getFirstPageForNamedEntityInAllCategories')
+    const wrapper = shallowMount(DocumentTabNamedEntities, {
+      global: {
+        plugins: core.plugins,
+        renderStubDefaultSlot: true
+      },
+      props: { document }
     })
+
     wrapper.setData({ filterToken: 'lou' })
     await wrapper.vm.getFirstPageInAllCategories()
 
     const pills = wrapper.findAll('b-badge-stub')
     expect(pills).toHaveLength(1)
-    expect(pills.at(0).find('b-badge-stub').text()).toBe('loulou')
+    expect(pills.at(0).text()).toBe('loulou')
     expect(pills.at(0).classes()).toContain('border-category-person')
   })
 
   it('should display a specific error message if no names finding task has been run on that document', async () => {
     await letData(es).have(new IndexedDocument(id, index)).commit()
-    document = await store.dispatch('document/get', { id, index })
-    await store.dispatch('document/getFirstPageForNamedEntityInAllCategories')
-    wrapper = shallowMount(DocumentTabNamedEntities, {
-      i18n,
-      localVue,
-      store,
-      wait,
-      propsData: { document },
-      sync: false
+
+    const document = await core.store.dispatch('document/get', { id, index })
+    await core.store.dispatch('document/getFirstPageForNamedEntityInAllCategories')
+
+    const wrapper = shallowMount(DocumentTabNamedEntities, {
+      global: {
+        plugins: core.plugins,
+        renderStubDefaultSlot: true
+      },
+      props: { document }
     })
 
     expect(wrapper.findAll('.document__named-entities--not--searched')).toHaveLength(1)
@@ -98,15 +96,16 @@ describe('DocumentTabNamedEntities.vue', () => {
 
   it('should display a specific error message if no named entities found after names finding task', async () => {
     await letData(es).have(new IndexedDocument(id, index).withPipeline('CORENLP')).commit()
-    document = await store.dispatch('document/get', { id, index })
-    await store.dispatch('document/getFirstPageForNamedEntityInAllCategories')
-    wrapper = shallowMount(DocumentTabNamedEntities, {
-      i18n,
-      localVue,
-      store,
-      wait,
-      propsData: { document },
-      sync: false
+
+    const document = await core.store.dispatch('document/get', { id, index })
+    await core.store.dispatch('document/getFirstPageForNamedEntityInAllCategories')
+
+    const wrapper = shallowMount(DocumentTabNamedEntities, {
+      global: {
+        plugins: core.plugins,
+        renderStubDefaultSlot: true
+      },
+      props: { document }
     })
 
     expect(wrapper.findAll('.document__named-entities--not--found')).toHaveLength(1)
