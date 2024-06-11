059a2c0bfd6e059f232fbdf3df823e533ea64278
Pierre Romera <hello@pirhoo.com>
2024-06-11 10:57:01 +0000

    fix: use CoreSetup

--- tests/unit/specs/pages/UserHistory.spec.js
+++ tests/unit/specs/pages/UserHistory.spec.js
@@ -1,75 +1,57 @@
-import { createLocalVue, shallowMount, mount } from '@vue/test-utils'
+import { shallowMount, mount } from '@vue/test-utils'
 import VueRouter from 'vue-router'
 
 import { flushPromises } from '~tests/unit/tests_utils'
-import { Core } from '@/core'
+import CoreSetup from '~tests/unit/CoreSetup'
 import UserHistory from '@/pages/UserHistory'
 
 describe('UserHistory.vue', () => {
-  let i18n, localVue, store, wait, router
-  let api
-  let wrapper = null
+  const routes = [
+    {
+      name: 'user-history',
+      path: '/user-history',
+      children: [
+        { name: 'user-history.document.list', path: 'document' },
+        { name: 'user-history.saved-search.list', path: 'search' }
+      ]
+    }
+  ]
 
-  beforeAll(() => {
+  let api, core
+
+  beforeEach(() => {
     api = {
       getUserHistory: vi.fn(),
       deleteUserHistory: vi.fn()
     }
-    const core = Core.init(createLocalVue(), api).useAll()
-    i18n = core.i18n
-    localVue = core.localVue
-    store = core.store
-    wait = core.wait
-  })
-
-  beforeEach(() => {
-    vi.clearAllMocks()
-    router = new VueRouter({
-      routes: [
-        {
-          name: 'user-history',
-          path: '/user-history',
-          children: [
-            { name: 'user-history.document.list', path: 'document' },
-            { name: 'user-history.saved-search.list', path: 'search' }
-          ]
-        }
-      ]
-    })
-  })
-
-  afterEach(() => {
-    wrapper.destroy()
+    core = CoreSetup.init(api).useAll().useRouter(routes)
   })
 
   it('should load the history page', async () => {
-    await router.replace({ name: 'user-history' })
-    wrapper = shallowMount(UserHistory, { i18n, localVue, router, store, wait })
+    await core.router.replace({ name: 'user-history' })
+    const wrapper = shallowMount(UserHistory, { global: { plugins: core.plugins } })
     expect(wrapper.find('page-header-stub').exists()).toBeTruthy()
   })
 
   it('should load the document history page by default', async () => {
-    await router.replace({ name: 'user-history' })
-    wrapper = mount(UserHistory, { i18n, localVue, router, store, wait })
+    await core.router.replace({ name: 'user-history' })
+    const wrapper = mount(UserHistory, { global: { plugins: core.plugins } })
     await flushPromises()
     expect(wrapper.vm.$route.name).toBe('user-history.document.list')
   })
 
   it('should call get user history when page is loaded', async () => {
-    await router.replace({ name: 'user-history.document.list' })
-    wrapper = shallowMount(UserHistory, { i18n, localVue, router, store, wait })
-    await wrapper.vm.$nextTick()
-    expect(api.getUserHistory).toBeCalledTimes(1)
+    await core.router.replace({ name: 'user-history.document.list' })
+    shallowMount(UserHistory, { global: { plugins: core.plugins } })
+    await flushPromises()
     expect(api.getUserHistory).toBeCalledWith('document', 0, 100, undefined, undefined, undefined)
   })
 
   it('should call delete user history api function is called', async () => {
-    await router.replace({ name: 'user-history.document.list' })
-    wrapper = shallowMount(UserHistory, { i18n, localVue, router, store, wait })
+    await core.router.replace({ name: 'user-history.document.list' })
+    const wrapper = shallowMount(UserHistory, { global: { plugins: core.plugins } })
     await wrapper.vm.deleteUserHistory()
-    await wrapper.vm.$nextTick()
-
-    expect(api.deleteUserHistory).toBeCalledTimes(1)
+    await flushPromises()
     expect(api.deleteUserHistory).toBeCalledWith('document')
   })
 })
