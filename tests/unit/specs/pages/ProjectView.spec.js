524e5c20f94b50c1b4bf9b003c13c741b77d1050
Pierre Romera <hello@pirhoo.com>
2024-06-11 10:11:51 +0000

    fix: use CoreSetup

--- tests/unit/specs/pages/ProjectView.spec.js
+++ tests/unit/specs/pages/ProjectView.spec.js
@@ -1,33 +1,26 @@
-import { createLocalVue, mount } from '@vue/test-utils'
-import VueRouter from 'vue-router'
+import { mount } from '@vue/test-utils'
 
-import { Core } from '@/core'
+import CoreSetup from '~tests/unit/CoreSetup'
 import ProjectView from '@/pages/ProjectView'
 
 describe('ProjectView.vue', () => {
-  let config, i18n, localVue, router, store, wait
+  let core
 
-  beforeAll(() => {
-    const core = Core.init(createLocalVue()).useAll()
-    config = core.config
-    i18n = core.i18n
-    localVue = core.localVue
-    store = core.store
-    router = core.router
-    const routes = [
-      {
-        name: 'project.view.insights',
-        path: 'insights'
-      },
-      {
-        name: 'project.view.edit',
-        path: 'edit'
-      }
-    ]
-    router = new VueRouter({ routes })
-    wait = core.wait
+  beforeEach(() => {
+    core = CoreSetup.init()
+      .useAll()
+      .useRouter([
+        {
+          name: 'project.view.insights',
+          path: '/insights'
+        },
+        {
+          name: 'project.view.edit',
+          path: '/edit'
+        }
+      ])
     // Ensure the local-datashare project can be found
-    config.set('projects', [{ name: 'local-datashare', label: 'Default', sourcePath: '/' }])
+    core.config.set('projects', [{ name: 'local-datashare', label: 'Default', sourcePath: '/' }])
   })
 
   afterAll(() => {
@@ -35,28 +28,25 @@ describe('ProjectView.vue', () => {
   })
 
   it('contains an edit link in LOCAL mode', async () => {
-    config.set('mode', 'LOCAL')
-    const stubs = { RouterView: true }
-    const propsData = { name: 'local-datashare' }
-    const wrapper = mount(ProjectView, { localVue, store, router, wait, i18n, stubs, propsData })
+    core.config.set('mode', 'LOCAL')
+    const props = { name: 'local-datashare' }
+    const wrapper = mount(ProjectView, { global: { plugins: core.plugins }, props })
     await wrapper.vm.$nextTick()
     expect(wrapper.find('.project-view__tab--edit').exists()).toBe(true)
   })
 
   it('contains an edit link in EMBEDDED mode', async () => {
-    config.set('mode', 'EMBEDDED')
-    const stubs = { RouterView: true }
-    const propsData = { name: 'local-datashare' }
-    const wrapper = mount(ProjectView, { localVue, store, router, wait, i18n, stubs, propsData })
+    core.config.set('mode', 'EMBEDDED')
+    const props = { name: 'local-datashare' }
+    const wrapper = mount(ProjectView, { global: { plugins: core.plugins }, props })
     await wrapper.vm.$nextTick()
     expect(wrapper.find('.project-view__tab--edit').exists()).toBe(true)
   })
 
   it('not contains an edit link in SERVER mode', async () => {
-    config.set('mode', 'SERVER')
-    const stubs = { RouterView: true }
-    const propsData = { name: 'local-datashare' }
-    const wrapper = mount(ProjectView, { localVue, store, router, wait, i18n, stubs, propsData })
+    core.config.set('mode', 'SERVER')
+    const props = { name: 'local-datashare' }
+    const wrapper = mount(ProjectView, { global: { plugins: core.plugins }, props })
     await wrapper.vm.$nextTick()
     expect(wrapper.find('.project-view__tab--edit').exists()).toBe(false)
   })
