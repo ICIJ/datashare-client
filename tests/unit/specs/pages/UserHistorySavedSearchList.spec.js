945aba15cbfcab63c8b3f6ff5c8e7b3cdf3cab79
Pierre Romera <hello@pirhoo.com>
2024-06-11 12:42:00 +0000

    fix: use CoreSetup and watch for change on events prop

--- tests/unit/specs/pages/UserHistorySavedSearchList.spec.js
+++ tests/unit/specs/pages/UserHistorySavedSearchList.spec.js
@@ -1,9 +1,9 @@
-import { createLocalVue, shallowMount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 
-import { Core } from '@/core'
+import CoreSetup from '~tests/unit/CoreSetup'
 import UserHistorySavedSearchList from '@/pages/UserHistorySavedSearchList'
 
-const propsData = {
+const props = {
   events: [
     {
       id: 'id_01',
@@ -37,26 +37,16 @@ const propsData = {
 }
 
 describe('UserHistorySavedSearchList.vue', () => {
-  let i18n, localVue, api
+  let api, wrapper
 
-  let wrapper = null
-
-  beforeAll(() => {
-    api = { deleteUserHistoryEvent: vi.fn() }
-    const core = Core.init(createLocalVue(), api).useAll()
-    i18n = core.i18n
-    localVue = core.localVue
-  })
   beforeEach(async () => {
-    api.deleteUserHistoryEvent.mockClear()
-    api.deleteUserHistoryEvent.mockResolvedValue({})
-
-    wrapper = await shallowMount(UserHistorySavedSearchList, { i18n, localVue, propsData })
+    api = { deleteUserHistoryEvent: vi.fn().mockResolvedValue({}) }
+    const { plugins } = CoreSetup.init(api).useAll()
+    wrapper = await shallowMount(UserHistorySavedSearchList, { global: { plugins }, props })
   })
 
   it('should NOT display a list of search', async () => {
-    const propsData = { events: [] }
-    wrapper = await shallowMount(UserHistorySavedSearchList, { i18n, localVue, propsData })
+    await wrapper.setProps({ events: [] })
     expect(wrapper.findAll('.user-history-saved-search-list__list__item')).toHaveLength(0)
   })
 
@@ -92,10 +82,7 @@ describe('UserHistorySavedSearchList.vue', () => {
       uri: 'uri_01',
       creationDate: '2023-02-26T09:56:47.661+00:00'
     }
-
-    const propsData = { events: [event] }
-    wrapper = await shallowMount(UserHistorySavedSearchList, { i18n, localVue, propsData })
-
+    await wrapper.setProps({ events: [event] })
     expect(wrapper.find('.user-history-saved-search-list__list__item__date').text()).toBe('2023/02/26 09:56')
   })
 
