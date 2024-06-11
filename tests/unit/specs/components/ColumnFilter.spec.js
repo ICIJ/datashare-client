96b4c23f37337fa395be8ff77abf30a7e417584f
Pierre Romera <hello@pirhoo.com>
2024-06-11 08:59:46 +0000

    fix: use CoreSetup

--- tests/unit/specs/components/ColumnFilter.spec.js
+++ tests/unit/specs/components/ColumnFilter.spec.js
@@ -1,11 +1,10 @@
-import { createLocalVue, shallowMount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 
+import CoreSetup from '~tests/unit/CoreSetup'
 import ColumnFilter from '@/components/ColumnFilter'
-import { Core } from '@/core'
 
 describe('ColumnFilter.vue', () => {
-  let wrapper = null
-  const { i18n, localVue } = Core.init(createLocalVue()).useAll()
+  let plugins
 
   function createContainer() {
     const div = document.createElement('div')
@@ -13,26 +12,37 @@ describe('ColumnFilter.vue', () => {
     return div
   }
 
+  beforeEach(() => {
+    const core = CoreSetup.init().useAll()
+    plugins = core.plugins
+  })
+
   it('display the name of the filter with a button and a badge', async () => {
-    const propsData = { id: 'filter_id', name: 'filter_name', active: false }
+    const props = { id: 'filter_id', name: 'filter_name', active: false }
     const attachTo = createContainer()
 
-    wrapper = shallowMount(ColumnFilter, { attachTo, i18n, localVue, propsData })
+    const wrapper = shallowMount(ColumnFilter, {
+      attachTo,
+      global: { plugins, renderStubDefaultSlot: true },
+      props
+    })
+
     expect(wrapper.find('.column-filter').exists()).toBeTruthy()
-    expect(wrapper.find('.column-filter__label').text()).toBe('filter_name')
     const toggleSelector = '#column-filter__toggle--filter_id-id.column-filter__toggle--filter_id'
     expect(wrapper.find(toggleSelector).exists()).toBeTruthy()
     expect(wrapper.find('column-filter-badge-stub').exists()).toBeTruthy()
     expect(wrapper.find('b-popover-stub').exists()).toBeTruthy()
   })
 
-  it('shows set badge to active when filter is active', async () => {
-    const propsData = { id: 'filter_id', name: 'filter_name', active: false }
-    wrapper = shallowMount(ColumnFilter, { i18n, localVue, propsData })
-    expect(wrapper.find('column-filter-badge-stub').vm?.active).toBe(false)
-    await wrapper.setProps({ active: true })
-    expect(wrapper.find('column-filter-badge-stub').vm?.active).toBe(true)
-  })
+  it('sets badge to active when filter is active', async () => {
+    const props = { id: 'filter_id', name: 'filter_name', active: false }
+    const wrapper = shallowMount(ColumnFilter, {
+      global: { plugins, renderStubDefaultSlot: true },
+      props
+    })
 
-  // TODO CD: test popover with slot?
+    expect(wrapper.findComponent({ name: 'column-filter-badge' }).props('active')).toBe(false)
+    await wrapper.setProps({ active: true })
+    expect(wrapper.findComponent({ name: 'column-filter-badge' }).props('active')).toBe(true)
+  })
 })
