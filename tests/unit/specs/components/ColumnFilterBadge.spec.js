7d7f0d827503a858a3383cc02049677d6c6ebb89
Pierre Romera <hello@pirhoo.com>
2024-06-11 08:52:08 +0000

    fix: use CoreSetup

--- tests/unit/specs/components/ColumnFilterBadge.spec.js
+++ tests/unit/specs/components/ColumnFilterBadge.spec.js
@@ -1,19 +1,30 @@
-import { createLocalVue, shallowMount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 
+import CoreSetup from '~tests/unit/CoreSetup'
 import ColumnFiltersBadge from '@/components/ColumnFilterBadge'
-import { Core } from '@/core'
 
 describe('ColumnFiltersBadge.vue', () => {
-  const { i18n, localVue } = Core.init(createLocalVue()).useAll()
-  let wrapper = null
+  let plugins, wrapper
+
+  beforeEach(() => {
+    const core = CoreSetup.init().useAll()
+    plugins = core.plugins
+  })
 
   it('should show icon badge when no counter', async () => {
-    wrapper = shallowMount(ColumnFiltersBadge, { i18n, localVue })
+    wrapper = shallowMount(ColumnFiltersBadge, {
+      global: { plugins, renderStubDefaultSlot: true },
+      props: { active: true }
+    })
     const badgeWrapper = wrapper.find('.column-filter-badge__icon')
     expect(badgeWrapper.exists()).toBeTruthy()
   })
+
   it('hides icon badge when inactive', async () => {
-    wrapper = shallowMount(ColumnFiltersBadge, { i18n, localVue, propsData: { active: false } })
+    wrapper = shallowMount(ColumnFiltersBadge, {
+      global: { plugins },
+      props: { active: false }
+    })
     expect(wrapper.find('.column-filter-badge').exists()).toBeTruthy()
     expect(wrapper.find('.column-filter-badge--inactive').exists()).toBeTruthy()
     await wrapper.setProps({ active: true })
@@ -21,7 +32,10 @@ describe('ColumnFiltersBadge.vue', () => {
   })
 
   it('should show counter badge with number', async () => {
-    wrapper = shallowMount(ColumnFiltersBadge, { i18n, localVue, propsData: { counter: 2 } })
+    wrapper = shallowMount(ColumnFiltersBadge, {
+      global: { plugins, renderStubDefaultSlot: true },
+      props: { counter: 2 }
+    })
     const badgeWrapper = wrapper.find('.column-filter-badge__counter')
     expect(badgeWrapper.exists()).toBeTruthy()
     expect(badgeWrapper.text()).toBe('2')
