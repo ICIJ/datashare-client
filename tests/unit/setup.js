5b4b83a9189f43a4cae61f60af32534d0adeb201
Pierre Romera <hello@pirhoo.com>
2024-06-12 13:50:40 +0000

    refactor: use defineProperty to setup mocks

--- tests/unit/setup.js
+++ tests/unit/setup.js
@@ -31,17 +31,20 @@ global.ResizeObserver = vi.fn().mockImplementation(() => ({
   disconnect: vi.fn()
 }))
 
-document.fonts = {}
-document.fonts.ready = Promise.resolve()
-
-window.matchMedia = vi.fn().mockImplementation((query) => {
-  return {
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: vi.fn().mockImplementation((query) => ({
     matches: false,
     media: query,
     onchange: null,
-    addListener: vi.fn(),
-    removeListener: vi.fn()
-  }
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn()
+  }))
 })
 
-window.scrollTo = vi.fn()
+Object.defineProperty(document, 'fonts', {
+  ready: Promise.resolve()
+})
+
+Object.defineProperty(window, 'scrollTo', vi.fn())
