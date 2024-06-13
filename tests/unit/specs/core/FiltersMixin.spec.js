9c5cc49de0f0b2171b6042e5644772301e5ad81b
Pierre Romera <hello@pirhoo.com>
2024-06-13 15:53:06 +0000

    test: add FiltersMixins test

---
+++ tests/unit/specs/core/FiltersMixin.spec.js
@@ -0,0 +1,59 @@
+import { Core } from '@/core'
+
+describe('HooksMixin', () => {
+  let api, core
+
+  beforeEach(() => {
+    api = {
+      createProject: vi.fn(),
+      isDownloadAllowed: vi.fn(),
+      getUser: vi.fn(),
+      getSettings: vi.fn().mockResolvedValue({}),
+      getProject: vi.fn().mockResolvedValue({})
+    }
+  })
+
+  it('register an email mixin', () => {
+    core = Core.init(api).useAll()
+    core.registerFilter({
+      type: 'FilterNamedEntity',
+      options: {
+        order: 65,
+        name: 'namedEntityEmail',
+        key: 'byMentions',
+        category: 'EMAIL',
+        isSearchable: true
+      }
+    })
+
+    const filter = core.store.getters['search/getFilter']({ name: 'namedEntityEmail' })
+    expect(filter).toBeDefined()
+  })
+
+  it('should register the filter after the global event "datashare:ready"', () => {
+    const core = Core.init(api).useAll()
+
+    return new Promise((resolve) => {
+      const ready = ({ detail }) => {
+        detail.core.registerFilter({
+          type: 'FilterNamedEntity',
+          options: {
+            order: 65,
+            name: 'namedEntityEmail',
+            key: 'byMentions',
+            category: 'EMAIL',
+            isSearchable: true
+          }
+        })
+
+        const filter = core.store.getters['search/getFilter']({ name: 'namedEntityEmail' })
+        expect(filter).toBeDefined()
+        resolve()
+      }
+
+      document.addEventListener('datashare:ready', ready, { once: true })
+
+      return core.configure()
+    })
+  })
+})
