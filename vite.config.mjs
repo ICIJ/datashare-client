484affdcbb071abdd8f6f91b2a34307384c81682
Pierre Romera <hello@pirhoo.com>
2024-06-19 18:20:25 +0000

    fix: restore VITE_GIT_HASH

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,11 +1,13 @@
 import { BootstrapVueNextResolver } from 'unplugin-vue-components/resolvers'
 import { defineConfig, loadEnv } from 'vite'
 import { resolve } from 'path'
+import * as childProcess from 'child_process'
 import Components from 'unplugin-vue-components/vite'
 import vue from '@vitejs/plugin-vue'
 
 export default ({ mode }) => {
-  process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''))
+  const VITE_GIT_HASH = childProcess.execSync('git rev-parse HEAD').toString()
+  process.env = Object.assign(process.env, { VITE_GIT_HASH, ...loadEnv(mode, process.cwd(), '') })
 
   return defineConfig({
     plugins: [
