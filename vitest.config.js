6282087afd1314eb8228ffca84c2cd48680b2579
Caroline Desprat <cdesprat@icij.org>
2024-06-21 14:46:39 +0000

    feat: split vitest config and vite config

---
+++ vitest.config.js
@@ -0,0 +1,18 @@
+import { fileURLToPath } from 'node:url'
+import { resolve } from 'path'
+import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
+import viteConfig from './vite.config'
+
+export default defineConfig(configEnv => mergeConfig(
+  viteConfig(configEnv),
+  defineConfig({
+    test: {
+      globals: true,
+      reporters: 'basic',
+      environment: 'jsdom',
+      exclude: [...configDefaults.exclude, 'e2e/**'],
+      root: fileURLToPath(new URL('./', import.meta.url)),
+      setupFiles: [resolve(__dirname, 'tests/unit/setup.js')]
+    }
+  })
+))
