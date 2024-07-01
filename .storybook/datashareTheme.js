6612450daf08c1ae07f256f7e8b62a42f90d5531
Pierre Romera <hello@pirhoo.com>
2024-07-01 10:48:03 +0000

    doc: configure theme

--- .storybook/datashareTheme.js
+++
@@ -1,9 +0,0 @@
-
-
-import { create } from '@storybook/theming/create';
-export default create({
-    base: 'light',
-    brandTitle: 'Datashare design system',
-    brandUrl: 'https://datashare.icij.org',
-    brandTarget: '_self',
-  });
