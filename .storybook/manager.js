8818db15aa8e4f2a6527a82fd0f31d812ba3c703
Pierre Romera <hello@pirhoo.com>
2024-07-01 10:48:03 +0000

    doc: configure theme

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,6 +1,4 @@
-import { addons } from '@storybook/manager-api';
-import datashareTheme from './datashareTheme';
+import { addons } from '@storybook/manager-api'
+import theme from './theme'
 
-addons.setConfig({
-    theme: datashareTheme,
-  });
+addons.setConfig({ navSize: 300, theme })
