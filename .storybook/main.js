6f4e83919676c8c0974d159efb753620cf45e7e9
Caroline Desprat <cdesprat@icij.org>
2024-07-01 09:39:43 +0000

    feat: add basic UI components (button, icon, text) to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,15 @@
 /** @type { import('@storybook/vue3-vite').StorybookConfig } */
 const config = {
+  core: {
+    disableTelemetry: true // 👈 Disables telemetry
+  },
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    '@storybook/addon-themes',
   ],
   framework: {
     name: "@storybook/vue3-vite",
--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,9 @@
-import { BootstrapVueNextResolver } from 'unplugin-vue-components/resolvers'
 import { defineConfig, loadEnv } from 'vite'
 import { resolve } from 'path'
 import * as childProcess from 'child_process'
 import Components from 'unplugin-vue-components/vite'
 import vue from '@vitejs/plugin-vue'
+import { BootstrapVueNextResolver } from 'unplugin-vue-components/resolvers'
 
 export default ({ mode }) => {
   const VITE_GIT_HASH = childProcess.execSync('git rev-parse HEAD').toString()
