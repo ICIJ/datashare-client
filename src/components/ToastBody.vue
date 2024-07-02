01440db8a973db0b221fdcbd3ba52b664c0befa1
Pierre Romera <hello@pirhoo.com>
2024-07-02 09:49:25 +0000

    feat: add noIcon option and slots

--- src/components/ToastBody.vue
+++ src/components/ToastBody.vue
@@ -1,9 +1,18 @@
 <script setup>
-import { computed } from 'vue'
+import { computed, useSlots } from 'vue'
+
+import Icon from '@/components/Icon'
 
 const VARIANTS = {
   default: 'primary',
   error: 'danger'
+}
+
+const ICONS = {
+  default: 'info',
+  success: 'check',
+  danger: 'x',
+  warning: 'warning'
 }
 
 const props = defineProps({
@@ -15,8 +24,7 @@ const props = defineProps({
     type: Function
   },
   body: {
-    type: String,
-    required: true
+    type: String
   },
   title: {
     type: String
@@ -27,8 +35,17 @@ const props = defineProps({
   linkLabel: {
     type: String,
     default: null
+  },
+  icon: {
+    type: String,
+    default: null
+  },
+  noIcon: {
+    type: Boolean
   }
 })
+
+const slots = useSlots()
 
 const classList = computed(() => {
   return {
@@ -36,22 +53,30 @@ const classList = computed(() => {
   }
 })
 
-const variant = computed(() => VARIANTS[props.toastProps.type ?? 'default'] ?? props.toastProps.type)
-const hasLink = computed(() => !!props.href)
+const variant = computed(() => VARIANTS[props.toastProps?.type ?? 'default'] ?? props.toastProps?.type)
+const icon = computed(() => ICONS[props.toastProps?.type ?? 'default'] ?? 'info')
+const hasLink = computed(() => !!props.href || !!slots.link)
 const linkLabelDisplay = computed(() => props.linkLabel ?? 'See more')
 const linkClassList = computed(() => [`btn-outline-${variant.value}`])
 </script>
 
 <template>
   <div class="toast-body d-flex align-items-center" :class="classList">
+    <div v-if="!noIcon" class="toast-body__icon pe-3">
+      <Icon :icon="icon" :variant="variant" />
+    </div>
     <div class="toast-body__content flex-grow-1">
       <h5 v-if="title" class="toast-body__content__title">{{ title }}</h5>
-      <p class="toast-body__content__body m-0">{{ body }}</p>
+      <p class="toast-body__content__body m-0">
+        <slot>{{ body }}</slot>
+      </p>
     </div>
     <div v-if="hasLink" class="toast-body__link pe-3">
-      <a :href="href" class="btn btn-sm text-nowrap" :class="linkClassList" @click.passive="closeToast">
-        {{ linkLabelDisplay }}
-      </a>
+      <slot name="link" v-bind="{ closeToast, linkClassList, linkLabelDisplay }">
+        <a :href="href" class="btn btn-sm text-nowrap" :class="linkClassList" @click.passive="closeToast">
+          {{ linkLabelDisplay }}
+        </a>
+      </slot>
     </div>
   </div>
 </template>
@@ -73,7 +98,8 @@ const linkClassList = computed(() => [`btn-outline-${variant.value}`])
     }
   }
 
-  &__link .btn:not(:hover) {
+  &__link .btn:not(:hover),
+  &__link:deep(.btn:not(:hover)) {
     background: #fff;
   }
 }
