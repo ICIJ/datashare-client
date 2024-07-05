688ec6b46d39575940926040d431054abde68866
Pierre Romera <hello@pirhoo.com>
2024-07-05 10:50:33 +0000

    fix: add missing `to`

--- src/stories/components/AppSidebarSection.stories.js
+++ src/stories/components/AppSidebarSection.stories.js
@@ -47,13 +47,13 @@ export default {
     template: `
       <div :style="style">
         <app-sidebar-section v-bind="args" :to="{ name: 'home' }">
-          <app-sidebar-section-entry icon="dots-nine" :action-to="{ name: 'home' }" action-title="Add project">
+          <app-sidebar-section-entry icon="dots-nine" :to="{ name: 'home' }" :action-to="{ name: 'home' }" action-title="Add project">
             All projects
           </app-sidebar-section-entry>
-          <app-sidebar-section-entry :active="args.active" icon="push-pin">
+          <app-sidebar-section-entry :to="{ name: 'home' }" :active="args.active" icon="push-pin">
             Banana Papers
           </app-sidebar-section-entry>
-          <app-sidebar-section-entry icon="push-pin">
+          <app-sidebar-section-entry :to="{ name: 'home' }" icon="push-pin">
             Citrus Confidential
           </app-sidebar-section-entry>
         </app-sidebar-section>
