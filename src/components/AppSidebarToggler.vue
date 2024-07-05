3ac720c707a045782f70f57c3f954b7e5c135dc7
Pierre Romera <hello@pirhoo.com>
2024-07-05 08:45:10 +0000

    feat: add sidebar toggler

---
+++ src/components/AppSidebarToggler.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { computed } from 'vue'
+
+import IconButton from '@/components/IconButton'
+
+const props = defineProps({
+  compact: {
+    type: Boolean
+  }
+})
+
+const emit = defineEmits(['toggle'])
+
+const toggle = () => {
+  emit('toggle', !props.compact)
+}
+
+const classList = computed(() => {
+  return {
+    'app-sidebar-toggler--compact': props.compact
+  }
+})
+
+const title = computed(() => {
+  return props.compact ? 'Extend sidebar' : 'Reduce sidebar'
+})
+</script>
+
+<template>
+  <icon-button
+    v-b-tooltip.right
+    icon-left="sidebar-simple"
+    square
+    hide-label
+    variant="secondary"
+    class="app-sidebar-toggler"
+    :title="title"
+    :class="classList"
+    @click="toggle"
+  >
+    {{ title }}
+  </icon-button>
+</template>
