9c2003ab7eb7018dbf0e09c5a76e59af1ce679b7
Pierre Romera <hello@pirhoo.com>
2024-07-05 17:08:04 +0000

    feat: add navigation breadcrumb

---
+++ src/components/NavigationBreadcrumbLink.vue
@@ -0,0 +1,103 @@
+<script setup>
+import { computed } from 'vue'
+import { capitalize, isFunction } from 'lodash'
+import { useRouter } from 'vue-router'
+
+import PhosphorIcon from '@/components/PhosphorIcon'
+
+function castFunction(value) {
+  if (isFunction(value)) {
+    return value
+  }
+  return () => value
+}
+
+const props = defineProps({
+  routeName: {
+    type: String
+  },
+  currentRouteName: {
+    type: String
+  },
+  noCaret: {
+    type: Boolean
+  },
+  active: {
+    type: Boolean
+  },
+  icon: {
+    type: String
+  },
+  title: {
+    type: String
+  }
+})
+
+const router = useRouter()
+
+const currentRoute = computed(() => {
+  const name = props.currentRouteName ?? router.currentRoute.value?.name
+  try {
+    return router.resolve({ name })
+  } catch {
+    return null
+  }
+})
+
+const route = computed(() => {
+  try {
+    return router.resolve({ name: props.routeName })
+  } catch {
+    return null
+  }
+})
+
+const routeLocation = computed(() => {
+  const { name, params, query } = route.value ?? {}
+  return { name, params, query }
+})
+
+const title = computed(() => {
+  const name = route.value?.name.split('.').pop()
+  const fn = castFunction(props.title ?? route.value?.meta?.title ?? capitalize(name))
+  return fn()
+})
+
+const icon = computed(() => {
+  return props.icon ?? route.value?.meta?.icon
+})
+
+const isActive = computed(() => {
+  return props.active || route.value.name === currentRoute.value?.name
+})
+
+const classList = computed(() => {
+  return {
+    'navigation-breadcrumb-link--active': isActive.value
+  }
+})
+</script>
+
+<template>
+  <router-link v-if="route" :to="routeLocation" class="navigation-breadcrumb-link" :class="classList">
+    <phosphor-icon v-if="icon" class="navigation-breadcrumb-link__icon me-2" :name="icon" />
+    <span class="navigation-breadcrumb-link__label">
+      <slot>{{ title }}</slot>
+    </span>
+    <phosphor-icon v-if="!noCaret" class="navigation-breadcrumb-link__caret mx-2" size="1em" name="caret-right" />
+  </router-link>
+</template>
+
+<style lang="scss" scoped>
+.navigation-breadcrumb-link {
+  display: inline-flex;
+  align-items: center;
+  color: var(--bs-secondary-color);
+  padding: $spacer-xxs 0;
+
+  &--active {
+    color: var(--bs-body-color);
+    border-bottom: 1px solid var(--bs-secondary);
+  }
+}
+</style>
---
+++ src/stories/components/NavigationBreadcrumb.stories.js
@@ -0,0 +1,130 @@
+import { vueRouter } from 'storybook-vue3-router'
+
+import IconButton from '@/components/IconButton'
+import NavigationBreadcrumb from '@/components/NavigationBreadcrumb'
+import NavigationBreadcrumbEntry from '@/components/NavigationBreadcrumbEntry'
+
+const routes = [
+  {
+    path: '/',
+    children: [
+      {
+        path: '/tasks',
+        name: 'tasks',
+        meta: {
+          icon: 'rocket-launch'
+        },
+        children: [
+          {
+            path: '/tasks/batch-searches',
+            name: 'tasks.batch-searches',
+            meta: {
+              icon: 'list-magnifying-glass'
+            },
+            children: [
+              {
+                path: '/tasks/batch-searches/view',
+                name: 'tasks.batch-searches.view',
+                meta: {
+                  icon: null
+                }
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+]
+
+export default {
+  decorators: [vueRouter(routes)],
+  title: 'Components/NavigationBreadcrumb',
+  tags: ['autodocs'],
+  render: () => ({
+    components: {
+      NavigationBreadcrumb
+    },
+    template: `
+      <navigation-breadcrumb current-route-name="tasks.batch-searches.view" />
+    `
+  })
+}
+
+export const Default = {}
+
+export const ActiveSlot = {
+  render: () => ({
+    components: {
+      NavigationBreadcrumb
+    },
+    template: `
+      <navigation-breadcrumb current-route-name="tasks.batch-searches.view">
+        <template #active>
+          Rich people in EU
+        </template>
+      </navigation-breadcrumb>
+    `
+  })
+}
+
+export const ButtonAddon = {
+  render: () => ({
+    components: {
+      IconButton,
+      NavigationBreadcrumb,
+      NavigationBreadcrumbEntry
+    },
+    template: `
+      <navigation-breadcrumb current-route-name="tasks.batch-searches.view">
+        <template #addon>
+          <navigation-breadcrumb-entry>
+            <icon-button variant="outline-secondary" icon-left="download-simple">
+              Download 8,506 documents
+            </icon-button>
+          </navigation-breadcrumb-entry>
+        </template>
+      </navigation-breadcrumb>
+    `
+  })
+}
+
+export const MutltipleAddons = {
+  render: () => ({
+    components: {
+      IconButton,
+      NavigationBreadcrumb,
+      NavigationBreadcrumbEntry
+    },
+    template: `
+      <navigation-breadcrumb current-route-name="tasks.batch-searches.view">
+        <template #addon>
+          <navigation-breadcrumb-entry>
+            <icon-button variant="outline-secondary" icon-left="download-simple">
+              Download 8,506 documents
+            </icon-button>
+          </navigation-breadcrumb-entry>
+          <navigation-breadcrumb-entry class="ms-auto">
+            <icon-button
+              tooltip-placement="left"
+              variant="primary"
+              pill
+              square
+              hide-label
+              icon-left="plus"
+              label="Start a batch search" />
+          </navigation-breadcrumb-entry>
+          <navigation-breadcrumb-entry class="pe-0">
+            <icon-button
+              tooltip-placement="left"
+              variant="link"
+              square
+              hide-label
+              icon-left="sliders-horizontal"
+              label="Configure current view" />
+          </navigation-breadcrumb-entry>
+        </template>
+      </navigation-breadcrumb>
+    `
+  })
+}
