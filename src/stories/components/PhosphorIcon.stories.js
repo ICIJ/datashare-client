93125130df8a882e137e9fafb84572195116b648
Pierre Romera <hello@pirhoo.com>
2024-07-05 09:25:39 +0000

    feat: add spinning state

--- src/stories/components/PhosphorIcon.stories.js
+++ src/stories/components/PhosphorIcon.stories.js
@@ -7,7 +7,18 @@ export default {
     variant: {
       control: { type: 'select' },
       options: ['primary', 'secondary', 'tertiary', 'body-color']
+    },
+    weight: {
+      control: { type: 'select' },
+      options: ['thin', 'light', 'regular', 'bold', 'fill', 'duotone']
+    },
+    spin: {
+      control: { type: 'boolean' }
     }
+  },
+  args: {
+    weight: 'regular',
+    spin: false
   },
   render: (args) => ({
     components: {
@@ -19,49 +30,46 @@ export default {
       }
     },
     template: `
-    Regular
-    <br/>
-    <phosphor-icon v-bind="args"/>
-    <phosphor-icon v-bind="args" variant="primary"/>
-    <phosphor-icon v-bind="args" variant="secondary"/>
-    <phosphor-icon v-bind="args" variant="tertiary"/>
-
-    <br/>
-    Bold
-    <br/>
-    <phosphor-icon v-bind="args" weight="bold"/>
-    <phosphor-icon v-bind="args" variant="primary" weight="bold"/>
-    <phosphor-icon v-bind="args" variant="secondary" weight="bold"/>
-    <phosphor-icon v-bind="args" variant="tertiary" weight="bold"/>
-    <br/>
-    Fill
-    <br/>
-    <phosphor-icon v-bind="args" weight="fill"/>
-    <phosphor-icon v-bind="args" variant="primary" weight="fill"/>
-    <phosphor-icon v-bind="args" variant="secondary" weight="fill"/>
-    <phosphor-icon v-bind="args" variant="tertiary" weight="fill"/>
-    <br/>
-    Duotone
-    <br/>
-    <phosphor-icon v-bind="args" weight="duotone"/>
-    <phosphor-icon v-bind="args" variant="primary" weight="duotone"/>
-    <phosphor-icon v-bind="args" variant="secondary" weight="duotone"/>
-    <phosphor-icon v-bind="args" variant="tertiary" weight="duotone"/>
+      <phosphor-icon v-bind="args"/>
     `
   })
 }
 
-// // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Default = {
   args: {
     name: 'User',
-    size: '24'
+    size: '32'
   }
 }
 
-export const Larger = {
+export const WeightBold = {
   args: {
     name: 'User',
+    weight: 'bold',
     size: '32'
+  }
+}
+
+export const WeightFill = {
+  args: {
+    name: 'User',
+    weight: 'fill',
+    size: '32'
+  }
+}
+
+export const WeightDuotone = {
+  args: {
+    name: 'User',
+    weight: 'duotone',
+    size: '32'
+  }
+}
+
+export const Spinning = {
+  args: {
+    name: 'circle-notch',
+    size: '32',
+    spin: true
   }
 }
