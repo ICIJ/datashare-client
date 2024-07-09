dae140033285c81cbb674a6a8a3daa9ffdcc292a
Pierre Romera <hello@pirhoo.com>
2024-07-09 17:20:30 +0000

    chore: update Murmur

--- tests/unit/setup.js
+++ tests/unit/setup.js
@@ -1,13 +1,11 @@
 import 'whatwg-fetch'
 import { fas as fasIcons } from '@fortawesome/free-solid-svg-icons'
 import { far as farIcons } from '@fortawesome/free-regular-svg-icons'
-import { fab as fabIcons } from '@fortawesome/free-brands-svg-icons'
 import { library as fortawesome } from '@fortawesome/fontawesome-svg-core'
 
 // Register all icons
 fortawesome.add(...Object.values(fasIcons))
 fortawesome.add(...Object.values(farIcons))
-fortawesome.add(...Object.values(fabIcons))
 
 // This is a temporary workaround to avoid the following console.log
 // that are printed during the tests by vue-wait and vue3-shortkey.
