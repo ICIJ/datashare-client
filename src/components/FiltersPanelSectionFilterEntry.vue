f31f32041b77ff80f3eeef7f44aec3930916bca6
Pierre Romera <hello@pirhoo.com>
2024-07-10 15:01:32 +0000

    feat: scafolder filters panel components

---
+++ src/components/FiltersPanelSectionFilterEntry.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <div class="filters-panel-section-filter-entry"></div>
+</template>
---
+++ src/stories/components/FiltersPanelSearch.stories.js
@@ -0,0 +1,13 @@
+export default {
+  title: 'Components/FiltersPanel/Search',
+  tags: ['autodocs'],
+  argTypes: {},
+  args: {},
+  render: (args) => ({
+    components: { },
+    setup: () => ({ args }),
+    template: ``
+  })
+}
+
+export const Default = {}
