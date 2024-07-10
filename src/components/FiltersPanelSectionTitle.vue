265c5b545882996bcf57ef8b457ccf8ba80bd7e2
Pierre Romera <hello@pirhoo.com>
2024-07-10 15:01:32 +0000

    feat: scafolder filters panel components

---
+++ src/components/FiltersPanelSectionTitle.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+  title: {
+    type: String
+  }
+})
+</script>
+
+<template>
+  <h3 class="filters-panel-section-title text-primary-emphasis">
+    <slot>{{ title }}</slot>
+  </h3>
+</template>
+
+<style lang="scss" scoped>
+.filters-panel-section-title {
+  font-size: 1em;
+}
+</style>
