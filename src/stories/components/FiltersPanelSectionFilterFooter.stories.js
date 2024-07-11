6751e586da1abf74ae441de030d193d515741462
Pierre Romera <hello@pirhoo.com>
2024-07-11 10:19:47 +0000

    doc: add functional test

--- src/stories/components/FiltersPanelSectionFilterFooter.stories.js
+++ src/stories/components/FiltersPanelSectionFilterFooter.stories.js
@@ -1,12 +1,26 @@
+import FiltersPanelSectionFilterFooter from '@/components/FiltersPanelSectionFilterFooter'
+
 export default {
   title: 'Components/FiltersPanel/Section/Filter/Footer',
   tags: ['autodocs'],
+  component: FiltersPanelSectionFilterFooter,
   argTypes: {},
-  args: {},
+  args: {
+    hideContextualize: false,
+    hideExclude: false,
+    hideShowMore: false,
+    hideSort: false
+  },
   render: (args) => ({
-    components: { },
+    components: {
+      FiltersPanelSectionFilterFooter
+    },
     setup: () => ({ args }),
-    template: ``
+    template: `
+      <div class="p-5" style="background-color: var(--bs-light-bg-subtle);">
+        <filters-panel-section-filter-footer  v-bind="args" />
+      </div>
+    `
   })
 }
 
