2159b054c22cb2d30fb571e9e36551563fcca7cf
Pierre Romera <hello@pirhoo.com>
2024-07-11 11:00:42 +0000

    style: add spacing

--- src/components/FiltersPanelSection.vue
+++ src/components/FiltersPanelSection.vue
@@ -9,7 +9,7 @@ defineProps({
 </script>
 
 <template>
-  <div class="filters-panel-section">
+  <div class="filters-panel-section mb-5">
     <filters-panel-section-title :title="title">
       <slot name="title" />
     </filters-panel-section-title>
