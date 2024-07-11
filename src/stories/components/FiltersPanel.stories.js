c87dd9ba43e388f376656738165e0184415293be
Pierre Romera <hello@pirhoo.com>
2024-07-11 11:00:50 +0000

    doc: use real life example

--- src/stories/components/FiltersPanel.stories.js
+++ src/stories/components/FiltersPanel.stories.js
@@ -11,7 +11,7 @@ export default {
   tags: ['autodocs'],
   argTypes: {},
   args: {
-    collapseFilters: false
+    collapseTags: false
   },
   render: (args) => ({
     components: {
@@ -23,31 +23,60 @@ export default {
       FiltersPanelSectionFilterEntry,
       FiltersPanelSectionFilterFooter
     },
+    data() {
+      return {
+        values: {}
+      }
+    },
+    computed: {
+      count() {
+        return Object.values(this.values).filter((v) => v).length
+      }
+    },
     setup: () => ({ args }),
     template: `
       <div style="max-width: 320px; background-color: var(--bs-light-bg-subtle);" class="p-3">
         <filters-panel-toggler class="mb-5" />
         <filters-panel-search class="mb-5" />
         <filters-panel-section title="Documents info">
-          <filters-panel-section-filter title="Project" :count="1" icon="circles-three-plus" :collapse="args.collapseFilters">
-            <filters-panel-section-filter-entry label="Banana Papers" value="banana-papers" model-value :count="17000000" />
-            <filters-panel-section-filter-entry label="Citrus Confidential" value="citrus-confidential" :count="10000000" />
-            <filters-panel-section-filter-entry label="FigCEN Files" value="figcen-files" :count="300000" />
+          <filters-panel-section-filter title="Project" icon="circles-three-plus" collapse />
+          <filters-panel-section-filter title="Path" icon="tree-structure" collapse />
+          <filters-panel-section-filter title="File type" icon="file-text" collapse />
+          <filters-panel-section-filter title="Creation date" icon="calendar-blank" collapse />
+          <filters-panel-section-filter title="Language" icon="globe-hemisphere-west" :count="4" collapse />
+          <filters-panel-section-filter title="Indexing date" icon="calendar-plus" collapse />
+          <filters-panel-section-filter title="Extraction lebel" icon="paperclip" collapse />
+          <filters-panel-section-filter title="Has attachments" icon="paperclip" collapse />
+        </filters-panel-section>
+        <filters-panel-section title="User(s) data">
+          <filters-panel-section-filter title="Starred" icon="star" collapse />
+          <filters-panel-section-filter title="With note(s)" icon="note-blank" :count="1" collapse />
+          <filters-panel-section-filter title="In your folder(s)" icon="folder-open" collapse />
+          <filters-panel-section-filter title="Commmented by" icon="chats-teardrop" collapse />
+          <filters-panel-section-filter title="Tags" icon="tag" :count="count" :collapse="args.collapseTags" @toggle="args.collapseTags = $event">
+            <filters-panel-section-filter-entry label="colombia" v-model="values['colombia']" :count="89233" />
+            <filters-panel-section-filter-entry label="mongolia" v-model="values['mongolia']" :count="9276" />
+            <filters-panel-section-filter-entry label="japan" v-model="values['japan']" :count="8878" />
+            <filters-panel-section-filter-entry label="russia" v-model="values['russia']" :count="1233" />
+            <filters-panel-section-filter-entry label="france" v-model="values['france']" :count="437" />
+            <filters-panel-section-filter-entry label="bolivia" v-model="values['bolivia']" :count="389" />
+            <filters-panel-section-filter-entry label="estonia" v-model="values['estonia']" :count="388" />
+            <filters-panel-section-filter-entry label="china" v-model="values['china']" :count="211" />
+            <filters-panel-section-filter-entry label="usa" v-model="values['usa']" :count="210" />
+            <filters-panel-section-filter-entry label="germany" v-model="values['germany']" :count="148" />
+            <filters-panel-section-filter-entry label="sudan" v-model="values['sudan']" :count="135" />
+            <filters-panel-section-filter-entry label="australia" v-model="values['australia']" :count="36" />
+            <filters-panel-section-filter-entry label="south-africa" v-model="values['south-africa']" :count="15" />
           </filters-panel-section-filter>
-          <filters-panel-section-filter title="File type" icon="file-text" :collapse="args.collapseFilters">
-            <filters-panel-section-filter-entry label="Portable Document Format (PDF)" value="application/pdf" />
-            <filters-panel-section-filter-entry label="JPEG" value="image/jpg" />
-            <filters-panel-section-filter-entry label="Excel 95-2003 Wordbook" value="application/vnd.ms-excel" />
-          </filters-panel-section-filter>
-          <filters-panel-section-filter title="Tags" icon="tag" :collapse="args.collapseFilters">
-            <filters-panel-section-filter-entry label="russia" />
-            <filters-panel-section-filter-entry label="france" />
-            <filters-panel-section-filter-entry label="china" />
-            <filters-panel-section-filter-entry label="usa" />
-            <filters-panel-section-filter-entry label="germany" />
-            <filters-panel-section-filter-entry label="sudan" />
-            <filters-panel-section-filter-entry label="australia" />
-          </filters-panel-section-filter>
+          <filters-panel-section-filter title="Recommended by" icon="newspaper-clipping" collapse />
+        </filters-panel-section>
+        <filters-panel-section title="Named entities">
+          <filters-panel-section-filter title="People" icon="users" collapse />
+          <filters-panel-section-filter title="Organizations" icon="buildings" collapse />
+          <filters-panel-section-filter title="Locations" icon="map-pin" collapse />
+          <filters-panel-section-filter title="Email addresses" icon="at" collapse />
+          <filters-panel-section-filter title="Phone numbers" icon="phone" collapse />
+          <filters-panel-section-filter title="IBAN" icon="currency-circle-dollar" collapse />
         </filters-panel-section>
       </div>
     `
