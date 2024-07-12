5a7b532d30e9a059331c9573ec63b32aa3d00782
Caroline Desprat <cdesprat@icij.org>
2024-07-12 15:14:38 +0000

    feat: add collapse and style on page settings

--- src/components/PageSettingsSection.vue
+++ src/components/PageSettingsSection.vue
@@ -1,20 +1,23 @@
 <template>
+  <page-settings-section-group :label="label" v-model="localOpen" class="page-settings-section">
     <component :is="formGroup"
                id="checkbox-group-1"
+               class="page-settings-section__input-group"
                v-model="localValue"
                :name="name"
                stacked
     >
-      <component :is="formInput" v-for="(option,index) in options" :key="index" :value="option.value">
+      <component :is="formInput" v-for="(option,index) in options" :key="index" :value="option.value" class="page-settings-section__input-group__input">
         <page-settings-entry :text="option.text" :icon="option.icon" />
       </component>
     </component>
+  </page-settings-section-group>
 </template>
 
 <script setup>
-import {computed, ref} from "vue";
-import {PhosphorIcon} from "@icij/murmur-next";
+import {computed} from "vue"
 import PageSettingsEntry from "@/components/PageSettingsEntry.vue";
+import PageSettingsSectionGroup from "@/components/PageSettingsSectionGroup.vue";
 import {BFormCheckbox, BFormCheckboxGroup, BFormRadio, BFormRadioGroup} from "bootstrap-vue-next";
 defineOptions({
   name:"PageSettingsSection"
@@ -23,6 +26,10 @@ defineOptions({
 const RADIO = "radio"
 const CHECKBOX = "checkbox"
 const props = defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
   type: {
     type: String,
     required: true,
@@ -42,7 +49,10 @@ const modelValue = defineModel({
   type: [String, Array],
   default: () => [],
 })
-
+const open = defineModel('open',{
+  type:Boolean,
+  default: true,
+})
 const formGroup = computed(() => {
   return props.type === RADIO ? BFormRadioGroup : BFormCheckboxGroup
 })
@@ -55,5 +65,24 @@ const localValue = computed({
     modelValue.value = newVal
   }
 })
+const localOpen = computed({
+  get: () => open.value,
+  set: (newVal) => {
+    console.log("test",newVal)
+    open.value = newVal
+  }
+})
 </script>
 
+<style lang="scss" scoped>
+.page-settings{
+  &-section{
+    &__input-group{
+
+    margin-left: 1em !important;
+    }
+    margin-bottom: 1em !important;
+  }
+}
+
+</style>
