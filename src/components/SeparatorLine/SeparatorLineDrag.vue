1fdccd84e8e0e25a7e2d6d614769a67f1e13fec3
Pierre Romera <hello@pirhoo.com>
2024-07-12 18:11:54 +0000

    feat: add separator line

---
+++ src/components/SeparatorLine/SeparatorLineDrag.vue
@@ -0,0 +1,16 @@
+<script setup>
+import IconButton from '@/components/IconButton'
+</script>
+
+<template>
+  <icon-button
+    square
+    pill
+    hide-label
+    icon-left="hand-grabbing"
+    label="Drag"
+    size="sm"
+    variant="outline-dark"
+    class="separator-line-drag"
+  />
+</template>
---
+++ src/stories/components/SeparatorLine/SeparatorLine.stories.js
@@ -0,0 +1,60 @@
+import SepratorLine from '@/components/SeparatorLine/SeparatorLine'
+
+export default {
+  components: { SepratorLine },
+  title: 'Components/SepratorLine',
+  component: SepratorLine,
+  tags: ['autodocs'],
+  args: {
+    active: true,
+    min: 180,
+    reduceDisabled: false,
+    expandDisabled: false
+  },
+  render: (args) => ({
+    components: {
+      SepratorLine
+    },
+    setup() {
+      return {
+        args
+      }
+    },
+    data() {
+      return {
+        left: 250
+      }
+    },
+    computed: {
+      separatorLineStyle() {
+        return {
+          position: 'absolute',
+          top: 0,
+          left: `${this.left}px`
+        }
+      },
+      placeholderStyle() {
+        return {
+          width: `calc(${this.left}px + 1em)`
+        }
+      }
+    },
+    template: `
+      <div style="position: relative; height: 400px; max-height: 80vh; background-color: var(--bs-light-bg-subtle); overflow: hidden; display: flex;">
+        <seprator-line
+          :style="separatorLineStyle"
+          @drag="left = $event"
+          @reduce="left = $event"
+          @expand="left = $event"
+          v-bind="args" />
+
+        <div class="bg-primary-subtle d-flex align-items-center justify-content-center" :style="placeholderStyle">
+          <code v-if="args.min < left">min-width: {{ args.min }}px</code>
+        </div>
+        <div class="bg-secondary-subtle flex-grow-1"></div>
+      </div>
+    `
+  })
+}
+
+export const Default = {}
