70eca82142bb82fce8a1c272c43c87171cbc93cb
Pierre Romera <hello@pirhoo.com>
2024-07-12 11:33:53 +0000

    feat: add search breadcrumb components

---
+++ src/components/SearchBreadcrumb/SearchBreadcrumbToggler.vue
@@ -0,0 +1,18 @@
+<script setup>
+import IconButton from '@/components/IconButton'
+
+const emit = defineEmits(['close'])
+</script>
+
+<template>
+  <icon-button
+    class="search-breadcrumb-toggler"
+    variant="primary"
+    :label="$t('searchBreadcrumbToggler.close')"
+    hide-label
+    icon-left="x"
+    @click="emit('close')"
+  >
+    <slot>{{ $t('searchBreadcrumbToggler.close') }}</slot>
+  </icon-button>
+</template>
---
+++ src/stories/components/SearchBreadcrumb/SearchBreadcrumbEntry.stories.js
@@ -0,0 +1,65 @@
+import SearchBreadcrumbEntry from '@/components/SearchBreadcrumb/SearchBreadcrumbEntry'
+
+export default {
+  decorators: [],
+  title: 'Components/SearchBreadcrumb/SearchBreadcrumbEntry',
+  tags: ['autodocs'],
+  args: {
+    query: 'London AND Biden AND -JO',
+    occurrences: 987
+  },
+  render: (args) => ({
+    components: {
+      SearchBreadcrumbEntry
+    },
+    setup() {
+      return { args }
+    },
+    template: `
+      <search-breadcrumb-entry v-bind="args" />
+    `
+  })
+}
+
+export const Default = {}
+
+export const QueryWithOperator = {
+  args: {
+    query: 'Paulette OR Pierre',
+    occurrences: 452645
+  }
+}
+
+export const QueryWithFilter = {
+  args: {
+    query: 'contentType:"application/pdf"',
+    occurrences: 5687
+  }
+}
+
+export const QueryWithError = {
+  args: {
+    query: 'contentType:application/pdf',
+    occurrences: 0
+  }
+}
+
+export const Filter = {
+  args: {
+    query: '',
+    filter: 'contentType',
+    value: 'application/pdf',
+    occurrences: 5687
+  }
+}
+
+export const FilterWithColor = {
+  args: {
+    query: '',
+    filter: 'namedEntityPerson',
+    value: 'Shakira',
+    icon: 'music-notes',
+    color: '#a50000',
+    occurrences: 5687
+  }
+}
