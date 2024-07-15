dac005185c53c26240ff943f65196acebb6719d9
Pierre Romera <hello@pirhoo.com>
2024-07-15 16:07:49 +0000

    feat: make nav breadcrumb footer more responsive

--- src/components/SearchBreadcrumb/SearchBreadcrumbFooter.vue
+++ src/components/SearchBreadcrumb/SearchBreadcrumbFooter.vue
@@ -21,7 +21,7 @@ defineProps({
 </script>
 
 <template>
-  <div class="search-breadcrumb-footer text-end d-flex justify-content-end">
+  <div class="search-breadcrumb-footer text-end d-flex justify-content-end flex-wrap">
     <icon-button v-if="!hideClearFilters" variant="link" icon-left="eraser" class="ms-3">
       {{ $t('searchBreadcrumbFooter.clearFilters') }}
     </icon-button>
@@ -31,11 +31,13 @@ defineProps({
     <icon-button v-if="!hideClearFiltersAndQueries" variant="link" icon-left="arrow-counter-clockwise" class="ms-3">
       {{ $t('searchBreadcrumbFooter.clearFiltersAndQueries') }}
     </icon-button>
-    <icon-button v-if="!hideSaveSearch" variant="outline-dark" icon-left="floppy-disk" class="ms-3">
-      {{ $t('searchBreadcrumbFooter.saveSearch') }}
-    </icon-button>
-    <icon-button v-if="!hideCreateAlert" variant="outline-dark" icon-left="siren" class="ms-3">
-      {{ $t('searchBreadcrumbFooter.createAlert') }}
-    </icon-button>
+    <div class="d-block d-md-inline-block">
+      <icon-button v-if="!hideSaveSearch" variant="outline-dark" icon-left="floppy-disk" class="ms-3">
+        {{ $t('searchBreadcrumbFooter.saveSearch') }}
+      </icon-button>
+      <icon-button v-if="!hideCreateAlert" variant="outline-dark" icon-left="siren" class="ms-3">
+        {{ $t('searchBreadcrumbFooter.createAlert') }}
+      </icon-button>
+    </div>
   </div>
 </template>
