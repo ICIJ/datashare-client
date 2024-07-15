24981e133c14945ddbe04d3709c4d57a1fa4ef97
Pierre Romera <hello@pirhoo.com>
2024-07-15 16:25:08 +0000

    feat: add occurences tooltip

--- src/components/SearchBreadcrumb/SearchBreadcrumbEntryOccurrences.vue
+++ src/components/SearchBreadcrumb/SearchBreadcrumbEntryOccurrences.vue
@@ -1,14 +1,35 @@
 <script setup>
-defineProps({
+import { computed } from 'vue'
+
+const props = defineProps({
   occurrences: {
+    type: Number,
+    default: 0
+  },
+  previousOccurences: {
     type: Number,
     default: 0
   }
 })
+
+const lessOccurrences = computed(() => {
+  return Math.max(0, props.previousOccurences - props.occurrences)
+})
 </script>
 
 <template>
-  <div class="search-breadcrumb-entry-occurrences d-inline-flex p-2 mb-2">
+  <div
+    v-b-tooltip.top="{ offset: '0' }"
+    class="search-breadcrumb-entry-occurrences d-inline-flex p-2 mb-2"
+    :title="$tc('searchBreadcrumbEntryOccurences.title', lessOccurrences, { lessOccurrences: $n(lessOccurrences) })"
+  >
     {{ $tc('searchBreadcrumbEntryOccurences.label', occurrences, { occurrences: $n(occurrences) }) }}
   </div>
 </template>
+
+<style lang="scss" scoped>
+.search-breadcrumb-entry-occurrences:hover {
+  text-decoration: underline;
+  color: var(--bs-body-color);
+}
+</style>
