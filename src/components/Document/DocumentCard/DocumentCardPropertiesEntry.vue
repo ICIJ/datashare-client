34204edcfac7790c017245df1a98377d709b9fad
Pierre Romera <hello@pirhoo.com>
2024-07-16 16:47:15 +0000

    feat: add DocumentCard

---
+++ src/components/Document/DocumentCard/DocumentCardPropertiesEntry.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { PhosphorIcon } from '@icij/murmur-next'
+
+defineProps({
+  document: {
+    type: Object
+  },
+  property: {
+    type: String
+  },
+  title: {
+    type: String
+  },
+  icon: {
+    type: String
+  },
+  hideIcon: {
+    type: Boolean
+  }
+})
+</script>
+
+<template>
+  <div class="document-card-properties-entry d-flex px-1 mb-2 text-tertiary-emphasis">
+    <div v-if="!hideIcon" class="document-card-properties-entry__icon pe-2">
+      <phosphor-icon :name="icon ?? property" />
+    </div>
+    <span class="visually-hidden">{{ title ?? property }}:</span>
+    <div class="document-card-properties-entry__value">
+      <slot>{{ document[property] }}</slot>
+    </div>
+  </div>
+</template>
---
+++ src/components/Document/DocumentCard/DocumentCardPropertiesEntryContentType.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { computed } from 'vue'
+
+import DocumentCardPropertiesEntry from '@/components/Document/DocumentCard/DocumentCardPropertiesEntry'
+import { getDocumentTypeLabel } from '@/utils/utils'
+
+const props = defineProps({
+  document: {
+    type: Object
+  },
+  property: {
+    type: String
+  }
+})
+
+const label = computed(() => {
+  return getDocumentTypeLabel(props.document.contentType)
+})
+</script>
+
+<template>
+  <document-card-properties-entry :document="document" :property="property" icon="file-text">
+    {{ label }}
+  </document-card-properties-entry>
+</template>
