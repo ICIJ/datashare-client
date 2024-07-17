438fb276f6ca3f8d42e4bbaf8f5b1654574740e8
Pierre Romera <hello@pirhoo.com>
2024-07-17 13:01:45 +0000

    feat: add DatetimeDisplay

---
+++ src/components/DatetimeDisplay/DatetimeDisplay.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+import { FORMAT_SHORT, FORMAT_LONG, FORMAT_FROM_NOW, humanLongDate, humanShortDate, fromNow } from '@/utils/humanDate'
+
+const props = defineProps({
+  value: {
+    type: [String, Number, Date],
+    required: true
+  },
+  format: {
+    type: String,
+    validator: (value) => [FORMAT_SHORT, FORMAT_LONG, FORMAT_FROM_NOW].includes(value)
+  }
+})
+
+const title = computed(() => {
+  if (props.format !== FORMAT_LONG) {
+    return humanLongDate(props.value, useI18n().locale.value)
+  }
+  return null
+})
+
+const display = computed(() => {
+  if (!props.value) {
+    return ''
+  }
+
+  switch (props.format) {
+    case FORMAT_LONG:
+      return humanLongDate(props.value, useI18n().locale.value)
+    case FORMAT_FROM_NOW:
+      return fromNow(props.value, useI18n().locale.value)
+    default:
+      return humanShortDate(props.value, useI18n().locale.value)
+  }
+})
+</script>
+
+<template>
+  <span v-b-tooltip.body class="datetime-display" :title="title">
+    <slot v-bind="{ display }">
+      {{ display }}
+    </slot>
+  </span>
+</template>
