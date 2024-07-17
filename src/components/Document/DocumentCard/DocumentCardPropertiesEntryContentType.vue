b562ef9f907b6c2b20b11aba348e4a62e2732d13
Pierre Romera <hello@pirhoo.com>
2024-07-17 14:32:25 +0000

    refactor: use displays for content length, content type, language, tags and extraction level

--- src/components/Document/DocumentCard/DocumentCardPropertiesEntryContentType.vue
+++ src/components/Document/DocumentCard/DocumentCardPropertiesEntryContentType.vue
@@ -1,10 +1,8 @@
 <script setup>
-import { computed } from 'vue'
-
 import DocumentCardPropertiesEntry from '@/components/Document/DocumentCard/DocumentCardPropertiesEntry'
-import { getDocumentTypeLabel } from '@/utils/utils'
+import DisplayContentType from '@/components/Display/DisplayContentType'
 
-const props = defineProps({
+defineProps({
   document: {
     type: Object
   },
@@ -12,14 +10,10 @@ const props = defineProps({
     type: String
   }
 })
-
-const label = computed(() => {
-  return getDocumentTypeLabel(props.document.contentType)
-})
 </script>
 
 <template>
   <document-card-properties-entry :document="document" :property="property" icon="file-text">
-    {{ label }}
+    <display-content-type :value="document.contentType" />
   </document-card-properties-entry>
 </template>
