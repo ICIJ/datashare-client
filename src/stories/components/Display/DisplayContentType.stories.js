e9c24ee04b4b6c130baed12476231207cecb8460
Pierre Romera <hello@pirhoo.com>
2024-07-17 14:32:15 +0000

    feat: add displays for content length, content type, language, tags and extraction level

---
+++ src/stories/components/Display/DisplayContentType.stories.js
@@ -0,0 +1,17 @@
+import DisplayContentType from '@/components/Display/DisplayContentType'
+
+export default {
+  title: 'Components/Display/DisplayContentType',
+  tags: ['autodocs'],
+  component: DisplayContentType,
+  argTypes: {
+    value: {
+      control: 'string'
+    }
+  },
+  args: {
+    value: 'application/pdf'
+  }
+}
+
+export const Default = {}
