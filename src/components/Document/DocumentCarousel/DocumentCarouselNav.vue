832874d02546db9f206b0493f3ec4d7f0fe132c6
Pierre Romera <hello@pirhoo.com>
2024-07-18 13:33:43 +0000

    feat: add document carousel

---
+++ src/components/Document/DocumentCarousel/DocumentCarouselNav.vue
@@ -0,0 +1,25 @@
+<script setup>
+import IconButton from '@/components/IconButton'
+
+defineProps({
+  icon: {
+    type: String,
+    required: true
+  },
+  label: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <icon-button
+    class="document-carousel-bar border-0"
+    square
+    variant="outline-light"
+    :icon-left="icon"
+    hide-label
+    :label="label"
+  />
+</template>
