63c542aae50f91b5ace04eae86a970878085241e
Pierre Romera <hello@pirhoo.com>
2024-07-18 17:30:58 +0000

    doc: add story for "all" item

---
+++ src/stories/components/Project/ProjectDropdownSelector/ProjectDropdownSelectorAll.stories.js
@@ -0,0 +1,9 @@
+import ProjectDropdownSelectorAll from '@/components/Project/ProjectDropdownSelector/ProjectDropdownSelectorAll'
+
+export default {
+  title: 'Components/Project/ProjectDropdownSelector/ProjectDropdownSelectorAll',
+  tags: ['autodocs'],
+  component: ProjectDropdownSelectorAll
+}
+
+export const Default = {}
