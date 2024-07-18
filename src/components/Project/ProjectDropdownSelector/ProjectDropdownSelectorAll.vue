d8a7ef47ab3036d2a8cb21d75126d8471854f27f
Pierre Romera <hello@pirhoo.com>
2024-07-18 17:06:37 +0000

    feat: add "all projects" checkbox

---
+++ src/components/Project/ProjectDropdownSelector/ProjectDropdownSelectorAll.vue
@@ -0,0 +1,21 @@
+<script setup>
+import ProjectDropdownSelectorCheckbox from './ProjectDropdownSelectorCheckbox'
+
+defineProps({
+  modelValue: {
+    type: Boolean
+  }
+})
+</script>
+
+<template>
+  <li class="project-dropdown-selector-all">
+    <project-dropdown-selector-checkbox
+      :model-value="modelValue"
+      :disabled="modelValue"
+      @update:model-value="$emit('update:modelValue', $event)"
+    >
+      All projects
+    </project-dropdown-selector-checkbox>
+  </li>
+</template>
---
+++ src/stories/components/Project/ProjectDropdownSelector/ProjectDropdownSelectorEntry.stories.js
@@ -0,0 +1,15 @@
+import ProjectDropdownSelectorEntry from '@/components/Project/ProjectDropdownSelector/ProjectDropdownSelectorEntry'
+
+export default {
+  title: 'Components/Project/ProjectDropdownSelector/ProjectDropdownSelectorEntry',
+  tags: ['autodocs'],
+  component: ProjectDropdownSelectorEntry,
+  args: {
+    project: {
+      name: 'banana-papers',
+      label: 'Banana Papers'
+    }
+  }
+}
+
+export const Default = {}
