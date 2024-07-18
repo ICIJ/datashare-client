9779936223691db3773e476935382f87ff7909be
Pierre Romera <hello@pirhoo.com>
2024-07-18 16:11:45 +0000

    refactor: split component in several components

---
+++ src/components/Project/ProjectDropdownSelectorButtonContent.vue
@@ -0,0 +1,73 @@
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  projects: {
+    type: Array
+  }
+})
+
+const hasOneProject = computed(() => {
+  return props.projects.length === 1
+})
+
+const isLastProjectSlice = (p) => {
+  return p === props.projects.length - 1
+}
+</script>
+
+<template>
+  <div class="project-dropdown-selector-button-content">
+    <span v-for="(project, p) in projects" :key="p" class="project-dropdown-selector-button-content__project">
+      <project-thumbnail
+        :project="project"
+        no-caption
+        width="1.2em"
+        class="project-dropdown-selector-button-content__project__thumbnail rounded"
+      />
+      <template v-if="hasOneProject">
+        <span class="ms-2">
+          {{ project?.label || project?.name }}
+        </span>
+      </template>
+      <template v-else-if="isLastProjectSlice(p)">
+        <span class="ms-2">
+          {{ $t('searchBarInputDropdownForProjects.projectsCount', projects.length) }}
+        </span>
+      </template>
+    </span>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.project-dropdown-selector-button-content {
+  display: flex;
+
+  &__project {
+    display: inline-flex;
+    align-items: center;
+
+    &:not(:first-of-type) &__thumbnail {
+      box-shadow: -1px 0 0 0 #fff;
+      margin-left: -0.5em;
+    }
+
+    .project-dropdown-selector--sliced &:last-of-type &__thumbnail {
+      background: $text-muted !important;
+
+      &:after {
+        content: '+';
+        color: #fff;
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/Project/ProjectDropdownSelectorSearch.vue
@@ -0,0 +1,35 @@
+<script setup>
+defineProps({
+  query: {
+    type: String,
+    default: ''
+  },
+  hasMatches: {
+    type: Boolean,
+    default: true
+  }
+})
+
+const emit = defineEmits(['update:model-value', 'blur', 'up', 'down', 'enter'])
+</script>
+
+<template>
+  <li class="project-dropdown-selector-search">
+    <div class="b-dropdown-form pt-1 mb-3">
+      <search-form-control
+        :model-value="query"
+        autofocus
+        shado
+        placeholder="Search projects"
+        @update:model-value="emit('update:model-value', $event)"
+        @blur="emit('blur', $event)"
+        @up="emit('up', $event)"
+        @down="emit('down', $event)"
+        @enter="emit('enter', $event)"
+      />
+    </div>
+    <div v-if="!hasMatches" class="text-center small text-muted pb-2">
+      {{ $t('searchBarInputDropdownForProjects.noMatches') }}
+    </div>
+  </li>
+</template>
