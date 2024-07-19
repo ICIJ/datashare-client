cf18db28657e44bf81f99da9c25c01687eef52fa
Pierre Romera <hello@pirhoo.com>
2024-07-19 15:47:31 +0000

    feat: configure app modal

---
+++ src/stories/components/AppModal/AppModal.stories.js
@@ -0,0 +1,49 @@
+import AppModal from '@/components/AppModal/AppModal'
+import { ArgTypes } from '@storybook/blocks'
+
+export default {
+  title: 'Components/AppModal/AppModal',
+  tags: ['autodocs'],
+  component: AppModal,
+  argTypes: {
+    size: {
+      control: 'select',
+      options: ['sm', 'md', 'lg', 'xl']
+    }
+  },
+  args: {
+    title: 'Are you sure?',
+    default: 'Let’s explain here the consequences of the action if needed so users don’t make any mistake.',
+    image: 'https://i.imgur.com/1eWgPCp.png',
+    imageWidth: '60px',
+    modelValue: true,
+    fullscreen: false,
+    hideBackdrop: false,
+    hideFooter: false,
+    hideHeader: false,
+    size: 'md'
+  },
+  parameters: {
+    slots: {
+      default: `Default slot content`
+    }
+  },
+  render(args) {
+    return {
+      components: { AppModal },
+      template: `
+        <button class="btn btn-primary" @click="args.modelValue = !args.modelValue">
+          Toggle model
+        </button>
+        <app-modal v-bind="args" @update:modelValue="args.modelValue = $event">
+          {{ args.default }}
+        </app-modal>
+      `,
+      data() {
+        return { args }
+      }
+    }
+  }
+}
+
+export const Default = {}
