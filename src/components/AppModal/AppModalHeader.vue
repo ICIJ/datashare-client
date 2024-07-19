3bf45a2acf9189da3ee2794646186ad817a10bb1
Pierre Romera <hello@pirhoo.com>
2024-07-19 15:47:31 +0000

    feat: configure app modal

---
+++ src/components/AppModal/AppModalHeader.vue
@@ -0,0 +1,69 @@
+<script setup>
+import IconButton from '@/components/IconButton'
+
+defineProps({
+  title: {
+    type: String
+  },
+  image: {
+    type: String
+  },
+  imageAlt: {
+    type: String
+  },
+  imageWidth: {
+    type: String,
+    default: '60px'
+  }
+})
+</script>
+
+<template>
+  <div class="app-modal-header w-100">
+    <slot name="header-close">
+      <icon-button
+        icon-left="x"
+        hide-label
+        tooltip-placement="right"
+        label="Close"
+        class="app-modal-header__close ms-auto"
+        @click="$emit('close')"
+      />
+    </slot>
+    <slot name="image">
+      <img v-if="image" class="app-modal-header__image" :width="imageWidth" :src="image" :alt="imageAlt" />
+    </slot>
+    <slot>
+      <h3 class="app-modal-header__title text-center mt-2 flex-grow-1">{{ title }}</h3>
+    </slot>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.app-modal-header {
+  position: relative;
+  min-height: 2.5rem;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding: 0 $spacer-xxl;
+
+  &__title {
+    font-weight: 500;
+    font-size: 1em;
+    color: var(--bs-emphasis-color);
+  }
+
+  &__close {
+    position: absolute;
+    right: 0;
+    top: 0;
+  }
+
+  &__image {
+    max-width: 100%;
+  }
+}
+</style>
