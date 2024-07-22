85f22de2ace2061db5b169108b79dda2ba36e5c9
Pierre Romera <hello@pirhoo.com>
2024-07-22 10:42:34 +0000

    feat: add FooterActions

---
+++ src/components/FormActions/FormActions.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { provide } from 'vue'
+
+const props = defineProps({
+  size: {
+    type: String,
+    default: 'md'
+  },
+  variant: {
+    type: String,
+    default: 'outline-lighter'
+  },
+  tag: {
+    type: String,
+    default: 'div'
+  },
+  ariaLabel: {
+    type: String
+  }
+})
+
+provide('size', props.size)
+provide('variant', props.variant)
+</script>
+
+<template>
+  <component :is="tag" class="form-actions" :aria-label="ariaLabel">
+    <slot />
+  </component>
+</template>
+
+<style lang="scss" scoped>
+.form-actions {
+  display: flex;
+  justify-content: flex-end;
+  flex-wrap: wrap;
+  column-gap: $spacer-lg;
+  row-gap: $spacer-sm;
+
+  @include media-breakpoint-down(md) {
+    justify-content: flex-start;
+  }
+}
+</style>
---
+++ src/stories/components/FormActions/FormActions.stories.js
@@ -0,0 +1,62 @@
+import FormActions from '@/components/FormActions/FormActions'
+import IconButton from '@/components//IconButton'
+
+export default {
+  title: 'Components/FormActions/FormActions',
+  tags: ['autodocs'],
+  component: FormActions,
+  argTypes: {
+    size: {
+      control: 'select',
+      options: ['sm', 'md', 'lg']
+    },
+    variant: {
+      control: 'select',
+      options: [
+        'primary',
+        'secondary',
+        'tertiary',
+        'danger',
+        'info',
+        'success',
+        'warning',
+        'light',
+        'dark',
+        'lighter',
+        'darker',
+        'outline-primary',
+        'outline-secondary',
+        'outline-tertiary',
+        'outline-danger',
+        'outline-info',
+        'outline-success',
+        'outline-warning',
+        'outline-light',
+        'outline-dark',
+        'outline-lighter',
+        'outline-darker'
+      ]
+    }
+  },
+  args: {
+    size: 'md',
+    variant: 'outline-lighter'
+  },
+  render(args) {
+    return {
+      components: { FormActions, IconButton },
+      template: `
+        <form-actions v-bind="args">
+          <icon-button icon-left="x">Cancel</icon-button>
+          <icon-button icon-left="arrow-counter-clockwise">Reset</icon-button>
+          <icon-button variant="primary" icon-right="caret-right">Proceed</icon-button>
+        </form-actions>
+      `,
+      data() {
+        return { args }
+      }
+    }
+  }
+}
+
+export const Default = {}
