c59c2b1b7adfd1a21395467362e610a28c2c6ee5
Pierre Romera <hello@pirhoo.com>
2024-07-23 08:12:00 +0000

    feat: add document local search

---
+++ src/components/Document/DocumentLocalSearch/DocumentLocalSearch.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { computed } from 'vue'
+
+import DocumentLocalSearchInput from './DocumentLocalSearchInput'
+import DocumentLocalSearchNav from './DocumentLocalSearchNav'
+import DocumentLocalSearchOccurrences from './DocumentLocalSearchOccurrences'
+
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: ''
+  },
+  activeIndex: {
+    type: Number,
+    default: 0
+  },
+  occurrences: {
+    type: Number,
+    default: null
+  }
+})
+
+const disabledPrevious = computed(() => props.activeIndex === 0)
+const disabledNext = computed(() => props.activeIndex === props.occurrences || props.occurrences === null)
+</script>
+
+<template>
+  <div class="document-local-search d-inline-flex">
+    <document-local-search-input :model-value="modelValue" @update:modelValue="$emit('update:modelValue', $event)" />
+    <document-local-search-nav
+      :disabled-previous="disabledPrevious"
+      :disabled-next="disabledNext"
+      class="ms-2"
+      @previous="$emit('update:activeIndex', activeIndex - 1)"
+      @next="$emit('update:activeIndex', activeIndex + 1)"
+    />
+    <document-local-search-occurrences
+      :active-index="activeIndex"
+      :occurrences="occurrences"
+      class="ms-2 align-self-center"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.document-local-search {
+  display: flex;
+  display: inline-flex;
+  border-radius: var(--bs-border-radius);
+}
+</style>
---
+++ src/components/Document/DocumentLocalSearch/DocumentLocalSearchNav.vue
@@ -0,0 +1,29 @@
+<script setup>
+import DocumentLocalSearchNavItem from './DocumentLocalSearchNavItem'
+
+defineProps({
+  disabledPrevious: {
+    type: Boolean
+  },
+  disabledNext: {
+    type: Boolean
+  }
+})
+</script>
+
+<template>
+  <b-button-group size="sm">
+    <document-local-search-nav-item
+      :disabled="disabledPrevious"
+      icon="caret-up"
+      label="Find the previous occurrence"
+      @click="$emit('previous')"
+    />
+    <document-local-search-nav-item
+      :disabled="disabledNext"
+      icon="caret-down"
+      label="Find the next occurrence"
+      @click="$emit('next')"
+    />
+  </b-button-group>
+</template>
