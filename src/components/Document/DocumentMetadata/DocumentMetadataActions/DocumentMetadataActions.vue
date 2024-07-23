39f016d79c68d6fd85827dcb0c3d7104deddce79
Pierre Romera <hello@pirhoo.com>
2024-07-23 12:54:07 +0000

    feat: add document metadata actions

---
+++ src/components/Document/DocumentMetadata/DocumentMetadataActions/DocumentMetadataActions.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { computed } from 'vue'
+
+import DocumentMetadataActionsEntry from './DocumentMetadataActionsEntry'
+
+const props = defineProps({
+  pinned: {
+    type: Boolean
+  }
+})
+
+const pinIconWeight = computed(() => (props.pinned ? 'fill' : null))
+const pinIconHoverWeight = computed(() => (props.pinned ? 'fill' : 'bold'))
+</script>
+
+<template>
+  <div class="document-metdata-actions">
+    <slot>
+      <document-metadata-actions-entry icon="magnifying-glass" @click="$emit('search')" />
+      <document-metadata-actions-entry icon="clipboard" @click="$emit('copy')" />
+      <document-metadata-actions-entry
+        icon="push-pin"
+        :icon-weight="pinIconWeight"
+        :icon-hover-weight="pinIconHoverWeight"
+        @click="$emit('update:pinned', !pinned)"
+      />
+    </slot>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.document-metdata-actions {
+  display: inline-flex;
+  gap: 2px;
+}
+</style>
---
+++ src/components/Document/DocumentMetadata/DocumentMetadataActions/DocumentMetadataActionsEntry.vue
@@ -0,0 +1,35 @@
+<script setup>
+import IconButton from '@/components/IconButton'
+
+defineProps({
+  icon: {
+    type: String,
+    required: true
+  },
+  iconWeight: {
+    type: String,
+    default: 'normal'
+  },
+  iconHoverWeight: {
+    type: String,
+    default: 'bold'
+  },
+  label: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <icon-button
+    class="border-0"
+    hide-label
+    variant="outline-tertiary"
+    square
+    :label="label"
+    :icon-left="icon"
+    :icon-left-weight="iconWeight"
+    :icon-left-hover-weight="iconHoverWeight"
+  />
+</template>
