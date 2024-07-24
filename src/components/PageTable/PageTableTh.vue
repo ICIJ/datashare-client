7bb69dca829b41c2aaae52543f7cd037332e3e7d
Pierre Romera <hello@pirhoo.com>
2024-07-24 12:11:35 +0000

    feat: add compact column

--- src/components/PageTable/PageTableTh.vue
+++ src/components/PageTable/PageTableTh.vue
@@ -12,12 +12,25 @@ const props = defineProps({
   },
   emphasis: {
     type: Boolean
+  },
+  hideLabel: {
+    type: Boolean
+  },
+  compact: {
+    type: Boolean
   }
 })
 
 const classList = computed(() => {
   return {
-    'page-table-th--emphasis': props.emphasis
+    'page-table-th--emphasis': props.emphasis,
+    'page-table-th--compact': props.hideLabel
+  }
+})
+
+const labelClassList = computed(() => {
+  return {
+    'visually-hidden': props.hideLabel
   }
 })
 </script>
@@ -26,7 +39,13 @@ const classList = computed(() => {
   <b-th class="page-table-th text-nowrap" :class="classList">
     <slot>
       <phosphor-icon v-if="icon" :name="icon" class="me-1" />
-      {{ label }}
+      <span :class="labelClassList">{{ label }}</span>
     </slot>
   </b-th>
 </template>
+
+<style lang="scss" scoped>
+.page-table-th--compact {
+  width: 2rem;
+}
+</style>
