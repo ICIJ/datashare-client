a4ed25a7eed77dd0003c1d45f48ac4a9a677f811
Caroline Desprat <cdesprat@icij.org>
2024-07-24 15:40:01 +0000

    clean

--- src/stories/components/Document/DocumentActionsGroup/DocumentActionsGroup.stories.js
+++ src/stories/components/Document/DocumentActionsGroup/DocumentActionsGroup.stories.js
@@ -1,11 +1,6 @@
 import IconButton from '@/components/IconButton'
-import { vueRouter } from 'storybook-vue3-router'
-import DocumentActionsGroup from '@/components/Document/DocumentActionsGroup/DocumentActionsGroup'
-import {BFormCheckbox} from 'bootstrap-vue-next'
-const routes = [{  name: 'document-modal',path: '/document-modal' }]
 
 export default {
-  decorators: [vueRouter(routes)],
   components: { DocumentActionsGroup },
   title: 'Components/Document/DocumentActionsGroup',
   component: IconButton,
