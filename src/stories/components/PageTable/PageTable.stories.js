6d550f2a3aeb9d624436a14b293f7f1c455e79a5
Pierre Romera <hello@pirhoo.com>
2024-07-24 13:32:35 +0000

    doc: add more example

--- src/stories/components/PageTable/PageTable.stories.js
+++ src/stories/components/PageTable/PageTable.stories.js
@@ -48,9 +48,9 @@ export default {
           <td><a href="#" target="_self">Inter IKEA Investment S.à r.l._cover letter 2010-2011 tax returns.pdf</a></td>
           <td>56</td>
           <td>PDF</td>
-          <td><project-label project="Project 1" hide-thumbnail /></td>
+          <td><project-label class="text-nowrap" project="Project 1" hide-thumbnail /></td>
           <td>John Doe</td>
-          <td><display-datetime value="2021-09-01" /></td>
+          <td><display-datetime class="text-nowrap" value="2021-09-01" /></td>
           <page-table-td-actions>
             <icon-button variant="outline-tertiary" square hide-label size="sm" icon-left="star" class="border-0 me-1" />
             <icon-button variant="outline-tertiary" square hide-label size="sm" icon-left="download-simple" class="border-0 me-1" />
@@ -62,9 +62,9 @@ export default {
           <td><a href="#" target="_self">Business Plan 2016 for investors Grughtel Corporation SA</a></td>
           <td>57</td>
           <td>DOCX</td>
-          <td><project-label project="Project 2" hide-thumbnail /></td>
+          <td><project-label class="text-nowrap" project="Project 2" hide-thumbnail /></td>
           <td>Jane Doe</td>
-          <td><display-datetime value="2021-09-02" /></td>
+          <td><display-datetime class="text-nowrap" value="2021-09-02" /></td>
           <page-table-td-actions>
             <icon-button variant="outline-tertiary" square hide-label size="sm" icon-left="star" class="border-0 me-1" />
             <icon-button variant="outline-tertiary" square hide-label size="sm" icon-left="download-simple" class="border-0 me-1" />
@@ -76,9 +76,9 @@ export default {
           <td><a href="#" target="_self">Notif_C3.059249_Inter IKEA Finance SA_01062010.pdf</a></td>
           <td>4</td>
           <td>XLSX</td>
-          <td><project-label project="Project 3" hide-thumbnail /></td>
+          <td><project-label class="text-nowrap" project="Project 3" hide-thumbnail /></td>
           <td>Jan Doe</td>
-          <td><display-datetime value="2021-09-03" /></td>
+          <td><display-datetime class="text-nowrap" value="2021-09-03" /></td>
           <page-table-td-actions>
             <icon-button variant="outline-tertiary" square hide-label size="sm" icon-left="star" class="border-0 me-1" />
             <icon-button variant="outline-tertiary" square hide-label size="sm" icon-left="download-simple" class="border-0 me-1" />
@@ -90,9 +90,9 @@ export default {
           <td><a href="#" target="_self">Inter Ikea Holding S.A._C4.135848_2011_CIT.MBT.NWT Return_EN.pdf</a></td>
           <td>3</td>
           <td>PDF</td>
-          <td><project-label project="Project 4" hide-thumbnail /></td>
+          <td><project-label class="text-nowrap" project="Project 4" hide-thumbnail /></td>
           <td>Jin Doe</td>
-          <td><display-datetime value="2021-09-04" /></td>
+          <td><display-datetime class="text-nowrap" value="2021-09-04" /></td>
           <page-table-td-actions>
             <icon-button variant="outline-tertiary" square hide-label size="sm" icon-left="star" class="border-0 me-1" />
             <icon-button variant="outline-tertiary" square hide-label size="sm" icon-left="download-simple" class="border-0 me-1" />
@@ -139,9 +139,9 @@ export const WithoutSelectMode = {
           <td><a href="#" target="_self">Richest people in EU</a></td>
           <td>56</td>
           <td>371</td>
-          <td><project-label project="Project 1" hide-thumbnail /></td>
+          <td><project-label class="text-nowrap" project="Project 1" hide-thumbnail /></td>
           <td>John Doe</td>
-          <td><display-datetime value="2021-09-01" /></td>
+          <td><display-datetime class="text-nowrap" value="2021-09-01" /></td>
           <page-table-td-actions>
             <icon-button variant="outline-tertiary" square hide-label size="sm" icon-left="magnifying-glass" class="border-0 me-1" />
             <icon-button variant="outline-tertiary" square hide-label size="sm" icon-left="arrow-clockwise" class="border-0 me-1" />
@@ -154,9 +154,9 @@ export const WithoutSelectMode = {
           <td><a href="#" target="_self">Demo Maxime</a></td>
           <td>57</td>
           <td>455</td>
-          <td><project-label project="Project 2" hide-thumbnail /></td>
+          <td><project-label class="text-nowrap" project="Project 2" hide-thumbnail /></td>
           <td>Jane Doe</td>
-          <td><display-datetime value="2021-09-02" /></td>
+          <td><display-datetime class="text-nowrap" value="2021-09-02" /></td>
           <page-table-td-actions>
             <icon-button variant="outline-tertiary" square hide-label size="sm" icon-left="magnifying-glass" class="border-0 me-1" />
             <icon-button variant="outline-tertiary" square hide-label size="sm" icon-left="arrow-clockwise" class="border-0 me-1" />
@@ -169,9 +169,9 @@ export const WithoutSelectMode = {
           <td><a href="#" target="_self">Employees of company TYU</a></td>
           <td>4</td>
           <td>784</td>
-          <td><project-label project="Project 3" hide-thumbnail /></td>
+          <td><project-label class="text-nowrap" project="Project 3" hide-thumbnail /></td>
           <td>Jan Doe</td>
-          <td><display-datetime value="2021-09-03" /></td>
+          <td><display-datetime class="text-nowrap" value="2021-09-03" /></td>
           <page-table-td-actions>
             <icon-button variant="outline-tertiary" square hide-label size="sm" icon-left="magnifying-glass" class="border-0 me-1" />
             <icon-button variant="outline-tertiary" square hide-label size="sm" icon-left="arrow-clockwise" class="border-0 me-1" />
@@ -184,9 +184,9 @@ export const WithoutSelectMode = {
           <td><a href="#" target="_self">Banks in Singapore </a></td>
           <td>30</td>
           <td>789</td>
-          <td><project-label project="Project 4" hide-thumbnail /></td>
+          <td><project-label class="text-nowrap" project="Project 4" hide-thumbnail /></td>
           <td>Jin Doe</td>
-          <td><display-datetime value="2021-09-04" /></td>
+          <td><display-datetime class="text-nowrap" value="2021-09-04" /></td>
           <page-table-td-actions>
             <icon-button variant="outline-tertiary" square hide-label size="sm" icon-left="magnifying-glass" class="border-0 me-1" />
             <icon-button variant="outline-tertiary" square hide-label size="sm" icon-left="arrow-clockwise" class="border-0 me-1" />
