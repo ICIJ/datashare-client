66ce139c165d7f24a15da11418a0013ca1653c26
Caroline Desprat <cdesprat@icij.org>
2024-07-25 13:44:25 +0000

    feat: LinkedDocumentSection LinkedDocumentList LinkedDocumentListEntry

---
+++ src/components/LinkedDocument/LinkedDocumentListEntry.vue
@@ -0,0 +1,20 @@
+<template>
+  <span class="d-inline-flex align-items-center"
+    ><component :is="findContentTypeIcon(contentType)" class="me-2" /><router-link :to="url"
+      ><slot>{{ name }}</slot></router-link
+    ></span
+  >
+</template>
+
+<script setup>
+import { findContentTypeIcon } from '@/utils/phosphor-icon-files'
+
+defineOptions({
+  name: 'LinkedDocumentListEntry'
+})
+defineProps({
+  contentType: { type: String, default: 'file' },
+  url: { type: String, required: true },
+  name: { type: String, default: 'No name provided' }
+})
+</script>
