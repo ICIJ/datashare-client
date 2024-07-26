fdabb0f3b0774d16a56126254e0e3b15ac58cfe7
Caroline Desprat <cdesprat@icij.org>
2024-07-26 15:45:25 +0000

    feat: add DocumentUserActions

---
+++ src/components/Document/DocumentUserActions/DocumentUserActions.vue
@@ -0,0 +1,51 @@
+<template>
+  <b-button-group class="document-user-actions">
+    <document-user-actions-entry v-if="showTags" :hide-label="hideLabels" icon="tag" label="Tags" value="3+" />
+    <document-user-actions-entry
+      v-if="showComments"
+      :hide-label="hideLabels"
+      icon="chats-teardrop"
+      label="Comments"
+      value="3+"
+    />
+    <document-user-actions-entry
+      v-if="showRecommended"
+      :hide-label="hideLabels"
+      icon="user-gear"
+      label="Recommended by"
+      value="3+"
+    />
+    <document-user-actions-entry v-if="showFolders" :hide-label="hideLabels" icon="folder" label="Folder" value="3+" />
+    <document-user-actions-entry v-if="showNotes" :hide-label="hideLabels" icon="note-blank" label="Notes" value="3+" />
+  </b-button-group>
+</template>
+<script setup>
+import DocumentUserActionsEntry from '@/components/Document/DocumentUserActions/DocumentUserActionsEntry'
+defineOptions({ name: 'DocumentUserActions' })
+defineProps({
+  showTags: {
+    type: Boolean,
+    default: true
+  },
+  showComments: {
+    type: Boolean,
+    default: false
+  },
+  showRecommended: {
+    type: Boolean,
+    default: false
+  },
+  showFolders: {
+    type: Boolean,
+    default: false
+  },
+  showNotes: {
+    type: Boolean,
+    default: false
+  },
+  hideLabels: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
---
+++ src/stories/components/Document/DocumentUserActions/DocumentUserActionsEntry.stories.js
@@ -0,0 +1,12 @@
+import DocumentUserActionsEntry from '@/components/Document/DocumentUserActions/DocumentUserActionsEntry'
+
+export default {
+  title: 'Components/Document/DocumentUserActions/DocumentUserActionsEntry',
+  tags: ['autodocs'],
+  component: DocumentUserActionsEntry,
+  args: {}
+}
+
+export const Default = {
+  args: { icon: 'tag', label: 'Tag', value: '3' }
+}
