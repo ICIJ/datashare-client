e7f6fc638598f7aef3ea56d3964fda6cd58dcaf5
Pierre Romera <hello@pirhoo.com>
2024-07-31 17:00:50 +0000

    feat: add FormControlRange

---
+++ src/components/FormControl/FormControlRange/FormControlRange.vue
@@ -0,0 +1,53 @@
+<script setup>
+defineProps({
+  modelValue: {
+    type: Number,
+    default: 0
+  },
+  min: {
+    type: Number,
+    default: 0
+  },
+  max: {
+    type: Number,
+    default: 10
+  },
+  step: {
+    type: Number,
+    default: 1
+  }
+})
+</script>
+
+<template>
+  <div class="form-control-range d-inline-block">
+    <div class="form-control-range__wrapper">
+      <form-control-range-slider
+        class="mb-3"
+        :model-value="modelValue"
+        :min="min"
+        :max="max"
+        :step="step"
+        @update:modelValue="$emit('update:modelValue', $event)"
+      />
+      <form-control-range-ticks
+        :model-value="modelValue"
+        :min="min"
+        :max="max"
+        :step="step"
+        @update:modelValue="$emit('update:modelValue', $event)"
+      />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.form-control-range {
+  padding-top: $spacer-xs;
+  overflow: hidden;
+
+  &__wrapper {
+    margin: 0 -1 * math.div($spacer-xl, 2);
+  }
+}
+</style>
