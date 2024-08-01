50af14ada4182a2f1557fa25c6b853a1f40c3b1a
Pierre Romera <hello@pirhoo.com>
2024-08-01 16:23:09 +0000

    feat: add DisplayDatetime derivatives

---
+++ src/components/Display/DisplayDatetimeFromNow.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { FORMAT_FROM_NOW } from '@/utils/humanDate'
+
+const props = defineProps({
+  value: {
+    type: [String, Number, Date],
+    required: true
+  },
+  noTooltip: {
+    type: Boolean
+  }
+})
+</script>
+
+<template>
+  <display-datetime :value="props.value" :format="FORMAT_FROM_NOW" :no-tooltip="props.noTooltip" />
+</template>
---
+++ src/components/Display/DisplayDatetimeMonth.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { FORMAT_MONTH } from '@/utils/humanDate'
+
+const props = defineProps({
+  value: {
+    type: [String, Number, Date],
+    required: true
+  },
+  noTooltip: {
+    type: Boolean
+  }
+})
+</script>
+
+<template>
+  <display-datetime :value="props.value" :format="FORMAT_MONTH" :no-tooltip="props.noTooltip" />
+</template>
