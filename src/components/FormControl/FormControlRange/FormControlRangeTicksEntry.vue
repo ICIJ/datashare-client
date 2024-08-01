94f77e80c55d760cdc742ae63a9447e28927f31c
Pierre Romera <hello@pirhoo.com>
2024-08-01 08:23:28 +0000

    feat: add focus visible

--- src/components/FormControl/FormControlRange/FormControlRangeTicksEntry.vue
+++ src/components/FormControl/FormControlRange/FormControlRangeTicksEntry.vue
@@ -18,9 +18,9 @@ const classList = computed(() => {
 </script>
 
 <template>
-  <div class="form-control-range-ticks-entry" :class="classList">
+  <button type="button" class="form-control-range-ticks-entry btn btn-outline-action" :class="classList">
     {{ value }}
-  </div>
+  </button>
 </template>
 
 <style lang="scss" scoped>
@@ -29,15 +29,21 @@ const classList = computed(() => {
   text-align: center;
   border-radius: var(--bs-border-radius);
   font-variant-numeric: tabular-nums;
-  cursor: pointer;
-
   flex-grow: 1;
   flex-basis: 0;
   width: max-content;
 
+  --bs-btn-bg: transparent;
+  --bs-btn-color: var(--bs-link-color);
+  --bs-btn-border-color: transparent;
+  --bs-btn-hover-bg: var(--bs-btn-bg);
+  --bs-btn-hover-color: var(--bs-btn-color);
+  --bs-btn-hover-border-color: var(--bs-action);
+  --bs-btn-focus-box-shadow: 0 0 0 #{$btn-focus-width} #{mix($action, $white)} inset;
+
   &--active {
-    background-color: var(--bs-action);
-    color: var(--bs-white);
+    --bs-btn-bg: var(--bs-action);
+    --bs-btn-color: var(--bs-white);
   }
 }
 </style>
