9d4e8795fd53db0442eae931af67961654a81eb9
Caroline Desprat <cdesprat@icij.org>
2024-08-01 16:37:05 +0000

    feat: add document user tags

---
+++ src/components/Document/DocumentUserActions/DocumentUserTags/DocumentUserTags.vue
@@ -0,0 +1,49 @@
+<script setup>
+import DocumentUserActionsCard from '@/components/Document/DocumentUserActions/DocumentUserActionsCard'
+import DisplayTags from '@/components/Display/DisplayTags'
+import ButtonIcon from '@/components/Button/ButtonIcon'
+
+const props = defineProps({
+  tags: {
+    type: Array,
+    default: () => []
+  },
+  othersTags: {
+    type: Array,
+    default: () => []
+  },
+  isServer: { type: Boolean, default: false }
+})
+
+const nbTags = props.tags.length + props.othersTags.length
+const title = `${nbTags} tags`
+const tagListOthers = 'Added by others'
+const tagListYours = 'Added by you'
+const tagWarning = 'Your tags are public to project members'
+const actionLabel = 'Add a tag'
+const noTags = 'No tags added yet.'
+</script>
+
+<template>
+  <document-user-actions-card
+    icon="tag"
+    :title="title"
+    :is-split="isServer"
+    :show-warning="isServer"
+    :list-name-others="tagListOthers"
+    :list-name-yours="tagListYours"
+  >
+    <template #others>
+      <display-tags v-if="othersTags.length" :value="othersTags" />
+      <span v-else>{{ noTags }}</span>
+    </template>
+    <template #yours>
+      <display-tags v-if="tags.length" :value="tags" />
+      <span v-else>{{ noTags }}</span>
+    </template>
+    <template #footer-warning>{{ tagWarning }}</template>
+    <template #footer>
+      <button-icon icon-left="tag" :label="actionLabel" />
+    </template>
+  </document-user-actions-card>
+</template>
