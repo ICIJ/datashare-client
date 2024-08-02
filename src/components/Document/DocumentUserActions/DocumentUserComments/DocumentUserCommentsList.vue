59744d716377c3710ab1495a53eb26a91c8502df
Caroline Desprat <cdesprat@icij.org>
2024-08-02 13:30:45 +0000

    feat: sort comments by date + i18n

--- src/components/Document/DocumentUserActions/DocumentUserComments/DocumentUserCommentsList.vue
+++ src/components/Document/DocumentUserActions/DocumentUserComments/DocumentUserCommentsList.vue
@@ -1,8 +1,9 @@
 <script setup>
 import { PhosphorIcon } from '@icij/murmur-next'
 import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
 
-import DocumentUserCommentsListEntry from '@/components/Document/DocumentUserActions/DocumentUserComments/DocumentUserCommentsListEntry.vue'
+import DocumentUserCommentsListEntry from '@/components/Document/DocumentUserActions/DocumentUserComments/DocumentUserCommentsListEntry'
 
 defineOptions({ name: 'DocumentUserCommentsList' })
 
@@ -14,27 +15,39 @@ const props = defineProps({
   comments: { type: Array, default: () => [] },
   height: { type: String, default: '250px' }
 })
+const { t } = useI18n()
+
 const style = {
   '--document-user-comments-list__comments__list--height': props.height
 }
 
 const firstComment = '#comment-0'
 const lastComment = props.comments.length ? `#comment-${props.comments.length - 1}` : ''
+
+const hideComments = t('documentUserCommentsList.hideComments')
+const showComments = t('documentUserCommentsList.showComments')
+const noComments = t('documentUserCommentsList.noComments')
+const sortingText = t('documentUserCommentsList.sortingText')
+const goToOldest = t('documentUserCommentsList.goToOldest')
+const goToRecent = t('documentUserCommentsList.goToRecent')
+
+const goToOldestIcon = 'arrow-up'
+const goToRecentIcon = 'arrow-down'
+
+const displayComments = computed(() => (open.value ? hideComments : showComments))
+
 const closedEye = computed(() => {
   return open.value ? 'regular' : 'fill'
 })
-
-const sortingText = 'From oldest to most recent'
-const displayComments = computed(() => (open.value ? 'Hide comments' : 'Show comments'))
-const goToOldest = 'Go to oldest'
-const goToRecent = 'Go to most recent'
-const noComments = 'No comments on this document yet.'
+const sortedComments = computed(() => [...props.comments].sort((a, b) => a.date - b.date))
 </script>
 
 <template>
   <section class="document-user-comments-list">
     <header class="d-flex justify-content-between align-items-center">
-      <span><phosphor-icon name="sortAscending" />{{ sortingText }}</span>
+      <span class="d-inline-flex align-items-center"
+        ><phosphor-icon name="sortAscending" class="me-1" />{{ sortingText }}</span
+      >
       <span
         class="document-user-comments-list__display-comments btn btn-outline-link d-inline-flex justify-content-between"
         @click="open = !open"
@@ -44,11 +57,13 @@ const noComments = 'No comments on this document yet.'
     <section v-if="open" class="document-user-comments-list__comments py-2">
       <template v-if="comments.length">
         <header class="text-center py-2">
-          <a :href="firstComment">{{ goToOldest }}</a>
+          <a :href="firstComment" class="d-inline-flex align-items-center"
+            ><phosphor-icon :name="goToOldestIcon" class="me-1" />{{ goToOldest }}</a
+          >
         </header>
         <article class="document-user-comments-list__comments__list d-block overflow-y-scroll" :style="style">
           <document-user-comments-list-entry
-            v-for="(comment, index) in comments"
+            v-for="(comment, index) in sortedComments"
             :id="`comment-${index}`"
             :key="index"
             :text="comment.text"
@@ -58,7 +73,9 @@ const noComments = 'No comments on this document yet.'
           />
         </article>
         <footer class="text-center py-2">
-          <a :href="lastComment">{{ goToRecent }}</a>
+          <a :href="lastComment" class="d-inline-flex align-items-center"
+            ><phosphor-icon :name="goToRecentIcon" class="me-1" />{{ goToRecent }}</a
+          >
         </footer>
       </template>
       <template v-else>
