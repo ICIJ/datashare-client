eb2c0609104d536b6b94f162035672968ef46c59
Caroline Desprat <cdesprat@icij.org>
2024-08-05 14:57:55 +0000

    fix: add filter on action to display

--- src/stories/components/Document/DocumentUserActions/DocumentUserActions.stories.js
+++ src/stories/components/Document/DocumentUserActions/DocumentUserActions.stories.js
@@ -5,18 +5,26 @@ export default {
   tags: ['autodocs'],
   component: DocumentUserActions,
   args: {
+    dropdown: false,
+    compact: false,
     showTags: true,
     showComments: false,
     showRecommended: false,
     showFolders: false,
     showNotes: false,
     hideLabels: false,
-    hideTooltips: false
+    hideTooltips: false,
+    tags: 0,
+    comments: 0,
+    recommended: 0,
+    folders: 0,
+    notes: 0
   }
 }
 
 export const Default = {
   args: {
+    compact: false,
     showTags: true,
     tags: 3
   }
@@ -29,6 +37,7 @@ export const Compact = {
 }
 export const Complete = {
   args: {
+    compact: false,
     showTags: true,
     showComments: true,
     showRecommended: true,
@@ -41,8 +50,50 @@ export const Complete = {
     notes: 3
   }
 }
-export const Dropdown = {
+export const CompleteCompact = {
   args: {
+    compact: true,
+    showTags: true,
+    showComments: true,
+    showRecommended: true,
+    showFolders: true,
+    showNotes: true,
+    tags: 3,
+    comments: 4,
+    recommended: 0,
+    folders: 5,
+    notes: 3
+  }
+}
+export const DropdownDefault = {
+  args: {
+    compact: true,
+    dropdown: true,
+    showTags: true,
+    showComments: true,
+    tags: 3,
+    comments: 4
+  }
+}
+export const DropdownWithoutTooltip = {
+  args: {
+    hideTooltips: true,
+    dropdown: true,
+    showTags: true,
+    showComments: true,
+    showRecommended: true,
+    showFolders: true,
+    showNotes: true,
+    tags: 3,
+    comments: 4,
+    recommended: 0,
+    folders: 5,
+    notes: 3
+  }
+}
+export const DropdownCompact = {
+  args: {
+    compact: true,
     dropdown: true,
     showTags: true,
     showComments: true,
