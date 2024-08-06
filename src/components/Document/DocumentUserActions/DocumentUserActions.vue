4b65fd2657656e5ddaca82aa898e4404c4cc5886
Caroline Desprat <cdesprat@icij.org>
2024-08-06 07:52:28 +0000

    feat: split document user actions into DocumentUserActionsDropdown and DocumentUserActionsList

--- src/components/Document/DocumentUserActions/DocumentUserActions.vue
+++ src/components/Document/DocumentUserActions/DocumentUserActions.vue
@@ -1,23 +1,20 @@
 <template>
-  <b-dropdown
-    v-if="dropdown"
-    split
-    variant="outline-action"
-    class="document-user-actions document-user-actions--dropdown text-action-emphasis bg-action-subtle p-0"
-  >
-    <template #button-content><document-user-actions-entry v-bind="tagAction" /></template>
-    <document-user-actions-entry v-for="action in otherActions" :key="action.name" v-bind="action" dropdown-item />
-  </b-dropdown>
-  <b-button-group v-else class="document-user-actions justify-content-between text-action-emphasis bg-action-subtle">
-    <document-user-actions-entry v-for="action in actionsDisplayed" :key="action.name" v-bind="action" />
-  </b-button-group>
+  <component
+    :is="component"
+    :class="classList"
+    :items="actionsDisplayed"
+    :compact="compact"
+    :hide-tooltips="hideTooltips"
+    :hide-labels="hideLabels"
+  />
 </template>
 <script setup>
 import { useI18n } from 'vue-i18n'
 import { computed } from 'vue'
 import { capitalize } from 'lodash'
 
-import DocumentUserActionsEntry from '@/components/Document/DocumentUserActions/DocumentUserActionsEntry'
+import DocumentUserActionsDropdown from '@/components/Document/DocumentUserActions/DocumentUserActionsDropdown'
+import DocumentUserActionsList from '@/components/Document/DocumentUserActions/DocumentUserActionsList'
 
 defineOptions({ name: 'DocumentUserActions' })
 
@@ -27,6 +24,14 @@ const props = defineProps({
     default: false
   },
   compact: {
+    type: Boolean,
+    default: false
+  },
+  hideTooltips: {
+    type: Boolean,
+    default: false
+  },
+  hideLabels: {
     type: Boolean,
     default: false
   },
@@ -47,14 +52,6 @@ const props = defineProps({
     default: false
   },
   showNotes: {
-    type: Boolean,
-    default: false
-  },
-  hideLabels: {
-    type: Boolean,
-    default: false
-  },
-  hideTooltips: {
     type: Boolean,
     default: false
   },
@@ -94,29 +91,27 @@ const icons = {
   [USER_ACTIONS.FOLDERS]: 'folder',
   [USER_ACTIONS.NOTES]: 'note-blank'
 }
-const actions = Object.values(USER_ACTIONS).map((action) => ({
-  name: action,
-  show: props[`show${capitalize(action)}`],
-  icon: icons[action],
-  label: t(`documentUserActions.${action}`, { n: props[action] }),
-  value: props[action].toString(),
-  hideTooltip: props.hideTooltips,
-  hideLabel: props.hideLabels,
-  compact: props.compact
-}))
+const actions = computed(() => {
+  return Object.values(USER_ACTIONS).map((action) => ({
+    name: action,
+    show: props[`show${capitalize(action)}`],
+    icon: icons[action],
+    label: t(`documentUserActions.${action}`, { n: props[action] }),
+    value: props[action].toString(),
+    hideTooltip: props.hideTooltips,
+    hideLabel: props.hideLabels,
+    compact: props.compact
+  }))
+})
 const actionsDisplayed = computed(() => {
-  return actions.filter((action) => action.show === true)
+  return actions.value.filter((action) => action.show === true)
 })
-const tagAction = computed(() => {
-  const tag = { ...actions[0] }
-  const label = props.compact ? tag.value : tag.label
-  tag.tag = 'span'
-  tag.label = label
-  tag.tooltipLabel = tag.label
-  return tag
+
+const component = computed(() => {
+  return props.dropdown ? DocumentUserActionsDropdown : DocumentUserActionsList
 })
-const otherActions = computed(() => {
-  return actions.slice(1).filter((action) => action.show === true)
+const classList = computed(() => {
+  return 'text-action-emphasis bg-action-subtle'
 })
 </script>
 <style lang="scss">
@@ -124,7 +119,7 @@ const otherActions = computed(() => {
   &--dropdown {
     & > .btn:first-child {
       border: 0;
-      padding: 0 0.5em 0 0;
+      padding: 0;
       text-align: center;
       background: inherit;
       &:hover {
