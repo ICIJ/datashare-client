1833bfeea7981677840eeb9912294e4c17efef11
Pierre Romera <hello@pirhoo.com>
2024-08-07 16:26:55 +0000

    feat: improve responsivness

--- src/components/Project/ProjectCard/ProjectCard.vue
+++ src/components/Project/ProjectCard/ProjectCard.vue
@@ -16,10 +16,10 @@ defineProps({
 
 <template>
   <div class="project-card border rounded p-4 d-flex gap-3">
-    <project-card-thumbnail :project="project">
+    <project-card-thumbnail :project="project" class="flex-grow-1">
       <slot name="thumbnail" />
     </project-card-thumbnail>
-    <div class="d-flex flex-column gap-4 h-100 w-100">
+    <div class="d-flex flex-column gap-4 h-100 w-100 flex-truncate">
       <div>
         <project-card-label :project="project" class="mb-2">
           <slot name="label" />
@@ -28,11 +28,11 @@ defineProps({
           <slot name="description" />
         </project-card-description>
       </div>
-      <div class="d-flex flex-md-row flex-column lh-md-1 mt-auto">
+      <div class="d-flex flex-lg-row flex-column lh-lg-1 mt-auto">
         <project-card-update-date :project="project">
           <slot name="update-date" />
         </project-card-update-date>
-        <project-card-documents-count :project="project" class="ms-md-auto">
+        <project-card-documents-count :project="project" class="ms-lg-auto">
           <slot name="documents-count" />
         </project-card-documents-count>
       </div>
