6a4ce9a20e7682094202832e62b92cda2ad7c83e
Pierre Romera <hello@pirhoo.com>
2024-08-07 12:35:06 +0000

    feat: add ProjectCard icij/datashare#1485

---
+++ src/components/Project/ProjectCard/ProjectCardDocumentsCount.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { PhosphorIcon } from '@icij/murmur-next'
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+import humanNumber from '@/utils/humanNumber'
+
+const props = defineProps({
+  project: {
+    type: Object,
+    required: true
+  }
+})
+
+const { tm } = useI18n()
+
+const humanDocumentsCount = computed(() => {
+  return humanNumber(props.project.documentsCount, tm('human.number'))
+})
+</script>
+
+<template>
+  <div
+    v-if="project.documentsCount"
+    v-b-tooltip.body
+    class="project-card-documents-count text-secondary-emphasis d-inline-flex gap-1 align-items-center flex-wrap"
+    :title="$n(project.documentsCount)"
+  >
+    <slot>
+      <phosphor-icon name="files" class="me-1" />
+      {{ $t('projectCardDocumentsCount.label', { humanDocumentsCount }) }}
+    </slot>
+  </div>
+</template>
---
+++ src/components/Project/ProjectCard/ProjectCardLabel.vue
@@ -0,0 +1,24 @@
+<script setup>
+import ProjectLabel from '@/components/Project/ProjectLabel'
+
+defineProps({
+  project: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <h3 class="project-card-label text-dark-emphasis">
+    <project-label :project="project" hide-thumbnail />
+  </h3>
+</template>
+
+<style scoped>
+.project-card-label {
+  margin: 0;
+  font-size: 20px;
+  font-weight: 700;
+}
+</style>
