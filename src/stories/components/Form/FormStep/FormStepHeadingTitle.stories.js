9025d6a8aea052c3216eaee6a1463c10ada48f96
Pierre Romera <hello@pirhoo.com>
2024-08-08 09:33:25 +0000

    feat: add FormStep components

---
+++ src/stories/components/Form/FormStep/FormStepHeadingTitle.stories.js
@@ -0,0 +1,11 @@
+import FormStepHeadingTitle from '@/components/Form/FormStep/FormStepHeadingTitle'
+
+export default {
+  title: 'Components/Form/FormStep/FormStepHeadingTitle',
+  component: FormStepHeadingTitle,
+  args: {
+    title: 'Name, project(s) and description'
+  }
+}
+
+export const Default = {}
