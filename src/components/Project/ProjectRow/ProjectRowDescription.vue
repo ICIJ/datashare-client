d1d5602af14c596426cc05e8ec6dfbbea09be96a
Pierre Romera <hello@pirhoo.com>
2024-08-08 16:47:47 +0000

    feat: add project row

---
+++ src/components/Project/ProjectRow/ProjectRowDescription.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  project: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <td class="project-row-description">
+    <slot>{{ project.description }}</slot>
+  </td>
+</template>
---
+++ src/components/Project/ProjectRow/ProjectRowLabel.vue
@@ -0,0 +1,21 @@
+<script setup>
+import ProjectRowThumbnail from './ProjectRowThumbnail'
+
+import ProjectLabel from '@/components/Project/ProjectLabel'
+
+defineProps({
+  project: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <td class="project-row-label">
+    <div class="d-flex gap-3 align-items-start">
+      <project-row-thumbnail :project="project" rounded class="flex-shrink-0" />
+      <project-label :project="project" hide-thumbnail class="fw-medium" />
+    </div>
+  </td>
+</template>
