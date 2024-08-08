9c8312a52ea808a0da4415e3d81ecf4e3c77913b
Caroline Desprat <cdesprat@icij.org>
2024-08-08 15:34:30 +0000

    feat: improve tags user action

--- src/stories/components/Document/DocumentUserActionsCard/DocumentUserTags/DocumentUserTags.stories.js
+++ src/stories/components/Document/DocumentUserActionsCard/DocumentUserTags/DocumentUserTags.stories.js
@@ -1,3 +1,5 @@
+import uniqueId from 'lodash/uniqueId'
+
 import DocumentUserTags from '@/components/Document/DocumentUserActionsCard/DocumentUserTags/DocumentUserTags'
 
 export default {
@@ -6,24 +8,38 @@ export default {
   component: DocumentUserTags,
   args: {
     modelValue: [],
+    username: 'jsmith',
     options: ['toto', 'titi', 'tata'],
     listNameOthers: 'Added by others',
     listNameYours: 'Added by you'
   }
 }
+const tag = (tagName, username) => {
+  return {
+    tag: tagName,
+    username
+  }
+}
+const tagMine = (tagName) => tag(tagName, 'jsmith')
+const tagOther = (tagName) => tag(tagName, uniqueId('other'))
+const tags = [tagMine('toto'), tagMine('titi'), tagMine('tata'), tagOther('riri'), tagOther('fifi'), tagOther('loulou')]
 export const Default = {}
 export const WithTags = {
-  args: { modelValue: ['toto', 'titi', 'tata'], options: ['toto', 'titi', 'tata', 'test'] }
+  args: {
+    modelValue: tags,
+    options: ['toto', 'titi', 'tata', 'test']
+  }
 }
 
 export const isServer = {
-  isServer: true
+  args: {
+    isServer: true
+  }
 }
 
 export const isServerWithTags = {
   args: {
     isServer: true,
-    modelValue: ['toto', 'titi', 'tata'],
-    othersTags: ['riri', 'fifi', 'loulou']
+    modelValue: tags
   }
 }
