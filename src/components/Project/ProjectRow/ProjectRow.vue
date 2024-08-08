aa7856766f11bc75c36d58a473f26f4738cd9ce3
Pierre Romera <hello@pirhoo.com>
2024-08-08 16:47:47 +0000

    feat: add project row

---
+++ src/components/Project/ProjectRow/ProjectRow.vue
@@ -0,0 +1,26 @@
+<script setup>
+import ProjectRowLabel from './ProjectRowLabel'
+import ProjectRowDescription from './ProjectRowDescription'
+import ProjectRowDocumentsCount from './ProjectRowDocumentsCount'
+import ProjectRowUpdateDate from './ProjectRowUpdateDate'
+
+import PageTableTr from '@/components/PageTable/PageTableTr'
+
+defineProps({
+  project: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <page-table-tr class="project-card-row">
+    <project-row-label :project="project" />
+    <project-row-description :project="project" />
+    <project-row-documents-count :project="project" />
+    <project-row-update-date :project="project" />
+    <project-row-links :project="project" />
+    <project-row-actions :project="project" />
+  </page-table-tr>
+</template>
---
+++ src/components/Project/ProjectRow/ProjectRowLinks.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { computed } from 'vue'
+
+import ButtonIcon from '@/components/Button/ButtonIcon'
+
+const props = defineProps({
+  project: {
+    type: Object,
+    required: true
+  }
+})
+
+const toProjectInsights = computed(() => ({
+  name: 'project.view.insights',
+  params: {
+    name: props.project.name
+  }
+}))
+
+const toProjectSearch = computed(() => ({
+  name: 'search',
+  query: {
+    indices: [props.project.name]
+  }
+}))
+</script>
+
+<template>
+  <td class="project-row-link">
+    <div class="d-flex gap-3">
+      <slot>
+        <button-icon
+          :to="toProjectInsights"
+          icon-left="chart-bar"
+          variant="outline-tertiary"
+          truncate
+          class="fw-medium"
+          :label="$t('projectCardFooter.insights')"
+        />
+        <button-icon
+          :to="toProjectSearch"
+          icon-left="magnifying-glass"
+          variant="outline-primary"
+          truncate
+          class="fw-medium"
+          :label="$t('projectCardFooter.search')"
+        />
+      </slot>
+    </div>
+  </td>
+</template>
