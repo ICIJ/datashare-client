130d0faa77f5fe6a27b198341fee390946be4e6c
Pierre Romera <hello@pirhoo.com>
2024-08-08 13:51:04 +0000

    feat: add document translation alert

---
+++ src/components/Document/DocumentTranslationAlert/DocumentTranslationAlertToggler.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+const props = defineProps({
+  active: {
+    type: Boolean
+  }
+})
+
+const { t } = useI18n()
+
+const label = computed(() => {
+  const message = props.active ? 'viewOriginal' : 'viewTranslation'
+  return t(`documentTranslationAlertToggler.${message}`)
+})
+</script>
+
+<template>
+  <button-icon
+    class="document-translation-alert-toggler"
+    variant="outline-warning"
+    :label="label"
+    @click="$emit('update:active', !active)"
+  />
+</template>
