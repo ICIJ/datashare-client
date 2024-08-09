290518c2b48d0c3ef98817c511da1c1d784690a6
Pierre Romera <hello@pirhoo.com>
2024-08-09 16:34:46 +0000

    feat: hide tooltip

--- src/components/Search/SearchResults/SearchResultsActions/SearchResultsActionsStar.vue
+++ src/components/Search/SearchResults/SearchResultsActions/SearchResultsActionsStar.vue
@@ -9,5 +9,5 @@ defineProps({
 </script>
 
 <template>
-  <button-icon icon-left="star" :label="$t('searchResultsActions.star')" :hide-label="hideLabel" />
+  <button-icon icon-left="star" :label="$t('searchResultsActions.star')" :hide-label="hideLabel" hide-tooltip />
 </template>
