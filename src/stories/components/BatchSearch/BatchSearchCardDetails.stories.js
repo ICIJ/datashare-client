c356d111f8c2278781cbb05bbab2e577ff8e0311
Caroline Desprat <cdesprat@icij.org>
2024-08-09 15:33:59 +0000

    feat: add batch search card details

---
+++ src/stories/components/BatchSearch/BatchSearchCardDetails.stories.js
@@ -0,0 +1,30 @@
+import { vueRouter } from 'storybook-vue3-router'
+
+import BatchSearchCardDetails from '@/components/BatchSearch/BatchSearchCardDetails'
+import { storeDecoratorPipelineChainByCategory } from '~storybook/decorators/vuex'
+const routes = [
+  {
+    path: '/project/:name',
+    name: 'project.view'
+  }
+]
+export default {
+  decorators: [vueRouter(routes), storeDecoratorPipelineChainByCategory],
+  title: 'Components/BatchSearch/BatchSearchCardDetails',
+  component: BatchSearchCardDetails,
+  tags: ['autodocs'],
+  args: {
+    name: 'Richest people in the EU',
+    status: 'success',
+    date: new Date(),
+    author: 'jsmith',
+    visibility: true,
+    phraseMatch: true,
+    proximity: 1,
+    fuzziness: 2,
+    projects: ['banana papers', 'citrus confidential'],
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque malesuada ex egestas sem fringilla euismod. Curabitur cursus mattis lectus, a dignissim libero mattis dapibus. Nulla at lectus imperdiet, sollicitudin nunc ut, consectetur sapien. Morbi fermentum dolor ac ultrices commodo. Sed dictum posuere lobortis. Suspendisse hendrerit elit at convallis suscipit. Nullam non massa eu eros tempor varius id id dolor. Vestibulum eu sagittis arcu, nec sagittis turpis. Donec maximus aliquet tortor, a blandit ligula laoreet eu. In quis est sed urna lacinia sagittis ut ac lectus. Morbi ut ipsum et libero dignissim pretium eu ut diam. Vestibulum ante ips'
+  }
+}
+export const Default = {}
