31a28fbbe1ac16c84727f60d1a6c2daa99276da3
Pierre Romera <hello@pirhoo.com>
2024-08-09 10:53:31 +0000

    build: switch to module package

--- vitest.config.js
+++ vitest.config.js
@@ -2,7 +2,7 @@ import { fileURLToPath } from 'node:url'
 import { resolve } from 'path'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 
-import viteConfig from './vite.config.mjs'
+import viteConfig from './vite.config.js'
 
 export default defineConfig((configEnv) =>
   mergeConfig(
