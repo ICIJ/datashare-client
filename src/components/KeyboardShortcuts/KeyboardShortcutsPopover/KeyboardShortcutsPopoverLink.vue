3a757aa11da6ae4f5e87cb1a588f14edc3fa8fea
Pierre Romera <hello@pirhoo.com>
2024-08-12 16:10:59 +0000

    feat: add keyboard popover

---
+++ src/components/KeyboardShortcuts/KeyboardShortcutsPopover/KeyboardShortcutsPopoverLink.vue
@@ -0,0 +1,16 @@
+<script setup>
+import ButtonIcon from '@/components/Button/ButtonIcon'
+</script>
+
+<template>
+  <button-icon
+    truncate
+    variant="outline-action"
+    class="keyboard-shortcuts-popover-link"
+    icon-right="caret-right"
+    icon-right-variant="secondary"
+    :to="{ name: 'keyboard-shortcuts' }"
+  >
+    {{ $t('keyboardShortcutsPopoverLink.label') }}
+  </button-icon>
+</template>
---
+++ src/components/KeyboardShortcuts/KeyboardShortcutsPopover/KeyboardShortcutsPopoverTitle.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { PhosphorIcon } from '@icij/murmur-next'
+</script>
+
+<template>
+  <h4 class="keyboard-shortcuts-popover-title d-flex align-items-center text-truncate">
+    <phosphor-icon name="keyboard" class="me-2 flex-shrink-0" />
+    {{ $t('keyboardShortcutsPopoverTitle.label') }}
+  </h4>
+</template>
+
+<style lang="scss" scoped>
+.keyboard-shortcuts-popover-title {
+  font-size: $font-size-base;
+  color: var(--bs-action-text-emphasis);
+  font-weight: 500;
+  line-height: 1.5rem;
+  margin: 0;
+  padding: 0;
+}
+</style>
