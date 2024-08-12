f83f3be620d505459eb231031888e923775bf408
Pierre Romera <hello@pirhoo.com>
2024-08-12 14:11:59 +0000

    feat: add keyboard shortcuts section

---
+++ src/components/KeyboardShortcuts/KeyboardShortcutsSection/KeyboardShortcutsSectionEntry.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { computed } from 'vue'
+import { isArray, trim } from 'lodash'
+import { getShortkeyOS } from '@/utils/utils'
+
+import KeyboardShortcutsSectionEntryKey from './KeyboardShortcutsSectionEntryKey'
+
+const props = defineProps({
+  label: {
+    type: String,
+    required: true
+  },
+  keys: {
+    type: [String, Array],
+    required: true
+  },
+  macKeys: {
+    type: [String, Array]
+  }
+})
+
+const allKeys = computed(() => {
+  // Use macKeys if the OS is macOS and macKeys is provided.
+  const keys = getShortkeyOS() === 'mac' && props.macKeys ? props.macKeys : props.keys
+  return isArray(keys) ? keys : keys.split('+').map(trim)
+})
+</script>
+
+<template>
+  <div class="keyboard-shortcuts-section-entry d-flex gap-3 align-items-center py-2">
+    <div class="keyboard-shortcuts-section-entry__label">
+      <slot name="label">{{ label }}</slot>
+    </div>
+    <div class="keyboard-shortcuts-section-entry__keys d-flex gap-3 align-items-center">
+      <slot>
+        <keyboard-shortcuts-section-entry-key v-for="(key, i) in allKeys" :key="i" :value="key" />
+      </slot>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.keyboard-shortcuts-section-entry {
+
+  &__label {
+    max-width: 290px;
+    width: 100%;
+  }
+}
+</style>
---
+++ src/components/KeyboardShortcuts/KeyboardShortcutsSection/KeyboardShortcutsSectionTitle.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <h4 class="keyboard-shortcuts-section-title">{{ title }}</h4>
+</template>
+
+<style lang="scss" scoped>
+.keyboard-shortcuts-section-title {
+  font-size: $font-size-lg;
+  line-height: 1.5rem;
+  font-weight: 700;
+  margin-bottom: $spacer;
+  padding: 0;
+}
+</style>
