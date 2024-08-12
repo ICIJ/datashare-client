34937b25e70217f9873b131488d29efce7eefe0e
Pierre Romera <hello@pirhoo.com>
2024-08-12 14:11:59 +0000

    feat: add keyboard shortcuts section

---
+++ src/components/KeyboardShortcuts/KeyboardShortcutsSection/KeyboardShortcutsSection.vue
@@ -0,0 +1,17 @@
+<script setup>
+import KeyboardShortcutsSectionTitle from '@/components/KeyboardShortcuts/KeyboardShortcutsSection/KeyboardShortcutsSectionTitle'
+
+defineProps({
+  title: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="keyboard-shortcuts-section">
+    <keyboard-shortcuts-section-title :title="title" />
+    <slot />
+  </div>
+</template>
