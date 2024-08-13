969f4746633e0c20d8010c079655cd51cce9de9c
Pierre Romera <hello@pirhoo.com>
2024-08-13 17:16:49 +0000

    feat: add counter to button

--- src/components/Button/ButtonToggleSearchBreadcrumb.vue
+++ src/components/Button/ButtonToggleSearchBreadcrumb.vue
@@ -1,15 +1,53 @@
 <script setup>
+import { computed } from 'vue'
+
 import ButtonIcon from '@/components/Button/ButtonIcon'
+
+const props = defineProps({
+  counter: {
+    type: Number,
+    default: 0
+  },
+  active: {
+    type: Boolean
+  },
+  loading: {
+    type: Boolean
+  },
+  disabled: {
+    type: Boolean
+  }
+})
+
+const nonzeroCounter = computed(() => {
+  return props.counter || null
+})
+
+const variant = computed(() => {
+  return props.active ? 'action' : 'outline-dark'
+})
+
+const counterVariant = computed(() => {
+  return props.disabled ? 'dark' : 'action'
+})
+
+const iconLeftWeight = computed(() => {
+  return props.loading ? 'regular' : 'fill'
+})
 </script>
 
 <template>
   <button-icon
+    :counter="nonzeroCounter"
+    :counter-variant="counterVariant"
+    :disabled="disabled"
+    :icon-left-weight="iconLeftWeight"
     :label="$t('buttonToggleSearchBreadcrumb.label')"
+    :loading="loading"
+    :variant="variant"
     class="button-toggle-search-breadcrumb"
     hide-label
-    icon-left-weight="fill"
     icon-left="path"
     square
-    variant="outline-dark"
   />
 </template>
