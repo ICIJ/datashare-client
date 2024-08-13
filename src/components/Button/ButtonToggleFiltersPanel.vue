8d45f2e07c233c2c1e978af8787b8d4facc4a989
Pierre Romera <hello@pirhoo.com>
2024-08-13 14:59:28 +0000

    feat: add ButtonToggleFiltersPanel icij/datashare##1522

---
+++ src/components/Button/ButtonToggleFiltersPanel.vue
@@ -0,0 +1,12 @@
+<script setup>
+import ButtonIcon from '@/components/Button/ButtonIcon'
+</script>
+
+<template>
+  <button-icon
+    class="button-toggle-filters-panel"
+    :label="$t('buttonToggleFiltersPanel.label')"
+    icon-left="funnel"
+    variant="outline-dark"
+  />
+</template>
