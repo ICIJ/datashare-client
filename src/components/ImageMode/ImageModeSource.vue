01a33c09190a0e4a6a6ef12c5dbfffaea4c777bd
Pierre Romera <hello@pirhoo.com>
2024-08-13 12:24:53 +0000

    feat: add ImageMode component

---
+++ src/components/ImageMode/ImageModeSource.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref, getCurrentInstance, onMounted } from 'vue'
+
+const element = ref(null)
+
+const props = defineProps({
+  src: {
+    type: String,
+    required: true
+  },
+  colorMode: {
+    type: String,
+    default: 'light'
+  }
+})
+
+onMounted(() => {
+  const { exposed } = getCurrentInstance()
+  element.value.__exposed__ = exposed
+})
+
+defineExpose({ ...props })
+</script>
+
+<template>
+  <source ref="element" class="image-mode-source" :src="src" />
+</template>
