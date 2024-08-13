2d15fdcf0cd5e6ce3e2eddae3a0df15e1b088648
Pierre Romera <hello@pirhoo.com>
2024-08-13 15:10:13 +0000

    feat: add ButtonToTop icij/datashare##1517

---
+++ src/components/Button/ButtonToTop.vue
@@ -0,0 +1,7 @@
+<script setup>
+import ButtonIcon from '@/components/Button/ButtonIcon'
+</script>
+
+<template>
+  <button-icon class="button-to-top" :label="$t('buttonToTop.label')" variant="action" icon-right="arrow-circle-up" />
+</template>
