f5bdde4dbf83af64ecb5ec4226f72406ff6790e6
Pierre Romera <hello@pirhoo.com>
2024-08-13 14:43:08 +0000

    feat: add ButtonToggleAdvancedSearch icij/datashare#1518

---
+++ src/components/Button/ButtonToggleAdvancedSearch.vue
@@ -0,0 +1,12 @@
+<script setup>
+import ButtonIcon from '@/components/Button/ButtonIcon'
+</script>
+
+<template>
+  <button-icon
+    class="button-toggle-advanced-search"
+    :label="$t('buttonToggleAdvancedSearch.label')"
+    icon-right="eyeglasses"
+    variant="outline-tertiary"
+  />
+</template>
