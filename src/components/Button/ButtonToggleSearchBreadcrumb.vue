8a8f07d40cd6b67ba538151c89115bbcdf0ec3a5
Pierre Romera <hello@pirhoo.com>
2024-08-13 15:05:48 +0000

    feat: add ButtonToggleSearchBreadcrumb icij/datashare##1516

---
+++ src/components/Button/ButtonToggleSearchBreadcrumb.vue
@@ -0,0 +1,15 @@
+<script setup>
+import ButtonIcon from '@/components/Button/ButtonIcon'
+</script>
+
+<template>
+  <button-icon
+    :label="$t('buttonToggleSearchBreadcrumb.label')"
+    class="button-toggle-search-breadcrumb"
+    hide-label
+    icon-left-weight="fill"
+    icon-left="path"
+    square
+    variant="outline-dark"
+  />
+</template>
