a1b10c4f6e6e411388e587a7ec1deb39f317b453
Pierre Romera <hello@pirhoo.com>
2024-08-13 14:59:41 +0000

    feat: add ButtonToggleFiltersPanel icij/datashare##1522

---
+++ src/stories/components/Button/ButtonToggleFiltersPanel.stories.js
@@ -0,0 +1,12 @@
+import ButtonToggleFiltersPanel from '@/components/Button/ButtonToggleFiltersPanel'
+
+export default {
+  tags: ['autodocs'],
+  title: 'Components/Button/ButtonToggleFiltersPanel',
+  component: ButtonToggleFiltersPanel,
+  args: {
+    loading: true
+  }
+}
+
+export const Default = {}
