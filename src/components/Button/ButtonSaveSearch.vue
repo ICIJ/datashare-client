88001a73c1bf02edfca66d2a4bd74886bbc3de0d
Pierre Romera <hello@pirhoo.com>
2024-08-13 14:37:41 +0000

    feat: add ButtonSaveSearch icij/datashare#1520

---
+++ src/components/Button/ButtonSaveSearch.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { computed } from 'vue'
+import { PhFloppyDisk, PhCheck } from '@phosphor-icons/vue'
+import { useI18n } from 'vue-i18n'
+
+import ButtonIcon from '@/components/Button/ButtonIcon'
+import { useBreakpoints } from '@/composables/breakpoints'
+
+const props = defineProps({
+  saved: {
+    type: Boolean
+  },
+  loading: {
+    type: Boolean
+  },
+  compactBreakpoint: {
+    type: String,
+    default: 'md'
+  }
+})
+
+const iconLeft = computed(() => {
+  return props.saved ? PhCheck : PhFloppyDisk
+})
+
+const { breakpointDown } = useBreakpoints()
+
+const compact = computed(() => {
+  return breakpointDown.value[props.compactBreakpoint]
+})
+
+const { t } = useI18n()
+
+const label = computed(() => {
+  if (props.loading) {
+    return t('buttonSaveSearch.labelSaving')
+  }
+
+  if (props.saved) {
+    return t('buttonSaveSearch.labelSaved')
+  }
+
+  return t('buttonSaveSearch.label')
+})
+</script>
+
+<template>
+  <button-icon
+    class="button-save-search"
+    variant="outline-dark"
+    :label="label"
+    :loading="loading"
+    :icon-left="iconLeft"
+    :hide-label="compact"
+    :square="compact"
+  />
+</template>
