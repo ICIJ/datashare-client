e8c77e7432f2dfdc32bd003c8e14b2aed0d3af52
Pierre Romera <hello@pirhoo.com>
2024-08-13 14:43:08 +0000

    feat: add ButtonToggleAdvancedSearch icij/datashare#1518

---
+++ src/stories/components/Button/ButtonToggleAdvancedSearch.stories.js
@@ -0,0 +1,12 @@
+import ButtonToggleAdvancedSearch from '@/components/Button/ButtonToggleAdvancedSearch'
+
+export default {
+  tags: ['autodocs'],
+  title: 'Components/Button/ButtonToggleAdvancedSearch',
+  component: ButtonToggleAdvancedSearch,
+  args: {
+    loading: false
+  }
+}
+
+export const Default = {}
