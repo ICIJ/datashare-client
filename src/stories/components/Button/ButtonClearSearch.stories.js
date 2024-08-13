d54abe547780191ff69a1e0c84f562b737a0f38b
Pierre Romera <hello@pirhoo.com>
2024-08-13 14:20:32 +0000

    feat: add ButtonAdd (icij/datashare#1515) and ButtonClearSearch (icij/datashare#1519)

---
+++ src/stories/components/Button/ButtonClearSearch.stories.js
@@ -0,0 +1,12 @@
+import ButtonClearSearch from '@/components/Button/ButtonClearSearch'
+
+export default {
+  tags: ['autodocs'],
+  title: 'Components/Button/ButtonClearSearch',
+  component: ButtonClearSearch,
+  args: {
+    loading: false
+  }
+}
+
+export const Default = {}
