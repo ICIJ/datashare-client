a82a3bd8a1ed01951b4ebf502abd66bdef8f958a
Pierre Romera <hello@pirhoo.com>
2024-08-13 12:51:02 +0000

    feat: use ImageMode in AppModal

--- src/stories/components/AppModal/AppModal.stories.js
+++ src/stories/components/AppModal/AppModal.stories.js
@@ -1,4 +1,7 @@
 import AppModal from '@/components/AppModal/AppModal'
+import ImageModeSource from '@/components/ImageMode/ImageModeSource'
+import image from '@/assets/images/illustrations/app-modal-default-light.svg'
+import imageDark from '@/assets/images/illustrations/app-modal-default-dark.svg'
 
 export default {
   title: 'Components/AppModal/AppModal',
@@ -11,9 +14,10 @@ export default {
     }
   },
   args: {
+    image,
+    imageDark,
     title: 'Are you sure?',
     default: 'Let’s explain here the consequences of the action if needed so users don’t make any mistake.',
-    image: 'https://i.imgur.com/1eWgPCp.png',
     imageWidth: '60px',
     bodyClass: 'text-secondary-emphasis',
     modelValue: true,
@@ -30,13 +34,19 @@ export default {
   },
   render(args) {
     return {
-      components: { AppModal },
+      components: {
+        AppModal,
+        ImageModeSource
+      },
       template: `
         <button class="btn btn-action" @click="args.modelValue = !args.modelValue">
           Toggle modal
         </button>
         <app-modal v-bind="args" @update:modelValue="args.modelValue = $event">
           {{ args.default }}
+          <template #header-image-source>
+            <image-mode-source :src="args.imageDark" color-mode="dark" />
+          </template>
         </app-modal>
       `,
       data() {
