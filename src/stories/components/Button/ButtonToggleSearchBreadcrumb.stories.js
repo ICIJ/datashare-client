53cd0560038fc20b962e866647fee999513825b6
Pierre Romera <hello@pirhoo.com>
2024-08-13 15:05:48 +0000

    feat: add ButtonToggleSearchBreadcrumb icij/datashare##1516

---
+++ src/stories/components/Button/ButtonToggleSearchBreadcrumb.stories.js
@@ -0,0 +1,13 @@
+import ButtonToggleSearchBreadcrumb from '@/components/Button/ButtonToggleSearchBreadcrumb'
+
+export default {
+  tags: ['autodocs'],
+  title: 'Components/Button/ButtonToggleSearchBreadcrumb',
+  component: ButtonToggleSearchBreadcrumb,
+  args: {
+    loading: false,
+    disabled: false
+  }
+}
+
+export const Default = {}
