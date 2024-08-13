dee49e5da1ca2106f4e744cd7b3c202d38d02c45
Pierre Romera <hello@pirhoo.com>
2024-08-13 15:18:47 +0000

    feat: add compact mode to ButtonToggleAdvancedSearch icij/datashare#1518

--- src/components/Button/ButtonToggleAdvancedSearch.vue
+++ src/components/Button/ButtonToggleAdvancedSearch.vue
@@ -1,11 +1,36 @@
 <script setup>
+import { computed } from 'vue'
+
 import ButtonIcon from '@/components/Button/ButtonIcon'
+import { useBreakpoints } from '@/composables/breakpoints'
+
+const props = defineProps({
+  saved: {
+    type: Boolean
+  },
+  loading: {
+    type: Boolean
+  },
+  compactBreakpoint: {
+    type: String,
+    default: 'md'
+  }
+})
+
+const { breakpointDown } = useBreakpoints()
+
+const compact = computed(() => {
+  return breakpointDown.value[props.compactBreakpoint]
+})
 </script>
 
 <template>
   <button-icon
     class="button-toggle-advanced-search"
     :label="$t('buttonToggleAdvancedSearch.label')"
+    :hide-label="compact"
+    :square="compact"
+    :loading="loading"
     icon-right="eyeglasses"
     variant="outline-tertiary"
   />
