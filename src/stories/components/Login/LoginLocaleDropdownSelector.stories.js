3ec04982cd013955100fe4bd68c5fa3d53aea4d2
Pierre Romera <hello@pirhoo.com>
2024-08-14 10:13:25 +0000

    feat: add LoginLocaleDropdownSelector

---
+++ src/stories/components/Login/LoginLocaleDropdownSelector.stories.js
@@ -0,0 +1,18 @@
+import LoginLocaleDropdownSelector from '@/components/Login/LoginLocaleDropdownSelector'
+
+export default {
+  title: 'Components/Login/LoginLocaleDropdownSelector',
+  component: LoginLocaleDropdownSelector,
+  tags: ['autodocs'],
+  decorators: [
+    () => ({
+      template: `
+        <div class="p-5 text-center">
+          <story />
+        </div>
+      `
+    })
+  ]
+}
+
+export const Default = {}
