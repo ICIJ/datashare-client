97ca4cfcdd23abc2905d289a0610d60812b8bbc4
Caroline Desprat <cdesprat@icij.org>
2024-08-21 16:25:41 +0000

    feat: use form actions for document user actions

--- src/stories/components/Document/DocumentUser/DocumentUserActions/DocumentUserActions.stories.js
+++ src/stories/components/Document/DocumentUser/DocumentUserActions/DocumentUserActions.stories.js
@@ -5,14 +5,13 @@ export default {
   tags: ['autodocs'],
   component: DocumentUserActions,
   args: {
-    dropdown: false,
     compact: false,
     showTags: true,
     showComments: false,
     showRecommended: false,
     showFolders: false,
     showNotes: false,
-    hideLabels: false,
+    shorterLabels: false,
     hideTooltips: false,
     tags: 0,
     comments: 0,
@@ -29,15 +28,25 @@ export const Default = {
     tags: 3
   }
 }
+export const ShorterLabels = {
+  args: {
+    shorterLabels: true,
+    compact: false,
+    showTags: true,
+    tags: 3
+  }
+}
 export const Compact = {
   args: {
     compact: true,
     tags: 3
   }
 }
-export const Complete = {
+
+export const CompleteShorter = {
   args: {
     compact: false,
+    shorterLabels: true,
     showTags: true,
     showComments: true,
     showRecommended: true,
@@ -62,19 +71,11 @@ export const CompleteCompact = {
     comments: 4,
     recommended: 0,
     folders: 5,
-    notes: 3
+    notes: 3,
+    hideTooltips: true
   }
 }
-export const DropdownDefault = {
-  args: {
-    compact: true,
-    dropdown: true,
-    showTags: true,
-    showComments: true,
-    tags: 3,
-    comments: 4
-  }
-}
+
 export const DropdownWithoutTooltip = {
   args: {
     hideTooltips: true,
