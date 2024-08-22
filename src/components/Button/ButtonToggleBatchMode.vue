58158cb82985a07ab30dcd718ade5b32d21712a1
Caroline Desprat <cdesprat@icij.org>
2024-08-22 10:35:08 +0000

    feat: add button download documents #datashare/1525

---
+++ src/components/Button/ButtonToggleBatchMode.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+import ButtonIcon from '@/components/Button/ButtonIcon'
+import { VARIANT } from '@/enums/variants'
+const props = defineProps({
+  active: {
+    type: Boolean,
+    default: false
+  }
+})
+const { t } = useI18n()
+const variant = computed(() => {
+  return props.active ? VARIANT.ACTION : VARIANT.OUTLINE_ACTION
+})
+const label = computed(() => {
+  return props.active ? t('buttonToggleBatchMode.labelSingleFlow') : t('buttonToggleBatchMode.labelBatchMode')
+})
+</script>
+
+<template>
+  <button-icon class="button-toggle-batch-mode" :label="label" icon-left="stack" :variant="variant" square hide-label />
+</template>
---
+++ src/stories/components/Button/ButtonToggleBatchMode.stories.js
@@ -0,0 +1,15 @@
+import ButtonToggleBatchMode from '@/components/Button/ButtonToggleBatchMode'
+
+export default {
+  tags: ['autodocs'],
+  title: 'Components/Button/ButtonToggleBatchMode',
+  component: ButtonToggleBatchMode,
+  args: {
+    active: false
+  }
+}
+
+export const Default = {}
+export const Active = {
+  args: { active: true }
+}
