74716c29dd3aab3048c1909c68b3a64b83ccc314
Caroline Desprat <cdesprat@icij.org>
2024-08-22 10:45:02 +0000

    refactor: rename Sizes to BUTTON_SIZES

--- src/enums/sizes.js
+++ src/enums/sizes.js
@@ -5,5 +5,5 @@ const LG = 'lg'
 const XL = 'xl'
 const XXL = 'xxl'
 export const SIZE = Object.freeze({ XS, SM, MD, LG, XL, XXL })
-export const Sizes = [SM, MD, LG]
+export const BUTTON_SIZES = [SM, MD, LG]
 export const BREAKPOINT_SIZES = Object.values(SIZE)
