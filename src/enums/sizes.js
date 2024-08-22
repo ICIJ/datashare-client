4fd1f3ce1ee94b76f6fe901051e89d1db50a5905
Caroline Desprat <cdesprat@icij.org>
2024-08-22 11:06:29 +0000

    feat: add enum validators

--- src/enums/sizes.js
+++ src/enums/sizes.js
@@ -6,4 +6,7 @@ const XL = 'xl'
 const XXL = 'xxl'
 export const SIZE = Object.freeze({ XS, SM, MD, LG, XL, XXL })
 export const BUTTON_SIZES = [SM, MD, LG]
+export const buttonSizeValidator = (v) => BUTTON_SIZES.includes(v)
+
 export const BREAKPOINT_SIZES = Object.values(SIZE)
+export const breakpointSizeValidator = (v) => BREAKPOINT_SIZES.includes(v)
