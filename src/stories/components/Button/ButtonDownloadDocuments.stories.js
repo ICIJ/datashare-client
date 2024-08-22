28be3d7b32f1d1d5571144a3b1f2f6d8becf7bd2
Caroline Desprat <cdesprat@icij.org>
2024-08-22 09:35:37 +0000

    feat: add button download documents #datashare/1524

---
+++ src/stories/components/Button/ButtonDownloadDocuments.stories.js
@@ -0,0 +1,9 @@
+import ButtonDownloadDocuments from '@/components/Button/ButtonDownloadDocuments'
+
+export default {
+  tags: ['autodocs'],
+  title: 'Components/Button/ButtonDownloadDocuments',
+  component: ButtonDownloadDocuments
+}
+
+export const Default = {}
