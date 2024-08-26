c8a76d627bfa11fbc967bc9498dbf30ca957549b
Pierre Romera <hello@pirhoo.com>
2024-08-26 16:14:27 +0000

    test: stub all child component

--- tests/unit/specs/components/Api.spec.js
+++ tests/unit/specs/components/Api.spec.js
@@ -1,34 +1,32 @@
-import { mount, shallowMount } from '@vue/test-utils'
+import { mount, flushPromises, shallowMount } from '@vue/test-utils'
 import { removeCookie, setCookie } from 'tiny-cookie'
 
-import { flushPromises } from '~tests/unit/tests_utils'
 import ApiPage from '@/components/Api'
 import { getMode, MODE_NAME } from '@/mode'
 import CoreSetup from '~tests/unit/CoreSetup'
+import { render } from 'jsdoc-to-markdown'
 
 describe('Api.vue', () => {
-  let config, plugins, api
-
-  beforeAll(() => {
-    api = {
-      createApiKey: vi.fn(),
-      getApiKey: vi.fn(),
-      deleteApiKey: vi.fn()
-    }
-    const core = CoreSetup.init(api, getMode(MODE_NAME.SERVER)).useAll()
-    config = core.config
-    plugins = core.plugins
-    setCookie(process.env.VITE_DS_COOKIE_NAME, { login: 'doe' }, JSON.stringify)
-  })
+  let plugins, api
 
   beforeEach(() => {
-    config.merge({ mode: MODE_NAME.SERVER })
-    vi.clearAllMocks()
-    api.getApiKey.mockResolvedValue({ hashedKey: null })
+    api = {
+      createApiKey: vi.fn(),
+      getApiKey: vi.fn().mockResolvedValue({ hashedKey: null }),
+      deleteApiKey: vi.fn()
+    }
+
+    const core = CoreSetup.init(api, getMode(MODE_NAME.SERVER)).useAll()
+    plugins = core.plugins
+
+    setCookie(process.env.VITE_DS_COOKIE_NAME, { login: 'doe' }, JSON.stringify)
+    core.config.merge({ mode: MODE_NAME.SERVER })
   })
 
-  afterAll(() => {
+  afterEach(async () => {
     removeCookie(process.env.VITE_DS_COOKIE_NAME)
+    await flushPromises()
+    vi.clearAllMocks()
   })
 
   it('should display a button to generate the API key by default', () => {
@@ -38,15 +36,14 @@ describe('Api.vue', () => {
 
   it('should display no rows by default', () => {
     const wrapper = shallowMount(ApiPage, { global: { plugins } })
-
     expect(wrapper.findAll('.api__key')).toHaveLength(0)
   })
 
   it('should request the creation of the API key', async () => {
-    const wrapper = mount(ApiPage, { global: { plugins } }) // for b-button
+    const wrapper = shallowMount(ApiPage, { global: { plugins } })
     api.getApiKey.mockResolvedValue({ apiKey: '123456abcdef', hashedKey: 'test' })
     api.createApiKey.mockResolvedValue({ apiKey: '123456abcdef', hashedKey: 'test' })
-    await wrapper.find('.api .api__create-key .btn').trigger('click')
+    await wrapper.find('.api .api__create-key b-button-stub').trigger('click')
     await flushPromises()
 
     expect(api.getApiKey).toBeCalledTimes(2)
