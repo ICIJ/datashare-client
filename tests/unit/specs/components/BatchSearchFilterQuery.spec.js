4d7efa05e85c8670ae459b2c44f2526d9c72b8cc
Pierre Romera <hello@pirhoo.com>
2024-08-26 16:41:00 +0000

    fix: remove legacy test

--- tests/unit/specs/components/BatchSearchFilterQuery.spec.js
+++ tests/unit/specs/components/BatchSearchFilterQuery.spec.js
@@ -28,6 +28,7 @@ describe('BatchSearchFilterQuery.vue', () => {
 
   describe('data change on search params changed', () => {
     let router, config
+
     beforeAll(() => {
       config = core.config
       const routes = [
@@ -40,9 +41,11 @@ describe('BatchSearchFilterQuery.vue', () => {
       router = core.router
       plugins = core.plugins
     })
+
     beforeEach(async () => {
       await core.router.replace({ path: '/batch-search', query: {} })
     })
+
     it('search updated on filterByQuery', async () => {
       const wrapper = shallowMount(BatchSearchFilterQuery, { global: { plugins } })
       expect(wrapper.vm.search).toBe('')
@@ -71,6 +74,7 @@ describe('BatchSearchFilterQuery.vue', () => {
       expect(wrapper.vm.$route.query.query).toBe('test')
       expect(wrapper.vm.$route.query.page).toBe('1')
     })
+
     it('gets query from search params', async () => {
       await router.push({
         name: 'task.batch-search.list',
@@ -90,33 +94,10 @@ describe('BatchSearchFilterQuery.vue', () => {
         query: { query: 'search_from_url' }
       })
       const wrapper = mount(BatchSearchFilterQuery, { global: { plugins } })
-
       const searchSpy = vi.spyOn(wrapper.vm, 'filterByQuery')
       expect(searchSpy).not.toBeCalled()
-
-      await wrapper.find('.btn-action').trigger('submit')
-
+      await wrapper.find('button').trigger('submit')
       expect(searchSpy).toBeCalledTimes(1)
-    })
-
-    it('should have author field in server mode in fieldOptions', () => {
-      config.merge({ mode: 'SERVER' })
-      const wrapper = mount(BatchSearchFilterQuery, { global: { plugins } })
-
-      const fields = wrapper.findAll('.search-bar-input-dropdown__option')
-      expect(fields).toHaveLength(4)
-      const field = wrapper.find('.search-bar-input-dropdown__option:nth-child(4)')
-      expect(field.text()).toContain('Author')
-    })
-
-    it('should not have author field in local mode in fieldOptions', () => {
-      config.merge({ mode: 'LOCAL' })
-      const wrapper = mount(BatchSearchFilterQuery, { global: { plugins } })
-
-      const fieldsLOCAL = wrapper.findAll('.search-bar-input-dropdown__option')
-      expect(fieldsLOCAL).toHaveLength(3)
-      const noAuthorField = wrapper.find('.search-bar-input-dropdown__option:nth-child(4)')
-      expect(noAuthorField.exists()).toBeFalsy()
     })
   })
 })
