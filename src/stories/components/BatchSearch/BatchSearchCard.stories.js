5ab0133e6713a5cf6fee18411c094ca3b9ff62f4
Caroline Desprat <cdesprat@icij.org>
2024-08-29 09:42:03 +0000

    story: add batch search without results

--- src/stories/components/BatchSearch/BatchSearchCard.stories.js
+++ src/stories/components/BatchSearch/BatchSearchCard.stories.js
@@ -29,6 +29,22 @@ const batchSearch = {
   description:
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque malesuada ex egestas sem fringilla euismod. Curabitur cursus mattis lectus, a dignissim libero mattis dapibus. Nulla at lectus imperdiet, sollicitudin nunc ut, consectetur sapien. Morbi fermentum dolor ac ultrices commodo. Sed dictum posuere lobortis. Suspendisse hendrerit elit at convallis suscipit. Nullam non massa eu eros tempor varius id id dolor. Vestibulum eu sagittis arcu, nec sagittis turpis. Donec maximus aliquet tortor, a blandit ligula laoreet eu. In quis est sed urna lacinia sagittis ut ac lectus. Morbi ut ipsum et libero dignissim pretium eu ut diam. Vestibulum ante ips'
 }
+const batchSearchNoResults = {
+  uuid: 'aabc',
+  nbResults: 0,
+  nbQueriesWithoutResults: 0,
+  nbQueries: 6,
+  name: 'Richest people in the EU',
+  status: 'success',
+  date: new Date(),
+  author: 'jsmith',
+  visibility: true,
+  phraseMatch: false,
+  proximity: 1,
+  fuzziness: 2,
+  projects: ['banana papers', 'citrus confidential'],
+  description: 'Lorem ipsum dolor sit id doante ips'
+}
 export default {
   decorators: [vueRouter(routes), storeDecoratorPipelineChainByCategory],
   title: 'Components/BatchSearch/BatchSearchCard',
@@ -39,3 +55,8 @@ export default {
   }
 }
 export const Default = {}
+export const NoResults = {
+  args: {
+    batchSearch: batchSearchNoResults
+  }
+}
