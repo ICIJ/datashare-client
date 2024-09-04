7a3daf9b6f17e29d94894ed94674673e64a1f655
Pierre Romera <hello@pirhoo.com>
2024-09-04 08:33:09 +0000

    feat: chage icon hover variant

--- src/components/KeyboardShortcuts/KeyboardShortcutsPopover/KeyboardShortcutsPopoverLink.vue
+++ src/components/KeyboardShortcuts/KeyboardShortcutsPopover/KeyboardShortcutsPopoverLink.vue
@@ -9,8 +9,15 @@ import ButtonIcon from '@/components/Button/ButtonIcon'
     class="keyboard-shortcuts-popover-link"
     icon-right="caret-right"
     icon-right-variant="secondary"
+    icon-right-hover-variant="inherit"
     :to="{ name: 'keyboard-shortcuts' }"
   >
     {{ $t('keyboardShortcutsPopoverLink.label') }}
   </button-icon>
 </template>
+
+<style lang="scss">
+.keyboard-shortcuts-popover-link > .phosphor-icon > svg {
+  @include transition(fill 0.15s ease-in-out);
+}
+</style>
