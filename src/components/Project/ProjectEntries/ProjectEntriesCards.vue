7c610d14d8e24ccd6b8fe3a7f48d9e6cb1ec4fc3
Pierre Romera <hello@pirhoo.com>
2024-09-05 15:09:43 +0000

    feat: add project entries cards and table

---
+++ src/components/Project/ProjectEntries/ProjectEntriesCards.vue
@@ -0,0 +1,19 @@
+<script setup>
+import ProjectCard from '@/components/Project/ProjectCard/ProjectCard'
+
+defineProps({
+  projects: {
+    type: Array
+  }
+})
+</script>
+
+<template>
+  <div class="project-entries-cards container-fluid">
+    <div class="row g-4">
+      <div v-for="project in projects" :key="project.name" class="col-12 col-lg-6 col-xxl-4">
+        <project-card :project="project" class="h-100" />
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/Project/ProjectEntries/ProjectEntriesTableHead.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { useI18n } from 'vue-i18n'
+
+import PageTableTh from '@/components/PageTable/PageTableTh'
+
+const { t } = useI18n()
+</script>
+
+<template>
+  <page-table-th :label="t('projectEntriesTableHead.name')" icon="circles-three-plus" emphasis />
+  <page-table-th :label="t('projectEntriesTableHead.description')" />
+  <page-table-th :label="t('projectEntriesTableHead.documents')" icon="files" number />
+  <page-table-th :label="t('projectEntriesTableHead.updatedOn')" icon="calendar-blank" />
+  <page-table-th :label="t('projectEntriesTableHead.links')" hide-label />
+  <page-table-th :label="t('projectEntriesTableHead.actions')" hide-label />
+</template>
