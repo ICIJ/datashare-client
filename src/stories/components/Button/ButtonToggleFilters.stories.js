54d2fa519109f176eb0975449d5120a6e3b1cb82
Pierre Romera <hello@pirhoo.com>
2024-09-05 10:26:59 +0000

    refactor: rename with "panel" suffix

---
+++ src/stories/components/Button/ButtonToggleFilters.stories.js
@@ -0,0 +1,13 @@
+import ButtonToggleFilters from '@/components/Button/ButtonToggleFilters'
+
+export default {
+  tags: ['autodocs'],
+  title: 'Components/Button/ButtonToggleFilters',
+  component: ButtonToggleFilters,
+  args: {
+    loading: false,
+    active: false
+  }
+}
+
+export const Default = {}
