b10a3abf0ce8a7370d071a4c57ceec0ee364e754
Pierre Romera <hello@pirhoo.com>
2024-09-05 10:28:00 +0000

    feat: add button toggle sidebar

---
+++ src/stories/components/Button/ButtonToggleSidebar.stories.js
@@ -0,0 +1,12 @@
+import ButtonToggleSidebar from '@/components/Button/ButtonToggleSidebar'
+
+export default {
+  tags: ['autodocs'],
+  title: 'Components/Button/ButtonToggleSidebar',
+  component: ButtonToggleSidebar,
+  args: {
+    compact: true
+  }
+}
+
+export const Default = {}
