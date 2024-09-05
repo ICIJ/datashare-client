1c2448f1e8c9fa2c98a68238d764a383a5e46be5
Pierre Romera <hello@pirhoo.com>
2024-09-05 16:07:20 +0000

    refactor: migrate to composition API

--- src/components/Project/ProjectLabel.vue
+++ src/components/Project/ProjectLabel.vue
@@ -1,3 +1,55 @@
+<script setup>
+import { computed } from 'vue'
+import { isObject, startCase } from 'lodash'
+
+import { useCore } from '@/composables/core'
+import ProjectThumbnail from '@/components/Project/ProjectThumbnail'
+
+const props = defineProps({
+  /**
+   * The project to use to generate the thumbnail. Can contain `name`, `label` and `logoUrl` which
+   * will be used to generate the thumbnail consistently. If passed as a string, the project will be
+   * retrieved from the config.
+   */
+  project: {
+    type: [Object, String],
+    required: true
+  },
+  /**
+   * Hide the project thumbnail.
+   */
+  hideThumbnail: {
+    type: Boolean
+  },
+  /**
+   * Size of the project thumbnail
+   */
+  thumbnailWidth: {
+    type: String,
+    default: '1.25em'
+  },
+  /**
+   * Remove the project caption on thumbnail.
+   */
+  noCaption: {
+    type: Boolean
+  }
+})
+
+const { core } = useCore()
+
+const resolvedProject = computed(() => {
+  if (isObject(props.project)) {
+    return core?.findProject(props.project.name) ?? props.project
+  }
+  return core?.findProject(props.project) ?? { name: props.project }
+})
+
+const showThumbnail = computed(() => !props.hideThumbnail)
+
+const projectDisplay = computed(() => resolvedProject.value.label ?? startCase(resolvedProject.value.name))
+</script>
+
 <template>
   <span class="project-label">
     <project-thumbnail
@@ -11,64 +63,6 @@
     <span class="project-label__display">{{ projectDisplay }}</span>
   </span>
 </template>
-
-<script>
-import { isObject, startCase } from 'lodash'
-
-import ProjectThumbnail from '@/components/Project/ProjectThumbnail'
-
-/**
- * Generate a project's label
- */
-export default {
-  name: 'ProjectLabel',
-  components: { ProjectThumbnail },
-  props: {
-    /**
-     * The project to use to generate the thumbnail. Can contain `name`, `label` and `logoUrl` which
-     * will be used to generate the thumbnail consistently. If passed as a string, the project will be
-     * retrieved from the config.
-     */
-    project: {
-      type: [Object, String],
-      required: true
-    },
-    /**
-     * Hide the project thumbnail.
-     */
-    hideThumbnail: {
-      type: Boolean
-    },
-    /**
-     * Size of the project thumbnail
-     */
-    thumbnailWidth: {
-      type: String,
-      default: '1.25em'
-    },
-    /**
-     * Remove the project caption on thumbnail.
-     */
-    noCaption: {
-      type: Boolean
-    }
-  },
-  computed: {
-    projectDisplay() {
-      return this.resolvedProject.label ?? startCase(this.resolvedProject.name)
-    },
-    resolvedProject() {
-      if (isObject(this.project)) {
-        return this.$core?.findProject(this.project.name) ?? this.project
-      }
-      return this.$core?.findProject(this.project) ?? { name: this.project }
-    },
-    showThumbnail() {
-      return !this.hideThumbnail
-    }
-  }
-}
-</script>
 
 <style lang="scss" scoped>
 .project-label {
