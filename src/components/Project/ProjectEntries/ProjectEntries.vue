b41b8e3f7103f78cb559305d5b381e6224c0c6ed
Pierre Romera <hello@pirhoo.com>
2024-09-05 15:09:43 +0000

    feat: add project entries cards and table

---
+++ src/components/Project/ProjectEntries/ProjectEntries.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { computed } from 'vue'
+
+import ProjectEntriesCards from './ProjectEntriesCards'
+import ProjectEntriesTable from './ProjectEntriesTable'
+
+import { LAYOUTS, layoutValidator } from '@/enums/layouts'
+
+const props = defineProps({
+  projects: {
+    type: Array
+  },
+  layout: {
+    type: String,
+    validator: layoutValidator,
+    default: LAYOUTS.CARDS
+  }
+})
+
+const is = computed(() => {
+  return props.layout === LAYOUTS.CARDS ? ProjectEntriesCards : ProjectEntriesTable
+})
+</script>
+
+<template>
+  <div class="project-entries">
+    <component :is="is" :projects="projects" />
+  </div>
+</template>
---
+++ src/components/Project/ProjectEntries/ProjectEntriesTable.vue
@@ -0,0 +1,26 @@
+<script setup>
+import ProjectEntriesTableHead from './ProjectEntriesTableHead'
+import ProjectEntriesTableBody from './ProjectEntriesTableBody'
+
+import PageTable from '@/components/PageTable/PageTable'
+
+defineProps({
+  projects: {
+    type: Array
+  }
+})
+</script>
+<template>
+  <div class="project-entries-table container-fluid">
+    <page-table>
+      <template #colgroup>
+        <col style="min-width: 200px" />
+        <col style="min-width: 200px" />
+      </template>
+      <template #thead>
+        <project-entries-table-head />
+      </template>
+      <project-entries-table-body :projects="projects" />
+    </page-table>
+  </div>
+</template>
