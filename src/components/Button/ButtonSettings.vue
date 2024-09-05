e541a21bbcea27ddd23370a312e238018e5cede0
Pierre Romera <hello@pirhoo.com>
2024-09-05 08:36:35 +0000

    feat: add button settings

---
+++ src/components/Button/ButtonSettings.vue
@@ -0,0 +1,20 @@
+<script setup>
+import ButtonIcon from '@/components/Button/ButtonIcon'
+
+defineProps({
+  label: {
+    type: String
+  }
+})
+</script>
+
+<template>
+  <button-icon
+    :label="label ?? $t('buttonSettings.label')"
+    class="button-add border-0"
+    hide-label
+    icon-left="sliders-horizontal"
+    square
+    variant="outline-secondary"
+  />
+</template>
