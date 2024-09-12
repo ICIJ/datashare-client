9c770387ecffb5861c72584ab1d5d5139ba25e89
Pierre Romera <hello@pirhoo.com>
2024-09-12 15:08:42 +0000

    feat: add new project form

--- src/components/ProjectForm.vue
+++ src/components/ProjectForm.vue
@@ -1,187 +1,164 @@
-<script>
+<script setup>
+import { ref, watch, computed } from 'vue'
 import { every, cloneDeep, kebabCase } from 'lodash'
-import { BCardBody, BCardFooter } from 'bootstrap-vue-next'
 
 import InlineDirectoryPicker from '@/components/InlineDirectoryPicker'
+import ButtonIcon from '@/components/Button/ButtonIcon'
+import FormFieldset from '@/components/Form/FormFieldset/FormFieldset'
 import { slugger, isUrl } from '@/utils/strings'
+import { useCore } from '@/composables/core'
 
-/**
- * Build project form (to create or edit a project).
- */
-export default {
-  name: 'ProjectForm',
-  components: {
-    InlineDirectoryPicker
+// Props
+const props = defineProps({
+  disabled: {
+    type: Boolean
   },
-  props: {
-    /**
-     * Display the form in a card
-     */
-    card: {
-      type: Boolean
-    },
-    /**
-     * Disable all inputs
-     */
-    disabled: {
-      type: Boolean
-    },
-    /**
-     * Default values of the form
-     */
-    values: {
-      type: Object,
-      default: () => ({})
-    },
-    /**
-     * Freeze name and sourcePath in edit mode
-     */
-    edit: {
-      type: Boolean
-    },
-    /**
-     * Show delete project button
-     */
-    showDeleteButton: {
-      type: Boolean
-    }
+  values: {
+    type: Object,
+    default: () => ({})
   },
-  emits: ['submit', 'delete'],
-  data() {
-    return {
-      form: this.initialFormValues()
-    }
+  edit: {
+    type: Boolean
   },
-  computed: {
-    bodyComponent() {
-      return this.card ? BCardBody : 'div'
-    },
-    footerComponent() {
-      return this.card ? BCardFooter : 'div'
-    },
-    classList() {
-      return this.card ? ['card'] : []
-    },
-    valid() {
-      return every([
-        !this.disabled,
-        this.isPresent(this.form.name),
-        this.isPresent(this.form.label) && !this.isReservedWord(this.form.label),
-        !this.isPresent(this.form.sourceUrl) || this.isUrl(this.form.sourceUrl),
-        !this.isPresent(this.form.logoUrl) || this.isUrl(this.form.logoUrl)
-      ])
-    }
-  },
-  watch: {
-    'form.label': function (label) {
-      // The name cannot change when `edit` is set
-      if (!this.edit) {
-        // Transform to kebab case first to insert a "minus" when input is using camel case
-        this.form.name = slugger(kebabCase(label)).toLowerCase()
-      }
-    }
-  },
-  methods: {
-    isReservedWord(value) {
-      const reserved = ['new', 'edit', 'delete']
-      return reserved.includes(value.toLowerCase())
-    },
-    isUrl(value) {
-      return isUrl(value)
-    },
-    isPresent(value) {
-      return value?.trim()?.length > 0
-    },
-    isBlank(value) {
-      return !this.isPresent(value)
-    },
-    submit() {
-      if (this.valid) {
-        this.$emit('submit', cloneDeep(this.form))
-      }
-    },
-    initialFormValues() {
-      return {
-        name: null,
-        label: null,
-        sourcePath: this.$config.get('dataDir'),
-        allowFromMask: '*.*.*.*',
-        description: null,
-        logoUrl: null,
-        sourceUrl: null,
-        publisherName: null,
-        maintainerName: null,
-        // Merge with this property to be able to initialize
-        // the form with an existing project
-        ...this.values
-      }
-    },
-    reset() {
-      this.form = this.initialFormValues()
-    },
-    emitDelete() {
-      this.$emit('delete', this.values.name)
-    }
+  showDeleteButton: {
+    type: Boolean
+  }
+})
+
+const emit = defineEmits(['submit', 'delete'])
+
+const { core } = useCore()
+
+const form = ref(initialFormValues())
+
+function isReservedWord(value) {
+  const reserved = ['new', 'edit', 'delete']
+  return reserved.includes(value.toLowerCase())
+}
+
+function isPresent(value) {
+  return value?.trim()?.length > 0
+}
+
+function isBlank(value) {
+  return !isPresent(value)
+}
+
+function submit() {
+  if (valid.value) {
+    emit('submit', cloneDeep(form.value))
   }
 }
+
+function initialFormValues() {
+  return {
+    name: null,
+    label: null,
+    sourcePath: core.config.get('dataDir'),
+    allowFromMask: '*.*.*.*',
+    description: null,
+    logoUrl: null,
+    sourceUrl: null,
+    publisherName: null,
+    maintainerName: null,
+    ...props.values
+  }
+}
+
+function reset() {
+  form.value = initialFormValues()
+}
+
+function emitDelete() {
+  emit('delete', props.values.name)
+}
+
+const valid = computed(() => {
+  return every([
+    !props.disabled,
+    isPresent(form.value.name),
+    isPresent(form.value.label) && !isReservedWord(form.value.label),
+    !isPresent(form.value.sourceUrl) || isUrl(form.value.sourceUrl),
+    !isPresent(form.value.logoUrl) || isUrl(form.value.logoUrl)
+  ])
+})
+
+watch(
+  () => form.value.label,
+  (label) => {
+    if (!props.edit) {
+      form.value.name = slugger(kebabCase(label)).toLowerCase()
+    }
+  }
+)
 </script>
 
 <template>
-  <b-form class="project-form" :class="classList" novalidate @submit.stop.prevent="submit">
-    <component :is="bodyComponent">
-      <b-form-row>
-        <b-col>
-          <b-form-group
-            class="project-form__group project-form__group--label"
-            :invalid-feedback="$t('projectForm.form.label.invalidFeedback', { label: form.label })"
-            :label="$t('projectForm.form.label.label')"
-            :description="$t('projectForm.form.label.description')"
-            :disabled="disabled"
-            :validated="isPresent(form.label) && !isReservedWord(form.label)"
-          >
-            <b-form-input
-              v-model="form.label"
-              name="label"
-              type="text"
-              placeholder=""
-              required
-              :state="isBlank(form.label) ? null : !isReservedWord(form.label)"
-            />
-          </b-form-group>
-        </b-col>
-        <b-col>
-          <b-form-group
-            :label="$t('projectForm.form.name.label')"
-            :description="$t('projectForm.form.name.description')"
-            :disabled="disabled"
-          >
-            <b-form-input v-model="form.name" name="name" type="text" placeholder="" required readonly />
-          </b-form-group>
-        </b-col>
-      </b-form-row>
-      <b-form-group
+  <b-form class="project-form" novalidate @submit.stop.prevent="submit">
+    <div>
+      <form-fieldset
+        class="project-form__group project-form__group--label"
+        icon="text-aa"
+        required
+        :label="$t('projectForm.form.label.label')"
+        :invalid-feedback="$t('projectForm.form.label.invalidFeedback', { label: form.label })"
+        :disabled="disabled"
+        :validated="isPresent(form.label) && !isReservedWord(form.label)"
+      >
+        <b-form-input
+          v-model="form.label"
+          name="label"
+          type="text"
+          required
+          :placeholder="$t('projectForm.form.label.placeholder')"
+          :state="isBlank(form.label) ? null : !isReservedWord(form.label)"
+        />
+      </form-fieldset>
+      <form-fieldset
         class="project-form__group project-form__group--name"
+        icon="text-aa"
+        :label="$t('projectForm.form.name.label')"
+        :disabled="disabled"
+      >
+        <b-form-input
+          v-model="form.name"
+          name="name"
+          type="text"
+          :placeholder="$t('projectForm.form.name.placeholder')"
+          required
+          readonly
+        />
+      </form-fieldset>
+      <form-fieldset
+        class="project-form__group project-form__group--description"
+        icon="text-align-left"
         :label="$t('projectForm.form.description.label')"
-        :description="$t('projectForm.form.description.description')"
         :disabled="disabled"
         :validated="isPresent(form.description)"
       >
-        <b-form-textarea v-model="form.description" name="description" placeholder="" rows="3" max-rows="8" />
-      </b-form-group>
-      <b-form-group
+        <b-form-textarea
+          v-model="form.description"
+          name="description"
+          :placeholder="$t('projectForm.form.description.placeholder')"
+          rows="3"
+          max-rows="8"
+        />
+      </form-fieldset>
+      <form-fieldset
         v-if="!edit"
         class="project-form__group project-form__group--source-path"
+        required
+        icon="folder-open"
         :label="$t('projectForm.form.sourcePath.label')"
-        :description="$t('projectForm.form.sourcePath.description')"
         :disabled="disabled"
         :validated="isPresent(form.sourcePath)"
       >
         <inline-directory-picker v-model:path="form.sourcePath" />
-      </b-form-group>
-      <b-form-group
+      </form-fieldset>
+      <form-fieldset
         class="project-form__group project-form__group--logo-url"
+        icon="ilage"
         :label="$t('projectForm.form.logoUrl.label')"
-        :description="$t('projectForm.form.logoUrl.description')"
         :disabled="disabled"
         :validated="isUrl(form.logoUrl)"
       >
@@ -190,50 +167,54 @@ export default {
           :state="isBlank(form.logoUrl) ? null : isUrl(form.logoUrl)"
           name="logoUrl"
           type="url"
-          placeholder="https://..."
+          :placeholder="$t('projectForm.form.logoUrl.placeholder')"
         />
-      </b-form-group>
-      <b-form-group
+      </form-fieldset>
+      <form-fieldset
         class="project-form__group project-form__group--maintainer-name"
+        icon="user-square"
         :label="$t('projectForm.form.maintainerName.label')"
-        :description="$t('projectForm.form.maintainerName.description')"
         :disabled="disabled"
         :validated="isPresent(form.maintainerName)"
       >
-        <b-form-input v-model="form.maintainerName" name="maintainerName" type="text" placeholder="" />
-      </b-form-group>
-      <b-form-row>
-        <b-col>
-          <b-form-group
-            class="project-form__group project-form__group--publisher-name"
-            :label="$t('projectForm.form.publisherName.label')"
-            :description="$t('projectForm.form.publisherName.description')"
-            :disabled="disabled"
-            :validated="isPresent(form.publisherName)"
-          >
-            <b-form-input v-model="form.publisherName" name="publisherName" type="text" placeholder="" />
-          </b-form-group>
-        </b-col>
-        <b-col>
-          <b-form-group
-            class="project-form__group project-form__group--source-url"
-            :label="$t('projectForm.form.sourceUrl.label')"
-            :description="$t('projectForm.form.sourceUrl.description')"
-            :disabled="disabled"
-            :validated="isUrl(form.sourceUrl)"
-          >
-            <b-form-input
-              v-model="form.sourceUrl"
-              name="sourceUrl"
-              type="url"
-              placeholder="https://..."
-              :state="isBlank(form.sourceUrl) ? null : isUrl(form.sourceUrl)"
-            />
-          </b-form-group>
-        </b-col>
-      </b-form-row>
-    </component>
-    <component :is="footerComponent" class="d-flex">
+        <b-form-input
+          v-model="form.maintainerName"
+          name="maintainerName"
+          type="text"
+          :placeholder="$t('projectForm.form.maintainerName.placeholder')"
+        />
+      </form-fieldset>
+      <form-fieldset
+        class="project-form__group project-form__group--publisher-name"
+        icon="user-square"
+        :label="$t('projectForm.form.publisherName.label')"
+        :disabled="disabled"
+        :validated="isPresent(form.publisherName)"
+      >
+        <b-form-input
+          v-model="form.publisherName"
+          name="publisherName"
+          type="text"
+          :placeholder="$t('projectForm.form.publisherName.placeholder')"
+        />
+      </form-fieldset>
+      <form-fieldset
+        class="project-form__group project-form__group--source-url"
+        icon="link"
+        :label="$t('projectForm.form.sourceUrl.label')"
+        :disabled="disabled"
+        :validated="isUrl(form.sourceUrl)"
+      >
+        <b-form-input
+          v-model="form.sourceUrl"
+          name="sourceUrl"
+          type="url"
+          :placeholder="$t('projectForm.form.sourceUrl.placeholder')"
+          :state="isBlank(form.sourceUrl) ? null : isUrl(form.sourceUrl)"
+        />
+      </form-fieldset>
+    </div>
+    <div class="d-flex">
       <confirm-button
         v-if="showDeleteButton"
         type="button"
@@ -244,17 +225,19 @@ export default {
         <fa icon="trash-can" class="me-1" />
         <slot name="delete-text">{{ $t('projectForm.delete') }}</slot>
       </confirm-button>
-      <confirm-button
+      <button-icon
         type="button"
+        variant="outline-light"
+        icon-left="arrow-counter-clockwise"
         class="project-form__action--reset btn btn-outline-action ms-auto"
-        :confirmed="reset"
-        :label="$t('projectForm.resetConfirmation')"
+        :label="$t('projectForm.reset')"
+        @click="reset"
       >
-        <slot name="reset-text">{{ $t('projectForm.reset') }}</slot>
-      </confirm-button>
+        <slot name="reset-text" />
+      </button-icon>
       <b-button type="submit" variant="action" class="ms-2" :disabled="!valid">
         <slot name="submit-text">{{ $t('projectForm.submit') }}</slot>
       </b-button>
-    </component>
+    </div>
   </b-form>
 </template>
