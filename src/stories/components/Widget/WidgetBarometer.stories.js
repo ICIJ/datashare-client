d67776bb6d88b86d08e87dc94750b6950f4c785f
Pierre Romera <hello@pirhoo.com>
2024-09-25 15:41:13 +0000

    refactor: ensure widget barometer is atomic

--- src/stories/components/Widget/WidgetBarometer.stories.js
+++ src/stories/components/Widget/WidgetBarometer.stories.js
@@ -1,43 +1,8 @@
+import WidgetBarometer from '@/components/Widget/WidgetBarometer'
 import { variantsArgType } from '~storybook/utils'
 import { getCategoryIcon } from '@/utils/entity'
 import { ENTITY_CATEGORY } from '@/enums/entityCategories'
 import { VARIANT } from '@/enums/variants'
-import WidgetBarometer from '@/components/Widget/WidgetBarometer'
-import WidgetBarometerEntry from '@/components/Widget/WidgetBarometerEntry'
-import WidgetBarometerEntryDocuments from '@/components/Widget/WidgetBarometerEntryDocuments'
-import WidgetBarometerEntryDiskUsage from '@/components/Widget/WidgetBarometerEntryDiskUsage'
-const documents = {
-  nbDocuments: 43,
-  nbDocumentsOnDisks: 123
-}
-const diskUsage = {
-  size: 300050005050
-}
-const persons = {
-  icon: getCategoryIcon(ENTITY_CATEGORY.PERSON),
-  label: 'Persons',
-  value: 9000,
-  variant: VARIANT.CATEGORY_PERSON
-}
-const organizations = {
-  icon: getCategoryIcon(ENTITY_CATEGORY.ORGANIZATION),
-  label: 'Organizations',
-  value: 9000,
-  variant: VARIANT.CATEGORY_ORGANIZATION
-}
-const locations = {
-  icon: getCategoryIcon(ENTITY_CATEGORY.LOCATION),
-  label: 'Locations',
-  value: 9000,
-  variant: VARIANT.CATEGORY_LOCATION
-}
-const emails = {
-  icon: getCategoryIcon(ENTITY_CATEGORY.EMAIL),
-  label: 'Emails',
-  value: 9000,
-  variant: VARIANT.CATEGORY_EMAIL
-}
-const args = { documents, diskUsage, persons, organizations, locations, emails }
 
 export default {
   title: 'Components/Widget/WidgetBarometer',
@@ -46,27 +11,57 @@ export default {
   argTypes: {
     variant: variantsArgType
   },
-  args,
-  render: (args) => ({
-    components: {
-      WidgetBarometer,
-      WidgetBarometerEntry,
-      WidgetBarometerEntryDocuments,
-      WidgetBarometerEntryDiskUsage
-    },
-    setup() {
-      return { args }
-    },
-    template: `
-      <widget-barometer>
-        <WidgetBarometerEntryDocuments  v-bind="args.documents"/>
-        <WidgetBarometerEntryDiskUsage  v-bind="args.diskUsage"/>
-        <WidgetBarometerEntry  v-bind="args.persons"/>
-        <WidgetBarometerEntry v-bind="args.organizations"/>
-        <WidgetBarometerEntry v-bind="args.locations"/>
-        <WidgetBarometerEntry v-bind="args.emails"/>
-      </widget-barometer>
-    `
-  })
+  args: {
+    icon: 'floppy-disk',
+    label: 'records',
+    value: 30000,
+    variant: 'action'
+  },
+  decorators: [
+    () => ({
+      template: '<div class="bg-tertiary-subtle rounded-1 p-4"><story /></div>'
+    })
+  ]
 }
+
 export const Default = {}
+
+export const ForPeople = {
+  args: {
+    icon: getCategoryIcon(ENTITY_CATEGORY.PERSON),
+    label: 'Persons',
+    value: 9000,
+    variant: VARIANT.CATEGORY_PERSON,
+    borderVariant: VARIANT.CATEGORY_PERSON
+  }
+}
+
+export const ForOrganization = {
+  args: {
+    icon: getCategoryIcon(ENTITY_CATEGORY.ORGANIZATION),
+    label: 'Organizations',
+    value: 9000,
+    variant: VARIANT.CATEGORY_ORGANIZATION,
+    borderVariant: VARIANT.CATEGORY_ORGANIZATION
+  }
+}
+
+export const ForLocation = {
+  args: {
+    icon: getCategoryIcon(ENTITY_CATEGORY.LOCATION),
+    label: 'Locations',
+    value: 9000,
+    variant: VARIANT.CATEGORY_LOCATION,
+    borderVariant: VARIANT.CATEGORY_LOCATION
+  }
+}
+
+export const ForEmail = {
+  args: {
+    icon: getCategoryIcon(ENTITY_CATEGORY.EMAIL),
+    label: 'Emails',
+    value: 9000,
+    variant: VARIANT.CATEGORY_EMAIL,
+    borderVariant: VARIANT.CATEGORY_EMAIL
+  }
+}
