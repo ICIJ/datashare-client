9ed04432ad9c58c3f0e53d05f1591289a45710c3
Caroline Desprat <cdesprat@icij.org>
2024-09-26 08:32:25 +0000

    feat: create settings pages

---
+++ src/views/Settings/SettingsViewDocumentProcessing.vue
@@ -0,0 +1,12 @@
+<script setup>
+/**
+ * A list of settings for the document processing.
+ */
+
+defineOptions({ name: 'SettingsViewDocumentProcessing' })
+</script>
+<template>
+  <div class="settings-view-document-processing my-4">settings-view-document-processing</div>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ src/views/Settings/SettingsViewPlugins.vue
@@ -0,0 +1,12 @@
+<script setup>
+/**
+ * A list of plugins for the backend.
+ */
+
+defineOptions({ name: 'SettingsViewPlugins' })
+</script>
+<template>
+  <div class="settings-view-plugins my-4">settings-view-plugins</div>
+</template>
+
+<style scoped lang="scss"></style>
