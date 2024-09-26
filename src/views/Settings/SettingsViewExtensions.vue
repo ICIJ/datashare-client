2e484bcb4454c07f610ec21a676c9d0dc02251ce
Caroline Desprat <cdesprat@icij.org>
2024-09-26 08:32:25 +0000

    feat: create settings pages

---
+++ src/views/Settings/SettingsViewExtensions.vue
@@ -0,0 +1,12 @@
+<script setup>
+/**
+ * A list of extensions for the frontend.
+ */
+
+defineOptions({ name: 'SettingsViewExtensions' })
+</script>
+<template>
+  <div class="settings-view-extensions my-4">settings-view-extensions</div>
+</template>
+
+<style scoped lang="scss"></style>
