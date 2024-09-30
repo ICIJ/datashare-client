76f795c48450bd7170023000fac818ebce9c6bdc
Caroline Desprat <cdesprat@icij.org>
2024-09-30 14:01:44 +0000

    feat: create addon url input

---
+++ src/stories/components/Addon/AddonUrlInput.stories.js
@@ -0,0 +1,13 @@
+import AddonUrlInput from '@/components/Addon/AddonUrlInput'
+
+export default {
+  tags: ['autodocs'],
+  title: 'Components/Addon/AddonUrlInput',
+  component: AddonUrlInput,
+  args: {
+    modelValue: 'http://localhost:8080',
+    loading: false
+  }
+}
+
+export const Default = {}
