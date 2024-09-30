b6425843a89c7e21404a53bdd74d1a587e2a9b45
Caroline Desprat <cdesprat@icij.org>
2024-09-30 16:21:56 +0000

    feat: create a unified way of adding toast and wait to promises

--- src/composables/core.js
+++ src/composables/core.js
@@ -1,4 +1,5 @@
-import { getCurrentInstance } from 'vue'
+import { getCurrentInstance, ref } from 'vue'
+import uniqueId from 'lodash/uniqueId'
 
 export function useCore() {
   // `getCurrentInstance` is a Vue Composition API function that gives us access to the current component instance.
@@ -15,6 +16,36 @@ export function useCore() {
   // This `proxy` allows us to access properties like `$core` and `$toast` that are globally provided by the "core" plugin.
   const { proxy } = instance
 
+  function useApi(promise, { toast: { successMessage, errorMessage }, isLoading, waitLoaderId }) {
+    const data = ref(null)
+    const error = ref(null)
+    const loadingPromise = ref()
+    const loaderId = waitLoaderId ?? uniqueId('waiter')
+    const toastedPromise = () => {
+      proxy.$wait.start(loaderId)
+      isLoading.value = true
+      loadingPromise.value = promise
+        .then((res) => {
+          data.value = res
+          if (successMessage) {
+            proxy.$toast.success(successMessage)
+          }
+        })
+        .catch((err) => {
+          error.value = err
+          if (errorMessage) {
+            proxy.$toast.error(errorMessage)
+          }
+        })
+        .finally(() => {
+          proxy.$wait.end(loaderId)
+          isLoading.value = false
+        })
+    }
+    toastedPromise()
+    return { data, error, loadingPromise, isLoading, loaderId }
+  }
+
   // We return an object with the global `$core` and `$toast` properties.
   // These properties can then be destructured and used in any component that calls `useCore`.
   return {
@@ -23,6 +54,7 @@ export function useCore() {
     // `proxy.$toast` gives us access to the global `$toast` object provided by the "core" plugin.
     toast: proxy.$toast,
     // `proxy.$wait` gives us access to the global `$wait` object provided by the "core" plugin
-    wait: proxy.$wait
+    wait: proxy.$wait,
+    useApi
   }
 }
