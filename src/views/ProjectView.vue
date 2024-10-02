bbe42a421f350e14be3d186be0c313f0367d79da
Pierre Romera <hello@pirhoo.com>
2024-10-02 11:33:03 +0000

    feat: add search bar

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -1,8 +1,21 @@
 <script setup>
+import { computed } from 'vue'
+
 import PageContainer from '@/components/PageContainer/PageContainer'
 import PageHeader from '@/components/PageHeader/PageHeader'
+import SearchBar from '@/components/Search/SearchBar/SearchBar'
 import TabGroupNavigation from '@/components/TabGroup/TabGroupNavigation/TabGroupNavigation'
 import TabGroupNavigationEntry from '@/components/TabGroup/TabGroupNavigation/TabGroupNavigationEntry'
+
+const indices = computed(() => {
+  return [props.name]
+})
+
+const props = defineProps({
+  name: {
+    type: String
+  }
+})
 </script>
 
 <template>
@@ -10,7 +23,8 @@ import TabGroupNavigationEntry from '@/components/TabGroup/TabGroupNavigation/Ta
     <page-header no-toggle-settings />
     <page-container fluid>
       <div class="bg-tertiary-subtle rounded-1 p-4">
-        <tab-group-navigation>
+        <search-bar class="my-4 py-3 mx-3" size="lg" :indices="indices" hide-field-dropdown hide-projects-dropdown />
+        <tab-group-navigation class="mx-3">
           <tab-group-navigation-entry icon="chart-bar" :to="{ name: 'project.view.insights' }">
             {{ $t('projectView.nav.insights') }}
           </tab-group-navigation-entry>
@@ -27,7 +41,7 @@ import TabGroupNavigationEntry from '@/components/TabGroup/TabGroupNavigation/Ta
             {{ $t('projectView.nav.history') }}
           </tab-group-navigation-entry>
         </tab-group-navigation>
-        <router-view keep-alive />
+        <router-view />
       </div>
     </page-container>
   </div>
