cc04bc8c695cf81055b13628db282c751f4761c1
Pierre Romera <hello@pirhoo.com>
2024-10-02 10:26:37 +0000

    refactor: rename to WidgetDocuments

--- src/store/widgets/WidgetDocuments.js
+++ src/store/widgets/WidgetDocuments.js
@@ -1,14 +1,14 @@
 import WidgetEmpty from './WidgetEmpty'
 
-import Component from '@/components/Widget/WidgetFileBarometer'
+import Component from '@/components/Widget/WidgetDocuments'
 
 /**
  * Widget to display the number of indexed files on the insights page
  */
-class WidgetFileBarometer extends WidgetEmpty {
+class WidgetDocuments extends WidgetEmpty {
   get component() {
     return Component
   }
 }
 
-export default WidgetFileBarometer
+export default WidgetDocuments
