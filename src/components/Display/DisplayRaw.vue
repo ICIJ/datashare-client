63f49ef2554ae307dea2ab3383f1ad660b5ad168
Pierre Romera <hello@pirhoo.com>
2024-10-03 15:14:20 +0000

    feat: raw value display

---
+++ src/components/Display/DisplayRaw.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps({
+  value: {
+    type: [String, Number, Boolean, Date]
+  }
+})
+</script>
+
+<template>
+  <span>
+    <slot>{{ value }}</slot>
+  </span>
+</template>
