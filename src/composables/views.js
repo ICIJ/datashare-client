47f9ab12e6302d6f42882b0cf28c278b0c660029
Pierre Romera <hello@pirhoo.com>
2024-10-07 10:12:30 +0000

    feat: add extra var

--- src/composables/views.js
+++ src/composables/views.js
@@ -4,20 +4,26 @@ import { useStore } from 'vuex'
 export function useViews() {
   const store = useStore()
 
+  const isSidebarClosed = computed(() => store?.state.app.sidebar.closed)
+
   const toggleSidebar = computed({
-    get: () => !store?.state.app.sidebar.closed,
+    get: () => !isSidebarClosed.value,
     set: (value) => store?.dispatch('app/toggleSidebarClosed', !value)
   })
 
+  const isSettingsClosed = computed(() => store?.state.app.settings.closed)
+
   const toggleSettings = computed({
-    get: () => !store?.state.app.settings.closed,
+    get: () => !isSettingsClosed.value,
     set: (value) => store?.dispatch('app/toggleSettingsClosed', !value)
   })
 
+  const isFiltersClosed = computed(() => store?.state.app.filters.closed)
+
   const toggleFilters = computed({
-    get: () => !store?.state.app.filters.closed,
+    get: () => !isFiltersClosed.value,
     set: (value) => store?.dispatch('app/toggleFiltersClosed', !value)
   })
 
-  return { toggleSidebar, toggleSettings, toggleFilters }
+  return { isSidebarClosed, toggleSidebar, isSettingsClosed, toggleSettings, isFiltersClosed, toggleFilters }
 }
