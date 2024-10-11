75b8b914661cc99ad7ca03cd45c391b486ad0eaf
Pierre Romera <hello@pirhoo.com>
2024-10-11 10:03:00 +0000

    feat: use compact mode

--- src/components/Project/ProjectEntries/ProjectEntriesTableBody.vue
+++ src/components/Project/ProjectEntries/ProjectEntriesTableBody.vue
@@ -4,9 +4,19 @@ import ProjectRow from '@/components/Project/ProjectRow/ProjectRow'
 defineProps({
   projects: {
     type: Array
+  },
+  compactBreakpoint: {
+    type: String,
+    default: 'md'
   }
 })
 </script>
+
 <template>
-  <project-row v-for="project in projects" :key="project.name" :project="project" />
+  <project-row
+    v-for="project in projects"
+    :key="project.name"
+    :project="project"
+    :compact-breakpoint="compactBreakpoint"
+  />
 </template>
--- src/components/Project/ProjectEntries/ProjectEntriesTableHead.vue
+++ src/components/Project/ProjectEntries/ProjectEntriesTableHead.vue
@@ -1,15 +1,30 @@
 <script setup>
+import { computed } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { PhCirclesThreePlus, PhFiles, PhCalendarBlank } from '@phosphor-icons/vue'
 
 import PageTableTh from '@/components/PageTable/PageTableTh'
+import { useBreakpoints } from '@/composables/breakpoints'
+
+const { breakpointDown } = useBreakpoints()
+
+const props = defineProps({
+  compactBreakpoint: {
+    type: String,
+    default: 'md'
+  }
+})
 
 const { t } = useI18n()
+
+const compact = computed(() => {
+  return breakpointDown.value[props.compactBreakpoint]
+})
 </script>
 
 <template>
   <page-table-th :label="t('projectEntriesTableHead.name')" :icon="PhCirclesThreePlus" emphasis sortable name="name" />
-  <page-table-th :label="t('projectEntriesTableHead.description')" />
+  <page-table-th v-if="!compact" :label="t('projectEntriesTableHead.description')" />
   <page-table-th
     :label="t('projectEntriesTableHead.documents')"
     :icon="PhFiles"
