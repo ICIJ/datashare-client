ec4b7dffdca1a53a62a9800bf1df85330ae286f5
Pierre Romera <hello@pirhoo.com>
2024-10-11 22:29:32 +0000

    feat: add grid layout

--- src/enums/layouts.js
+++ src/enums/layouts.js
@@ -1,5 +1,6 @@
-const TABLE = 'table'
 const GRID = 'grid'
+const LIST = 'list'
+const TABLE = 'table'
 
-export const LAYOUTS = Object.freeze({ TABLE, GRID })
+export const LAYOUTS = Object.freeze({ LIST, TABLE, GRID })
 export const layoutValidator = (l) => Object.values(LAYOUTS).includes(l)
