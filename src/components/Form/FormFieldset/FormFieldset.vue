2ada3433445a20f2870d0bec3bc9a6e1a816f348
Caroline Desprat <cdesprat@icij.org>
2024-10-16 13:53:18 +0000

    feat: add compact and compact auto mode for form fieldset

--- src/components/Form/FormFieldset/FormFieldset.vue
+++ src/components/Form/FormFieldset/FormFieldset.vue
@@ -3,6 +3,8 @@ import { computed } from 'vue'
 import { PhosphorIcon } from '@icij/murmur-next'
 
 import { VARIANT_PLAIN, variantValidator } from '@/enums/variants'
+import { useBreakpoints } from '@/composables/breakpoints'
+import { breakpointSizeValidator, SIZE } from '@/enums/sizes'
 
 const props = defineProps({
   label: {
@@ -31,11 +33,29 @@ const props = defineProps({
   labelFor: {
     type: String
   },
+  compact: { type: Boolean, default: false },
+  description: { type: String },
   required: {
     type: Boolean
+  },
+  compactAuto: {
+    type: Boolean,
+    default: false
+  },
+  compactAutoBreakpoint: {
+    type: String,
+    default: SIZE.LG,
+    validator: breakpointSizeValidator
   }
 })
+const { breakpointDown } = useBreakpoints()
 
+const isCompact = computed(() => {
+  // If compactAuto is true, use the compactAutoBreakpoint value to determine if the
+  // form actions should be compact. This is done through the reactive breakpointDown value.
+  // Alternatively, if compactAuto is false, use the compact prop value.
+  return (props.compactAuto && breakpointDown.value[props.compactAutoBreakpoint]) || props.compact
+})
 const classList = computed(() => {
   return {
     'form-fieldset--required': props.required
@@ -52,16 +72,25 @@ const classList = computed(() => {
     :label-cols-lg="labelColsLg"
     :label="label"
     :label-for="labelFor"
+    :description="isCompact ? description : null"
   >
     <template #label>
-      <div class="form-fieldset__label text-secondary-emphasis">
+      <div class="form-fieldset__label text-body-emphasis">
         <phosphor-icon v-if="icon" :name="icon" :variant="iconVariant" class="me-2" />
         <slot name="label" v-bind="{ labelFor }">
           {{ label }}
         </slot>
       </div>
     </template>
-    <slot />
+    <template v-if="!isCompact">
+      <div class="col-4">
+        <slot />
+      </div>
+      <span class="form-fieldset__description-side text-secondary-emphasis">{{ description }}</span>
+    </template>
+    <template v-else>
+      <slot />
+    </template>
   </b-form-group>
 </template>
 
@@ -71,6 +100,14 @@ const classList = computed(() => {
 
   &--required &__label:after {
     content: ' *';
+  }
+  & .col:has(.form-fieldset__description-side) {
+    display: flex;
+    gap: 1em;
+  }
+  &__description-side {
+    display: flex;
+    align-items: center;
   }
 }
 </style>
