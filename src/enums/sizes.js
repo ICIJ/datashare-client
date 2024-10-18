41e05de191155d19a9c00109fbeefb7037c58321
Pierre Romera <hello@pirhoo.com>
2024-10-18 10:58:27 +0000

    feat: add input size to enums

--- src/enums/sizes.js
+++ src/enums/sizes.js
@@ -8,5 +8,8 @@ export const SIZE = Object.freeze({ XS, SM, MD, LG, XL, XXL })
 export const BUTTON_SIZES = [SM, MD, LG]
 export const buttonSizeValidator = (v) => BUTTON_SIZES.includes(v)
 
+export const INPUT_SIZES = [SM, MD, LG]
+export const inputSizeValidator = (v) => INPUT_SIZES.includes(v)
+
 export const BREAKPOINT_SIZES = Object.values(SIZE)
 export const breakpointSizeValidator = (v) => BREAKPOINT_SIZES.includes(v)
