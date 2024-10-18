0e29711d6bc289adc5fb785ea84ee8747b20808a
Pierre Romera <hello@pirhoo.com>
2024-10-18 15:27:45 +0000

    refactor: delete legacy tests

---
+++ src/mixins/filters.js
@@ -0,0 +1,47 @@
+import { get, last } from 'lodash'
+
+import utils from '@/mixins/utils'
+
+export default {
+  mixins: [utils],
+  methods: {
+    labelToHuman(label) {
+      if (this.$te(label)) {
+        return this.$t(label)
+      } else if (this.$te('global.' + label)) {
+        return this.$t('global.' + label)
+      } else if (this.$te('filter.' + label)) {
+        return this.$t('filter.' + label)
+      } else {
+        return this.translationKeyToHuman(label)
+      }
+    },
+    translationKeyToHuman(label = '') {
+      return last(String(label).split('.'))
+    },
+    getFilterByName(name) {
+      return this.$store.getters['search/getFilter']({ name })
+    },
+    getFilterValuesByName(name) {
+      return get(this, `$store.state.search.values.${name}`, [])
+    },
+    setFilterValue(filter, item) {
+      this.$store.commit('search/setFilterValue', filter.itemParam(item))
+    },
+    removeFilterValue({ name }) {
+      this.$store.commit('search/removeFilter', name)
+    },
+    async refreshRouteAndSearch() {
+      await this.refreshRoute()
+      await this.refreshSearch()
+    },
+    refreshRoute() {
+      const name = 'search'
+      const query = this.$store.getters['search/toRouteQuery']()
+      return this.$router.push({ name, query }).catch(() => {})
+    },
+    refreshSearch() {
+      return this.$store.dispatch('search/query')
+    }
+  }
+}
