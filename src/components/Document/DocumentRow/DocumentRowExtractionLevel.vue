71ac7fb184a706b759e40edb2ce4be8cab58e702
Pierre Romera <hello@pirhoo.com>
2024-10-22 14:03:45 +0000

    feat: add document row

---
+++ src/components/Document/DocumentRow/DocumentRowExtractionLevel.vue
@@ -0,0 +1,15 @@
+<script setup>
+import DisplayExtractionLevel from '@/components/Display/DisplayExtractionLevel'
+
+defineProps({
+  document: {
+    type: Object
+  }
+})
+</script>
+
+<template>
+  <td>
+    <display-extraction-level :value="document.extractionLevel" />
+  </td>
+</template>
---
+++ src/composables/search-settings.js
@@ -0,0 +1,55 @@
+import { useI18n } from 'vue-i18n'
+
+export function useSearchSettings () {
+  const { t } = useI18n()
+
+  const propertiesOrder = [
+    'thumbnail',
+    'title',
+    'highlights',
+    'author',
+    'path',
+    'tags',
+    'contentLength',
+    'contentTextLength',
+    'contentType',
+    'creationDate',
+    'extractionLevel',
+    'language',
+    'project'
+  ]
+
+  const propertiesLabel = {
+    thumbnail: t('documentRow.properties.thumbnail'),
+    title: t('documentRow.properties.title'),
+    highlights: t('documentRow.properties.highlights'),
+    author: t('documentRow.properties.author'),
+    path: t('documentRow.properties.path'),
+    tags: t('documentRow.properties.tags'),
+    contentLength: t('documentRow.properties.contentLength'),
+    contentTextLength: t('documentRow.properties.contentTextLength'),
+    contentType: t('documentRow.properties.contentType'),
+    creationDate: t('documentRow.properties.creationDate'),
+    extractionLevel: t('documentRow.properties.extractionLevel'),
+    language: t('documentRow.properties.language'),
+    project: t('documentRow.properties.project')
+  }
+
+  const propertiesIcon = {
+    thumbnail: null,
+    title: 'file-text',
+    highlights: 'quotes',
+    author: 'user-circle',
+    path: 'tree-structure',
+    tags: 'hash',
+    contentLength: 'file',
+    contentTextLength: 'text-columns',
+    contentType: 'file-text',
+    creationDate: 'calendar',
+    extractionLevel: 'note-blank',
+    language: 'globe-hemisphere-west',
+    project: 'circles-three-plus'
+  }
+
+  return { propertiesOrder, propertiesLabel, propertiesIcon }
+}
