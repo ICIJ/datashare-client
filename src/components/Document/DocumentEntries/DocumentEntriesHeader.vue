f048b9f39d92da3987efafb5903ee294434e9690
Pierre Romera <hello@pirhoo.com>
2024-10-23 17:39:04 +0000

    feat: disable select mode when no documents

--- src/components/Document/DocumentEntries/DocumentEntriesHeader.vue
+++ src/components/Document/DocumentEntries/DocumentEntriesHeader.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed, useTemplateRef } from 'vue'
+import { computed, toRef, useTemplateRef, watch } from 'vue'
 import { TinyPagination } from '@icij/murmur-next'
 
 import useResizeObserver from '@/composables/resize-observer'
@@ -31,11 +31,13 @@ const elementRef = useTemplateRef('element')
 const { state: elementState } = useResizeObserver(elementRef)
 const elementWidth = computed(() => elementState.offsetWidth)
 const compact = computed(() => elementWidth.value <= props.compactThreshold)
+// Hide the batch mode toggle if there are no items to select
+watch(toRef(props, 'total'), (total) => (selectMode.value = selectMode.value && total > 0))
 </script>
 
 <template>
   <div ref="element" class="document-entries-header d-flex align-items-center justify-content-start gap-1 py-3">
-    <button-toggle-batch-mode v-model:active="selectMode" :loading="loading" />
+    <button-toggle-batch-mode v-model:active="selectMode" :loading="loading" :disabled="total === 0" />
     <tiny-pagination :key="total" v-model="page" row :total-rows="total" :per-page="perPage" :compact="compact">
       <template #number-of-rows="{ lastRangeRow: to }">
         <template v-if="compact">
