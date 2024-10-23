5e44ff6d6c7e078a9fd76b2a96dacc98668e1f17
Pierre Romera <hello@pirhoo.com>
2024-10-23 09:59:00 +0000

    refactor: remove get/set options

--- src/composables/url-params.js
+++ src/composables/url-params.js
@@ -224,7 +224,7 @@ export function useUrlParam(queryParam, initialOrOptions = {}) {
  * between the URL and the reactive values for multiple parameters.
  *
  * @param {string[]} queryParams - The query parameters to sync.
- * @param {string|Object} [initialOrOptions={}] - Either an initial value or an options object (e.g., get/set methods).
+ * @param {string|Object} [initialOrOptions={}] - Either an initial value or an options object (e.g., initialValue/transform methods).
  * @returns {ComputedRef} - A computed reference to the synchronized values.
  */
 export function useUrlParams(queryParams, initialOrOptions = {}) {
@@ -233,33 +233,25 @@ export function useUrlParams(queryParams, initialOrOptions = {}) {
 
   // Handle both simple initial value or options object
   const options = isObject(initialOrOptions) ? initialOrOptions : { initialValue: initialOrOptions }
-  const { initialValue = null, get, set, transform = identity } = options
+  const { initialValue = null, transform = identity } = options
 
   // Determine the getter function (from options or fallback to initialValue)
-  const getValue = isFunction(get) ? () => castArray(get()) : () => initialValue
+  const get = () => {
+    const values = queryParams.map((param) => route.query[param])
+    // If all query parameters exist in the URL, transform and return them, else return the reactive values
+    return (values.some(isUndefined) ? initialValue : values).map(transform)
+  }
 
   // Determine the setter function (from options or fallback to updating the query parameters in the URL)
-  const setValue = isFunction(set)
-    ? (values) => set(castArray(values))
-    : (values) => batchQueryParamUpdate(router, route, queryParams, values)
+  const set = (values) => batchQueryParamUpdate(router, route, queryParams, values.map(transform))
 
   // Create a computed property that synchronizes the query parameters with the reactive values
-  const param = computed({
-    get() {
-      const values = queryParams.map((param) => route.query[param])
-      // If all query parameters exist in the URL, transform and return them, else return the reactive values
-      return values.some(isUndefined) ? getValue() : values
-    },
-    set: setValue
-  })
+  const param = computed({ get, set })
 
   // Watch the query parameters in the URL and sync them with the reactive values when they change
-  watch(
-    () => queryParams.map((param) => route.query[param]),
-    (newValues) => {
-      param.value = newValues.every((v) => v !== undefined) ? newValues.map(transform) : getValue()
-    }
-  )
+  watch(get, (newValues) => {
+    param.value = newValues.some(isUndefined) ? get() : newValues.map(transform)
+  })
 
   return param
 }
