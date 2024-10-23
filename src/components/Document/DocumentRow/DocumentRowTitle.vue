4f1bf58ab9c446455f339289d4233e8456ea81ba
Pierre Romera <hello@pirhoo.com>
2024-10-23 17:39:37 +0000

    feat: add link to document

--- src/components/Document/DocumentRow/DocumentRowTitle.vue
+++ src/components/Document/DocumentRow/DocumentRowTitle.vue
@@ -1,13 +1,21 @@
 <script setup>
-defineProps({
+import { computed } from 'vue'
+
+const props = defineProps({
   document: {
     type: Object
   }
+})
+
+const to = computed(() => {
+  return { name: 'document', params: props.document.routerParams }
 })
 </script>
 
 <template>
   <td>
-    {{ document.title }}
+    <router-link :to="to" class="fw-medium">
+      {{ document.title }}
+    </router-link>
   </td>
 </template>
