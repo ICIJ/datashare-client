a48ceffb3124312ab22b89779776f3976a55b502
Pierre Romera <hello@pirhoo.com>
2024-10-24 12:09:13 +0000

    refactor: rename "selected" to "selection"

--- tests/unit/specs/composables/selection.spec.js
+++ tests/unit/specs/composables/selection.spec.js
@@ -1,70 +1,71 @@
-import { useSelection } from '@/composables/selection'
 import { ref } from 'vue'
+
+import { useSelection } from '@/composables/selection'
 
 describe('useSelection composable', () => {
   it('should initialize with an empty selection', () => {
-    const { selected } = useSelection()
-    expect(selected.value).toEqual([])
+    const { selection } = useSelection()
+    expect(selection.value).toEqual([])
   })
 
   it('should initialize with a given selection', () => {
-    const initialSelected = ref([1, 2, 3])
-    const { selected } = useSelection(initialSelected)
-    expect(selected.value).toEqual([1, 2, 3])
+    const initialSelection = ref([1, 2, 3])
+    const { selection } = useSelection(initialSelection)
+    expect(selection.value).toEqual([1, 2, 3])
   })
 
-  it('should add value to selected', () => {
-    const { selected, addToSelected } = useSelection()
-    addToSelected(1)
-    expect(selected.value).toEqual([1])
+  it('should add value to selection', () => {
+    const { selection, addToSelection } = useSelection()
+    addToSelection(1)
+    expect(selection.value).toEqual([1])
   })
 
-  it('should not add duplicate values to selected', () => {
-    const { selected, addToSelected } = useSelection()
-    addToSelected(1)
-    addToSelected(1)
-    expect(selected.value).toEqual([1])
+  it('should not add duplicate values to selection', () => {
+    const { selection, addToSelection } = useSelection()
+    addToSelection(1)
+    addToSelection(1)
+    expect(selection.value).toEqual([1])
   })
 
-  it('should remove value from selected', () => {
-    const { selected, addToSelected, removeFromSelected } = useSelection()
-    addToSelected(1)
-    removeFromSelected(1)
-    expect(selected.value).toEqual([])
+  it('should remove value from selection', () => {
+    const { selection, addToSelection, removeFromSelection } = useSelection()
+    addToSelection(1)
+    removeFromSelection(1)
+    expect(selection.value).toEqual([])
   })
 
-  it('should not remove value that is not in selected', () => {
-    const { selected, addToSelected, removeFromSelected } = useSelection()
-    addToSelected(1)
-    removeFromSelected(2)
-    expect(selected.value).toEqual([1])
+  it('should not remove value that is not in selection', () => {
+    const { selection, addToSelection, removeFromSelection } = useSelection()
+    addToSelection(1)
+    removeFromSelection(2)
+    expect(selection.value).toEqual([1])
   })
 
   it('should toggle selection correctly', () => {
-    const { selected, toggleSelection } = useSelection()
+    const { selection, toggleSelection } = useSelection()
     toggleSelection(1, true)
-    expect(selected.value).toEqual([1])
+    expect(selection.value).toEqual([1])
 
     toggleSelection(1, false)
-    expect(selected.value).toEqual([])
+    expect(selection.value).toEqual([])
   })
 
-  it('should correctly identify if a value is selected', () => {
-    const { addToSelected, isSelected } = useSelection()
-    addToSelected(1)
-    expect(isSelected(1)).toBe(true)
-    expect(isSelected(2)).toBe(false)
+  it('should correctly identify if a value is selection', () => {
+    const { addToSelection, isSelection } = useSelection()
+    addToSelection(1)
+    expect(isSelection(1)).toBe(true)
+    expect(isSelection(2)).toBe(false)
   })
 
-  it('should correctly get and set selectedValues via proxy', () => {
-    const { selected, selectedValues } = useSelection()
+  it('should correctly get and set selectionValues via proxy', () => {
+    const { selection, selectionValues } = useSelection()
 
     // Use proxy to set selection
-    selectedValues.value[1] = true
-    expect(selected.value).toEqual([1])
+    selectionValues.value[1] = true
+    expect(selection.value).toEqual([1])
 
     // Use proxy to unset selection
-    selectedValues.value['1'] = false
-    expect(selected.value).toEqual([])
+    selectionValues.value['1'] = false
+    expect(selection.value).toEqual([])
   })
 })
