30d386d4523e3f8ec6a9ab61d8790a0e8c07980a
Caroline Desprat <cdesprat@icij.org>
2024-10-25 15:50:50 +0000

    refactor: rename tasks into task

--- src/router/index.js
+++ src/router/index.js
@@ -104,7 +104,7 @@ export const routes = [
           settings: () => import('@/views/Task/TasksListSettings')
         },
         meta: {
-          title: 'tasks.title'
+          title: 'task.title'
         },
         children: [
           {
