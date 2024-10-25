6686b7a8894a3ebc7594ce5f3543288b3e74ffe2
Pierre Romera <hello@pirhoo.com>
2024-10-25 10:27:56 +0000

    refactor: split document actions group entries into tiny components

---
+++ src/components/Document/DocumentActionsGroup/DocumentActionsGroupEntryDownload.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { computed, useTemplateRef } from 'vue'
+import { useStore } from 'vuex'
+
+import DocumentActionsGroupEntry from './DocumentActionsGroupEntry'
+
+import DocumentDownloadPopover from '@/components/Document/DocumentDownloadPopover/DocumentDownloadPopover'
+
+const { document } = defineProps({
+  /**
+   * The current document
+   */
+  document: {
+    type: Object
+  },
+  /**
+   * Position of the button tooltip
+   */
+  tooltipPlacement: {
+    type: String,
+    default: 'bottom'
+  },
+  /**
+   * Use vertical layout for the button
+   */
+  vertical: {
+    type: Boolean
+  }
+})
+
+const elementRef = useTemplateRef('element')
+
+const store = useStore()
+
+const isDownloadAllowed = computed(() => {
+  // Use nullish coalescing operator to allow download if the store/getter is undefined
+  return store?.getters['downloads/isDownloadAllowed'](document) ?? true
+})
+</script>
+
+<template>
+  <span>
+    <document-actions-group-entry
+      ref="element"
+      icon="download-simple"
+      :label="$t('documentActionsGroup.download')"
+      hide-tooltip
+      :vertical="vertical"
+      :disabled="!isDownloadAllowed"
+    />
+    <document-download-popover :target="elementRef" :document="document" :placement="tooltipPlacement" />
+  </span>
+</template>
---
+++ src/components/Document/DocumentActionsGroup/DocumentActionsGroupEntryStar.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+
+import DocumentActionsGroupEntry from './DocumentActionsGroupEntry'
+
+const { document } = defineProps({
+  /**
+   * The current document
+   */
+  document: {
+    type: Object
+  },
+  /**
+   * Position of the button tooltip
+   */
+  tooltipPlacement: {
+    type: String,
+    default: 'bottom'
+  },
+  /**
+   * Use vertical layout for the button
+   */
+  vertical: {
+    type: Boolean
+  }
+})
+
+const store = useStore()
+
+const isStarred = computed(() => {
+  return store?.getters['starred/isStarred'](document)
+})
+</script>
+
+<template>
+  <document-actions-group-entry
+    icon="star"
+    :label="$t('documentActionsGroup.star')"
+    :tooltip-placement="tooltipPlacement"
+    :vertical="vertical"
+    :fill="isStarred"
+  />
+</template>
