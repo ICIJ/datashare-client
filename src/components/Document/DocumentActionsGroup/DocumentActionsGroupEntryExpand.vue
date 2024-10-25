9b7eb02d95f764da21c904b983b261eec29610fc
Pierre Romera <hello@pirhoo.com>
2024-10-25 12:30:18 +0000

    refactor: use window's location origin

--- src/components/Document/DocumentActionsGroup/DocumentActionsGroupEntryExpand.vue
+++ src/components/Document/DocumentActionsGroup/DocumentActionsGroupEntryExpand.vue
@@ -1,7 +1,10 @@
 <script setup>
+import { computed } from 'vue'
+import { useRouter } from 'vue-router'
+
 import DocumentActionsGroupEntry from './DocumentActionsGroupEntry'
 
-defineProps({
+const { document } = defineProps({
   /**
    * The current document
    */
@@ -22,11 +25,20 @@ defineProps({
     type: Boolean
   }
 })
+
+const router = useRouter()
+
+const href = computed(() => {
+  const { href } = router.resolve({ name: 'document-standalone', params: document.routerParams })
+  return window.location.origin + href
+})
 </script>
 
 <template>
   <document-actions-group-entry
     icon="arrows-out-simple"
+    target="_blank"
+    :href="href"
     :label="$t('documentActionsGroup.expand')"
     :tooltip-placement="tooltipPlacement"
     :vertical="vertical"
