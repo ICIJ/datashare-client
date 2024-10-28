18276ac26429a94a88160a4078ac8ca00c5c39e2
Caroline Desprat <cdesprat@icij.org>
2024-10-28 14:27:17 +0000

    feat: add project as query parameter in the task addition new form

--- src/views/Task/DocumentAddition/TaskDocumentAdditionNew.vue
+++ src/views/Task/DocumentAddition/TaskDocumentAdditionNew.vue
@@ -2,13 +2,15 @@
   <page-header no-toggle-settings />
   <div class="project-new">
     <page-container fluid>
-      <task-document-addition-form />
+      <task-document-addition-form :project-name="project" />
     </page-container>
   </div>
 </template>
 
 <script setup>
 import PageContainer from '@/components/PageContainer/PageContainer'
-import TaskDocumentAdditionForm from '@/components/Task/TaskDocumentAddition/TaskDocumentAdditionForm'
 import PageHeader from '@/components/PageHeader/PageHeader'
+import TaskDocumentAdditionForm from '@/components/Task/TaskDocumentAddition/TaskDocumentAdditionForm'
+import { useUrlParam } from '@/composables/url-params'
+const project = useUrlParam('project', null)
 </script>
