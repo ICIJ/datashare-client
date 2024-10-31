ce24e1e7322fe2bb8baf176c18cc56c2e3ea2e4b
Pierre Romera <hello@pirhoo.com>
2024-10-31 15:19:00 +0000

    refactor: reorganized document component into views

---
+++ src/views/Document/DocumentView/DocumentViewTabs/DocumentViewTabsText.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+import { useDocument } from '@/composables/document'
+import DocumentNotes from '@/components/Document/DocumentNotes'
+import DocumentThread from '@/components/Document/DocumentThread'
+import DocumentTranslation from '@/components/Document/DocumentTranslation/DocumentTranslation'
+
+const { document } = useDocument()
+const route = useRoute()
+const q = computed(() => route.query.q)
+</script>
+
+<template>
+  <div class="document-view-tabs-text">
+    <document-notes :path="document.path" />
+    <document-thread v-if="document.isEmail" :document="document" />
+    <document-translation v-else :document="document" :q="q" />
+  </div>
+</template>
