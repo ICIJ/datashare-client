0e195932c6045ff61f34f8a442ab7617e9866ac3
Pierre Romera <hello@pirhoo.com>
2024-10-31 15:21:58 +0000

    refactor: splut document view in pieces

---
+++ src/views/Document/DocumentView/DocumentViewNav.vue
@@ -0,0 +1,14 @@
+<script setup>
+import DocumentSearchNav from '@/components/Document/DocumentSearchNav/DocumentSearchNav'
+
+defineProps({
+  document: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <document-search-nav :document="document" />
+</template>
---
+++ src/views/Document/DocumentView/DocumentViewUserActions.vue
@@ -0,0 +1,14 @@
+<script setup>
+import DocumentUserActions from '@/components/Document/DocumentUser/DocumentUserActions/DocumentUserActions'
+
+defineProps({
+  document: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <document-user-actions :document="document" show-tags show-recommended />
+</template>
