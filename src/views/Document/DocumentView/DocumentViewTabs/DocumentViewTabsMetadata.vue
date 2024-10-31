21854a3d7d93b8406eb260354ac4e476473c385a
Pierre Romera <hello@pirhoo.com>
2024-10-31 18:29:31 +0000

    feat: implement metadata display

--- src/views/Document/DocumentView/DocumentViewTabs/DocumentViewTabsMetadata.vue
+++ src/views/Document/DocumentView/DocumentViewTabs/DocumentViewTabsMetadata.vue
@@ -1,339 +1,224 @@
+<script setup>
+import Fuse from 'fuse.js'
+import { computed, ref } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { useStore } from 'vuex'
+import { filter, map, property, orderBy } from 'lodash'
+
+import ProjectLink from '@/components/Project/ProjectLink'
+import DocumentMetadata from '@/components/Document/DocumentMetadata/DocumentMetadata'
+import FormControlSearch from '@/components/Form/FormControl/FormControlSearch'
+import { getDocumentTypeLabel, getExtractionLevelTranslationKey } from '@/utils/utils'
+import { useDocument } from '@/composables/document'
+
+const { t } = useI18n()
+const { document, documentPath, documentDirname, parentDocument } = useDocument()
+const store = useStore()
+const q = ref('')
+
+const pinned = computed({
+  get: () => store.getters['app/getSettings']('documentViewMetadata', 'pinned'),
+  set: (pinned) => store.commit('app/setSettings', { view: 'documentViewMetadata', pinned })
+})
+
+const canonicalItems = computed(() => [
+  {
+    name: '_id',
+    label: t('document.id'),
+    classList: 'document-view-tabs-metadata__entry--id',
+    value: document.value.id
+  },
+  {
+    name: '_index',
+    label: t('document.project'),
+    classList: 'document-view-tabs-metadata__entry--project',
+    value: document.value.index,
+    icon: 'circles-three-plus',
+    component: ProjectLink,
+    componentBinding: {
+      project: document.value.index
+    }
+  },
+  {
+    name: 'title',
+    label: t('document.title'),
+    classList: 'document-view-tabs-metadata__entry--title',
+    icon: 'file-text',
+    value: document.value.title
+  },
+  {
+    name: 'path',
+    label: t('document.path'),
+    classList: 'document-view-tabs-metadata__entry--path',
+    icon: 'tree-structure',
+    value: documentPath.value
+  },
+  {
+    name: 'dirname',
+    label: t('document.dirname'),
+    classList: 'document-view-tabs-metadata__entry--dirname',
+    icon: 'folder',
+    value: documentDirname.value
+  },
+  {
+    name: 'metadata.tika_metadata_dcterms_created',
+    label: t('document.creationDate'),
+    classList: 'document-view-tabs-metadata__entry--creation-date',
+    icon: 'calendar',
+    value: document.value.meta('dcterms_created')
+  },
+  {
+    name: 'metadata.tika_metadata_dc_creator',
+    label: t('document.author'),
+    classList: 'document-view-tabs-metadata__entry--author',
+    icon: 'user-circle',
+    value: document.value.meta('dc_creator')
+  },
+  {
+    name: 'extractionDate',
+    label: t('document.extractionDate'),
+    classList: 'document-view-tabs-metadata__entry--extraction-date',
+    icon: 'calendar',
+    value: document.value.source.extractionDate
+  },
+  {
+    name: 'contentLength',
+    label: t('document.size'),
+    classList: 'document-view-tabs-metadata__entry--content-length',
+    icon: 'file',
+    value: document.value.humanSize,
+    rawValue: document.value.contentLength
+  },
+  {
+    name: 'language',
+    label: t('document.contentLanguage'),
+    classList: 'document-view-tabs-metadata__entry--language',
+    icon: 'globe-hemisphere-west',
+    value: t(`filter.lang.${document.value.source.language}`),
+    rawValue: document.value.source.language
+  },
+  {
+    name: 'metadata.tika_metadata_content_type',
+    label: t('document.contentType'),
+    classList: 'document-view-tabs-metadata__entry--content-type',
+    value: getDocumentTypeLabel(document.value.source.contentType),
+    rawValue: document.value.source.contentType
+  },
+  {
+    name: 'contentEncoding',
+    label: t('document.contentEncoding'),
+    classList: 'document-view-tabs-metadata__entry--content-encoding',
+    value: document.value.source.contentEncoding
+  },
+  {
+    name: 'extractionLevel',
+    label: t('filter.extractionLevel'),
+    classList: 'document-view-tabs-metadata__entry--tree-level',
+    icon: 'note-blank',
+    value: t(getExtractionLevelTranslationKey(document.value.source.extractionLevel)),
+    rawValue: document.value.source.extractionLevel
+  },
+  {
+    name: 'metadata.tika_metadata_message_raw_header_thread_index',
+    label: t('document.threadIndex'),
+    classList: 'document-view-tabs-metadata__entry--thread',
+    icon: 'envelope',
+    value: document.value.threadIndex
+  },
+  {
+    name: 'parentDocument',
+    label: t('document.parent'),
+    classList: 'document-view-tabs-metadata__entry--parent',
+    icon: 'file',
+    value: parentDocument?.value?.basename
+  },
+  {
+    name: 'contentTextLength',
+    label: t('document.contentTextLength'),
+    classList: 'document-view-tabs-metadata__entry--content-text-length',
+    icon: 'text-columns',
+    value: document.value.source.contentTextLength
+  }
+])
+
+const canonicalItemsNames = computed(() => map(canonicalItems.value, 'name'))
+
+const availableCanonicalItems = computed(() => filter(canonicalItems.value, (field) => field.value))
+
+const metadataItemsNames = computed(() => {
+  return filter(document.value.metas, (name) => {
+    return !canonicalItemsNames.value.includes(`metadata.${name}`) && !canonicalItemsNames.value.includes(name)
+  })
+})
+
+const metadataItems = computed(() => {
+  return metadataItemsNames.value.map((name) => {
+    const label = document.value.shortMetaName(name)
+    const value = document.value.meta(name)
+    return { label, name: `metadata.${name}`, value }
+  })
+})
+
+const availableItems = computed(() => {
+  return availableCanonicalItems.value.concat(metadataItems.value).map((item) => {
+    item.pinned = !!pinned.value[item.name]
+    return item
+  })
+})
+
+const sortedItems = computed(() => orderBy(availableItems.value, ['pinned'], ['desc']))
+
+const fuse = computed(() => {
+  return new Fuse(sortedItems.value, {
+    distance: 1,
+    shouldSort: false,
+    keys: ['label', 'name', 'value']
+  })
+})
+
+const items = computed(() => {
+  if (q.value) {
+    return fuse.value.search(q.value).map(property('item'))
+  } else {
+    return sortedItems.value
+  }
+})
+</script>
+
 <template>
-  <div class="container-fluid py-3 document__content">
-    <div class="document__content__tags mb-3">
-      <h5>
-        {{ $t('document.tags') }}
-      </h5>
-      <p class="text-muted">
-        {{ $t('document.tagsVisibility') }}
-      </p>
-      <document-tags-form :document="document" :tags="tags" :display-tags="true" :display-form="true" />
-    </div>
-
-    <div class="document__content__shortcuts mb-3">
-      <h5 class="mb-3">
-        {{ $t('document.shortcuts') }}
-      </h5>
-      <ul class="list-inline">
-        <li class="document__content__shortcuts__children me-4 list-inline-item mb-3">
-          <router-link :to="searchChildrenDocumentParams" class="btn btn-action btn-sm">
-            <fa icon="paperclip" class="me-2" />
-            {{ $t('document.searchChildrenDocument') }}
-          </router-link>
-        </li>
-        <li class="document__content__shortcuts__folder list-inline-item mb-3">
-          <router-link :to="searchDirnameDocumentParams" class="btn btn-action btn-sm">
-            <fa icon="folder-open" class="me-2" />
-            {{ $t('document.searchDirnameDocument') }}
-          </router-link>
-        </li>
-      </ul>
-    </div>
-
-    <div class="document__content__details">
-      <h5>
-        {{ $t('document.details') }}
-      </h5>
-      <p class="text-muted">
-        {{ $t('document.detailsInfo') }}
-      </p>
-      <b-table :items="items" :fields="fields" :tbody-tr-class="itemRowClass" responsive striped borderless>
-        <template #cell(label)="{ item: { name, label, value } }">
-          <div class="fw-bold d-flex justify-content-between">
-            <div class="text-truncate me-1 w-100" :title="name">
-              <var>{{ startCase(document.shortMetaName(label || name)) }}</var>
-            </div>
-            <div class="ms-auto document__content__details__item__label__search">
-              <router-link :to="{ name: 'search', query: { q: document.metaAsQueryParam(name, value), indices } }">
-                <fa icon="magnifying-glass" />
-              </router-link>
-            </div>
-          </div>
-        </template>
-        <template #cell(value)="{ item: field }">
-          <component :is="field.component" v-if="field.component" v-bind="field.componentBinding" />
-          <span v-else-if="field.value === 'unknown'" class="text-muted">
-            {{ $t('document.unknown') }}
-          </span>
-          <div v-else class="input-group input-group-sm document__content__details__item__input-group">
-            <input
-              :value="field.value"
-              readonly
-              type="text"
-              class="form-control document__content__details__item__input-group__input"
-            />
-            <haptic-copy
-              class="btn btn-tertiary document__content__details__item__input-group__copy"
-              hide-label
-              :text="String(field.value)"
-              tooltip-placement="left"
-            />
-          </div>
-        </template>
-      </b-table>
-    </div>
+  <div class="document-view-tabs-metadata w-100 d-flex flex-column gap-3">
+    <form-control-search v-model="q" placeholder="Search in metadata" clear-text />
+    <transition-group tag="div" name="list">
+      <document-metadata
+        v-for="item in items"
+        :key="item.name"
+        :name="item.label"
+        :icon="item.icon"
+        :label="item.name"
+        :value="item.value"
+        :description="item.description"
+        :class="item.classList"
+        :pinned="!!pinned[item.name]"
+        class="document-view-tabs-metadata__entry"
+        @update:pinned="pinned[item.name] = $event"
+      >
+        <component :is="item.component" v-if="item.component" v-bind="item.componentBinding" />
+      </document-metadata>
+    </transition-group>
   </div>
 </template>
 
-<script>
-import { filter, get, map, startCase, uniq } from 'lodash'
-import { mapState } from 'vuex'
-
-import DocumentTagsForm from '@/components/DocumentTagsForm'
-import ProjectLink from '@/components/Project/ProjectLink'
-import { getDocumentTypeLabel, getExtractionLevelTranslationKey } from '@/utils/utils'
-
-/**
- * A panel displaying details and metadata about an indexed document.
- */
-export default {
-  name: 'DocumentTabDetails',
-  components: {
-    DocumentTagsForm,
-    ProjectLink
-  },
-  props: {
-    /**
-     * The selected document
-     */
-    document: {
-      type: Object,
-      required: true
-    },
-    /**
-     * The parent document (if any)
-     */
-    parentDocument: {
-      type: Object,
-      default: () => {}
-    }
-  },
-  computed: {
-    ...mapState('document', ['tags']),
-    documentPath() {
-      if (this.$config.get('mountedDataDir')) {
-        return this.document.source.path.replace(this.$config.get('dataDir'), this.$config.get('mountedDataDir'))
-      } else {
-        return this.document.source.path
-      }
-    },
-    documentDirname() {
-      if (this.$config.get('mountedDataDir')) {
-        return this.document.source.dirname.replace(this.$config.get('dataDir'), this.$config.get('mountedDataDir'))
-      } else {
-        return this.document.source.dirname
-      }
-    },
-    index() {
-      return this.document.index
-    },
-    indices() {
-      return uniq([this.index, ...this.$store.state.search.indices]).join(',')
-    },
-    fields() {
-      return [
-        {
-          key: 'label',
-          sortable: true,
-          tdClass: 'align-middle document__content__details__item__label'
-        },
-        {
-          key: 'value',
-          tdClass: 'align-middle document__content__details__item__value'
-        }
-      ]
-    },
-    items() {
-      return this.presentCanonicalItems.concat(this.metaItems)
-    },
-    metaItems() {
-      return this.metaItemsNames.map((name) => {
-        const label = this.document.shortMetaName(name)
-        const value = this.document.meta(name)
-        return { label, name, value }
-      })
-    },
-    metaItemsNames() {
-      return filter(this.document.metas, (name) => {
-        return !this.canonicalItemsNames.includes(`metadata.${name}`) && !this.canonicalItemsNames.includes(name)
-      })
-    },
-    canonicalItems() {
-      return [
-        {
-          name: '_id',
-          label: this.$t('document.id'),
-          trClass: 'document__content__id',
-          value: this.document.id
-        },
-        {
-          name: '_index',
-          label: this.$t('document.project'),
-          trClass: 'document__content__project',
-          value: this.document.index,
-          component: ProjectLink,
-          componentBinding: {
-            project: this.document.index
-          }
-        },
-        {
-          name: 'title',
-          label: this.$t('document.title'),
-          trClass: 'document__content__title',
-          value: this.document.title
-        },
-        {
-          name: 'path',
-          label: this.$t('document.path'),
-          trClass: 'document__content__path',
-          value: this.documentPath
-        },
-        {
-          name: 'dirname',
-          label: this.$t('document.dirname'),
-          trClass: 'document__content__dirname',
-          value: this.documentDirname
-        },
-        {
-          name: 'tika_metadata_resourcename',
-          label: this.$t('document.name'),
-          trClass: 'document__content__basename',
-          value: this.document.meta('resourcename')
-        },
-        {
-          name: 'tika_metadata_dcterms_created',
-          label: this.$t('document.creationDate'),
-          trClass: 'document__content__creation-date',
-          value: this.document.meta('dcterms_created')
-        },
-        {
-          name: 'tika_metadata_dc_creator',
-          label: this.$t('document.author'),
-          trClass: 'document__content__author',
-          value: this.document.meta('dc_creator')
-        },
-        {
-          name: 'extractionDate',
-          label: this.$t('document.extractionDate'),
-          trClass: 'document__content__extraction-date',
-          value: this.document.source.extractionDate
-        },
-        {
-          name: 'contentLength',
-          label: this.$t('document.size'),
-          trClass: 'document__content__content-length',
-          value: this.document.humanSize,
-          rawValue: this.document.contentLength
-        },
-        {
-          name: 'language',
-          label: this.$t('document.contentLanguage'),
-          trClass: 'document__content__language',
-          value: this.$t(`filter.lang.${this.document.source.language}`),
-          rawValue: this.document.source.language
-        },
-        {
-          name: 'tika_metadata_content_type',
-          label: this.$t('document.contentType'),
-          trClass: 'document__content__content-type',
-          value: this.getDocumentTypeLabel(this.document.source.contentType),
-          rawValue: this.document.source.contentType
-        },
-        {
-          name: 'contentEncoding',
-          label: this.$t('document.contentEncoding'),
-          trClass: 'document__content__content-encoding',
-          value: this.document.source.contentEncoding
-        },
-        {
-          name: 'extractionLevel',
-          label: this.$t('filter.extractionLevel'),
-          trClass: 'document__content__tree-level',
-          value: this.$t(this.getExtractionLevelTranslationKey(this.document.source.extractionLevel)),
-          rawValue: this.document.source.extractionLevel
-        },
-        {
-          name: 'tika_metadata_message_raw_header_thread_index',
-          label: this.$t('document.threadIndex'),
-          trClass: 'document__content__thread',
-          value: this.document.threadIndex
-        },
-        {
-          name: 'parentDocument',
-          label: this.$t('document.parent'),
-          trClass: 'document__content__parent',
-          value: get(this, 'parentDocument.basename', null)
-        },
-        {
-          name: 'contentTextLength',
-          label: this.$t('document.contentTextLength'),
-          trClass: 'document__content__content-text-length',
-          value: this.document.source.contentTextLength
-        }
-      ]
-    },
-    canonicalItemsNames() {
-      return map(this.canonicalItems, 'name')
-    },
-    presentCanonicalItems() {
-      return filter(this.canonicalItems, (field) => field.value)
-    },
-    searchChildrenDocumentParams() {
-      const index = this.index
-      const q = `_routing:${this.document.id}`
-      const query = { q, index }
-      return { name: 'search', query }
-    },
-    searchDirnameDocumentParams() {
-      const index = this.index
-      const q = `dirname:"${this.documentDirname}"`
-      const query = { q, index }
-      return { name: 'search', query }
-    }
-  },
-  async created() {
-    await this.$store.dispatch('document/getTags')
-  },
-  methods: {
-    getDocumentTypeLabel,
-    getExtractionLevelTranslationKey,
-    itemRowClass(item) {
-      return ['document__content__details__item', item.trClass]
-    },
-    startCase
-  }
+<style>
+.list-move {
+  transition: all 0.3s ease;
 }
-</script>
 
-<style lang="scss">
-.document {
-  &__content {
-    &__details {
-      &__item {
-        &__label {
-          max-width: 6em;
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+}
 
-          &__search {
-            visibility: hidden;
-          }
-        }
-
-        &:hover &__label__search {
-          visibility: visible;
-        }
-
-        &__input-group {
-          & &__input,
-          & &__copy {
-            background: $input-bg;
-          }
-
-          & &__copy {
-            border-color: $input-border-color;
-          }
-        }
-      }
-    }
-  }
+.list-leave-active {
+  position: absolute;
 }
 </style>
