28ecdd30d112830ae21e22954f973c8a4507ef71
Pierre Romera <hello@pirhoo.com>
2024-10-31 15:19:00 +0000

    refactor: reorganized document component into views

---
+++ src/views/Document/DocumentView/DocumentViewTabs/DocumentViewTabs.vue
@@ -0,0 +1,28 @@
+<script setup>
+import TabGroupNavigation from '@/components/TabGroup/TabGroupNavigation/TabGroupNavigation'
+import TabGroupNavigationEntry from '@/components/TabGroup/TabGroupNavigation/TabGroupNavigationEntry'
+
+defineProps({
+  document: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <tab-group-navigation nowrap flush>
+    <tab-group-navigation-entry icon="text-align-left" :to="{ name: 'document.text' }">
+      {{ $t('documentViewTabs.text') }}
+    </tab-group-navigation-entry>
+    <tab-group-navigation-entry icon="file" :to="{ name: 'document.viewer' }">
+      {{ $t('documentViewTabs.viewer') }}
+    </tab-group-navigation-entry>
+    <tab-group-navigation-entry icon="info" :to="{ name: 'document.metadata' }">
+      {{ $t('documentViewTabs.metadata') }}
+    </tab-group-navigation-entry>
+    <tab-group-navigation-entry icon="users-three" :to="{ name: 'document.entities' }">
+      {{ $t('documentViewTabs.entities') }}
+    </tab-group-navigation-entry>
+  </tab-group-navigation>
+</template>
