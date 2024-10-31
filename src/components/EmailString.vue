704575388101e84fc8b6be0d87ce94b9add3e20b
Pierre Romera <hello@pirhoo.com>
2024-10-31 12:43:50 +0000

    feat: migrate to vue 3

--- src/components/EmailString.vue
+++ src/components/EmailString.vue
@@ -1,109 +1,78 @@
-<template>
-  <component :is="tag" class="email-string">
-    <span>
-      {{ nameOrRawEmail }}
-      <b-popover
-        v-if="mounted"
-        :target="$el"
-        triggers="hover focus"
-        custom-class="email-string__popover"
-        placement="bottom"
-      >
-        <template #title>
-          <div class="pt-2">{{ nameWithoutEmail }}</div>
-          <div class="text-muted small fw-normal py-2">{{ emailWithoutName || email }}</div>
-        </template>
-        <div class="text-end email-string__popover__content">
-          <router-link :to="{ name: 'search', query: { q: qReceived, indices } }" class="btn btn-sm btn-action">
-            <fa
-              mask="square"
-              icon="arrow-down"
-              transform="shrink-3 up-2"
-              class="email-string__popover__content__icon"
-            ></fa>
-            {{ $t('email.receivedLink') }}
-          </router-link>
-          <router-link :to="{ name: 'search', query: { q: qSent, indices } }" class="btn btn-sm btn-action ms-1">
-            <fa
-              mask="square"
-              icon="arrow-up"
-              transform="shrink-3 down-2"
-              class="email-string__popover__content__icon"
-            ></fa>
-            {{ $t('email.sentLink') }}
-          </router-link>
-        </div>
-      </b-popover>
-    </span>
-  </component>
-</template>
-
-<script>
+<script setup>
+import { computed, useTemplateRef } from 'vue'
+import { useStore } from 'vuex'
+import { PhosphorIcon } from '@icij/murmur-next'
 import trim from 'lodash/trim'
 
 const EMAIL_REGEX = /(.+)\<(.+)\>/i
 
-/**
- * Rich email display with shortcuts to the global search.
- */
-export default {
-  name: 'EmailString',
-  props: {
-    /**
-     * Email to display.
-     */
-    email: {
-      type: String
-    },
-    /**
-     * Specify the HTML tag to render instead of the default tag.
-     */
-    tag: {
-      type: String,
-      default: 'span'
-    }
-  },
-  data() {
-    return {
-      mounted: false
-    }
-  },
-  computed: {
-    nameOrRawEmail() {
-      return this.nameWithoutEmail || this.email
-    },
-    nameWithoutEmail() {
-      const matches = String(this.email).match(EMAIL_REGEX)
-      return matches ? trim(matches[1]) : null
-    },
-    emailWithoutName() {
-      const matches = String(this.email).match(EMAIL_REGEX)
-      return matches ? trim(matches[2]) : null
-    },
-    indices() {
-      return this.$store.state.search.indices
-    },
-    qReceived() {
-      const field = 'metadata.tika_metadata_message_to'
-      return `${field}:"${this.emailWithoutName || this.email}"`
-    },
-    qSent() {
-      const field = 'metadata.tika_metadata_message_from'
-      return `${field}:"${this.emailWithoutName || this.email}"`
-    }
-  },
-  mounted() {
-    this.mounted = true
+const props = defineProps({
+  email: String,
+  tag: {
+    type: String,
+    default: 'span'
   }
-}
+})
+
+const elementRef = useTemplateRef('element')
+
+const nameWithoutEmail = computed(() => {
+  const matches = String(props.email).match(EMAIL_REGEX)
+  return matches ? trim(matches[1], ' "\'`') : null
+})
+
+const emailWithoutName = computed(() => {
+  const matches = String(props.email).match(EMAIL_REGEX)
+  return matches ? trim(matches[2], ' "\'`') : null
+})
+
+const nameOrRawEmail = computed(() => {
+  return nameWithoutEmail.value || props.email
+})
+
+const store = useStore()
+
+const indices = computed(() => store.state.search.indices)
+
+const qReceived = computed(() => {
+  const field = 'metadata.tika_metadata_message_to'
+  return `${field}:"${emailWithoutName.value || props.email}"`
+})
+
+const qSent = computed(() => {
+  const field = 'metadata.tika_metadata_message_from'
+  return `${field}:"${emailWithoutName.value || props.email}"`
+})
 </script>
+
+<template>
+  <component :is="tag" ref="element" class="email-string">
+    {{ nameOrRawEmail }}
+  </component>
+  <b-popover :target="elementRef" custom-class="email-string__popover" placement="bottom">
+    <div class="email-string__popover__content">
+      <div class="h6 m-0">{{ nameWithoutEmail }}</div>
+      <div class="mb-3">{{ emailWithoutName || email }}</div>
+      <div class="d-flex flex-wrap gap-3">
+        <router-link :to="{ name: 'search', query: { q: qReceived, indices } }" class="btn btn-action">
+          <phosphor-icon name="tray-arrow-down" class="email-string__popover__content__icon" />
+          {{ $t('email.receivedLink') }}
+        </router-link>
+        <router-link :to="{ name: 'search', query: { q: qSent, indices } }" class="btn btn-action">
+          <phosphor-icon name="tray-arrow-up" class="email-string__popover__content__icon" />
+          {{ $t('email.sentLink') }}
+        </router-link>
+      </div>
+    </div>
+  </b-popover>
+</template>
 
 <style lang="scss">
 .email-string {
+  display: inline-block;
+
   &__popover {
-    &__content {
-      min-width: 250px;
-    }
+    min-width: 450px;
   }
 }
 </style>
