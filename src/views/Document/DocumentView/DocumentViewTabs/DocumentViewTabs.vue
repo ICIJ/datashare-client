ec0ee5ee9b8e9094087587cb6dd098949e69b122
Pierre Romera <hello@pirhoo.com>
2024-11-04 12:34:54 +0000

    feat: add "base" prop to document view tabs

--- src/views/Document/DocumentView/DocumentViewTabs/DocumentViewTabs.vue
+++ src/views/Document/DocumentView/DocumentViewTabs/DocumentViewTabs.vue
@@ -6,22 +6,26 @@ defineProps({
   document: {
     type: Object,
     required: true
+  },
+  base: {
+    type: String,
+    default: 'document'
   }
 })
 </script>
 
 <template>
   <tab-group-navigation nowrap flush>
-    <tab-group-navigation-entry icon="text-align-left" :to="{ name: 'document.text' }">
+    <tab-group-navigation-entry icon="text-align-left" :to="{ name: `${base}.text` }">
       {{ $t('documentViewTabs.text') }}
     </tab-group-navigation-entry>
-    <tab-group-navigation-entry icon="file" :to="{ name: 'document.viewer' }">
+    <tab-group-navigation-entry icon="file" :to="{ name: `${base}.viewer` }">
       {{ $t('documentViewTabs.viewer') }}
     </tab-group-navigation-entry>
-    <tab-group-navigation-entry icon="info" :to="{ name: 'document.metadata' }">
+    <tab-group-navigation-entry icon="info" :to="{ name: `${base}.metadata` }">
       {{ $t('documentViewTabs.metadata') }}
     </tab-group-navigation-entry>
-    <tab-group-navigation-entry icon="users-three" :to="{ name: 'document.entities' }">
+    <tab-group-navigation-entry icon="users-three" :to="{ name: `${base}.entities` }">
       {{ $t('documentViewTabs.entities') }}
     </tab-group-navigation-entry>
   </tab-group-navigation>
