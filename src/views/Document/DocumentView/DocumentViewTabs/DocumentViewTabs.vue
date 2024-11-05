9e892bfeeaac09e92c4f1522910e32287c814f07
Pierre Romera <hello@pirhoo.com>
2024-11-05 11:03:13 +0000

    feat: use new computed documentRoute from composable

--- src/views/Document/DocumentView/DocumentViewTabs/DocumentViewTabs.vue
+++ src/views/Document/DocumentView/DocumentViewTabs/DocumentViewTabs.vue
@@ -1,31 +1,25 @@
 <script setup>
+import { useDocument } from '@/composables/document'
+
 import TabGroupNavigation from '@/components/TabGroup/TabGroupNavigation/TabGroupNavigation'
 import TabGroupNavigationEntry from '@/components/TabGroup/TabGroupNavigation/TabGroupNavigationEntry'
 
-defineProps({
-  document: {
-    type: Object,
-    required: true
-  },
-  base: {
-    type: String,
-    default: 'document'
-  }
-})
+
+const { documentRoute } = useDocument()
 </script>
 
 <template>
   <tab-group-navigation nowrap flush>
-    <tab-group-navigation-entry icon="text-align-left" :to="{ name: `${base}.text` }">
+    <tab-group-navigation-entry icon="text-align-left" :to="{ name: `${documentRoute.name}.text` }">
       {{ $t('documentViewTabs.text') }}
     </tab-group-navigation-entry>
-    <tab-group-navigation-entry icon="file" :to="{ name: `${base}.viewer` }">
+    <tab-group-navigation-entry icon="file" :to="{ name: `${documentRoute.name}.viewer` }">
       {{ $t('documentViewTabs.viewer') }}
     </tab-group-navigation-entry>
-    <tab-group-navigation-entry icon="info" :to="{ name: `${base}.metadata` }">
+    <tab-group-navigation-entry icon="info" :to="{ name: `${documentRoute.name}.metadata` }">
       {{ $t('documentViewTabs.metadata') }}
     </tab-group-navigation-entry>
-    <tab-group-navigation-entry icon="users-three" :to="{ name: `${base}.entities` }">
+    <tab-group-navigation-entry icon="users-three" :to="{ name: `${documentRoute.name}.entities` }">
       {{ $t('documentViewTabs.entities') }}
     </tab-group-navigation-entry>
   </tab-group-navigation>
