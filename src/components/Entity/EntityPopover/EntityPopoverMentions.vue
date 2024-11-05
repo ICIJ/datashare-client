e20be8948684f12738442baad4194ed9f0a58a2a
Pierre Romera <hello@pirhoo.com>
2024-11-05 11:22:29 +0000

    refactor: ensure offset prop starts at 0

--- src/components/Entity/EntityPopover/EntityPopoverMentions.vue
+++ src/components/Entity/EntityPopover/EntityPopoverMentions.vue
@@ -1,10 +1,15 @@
 <script setup>
+import { computed } from 'vue'
 import { TinyPagination } from '@icij/murmur-next'
 
 import EntityPopoverMentionOccurrences from './EntityPopoverMentionOccurrences'
 import EntityPopoverMentionExcerpt from './EntityPopoverMentionExcerpt'
 
-const offset = defineModel('offset', { type: Number, default: 1 })
+const offset = defineModel('offset', { type: Number, default: 0 })
+const page = computed({
+  get: () => offset.value + 1,
+  set: (value) => (offset.value = value - 1)
+})
 
 defineProps({
   mention: {
@@ -26,7 +31,7 @@ defineProps({
 <template>
   <div class="d-flex flex-column align-items-center gap-3">
     <entity-popover-mention-excerpt :mention="mention" :excerpt="excerpt" />
-    <tiny-pagination v-if="offsets > 1" v-model="offset" :per-page="1" :total-rows="offsets" compact />
+    <tiny-pagination v-if="offsets > 1" v-model="page" :per-page="1" :total-rows="offsets" compact />
     <entity-popover-mention-occurrences :offsets="offsets" :projects="projects" class="text-secondary-emphasis" />
   </div>
 </template>
