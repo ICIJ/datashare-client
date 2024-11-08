a9b4588d21c60e695da93a2aab94fa17a5370126
Pierre Romera <hello@pirhoo.com>
2024-11-08 18:18:46 +0000

    refactor: split in to atomic components

---
+++ src/components/Button/ButtonTag.vue
@@ -0,0 +1,27 @@
+<script setup>
+import SearchParameter from '@/components/Search/SearchParameter/SearchParameter'
+
+defineOptions({ name: 'ButtonTag' })
+
+defineProps({
+  label: {
+    type: [Array, String]
+  },
+  noXIcon: {
+    type: Boolean
+  }
+})
+
+const emit = defineEmits(['delete'])
+</script>
+
+<template>
+  <search-parameter
+    icon="hash"
+    filter="tag"
+    class="text-action-emphasis"
+    :term="label"
+    :no-x-icon="noXIcon"
+    @click:x="emit('delete', label)"
+  />
+</template>
