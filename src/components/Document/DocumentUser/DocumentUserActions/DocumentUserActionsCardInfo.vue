0128f97d6023c0697cb79e044ef08339d184d8a0
Pierre Romera <hello@pirhoo.com>
2024-11-08 13:55:22 +0000

    feat: add DocumentUserActionsCardInfo

---
+++ src/components/Document/DocumentUser/DocumentUserActions/DocumentUserActionsCardInfo.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { PhosphorIcon } from '@icij/murmur-next'
+
+import { ICON_WEIGHTS } from '@/enums/iconWeights'
+
+defineOptions({ name: 'DocumentUserActionsCardInfo' })
+
+defineProps({
+  icon: {
+    type: [String, Array, Object],
+    default: 'info'
+  },
+  iconWeight: {
+    type: String,
+    default: ICON_WEIGHTS.REGULAR
+  }
+})
+</script>
+
+<template>
+  <p class="document-user-actions-card-info text-light-emphasis d-flex gap-1 align-items-start">
+    <phosphor-icon name="info" />
+    <span>
+      <slot />
+    </span>
+  </p>
+</template>
