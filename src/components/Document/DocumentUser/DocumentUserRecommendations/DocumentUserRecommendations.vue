804d0e6980cb1d91e18bf230377ad0db7e85584a
Pierre Romera <hello@pirhoo.com>
2024-11-08 17:18:24 +0000

    refactor: merge DocumentUserActionsCardRecommendations into DocumentUserRecommendations

--- src/components/Document/DocumentUser/DocumentUserRecommendations/DocumentUserRecommendations.vue
+++ src/components/Document/DocumentUser/DocumentUserRecommendations/DocumentUserRecommendations.vue
@@ -1,67 +1,41 @@
 <script setup>
-import { useI18n } from 'vue-i18n'
 import { computed } from 'vue'
-import { uniq } from 'lodash'
 
+import ButtonToggleRecommendation from '@/components/Button/ButtonToggleRecommendation'
 import DocumentUserActionsCard from '@/components/Document/DocumentUser/DocumentUserActions/DocumentUserActionsCard'
-import DocumentUserRecommendationsAction from '@/components/Document/DocumentUser/DocumentUserRecommendations/DocumentUserRecommendationsAction'
-import DisplayUser from '@/components/Display/DisplayUser'
-import SearchParameter from '@/components/Search/SearchParameter/SearchParameter'
 
 defineOptions({ name: 'DocumentUserRecommendations' })
-const usernames = defineModel({ type: Array, default: () => [] })
 
-const props = defineProps({
-  username: { type: String }
-})
+const modelValue = defineModel({ type: Boolean, default: false })
+const recommended = defineModel('recommended', { type: Boolean, default: false })
 
-const { t } = useI18n()
-
-const title = computed(() => t('documentUserActions.recommended', usernames.value.length))
-const warning = t('documentUserRecommendations.warning')
-const noRecommendations = t('documentUserRecommendations.noRecommendations')
-const recommendationIcon = 'user-gear'
-
-const uniqUsernames = computed(() => {
-  return uniq(usernames.value)
-})
-
-const recommended = computed({
-  get: () => {
-    return uniqUsernames.value.indexOf(props.username) > -1
-  },
-  set: (recoByMe) => {
-    if (recoByMe) {
-      usernames.value = [...uniqUsernames.value, props.username]
-    } else {
-      usernames.value = uniqUsernames.value.filter((u) => u !== props.username)
-    }
+const { recommendedBy } = defineProps({
+  recommendedBy: {
+    type: Array,
+    default: () => []
   }
 })
+
+const count = computed(() => recommendedBy.length)
 </script>
 
 <template>
-  <document-user-actions-card :title="title" :icon="recommendationIcon" show-warning action-end>
-    <template #content>
-      <div class="d-flex gap-2">
-        <display-user
-          v-for="(user, index) in usernames"
-          :key="index"
-          :tag="SearchParameter"
-          :icon="recommendationIcon"
-          :term="user"
-          filter="recommendedBy"
-          :value="user"
-          class="text-action-emphasis"
-        />
-      </div>
-      <span v-if="!usernames.length">{{ noRecommendations }}</span>
+  <document-user-actions-card
+    v-model="modelValue"
+    :title="$t('documentUserActions.recommendations', count)"
+    icon="eyes"
+    icon-weight="fill"
+    show-warning
+    action-end
+  >
+    <div v-if="count" class="d-flex gap-2 flex-wrap align-items-top">
+      <button-recommendation v-for="user in recommendedBy" :key="user" :label="user" />
+    </div>
+    <template #action-warning>
+      {{ $t('documentUserRecommendations.warning') }}
     </template>
-    <template #action-warning>{{ warning }}</template>
     <template #action>
-      <document-user-recommendations-action v-model="recommended" class="d-inline-flex align-self-end" />
+      <button-toggle-recommendation v-model:active="recommended" class="align-self-end" />
     </template>
   </document-user-actions-card>
 </template>
-
-<style scoped lang="scss"></style>
