3f2ed54957f291758f3d7eac4c8008cb7eab0a66
Pierre Romera <hello@pirhoo.com>
2024-11-08 18:47:07 +0000

    refactor: remove unused model

--- src/stories/components/Document/DocumentUser/DocumentUserTags/DocumentUserTagsAction.stories.js
+++ src/stories/components/Document/DocumentUser/DocumentUserTags/DocumentUserTagsAction.stories.js
@@ -6,22 +6,13 @@ export default {
   component: DocumentUserTagsAction,
   args: {
     modelValue: ['test'],
-    options: ['Foo', 'Bar', 'Baz', 'Qux']
+    tags: ['Foo', 'Bar', 'Baz', 'Qux']
   }
 }
 export const Default = {}
+
 export const WithTags = {
-  args: { tags: ['toto', 'titi', 'tata'] }
-}
-
-export const IsServer = {
-  isServer: true
-}
-
-export const IsServerWithTags = {
   args: {
-    isServer: true,
-    tags: ['toto', 'titi', 'tata'],
-    othersTags: ['toto', 'titi', 'tata']
+    tags: ['toto', 'titi', 'tata']
   }
 }
