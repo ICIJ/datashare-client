059a5454f8c3134312fe9d0016f8d60347d9f039
Pierre Romera <hello@pirhoo.com>
2024-11-08 18:47:07 +0000

    refactor: remove unused model

--- src/components/Document/DocumentUser/DocumentUserTags/DocumentUserTagsAction.vue
+++ src/components/Document/DocumentUser/DocumentUserTags/DocumentUserTagsAction.vue
@@ -1,37 +1,26 @@
 <script setup>
-import { useI18n } from 'vue-i18n'
-
 import FormControlTag from '@/components/Form/FormControl/FormControlTag/FormControlTag'
 
 defineOptions({ name: 'DocumentUserTagsAction' })
 
 const modelValue = defineModel({ type: Array, required: true })
+
 defineProps({
   options: {
     type: Array,
     default: () => []
   }
 })
-
-const emit = defineEmits(['update:tags'])
-const { t } = useI18n()
-const placeholder = t('documentUserTagsAction.placeholder')
-async function tagAdded($event) {
-  emit('update:tags', $event)
-}
 </script>
 
 <template>
-  <div>
-    <form-control-tag
-      v-model="modelValue"
-      :options="options"
-      class="d-flex-grow-1"
-      :placeholder="placeholder"
-      no-duplicates
-      no-clear
-      no-tags
-      @update:model-value="tagAdded"
-    />
-  </div>
+  <form-control-tag
+    v-model="modelValue"
+    :options="options"
+    class="w-100"
+    no-duplicates
+    no-tags
+    no-clear
+    :placeholder="$t('documentUserTagsAction.placeholder')"
+  />
 </template>
