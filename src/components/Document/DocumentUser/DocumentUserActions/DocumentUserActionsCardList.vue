50a20cd81345f26060ecc53fe1888bfa5456482a
Pierre Romera <hello@pirhoo.com>
2024-11-08 18:46:28 +0000

    feat: add body class and hide automaticaly

--- src/components/Document/DocumentUser/DocumentUserActions/DocumentUserActionsCardList.vue
+++ src/components/Document/DocumentUser/DocumentUserActions/DocumentUserActionsCardList.vue
@@ -2,16 +2,35 @@
 import DocumentUserActionsCardListTitle from '@/components/Document/DocumentUser/DocumentUserActions/DocumentUserActionsCardListTitle'
 
 defineOptions({ name: 'DocumentUserActionsCardList' })
+
 defineProps({
-  title: { type: String, default: null }
+  title: {
+    type: String,
+    default: null
+  },
+  bodyClass: {
+    type: [String, Array, Object]
+  }
 })
 </script>
 
 <template>
-  <div class="d-flex flex-column gap-3">
-    <document-user-actions-card-list-title :title="title" />
-    <div>
+  <div class="document-user-actions-card-list">
+    <document-user-actions-card-list-title v-if="title" :title="title" />
+    <div class="document-user-actions-card-list__body" :class="bodyClass">
       <slot />
     </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.document-user-actions-card-list {
+  display: flex;
+  flex-direction: column;
+  gap: $spacer;
+
+  &:has(.document-user-actions-card-list__body:empty) {
+    display: none;
+  }
+}
+</style>
