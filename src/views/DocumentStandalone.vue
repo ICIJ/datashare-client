d92ddb9e5ed313fdf17c0e89e94a3d3cb4b8a829
Pierre Romera <hello@pirhoo.com>
2024-11-11 11:26:12 +0000

    feat: add floating zone to standalone view

--- src/views/DocumentStandalone.vue
+++ src/views/DocumentStandalone.vue
@@ -1,10 +1,6 @@
-<template>
-  <div class="document-standalone mx-5 my-3">
-    <document-view :id="id" :routing="routing" :index="index" :q="q" />
-  </div>
-</template>
-
 <script setup>
+import { useId, provide } from 'vue'
+
 import DocumentView from '@/views/Document/DocumentView/DocumentView'
 
 defineProps({
@@ -22,4 +18,50 @@ defineProps({
     default: ''
   }
 })
+
+const documentViewFloatingId = useId()
+provide('documentViewFloatingId', documentViewFloatingId)
 </script>
+
+<template>
+  <div class="document-standalone px-5 py-3">
+    <div :id="documentViewFloatingId" class="document-standalone__floating"></div>
+    <div class="document-standalone__view">
+      <document-view :id="id" :routing="routing" :index="index" :q="q" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" floating>
+.document-standalone {
+  display: flex;
+  height: 100%;
+
+  &__floating {
+    flex: 470px 0 0;
+    position: sticky;
+    z-index: 100;
+    top: $spacer;
+    left: 0;
+    right: 0;
+    height: 100%;
+    max-height: calc(100vh - #{$spacer * 2});
+    padding: $spacer;
+    margin-right: $spacer;
+    box-shadow: 0 $spacer 0 0 var(--bs-body-bg);
+    background: var(--bs-action-bg-subtle);
+    border-radius: var(--bs-border-radius);
+    display: none;
+    overflow: auto;
+
+    &:has(*:not(:empty)) {
+      display: block;
+    }
+  }
+
+  &__view {
+    min-width: 0;
+    width: 100%;
+  }
+}
+</style>
