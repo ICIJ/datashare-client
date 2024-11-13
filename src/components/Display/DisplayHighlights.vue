85bd232d1340f6dfd05f3c5fbe86798113954e50
Pierre Romera <hello@pirhoo.com>
2024-11-13 11:27:12 +0000

    feat: add DisplayHighlights component

---
+++ src/components/Display/DisplayHighlights.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+  value: {
+    type: Array,
+    required: true
+  },
+  separator: {
+    type: String,
+    default: '<br class="mb-3" />'
+  }
+})
+</script>
+
+<template>
+  <div class="display-highlights" v-html="value.join(separator)"></div>
+</template>
