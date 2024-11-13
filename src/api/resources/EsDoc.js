8117843169667c09277b339878cec41ccdc0644c
Pierre Romera <hello@pirhoo.com>
2024-11-13 11:29:07 +0000

    refactor: use getter and matches function

--- src/api/resources/EsDoc.js
+++ src/api/resources/EsDoc.js
@@ -1,26 +1,34 @@
-import { cloneDeep, extend, get, set } from 'lodash'
+import { cloneDeep, get, matches, set } from 'lodash'
 
 const _raw = '_RAW'
 
 export default class EsDoc {
   constructor(raw = {}) {
     this[_raw] = cloneDeep(raw)
-    this.map(raw)
-  }
-  map(raw) {
-    // Map the given object to document attribute
-    return extend(this, {
-      id: raw._id,
-      routing: raw._routing || raw._id,
-      version: raw._version,
-      type: raw._type
-    })
   }
   get(path, defaultValue) {
     return get(this.raw, path, defaultValue)
   }
   set(path, value) {
     return set(this.raw, path, value)
+  }
+  get id() {
+    return this.raw._id
+  }
+  get index() {
+    return this.raw._index
+  }
+  get routing() {
+    return this.raw._routing || this.id
+  }
+  get version() {
+    return this.raw._version
+  }
+  get type() {
+    return this.raw_.type
+  }
+  get eq() {
+    return matches({ id: this.id, index: this.index })
   }
   get source() {
     return this.get('_source', {})
