101bbd0e112a93a33178b89482905d25c711a8c4
Caroline Desprat <cdesprat@icij.org>
2024-11-14 16:38:40 +0000

    feat: add table pages for batch download and batch searches

--- src/views/Task/Documents/TaskDocumentsList.vue
+++ src/views/Task/Documents/TaskDocumentsList.vue
@@ -1,9 +1,22 @@
 <script setup>
 import Task from '@/views/Task/Task'
+import TaskList from '@/components/Task/TaskList'
+import DisplayStatus from '@/components/Display/DisplayStatus'
+import DisplayDatetimeFromNow from '@/components/Display/DisplayDatetimeFromNow'
+import DisplayProgress from '@/components/Display/DisplayProgress'
 </script>
 <template>
   <task
+    v-slot="{ tasks, columns }"
     :task-filter="['org.icij.datashare.tasks.EnqueueFromIndexTask', 'org.icij.datashare.tasks.ScanTask']"
     page-name="documents"
-  />
+    show-add
+  >
+    <task-list :tasks="tasks" :columns="columns" :stoppable="true">
+      <template #cell(state)="{ item }"><display-status :value="item.state" /></template>
+      <template #cell(createdAt)="{ item }"><display-datetime-from-now :value="item.createdAt" /></template>
+      <template #cell(progress)="{ item }"><display-progress :value="item.progress" /></template>
+      <template #cell(name)="{ item }">{{ item.name }}</template>
+    </task-list>
+  </task>
 </template>
