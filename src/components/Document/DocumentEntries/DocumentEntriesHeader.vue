c6928e25021efd36c6f50d57b1c25eb548cc893b
Pierre Romera <hello@pirhoo.com>
2024-11-15 15:38:36 +0000

    feat: implement run download

--- src/components/Document/DocumentEntries/DocumentEntriesHeader.vue
+++ src/components/Document/DocumentEntries/DocumentEntriesHeader.vue
@@ -1,10 +1,16 @@
 <script setup>
 import { computed, toRef, useTemplateRef, watch } from 'vue'
 import { TinyPagination } from '@icij/murmur-next'
+import { useI18n } from 'vue-i18n'
+import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
 
 import { useCompact } from '@/composables/compact'
+import { useCore } from '@/composables/core'
 import ButtonToggleBatchMode from '@/components/Button/ButtonToggleBatchMode'
 import ButtonDownloadDocuments from '@/components/Button/ButtonDownloadDocuments'
+import byteSize from '@/utils/byteSize'
+import humanSize from '@/utils/humanSize'
 
 const selectMode = defineModel('selectMode', { type: Boolean, default: false })
 const page = defineModel('page', { type: Number, default: 1 })
@@ -29,6 +35,43 @@ const props = defineProps({
 
 const elementRef = useTemplateRef('element')
 const { compact } = useCompact(elementRef, { threshold: toRef(props, 'compactThreshold') })
+const { core, toast } = useCore()
+const { t, tm, n } = useI18n()
+const { dispatch, getters } = useStore()
+const router = useRouter()
+
+const batchDownloadDocumentsLabel = computed(() => {
+  const maxFiles = parseInt(core.config.get('batchDownloadMaxNbFiles'))
+  const maxSize = core.config.get('batchDownloadMaxSize')
+  const maxSizeBytes = byteSize(maxSize)
+  const locales = { maxFiles: n(maxFiles), maxSize: humanSize(maxSizeBytes, false, tm('human.size')) }
+
+  if (props.total > maxFiles) {
+    return t('documentEntriesHeader.batchDownloadDocumentsWarning', locales)
+  }
+
+  return t('documentEntriesHeader.batchDownloadDocuments', locales)
+})
+
+const batchDownloadDocumentsUri = computed(() => {
+  const from = 0
+  const query = { ...getters['search/toRouteQuery'](), from }
+  const { fullPath } = router.resolve({ name: 'search', query })
+  return fullPath
+})
+
+const runBatchDownload = async () => {
+  try {
+    await dispatch('search/runBatchDownload', batchDownloadDocumentsUri.value)
+    const { href } = router.resolve({ name: 'task.batch-download.list' })
+    const body = t('documentEntriesHeader.batchDownloadCreated')
+    const linkLabel = t('documentEntriesHeader.batchDownloadLink')
+    toast.success(body, { href, linkLabel })
+  } catch (_) {
+    const body = t('documentEntriesHeader.batchDownloadError')
+    toast.error(body)
+  }
+}
 
 const classList = computed(() => {
   return {
@@ -55,7 +98,7 @@ watch(toRef(props, 'total'), (total) => (selectMode.value = selectMode.value && 
             </template>
           </template>
         </tiny-pagination>
-        <button-download-documents />
+        <button-download-documents :label="batchDownloadDocumentsLabel" @click="runBatchDownload" />
       </div>
     </slot>
   </div>
