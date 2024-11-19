597b12c2a4a2d8a0d1603cf09824b2d9e13edab1
Pierre Romera <hello@pirhoo.com>
2024-11-19 09:46:02 +0000

    feat: add composable to delete project in a modal

---
+++ src/components/Project/ProjectDeletionModal.vue
@@ -0,0 +1,90 @@
+<script setup>
+import { computed, onBeforeMount, ref } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+import image from '@/assets/images/illustrations/app-modal-default-light.svg'
+import imageDark from '@/assets/images/illustrations/app-modal-default-dark.svg'
+import AppModal from '@/components/AppModal/AppModal'
+import DisplayNumber from '@/components/Display/DisplayNumber'
+import ProjectLabel from '@/components/Project/ProjectLabel'
+import { useProjectMetrics } from '@/composables/project'
+import { useWait } from '@/composables/wait'
+import { useCore } from '@/composables/core'
+
+const { project } = defineProps({
+  project: {
+    type: Object,
+    required: true
+  }
+})
+
+const { t } = useI18n()
+const emit = defineEmits(['success', 'error'])
+
+const documentsCount = ref(0)
+const tagsCount = ref(0)
+const recommendationsCount = ref(0)
+
+const { waitFor, loaderId } = useWait()
+const { core, toast } = useCore()
+const { fetchDocumentsCount, fetchTagsCount, fetchRecommendationsCount } = useProjectMetrics(project)
+
+async function confirmDeletion () {
+  try {
+    await core.api.deleteProject(project.name)
+    await core.deleteProject(project.name)
+    toast.success(t('projectDeletionModal.notify.succeed'))
+    emit('success')
+  } catch (error) {
+    toast.error(t('projectDeletionModal.notify.failed'))
+    emit('error')
+  }
+}
+
+const hasSomething = computed(() => documentsCount.value + tagsCount.value + recommendationsCount.value > 0)
+
+onBeforeMount(
+  waitFor(loaderId.value, async () => {
+    documentsCount.value = await fetchDocumentsCount()
+    tagsCount.value = await fetchTagsCount()
+    recommendationsCount.value = await fetchRecommendationsCount()
+  })
+)
+</script>
+
+<template>
+  <app-modal :image="image" :image-dark="imageDark" @ok="confirmDeletion">
+    <template #title>
+      <i18n-t keypath="projectDeletionModal.title">
+        <template #project>
+          <project-label :project="project" hide-thumbnail />
+        </template>
+      </i18n-t>
+    </template>
+    <v-wait :for="loaderId">
+      <i18n-t v-show="hasSomething" keypath="projectDeletionModal.body.all" tag="p">
+        <template #documents>
+          <i18n-t keypath="projectDeletionModal.body.documents" tag="span" :plural="documentsCount">
+            <template #count>
+              <display-number :value="documentsCount" />
+            </template>
+          </i18n-t>
+        </template>
+        <template #tags>
+          <i18n-t keypath="projectDeletionModal.body.tags" tag="span" :plural="tagsCount">
+            <template #count>
+              <display-number :value="tagsCount" />
+            </template>
+          </i18n-t>
+        </template>
+        <template #recommendations>
+          <i18n-t keypath="projectDeletionModal.body.recommendations" tag="span" :plural="recommendationsCount">
+            <template #count>
+              <display-number :value="recommendationsCount" />
+            </template>
+          </i18n-t>
+        </template>
+      </i18n-t>
+    </v-wait>
+  </app-modal>
+</template>
