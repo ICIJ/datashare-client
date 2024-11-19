0329ff2c512651bb540ea16e04194f0acefeb423
Pierre Romera <hello@pirhoo.com>
2024-11-19 10:16:44 +0000

    refactor: create pin button

---
+++ src/components/Button/ButtonTogglePin.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+
+const active = defineModel('active', {
+  type: Boolean,
+  default: false
+})
+
+defineProps({
+  variant: {
+    type: String,
+    default: 'link'
+  },
+  icon: {
+    type: String,
+    default: 'push-pin'
+  }
+})
+
+const weight = computed(() => {
+  return active.value ? 'fill' : 'regular'
+})
+
+const label = computed(() => {
+  return t(active.value ? 'buttonTogglePin.unpin' : 'buttonTogglePin.pin')
+})
+
+const togglePin = () => {
+  active.value = !active.value
+}
+</script>
+
+<template>
+  <button-icon
+    class="button-toggle-pin"
+    :icon-left="icon"
+    :icon-left-weight="weight"
+    :variant="variant"
+    :label="label"
+    @click="togglePin"
+  />
+</template>
