8ef70196ca031b588d309b50dda70d7143fa2be4
Pierre Romera <hello@pirhoo.com>
2024-11-19 10:59:31 +0000

    feat: add "modes" property to widget

--- src/store/widgets/WidgetEmpty.js
+++ src/store/widgets/WidgetEmpty.js
@@ -16,11 +16,12 @@ class WidgetEmpty {
    * [Bootstrap's grid system](https://bootstrap-vue.org/docs/components/layout#layout-and-grid-system)
    * @param order=0 {number} - Order to display among the others widgets
    */
-  constructor({ name = uniqueId('widget-'), card = true, cols = 12, order = 0 } = {}) {
+  constructor({ name = uniqueId('widget-'), card = true, cols = 12, order = 0, modes = null } = {}) {
     this.name = name
     this.card = card
     this.cols = cols
     this.order = order
+    this.modes = modes
   }
   bindState(state) {
     this[_STATE] = this[_STATE] || state
