4e56471e16a929e088357c74ec6b37e626df0833
Pierre Romera <hello@pirhoo.com>
2024-11-20 15:39:10 +0000

    fix: use correct icons import

--- bin/presets/phosphor-vue.js
+++ bin/presets/phosphor-vue.js
@@ -1,10 +1,10 @@
-import { property } from 'lodash'
+import keys from 'lodash/keys'
 import * as icons from '@phosphor-icons/vue'
 
 export function PhosphorVuePreset() {
   const nameMatch = (name) => name.match(/^Ph[A-Z]/)
 
   return {
-    '@phosphor-icons/vue': icons.map(property('name')).filter(nameMatch)
+    '@phosphor-icons/vue': keys(icons).filter(nameMatch)
   }
 }
