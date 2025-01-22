b8b381a78245f969a3e3590261d035b626449285
Pierre Romera <hello@pirhoo.com>
2025-01-22 16:58:45 +0100

    refactor: delete unused components

--- tests/unit/specs/components/Project/ProjectDeletionModal.spec.js
+++ tests/unit/specs/components/Project/ProjectDeletionModal.spec.js
@@ -1,4 +1,4 @@
-import { mount, shallowMount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 
 import CoreSetup from '~tests/unit/CoreSetup'
 import ProjectDeletionModal from '@/components/Project/ProjectDeletionModal'
