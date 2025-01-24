791761cc7b85c4d16c358d7eea26e802dc7bfa9c
Caroline Desprat <cdesprat@icij.org>
2025-01-24 15:20:48 +0000

    feat: add DocumentViewTabs props and tests

--- src/views/Document/DocumentView/DocumentViewTabs/DocumentViewTabs.vue
+++ src/views/Document/DocumentView/DocumentViewTabs/DocumentViewTabs.vue
@@ -1,24 +1,42 @@
 <script setup>
-import { useDocument } from '@/composables/document'
+import { useI18n } from 'vue-i18n'
+import { computed } from 'vue'
+
 import TabGroupNavigation from '@/components/TabGroup/TabGroupNavigation/TabGroupNavigation'
 import TabGroupNavigationEntry from '@/components/TabGroup/TabGroupNavigation/TabGroupNavigationEntry'
-
-const { documentRoute } = useDocument()
+const props = defineProps({
+  documentRoute: { type: Object }
+})
+const { t } = useI18n()
+const tc = (key) => computed(() => t(key))
+const entries = [
+  {
+    title: tc('documentViewTabs.text'),
+    icon: 'text-align-left',
+    to: { name: `${props.documentRoute.name}.text` }
+  },
+  {
+    title: tc('documentViewTabs.viewer'),
+    icon: 'file',
+    to: { name: `${props.documentRoute.name}.viewer` }
+  },
+  {
+    title: tc('documentViewTabs.metadata'),
+    icon: 'info',
+    to: { name: `${props.documentRoute.name}.metadata` }
+  },
+  {
+    title: tc('documentViewTabs.entities'),
+    icon: 'users-three',
+    to: { name: `${props.documentRoute.name}.entities` }
+  }
+]
 </script>
 
 <template>
   <tab-group-navigation nowrap>
-    <tab-group-navigation-entry icon="text-align-left" :to="{ name: `${documentRoute.name}.text` }">
-      {{ $t('documentViewTabs.text') }}
-    </tab-group-navigation-entry>
-    <tab-group-navigation-entry icon="file" :to="{ name: `${documentRoute.name}.viewer` }">
-      {{ $t('documentViewTabs.viewer') }}
-    </tab-group-navigation-entry>
-    <tab-group-navigation-entry icon="info" :to="{ name: `${documentRoute.name}.metadata` }">
-      {{ $t('documentViewTabs.metadata') }}
-    </tab-group-navigation-entry>
-    <tab-group-navigation-entry icon="users-three" :to="{ name: `${documentRoute.name}.entities` }">
-      {{ $t('documentViewTabs.entities') }}
+    <tab-group-navigation-entry v-for="entry in entries" :key="entry.title" lazy :icon="entry.icon" :to="entry.to">
+      {{ entry.title }}
     </tab-group-navigation-entry>
   </tab-group-navigation>
 </template>
