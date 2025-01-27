33978af390bfcdd7ebe34107388340b099e332db
Caroline Desprat <cdesprat@icij.org>
2025-01-27 18:32:25 +0000

    feat: add tests to task.vue

--- tests/unit/specs/components/Task/Task.spec.js
+++ tests/unit/specs/components/Task/Task.spec.js
@@ -1,47 +1,29 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, mount, flushPromises } from '@vue/test-utils'
 
 import Task from '@/views/Task/Task'
 import CoreSetup from '~tests/unit/CoreSetup'
+import TaskActions from '@/components/Task/TaskActions'
 
-describe('Task', () => {
+describe('Task.vue', () => {
   it('renders correctly', () => {
     const { plugins } = CoreSetup.init().useAll().useRouter()
 
     const wrapper = shallowMount(Task, { global: { plugins } })
     expect(wrapper.exists()).toBe(true)
   })
+  it('show a page header with task actions', () => {
+    const { plugins } = CoreSetup.init().useAll().useRouter()
 
-  /* it('should stop pending tasks', async () => {
-    store.commit('indexing/updateTasks', [{ name: 'foo.bar@123', progress: 0.5, state: 'RUNNING' }])
-    expect(store.state.indexing.tasks).toHaveLength(1)
-
-    await store.dispatch('indexing/stopPendingTasks')
-
-    expect(store.state.indexing.tasks).toHaveLength(0)
-    expect(api.stopPendingTasks).toBeCalledTimes(1)
+    const wrapper = mount(Task, { global: { plugins, renderStubDefaultSlot: true } })
+    const actions = wrapper.findComponent(TaskActions)
+    expect(actions.exists()).toBe(true)
   })
 
-  it('should stop the task named 456', async () => {
-    store.commit('indexing/updateTasks', [
-      { name: 'foo.bar@123', progress: 0.5, state: 'RUNNING' },
-      { name: 'foo.bar@456', progress: 0.7, state: 'RUNNING' }
-    ])
-    expect(store.state.indexing.tasks).toHaveLength(2)
-
-    await store.dispatch('indexing/stopTask', 'foo.bar@456')
-
-    expect(store.state.indexing.tasks).toHaveLength(1)
-    expect(api.stopTask).toBeCalledTimes(1)
-    expect(api.stopTask).toBeCalledWith('foo.bar@456')
+  it('should fetch tasks on mount', async () => {
+    const api = { getTasks: vi.fn().mockResolvedValue([{ state: 'DONE' }]) }
+    const { plugins } = CoreSetup.init(api).useAll().useRouter()
+    mount(Task, { global: { plugins } })
+    await flushPromises()
+    expect(api.getTasks).toHaveBeenCalledTimes(1)
   })
-
-  it('should delete done tasks', async () => {
-    store.commit('indexing/updateTasks', [{ name: 'foo.bar@123', progress: 0.5, state: 'DONE' }])
-    expect(store.state.indexing.tasks).toHaveLength(1)
-
-    await store.dispatch('indexing/deleteDoneTasks')
-
-    expect(store.state.indexing.tasks).toHaveLength(0)
-    expect(api.deleteDoneTasks).toBeCalledTimes(1)
-  }) */
 })
