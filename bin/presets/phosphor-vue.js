c744051d4cfd949f2abf96ba122c9ecc672f6520
Pierre Romera <hello@pirhoo.com>
2025-01-31 11:02:10 +0000

    build: use flat config for eslint and add Phosphor components

--- bin/presets/phosphor-vue.js
+++ bin/presets/phosphor-vue.js
@@ -1,10 +1,7 @@
-import keys from 'lodash/keys'
-import * as icons from '@phosphor-icons/vue'
+import { components } from '../icons'
 
 export function PhosphorVuePreset() {
-  const nameMatch = (name) => name.match(/^Ph[A-Z]/)
-
   return {
-    '@phosphor-icons/vue': keys(icons).filter(nameMatch)
+    '@phosphor-icons/vue': components
   }
 }
