794cf8a60200bb251007b65c38b5aedac2a3ac77
Pierre Romera <hello@pirhoo.com>
2025-01-31 16:57:58 +0000

    test: mock API calls

--- tests/unit/specs/components/Project/ProjectDeletionModal.spec.js
+++ tests/unit/specs/components/Project/ProjectDeletionModal.spec.js
@@ -5,32 +5,27 @@ import ProjectDeletionModal from '@/components/Project/ProjectDeletionModal'
 import esConnectionHelper from '~tests/unit/specs/utils/esConnectionHelper'
 
 describe('ProjectDeletionModal.vue', () => {
-  let api, plugins
-  let project
+  let api, plugins, project
 
   beforeEach(() => {
-    const { index: name, es } = esConnectionHelper.build()
-    project = { name }
-    api = { updateProject: vi.fn(), deleteProject: vi.fn(), elasticsearch: es }
-    const core = CoreSetup.init(api)
-      .useAll()
-      .useRouter([
-        {
-          name: 'project.list',
-          path: '/project'
-        }
-      ])
+    const { index: name, es: elasticsearch } = esConnectionHelper.build()
+    api = { elasticsearch, deleteProject: vi.fn(), getRecommendationsByProject: vi.fn() }
+    api.elasticsearch.deleteProject = vi.fn()
+    api.elasticsearch.countDocuments = vi.fn().mockResolvedValue(50)
+    api.elasticsearch.countTags = vi.fn().mockResolvedValue(2)
+    // The modal uses the router to redirect to the project list so the route must exist
+    const routes = [{ name: 'project.list', path: '/project' }]
+    const core = CoreSetup.init(api).useAll().useRouter(routes)
     // Ensure the local-datashare project can be found
     core.config.set('projects', [{ name, label: 'Default', sourcePath: '/' }])
     plugins = core.plugins
+    project = { name }
   })
 
   it('deletes the project when the model emits a ok event', async () => {
     const props = { project }
     const wrapper = shallowMount(ProjectDeletionModal, { global: { plugins }, props })
-
     expect(wrapper.vm.$core.projects).toHaveLength(1)
-
     await wrapper.trigger('ok')
     expect(api.deleteProject).toBeCalledWith(project.name)
     expect(wrapper.vm.$core.projects).toHaveLength(0)
