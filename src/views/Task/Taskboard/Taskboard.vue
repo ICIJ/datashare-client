2d2056241b601015a4ad51f362d861c725658086
Caroline Desprat <cdesprat@icij.org>
2025-02-12 17:12:16 +0000

    feat: add task board (wip)

---
+++ src/views/Task/Taskboard/Taskboard.vue
@@ -0,0 +1,14 @@
+<script setup>
+import TaskboardLatest from '@/views/Task/Taskboard/TaskboardLatest'
+import PageContainer from '@/components/PageContainer/PageContainer'
+import PageHeader from '@/components/PageHeader/PageHeader'
+</script>
+
+<template>
+  <page-header></page-header>
+  <page-container fluid>
+    <taskboard-latest />
+  </page-container>
+</template>
+
+<style scoped lang="scss"></style>
