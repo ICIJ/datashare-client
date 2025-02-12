b26d93eb1d4ced3a6861a2384d5d6bd673cf5f4d
Pierre Romera <hello@pirhoo.com>
2025-02-12 15:33:07 +0100

    feat: add Pinia to core

---
+++ src/store/pinia.js
@@ -0,0 +1,6 @@
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
+export const pinia = createPinia()
+
+pinia.use(piniaPluginPersistedstate)
