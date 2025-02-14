760dd91c3a34fa765f17c387336e36ddb6d3cc6d
Pierre Romera <hello@pirhoo.com>
2025-02-14 18:02:39 +0000

    refactor: use new pipelines store in PipelinesMixin

--- tests/unit/specs/core/PipelinesMixin.spec.js
+++ tests/unit/specs/core/PipelinesMixin.spec.js
@@ -1,31 +1,33 @@
 import { Core } from '@/core'
+import { usePipelinesStore } from '@/store/modules/pipelines'
 
 describe('PipelinesMixin', () => {
-  let core
+  let core, pipelinesStore
 
   beforeEach(async () => {
     core = Core.init().useAll()
+    pipelinesStore = usePipelinesStore()
   })
 
   it('should find several pipeline by their category', () => {
     core.registerPipeline({ category: 'foo' })
     core.registerPipeline({ category: 'baz' })
     core.registerPipeline({ category: 'baz' })
-    expect(core.store.getters['pipelines/getPipelinesByCategory']('baz')).toHaveLength(2)
+    expect(pipelinesStore.getPipelinesByCategory('baz')).toHaveLength(2)
   })
 
   it('should find one pipeline by its category on the current project', () => {
     core.registerPipelineForProject('first-index', { category: 'biz' })
-    expect(core.store.getters['pipelines/getPipelinesByCategory']('biz')).toHaveLength(0)
+    expect(pipelinesStore.getPipelinesByCategory('biz')).toHaveLength(0)
     core.store.commit('search/indices', 'first-index')
-    expect(core.store.getters['pipelines/getPipelinesByCategory']('biz')).toHaveLength(1)
+    expect(pipelinesStore.getPipelinesByCategory('biz')).toHaveLength(1)
   })
 
   it('should find no pipeline on the current project', () => {
     core.registerPipelineForProject('first-index', { category: 'flu' })
     core.store.commit('search/indices', 'first-index')
-    expect(core.store.getters['pipelines/getPipelinesByCategory']('flu')).toHaveLength(1)
+    expect(pipelinesStore.getPipelinesByCategory('flu')).toHaveLength(1)
     core.store.commit('search/indices', 'second-index')
-    expect(core.store.getters['pipelines/getPipelinesByCategory']('flu')).toHaveLength(0)
+    expect(pipelinesStore.getPipelinesByCategory('flu')).toHaveLength(0)
   })
 })
