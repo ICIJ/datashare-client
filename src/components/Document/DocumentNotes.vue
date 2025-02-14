f243c2a581c520f1d5d33dc8cd17a57b8d647ec3
Pierre Romera <hello@pirhoo.com>
2025-02-14 11:12:07 +0000

    refactor: use new document notes store with pinia and migrate to setup API

--- src/components/Document/DocumentNotes.vue
+++ src/components/Document/DocumentNotes.vue
@@ -6,37 +6,20 @@
   </div>
 </template>
 
-<script>
-import { mapState } from 'vuex'
+<script setup>
+import { ref, onBeforeMount } from 'vue'
+import { useStore } from 'vuex'
 
-/**
- * A list of notes related to a document's path and provided by the backend
- */
-export default {
-  name: 'DocumentNotes',
-  props: {
-    /**
-     * Path to the document
-     */
-    path: {
-      type: String
-    }
-  },
-  data() {
-    return {
-      notes: []
-    }
-  },
-  computed: {
-    ...mapState('search', ['index'])
-  },
-  async beforeMount() {
-    this.notes = await this.retrieveNotes(this.index, this.path)
-  },
-  methods: {
-    retrieveNotes(project, path) {
-      return this.$store.dispatch('documentNotes/retrieveNotes', { project, path })
-    }
-  }
-}
+import { useDocumentNotes } from '@/store/modules/documentNotes'
+
+const props = defineProps({ path: String })
+const notes = ref([])
+const store = useStore()
+const documentNotesStore = useDocumentNotes()
+
+onBeforeMount(async () => {
+  const project = store.state.search.index
+  const path = props.path
+  notes.value = await documentNotesStore.fetchNotesByPath({ project, path })
+})
 </script>
