856cf907d522ea4ff6221e9a6d8b98cd3dce5716
Pierre Romera <hello@pirhoo.com>
2025-02-14 17:16:11 +0000

    refactor: migrate hooks store to pinia

--- src/store/modules/hooks.js
+++ src/store/modules/hooks.js
@@ -2,58 +2,86 @@ import filter from 'lodash/filter'
 import find from 'lodash/find'
 import findIndex from 'lodash/findIndex'
 import orderBy from 'lodash/orderBy'
-import { markRaw } from 'vue'
+import { computed, reactive, markRaw } from 'vue'
+import { defineStore } from 'pinia'
 
 import { HookedComponent } from '@/store/hooks'
 
-export const state = {
-  // A list of registered hooks
-  registered: []
-}
+export const useHooksStore = defineStore('hooks', () => {
+  const registered = reactive([])
 
-export const getters = {
-  hookedComponents(state) {
-    return () => {
-      return orderBy(state.registered.map(HookedComponent.create), 'order', 'asc')
-    }
-  },
-  filterHookedComponentsByTarget(state, getters) {
-    return (target) => filter(getters.hookedComponents(), { target })
-  },
-  getHookedComponentByName(state, getters) {
-    return (name) => find(getters.hookedComponents(), { name })
-  }
-}
-
-export const mutations = {
-  register(state, { target, order, name, definition = {} }) {
-    const index = name ? findIndex(state.registered, { name }) : -1
+  /**
+   * Register a new hooked component
+   *
+   * @param {Object} options
+   * @param {string} options.target - The target hook
+   * @param {number} [options.order] - The order of the hooked component
+   * @param {string} [options.name] - The name of the hooked component
+   * @param {Object} [options.definition] - The definition of the hooked component
+   */
+  const register = ({ target, order, name, definition = {} }) => {
+    const index = name ? findIndex(registered, { name }) : -1
     if (index === -1) {
-      state.registered.push({
+      registered.push({
         target,
         order,
         name,
         definition: markRaw(definition)
       })
     }
-  },
-  unregister(state, name) {
-    const index = findIndex(state.registered, { name })
-    if (index > -1) {
-      state.registered.splice(index, 1)
-    }
-  },
-  reset(state) {
-    state.registered.splice(0, state.registered.length)
-  },
-  resetTarget(state, target) {
-    state.registered = filter(state.registered, (r) => r.target !== target)
   }
-}
 
-export default {
-  namespaced: true,
-  state,
-  getters,
-  mutations
-}
+  /**
+   * Unregister a hooked component
+   *
+   * @param {string} name - The name of the hooked component
+   */
+  const unregister = (name) => {
+    const index = findIndex(registered, { name })
+
+    if (index > -1) {
+      registered.splice(index, 1)
+    }
+  }
+
+  /**
+   * Reset all registered components
+   */
+  const reset = () => {
+    registered.splice(0, registered.length)
+  }
+
+  /**
+   * Reset all registered components for a specific target*
+   *
+   * @param {string} target - The target hook
+   */
+  const resetTarget = (target) => {
+    // This is a method to remove all registered components for a specific target
+    // without creating a new array reference
+    registered.splice(0, registered.length, ...registered.filter((r) => r.target !== target))
+  }
+
+  const components = computed(() => {
+    return orderBy(registered.map(HookedComponent.create), 'order', 'asc')
+  })
+
+  const filterComponentsByTarget = computed(() => {
+    return (target) => filter(components.value, { target })
+  })
+
+  const getComponentByName = computed(() => {
+    return (name) => find(components.value, { name })
+  })
+
+  return {
+    registered,
+    register,
+    unregister,
+    reset,
+    resetTarget,
+    components,
+    filterComponentsByTarget,
+    getComponentByName
+  }
+})
