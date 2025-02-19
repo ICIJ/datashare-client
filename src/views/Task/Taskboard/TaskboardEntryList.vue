137a50edd456d8cb9e2ba3c482ff232823367dae
Caroline Desprat <cdesprat@icij.org>
2025-02-19 11:00:33 +0000

    feat: add translation support to taskboard entries

--- src/views/Task/Taskboard/TaskboardEntryList.vue
+++ src/views/Task/Taskboard/TaskboardEntryList.vue
@@ -1,42 +1,45 @@
 <script setup>
+import { ref, computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+
 import TaskboardEntry from '@/views/Task/Taskboard/TaskboardEntry'
+const batchSearchesPublic = ref(13)
+const batchSearchesPrivate = ref(0)
+const batchDownloads = ref(45)
+const findEntities = ref(3)
+const documentAdditions = ref(4)
+const { t } = useI18n()
 const taskEntries = [
   {
-    title: 'Batch search',
+    key: 'task.taskboard.entries.batch-search',
     icon: 'list-magnifying-glass',
-    description:
-      'Get the results of each query of a list, all at once. For instance, instead of searching for all the politicians of your country one after the other, just upload the list and see the results for each one.',
-    info: '13 shared batch searches, 0 private batch search',
+    infoComp: computed(() => {
+      const nbPublic = t('task.taskboard.entries.batch-search.nbPublic', batchSearchesPublic.value)
+      const nbPrivate = t('task.taskboard.entries.batch-search.nbPrivate', batchSearchesPrivate.value)
+      return t('task.taskboard.entries.batch-search.info', { nbPublic, nbPrivate })
+    }),
     listLink: { name: 'task.batch-search.list' },
-    actionLink: { name: 'task.batch-search.new' },
-    actionText: 'Create a batch search'
+    actionLink: { name: 'task.batch-search.new' }
   },
   {
-    title: 'Batch downloads',
+    key: 'task.taskboard.entries.batch-download',
     icon: 'download-simple',
-    description: 'When your query has results, you can download all of them in a password-protected file.',
-    info: 'Batch search info',
-    listLink: { name: 'task.batch-download.list' }
+    listLink: { name: 'task.batch-download.list' },
+    info: batchDownloads.value
   },
   {
-    title: 'Entity recognition',
+    key: 'task.taskboard.entries.entity-recognition',
     icon: 'users',
-    description:
-      'Know quickly what’s in your files by asking Datashare to automatically recognize names of people, locations and organizations in your documents, as well as email addresses. ',
-    info: '3 entity recognitions',
+    info: findEntities.value,
     listLink: { name: 'task.entities.list' },
-    actionLink: { name: 'task.entities.new' },
-    actionText: 'Find entities'
+    actionLink: { name: 'task.entities.new' }
   },
   {
-    title: 'Document addition',
+    key: 'task.taskboard.entries.document-addition',
     icon: 'files',
-    description:
-      'Index documents that are on your computer or on a drive so you can search them in Datashare. Datashare can index all type of documents.',
-    info: 'Batch search info',
+    info: documentAdditions.value,
     listLink: { name: 'task.documents.list' },
-    actionLink: { name: 'task.documents.new' },
-    actionText: 'Add documents'
+    actionLink: { name: 'task.documents.new' }
   }
 ]
 </script>
@@ -45,7 +48,17 @@ const taskEntries = [
   <div class="taskboard-entry-list container-fluid bg-tertiary-subtle py-4">
     <div class="row g-3 justify-content-around flex-grow-1">
       <div v-for="(task, index) in taskEntries" :key="index" class="col-12 col-xl-5">
-        <taskboard-entry v-bind="task" class="h-100 d-flex flex-column"> </taskboard-entry>
+        <taskboard-entry
+          :icon="task.icon"
+          :title="t(`${task.key}.title`)"
+          :description="t(`${task.key}.description`)"
+          :info="task.infoComp ?? t(`${task.key}.info`, task.info)"
+          :list-link="task.listLink"
+          :action-link="task.actionLink"
+          :action-text="t(`${task.key}.actionText`)"
+          class="h-100 d-flex flex-column"
+        >
+        </taskboard-entry>
       </div>
     </div>
   </div>
