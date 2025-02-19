061a86a97fac4629054ad7132338d9f29fd107c2
Caroline Desprat <cdesprat@icij.org>
2025-02-19 16:31:28 +0000

    fix: add missing batch search table columns

--- src/views/Task/TaskBatchSearch/TaskBatchSearchList.vue
+++ src/views/Task/TaskBatchSearch/TaskBatchSearchList.vue
@@ -6,10 +6,25 @@ import DisplayDatetimeFromNow from '@/components/Display/DisplayDatetimeFromNow'
 import DisplayProgress from '@/components/Display/DisplayProgress'
 import { useTaskSettings } from '@/composables/task-settings'
 import { TASK_NAME } from '@/enums/taskNames'
+import DisplayNumber from '@/components/Display/DisplayNumber'
+import ProjectLink from '@/components/Project/ProjectLink'
+import DisplayUser from '@/components/Display/DisplayUser'
 const { propertiesModelValueOptions } = useTaskSettings('batch-search')
 
-function getBatchSearchName(task) {
-  return task.batchRecord.name
+function getProjects(item) {
+  return item.args.batchRecord.projects ?? []
+}
+function getLink(item) {
+  return {
+    name: 'task.batch-search.view',
+    params: { uuid: item.args.batchRecord?.uuid, indices: getProjects(item).join(',') }
+  }
+}
+function accessIcon(item) {
+  return item.args.batchRecord?.published ? 'eye' : 'eye-slash'
+}
+function accessText(item) {
+  return item.args.batchRecord?.published ? 'Shared' : 'Private to you'
 }
 </script>
 <template>
@@ -28,17 +43,24 @@ function getBatchSearchName(task) {
       @update:sort="updateSort"
       @update:order="updateOrder"
     >
-      <template #cell(id)="{ item: { id: uuid } }">
-        <router-link :to="{ name: 'task.batch-search.view.results', params: { indices: 'local-datashare', uuid } }">
-          {{ uuid }}
-        </router-link>
-      </template>
       <template #cell(state)="{ item }">
         <display-status :value="item.state" />
       </template>
-      <template #cell(name)="{ item }">
-        {{ getBatchSearchName(item) }}
+      <template #cell(access)="{ item }">
+        <phosphor-icon :title="accessText(item)" :name="accessIcon(item)" />
       </template>
+      <template #cell(name)="{ item }">
+        <router-link :to="getLink(item)"> {{ item.args.batchRecord?.name }}</router-link>
+      </template>
+      <template #cell(queries)="{ item }"> <display-number :value="item.args.batchRecord.nbQueries" /> </template>
+      <template #cell(documents)="{ item }"> <display-number :value="item.args.batchRecord.nbResults" /> </template>
+      <template #cell(projects)="{ item }">
+        <div class="d-flex gap-2">
+          <project-link v-for="project in getProjects(item)" :key="project.name" :project="project" />
+        </div>
+      </template>
+
+      <template #cell(author)="{ item }"><display-user :value="item.args.batchRecord?.user.id" /></template>
       <template #cell(createdAt)="{ item }">
         <display-datetime-from-now :value="item.createdAt" />
       </template>
