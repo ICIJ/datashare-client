d572ae87678572e410ccdf4101f944cdf35f6d18
Pierre Romera <hello@pirhoo.com>
2025-02-24 15:13:41 +0000

    refactor: mock api instance instead of passing it as a Core parameter

--- tests/unit/specs/components/Document/DocumentViewer/DocumentViewerJson.spec.js
+++ tests/unit/specs/components/Document/DocumentViewer/DocumentViewerJson.spec.js
@@ -1,17 +1,25 @@
 import { shallowMount } from '@vue/test-utils'
 
 import { flushPromises } from '~tests/unit/tests_utils'
+import CoreSetup from '~tests/unit/CoreSetup'
 import DocumentViewerJson from '@/components/Document/DocumentViewer/DocumentViewerJson'
 import JsonFormatter from '@/components/JsonFormatter'
-import CoreSetup from '~tests/unit/CoreSetup'
-import documentJson from '~tests/unit/resources/document.json'
+
+vi.mock('@/api/apiInstance', async () => {
+  const { default: data } = await import('~tests/unit/resources/document.json')
+
+  return {
+    apiInstance: {
+      getSource: vi.fn().mockResolvedValue({ data })
+    }
+  }
+})
 
 describe('DocumentViewerJson.vue', () => {
-  let api, wrapper
+  let wrapper
 
   beforeEach(async () => {
-    api = { getSource: vi.fn().mockResolvedValue({ data: documentJson }) }
-    const { plugins } = CoreSetup.init(api).useAll()
+    const { plugins } = CoreSetup.init().useAll()
     wrapper = shallowMount(DocumentViewerJson, { global: { plugins }, props: { document: { url: 'document.json' } } })
     await flushPromises()
   })
