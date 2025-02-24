5fc0c4267190490f2c17f8623a9486f27926e359
Pierre Romera <hello@pirhoo.com>
2025-02-24 15:37:41 +0000

    refactor: useRouterWithoutGuards in test setup

--- tests/unit/specs/composables/task-settings.spec.js
+++ tests/unit/specs/composables/task-settings.spec.js
@@ -28,19 +28,19 @@ describe('useTaskSettings', () => {
   }
 
   it('should initialize "app/task" settings with sort by to name / desc', () => {
-    const { store, router, i18n } = CoreSetup.init().useAll().useRouter()
+    const { store, router, i18n } = CoreSetup.init().useAll().useRouterWithoutGuards()
     const { sortBy } = withSetup({ store, router, i18n, composable: () => useTaskSettings('task') })
     expect(sortBy.value.modelValue).toEqual(['name', 'desc'])
   })
 
   it('should initialize "app/entities" task settings per page is 10', () => {
-    const { store, router, i18n } = CoreSetup.init().useAll().useRouter()
+    const { store, router, i18n } = CoreSetup.init().useAll().useRouterWithoutGuards()
     const { perPage } = withSetup({ store, router, i18n, composable: () => useTaskSettings('entities') })
     expect(perPage.value.modelValue).toEqual('10')
   })
 
   it('should initialize "app/batch-download" task settings propertiesModelValueOptions contains correct properties', () => {
-    const { store, router, i18n } = CoreSetup.init().useAll().useRouter()
+    const { store, router, i18n } = CoreSetup.init().useAll().useRouterWithoutGuards()
     const { propertiesModelValueOptions } = withSetup({
       store,
       router,
