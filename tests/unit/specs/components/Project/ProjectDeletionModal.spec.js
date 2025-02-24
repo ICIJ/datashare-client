3b0912ccc958179a984a29920960b57af268aabc
Pierre Romera <hello@pirhoo.com>
2025-02-24 15:14:47 +0000

    refactor: mock api instance instead of passing it as a Core parameter

--- tests/unit/specs/components/Project/ProjectDeletionModal.spec.js
+++ tests/unit/specs/components/Project/ProjectDeletionModal.spec.js
@@ -3,19 +3,29 @@ import { shallowMount } from '@vue/test-utils'
 import CoreSetup from '~tests/unit/CoreSetup'
 import ProjectDeletionModal from '@/components/Project/ProjectDeletionModal'
 import esConnectionHelper from '~tests/unit/specs/utils/esConnectionHelper'
+import { apiInstance as api } from '@/api/apiInstance'
+
+vi.mock('@/api/apiInstance', () => {
+  return {
+    apiInstance: {
+      deleteProject: vi.fn(),
+      getRecommendationsByProject: vi.fn(),
+      elasticsearch: {
+        countDocuments: vi.fn().mockResolvedValue(50),
+        countTags: vi.fn().mockResolvedValue(2)
+      }
+    }
+  }
+})
 
 describe('ProjectDeletionModal.vue', () => {
-  let api, plugins, project
+  let plugins, project
 
   beforeEach(() => {
-    const { index: name, es: elasticsearch } = esConnectionHelper.build()
-    api = { elasticsearch, deleteProject: vi.fn(), getRecommendationsByProject: vi.fn() }
-    api.elasticsearch.deleteProject = vi.fn()
-    api.elasticsearch.countDocuments = vi.fn().mockResolvedValue(50)
-    api.elasticsearch.countTags = vi.fn().mockResolvedValue(2)
+    const { index: name } = esConnectionHelper.build()
     // The modal uses the router to redirect to the project list so the route must exist
     const routes = [{ name: 'project.list', path: '/project' }]
-    const core = CoreSetup.init(api).useAll().useRouter(routes)
+    const core = CoreSetup.init().useAll().useRouter(routes)
     // Ensure the local-datashare project can be found
     core.config.set('projects', [{ name, label: 'Default', sourcePath: '/' }])
     plugins = core.plugins
