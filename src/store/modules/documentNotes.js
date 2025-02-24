41b7b38e6999a9bb2b3a888511636ec70b56c9bd
Pierre Romera <hello@pirhoo.com>
2025-02-24 15:06:57 +0000

    refactor: use top-level api instead of parameter

--- src/store/modules/documentNotes.js
+++ src/store/modules/documentNotes.js
@@ -2,66 +2,64 @@ import hasIn from 'lodash/hasIn'
 import { reactive } from 'vue'
 import { defineStore } from 'pinia'
 
-import { apiInstance } from '@/api/apiInstance'
+import { apiInstance as api } from '@/api/apiInstance'
 
-export const useDocumentNotesStore = (api = apiInstance) => {
-  return defineStore('documentNotes', () => {
-    const notes = reactive({})
+export const useDocumentNotesStore = defineStore('documentNotes', () => {
+  const notes = reactive({})
 
-    /**
-     * Delete all notes for each project
-     * @returns {void}
-     */
-    const reset = () => Object.keys(notes).forEach((key) => delete notes[key])
+  /**
+   * Delete all notes for each project
+   * @returns {void}
+   */
+  const reset = () => Object.keys(notes).forEach((key) => delete notes[key])
 
-    /**
-     * Set a list of notes for a given project
-     * @param {Object} options
-     * @param {string} options.project - The project name
-     * @param {Array} options.notes - The notes to set
-     * @returns {Array} The notes set
-     */
-    const set = ({ project, notes: values }) => {
-      notes[project] = values
-      return values
+  /**
+   * Set a list of notes for a given project
+   * @param {Object} options
+   * @param {string} options.project - The project name
+   * @param {Array} options.notes - The notes to set
+   * @returns {Array} The notes set
+   */
+  const set = ({ project, notes: values }) => {
+    notes[project] = values
+    return values
+  }
+
+  /**
+   * Fetch notes from the API for a given project
+   * @param {Object} options
+   * @param {string} options.project - The project name
+   * @returns {Promise<Array>} The notes set
+   */
+  const fetchNotes = async ({ project }) => {
+    const notes = await api.retrieveNotes(project)
+    return set({ project, notes })
+  }
+
+  /**
+   * Fetch notes once from the API for a given project
+   * @param {Object} options
+   * @param {string} options.project - The project name
+   * @returns {Promise<Array>} The notes set
+   */
+  const fetchNotesOnce = async ({ project }) => {
+    if (hasIn(notes, project)) {
+      return notes[project]
     }
+    return await fetchNotes({ project })
+  }
 
-    /**
-     * Fetch notes from the API for a given project
-     * @param {Object} options
-     * @param {string} options.project - The project name
-     * @returns {Promise<Array>} The notes set
-     */
-    const fetchNotes = async ({ project }) => {
-      const notes = await api.retrieveNotes(project)
-      return set({ project, notes })
-    }
+  /**
+   * Fetch notes for a given path.
+   * @param {Object} options
+   * @param {string} options.project - The project name
+   * @param {string} options.path - The path to filter on
+   * @returns {Promise<Array>} The notes set
+   */
+  const fetchNotesByPath = async ({ project, path }) => {
+    const notes = await fetchNotesOnce({ project })
+    return notes.filter((note) => path.startsWith(note.path))
+  }
 
-    /**
-     * Fetch notes once from the API for a given project
-     * @param {Object} options
-     * @param {string} options.project - The project name
-     * @returns {Promise<Array>} The notes set
-     */
-    const fetchNotesOnce = async ({ project }) => {
-      if (hasIn(notes, project)) {
-        return notes[project]
-      }
-      return await fetchNotes({ project })
-    }
-
-    /**
-     * Fetch notes for a given path.
-     * @param {Object} options
-     * @param {string} options.project - The project name
-     * @param {string} options.path - The path to filter on
-     * @returns {Promise<Array>} The notes set
-     */
-    const fetchNotesByPath = async ({ project, path }) => {
-      const notes = await fetchNotesOnce({ project })
-      return notes.filter((note) => path.startsWith(note.path))
-    }
-
-    return { notes, reset, set, fetchNotesByPath, fetchNotes, fetchNotesOnce }
-  })()
-}
+  return { notes, reset, set, fetchNotesByPath, fetchNotes, fetchNotesOnce }
+})
