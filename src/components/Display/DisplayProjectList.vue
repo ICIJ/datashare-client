9cdea2cc4d9250a150f7a3a2a4d9b2cea36e245f
Caroline Desprat <cdesprat@icij.org>
2025-02-24 14:24:31 +0000

    feat: create display project list UI component

---
+++ src/components/Display/DisplayProjectList.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { computed } from 'vue'
+import { isString } from 'lodash'
+
+import ProjectLink from '@/components/Project/ProjectLink'
+const props = defineProps({
+  values: {
+    type: [Array, String],
+    default: () => []
+  }
+})
+const projectList = computed(() => {
+  return isString(props.values) ? [props.values] : props.values
+})
+</script>
+
+<template>
+  <div class="d-flex gap-3">
+    <project-link v-for="(project, index) in projectList" :key="index" :project="project" />
+  </div>
+</template>
