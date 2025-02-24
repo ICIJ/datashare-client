e72d0539b25704183616988581b62690bc5946c5
Caroline Desprat <cdesprat@icij.org>
2025-02-24 14:23:44 +0000

    refactor: change script/template order

--- src/views/Task/TaskBatchSearch/TaskBatchSearchNew.vue
+++ src/views/Task/TaskBatchSearch/TaskBatchSearchNew.vue
@@ -1,12 +1,3 @@
-<template>
-  <page-header no-toggle-settings />
-  <div class="task-documents-new">
-    <page-container fluid>
-      <task-batch-search-form :name="projectName" />
-    </page-container>
-  </div>
-</template>
-
 <script setup>
 import PageContainer from '@/components/PageContainer/PageContainer'
 import PageHeader from '@/components/PageHeader/PageHeader'
@@ -19,3 +10,12 @@ defineProps({
   }
 })
 </script>
+
+<template>
+  <page-header no-toggle-settings />
+  <div class="task-documents-new">
+    <page-container fluid>
+      <task-batch-search-form :name="projectName" />
+    </page-container>
+  </div>
+</template>
