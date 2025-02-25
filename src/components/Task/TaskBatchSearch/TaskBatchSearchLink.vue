ac760df0ecba58798c0450d5d626ff4d47bafe6f
Caroline Desprat <cdesprat@icij.org>
2025-02-25 10:56:33 +0000

    feat: add task links

---
+++ src/components/Task/TaskBatchSearch/TaskBatchSearchLink.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { computed } from 'vue'
+import get from 'lodash/get'
+const props = defineProps({
+  item: { type: Object, required: true }
+})
+function getRecord(key, defaultValue = undefined) {
+  return get(props.item, `args.batchRecord.${key}`, defaultValue)
+}
+const to = computed(() => {
+  return {
+    name: 'task.batch-search.view',
+    params: { uuid: getRecord('uuid'), indices: getRecord('projects').join(',') }
+  }
+})
+</script>
+
+<template>
+  <router-link :to="to"> {{ getRecord('name') }}</router-link>
+</template>
