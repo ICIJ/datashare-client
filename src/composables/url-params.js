52e0d41d9e4043b32f21342254d013487b5ac510
Pierre Romera <hello@pirhoo.com>
2025-02-26 18:10:26 +0000

    feat: add useUrlPageParam helper

--- src/composables/url-params.js
+++ src/composables/url-params.js
@@ -369,6 +369,19 @@ export function useUrlParams(queryParams, initialOrOptions = {}) {
 }
 
 /**
+ * Creates a reactive page number synchronized with the 'page' URL query parameter.
+ * 
+ * @param {number} [max=Infinity] - The maximum page number allowed.
+ * @returns {ComputedRef<number>} - A computed reactive reference to the current page number.
+ */
+export function useUrlPageParam(max = Infinity) {
+  return useUrlParam('page', {
+    transform: (value) => Math.min(parseInt(value), max),
+    initialValue: 1
+  })
+}
+
+/**
  * Creates a reactive page number synchronized with the 'from' URL query parameter.
  *
  * This composable function calculates the page number based on the 'from' query parameter and 'perPage' value.
