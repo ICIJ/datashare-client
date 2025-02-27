95bb4d77002fa844771f0a429e0cb19e943c77e5
Pierre Romera <hello@pirhoo.com>
2025-02-27 15:21:22 +0000

    feat: add field icons and translation

--- src/components/Search/SearchSavedEntries/SearchSavedEntries.vue
+++ src/components/Search/SearchSavedEntries/SearchSavedEntries.vue
@@ -1,11 +1,16 @@
 <script setup>
-import ButtonIcon from '@/components/Button/ButtonIcon'
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+
 import DisplayDatetime from '@/components/Display/DisplayDatetime'
 import DisplayUser from '@/components/Display/DisplayUser'
+import SearchSavedEntriesRowActions from '@/components/Search/SearchSavedEntries/SearchSavedEntriesRowActions'
+import SearchSavedEntriesRowDetails from '@/components/Search/SearchSavedEntries/SearchSavedEntriesRowDetails'
 import PageTableGeneric from '@/components/PageTable/PageTableGeneric'
 
 const sort = defineModel('sort', { type: String, default: null })
 const order = defineModel('order', { type: String, default: 'desc' })
+const { t } = useI18n()
 
 defineProps({
   events: {
@@ -16,17 +21,20 @@ defineProps({
 const fields = [
   {
     value: 'name',
-    text: 'name',
+    icon: 'list-checks',
+    text: computed(() => t('searchSavedEntries.fields.name')),
     sortable: true,
     emphasis: true
   },
   {
     value: 'user',
-    text: 'author'
+    icon: 'user-circle',
+    text: computed(() => t('searchSavedEntries.fields.author')),
   },
   {
     value: 'creation_date',
-    text: 'creationDate',
+    icon: 'calendar-blank',
+    text: computed(() => t('searchSavedEntries.fields.creationDate')),
     sortable: true
   }
 ]
@@ -41,9 +49,9 @@ function searchParamsQuery(uri) {
 </script>
 
 <template>
-  <page-table-generic v-model:sort="sort" v-model:order="order" :items="events" :fields="fields" show-row-details>
+  <page-table-generic primary-key="id" v-model:sort="sort" v-model:order="order" :items="events" :fields="fields" show-row-details>
     <template #cell(name)="{ item }">
-      <router-link :to="{ name: 'search', query: searchParamsQuery(item.uri) }">
+      <router-link :to="{ name: 'search', query: searchParamsQuery(item.uri) }" class="fw-medium">
         {{ item.name }}
       </router-link>
     </template>
@@ -53,24 +61,11 @@ function searchParamsQuery(uri) {
     <template #cell(creation_date)="{ item }">
       <display-datetime :value="item.creationDate" />
     </template>
-    <template #row-actions="{ toggleDetails, detailsShowing }">
-      <div class="d-flex gap-2">
-        <button-icon
-          :icon-left="detailsShowing.value ? PhCaretUp : PhCaretDown"
-          icon-left-hover-weight="bold"
-          hide-label
-          square
-          size="sm"
-          variant="outline-secondary"
-          label="Toggle details"
-          @click="toggleDetails"
-        />
-      </div>
+    <template #row-actions="{ item, detailsShowing, toggleDetails }">
+      <search-saved-entries-row-actions :details-showing="detailsShowing" @toggle="toggleDetails" :event="item" />
     </template>
     <template #row-details="{ item }">
-      <div>
-        <pre>{{ item }}</pre>
-      </div>
+      <search-saved-entries-row-details :event="item" />
     </template>
   </page-table-generic>
 </template>
