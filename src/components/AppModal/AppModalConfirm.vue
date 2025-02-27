03ee7b890dc4fec28448f3aa26dfa6898c0661cc
Pierre Romera <hello@pirhoo.com>
2025-02-27 17:24:28 +0000

    feat: add illustration to confirm modal

--- src/components/AppModal/AppModalConfirm.vue
+++ src/components/AppModal/AppModalConfirm.vue
@@ -1,6 +1,9 @@
 <script setup>
 import { useAttrs } from 'vue'
 
+import image from '@/assets/images/illustrations/app-modal-default-light.svg'
+import imageDark from '@/assets/images/illustrations/app-modal-default-dark.svg'
+
 import AppModal from '@/components/AppModal/AppModal'
 
 defineProps({
@@ -14,7 +17,7 @@ const attrs = useAttrs()
 </script>
 
 <template>
-  <app-modal v-bind="attrs" :title="$t('appModalConfirm.title')" size="md">
+  <app-modal v-bind="attrs" :image="image" :image-dark="imageDark" :title="$t('appModalConfirm.title')" size="410px">
     <slot>
       {{ description || $t('appModalConfirm.description') }}
     </slot>
