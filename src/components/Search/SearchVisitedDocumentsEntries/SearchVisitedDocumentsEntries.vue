668462a6d18529f59928ddd83510c74f943bb4de
Pierre Romera <hello@pirhoo.com>
2025-02-28 17:24:17 +0000

    feat: add search visited documents entries

---
+++ src/components/Search/SearchVisitedDocumentsEntries/SearchVisitedDocumentsEntries.vue
@@ -0,0 +1,112 @@
+<script setup>
+import { computed, ref, watch } from 'vue'
+import bodybuilder from 'bodybuilder'
+import { groupBy, property, trimStart, uniq } from 'lodash'
+import { match } from 'path-to-regexp'
+
+import { useCore } from '@/composables/core'
+import ButtonToggleDay from '@/components/Button/ButtonToggleDay'
+import DocumentCard from '@/components/Document/DocumentCard/DocumentCard'
+import DocumentActionsGroup from '@/components/Document/DocumentActionsGroup/DocumentActionsGroup'
+import DisplayTime from '@/components/Display/DisplayTime'
+import EsDocList from '@/api/resources/EsDocList'
+import { useStarredStore } from '@/store/modules'
+
+const { events } = defineProps({
+  events: {
+    type: Array
+  }
+})
+
+const { core } = useCore()
+const starredStore = useStarredStore()
+
+const hits = ref([])
+// This is a list of the ids of the events.
+const eventsIds = computed(() => events.map(eventParams).map(property('id')))
+
+const entries = computed(() => {
+  return hits
+    .value
+    // This create a list of tuples where the first element is the document and the second is the event.
+    .map((document) => {
+      const event = events.find((event) => eventParams(event).id === document.id)
+      return [document, event]
+    })
+    // This filter out the documents that don't have a corresponding event.
+    .filter(([document, event]) => !!event)
+})
+
+const groupedEntries = computed(() => {
+  return groupBy(entries.value, ([, event]) => {
+    const modificationDate = new Date(event.modificationDate)
+    // The `toDateString` method returns only the date portion of the date.
+    // @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toDateString
+    // @example "Wed Jul 28 1993"
+    return modificationDate.toDateString()
+  })
+})
+
+const matchDocumentPath = match('/ds/:index/:id{/:routing}')
+
+function eventParams({ uri }) {
+  const path = `/${trimStart(uri.split('?').shift(0), '/')}`
+  const { params } = matchDocumentPath(path)
+  return params || {}
+}
+
+function buildEventsBody() {
+  const values = eventsIds.value
+  const excludes = ['content', 'content_translated']
+  return bodybuilder()
+    .size(values.length)
+    .rawOption('_source', { excludes })
+    .query('ids', { values })
+    .sort("_script", {
+      type: "number",
+      script: {
+        source: "params.order.indexOf(doc._id.value)",
+        params: { order: values }
+      }
+    })
+    .build()
+}
+
+async function fetch() {
+  const preference = 'search-visited-documents'
+  const body = buildEventsBody()
+  const indices = uniq(events.map(eventParams).map(property('index')))
+  const index = indices.join(',')
+  const response = await core.api.elasticsearch.search({ index, body, preference })
+  await starredStore.fetchIndicesStarredDocuments(indices)
+  hits.value = (new EsDocList(response)).hits
+}
+
+watch(() => (eventsIds.value), fetch, { deep: true, immediate: true })
+</script>
+
+<template>
+  <b-collapse visible v-for="(entry, key) of groupedEntries" :key="key">
+    <template #header="{ visible, toggle }">
+      <div class="text-center mb-3">
+        <button-toggle-day :date="key" :active="visible" @click="toggle" />
+      </div>
+    </template>
+    <div class="d-flex flex-column gap-3 mb-3">
+      <document-card
+        v-for="([document, event], i) in entry"
+        route-name="document-standalone"
+        :key="i"
+        :document="document"
+        :properties="['title', 'thumbnail', 'path', 'creationDate']"
+      >
+        <template #actions>
+          <div class="d-flex align-items-center">
+            <document-actions-group tooltip-placement="right" :document="document" />
+            <display-time :value="event.modificationDate" class="text-secondary" />
+          </div>
+        </template>
+      </document-card>
+    </div>
+  </b-collapse>    
+</template>
