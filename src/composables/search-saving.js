07691c350b328ce67e795677c3b5a8d4976f7c8e
Pierre Romera <hello@pirhoo.com>
2025-02-28 16:35:20 +0000

    fix: add missing return

--- src/composables/search-saving.js
+++ src/composables/search-saving.js
@@ -63,7 +63,7 @@ export function useSearchSaving() {
 
   function save({ name, id = null }) {
     if (id) {
-      core.api.renameSavedSearch(id, name)
+      return core.api.renameSavedSearch(id, name)
     }
     return core.api.addUserHistoryEvent(indices.value, 'SEARCH', name, searchRoute.value.fullPath)
   }
