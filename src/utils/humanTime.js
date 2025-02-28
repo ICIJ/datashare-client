cf3e7d197877b8b0bb3b4e6fdfec3e8dc1f865d4
Pierre Romera <hello@pirhoo.com>
2025-02-28 18:13:57 +0000

    refactor: lint

--- src/utils/humanTime.js
+++ src/utils/humanTime.js
@@ -2,7 +2,7 @@ import moment from 'moment'
 
 /**
  * Formats a date to a human-readable form: LT.
- * 
+ *
  * @param {string|Date} date - The date to be formatted.
  * @param {string} locale - The locale code for formatting the date.
  * @returns {string} The formatted date.
