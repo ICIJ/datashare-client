54a766354644c43d10ac6a684d374d00b89cff60
Pierre Romera <hello@pirhoo.com>
2025-02-28 16:35:03 +0000

    feat: add DisplayTime

---
+++ src/components/Display/DisplayTime.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+import { humanLongDate } from '@/utils/humanDate'
+import { humanTime } from '@/utils/humanTime'
+
+const { locale } = useI18n()
+
+const props = defineProps({
+  value: {
+    type: [String, Number, Date],
+    required: true
+  },
+  noTooltip: {
+    type: Boolean
+  }
+})
+
+const title = computed(() => {
+  if (!props.noTooltip) {
+    return humanLongDate(props.value, locale.value)
+  }
+  return null
+})
+
+const display = computed(() => {
+  if (!props.value) {
+    return ''
+  }
+
+  return humanTime(props.value, locale.value)
+})
+</script>
+
+<template>
+  <span
+    v-b-tooltip.body
+    class="display-time d-inline-flex align-items-center flex-wrap"
+    :title="title"
+    aria-label="time"
+  >
+    <slot v-bind="{ display }">
+      {{ display }}
+    </slot>
+  </span>
+</template>
