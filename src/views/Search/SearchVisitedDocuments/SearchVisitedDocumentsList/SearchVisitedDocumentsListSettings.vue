b511303a8e6a821d88fd5500809c8f7bc42ec0ba
Pierre Romera <hello@pirhoo.com>
2025-02-28 17:17:13 +0000

    fix: use correct i18n key

---
+++ src/views/Search/SearchVisitedDocuments/SearchVisitedDocumentsList/SearchVisitedDocumentsListSettings.vue
@@ -0,0 +1,109 @@
+<script setup>
+import { noop } from 'lodash'
+import { ref } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+import { useUrlPageParam, useUrlParamWithStore, useUrlParamsWithStore } from '@/composables/url-params'
+import PageSettings from '@/components/PageSettings/PageSettings'
+import PageSettingsSection from '@/components/PageSettings/PageSettingsSection'
+import { useViewSettings, SORT_ORDER_KEY, SORT_TYPE_KEY } from '@/composables/view-settings'
+import { useAppStore } from '@/store/modules'
+
+const { t } = useI18n()
+const { sortByLabel, tSortByOption, perPageLabel } = useViewSettings(t)
+const appStore = useAppStore()
+const view = 'searchVisitedDocumentsList'
+
+const page = useUrlPageParam()
+
+const perPage = ref({
+  label: perPageLabel('searchVisitedDocumentsList.title'),
+  type: 'radio',
+  open: true,
+  modelValue: useUrlParamWithStore('perPage', {
+    transform: (value) => Math.max(10, parseInt(value)),
+    get: () => appStore.getSettings(view, 'perPage'),
+    set: (perPage) => {
+      appStore.setSettings({ view, perPage })
+      page.value = 1
+    }
+  }),
+  options: [
+    {
+      value: 10
+    },
+    {
+      value: 25
+    },
+    {
+      value: 50
+    },
+    {
+      value: 100
+    }
+  ]
+})
+
+const sortBy = ref({
+  label: sortByLabel,
+  type: 'radio',
+  open: true,
+  modelValue: useUrlParamsWithStore(['sort', 'order'], {
+    get: () => appStore.getSettings(view, 'orderBy'),
+    set: (sort, order) => {
+      appStore.setSettings({ view, orderBy: [sort, order] })
+      page.value = 1
+    }
+  }),
+  options: [
+    {
+      value: ['name', 'asc'],
+      text: tSortByOption('name', SORT_ORDER_KEY.ASC, SORT_TYPE_KEY.ALPHA)
+    },
+    {
+      value: ['name', 'desc'],
+      text: tSortByOption('name', SORT_ORDER_KEY.DESC, SORT_TYPE_KEY.ALPHA)
+    },
+    {
+      value: ['modification_date', 'asc'],
+      text: tSortByOption('visitDate', SORT_ORDER_KEY.ASC, SORT_TYPE_KEY.DATE)
+    },
+    {
+      value: ['modification_date', 'desc'],
+      text: tSortByOption('visitDate', SORT_ORDER_KEY.DESC, SORT_TYPE_KEY.DATE)
+    }
+  ]
+})
+
+defineProps({
+  hide: {
+    type: Function,
+    default: noop
+  },
+  visible: {
+    type: Boolean
+  },
+  placement: {
+    type: String
+  }
+})
+</script>
+
+<template>
+  <page-settings title="Page settings" :hide="hide" :visible="visible" :placement="placement">
+    <page-settings-section
+      v-model="sortBy.modelValue"
+      v-model:open="sortBy.open"
+      :type="sortBy.type"
+      :options="sortBy.options"
+      :label="sortBy.label"
+    />
+    <page-settings-section
+      v-model="perPage.modelValue"
+      v-model:open="perPage.open"
+      :type="perPage.type"
+      :options="perPage.options"
+      :label="perPage.label"
+    />
+  </page-settings>
+</template>
