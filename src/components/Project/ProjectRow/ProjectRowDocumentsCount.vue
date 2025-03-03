7ef7d04d6767c1f4ac078ce20a20c33a5eb3c90a
Pierre Romera <hello@pirhoo.com>
2025-03-03 18:37:13 +0000

    fix: show count when 0 documents

--- src/components/Project/ProjectRow/ProjectRowDocumentsCount.vue
+++ src/components/Project/ProjectRow/ProjectRowDocumentsCount.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { computed } from 'vue'
+import { isNumber } from 'lodash'
 import { useI18n } from 'vue-i18n'
 
 import humanNumber from '@/utils/humanNumber'
@@ -21,7 +22,7 @@ const humanDocumentsCount = computed(() => {
 <template>
   <td class="project-row-documents-count text-end">
     <span
-      v-if="project.documentsCount"
+      v-if="isNumber(project.documentsCount)"
       v-b-tooltip.body
       class="text-secondary-emphasis"
       :title="$n(project.documentsCount)"
