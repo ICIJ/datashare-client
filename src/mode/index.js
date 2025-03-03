a92162742102781f14aba8ad222b2f353e443419
Pierre Romera <hello@pirhoo.com>
2025-03-03 14:13:06 +0000

    refactor: remove unused mode option

--- src/mode/index.js
+++ src/mode/index.js
@@ -7,19 +7,16 @@ export const MODE_NAME = Object.freeze({
 const MODES = Object.freeze({
   [MODE_NAME.LOCAL]: {
     modeName: MODE_NAME.LOCAL,
-    multipleProjects: true,
     manageDocuments: true,
     previewHost: import.meta.env?.VITE_DS_PREVIEW_HOST
   },
   [MODE_NAME.EMBEDDED]: {
     modeName: MODE_NAME.EMBEDDED,
-    multipleProjects: true,
     manageDocuments: true,
     previewHost: import.meta.env?.VITE_DS_PREVIEW_HOST
   },
   [MODE_NAME.SERVER]: {
     modeName: MODE_NAME.SERVER,
-    multipleProjects: true,
     manageDocuments: false,
     previewHost: import.meta.env?.VITE_DS_PREVIEW_HOST
   }
