8de84b26c2e126d8fff5c0721340116f513cc209
Pierre Romera <hello@pirhoo.com>
2025-03-03 14:13:19 +0000

    feat: add isLocal and isEmbedded to composable

--- src/composables/mode.js
+++ src/composables/mode.js
@@ -7,9 +7,11 @@ export function useMode() {
   const { core } = useCore()
   const mode = computed(() => core.mode)
   const isServer = computed(() => mode.value.modeName === MODE_NAME.SERVER)
+  const isLocal = computed(() => mode.value.modeName === MODE_NAME.LOCAL)
+  const isEmbedded = computed(() => mode.value.modeName === MODE_NAME.EMBEDDED)
   const isMode = (name) => mode.value.modeName === name.toUpperCase()
 
-  return { mode, isServer, isMode }
+  return { mode, isServer, isLocal, isEmbedded, isMode }
 }
 
 export default useMode
