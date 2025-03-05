d3d7f492c632dd34ce25fad226f1a57a4712c437
Caroline Desprat <cdesprat@icij.org>
2025-03-05 10:17:09 +0000

    refactor: use verb for emit events

--- src/components/Search/SearchSavedEntries/SearchSavedEntriesRowActions.vue
+++ src/components/Search/SearchSavedEntries/SearchSavedEntriesRowActions.vue
@@ -1,9 +1,9 @@
 <script setup>
 import { useI18n } from 'vue-i18n'
 
-import ButtonIcon from '@/components/Button/ButtonIcon'
 import { useSearchSavingModal, useRemoveSavedSearchModal } from '@/composables/search-saving'
 import PageTableToggleDetailsButton from '@/components/PageTable/PageTableToggleDetailsButton'
+import ButtonRowAction from '@/components/Button/ButtonRowAction'
 const detailsShowing = defineModel('toggle', { type: Boolean })
 const { event } = defineProps({
   event: {
@@ -20,40 +20,20 @@ const { show: showRemoveSavedSearchModal } = useRemoveSavedSearchModal()
 
 const confirmEditModal = async () => {
   await showEditModal(event)
-  emit('edited')
+  emit('edit')
 }
 
 const confirmRemovalModal = async () => {
   const title = t('searchSavedEntriesRowActions.removeTitle', { name: event.name })
   await showRemoveSavedSearchModal(event, { title })
-  emit('removed')
+  emit('remove')
 }
 </script>
 
 <template>
   <div class="d-flex gap-2">
-    <button-icon
-      :icon-left="PhPencilSimple"
-      icon-left-hover-weight="bold"
-      hide-label
-      square
-      size="sm"
-      variant="outline-secondary"
-      label="Edit"
-      class="border-0"
-      @click="confirmEditModal"
-    />
-    <button-icon
-      :icon-left="PhTrash"
-      icon-left-hover-weight="bold"
-      hide-label
-      square
-      size="sm"
-      variant="outline-secondary"
-      label="Delete"
-      class="border-0"
-      @click="confirmRemovalModal"
-    />
+    <button-row-action label="Edit" icon="pencil" @click="confirmEditModal" />
+    <button-row-action label="Delete" icon="trash" @click="confirmRemovalModal" />
     <page-table-toggle-details-button v-model="detailsShowing" />
   </div>
 </template>
