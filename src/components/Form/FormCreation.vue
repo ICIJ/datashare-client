08c0d07088c4e1e21e57537a502f72f09c13ee4b
Pierre Romera <hello@pirhoo.com>
2025-03-05 17:46:16 +0000

    feat: add submitIcon prop to FormCreation component

--- src/components/Form/FormCreation.vue
+++ src/components/Form/FormCreation.vue
@@ -16,6 +16,9 @@ const props = defineProps({
   submitLabel: {
     type: String
   },
+  submitIcon: {
+    type: [String, Object, Array]
+  },
   resetLabel: {
     type: String
   }
@@ -36,25 +39,23 @@ function submit(values) {
 <template>
   <b-form class="form-creation" novalidate @reset.stop.prevent="reset" @submit.stop.prevent="submit">
     <div aria-description="form-content" :class="contentClassList">
-      <slot></slot>
+      <slot />
     </div>
     <div class="d-flex justify-content-end mb-4" aria-description="form-footer">
-      <span class="d-flex gap-2">
+      <span class="d-flex gap-4">
         <button-icon
           type="reset"
           variant="outline-light"
           icon-left="arrow-counter-clockwise"
-          class="form-creation__action--reset btn btn-outline-action"
+          class="btn btn-outline-action"
           :label="resetLabel ?? t('global.reset')"
         >
           <slot name="reset-text" />
         </button-icon>
-        <b-button type="submit" variant="action" class="ms-2" :disabled="!valid">
+        <button-icon type="submit" variant="action" :disabled="!valid" :icon-right="submitIcon">
           <slot name="submit-text">{{ submitLabel ?? t('global.submit') }}</slot>
-        </b-button>
+        </button-icon>
       </span>
     </div>
   </b-form>
 </template>
-
-<style scoped lang="scss"></style>
