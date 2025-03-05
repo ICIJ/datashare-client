247ad8566e2c1a52952740e5b6251a22db149e58
Caroline Desprat <cdesprat@icij.org>
2025-03-05 10:07:32 +0000

    feat: create button row action

---
+++ src/components/Button/ButtonRowAction.vue
@@ -0,0 +1,24 @@
+<script setup>
+import ButtonIcon from '@/components/Button/ButtonIcon'
+import { SIZE, buttonSizeValidator } from '@/enums/sizes'
+defineProps({
+  icon: { type: [String, Object], required: true },
+  size: { type: String, validator: buttonSizeValidator, default: SIZE.SM },
+  label: { type: String },
+  disabled: { type: Boolean, default: false }
+})
+</script>
+
+<template>
+  <button-icon
+    :icon-left="icon"
+    icon-left-hover-weight="bold"
+    hide-label
+    :disabled="disabled"
+    square
+    :size="size"
+    variant="outline-secondary"
+    :label="label"
+    class="border-0"
+  />
+</template>
