f0ede21b8405eaae24b5e465519533aa30e128ea
Pierre Romera <hello@pirhoo.com>
2025-03-06 10:46:59 +0000

    feat: update Vue3Toastify configuration to hide progress bar and disable auto close

--- src/core/Core.js
+++ src/core/Core.js
@@ -196,7 +196,7 @@ class Core extends Behaviors {
     // to avoid adding them twice to the Vue instance.
     this.use(Murmur, { useI18n: false, useBootstrap: false })
     // Common plugins
-    this.use(Vue3Toastify, { clearOnUrlChange: false, hideProgressBar: false, autoClose: 5000 })
+    this.use(Vue3Toastify, { clearOnUrlChange: false, hideProgressBar: true, autoClose: false })
     this.use(VueShortkey, { prevent: settings.hotKeyPrevented })
     this.use(VueScrollTo)
     // Setup VCalendar manually since Webpack is not compatible with
