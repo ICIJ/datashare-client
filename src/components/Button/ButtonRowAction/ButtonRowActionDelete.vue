5633e0aea14ddd9c2aa863c9a4258df04631db27
Caroline Desprat <cdesprat@icij.org>
2025-03-07 11:19:25 +0000

    feat: create button row action buttons

---
+++ src/components/Button/ButtonRowAction/ButtonRowActionDelete.vue
@@ -0,0 +1,19 @@
+<script setup>
+import ButtonRowAction from '@/components/Button/ButtonRowAction/ButtonRowAction'
+import { useConfirmModal } from '@/composables/confirm'
+defineProps({
+  disabled: { type: Boolean }
+})
+const emit = defineEmits(['delete'])
+const { confirm: showConfirmModal } = useConfirmModal()
+
+async function onDelete() {
+  if (await showConfirmModal()) {
+    emit('delete')
+  }
+}
+</script>
+
+<template>
+  <button-row-action icon="trash" :disabled="disabled" :label="$t('buttonRowAction.delete')" @click="onDelete" />
+</template>
