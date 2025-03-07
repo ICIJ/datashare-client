fb478fb86543ad12bd99a1700e3a967e9503307e
Caroline Desprat <cdesprat@icij.org>
2025-03-07 11:19:25 +0000

    feat: create button row action buttons

---
+++ src/components/Button/ButtonRowAction/ButtonRowActionRelaunch.vue
@@ -0,0 +1,19 @@
+<script setup>
+import ButtonRowAction from '@/components/Button/ButtonRowAction/ButtonRowAction'
+import { useConfirmModal } from '@/composables/confirm'
+defineProps({
+  disabled: { type: Boolean }
+})
+const emit = defineEmits(['relaunch'])
+const { confirm: showConfirmModal } = useConfirmModal()
+
+async function onRelaunch() {
+  if (await showConfirmModal()) {
+    emit('relaunch')
+  }
+}
+</script>
+
+<template>
+  <button-row-action icon="copy" :disabled="disabled" :label="$t('buttonRowAction.relaunch')" @click="onRelaunch" />
+</template>
---
+++ src/components/Button/ButtonRowAction/ButtonRowActionStop.vue
@@ -0,0 +1,19 @@
+<script setup>
+import ButtonRowAction from '@/components/Button/ButtonRowAction/ButtonRowAction'
+import { useConfirmModal } from '@/composables/confirm'
+defineProps({
+  disabled: { type: Boolean }
+})
+const emit = defineEmits(['stop'])
+const { confirm: showConfirmModal } = useConfirmModal()
+
+async function onStop() {
+  if (await showConfirmModal()) {
+    emit('stop')
+  }
+}
+</script>
+
+<template>
+  <button-row-action icon="stop" :disabled="disabled" :label="$t('buttonRowAction.stop')" @click="onStop" />
+</template>
