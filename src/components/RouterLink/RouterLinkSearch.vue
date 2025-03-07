94cb8c533a997bfef01ea64d949ec2bf03f3a9ad
Pierre Romera <hello@pirhoo.com>
2025-03-07 10:01:30 +0000

    refactor: make `to` responsive

--- src/components/RouterLink/RouterLinkSearch.vue
+++ src/components/RouterLink/RouterLinkSearch.vue
@@ -1,14 +1,24 @@
 <script setup>
 const props = defineProps({
-  indices: { type: String, required: true },
-  query: { type: String, required: true }
+  indices: { 
+    type: String, 
+    required: true 
+  },
+  query: { 
+    type: String, 
+    required: true 
+  }
 })
-const query = { q: props.query, indices: props.indices }
-const to = {
-  name: 'search',
-  query
-}
+
+const to = computed(() => {
+  const name = 'search'
+  const query = { q: props.query, indices: props.indices }
+  return { name, query }
+})
 </script>
+
 <template>
-  <router-link :to="to"><slot /></router-link>
+  <router-link :to="to">
+    <slot />
+  </router-link>
 </template>
