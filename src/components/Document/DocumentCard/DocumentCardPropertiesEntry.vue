a02c189c80e6f3cf7b4cdf14663504ba117843c3
Pierre Romera <hello@pirhoo.com>
2025-03-07 17:01:02 +0000

    fix: adjust icon container to prevent shrinking in document card properties entry

--- src/components/Document/DocumentCard/DocumentCardPropertiesEntry.vue
+++ src/components/Document/DocumentCard/DocumentCardPropertiesEntry.vue
@@ -26,7 +26,7 @@ defineProps({
 
 <template>
   <div class="document-card-properties-entry d-flex px-1 mb-2 text-secondary-emphasis">
-    <div v-if="!hideIcon" class="document-card-properties-entry__icon pe-2">
+    <div v-if="!hideIcon" class="document-card-properties-entry__icon pe-2 flex-shrink-0">
       <phosphor-icon :name="icon ?? propertiesIcon[property] ?? property" />
     </div>
     <span class="visually-hidden">{{ title ?? propertiesLabel[property] ?? property }}:</span>
