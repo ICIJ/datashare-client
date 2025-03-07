3d98234fede1f74f28b944d0c01daad962977c2f
Pierre Romera <hello@pirhoo.com>
2025-03-07 08:47:53 +0000

    feat: create AppOverlay

---
+++ src/components/AppOverlay/AppOverlay.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { computed } from 'vue'
+import { PhosphorIcon } from '@icij/murmur-next'
+
+import { useContrastVariant } from '@/composables/contrast-variant'
+
+const { contrastVariant: contrastOverlayVariant, variant: overlayVariant } = useContrastVariant()
+
+const { spinnerSmall } = defineProps({
+  spinnerSmall: {
+    type: Boolean,
+    default: false
+  },
+  spinnerVariant: {
+    type: String
+  },
+  variant: {
+    type: String,
+    default: 'primary'
+  }
+})
+
+const size = computed(() => spinnerSmall ? 'md' : 'xl')
+</script>
+
+<template>
+  <b-overlay :opacity="0.7" :variant="overlayVariant ?? variant">
+    <template #overlay>
+      <phosphor-icon 
+        spin 
+        :size="size"
+        :name="PhCircleNotch" 
+        :variant="spinnerVariant ?? contrastOverlayVariant" />
+    </template>
+    <slot />
+  </b-overlay>
+</template>
