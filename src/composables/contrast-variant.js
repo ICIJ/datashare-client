5c74e48bcbbf1c4df8a24a0c328ce7e3448bef33
Pierre Romera <hello@pirhoo.com>
2025-03-07 08:47:44 +0000

    feat: export useContrastVariant outside of default

--- src/composables/contrast-variant.js
+++ src/composables/contrast-variant.js
@@ -2,9 +2,11 @@ import { computed } from 'vue'
 
 import { useColorMode } from './color-mode'
 
-export default function useContrastVariant({ dark = 'dark', light = 'light' } = {}) {
+export function useContrastVariant({ dark = 'dark', light = 'light' } = {}) {
   const { colorMode } = useColorMode()
   const contrastVariant = computed(() => (colorMode.value === 'dark' ? light : dark))
   const variant = computed(() => (colorMode.value === 'dark' ? dark : light))
   return { variant, contrastVariant }
 }
+
+export default useContrastVariant
