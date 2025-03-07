18faea94d3ba8a3b4f31240ce70282e97ffa7023
Pierre Romera <hello@pirhoo.com>
2025-03-07 10:01:03 +0000

    refactor: format props definition and remove invalid prop

--- src/components/RouterLink/RouterLinkBatchDownload.vue
+++ src/components/RouterLink/RouterLinkBatchDownload.vue
@@ -3,21 +3,30 @@ import { PhosphorIcon } from '@icij/murmur-next'
 import { basename } from 'path'
 import { computed } from 'vue'
 import get from 'lodash/get'
+
 const props = defineProps({
-  item: { type: Object, required: true }
+  item: { 
+    type: Object, 
+    required: true 
+  }
 })
+
 function getRecord(key, defaultValue = undefined) {
   return get(props.item, `args.batchDownload.${key}`, defaultValue)
 }
+
 const text = computed(() => {
   return decodeURI(basename(getRecord('filename')))
 })
+
 const href = computed(() => {
   return `/api/task/${props.item.id}/result`
 })
+
 const exists = computed(() => {
   return getRecord('exists', false)
 })
+
 const tag = computed(() => {
   return exists.value ? 'a' : 'span'
 })
@@ -29,7 +38,7 @@ const icon = computed(() => {
 
 <template>
   <component :is="tag" :href="href" target="_blank" class="text-nowrap">
-    <phosphor-icon :name="icon" fixed-width />
+    <phosphor-icon :name="icon" />
     {{ text }}
   </component>
 </template>
