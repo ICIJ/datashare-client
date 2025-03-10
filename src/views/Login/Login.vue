baafdb780ac675da92d6461663d25ab54b3ee57c
Pierre Romera <hello@pirhoo.com>
2025-03-10 19:55:49 +0000

    refactor: use $t  directly

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -1,11 +1,28 @@
+<script setup>
+import { computed, useTemplateRef } from 'vue'
+
+import LoginImage from '@/components/Login/LoginImage'
+import ButtonIcon from '@/components/Button/ButtonIcon'
+import I18nLocaleDropdown from '@/components/I18n/I18nLocaleDropdown'
+import { useCore } from '@/composables/core'
+import settings from '@/utils/settings'
+
+const { core } = useCore()
+
+const image = useTemplateRef('image')
+const signinUrl = import.meta.env.VITE_DS_AUTH_SIGNIN
+const signinRoute = computed(() => ({ path: signinUrl }))
+const helpLink = core.vue.config.globalProperties.$config.get('helpLink', settings.helpLink)
+</script>
+
 <template>
   <div class="login d-flex flex-column justify-content-between align-items-stretch gap-2 p-4 vh-100">
     <div class="login__enter d-flex flex-column align-items-center text-action-emphasis p-4">
       <login-image ref="image" style="width: 240px" />
-      <h3>{{ welcomeLabel }}</h3>
-      <p>{{ taglineLabel }}</p>
+      <h3>{{ $t('login.welcome') }}</h3>
+      <p>{{ $t('login.tagline') }}</p>
       <button-icon
-        :label="loginLabel"
+        :label="$t('login.account')"
         :to="signinRoute"
         class="login__enter_link mt-4"
         icon-left="user"
@@ -16,42 +33,20 @@
     <div class="login__assistance d-flex flex-column align-items-center justify-content-end gap-2">
       <button-icon
         class="login__assistance__help"
-        :label="askHelpLabel"
+        :label="$t('login.askHelp')"
         tag="a"
         :href="helpLink"
         variant="outline-secondary"
         @click="image.shake()"
       />
       <div class="login__assistance__locale d-flex align-items-center gap-2">
-        <span>
-          {{ switchLanguageLabel }}
-        </span>
+        {{ $t('login.switchLanguage') }}
         <i18n-locale-dropdown class="px-2" />
       </div>
     </div>
   </div>
 </template>
-<script setup>
-import { computed, ref } from 'vue'
-import { useI18n } from 'vue-i18n'
 
-import LoginImage from '@/components/Login/LoginImage'
-import ButtonIcon from '@/components/Button/ButtonIcon'
-import I18nLocaleDropdown from '@/components/I18n/I18nLocaleDropdown'
-import { useCore } from '@/composables/core'
-import settings from '@/utils/settings'
-const image = ref(null)
-const { t } = useI18n()
-const loginLabel = computed(() => t('login.account'))
-const welcomeLabel = computed(() => t('login.welcome'))
-const askHelpLabel = computed(() => t('login.askHelp'))
-const switchLanguageLabel = computed(() => t('login.switchLanguage'))
-const taglineLabel = computed(() => t('login.tagline'))
-const { core } = useCore()
-const signinUrl = import.meta.env.VITE_DS_AUTH_SIGNIN
-const signinRoute = computed(() => ({ path: signinUrl }))
-const helpLink = core.vue.config.globalProperties.$config.get('helpLink', settings.helpLink)
-</script>
 <style scoped lang="scss">
 .login {
   max-height: 630px;
