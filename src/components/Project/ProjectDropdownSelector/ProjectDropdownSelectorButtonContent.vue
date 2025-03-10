c43f9f7319a4d887c3934e8b31ca244b33199795
Pierre Romera <hello@pirhoo.com>
2025-03-10 23:24:30 +0000

    refactor: calculate sliced projects in button component

--- src/components/Project/ProjectDropdownSelector/ProjectDropdownSelectorButtonContent.vue
+++ src/components/Project/ProjectDropdownSelector/ProjectDropdownSelectorButtonContent.vue
@@ -6,37 +6,52 @@ import ProjectThumbnail from '@/components/Project/ProjectThumbnail'
 const props = defineProps({
   projects: {
     type: Array
+  },
+  selectedProjects: {
+    type: Array
+  },
+  sliceSize: {
+    type: Number,
+    default: 3
   }
 })
 
+const slicedProjects = computed(() => {
+  return props.selectedProjects.slice(0, props.sliceSize + 1)
+})
+
 const hasOneProject = computed(() => {
-  return props.projects.length === 1
+  return props.selectedProjects.length === 1
+})
+
+const classList = computed(() => {
+  return {
+    'project-dropdown-selector-button-content--sliced': props.selectedProjects.length > props.sliceSize
+  }
 })
 
 const isLastProjectSlice = (p) => {
-  return p === props.projects.length - 1
+  return p === slicedProjects.value.length - 1
 }
 </script>
 
 <template>
-  <div class="project-dropdown-selector-button-content">
-    <span v-for="(project, p) in projects" :key="p" class="project-dropdown-selector-button-content__project">
+  <div class="project-dropdown-selector-button-content" :class="classList">
+    <span v-for="(project, p) in slicedProjects" :key="p" class="project-dropdown-selector-button-content__project">
       <project-thumbnail
         :project="project"
         no-caption
         width="1.2em"
         class="project-dropdown-selector-button-content__project__thumbnail rounded"
       />
-      <template v-if="hasOneProject">
-        <span class="ms-2">
+      <span class="ms-2">
+        <template v-if="hasOneProject">
           {{ project?.label || project?.name }}
-        </span>
-      </template>
-      <template v-else-if="isLastProjectSlice(p)">
-        <span class="ms-2">
-          {{ $t('searchBarInputDropdownForProjects.projectsCount', projects.length, { count: projects.length }) }}
-        </span>
-      </template>
+        </template>
+        <template v-else-if="isLastProjectSlice(p)">
+          {{ $tc('searchBarInputDropdownForProjects.projectsCount', selectedProjects.length) }}
+        </template>
+      </span>
     </span>
   </div>
 </template>
@@ -51,10 +66,10 @@ const isLastProjectSlice = (p) => {
 
     &:not(:first-of-type) &__thumbnail {
       box-shadow: -1px 0 0 0 #fff;
-      margin-left: -0.5em;
+      margin-left: -1em;
     }
 
-    .project-dropdown-selector--sliced &:last-of-type &__thumbnail {
+    .project-dropdown-selector-button-content--sliced &:last-of-type &__thumbnail {
       background: $text-muted !important;
 
       &:after {
