1eee99e8a9f791cccc9ec1d29986edcb452d86fa
Pierre Romera <hello@pirhoo.com>
2025-03-10 14:43:54 +0000

    refactor: turn into ui-only

--- src/components/Button/ButtonRowAction/ButtonRowActionRelaunch.vue
+++ src/components/Button/ButtonRowAction/ButtonRowActionRelaunch.vue
@@ -1,37 +1,14 @@
 <script setup>
 import ButtonRowAction from '@/components/Button/ButtonRowAction/ButtonRowAction'
-import { useConfirmModal } from '@/composables/confirm'
 
 const { withModal } = defineProps({
   withModal: {
     type: Boolean,
     default: false
-  },
-  disabled: {
-    type: Boolean,
-    default: false
   }
 })
-
-const emit = defineEmits(['delete'])
-const { confirm: showConfirmModal } = useConfirmModal()
-
-async function onRelaunch() {
-  if (withModal === true) {
-    if (await showConfirmModal()) {
-      emit('relaunch')
-    }
-  } else {
-    emit('relaunch')
-  }
-}
 </script>
 
 <template>
-  <button-row-action
-    icon="arrow-counter-clockwise"
-    :disabled="disabled"
-    :label="$t('buttonRowAction.relaunch')"
-    @click="onRelaunch"
-  />
+  <button-row-action :icon="PhArrowCounterClockwise" :label="$t('buttonRowAction.relaunch')" />
 </template>
