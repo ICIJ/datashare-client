8d0b6a66e4e1cfa54c4352f9954ee9af6bcdc7bd
Pierre Romera <hello@pirhoo.com>
2025-03-10 14:45:30 +0000

    refactor: format

--- src/components/Button/ButtonRowAction/ButtonRowAction.vue
+++ src/components/Button/ButtonRowAction/ButtonRowAction.vue
@@ -1,26 +1,41 @@
 <script setup>
 import ButtonIcon from '@/components/Button/ButtonIcon'
 import { SIZE, buttonSizeValidator } from '@/enums/sizes'
+
 defineProps({
-  icon: { type: [String, Object], required: true },
-  size: { type: String, validator: buttonSizeValidator, default: SIZE.SM },
-  label: { type: String },
-  disabled: { type: Boolean, default: false },
-  to: { type: Object }
+  icon: { 
+    type: [String, Object], 
+    required: true 
+  },
+  size: { 
+    type: String, 
+    validator: buttonSizeValidator, 
+    default: SIZE.SM 
+  },
+  label: { 
+    type: String 
+  },
+  disabled: { 
+    type: Boolean, 
+    default: false 
+  },
+  to: { 
+    type: Object 
+  }
 })
 </script>
 
 <template>
   <button-icon
-    :to="to"
-    :icon-left="icon"
-    icon-left-hover-weight="bold"
-    hide-label
     :disabled="disabled"
-    square
-    :size="size"
-    variant="outline-secondary"
+    :icon-left="icon"
     :label="label"
+    :size="size"
+    :to="to"
+    hide-label
+    icon-left-hover-weight="bold"
+    square
+    variant="outline-secondary"
     class="border-0"
   />
 </template>
