6b6595af787218a2be69e93e8100515fbebca789
Pierre Romera <hello@pirhoo.com>
2025-03-10 14:41:42 +0000

    refactor: make ui-only

--- src/components/Button/ButtonRowAction/ButtonRowActionStop.vue
+++ src/components/Button/ButtonRowAction/ButtonRowActionStop.vue
@@ -1,19 +1,7 @@
 <script setup>
 import ButtonRowAction from '@/components/Button/ButtonRowAction/ButtonRowAction'
-import { useConfirmModal } from '@/composables/confirm'
-defineProps({
-  disabled: { type: Boolean }
-})
-const emit = defineEmits(['stop'])
-const { confirm: showConfirmModal } = useConfirmModal()
-
-async function onStop() {
-  if (await showConfirmModal()) {
-    emit('stop')
-  }
-}
 </script>
 
 <template>
-  <button-row-action icon="stop-circle" :disabled="disabled" :label="$t('buttonRowAction.stop')" @click="onStop" />
+  <button-row-action :icon="PhStopCircle" :label="$t('buttonRowAction.stop')" />
 </template>
