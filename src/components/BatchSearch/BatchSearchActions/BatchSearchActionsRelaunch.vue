c92bdb6812b5510f6a0165876ed38b56cc59e662
Pierre Romera <hello@pirhoo.com>
2025-03-10 14:41:16 +0000

    refactor: rename component and turn into ui-only

---
+++ src/components/BatchSearch/BatchSearchActions/BatchSearchActionsRelaunch.vue
@@ -0,0 +1,7 @@
+<script setup>
+import ButtonRowActionRelaunch from '@/components/Button/ButtonRowAction/ButtonRowActionRelaunch'
+</script>
+
+<template>
+  <button-row-action-relaunch />
+</template>
