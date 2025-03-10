2513ac6c750aa26c2684bf060284d84c39a9d136
Pierre Romera <hello@pirhoo.com>
2025-03-10 22:54:37 +0000

    refactor: use explicite entry description

--- src/components/Task/TaskBoard/TaskBoardEntryList.vue
+++ src/components/Task/TaskBoard/TaskBoardEntryList.vue
@@ -1,32 +1,17 @@
-<script setup>
-import TaskBoardEntry from '@/components/Task/TaskBoard/TaskBoardEntry'
-
-defineProps({
-  entries: { 
-    type: Array, 
-    default: () => [] 
-  }
-})
-</script>
-
 <template>
   <div class="task-board-entry-list container-fluid bg-tertiary-subtle py-4">
-    <div class="row justify-content-around g-5 mx-2">
-      <div v-for="(task, index) in entries" :key="index" class="col-12 col-xl-6">
-        <task-board-entry
-          :icon="task.icon"
-          :title="$t(`${task.key}.title`)"
-          :description="$t(`${task.key}.description`)"
-          :info="task.infoComp ?? $t(`${task.key}.info`, task.info)"
-          :list-link="task.listLink"
-          :action-link="task.actionLink"
-          :action-text="$t(`${task.key}.actionText`)"
-          class="h-100 d-flex flex-column"
-        >
-        </task-board-entry>
-      </div>
-    </div>
+    <slot />
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.task-board-entry-list {
+  display: grid;
+  gap: $spacer-xxl;
+  grid-template-columns: repeat(auto-fill, minmax(500px, 1fr));
+
+  @media (min-width: 1000px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+</style>
