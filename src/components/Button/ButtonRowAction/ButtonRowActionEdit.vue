a5d0ea1d012c9f0c2adaf01d9239e5d839fd61c1
Pierre Romera <hello@pirhoo.com>
2025-03-10 14:45:41 +0000

    refactor: simplify ButtonRowActionEdit component by removing props and streamlining template

--- src/components/Button/ButtonRowAction/ButtonRowActionEdit.vue
+++ src/components/Button/ButtonRowAction/ButtonRowActionEdit.vue
@@ -1,15 +1,7 @@
 <script setup>
 import ButtonRowAction from '@/components/Button/ButtonRowAction/ButtonRowAction'
-defineProps({
-  disabled: { type: Boolean }
-})
 </script>
 
 <template>
-  <button-row-action
-    icon="pencil-simple"
-    :disabled="disabled"
-    :label="$t('buttonRowAction.edit')"
-    @click="$emit('edit')"
-  />
+  <button-row-action :icon="PhPencilSimple" :label="$t('buttonRowAction.edit')" />
 </template>
