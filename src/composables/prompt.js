86bc7f37f65c36054d0491eaafa8cf8e18c31193
Pierre Romera <hello@pirhoo.com>
2025-03-10 14:44:07 +0000

    feat: add usePromptModal composable for modal handling

---
+++ src/composables/prompt.js
@@ -0,0 +1,32 @@
+import { h } from 'vue'
+import { useModalController } from 'bootstrap-vue-next'
+
+import AppModalPrompt from '@/components/AppModal/AppModalPrompt'
+
+export const usePromptModal = (Component = AppModalPrompt) => {
+  const modalController = useModalController()
+
+  function show(props = { inputAutofocus: true }) {
+    return new Promise((resolve) => {
+      const component = h(Component, {
+        onClose: resolve,
+        onHide: resolve,
+        onCancel: resolve,
+        onSubmit: resolve
+      })
+
+      modalController.show({ component, props })
+    })
+  }
+
+  function hide() {
+    return modalController.hide()
+  }
+
+  async function prompt(props) {
+    const { trigger, ...rest } = await show(props)
+    return trigger === 'submit' ? rest : null
+  }
+
+  return { show, hide, prompt }
+}
