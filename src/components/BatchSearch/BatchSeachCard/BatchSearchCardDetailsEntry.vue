13110be86b73dd543a8c7b121d94a116906046d7
Pierre Romera <hello@pirhoo.com>
2025-03-11 12:28:36 +0000

    refactor: format and add wrap

--- src/components/BatchSearch/BatchSeachCard/BatchSearchCardDetailsEntry.vue
+++ src/components/BatchSearch/BatchSeachCard/BatchSearchCardDetailsEntry.vue
@@ -15,20 +15,25 @@ defineProps({
 </script>
 
 <template>
-  <span class="batch-search-card-details-entry d-flex align-items-center justify-content-between gap-2" :title="label"
-    ><span class="d-flex align-items-center gap-2"
-      ><phosphor-icon
+  <div class="batch-search-card-details-entry d-flex align-items-center justify-content-between gap-2" :title="label">
+    <div class="d-flex  flex-wrap align-items-center gap-2">
+      <phosphor-icon
         v-if="icon"
         :name="icon"
         class="batch-search-card-details-entry__icon text-secondary-emphasis"
-      /><slot>{{ value }}</slot></span
-    ><span><slot name="end"></slot></span
-  ></span>
+      />
+      <slot>{{ value }}</slot>
+    </div>
+    <div>
+      <slot name="end" />
+    </div>
+  </div>
 </template>
+
 <style scoped lang="scss">
 .batch-search-card-details-entry {
   &__icon {
-    width: 27.5px;
+    width: 1.75rem;
   }
 }
 </style>
