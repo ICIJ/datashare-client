7ee72636b4286527c51d73802897bf08c3f87d8e
Pierre Romera <hello@pirhoo.com>
2025-03-12 15:34:33 +0000

    refactor: use dedicated batch search query property

--- src/views/Task/TaskBatchSearch/TaskBatchSearchQueryList.vue
+++ src/views/Task/TaskBatchSearch/TaskBatchSearchQueryList.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { computed, ref, onBeforeMount } from 'vue'
+import { computed, toRef, ref, onBeforeMount, watch } from 'vue'
+import { useRoute } from 'vue-router'
 
 import ButtonRowActionSearch from '@/components/Button/ButtonRowAction/ButtonRowActionSearch'
 import BatchSearchCard from '@/components/BatchSearch/BatchSeachCard/BatchSearchCard'
@@ -9,20 +10,28 @@ import PageHeader from '@/components/PageHeader/PageHeader'
 import PageTableGeneric from '@/components/PageTable/PageTableGeneric'
 import PageToolbar from '@/components/PageToolbar/PageToolbar'
 import { useCore } from '@/composables/core'
-import { useTaskSettings } from '@/composables/task-settings'
-import { useUrlParam, useUrlParamsWithStore, useUrlParamWithStore } from '@/composables/url-params'
+import { useBatchSearchQueryProperties } from '@/composables/batch-search-query-properties'
+import { useUrlParam, useUrlParamWithStore } from '@/composables/url-params'
 import { useAppStore, useTaskStore } from '@/store/modules'
 
 const props = defineProps({
-  uuid: { type: String, required: true },
-  indices: { type: [String, Object], required: true }
+  uuid: {
+    type: String,
+    required: true
+  },
+  indices: {
+    type: String,
+    required: true
+  }
 })
 
-const { core } = useCore()
+const route = useRoute()
 const appStore = useAppStore()
 const taskStore = useTaskStore()
-const settingView = 'batch-search-queries'
-const settings = useTaskSettings(settingView)
+const { core } = useCore()
+const { fields } = useBatchSearchQueryProperties()
+
+const settingsView = 'batch-search-queries'
 const queries = ref([])
 const batchSearch = ref(null)
 
@@ -35,50 +44,36 @@ const page = useUrlParam('page', {
 
 const perPage = useUrlParamWithStore('perPage', {
   transform: (value) => Math.max(10, parseInt(value)),
-  get: () => appStore.getSettings(settingView, 'perPage'),
-  set: (value) => appStore.setSettings({ view: settingView, perPage: parseInt(value) })
+  get: () => appStore.getSettings(settingsView, 'perPage'),
+  set: (value) => appStore.setSettings({ view: settingsView, perPage: parseInt(value) })
 })
 
-const orderBy = useUrlParamsWithStore(['sort', 'order'], {
-  get: () => appStore.getSettings(settingView, 'orderBy'),
-  set: (sort, order) => appStore.setSettings({ view: settingView, orderBy: [sort, order] })
+const from = computed(() => (page.value - 1) * perPage.value)
+
+const visibleFields = computed(() => {
+  return fields.filter((field) => {
+    return appStore.getSettings(settingsView, 'properties').includes(field.key)
+  })
 })
 
-const sort = computed({
-  get: () => orderBy.value?.[0],
-  set: (value) => (orderBy.value = [value, order.value])
-})
-
-const order = computed({
-  get: () => orderBy.value?.[1],
-  set: (value) => (orderBy.value = [sort.value, value])
-})
-
-const title = computed(() => batchSearch.value?.name ?? props.uuid.split('-')[0])
+const batchSearchName = computed(() => batchSearch.value?.name)
 const empty = computed(() => queries.value.length === 0)
 
-async function getBatchSearch() {
-  try {
-    // Fetch the task for this batch search
-    taskStore.tasks.push(await core.api.getTask(props.uuid))
-    // Then fetch the batch search record
-    batchSearch.value = await core.api.getBatchSearch(props.uuid)
-  } catch (error) {
-    batchSearch.value = null
-  }
+async function fetchBatchSearch() {
+  // Fetch the task for this batch search
+  taskStore.tasks.push(await core.api.getTask(props.uuid))
+  // Then fetch the batch search record
+  batchSearch.value = await core.api.getBatchSearch(props.uuid)
 }
 
-async function getBatchSearchQueries() {
-  try {
-    const queriesObjects = await core.api.getBatchSearchQueries(props.uuid)
-    queries.value = Object.keys(queriesObjects).map((k) => ({ query: k, nbHits: queriesObjects[k] }))
-  } catch (error) {
-    queries.value = []
-  }
+async function fetchBatchSearchQueries() {
+  const records = await core.api.getBatchSearchQueries(props.uuid, from.value, perPage.value, searchQuery.value)
+  // The queries are returned in an object
+  queries.value = Object.entries(records).map(([query, count]) => ({ query, count }))
 }
 
-onBeforeMount(getBatchSearchQueries)
-onBeforeMount(getBatchSearch)
+onBeforeMount(fetchBatchSearch)
+watch(toRef(route, 'query'), fetchBatchSearchQueries, { deep: true, immediate: true })
 </script>
 
 <template>
@@ -87,25 +82,28 @@ onBeforeMount(getBatchSearch)
       <template #breadcrumb>
         <navigation-breadcrumb-link route-name="task" />
         <navigation-breadcrumb-link route-name="task.batch-search.list" />
-        <navigation-breadcrumb-link route-name="task.batch-search-queries.list" :title="title" />
+        <navigation-breadcrumb-link route-name="task.batch-search-queries.list" :title="batchSearchName" />
       </template>
     </page-header>
     <b-row>
       <b-col lg="8" cols="12">
-        <page-toolbar v-model:searchQuery="searchQuery" v-model:page="page" :per-page="perPage" paginable />
-        <page-table-generic
-          v-if="!empty"
-          :items="queries"
-          :fields="settings.propertiesModelValueOptions.value"
-          :sort="sort"
-          :order="order"
-        >
+        <page-toolbar
+          v-model:searchQuery="searchQuery"
+          v-model:page="page"
+          :per-page="perPage"
+          :total-rows="batchSearch?.nbQueries ?? 0"
+          paginable
+          searchable
+        />
+        <page-table-generic v-if="!empty" :items="queries" :fields="visibleFields">
           <template #cell(query)="{ item }">
             <router-link :to="{ name: 'task.batch-search-queries.show', params: { query: item.query } }">
               {{ item.query }}
             </router-link>
           </template>
-          <template #cell(documents)="{ item }"> {{ item.nbHits }} </template>
+          <template #cell(count)="{ item }">
+            {{ item.count }}
+          </template>
           <template #row-actions="{ item }">
             <button-row-action-search :to="{ name: 'search', query: { indices, q: item.query } }" />
           </template>
