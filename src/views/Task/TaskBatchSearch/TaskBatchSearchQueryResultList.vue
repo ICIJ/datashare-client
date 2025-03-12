34bb72421ba865b79cc9e367765a58299ca55127
Pierre Romera <hello@pirhoo.com>
2025-03-12 13:12:45 +0000

    fix: use correct settings' view

--- src/views/Task/TaskBatchSearch/TaskBatchSearchQueryResultList.vue
+++ src/views/Task/TaskBatchSearch/TaskBatchSearchQueryResultList.vue
@@ -38,7 +38,7 @@ const appStore = useAppStore()
 const route = useRoute()
 const { core } = useCore()
 const { fields } = useBatchSearchResultProperties()
-const settingView = 'batch-search-queries-show'
+const settingView = 'batch-search-results'
 const hits = ref(null)
 const batchSearch = ref(null)
 
