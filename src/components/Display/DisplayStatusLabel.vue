2471b869c74c3348fcc61415bf3734836467ada7
Pierre Romera <hello@pirhoo.com>
2025-03-12 20:36:28 +0000

    feat: handle more status

--- src/components/Display/DisplayStatusLabel.vue
+++ src/components/Display/DisplayStatusLabel.vue
@@ -1,10 +1,11 @@
 <script setup>
 import { computed } from 'vue'
 import { useI18n } from 'vue-i18n'
+import { camelCase } from 'lodash'
 
 import { toVariant } from '@/utils/utils'
 
-const { t } = useI18n()
+const { t, te } = useI18n()
 
 const props = defineProps({
   value: {
@@ -19,8 +20,15 @@ const valueVariant = computed(() => {
   return toVariant(props.value)
 })
 
+const valueNormalized = computed(() => {
+  return camelCase(props.value)
+})
+
 const valueTitle = computed(() => {
-  return props.title ?? t(`displayStatus.${valueVariant.value}`)
+  if (te(`displayStatus.${valueNormalized.value}`)) {
+    return t(`displayStatus.${valueNormalized.value}`)
+  }
+  return t(`displayStatus.${valueVariant.value}`)
 })
 </script>
 
