dd3c41377ceecc8ec09d869977debe72270e8269
Pierre Romera <hello@pirhoo.com>
2025-03-12 09:27:55 +0000

    refactor: simplify implementation

--- src/components/Text/TextTruncate.vue
+++ src/components/Text/TextTruncate.vue
@@ -1,12 +1,4 @@
-<template>
-  <div>
-    {{ displayText }} <span v-if="isTruncated">...</span
-    ><b-button v-if="shouldShowSeeMoreButton" variant="link" class="py-0 px-1 align-baseline" @click="toggleSeeMore">
-      {{ isExpanded ? seeLess : seeMore }}
-    </b-button>
-  </div>
-</template>
-
+1
 <script setup>
 import { ref, computed } from 'vue'
 import { useI18n } from 'vue-i18n'
@@ -19,39 +11,33 @@ const props = defineProps({
   truncateLength: {
     type: Number,
     default: 100
+  },
+  ellipsis: {
+    type: String,
+    default: '... '
   }
 })
+
 const { t } = useI18n()
-const seeLess = t('textTruncate.seeLess')
-const seeMore = t('textTruncate.seeMore')
+const seeLess = computed(() => t('textTruncate.seeLess'))
+const seeMore = computed(() => t('textTruncate.seeMore'))
 const isExpanded = ref(false)
 
-const truncateText = (text, truncateLength) => {
-  if (text.length <= truncateLength) {
-    return text
-  }
+const toggleSeeMore = () => (isExpanded.value = !isExpanded.value)
 
-  const truncatedText = text.slice(0, truncateLength)
-  const lastSpaceIndex = truncatedText.lastIndexOf(' ')
-
-  return lastSpaceIndex === -1 ? '' : truncatedText.slice(0, lastSpaceIndex)
-}
-
-const displayText = computed(() => {
-  return isExpanded.value ? props.text : truncateText(props.text, props.truncateLength)
-})
-
-const isTruncated = computed(() => {
-  return !isExpanded.value && props.text.length > props.truncateLength
-})
-
-const shouldShowSeeMoreButton = computed(() => {
-  return props.text.length > props.truncateLength
-})
-
-const toggleSeeMore = () => {
-  isExpanded.value = !isExpanded.value
-}
+const truncatedText = computed(() => props.text.slice(0, props.truncateLength).trimEnd())
+const displayText = computed(() => (isExpanded.value ? props.text : truncatedText.value))
+const isTruncated = computed(() => !isExpanded.value && props.text.length > props.truncateLength)
+const shouldShowSeeMoreButton = computed(() => props.text.length > props.truncateLength)
+const seeMoreDisplay = computed(() => (isExpanded.value ? seeLess : seeMore))
 </script>
 
-<style scoped></style>
+<template>
+  <div class="text-truncate-wrapper">
+    <span class="text-truncate-wrapper__display">{{ displayText }}</span>
+    <span v-if="isTruncated">{{ ellipsis }}</span>
+    <b-button v-if="shouldShowSeeMoreButton" variant="link" class="p-0 align-baseline" @click="toggleSeeMore">
+      {{ seeMoreDisplay }}
+    </b-button>
+  </div>
+</template>
