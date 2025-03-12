57d3bfaa8c52d9d8c082b6ee795b6e95a9240e3c
Pierre Romera <hello@pirhoo.com>
2025-03-12 16:21:43 +0000

    refactor: lint

--- src/views/App.vue
+++ src/views/App.vue
@@ -14,7 +14,7 @@
     </div>
     <page-offcanvas v-if="hasSettings" v-model="showPageSettings" no-header>
       <template #default="{ visible, placement, hide }">
-        <component :is="settingComponent" :hide="hide" :visible="visible" :placement="placement" :key="route.name" />
+        <component :is="settingComponent" :key="route.name" :hide="hide" :visible="visible" :placement="placement" />
       </template>
     </page-offcanvas>
     <b-modal-orchestrator />
