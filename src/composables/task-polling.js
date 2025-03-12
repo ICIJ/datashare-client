69c4c24d693231f0304665cfbcf72cc9be1f8425
Pierre Romera <hello@pirhoo.com>
2025-03-12 18:59:58 +0000

    feat: ensure polling is triggered once and get task use API params

--- src/composables/task-polling.js
+++ src/composables/task-polling.js
@@ -1,15 +1,15 @@
-import { computed, toValue, toRef, watch, useId, onBeforeUnmount } from 'vue'
+import { computed, toValue, watch, useId, onBeforeUnmount } from 'vue'
 import { random } from 'lodash'
 
 import { usePolling } from '@/composables/polling'
 import { useCore } from '@/composables/core'
 import { useTaskStore } from '@/store/modules'
 
-export function useTaskPolling(taskNames = []) {
+export function useTaskPolling({ names = [], sortBy = [], searchQuery = null } = {}) {
   const taskStore = useTaskStore()
   const loaderId = useId()
   const { wait } = useCore()
-  const { registerPollOnce } = usePolling()
+  const { unregisteredPoll, registerPollOnce } = usePolling()
 
   async function onShotTask() {
     try {
@@ -28,29 +28,40 @@ export function useTaskPolling(taskNames = []) {
 
   async function stopPendingTasks() {
     await taskStore.stopPendingTasks()
-    await taskStore.getTasks(toValue(taskNames))
+    await getTasks()
   }
 
   async function removeDoneTasks() {
     await taskStore.removeDoneTasks()
-    await taskStore.getTasks(toValue(taskNames))
+    await getTasks()
   }
 
   async function getTasks() {
-    await taskStore.getTasks(toValue(taskNames))
+    await taskStore.getTasks({
+      names: toValue(names),
+      sort: toValue(sortBy)?.[0],
+      order: toValue(sortBy)?.[1] ?? 'asc',
+      // Filters can be build with arbitrary values
+      'args.batchRecord.name': toValue(searchQuery),
+      // The tasks API endpoint has limited support for pagination so we get all tasks at once
+      size: null,
+      from: 0
+    })
     // Continue to poll task if they are pending ones
     return hasPendingTasks.value
   }
 
-  function startPollingTasks() {
+  async function startPollingTasks() {
     const fn = getTasks
     const timeout = () => random(1000, 4000)
+    // Ensure the polling is registered before firing the first poll
+    unregisteredPoll({ fn })
     // Execute the `getTasks` method immediately
     // and if it's true, register a poll
-    return fn() && registerPollOnce({ fn, timeout })
+    return (await fn()) && registerPollOnce({ fn, timeout })
   }
 
-  watch(toRef(taskNames), onShotTask, { immediate: true })
+  watch(() => [names, sortBy], onShotTask, { immediate: true, deep: true })
   onBeforeUnmount(taskStore.reset)
 
   return { tasks, noTasks, getTasks, hasPendingTasks, hasDoneTasks, stopPendingTasks, removeDoneTasks, isLoading }
