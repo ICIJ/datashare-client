1b29ceac8ddb6ea8be5123c43f4df36717f78d98
Pierre Romera <hello@pirhoo.com>
2025-03-12 20:04:12 +0000

    feat: add DisplayStatusLabel component for dynamic status display

---
+++ src/components/Display/DisplayStatusLabel.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+import { toVariant } from '@/utils/utils'
+
+const { t } = useI18n()
+
+const props = defineProps({
+  value: {
+    type: String
+  },
+  title: {
+    type: String
+  }
+})
+
+const valueVariant = computed(() => {
+  return toVariant(props.value)
+})
+
+const valueTitle = computed(() => {
+  return props.title ?? t(`displayStatus.${valueVariant.value}`)
+})
+</script>
+
+<template>
+  {{ valueTitle }}
+</template>
