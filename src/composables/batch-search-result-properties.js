a8459be95048206d88f1ce6e0ca19b159b53b2b0
Pierre Romera <hello@pirhoo.com>
2025-03-13 10:24:00 +0000

    feat: mark query fields as required

--- src/composables/batch-search-result-properties.js
+++ src/composables/batch-search-result-properties.js
@@ -14,6 +14,7 @@ export function useBatchSearchResultProperties() {
     key: 'query',
     text: computed(() => t('task.batch-search-results.show.fields.query')),
     sortable: false,
+    required: true,
     emphasis: true
   })
 
