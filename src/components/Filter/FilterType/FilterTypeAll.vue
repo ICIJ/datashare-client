56ec0a1ce2853852dc495c1e7d13161f2ff7edcd
Pierre Romera <hello@pirhoo.com>
2025-03-14 11:10:18 +0000

    feat: add FilterTypeAll

---
+++ src/components/Filter/FilterType/FilterTypeAll.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { computed, ref, toRef, watch } from 'vue'
+
+import FiltersPanelSectionFilterEntry from '@/components/FiltersPanel/FiltersPanelSectionFilterEntry'
+import { useSearchFilter } from '@/composables/search-filter'
+
+const props = defineProps({
+  filter: {
+    type: Object,
+    required: true
+  }
+})
+
+const { computedAll, computedTotal, searchStore } = useSearchFilter()
+
+const all = computedAll(toRef(props, 'filter'))
+const total = computedTotal(toRef(props, 'filter'))
+
+// We track if the total was already visible before to avoid flickering
+// when hidding it (e.g. when the search is not ready).
+const hadTotal = ref(false)
+watch(total, (value, oldValue) => (hadTotal.value = oldValue !== null))
+
+const hideTotal = computed(() => total.value === null || (!searchStore.isReady && !hadTotal.value))
+</script>
+
+<template>
+  <filters-panel-section-filter-entry
+    v-model="all"
+    :label="$t('filterTypeAll.label')"
+    :count="total"
+    :hide-count="hideTotal"
+    :disabled="all"
+  >
+  </filters-panel-section-filter-entry>
+</template>
