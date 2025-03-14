4e18ac23484ea7504bcb08555646235de2333fd2
Pierre Romera <hello@pirhoo.com>
2025-03-14 23:13:04 +0000

    test: mock resize observer with a class

--- tests/unit/setup.js
+++ tests/unit/setup.js
@@ -1,15 +1,6 @@
 import 'whatwg-fetch'
 
-// This mock ensures that the EllipsisTooltip directove from @icij/murmur-next
-// is mocked and does not throw an error when used in the tests. It's a temporary
-// workaround until the directive is properly unmounted.
-vi.mock('@icij/murmur-next', async (importOriginal) => {
-  const original = await importOriginal()
-  const EllipsisTooltip = {}
-  return { ...original, EllipsisTooltip }
-})
-
-// This is a another temporary workaround to avoid the following console.log
+// This is a temporary workaround to avoid the following console.log
 // that are printed during the tests by vue-wait and vue3-shortkey.
 const SILENCED_LOGS = ['installing...', 'doing fixed mapping']
 // Save the original log method for later use
@@ -25,11 +16,11 @@ global.console = Object.assign(global.console, {
   })
 })
 
-global.ResizeObserver = vi.fn().mockImplementation(() => ({
-  observe: vi.fn(),
-  unobserve: vi.fn(),
-  disconnect: vi.fn()
-}))
+global.ResizeObserver = class {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
 
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
