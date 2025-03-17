f52c0301dbbf1a5c015f66b454cf76a060d05eef
Pierre Romera <hello@pirhoo.com>
2025-03-17 17:21:49 +0000

    refactor: simplify to only get additions/deletions icij/datashare#1677

--- src/utils/diff.js
+++ src/utils/diff.js
@@ -1,88 +1,33 @@
-import { difference, isEqual, isObject, isArray, intersection, pick } from 'lodash'
-
 /**
- * Compares two objects and returns the differences in additions, deletions, and updates.
- * @param {Object} previousObj - The original object.
- * @param {Object} obj - The new object.
- * @returns {Object} An object containing additions, deletions, and updates.
+ * Compares two lists of entries and returns the differences in additions and deletions.
+ * @param {[string, any][]} entriesA - The list of entries A.
+ * @param {[string, any][]} entriesB - The list of entries B.
+ * @returns {{$additions: Object, $deletions: Object}} An object containing additions and deletions.
  */
-export default function diff(previousObj, obj) {
-  const oldKeys = Object.keys(previousObj)
-  const newKeys = Object.keys(obj)
-
-  const additionsKeys = difference(newKeys, oldKeys)
-  const deletionsKeys = difference(oldKeys, newKeys)
-  const updatesKeys = getUpdatedKeys(previousObj, obj, oldKeys, newKeys)
-
+export default function diff(entriesA, entriesB) {
   return {
-    $additions: pick(obj, additionsKeys),
-    $deletions: pick(previousObj, deletionsKeys),
-    $updates: getUpdates(previousObj, obj, updatesKeys)
+    $additions: getDifferences(entriesB, entriesA),
+    $deletions: getDifferences(entriesA, entriesB)
   }
 }
 
 /**
- * Identifies keys that have been updated between two objects.
- * @param {Object} previousObj - The original object.
- * @param {Object} obj - The new object.
- * @param {Array} oldKeys - The keys from the original object.
- * @param {Array} newKeys - The keys from the new object.
- * @returns {Array} An array of keys that have different values between the two objects.
+ * Identifies differences from the first entries compared to the second entries.
+ * @param {[string, any][]} entriesA - The lust of entries A.
+ * @param {[string, any][]} entriesB - The list of entries B.
+ * @returns {Object} An object containing the differences.
  */
-function getUpdatedKeys(previousObj, obj, oldKeys, newKeys) {
-  const commonKeys = intersection(oldKeys, newKeys)
-  return commonKeys.filter((key) => !isEqual(previousObj[key], obj[key]))
-}
+function getDifferences(entriesA = [], entriesB = []) {
+  const differences = {}
 
-/**
- * Constructs the updates object by analyzing the differences between the values of the updated keys.
- * @param {Object} previousObj - The original object.
- * @param {Object} obj - The new object.
- * @param {Array} updatesKeys - The keys that have different values between the two objects.
- * @returns {Object} An object representing the updates, with differences in arrays or nested objects.
- */
-function getUpdates(previousObj, obj, updatesKeys) {
-  return updatesKeys.reduce((updates, key) => {
-    const oldValue = previousObj[key]
-    const newValue = obj[key]
-
-    if (isArray(oldValue) && isArray(newValue)) {
-      updates[key] = getArrayDifferences(oldValue, newValue)
-    } else if (isObject(oldValue) && isObject(newValue)) {
-      const nestedDiff = diff(oldValue, newValue)
-      if (hasDifferences(nestedDiff)) {
-        updates[key] = nestedDiff
+  entriesA.forEach(([keyA, valueA]) => {
+    if (!entriesB.some(([keyB, valueB]) => keyB === keyA && valueB === valueA)) {
+      differences[keyA] = differences[keyA] || []
+      if (!differences[keyA].includes(valueA)) {
+        differences[keyA].push(valueA)
       }
-    } else {
-      updates[key] = { oldValue, newValue }
     }
+  })
 
-    return updates
-  }, {})
-}
-
-/**
- * Gets the differences between two arrays, identifying additions and deletions.
- * @param {Array} oldArray - The original array.
- * @param {Array} newArray - The new array.
- * @returns {Object} An object containing additions and deletions in the array.
- */
-function getArrayDifferences(oldArray, newArray) {
-  const arrayAdditions = difference(newArray, oldArray)
-  const arrayDeletions = difference(oldArray, newArray)
-
-  const result = {}
-  if (arrayAdditions.length) result.$additions = arrayAdditions
-  if (arrayDeletions.length) result.$deletions = arrayDeletions
-
-  return result
-}
-
-/**
- * Checks if the nested differences object has any additions, deletions, or updates.
- * @param {Object} nestedDiff - The object representing nested differences.
- * @returns {boolean} True if there are any differences, false otherwise.
- */
-function hasDifferences({ additions = {}, deletions = {}, updates = {} }) {
-  return !!(Object.keys(additions) || Object.keys(deletions) || Object.keys(updates))
+  return differences
 }
