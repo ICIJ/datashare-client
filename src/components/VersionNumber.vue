f144f1e92adfd38bce8836d0624648bdeddf58fb
Pierre Romera <hello@pirhoo.com>
2025-03-19 18:58:31 +0000

    refactor: use b-tooltip target slot

--- src/components/VersionNumber.vue
+++ src/components/VersionNumber.vue
@@ -1,35 +1,34 @@
 <template>
-  <div>
-    <div :id="versionNumberId" class="version-number d-inline-block">v{{ serverVersion }}</div>
-    <b-tooltip :target="versionNumberId" :delay="tooltipDelay" :placement="tooltipPlacement" teleport-to="body">
-      <div class="version-number__tooltip text-nowrap text-start">
-        <div class="d-flex align-items-baseline version-number__tooltip__client py-2">
-          <div class="flex-grow-1 pe-5">
-            <phosphor-icon name="desktop" class="me-1" />
-            {{ $t('versionNumber.client') }}
-          </div>
-          <div class="font-monospace version-number__tooltip__client__value">
-            {{ shortClientHash }}
-          </div>
+  <b-tooltip :delay="tooltipDelay" :placement="tooltipPlacement" teleport-to="body">
+    <template #target>
+      <div class="version-number d-inline-block">v{{ serverVersion }}</div>
+    </template>
+    <div class="version-number__tooltip text-nowrap text-start">
+      <div class="d-flex align-items-baseline version-number__tooltip__client py-2">
+        <div class="flex-grow-1 pe-5">
+          <phosphor-icon name="desktop" class="me-1" />
+          {{ $t('versionNumber.client') }}
         </div>
-        <div class="d-flex align-items-baseline version-number__tooltip__server py-2">
-          <div class="flex-grow-1 pe-5">
-            <phosphor-icon name="hard-drives" class="me-1" />
-            {{ $t('versionNumber.server') }}
-          </div>
-          <div class="font-monospace version-number__tooltip__server__value">
-            {{ serverHash }}
-          </div>
+        <div class="font-monospace version-number__tooltip__client__value">
+          {{ shortClientHash }}
         </div>
       </div>
-    </b-tooltip>
-  </div>
+      <div class="d-flex align-items-baseline version-number__tooltip__server py-2">
+        <div class="flex-grow-1 pe-5">
+          <phosphor-icon name="hard-drives" class="me-1" />
+          {{ $t('versionNumber.server') }}
+        </div>
+        <div class="font-monospace version-number__tooltip__server__value">
+          {{ serverHash }}
+        </div>
+      </div>
+    </div>
+  </b-tooltip>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
 import { PhosphorIcon } from '@icij/murmur-next'
-import { uniqueId } from 'lodash'
 
 import { useCore } from '@/composables/core'
 
@@ -56,7 +55,6 @@ const serverVersion = ref(null)
 
 const clientHash = import.meta.env.VITE_GIT_HASH ?? ''
 const shortClientHash = ref(clientHash.substring(0, 7))
-const versionNumberId = uniqueId('version-number-')
 
 const { core } = useCore()
 
