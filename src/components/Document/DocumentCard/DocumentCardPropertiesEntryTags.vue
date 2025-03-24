50889323817ced7337bc47d6bec2415c1f9cbc51
Pierre Romera <hello@pirhoo.com>
2025-03-24 11:18:48 +0000

    feat: use "—" for no tags

--- src/components/Document/DocumentCard/DocumentCardPropertiesEntryTags.vue
+++ src/components/Document/DocumentCard/DocumentCardPropertiesEntryTags.vue
@@ -14,7 +14,8 @@ defineProps({
 </script>
 
 <template>
-  <document-card-properties-entry v-if="document.tags.length" :document="document" :property="property" icon="hash">
-    <display-tags :value="document.tags" />
+  <document-card-properties-entry :document="document" :property="property" icon="hash">
+    <display-tags :value="document.tags" v-if="document.tags.length" />
+    <template v-else>&#8212;</template>
   </document-card-properties-entry>
 </template>
