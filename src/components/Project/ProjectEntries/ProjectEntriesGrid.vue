4c8292c785149bc2c50d0ee41dab5ce418a845f5
Pierre Romera <hello@pirhoo.com>
2025-03-25 16:18:08 +0000

    feat: add loading state

--- src/components/Project/ProjectEntries/ProjectEntriesGrid.vue
+++ src/components/Project/ProjectEntries/ProjectEntriesGrid.vue
@@ -1,9 +1,13 @@
 <script setup>
 import ProjectCard from '@/components/Project/ProjectCard/ProjectCard'
+import ProjectCardPlaceholder from '@/components/Project/ProjectCard/ProjectCardPlaceholder'
 
 defineProps({
   projects: {
     type: Array
+  },
+  loading: {
+    type: Boolean
   }
 })
 </script>
@@ -11,7 +15,10 @@ defineProps({
 <template>
   <div class="project-entries-grid">
     <div class="row g-4">
-      <div v-for="project in projects" :key="project.name" class="col-12 col-xl-6">
+      <div v-for="i in Math.max(1, projects.length)" v-if="loading" :key="i" class="col-12 col-xl-6">
+        <project-card-placeholder />
+      </div>
+      <div v-for="project in projects" v-else :key="project.name" class="col-12 col-xl-6">
         <project-card :project="project" class="h-100" />
       </div>
     </div>
