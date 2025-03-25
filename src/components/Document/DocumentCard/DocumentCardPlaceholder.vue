1fabcf6f6fbe36d535310ef9b43d2e06c696ed7b
Pierre Romera <hello@pirhoo.com>
2025-03-25 16:53:01 +0000

    feat: use list of properties to add placeholder lines

--- src/components/Document/DocumentCard/DocumentCardPlaceholder.vue
+++ src/components/Document/DocumentCard/DocumentCardPlaceholder.vue
@@ -4,10 +4,10 @@ import { random } from 'lodash'
 
 import AppPlaceholder from '@/components/AppPlaceholder/AppPlaceholder'
 
-const { verticalActions } = defineProps({
+const props = defineProps({
   properties: {
-    type: Number,
-    default: 4
+    type: Array,
+    default: () => ['title', 'thumbnail', 'contentType']
   },
   verticalActions: {
     type: Boolean,
@@ -19,27 +19,29 @@ const { verticalActions } = defineProps({
   }
 })
 
+const hasThumbnail = computed(() => props.properties.includes('thumbnail'))
+
 const actionsClassList = computed(() => {
   return {
-    'document-card-placeholder__actions--vertical': verticalActions
+    'document-card-placeholder__actions--vertical': props.verticalActions
   }
 })
 </script>
 
 <template>
   <div v-for="i in repeat" :key="i" class="document-card-placeholder p-3 d-flex gap-3">
-    <div class="document-card-placeholder__thumbnail flex-grow-1">
+    <div v-if="hasThumbnail" class="document-card-placeholder__thumbnail flex-grow-1">
       <app-placeholder squared />
     </div>
     <div class="document-card-placeholder__properties d-flex flex-column gap-2 h-100 w-100">
       <app-placeholder :width="random(40, 70)" />
-      <div v-for="j in properties" :key="j" class="d-flex flex-grow-1 gap-1">
+      <div v-for="j in properties.length - 1" :key="j" class="d-flex flex-grow-1 gap-1 align-items-start">
         <app-placeholder width="1em" />
         <app-placeholder :width="random(5, 20)" />
       </div>
     </div>
     <div class="document-card-placeholder__actions" :class="actionsClassList">
-      <app-placeholder width="1.5rem" height="1.5rem" :repeat="4" />
+      <app-placeholder width="1.75rem" height="1.75rem" :repeat="4" />
     </div>
   </div>
 </template>
