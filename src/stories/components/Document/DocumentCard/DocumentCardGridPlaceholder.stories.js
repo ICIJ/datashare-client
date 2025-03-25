abf3f0b0442d501849ab7f979228a80cdd7a486e
Pierre Romera <hello@pirhoo.com>
2025-03-25 12:49:54 +0000

    feat: add DocumentCardGridPlaceholder

---
+++ src/stories/components/Document/DocumentCard/DocumentCardGridPlaceholder.stories.js
@@ -0,0 +1,14 @@
+import DocumentCardGridPlaceholder from '@/components/Document/DocumentCard/DocumentCardGridPlaceholder'
+
+export default {
+  title: 'Components/Document/DocumentCard/DocumentCardGridPlaceholder',
+  component: DocumentCardGridPlaceholder,
+  tags: ['autodocs'],
+  decorators: [
+    () => ({
+      template: '<div style="max-width: 260px"><story /></div>'
+    })
+  ]
+}
+
+export const Default = {}
