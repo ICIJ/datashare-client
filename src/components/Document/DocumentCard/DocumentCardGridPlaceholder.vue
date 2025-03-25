2801e92de101aed9b3d0ecdc7257c0c575d60e5e
Pierre Romera <hello@pirhoo.com>
2025-03-25 16:52:54 +0000

    feat: use list of properties to add placeholder lines

--- src/components/Document/DocumentCard/DocumentCardGridPlaceholder.vue
+++ src/components/Document/DocumentCard/DocumentCardGridPlaceholder.vue
@@ -1,25 +1,32 @@
 <script setup>
 import { random } from 'lodash'
+import { computed } from 'vue'
 
 import AppPlaceholder from '@/components/AppPlaceholder/AppPlaceholder'
 
-defineProps({
+const props = defineProps({
   properties: {
+    type: Array,
+    default: () => ['title', 'thumbnail', 'contentType']
+  },
+  repeat: {
     type: Number,
-    default: 4
+    default: 1
   }
 })
+
+const hasThumbnail = computed(() => props.properties.includes('thumbnail'))
 </script>
 
 <template>
-  <div class="document-card-grid-placeholder border p-3 d-flex gap-3">
+  <div v-for="i in repeat" :key="i" class="document-card-grid-placeholder border p-3 d-flex gap-3">
     <div class="document-card-grid-placeholder__wrapper flex-grow-1 d-flex flex-column gap-2">
-      <div class="document-card-grid-placeholder__wrapper__thumbnail">
+      <div v-if="hasThumbnail" class="document-card-grid-placeholder__wrapper__thumbnail">
         <app-placeholder squared />
       </div>
       <div class="document-card-grid-placeholder__wrapper__properties d-flex flex-column gap-2 h-100 w-100">
         <app-placeholder :width="random(70, 95)" />
-        <div v-for="i in properties" :key="i" class="d-flex flex-grow-1 gap-1">
+        <div v-for="j in properties.length - 1" :key="j" class="d-flex flex-grow-1 gap-1 align-items-start">
           <app-placeholder width="1em" />
           <app-placeholder :width="random(30, 70)" />
         </div>
