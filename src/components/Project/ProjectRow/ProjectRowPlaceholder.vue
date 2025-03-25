b0056a2b89e9e669fdc2305a5120e3ed5d903fe7
Pierre Romera <hello@pirhoo.com>
2025-03-25 19:14:50 +0000

    refactor: remove placeholder variant

--- src/components/Project/ProjectRow/ProjectRowPlaceholder.vue
+++ src/components/Project/ProjectRow/ProjectRowPlaceholder.vue
@@ -22,18 +22,18 @@ defineProps({
       <div class="d-flex gap-3 align-items-center">
         <template v-if="p === 'label'">
           <div class="flex-shrink-0">
-            <app-placeholder width="50px" height="50px" variant="tertiary" />
+            <app-placeholder width="50px" height="50px" />
           </div>
         </template>
-        <app-placeholder :width="random(80, 100)" variant="tertiary" />
+        <app-placeholder :width="random(80, 100)" />
       </div>
     </td>
     <td>
-      <app-placeholder width="115px" height="44px" variant="tertiary" />
+      <app-placeholder width="115px" height="44px" />
     </td>
     <td class="text-end">
       <div class="d-inline-flex gap-3 py-2">
-        <app-placeholder width="16px" height="16px" :repeat="3" variant="tertiary" />
+        <app-placeholder width="16px" height="16px" :repeat="3" />
       </div>
     </td>
   </page-table-tr>
