427d32debe51059466cff0db17979d3c4c5ef933
Pierre Romera <hello@pirhoo.com>
2025-03-25 16:52:05 +0000

    refactor: wrap loading state in template tags

--- src/components/Project/ProjectEntries/ProjectEntriesGrid.vue
+++ src/components/Project/ProjectEntries/ProjectEntriesGrid.vue
@@ -15,12 +15,16 @@ defineProps({
 <template>
   <div class="project-entries-grid">
     <div class="row g-4">
-      <div v-for="i in Math.max(1, projects.length)" v-if="loading" :key="i" class="col-12 col-xl-6">
-        <project-card-placeholder />
-      </div>
-      <div v-for="project in projects" v-else :key="project.name" class="col-12 col-xl-6">
-        <project-card :project="project" class="h-100" />
-      </div>
+      <template v-if="loading">
+        <div v-for="i in Math.max(1, projects.length)" :key="i" class="col-12 col-xl-6">
+          <project-card-placeholder />
+        </div>
+      </template>
+      <template v-else>
+        <div v-for="project in projects" :key="project.name" class="col-12 col-xl-6">
+          <project-card :project="project" class="h-100" />
+        </div>
+      </template>
     </div>
   </div>
 </template>
