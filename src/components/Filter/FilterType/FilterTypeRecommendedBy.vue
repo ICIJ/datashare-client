d486d22505194eecc5d38302ac09f5fc935cbc51
Pierre Romera <hello@pirhoo.com>
2025-03-26 14:02:41 +0100

    feat: add search to recommended by filter icij/datashare#1721

--- src/components/Filter/FilterType/FilterTypeRecommendedBy.vue
+++ src/components/Filter/FilterType/FilterTypeRecommendedBy.vue
@@ -1,4 +1,5 @@
 <script setup>
+import Fuse from 'fuse.js'
 import { computed, onBeforeMount } from 'vue'
 import { sortBy } from 'lodash'
 
@@ -6,6 +7,7 @@ import { useCore } from '@/composables/useCore'
 import { useSearchFilter } from '@/composables/useSearchFilter'
 import { useRecommendedStore } from '@/store/modules'
 import DisplayUser from '@/components/Display/DisplayUser'
+import FormControlSearch from '@/components/Form/FormControl/FormControlSearch'
 import FilterType from '@/components/Filter/FilterType/FilterType'
 import FiltersPanelSectionFilterEntry from '@/components/FiltersPanel/FiltersPanelSectionFilterEntry'
 
@@ -16,6 +18,8 @@ const props = defineProps({
   }
 })
 
+const query = defineModel('query', { type: String, default: '' })
+
 const recommendedStore = useRecommendedStore()
 const { core } = useCore()
 const { computedFilterValues, watchIndices, indices } = useSearchFilter()
@@ -23,11 +27,26 @@ const { computedFilterValues, watchIndices, indices } = useSearchFilter()
 const selected = computedFilterValues(props.filter)
 const currentUserId = computed(() => core.config.get('uid', 'local'))
 
-const entries = computed(() => {
+const recommendations = computed(() => {
   // We ensure that the current user is always first
   return sortBy(recommendedStore.byUsers, ({ user, count }) => {
     return user === currentUserId.value ? -1e9 : -count
   })
+})
+
+const fuse = computed(() => {
+  return new Fuse(recommendations.value, {
+    threshold: 0.1,
+    shouldSort: false,
+    keys: ['user']
+  })
+})
+
+const entries = computed(() => {
+  if (!query.value) {
+    return recommendations.value
+  }
+  return fuse.value.search(query.value).map(({ item }) => item)
 })
 
 function fetch() {
@@ -40,6 +59,14 @@ watchIndices(fetch)
 
 <template>
   <filter-type :filter="filter">
+    <template #search>
+      <form-control-search
+        v-model="query"
+        placeholder="Search..."
+        clear-text
+        class="filters-panel-section-filter__content__search mb-3"
+      />
+    </template>
     <b-form-checkbox-group v-model="selected">
       <template v-for="{ count, user } in entries" :key="user">
         <filters-panel-section-filter-entry :label="user" :value="user" :count="count">
