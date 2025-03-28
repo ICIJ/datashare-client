fef03c25ccddb092462b1c4a7d4444b7fe98ef9e
Pierre Romera <hello@pirhoo.com>
2025-03-28 17:12:03 +0100

    feat: pass down autofocus to subcomponent icij/datashare#1708

--- src/components/Project/ProjectDropdownSelector/ProjectDropdownSelectorSearch.vue
+++ src/components/Project/ProjectDropdownSelector/ProjectDropdownSelectorSearch.vue
@@ -9,6 +9,9 @@ defineProps({
   hasMatches: {
     type: Boolean,
     default: true
+  },
+  autofocus: {
+    type: Boolean
   }
 })
 
@@ -19,9 +22,10 @@ const emit = defineEmits(['update:model-value', 'blur', 'up', 'down', 'enter'])
   <li class="project-dropdown-selector-search">
     <div class="b-dropdown-form pt-1 mb-3">
       <form-control-search
+        ref="input"
         :model-value="query"
-        autofocus
-        shado
+        :autofocus="autofocus"
+        shadow
         :placeholder="$t('projectDropdownSelectorSearch.placeholder')"
         @update:model-value="emit('update:model-value', $event)"
         @blur="emit('blur', $event)"
