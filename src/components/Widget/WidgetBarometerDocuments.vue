2fee27655abc6b0925ab89819e861ac426023e61
Pierre Romera <hello@pirhoo.com>
2025-03-31 16:36:38 +0000

    feat: remove useless title

--- src/components/Widget/WidgetBarometerDocuments.vue
+++ src/components/Widget/WidgetBarometerDocuments.vue
@@ -2,19 +2,14 @@
   <widget-barometer class="widget-barometer-documents" icon="files">
     <i18n-t keypath="widget.barometer.document" :plural="nbDocuments">
       <template #n>
-        <display-number-human
-          v-if="nbDocuments !== null"
-          v-b-tooltip.body="{ delay: tooltipDelay }"
-          :value="nbDocuments"
-          :title="$n(nbDocuments)"
-        />
+        <display-number-human v-if="nbDocuments !== null" :value="nbDocuments" />
         <span v-else class="text-secondary">∞</span>
       </template>
     </i18n-t>
     <template v-if="nbDocumentsOnDisks !== null" #label>
       <i18n-t keypath="widget.barometer.amongWhichOnDisk" :plural="nbDocumentsOnDisks">
         <template #n>
-          <display-number-human v-b-tooltip.body :value="nbDocumentsOnDisks" :title="$n(nbDocumentsOnDisks)" />
+          <display-number-human :value="nbDocumentsOnDisks" />
         </template>
       </i18n-t>
     </template>
