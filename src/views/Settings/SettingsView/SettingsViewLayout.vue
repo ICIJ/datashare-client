01c050b020ccadafcb3fd3571329ca3e6bd2cdab
Pierre Romera <hello@pirhoo.com>
2025-04-03 11:51:46 +0000

    fix: make infoName prop not required

--- src/views/Settings/SettingsView/SettingsViewLayout.vue
+++ src/views/Settings/SettingsView/SettingsViewLayout.vue
@@ -11,7 +11,7 @@ import DismissableAlert from '@/components/Dismissable/DismissableAlert'
 defineOptions({ name: 'SettingsViewLayout' })
 
 const props = defineProps({
-  infoName: { type: String, required: true },
+  infoName: { type: String },
   infoLabel: { type: String },
   infoDismissLabel: { type: String },
   noResults: { type: Boolean, default: false }
@@ -20,6 +20,7 @@ const { t } = useI18n()
 
 const dismissLabel = computed(() => props.infoDismissLabel ?? t('settings.layout.infoDismiss'))
 </script>
+
 <template>
   <div class="settings-view-layout d-flex flex-column gap-4 px-2 py-4">
     <slot v-if="infoLabel" name="info" v-bind="{ infoDismissLabel, infoLabel, infoName }">
