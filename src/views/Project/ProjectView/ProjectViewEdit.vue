f311a04f384d32b0bacee27576342f8d6ee548bf
Pierre Romera <hello@pirhoo.com>
2025-04-03 11:22:49 +0000

    refactor: waitFor from useWait instead of useCore

--- src/views/Project/ProjectView/ProjectViewEdit.vue
+++ src/views/Project/ProjectView/ProjectViewEdit.vue
@@ -1,11 +1,12 @@
 <script setup>
 import { computed } from 'vue'
-import { get, uniqueId } from 'lodash'
+import { get } from 'lodash'
 import { useI18n } from 'vue-i18n'
 
 import AppOverlay from '@/components/AppOverlay/AppOverlay'
 import ProjectForm from '@/components/Project/ProjectForm'
 import { useCore } from '@/composables/useCore'
+import { useWait } from '@/composables/useWait'
 
 const OPERATION = Object.freeze({ UPDATE: 'update' })
 
@@ -15,26 +16,22 @@ const props = defineProps({
   }
 })
 
-const { core, toast, wait } = useCore()
+const { core, toast } = useCore()
+const { waitFor, isLoading } = useWait()
 const { t } = useI18n()
 
 const project = computed(() => core.findProject(props.name))
 
-const loaderId = uniqueId('project-view-edit:updating:')
-
-async function submit(project) {
+const submit = waitFor(async (project) => {
   try {
-    wait.start(loaderId.value)
     await core.api.updateProject(project)
     core.setProject(project)
     notifySucceed(OPERATION.UPDATE)
-    wait.end(loaderId.value)
     return redirectToProject(project)
   } catch (error) {
     notifyFailed(error, OPERATION.UPDATE)
-    wait.end(loaderId.value)
   }
-}
+})
 
 function notifySucceed(operation) {
   const body = t(`projectViewEdit.notify.${operation}.succeedBody`)
@@ -54,8 +51,8 @@ function redirectToProject({ name }) {
 
 <template>
   <div class="project-view-edit">
-    <app-overlay rounded="sm" :show="$wait.is('creating')">
-      <project-form edit card :disabled="$wait.is(loaderId)" :values="project" @submit="submit">
+    <app-overlay rounded="sm" :show="isLoading">
+      <project-form edit card :disabled="isLoading" :values="project" @submit="submit">
         <template #submit-text>
           {{ $t('projectViewEdit.submit') }}
         </template>
