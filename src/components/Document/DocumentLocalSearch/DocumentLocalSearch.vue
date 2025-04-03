86ba24f38def3038d9031ad5b8ff549da3b30534
Pierre Romera <hello@pirhoo.com>
2025-04-03 17:18:52 +0000

    feat: add local search navigation with useKeyboardShortcuts composable icij/datashare#1699

--- src/components/Document/DocumentLocalSearch/DocumentLocalSearch.vue
+++ src/components/Document/DocumentLocalSearch/DocumentLocalSearch.vue
@@ -1,9 +1,11 @@
 <script setup>
-import { computed } from 'vue'
+import { computed, useTemplateRef } from 'vue'
 
 import DocumentLocalSearchInput from './DocumentLocalSearchInput'
 import DocumentLocalSearchNav from './DocumentLocalSearchNav'
 import DocumentLocalSearchOccurrences from './DocumentLocalSearchOccurrences'
+
+import { useKeyboardShortcuts } from '@/composables/useKeyboardShortcuts'
 
 const modelValue = defineModel({ type: String, default: '' })
 const activeIndex = defineModel('activeIndex', { type: Number, default: 0 })
@@ -18,21 +20,40 @@ const props = defineProps({
   }
 })
 
+const input = useTemplateRef('input')
+
 const disabledPrevious = computed(() => !props.modelValue || props.activeIndex <= 1)
 const disabledNext = computed(() => !props.modelValue || props.activeIndex === props.occurrences)
+
+const focus = () => input.value.focus()
 const previous = () => (activeIndex.value = Math.max(1, activeIndex.value - 1))
 const next = () => (activeIndex.value = Math.min(props.occurrences, activeIndex.value + 1))
+
+const { wheneverActionShortcut, findActionKey } = useKeyboardShortcuts({
+  passive: false,
+  onEventFired(e) {
+    // Only prevent default when the shortcut to find in document is triggered. This
+    // way we can prevent the web browser from open the build-in "search in page".
+    if (findInDocumentKey.value) {
+      e.preventDefault()
+    }
+  }
+})
+
+// We get the key separetly in order to be able to use it in the `onEventFired` callback above
+const findInDocumentKey = findActionKey('findInDocument')
+// The "next occurence" shortcut ("enter") might overlap with the "previous occurence" shortcut ("shift+enter")
+// so we need to check if the "previous occurence" shortcut is not triggered before going next.
+const findPreviousOccurrenceKey = findActionKey('findPreviousOccurrence')
+
+wheneverActionShortcut('findInDocument', focus)
+wheneverActionShortcut('findPreviousOccurrence', previous)
+wheneverActionShortcut('findNextOccurrence', () => !findPreviousOccurrenceKey.value && next())
 </script>
 
 <template>
   <div class="document-local-search d-flex">
-    <document-local-search-input
-      v-model="modelValue"
-      class="flex-grow-1"
-      :loading="loading"
-      @keyup.enter="next"
-      @keyup.enter.shift="previous"
-    />
+    <document-local-search-input ref="input" v-model="modelValue" class="flex-grow-1" :loading="loading" />
     <document-local-search-nav
       :disabled-previous="disabledPrevious"
       :disabled-next="disabledNext"
