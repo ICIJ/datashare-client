0d28e20f877c5b154c20876146269df613396afc
Pierre Romera <hello@pirhoo.com>
2025-04-03 11:08:45 +0000

    feat: enhance useWait composable with scoped loaders and debounced state management

--- src/composables/useWait.js
+++ src/composables/useWait.js
@@ -1,36 +1,120 @@
-import { computed, getCurrentInstance, useId, toRef } from 'vue'
+import { computed, useId, toRef, getCurrentInstance } from 'vue'
+import { debounce, noop } from 'lodash'
 
-export function useWait() {
-  const instance = getCurrentInstance()
+import { useWaitStore } from '@/store/modules/wait'
 
-  // If `getCurrentInstance` is called outside of a Vue component setup function, it will return `null`.
-  if (!instance) {
-    throw new Error('useCore must be called within a Vue component setup function.')
+export function useWait({ throttle = 0, scoped = false } = {}) {
+  const waitStore = useWaitStore()
+  const loaderId = useId()
+  const isLoading = computed(() => waiting(loaderId))
+  const { uid } = getCurrentInstance()
+
+  /**
+   * If the composable is scoped, we use the uid of the component
+   * to ensure the loaders are unique to the component.
+   *
+   * @private
+   * @type {string}
+   */
+  const scopeKey = computed(() => (scoped ? uid : null))
+
+  /**
+   * Get the value of a loader
+   *
+   * @private
+   * @param {string|Ref}
+   * @return {boolean|undefined} - The value of the loader
+   */
+  const get = (id) => waitStore.get(scopeKey, id)
+
+  /**
+   * Set the value of a loader
+   * @param {string|Ref} id - The loader id
+   * @param {boolean} value - The value to set
+   * @return {boolean} - The value of the loader
+   */
+  const set = (id, value) => waitStore.set(scopeKey, id, value)
+
+  /**
+   * Set the value of a loader with a debounce
+   *
+   * @private
+   * @param {string|Ref}
+   * @param {boolean} value - The value to set
+   * @return {boolean} - The value of the loader
+   */
+  const debouncedSet = throttle ? debounce(set, throttle) : set
+
+  /**
+   * Start a loader
+   *
+   * @public
+   * @param {string|Ref} id - The loader id
+   */
+  function start(id) {
+    debouncedSet(id, true)
   }
 
-  // The `instance` object has a `proxy` property, which is the component's public instance.
-  // `proxy.$wait` gives us access to the global `$wait` object provided by vue-wait
-  const {
-    proxy: { $wait }
-  } = instance
+  /**
+   * End a loader
+   *
+   * @public
+   * @param {string|Ref} id - The loader id
+   */
+  function end(id) {
+    debouncedSet(id, false)
+  }
 
-  const loaderId = useId()
-  const isLoading = computed(() => $wait.is(loaderId))
+  /**
+   * Check if a loader is active
+   *
+   * @public
+   * @param {string|Ref} id - The loader id
+   * @return {boolean} - True if the loader is active=
+   */
+  function waiting(id) {
+    return !!get(id)
+  }
 
-  // `waitFor` is a higher-order function that takes an `id` and a function `fn`.
-  const waitFor = (id, fn) => {
+  /**
+   * Wait for a function to finish and start a loader
+   *
+   * @private
+   * @param {string|Ref} id - The loader id
+   * @param {function} fn - The function to wait for
+   * @return {function} - A function that starts the loader and waits for the function to finish
+   */
+  function waitForLoader(id, fn) {
     const idRef = toRef(id)
 
     return async (...args) => {
-      $wait.start(idRef.value)
+      start(idRef)
       const promise = await fn(...args)
-      $wait.end(idRef.value)
+      end(idRef)
       return promise
     }
   }
 
-  // `wait` is a helper function that takes a function `fn` and waits for the loader to finish.
-  const wait = (fn) => waitFor(loaderId, fn)
+  /**
+   * Wait for a function to finish and start a loader
+   *
+   * @public
+   * @param {function|string} fnOrId - The loader id or function to wait for
+   * @param {function} fn - The function to wait for
+   *
+   * @return {function} - A function that starts the loader and waits for the function to finish
+   */
+  function waitFor(fnOrId, fn = noop) {
+    if (typeof fnOrId === 'function') {
+      return waitForLoader(loaderId, fnOrId)
+    }
 
-  return { $wait, waitFor, wait, isLoading, loaderId }
+    if (typeof fnOrId === 'string' && typeof fn === 'function') {
+      return waitForLoader(fnOrId, fn)
+    }
+
+    throw new Error('The first argument must be a function or a string')
+  }
+
+  return { waitFor, isLoading, loaderId, start, end, waiting }
 }
