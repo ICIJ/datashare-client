510c2adc4d19665cc5921610e8895c4802496435
Caroline Desprat <cdesprat@icij.org>
2025-04-03 17:20:01 +0000

    feat: add batch tagging on documents through modal on search selection

--- src/views/Search/SearchSelection.vue
+++ src/views/Search/SearchSelection.vue
@@ -4,12 +4,14 @@ import { property } from 'lodash'
 import { useI18n } from 'vue-i18n'
 
 import { useCore } from '@/composables/useCore'
-import { useStarredStore } from '@/store/modules'
+import { useSearchStore, useStarredStore, useDocumentStore } from '@/store/modules'
 import { useSelection } from '@/composables/useSelection'
 import { useBreakpoints } from '@/composables/useBreakpoints'
 import { breakpointSizeValidator, SIZE } from '@/enums/sizes'
 import ButtonIcon from '@/components/Button/ButtonIcon'
 import FormActions from '@/components/Form/FormActions/FormActions'
+import { usePromptModal } from '@/composables/usePromptModal'
+import SearchSelectionAddTagsModal from '@/views/Search/SearchSelectionAddTagsModal'
 
 const selection = defineModel('selection', { type: Array, default: () => [] })
 
@@ -49,17 +51,44 @@ const { breakpointDown } = useBreakpoints()
 const isCompact = computed(() => breakpointDown.value[props.compactAutoBreakpoint] || props.compact)
 
 const starredStore = useStarredStore()
+const searchStore = useSearchStore()
+const documentStore = useDocumentStore()
 const { toastedPromise } = useCore()
 const { t } = useI18n()
-
+const nbSelection = computed(() => {
+  return selectionEntries.value.length
+})
+const noSelection = computed(() => {
+  return nbSelection.value === 0
+})
 const starSelection = async () => {
-  const successMessage = t('document.starred')
-  return toastedPromise(starredStore.starDocuments(selectionEntries.value), { successMessage })
+  return toastedPromise(starredStore.starDocuments(selectionEntries.value), { successMessage: t('document.starred') })
+}
+const { prompt: showAddTagsModal } = usePromptModal(SearchSelectionAddTagsModal)
+async function addTagsModal() {
+  const indices = searchStore.indices
+  const result = await showAddTagsModal({ indices, nbDocs: nbSelection.value })
+  // Only a valid submit returns value. Cancel or modal hide returns null.
+  if (result?.tags) {
+    await toastedPromise(tagSelection(selectionEntries.value, result.tags), {
+      successMessage: t('searchSelection.successTag', { nbTags: result.tags.length, nbDocs: nbSelection.value })
+    })
+  }
+}
+
+const tagSelection = async (documents, labels) => {
+  return documentStore.addTags({ documents, labels })
 }
 </script>
 
 <template>
-  <form-actions class="search-selection px-3 flex-shrink-1" variant="outline-tertiary" :compact="compact" compact-auto>
+  <form-actions
+    class="search-selection px-3 flex-shrink-1"
+    variant="outline-tertiary"
+    compact-variant="outline-tertiary"
+    :compact="compact"
+    compact-auto
+  >
     <template #start>
       <b-form-checkbox
         v-model="selected"
@@ -73,12 +102,14 @@ const starSelection = async () => {
       <button-icon
         :label="$t('searchSelection.star')"
         icon-left="star"
+        class="flex-shrink-0"
+        :disabled="noSelection"
         :hide-label="isCompact"
         :square="isCompact"
         @click="starSelection"
       />
     </template>
-    <button-icon :label="$t('searchSelection.tag')" icon-left="hash" />
+    <button-icon :label="$t('searchSelection.tag')" icon-left="hash" :disabled="noSelection" @click="addTagsModal" />
   </form-actions>
 </template>
 
