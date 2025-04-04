e8b2f7ba2b4723b1c96d8bf828183272221a567c
Pierre Romera <hello@pirhoo.com>
2025-04-04 17:37:43 +0000

    spec: install plugins with i18n

--- tests/unit/specs/components/Form/FormControl/FormControlSearch/FormControlSearch.spec.js
+++ tests/unit/specs/components/Form/FormControl/FormControlSearch/FormControlSearch.spec.js
@@ -1,17 +1,25 @@
 import { shallowMount, mount } from '@vue/test-utils'
 
 import FormControlSearch from '@/components/Form/FormControl/FormControlSearch'
+import CoreSetup from '~tests/unit/CoreSetup'
 
 describe('FormControlSearch', () => {
+  let global
+
+  beforeEach(() => {
+    const core = CoreSetup.init().useAll()
+    global = { plugins: core.plugins }
+  })
+
   it('renders without crashing', () => {
     const props = { modelValue: null }
-    const wrapper = shallowMount(FormControlSearch, { props })
+    const wrapper = shallowMount(FormControlSearch, { props, global })
     expect(wrapper.exists()).toBe(true)
   })
 
   it('update input value', async () => {
     const props = { modelValue: 'search1' }
-    const wrapper = mount(FormControlSearch, { props })
+    const wrapper = mount(FormControlSearch, { props, global })
     await wrapper.find('input').setValue('search2')
     expect(wrapper.emitted('update:modelValue')).toHaveLength(1)
     expect(wrapper.emitted().input).toHaveLength(2)
