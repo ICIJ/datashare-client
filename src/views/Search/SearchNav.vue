c3287a90cd96f883fa0c50de6370fb0a64abf917
Pierre Romera <hello@pirhoo.com>
2025-04-07 10:29:43 +0000

    refactor: remove unecessary modal check

--- src/views/Search/SearchNav.vue
+++ src/views/Search/SearchNav.vue
@@ -1,16 +1,13 @@
 <script setup>
-import { inject } from 'vue'
-
 import DocumentSearchNav from '@/components/Document/DocumentSearchNav/DocumentSearchNav'
 import { useSearchNav } from '@/composables/useSearchNav'
 
-const modal = inject('modal', false)
 const { isDocumentInPage, disabledPrevious, disabledNext, previous, next } = useSearchNav()
 </script>
 
 <template>
   <document-search-nav
-    v-if="isDocumentInPage && !modal"
+    v-if="isDocumentInPage"
     :disabled-previous="disabledPrevious"
     :disabled-next="disabledNext"
     @previous="previous"
