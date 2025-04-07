655edb7ab0c014ae300c375ba5e0d414add7f742
Pierre Romera <hello@pirhoo.com>
2025-04-07 13:39:59 +0000

    feat: hide modal on route change

--- src/components/Document/DocumentModal.vue
+++ src/components/Document/DocumentModal.vue
@@ -1,7 +1,13 @@
 <script setup>
+import { useId } from 'vue'
+import { useRoute } from 'vue-router'
+import { useModal } from 'bootstrap-vue-next'
+
 import AppModal from '@/components/AppModal/AppModal'
 import DocumentFloating from '@/components/Document/DocumentFloating'
 import DocumentView from '@/views/Document/DocumentView/DocumentView'
+import { onRouteLeaveNotMatch } from '@/composables/onRouteLeaveNotMatch'
+import { onRouteUpdateNotMatch } from '@/composables/onRouteUpdateNotMatch'
 
 const modelValue = defineModel({ type: Boolean, default: false })
 
@@ -20,10 +26,26 @@ defineProps({
     default: ''
   }
 })
+
+const route = useRoute()
+const modalId = useId()
+const { hide } = useModal(modalId)
+
+onRouteLeaveNotMatch(route.name, () => hide())
+onRouteUpdateNotMatch(route.name, () => hide())
 </script>
 
 <template>
-  <app-modal v-model="modelValue" body-class="py-0 px-5" no-footer no-header no-header-close fullscreen lazy>
+  <app-modal
+    :id="modalId"
+    v-model="modelValue"
+    body-class="py-0 px-5"
+    no-footer
+    no-header
+    no-header-close
+    fullscreen
+    lazy
+  >
     <document-floating class="my-3" no-reduce no-expand>
       <slot>
         <document-view :id="id" :routing="routing" :index="index" :q="q" />
