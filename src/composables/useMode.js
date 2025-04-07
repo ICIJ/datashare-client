e9178d0219d3cb05d94115ecc486fd66c6639de2
Pierre Romera <hello@pirhoo.com>
2025-04-07 16:25:16 +0000

    feat: add modeName

--- src/composables/useMode.js
+++ src/composables/useMode.js
@@ -6,12 +6,13 @@ import { MODE_NAME } from '@/mode'
 export function useMode() {
   const { core } = useCore()
   const mode = computed(() => core.mode)
-  const isServer = computed(() => mode.value.modeName === MODE_NAME.SERVER)
-  const isLocal = computed(() => mode.value.modeName === MODE_NAME.LOCAL)
-  const isEmbedded = computed(() => mode.value.modeName === MODE_NAME.EMBEDDED)
-  const isMode = (name) => mode.value.modeName === name.toUpperCase()
+  const modeName = computed(() => mode.value.modeName)
+  const isServer = computed(() => modeName.value === MODE_NAME.SERVER)
+  const isLocal = computed(() => modeName.value === MODE_NAME.LOCAL)
+  const isEmbedded = computed(() => modeName.value === MODE_NAME.EMBEDDED)
+  const isMode = (name) => modeName.value === name.toUpperCase()
 
-  return { mode, isServer, isLocal, isEmbedded, isMode }
+  return { mode, modeName, isServer, isLocal, isEmbedded, isMode }
 }
 
 export default useMode
