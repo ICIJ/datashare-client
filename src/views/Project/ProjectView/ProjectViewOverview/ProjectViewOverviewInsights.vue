ae7080ce5419ce5061a4b54e9969910ef50436cd
Pierre Romera <hello@pirhoo.com>
2025-04-08 12:14:57 +0000

    refactor: move widgets grid to separated component with a "section" filter icij/datashare#1701

--- src/views/Project/ProjectView/ProjectViewOverview/ProjectViewOverviewInsights.vue
+++ src/views/Project/ProjectView/ProjectViewOverview/ProjectViewOverviewInsights.vue
@@ -1,69 +1,15 @@
 <script setup>
-import { computed, watch, onBeforeMount, toRef } from 'vue'
+import ProjectViewOverviewWidgets from './ProjectViewOverviewWidgets'
 
-import { useMode } from '@/composables/useMode'
-import { useInsightsStore } from '@/store/modules'
-
-const props = defineProps({ name: String })
-const insightsStore = useInsightsStore()
-const { modeName } = useMode()
-
-const widgets = computed(() => {
-  return insightsStore.instantiatedWidgets.filter(({ modes }) => {
-    return !modes || modes.includes(modeName.value)
-  })
+defineProps({
+  name: {
+    type: String
+  }
 })
-
-watch(toRef(props, 'name'), insightsStore.setProject)
-onBeforeMount(() => insightsStore.setProject(props.name))
 </script>
 
 <template>
   <div class="project-view-overview-insights">
-    <div class="project-view-overview-insights__container">
-      <b-row class="align-items-stretch">
-        <b-col v-for="(widget, index) in widgets" :key="index" :lg="widget.cols">
-          <div class="project-view-overview-insights__container__widget" :class="{ card: widget.card }">
-            <component :is="widget.component" :widget="widget" :project="name" class="flex-grow-1" />
-          </div>
-        </b-col>
-      </b-row>
-    </div>
+    <project-view-overview-widgets :name="name" />
   </div>
 </template>
-
-<style lang="scss" scoped>
-.project-view-overview-insights {
-  &__container {
-    margin: $spacer-xl auto;
-
-    .row {
-      margin-right: 0;
-      margin-left: 0;
-
-      & > .col,
-      & > [class*='col-'] {
-        min-width: 0;
-        padding-right: $spacer;
-        padding-left: $spacer;
-      }
-    }
-
-    &__widget {
-      display: flex;
-      flex-direction: row;
-      margin-bottom: $spacer-xl;
-      min-height: calc(100% - #{$spacer-xl});
-      position: relative;
-
-      &.card {
-        border: 0;
-      }
-
-      & > * {
-        min-width: 0;
-      }
-    }
-  }
-}
-</style>
