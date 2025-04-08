818fe7187b1997705d6ede7dd8dcdccb2eb49917
Pierre Romera <hello@pirhoo.com>
2025-04-08 12:36:33 +0000

    refactor: move default empty state to widgets list

--- src/views/Project/ProjectView/ProjectViewOverview/ProjectViewOverviewGraph.vue
+++ src/views/Project/ProjectView/ProjectViewOverview/ProjectViewOverviewGraph.vue
@@ -1,10 +1,6 @@
 <script setup>
 import ProjectViewOverviewWidgets from './ProjectViewOverviewWidgets'
 
-import EmptyState from '@/components/EmptyState/EmptyState'
-import appBuilding from '@/assets/images/illustrations/app-building.svg'
-import appBuildingDark from '@/assets/images/illustrations/app-building-dark.svg'
-
 defineProps({
   name: {
     type: String
@@ -14,10 +10,6 @@ defineProps({
 
 <template>
   <div class="project-view-overview-graph">
-    <project-view-overview-widgets :name="name" section="graph">
-      <template #empty>
-        <empty-state :label="$t('global.building')" :image="appBuilding" :image-dark="appBuildingDark" />
-      </template>
-    </project-view-overview-widgets>
+    <project-view-overview-widgets :name="name" section="graph" />
   </div>
 </template>
