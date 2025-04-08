f8a7372f945c991726f2fdbfe37c3c159634a99c
Pierre Romera <hello@pirhoo.com>
2025-04-08 12:42:00 +0000

    refactor: use widget list to display content icij/datashare#1701

--- src/views/Project/ProjectView/ProjectViewOverview/ProjectViewOverviewPaths.vue
+++ src/views/Project/ProjectView/ProjectViewOverview/ProjectViewOverviewPaths.vue
@@ -1,7 +1,5 @@
 <script setup>
-import EmptyState from '@/components/EmptyState/EmptyState'
-import appBuilding from '@/assets/images/illustrations/app-building.svg'
-import appBuildingDark from '@/assets/images/illustrations/app-building-dark.svg'
+import ProjectViewOverviewWidgets from './ProjectViewOverviewWidgets'
 
 defineProps({
   name: {
@@ -11,7 +9,7 @@ defineProps({
 </script>
 
 <template>
-  <div class="project-view-overview-paths">
-    <empty-state :label="$t('global.building')" :image="appBuilding" :image-dark="appBuildingDark" />
+  <div class="project-view-overview-path">
+    <project-view-overview-widgets :name="name" section="path" />
   </div>
 </template>
