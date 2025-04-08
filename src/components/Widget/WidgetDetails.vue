bc9a781859e66e9fc41a0552cf52be76a3c3645c
Pierre Romera <hello@pirhoo.com>
2025-04-08 12:34:02 +0000

    refactor turn details tab in a list of widgets

--- src/components/Widget/WidgetDetails.vue
+++ src/components/Widget/WidgetDetails.vue
@@ -6,10 +6,11 @@ import { PhosphorIcon } from '@icij/murmur-next'
 
 import DisplayRaw from '@/components/Display/DisplayRaw'
 import DisplayDatetime from '@/components/Display/DisplayDatetime'
+import { useCore } from '@/composables/useCore'
 
 const props = defineProps({
   project: {
-    type: Object,
+    type: String,
     required: true
   }
 })
@@ -51,11 +52,13 @@ const fields = [
 ]
 
 const { t } = useI18n()
+const { core } = useCore()
 
 // Generate metadata for each field
 const metadata = computed(() => {
+  const project = core.findProject(props.project)
   return fields.map((field) => {
-    const rawValue = props.project[field.key]
+    const rawValue = project[field.key]
     const value = isFunction(field.formatter) ? field.formatter({ ...field, rawValue }) : rawValue
     const href = isFunction(field.href) ? field.href({ ...field, rawValue, value }) : null
     const label = t(`widget.project.fields.${field.key}`)
@@ -73,18 +76,18 @@ const availableMetadata = computed(() => {
 </script>
 
 <template>
-  <div class="project-details">
+  <b-card class="widget-details border-0" body-class="p-4">
     <div
       v-for="{ key, label, href, value, component, classList, icon } in availableMetadata"
       :key="key"
-      class="project-details__item"
+      class="widget-details__item"
     >
       <div class="row g-0">
-        <div class="col-12 col-md-3 project-details__item__label d-flex flex-truncate align-items-center">
+        <div class="col-12 col-md-3 widget-details__item__label d-flex flex-truncate align-items-center">
           <phosphor-icon v-if="icon" :name="icon" class="me-2 flex-shrink-0" />
           <span class="text-truncate">{{ label }}</span>
         </div>
-        <div class="col project-details__item__value ps-md-3 fw-medium">
+        <div class="col widget-details__item__value ps-md-3 fw-medium">
           <a v-if="href" :href="href" class="text-truncate d-block">
             <component :is="component" :value="value" :class="classList" />
           </a>
@@ -94,11 +97,13 @@ const availableMetadata = computed(() => {
         </div>
       </div>
     </div>
-  </div>
+  </b-card>
 </template>
 
 <style lang="scss" scope>
-.project-details {
+.widget-details {
+  height: 100%;
+
   &__item {
     &:not(:last-of-type) {
       margin-bottom: $spacer-xs;
