cb2ec4d54e173bc408391e87bcad7f5efde3cb5c
Pierre Romera <hello@pirhoo.com>
2025-04-08 10:22:16 +0000

    feat: add hooks to project card actions

--- src/components/Project/ProjectCard/ProjectCardActions.vue
+++ src/components/Project/ProjectCard/ProjectCardActions.vue
@@ -1,4 +1,5 @@
 <script setup>
+import Hook from '@/components/Hook/Hook'
 import ProjectActions from '@/components/Project/ProjectActions'
 
 defineProps({
@@ -11,6 +12,8 @@ defineProps({
 
 <template>
   <div class="project-actions">
+    <hook name="project-card-actions:before" :bind="{ project }" />
     <project-actions :project="project" />
+    <hook name="project-card-actions:after" :bind="{ project }" />
   </div>
 </template>
