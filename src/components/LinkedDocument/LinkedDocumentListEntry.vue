2c045cbd4280c0163a2f56cffa5fea6c755741cf
Caroline Desprat <carofip38@hotmail.com>
2025-04-10 17:25:49 +0200

    feat: update document route properties

--- src/components/LinkedDocument/LinkedDocumentListEntry.vue
+++ src/components/LinkedDocument/LinkedDocumentListEntry.vue
@@ -1,22 +1,24 @@
 <template>
-  <span class="linked-document-list-entry d-inline-flex align-items-center pe-2"
-    ><component :is="findContentTypeIcon(contentType)" class="tertiary me-2" /><router-link
-      :to="{ path: url }"
-      class="linked-document-list-entry__link d-inline-block text-nowrap"
-      ><slot>{{ name }}</slot></router-link
-    ></span
-  >
+  <span class="linked-document-list-entry d-inline-flex align-items-center gap-2"
+    ><phosphor-icon :name="findContentTypeIcon(contentType)" class="tertiary" />
+    <router-link-document :id="id" :index="index" :routing="routing">{{ name }}</router-link-document>
+  </span>
 </template>
 
 <script setup>
+import { PhosphorIcon } from '@icij/murmur-next'
+
 import { findContentTypeIcon } from '@/utils/phosphorIconFiles'
+import RouterLinkDocument from '@/components/RouterLink/RouterLinkDocument'
 
 defineOptions({
   name: 'LinkedDocumentListEntry'
 })
 defineProps({
   contentType: { type: String, default: 'file' },
-  url: { type: String, required: true },
+  id: { type: String, required: true },
+  routing: { type: String, required: true },
+  index: { type: String, required: true },
   name: { type: String, default: 'No name provided' }
 })
 </script>
