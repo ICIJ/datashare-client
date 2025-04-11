57e700a208bfc0bc1f7ca96027acd6568a802ff0
Caroline Desprat <carofip38@hotmail.com>
2025-04-11 16:47:34 +0200

    feat: use accordion in linked document card

--- src/components/LinkedDocument/LinkedDocumentSection.vue
+++ src/components/LinkedDocument/LinkedDocumentSection.vue
@@ -1,10 +1,6 @@
 <template>
   <section class="linked-document-section">
-    <p class="linked-document-section__title d-inline-flex gap-2 text-body-emphasis">
-      <phosphor-icon :name="icon" />
-      <slot name="title"> {{ documents.length }} {{ title }} </slot>
-    </p>
-    <div class="ms-4 d-flex flex-column flex-grow-1">
+    <div class="ms-3 d-flex flex-column flex-grow-1">
       <p class="linked-document-section__description">
         <slot name="description">{{ description }}</slot>
       </p>
@@ -13,50 +9,27 @@
           <linked-document-list :documents="documents" />
         </slot>
       </div>
-      <p>
-        <button-icon :icon-right="PhMagnifyingGlass" :label="t('linkedDocumentList.searchAll')" :to="toSearch" />
-      </p>
     </div>
   </section>
 </template>
 
 <script setup>
-import { useI18n } from 'vue-i18n'
-import { PhosphorIcon } from '@icij/murmur-next'
-
-import ButtonIcon from '@/components/Button/ButtonIcon'
 import LinkedDocumentList from '@/components/LinkedDocument/LinkedDocumentList'
 defineOptions({
   name: 'LinkedDocumentSection'
 })
 defineProps({
-  title: {
-    type: String,
-    required: true
-  },
-  icon: {
-    type: [String, Object, Array],
-    required: true
-  },
   description: {
     type: String,
     required: true
   },
   documents: {
     type: Array
-  },
-  toSearch: {
-    type: [Object, String]
   }
 })
-const { t } = useI18n()
 </script>
 <style lang="scss" scoped>
 .linked-document-section {
-  &__title {
-    font-weight: 500;
-    text-wrap: pretty;
-  }
   &__description {
     text-wrap: pretty;
   }
---
+++ src/components/LinkedDocument/LinkedDocumentSectionHeader.vue
@@ -0,0 +1,35 @@
+<template>
+  <header class="linked-document-section-header d-inline-flex gap-2 text-body-emphasis">
+    <phosphor-icon :name="icon" />
+    <router-link :to="toSearch">
+      <slot name="title"> {{ title }} </slot></router-link
+    >
+  </header>
+</template>
+
+<script setup>
+import { PhosphorIcon } from '@icij/murmur-next'
+
+defineOptions({
+  name: 'LinkedDocumentSection'
+})
+defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  icon: {
+    type: [String, Object, Array],
+    required: true
+  },
+  toSearch: {
+    type: [Object, String]
+  }
+})
+</script>
+<style lang="scss" scoped>
+.linked-document-section-header {
+  font-weight: 500;
+  text-wrap: pretty;
+}
+</style>
