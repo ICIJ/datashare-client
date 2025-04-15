8b1e960f65c049897c81290c5c2e1e3df7e2d277
Pierre Romera <hello@pirhoo.com>
2025-04-15 17:34:53 +0000

    refactor: remove console.log

--- src/components/LinkedDocument/LinkedDocumentCard.vue
+++ src/components/LinkedDocument/LinkedDocumentCard.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="linked-document-card bg-tertiary-subtle rounded row gx-0">
-    <slot name="siblings" v-bind="{ modelValue: open, onUpdateModelValue }"> </slot>
-    <slot name="children" v-bind="{ modelValue: open, onUpdateModelValue }"> </slot>
+    <slot name="siblings" v-bind="{ modelValue, onUpdateModelValue }"> </slot>
+    <slot name="children" v-bind="{ modelValue, onUpdateModelValue }"> </slot>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
 defineOptions({
   name: 'LinkedDocumentCard'
 })
-const open = ref(false)
-function onUpdateModelValue(modelValue) {
-  console.log(modelValue)
-  open.value = modelValue
+
+const modelValue = defineModel({ type: Boolean, default: false })
+
+function onUpdateModelValue(value) {
+  modelValue.value = value
 }
 </script>
