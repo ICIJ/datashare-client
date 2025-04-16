ea0f2125e243158c7c29f5a4715b7d28c178fcf7
Pierre Romera <hello@pirhoo.com>
2025-04-16 10:34:49 +0000

    feat: add empty state to task documents icij/datashare#1764

--- src/views/Task/TaskDocuments/TaskDocumentsList.vue
+++ src/views/Task/TaskDocuments/TaskDocumentsList.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { useI18n } from 'vue-i18n'
 
+import tasksDocumentsEmpty from '@/assets/images/illustrations/tasks-documents-empty.svg'
 import ButtonRowActionDelete from '@/components/Button/ButtonRowAction/ButtonRowActionDelete'
 import ButtonRowActionStop from '@/components/Button/ButtonRowAction/ButtonRowActionStop'
 import DisplayDatetimeFromNow from '@/components/Display/DisplayDatetimeFromNow'
@@ -8,6 +9,7 @@ import DisplayNumber from '@/components/Display/DisplayNumber'
 import DisplayProgress from '@/components/Display/DisplayProgress'
 import DisplayProjectList from '@/components/Display/DisplayProjectList'
 import DisplayStatus from '@/components/Display/DisplayStatus'
+import EmptyState from '@/components/EmptyState/EmptyState'
 import PageTableGeneric from '@/components/PageTable/PageTableGeneric'
 import { useTaskSettings } from '@/composables/useTaskSettings'
 import { getHumanTaskName, TASK_NAME } from '@/enums/taskNames'
@@ -35,44 +37,50 @@ function isRunning(item) {
 </script>
 
 <template>
-  <task-page
-    v-slot="{ tasks, sort, order, updateSort, updateOrder, empty, loading }"
-    :task-filter="[TASK_NAME.INDEX, TASK_NAME.SCAN]"
-    page-name="documents"
-    show-add
-  >
-    <page-table-generic
-      v-if="loading || !empty"
-      :items="tasks"
-      :fields="propertiesModelValueOptions"
-      :sort="sort"
-      :order="order"
-      :loading="loading"
-      @update:sort="updateSort"
-      @update:order="updateOrder"
-    >
-      <template #cell(state)="{ item }">
-        <display-status :value="item.state" />
-      </template>
-      <template #cell(name)="{ item }">
-        {{ t(getHumanTaskName(item.name)) }}
-      </template>
-      <template #cell(documents)="{ item }">
-        <display-number v-if="item?.result?.value && item.result.value[1]" :value="item.result.value[1]" />
-      </template>
-      <template #cell(project)="{ item }">
-        <display-project-list :values="getProject(item)" />
-      </template>
-      <template #cell(progress)="{ item }">
-        <display-progress :value="item.progress" />
-      </template>
-      <template #cell(createdAt)="{ item }">
-        <display-datetime-from-now :value="item.createdAt" />
-      </template>
-      <template #row-actions="{ item }">
-        <button-row-action-stop :disabled="!isRunning(item)" @stop="stopTask(item.id)" />
-        <button-row-action-delete @delete="taskStore.removeTask(item.id)" />
-      </template>
-    </page-table-generic>
+  <task-page :task-filter="[TASK_NAME.INDEX, TASK_NAME.SCAN]" page-name="documents" show-add>
+    <template #empty>
+      <empty-state
+        image-max-width="345px"
+        :image="tasksDocumentsEmpty"
+        :label="$t('task.documents.list.emptyStateLabel')"
+        :action-label="$t('task.documents.list.emptyStateAction')"
+        :action-to="{ name: 'task.documents.new' }"
+      />
+    </template>
+    <template #default="{ tasks, sort, order, updateSort, updateOrder, empty, loading }">
+      <page-table-generic
+        v-if="loading || !empty"
+        :items="tasks"
+        :fields="propertiesModelValueOptions"
+        :sort="sort"
+        :order="order"
+        :loading="loading"
+        @update:sort="updateSort"
+        @update:order="updateOrder"
+      >
+        <template #cell(state)="{ item }">
+          <display-status :value="item.state" />
+        </template>
+        <template #cell(name)="{ item }">
+          {{ t(getHumanTaskName(item.name)) }}
+        </template>
+        <template #cell(documents)="{ item }">
+          <display-number v-if="item?.result?.value && item.result.value[1]" :value="item.result.value[1]" />
+        </template>
+        <template #cell(project)="{ item }">
+          <display-project-list :values="getProject(item)" />
+        </template>
+        <template #cell(progress)="{ item }">
+          <display-progress :value="item.progress" />
+        </template>
+        <template #cell(createdAt)="{ item }">
+          <display-datetime-from-now :value="item.createdAt" />
+        </template>
+        <template #row-actions="{ item }">
+          <button-row-action-stop :disabled="!isRunning(item)" @stop="stopTask(item.id)" />
+          <button-row-action-delete @delete="taskStore.removeTask(item.id)" />
+        </template>
+      </page-table-generic>
+    </template>
   </task-page>
 </template>
