973d3d2f88a1493a26c406f752c4aa7dd6e26c74
Pierre Romera <hello@pirhoo.com>
2025-04-16 10:29:20 +0000

    feat: add empty state to task entities icij/datashare#1764

--- src/views/Task/TaskEntities/TaskEntitiesList.vue
+++ src/views/Task/TaskEntities/TaskEntitiesList.vue
@@ -1,14 +1,16 @@
 <script setup>
-import TaskPage from '@/views/Task/TaskPage'
-import PageTableGeneric from '@/components/PageTable/PageTableGeneric'
-import DisplayStatus from '@/components/Display/DisplayStatus'
-import DisplayProgress from '@/components/Display/DisplayProgress'
+import tasksEntitiesEmpty from '@/assets/images/illustrations/tasks-entities-empty.svg'
 import { useTaskSettings } from '@/composables/useTaskSettings'
 import DisplayDatetimeFromNow from '@/components/Display/DisplayDatetimeFromNow'
-import { TASK_NAME } from '@/enums/taskNames'
-import EntityButton from '@/components/Entity/EntityButton'
-import { ENTITY_CATEGORY } from '@/enums/entityCategories'
+import DisplayProgress from '@/components/Display/DisplayProgress'
 import DisplayProjectList from '@/components/Display/DisplayProjectList'
+import DisplayStatus from '@/components/Display/DisplayStatus'
+import EmptyState from '@/components/EmptyState/EmptyState'
+import EntityButton from '@/components/Entity/EntityButton'
+import PageTableGeneric from '@/components/PageTable/PageTableGeneric'
+import { ENTITY_CATEGORY } from '@/enums/entityCategories'
+import { TASK_NAME } from '@/enums/taskNames'
+import TaskPage from '@/views/Task/TaskPage'
 
 const settingName = 'entities'
 const { propertiesModelValueOptions } = useTaskSettings(settingName)
@@ -23,47 +25,52 @@ function getProject(item) {
 </script>
 
 <template>
-  <task-page
-    v-slot="{ tasks, sort, order, updateSort, updateOrder, empty, loading }"
-    :task-filter="[TASK_NAME.EXTRACT_NLP, TASK_NAME.ENQUEUE_FROM_INDEX]"
-    page-name="entities"
-    show-add
-  >
-    <page-table-generic
-      v-if="loading || !empty"
-      :items="tasks"
-      :fields="propertiesModelValueOptions"
-      :sort="sort"
-      :order="order"
-      :loading="loading"
-      @update:sort="updateSort"
-      @update:order="updateOrder"
-    >
-      <template #cell(state)="{ item }">
-        <display-status :value="item.state" />
-      </template>
-      <template #cell(entitiesToFind)="{ item }">
-        <div v-if="isPipelineEmail(item)" class="d-flex gap-2">
-          <entity-button :entity="{ mention: 'Email', category: ENTITY_CATEGORY.EMAIL }" />
-        </div>
-        <div v-else class="d-flex gap-2">
-          <entity-button :entity="{ mention: 'People', category: ENTITY_CATEGORY.PERSON }" />
-          <entity-button :entity="{ mention: 'Organization', category: ENTITY_CATEGORY.ORGANIZATION }" />
-          <entity-button :entity="{ mention: 'Location', category: ENTITY_CATEGORY.LOCATION }" />
-        </div>
-      </template>
-      <template #cell(pipeline)="{ item }">
-        {{ item.args.nlpPipeline }}
-      </template>
-      <template #cell(project)="{ item }">
-        <display-project-list :values="getProject(item)" />
-      </template>
-      <template #cell(progress)="{ item }">
-        <display-progress :value="item.progress" />
-      </template>
-      <template #cell(createdAt)="{ item }">
-        <display-datetime-from-now :value="item.createdAt" />
-      </template>
-    </page-table-generic>
+  <task-page :task-filter="[TASK_NAME.EXTRACT_NLP, TASK_NAME.ENQUEUE_FROM_INDEX]" page-name="entities" show-add>
+    <template #empty>
+      <empty-state
+        :image="tasksEntitiesEmpty"
+        :label="$t('task.entities.list.emptyStateLabel')"
+        :action-label="$t('task.entities.list.emptyStateAction')"
+        :action-to="{ name: 'task.entities.new' }"
+      />
+    </template>
+    <template #default="{ tasks, sort, order, updateSort, updateOrder, loading, empty }">
+      <page-table-generic
+        v-if="loading || !empty"
+        :items="tasks"
+        :fields="propertiesModelValueOptions"
+        :sort="sort"
+        :order="order"
+        :loading="loading"
+        @update:sort="updateSort"
+        @update:order="updateOrder"
+      >
+        <template #cell(state)="{ item }">
+          <display-status :value="item.state" />
+        </template>
+        <template #cell(entitiesToFind)="{ item }">
+          <div v-if="isPipelineEmail(item)" class="d-flex gap-2">
+            <entity-button :entity="{ mention: 'Email', category: ENTITY_CATEGORY.EMAIL }" />
+          </div>
+          <div v-else class="d-flex gap-2">
+            <entity-button :entity="{ mention: 'People', category: ENTITY_CATEGORY.PERSON }" />
+            <entity-button :entity="{ mention: 'Organization', category: ENTITY_CATEGORY.ORGANIZATION }" />
+            <entity-button :entity="{ mention: 'Location', category: ENTITY_CATEGORY.LOCATION }" />
+          </div>
+        </template>
+        <template #cell(pipeline)="{ item }">
+          {{ item.args.nlpPipeline }}
+        </template>
+        <template #cell(project)="{ item }">
+          <display-project-list :values="getProject(item)" />
+        </template>
+        <template #cell(progress)="{ item }">
+          <display-progress :value="item.progress" />
+        </template>
+        <template #cell(createdAt)="{ item }">
+          <display-datetime-from-now :value="item.createdAt" />
+        </template>
+      </page-table-generic>
+    </template>
   </task-page>
 </template>
