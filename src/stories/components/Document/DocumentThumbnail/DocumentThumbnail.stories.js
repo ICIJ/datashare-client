5c10a12f1246e5d2f30349741e289a3b09541759
Pierre Romera <hello@pirhoo.com>
2025-04-17 17:05:50 +0000

    doc: add more placeholder examples

--- src/stories/components/Document/DocumentThumbnail/DocumentThumbnail.stories.js
+++ src/stories/components/Document/DocumentThumbnail/DocumentThumbnail.stories.js
@@ -1,5 +1,5 @@
 import { markRaw } from 'vue'
-import { PhFilePdf } from '@phosphor-icons/vue'
+import { PhFilePdf, PhTextAlignLeft, PhImage } from '@phosphor-icons/vue'
 
 import { withMurmur } from '~storybook/decorators/murmur'
 import DocumentThumbnail from '@/components/Document/DocumentThumbnail/DocumentThumbnail'
@@ -27,14 +27,14 @@ export default {
       contentLength: 0,
       contentType: 'image/jpeg',
       isSupportedImage: true,
-      contentTypeIcon: markRaw(PhFilePdf)
+      contentTypeIcon: [markRaw(PhFilePdf), 'fill']
     }
   }
 }
 
 export const Default = {}
 
-export const WithPlaceholder = {
+export const WithPlaceholderPDF = {
   args: {
     size: SIZE.XS,
     crop: true,
@@ -44,9 +44,43 @@ export const WithPlaceholder = {
       extractionLevel: 0,
       inlineFullUrl: null,
       contentLength: 0,
-      contentType: 'image/jpeg',
+      contentType: 'application/pdf',
       isSupportedImage: true,
-      contentTypeIcon: markRaw(PhFilePdf)
+      contentTypeIcon: [markRaw(PhFilePdf), 'fill']
+    }
+  }
+}
+
+export const WithPlaceholderText = {
+  args: {
+    size: SIZE.XS,
+    crop: true,
+    fit: false,
+    hidePlaceholder: false,
+    document: {
+      extractionLevel: 0,
+      inlineFullUrl: null,
+      contentLength: 0,
+      contentType: 'text/plain',
+      isSupportedImage: true,
+      contentTypeIcon: markRaw(PhTextAlignLeft)
+    }
+  }
+}
+
+export const WithPlaceholderImage = {
+  args: {
+    size: SIZE.XS,
+    crop: true,
+    fit: false,
+    hidePlaceholder: false,
+    document: {
+      extractionLevel: 0,
+      inlineFullUrl: null,
+      contentLength: 0,
+      contentType: 'video/mp4',
+      isSupportedImage: true,
+      contentTypeIcon: [markRaw(PhImage), 'fill']
     }
   }
 }
