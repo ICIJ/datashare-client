cd12289db1937f374ca36cbf5f0a640758db9468
Pierre Romera <hello@pirhoo.com>
2025-04-23 19:12:33 +0000

    feat: add slots and target blank  icij/datashare#1753

--- src/components/Document/DocumentUser/DocumentUserComments/DocumentUserCommentsListEntry.vue
+++ src/components/Document/DocumentUser/DocumentUserComments/DocumentUserCommentsListEntry.vue
@@ -27,17 +27,25 @@ defineProps({
 <template>
   <section class="document-user-comments-list-entry py-2">
     <div class="d-flex align-items-center text-nowrap">
-      <display-user :value="username" class="fw-bold text-body-emphasis" />
-      <display-datetime class="ms-auto text-body-tertiary" :value="date" />
-      <button-icon
-        variant="link"
-        icon-left="arrowSquareOut"
-        hide-label
-        square
-        :href="href"
-        :label="$t('documentUserCommentsListEntry.goToComment')"
-      />
+      <slot name="header">
+        <display-user :value="username" class="fw-bold text-body-emphasis" />
+        <display-datetime class="ms-auto text-body-tertiary" :value="date" />
+        <button-icon
+          variant="link"
+          icon-left="arrowSquareOut"
+          hide-label
+          hide-tooltip
+          square
+          target="_blank"
+          :href="href"
+          :label="$t('documentUserCommentsListEntry.goToComment')"
+        />
+      </slot>
     </div>
-    <article class="ps-4">{{ text }}</article>
+    <article class="ps-4">
+      <slot name="text">
+        {{ text }}
+      </slot>
+    </article>
   </section>
 </template>
