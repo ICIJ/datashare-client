75c5e4ea543debcdab55c340cf3f29a8c2c1d94c
Pierre Romera <hello@pirhoo.com>
2025-04-28 08:01:28 +0000

    feat: remove px-5 on document modal in favor of default padding

--- src/components/Document/DocumentModal.vue
+++ src/components/Document/DocumentModal.vue
@@ -36,16 +36,7 @@ onRouteUpdateNotMatch(route.name, () => hide())
 </script>
 
 <template>
-  <app-modal
-    :id="modalId"
-    v-model="modelValue"
-    body-class="py-0 px-5"
-    no-footer
-    no-header
-    no-header-close
-    fullscreen
-    lazy
-  >
+  <app-modal :id="modalId" v-model="modelValue" body-class="py-0" no-footer no-header no-header-close fullscreen lazy>
     <document-floating class="my-3" no-reduce no-expand>
       <slot>
         <document-view :id="id" :routing="routing" :index="index" :q="q" />
