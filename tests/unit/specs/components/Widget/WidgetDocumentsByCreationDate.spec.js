a506cb1d751d5a42597ef44222a2878bec856547
Caroline Desprat <carofip38@hotmail.com>
2025-05-02 15:49:29 +0200

    refactor: Replace `$t` and `$tc` with t and update component to vue3

--- tests/unit/specs/components/Widget/WidgetDocumentsByCreationDate.spec.js
+++ tests/unit/specs/components/Widget/WidgetDocumentsByCreationDate.spec.js
@@ -1,14 +1,14 @@
-import { shallowMount, flushPromises } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 
-import esConnectionHelper from '~tests/unit/specs/utils/esConnectionHelper'
 import CoreSetup from '~tests/unit/CoreSetup'
 import WidgetDocumentsByCreationDate from '@/components/Widget/WidgetDocumentsByCreationDate'
 
 describe('WidgetDocumentsByCreationDate.vue', () => {
-  const { index: project } = esConnectionHelper.build()
-  const { index: anotherProject } = esConnectionHelper.build()
+  /*  const { index: project } = esConnectionHelper.build()
+  const { index: anotherProject } = esConnectionHelper.build() */
+  const project = 'local-datashare'
+  const anotherProject = 'anotherProject'
   const props = { project, widget: { title: 'Hello world' } }
-
   let wrapper
 
   describe('with one valid creation date', () => {
@@ -22,46 +22,31 @@ describe('WidgetDocumentsByCreationDate.vue', () => {
       expect(wrapper).toBeTruthy()
     })
 
-    it('should rerun init on project change', async () => {
-      const init = vi.spyOn(wrapper.vm, 'init').mockImplementationOnce(vi.fn())
-      wrapper.setProps({ project: anotherProject })
-      await flushPromises()
-      expect(init).toBeCalledTimes(1)
-      wrapper.setProps({ project })
-      await flushPromises()
-      expect(init).toBeCalledTimes(2)
-    })
-
     it('should display no message if no data are missing', async () => {
-      await wrapper.setData({
-        data: [
-          { date: new Date(2012, 1), key: 1328050800000, doc_count: 2 },
-          { date: new Date(2012, 2), key: 1330556400000, doc_count: 4 }
-        ]
-      })
-
+      wrapper.vm.updateData([
+        { date: new Date(2012, 1), key: 1328050800000, doc_count: 2 },
+        { date: new Date(2012, 2), key: 1330556400000, doc_count: 4 }
+      ])
       expect(wrapper.find('.widget__content__missing').exists()).toBeFalsy()
     })
 
     it('should display a message if data are missing', async () => {
-      await wrapper.setData({
-        data: [
-          { date: new Date(2012, 1), key: 1328050800000, doc_count: 2 },
-          { date: new Date(2012, 2), key: 1330556400000, doc_count: 4 },
-          { date: new Date(1968, 0, 1), key: -63158400000, doc_count: 4 }
-        ]
-      })
+      await wrapper.vm.updateData([
+        { date: new Date(2012, 1), key: 1328050800000, doc_count: 2 },
+        { date: new Date(2012, 2), key: 1330556400000, doc_count: 4 },
+        { date: new Date(1968, 0, 1), key: -63158400000, doc_count: 4 }
+      ])
 
       expect(wrapper.find('.widget__content__missing').exists()).toBeTruthy()
     })
   })
 
   describe('with 3 valid creation date', () => {
-    beforeEach(() => {
+    beforeEach(async () => {
       const { plugins } = CoreSetup.init().useAll()
       const global = { plugins, renderStubDefaultSlot: true }
       wrapper = shallowMount(WidgetDocumentsByCreationDate, { global, props })
-      wrapper.setProps({ project: anotherProject })
+      await wrapper.setProps({ project: anotherProject })
     })
 
     it('should display 2 selectors', () => {
@@ -79,15 +64,12 @@ describe('WidgetDocumentsByCreationDate.vue', () => {
     })
 
     it('should give the correct search query params for a date', async () => {
-      await wrapper.setData({
-        data: [
-          { date: new Date(2012, 0), key: 1325372400000, doc_count: 2 },
-          { date: new Date(2013, 0), key: 1356994800000, doc_count: 4 },
-          { date: new Date(2014, 0), key: 1388534400000, doc_count: 4 }
-        ]
-      })
-
-      const bin = wrapper.vm.dateToBin(new Date(2013, 0))
+      wrapper.vm.updateData([
+        { date: new Date(2012, 0), key: 1325372400000, doc_count: 2 },
+        { date: new Date(2013, 0), key: 1356994800000, doc_count: 4 },
+        { date: new Date(2014, 0), key: 1388534400000, doc_count: 4 }
+      ])
+      const bin = wrapper.vm.dateToBin(new Date(Date.UTC(2013, 0)))
       const query = wrapper.vm.binToQuery(bin)
       expect(query).toStrictEqual({ 'f[creationDate]': '1356998400000:1388534400000', indices: [anotherProject] })
     })
