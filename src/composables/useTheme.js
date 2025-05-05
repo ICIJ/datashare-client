0880c64d154c6d729455e5ecb315e3bdb37d65a7
Pierre Romera <hello@pirhoo.com>
2025-05-05 09:58:48 +0000

    refactor: use constants

--- src/composables/useTheme.js
+++ src/composables/useTheme.js
@@ -5,47 +5,58 @@ import themeLight from '@/assets/images/theme-light.png'
 import themeDark from '@/assets/images/theme-dark.png'
 
 let eventAdded = false
-
 export function useTheme() {
   const LOCAL_STORAGE_KEY = 'data-bs-theme'
-  const AUTOMATIC = 'automatic'
+
+  const THEMES = Object.freeze({
+    LIGHT: 'light',
+    DARK: 'dark',
+    AUTOMATIC: 'automatic'
+  })
+
   function getTheme() {
-    return localStorage.getItem(LOCAL_STORAGE_KEY) ?? AUTOMATIC
+    return localStorage.getItem(LOCAL_STORAGE_KEY) ?? THEMES.AUTOMATIC
   }
+
   function getAutomaticTheme() {
-    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? THEMES.DARK : THEMES.LIGHT
   }
-  if (!eventAdded) {
-    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
-      if (getTheme() === AUTOMATIC) {
-        setTheme(AUTOMATIC)
-      }
-    })
-    eventAdded = true
-  }
+
   function setTheme(theme) {
-    document.body?.setAttribute('data-bs-theme', theme === AUTOMATIC ? getAutomaticTheme() : theme)
+    document.body?.setAttribute('data-bs-theme', theme === THEMES.AUTOMATIC ? getAutomaticTheme() : theme)
     localStorage.setItem(LOCAL_STORAGE_KEY, theme)
   }
+
+  if (!eventAdded) {
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+      if (getTheme() === THEMES.AUTOMATIC) {
+        setTheme(THEMES.AUTOMATIC)
+      }
+    })
+
+    eventAdded = true
+  }
+
   const themes = reactive([
     {
       icon: 'paint-roller',
-      name: 'automatic',
+      name: THEMES.AUTOMATIC,
       label: `Automatic`,
       thumbnail: themeAuto
     },
     {
       icon: 'sun',
-      name: 'light',
+      name: THEMES.LIGHT,
       label: 'Light mode',
       thumbnail: themeLight
     },
     {
       icon: 'moon',
-      name: 'dark',
+      name: THEMES.DARK,
       label: 'Dark mode',
       thumbnail: themeDark
     }
   ])
+
   return { getTheme, setTheme, themes }
 }
