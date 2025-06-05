08eb123945e446e9b78ddda1a50db693dec07e7c
Pierre Romera <hello@pirhoo.com>
2025-06-05 17:05:25 +0000

    i18n: add missing translation key  icij/datashare#1793

--- src/components/FiltersPanel/FiltersPanelSearch.vue
+++ src/components/FiltersPanel/FiltersPanelSearch.vue
@@ -1,16 +1,25 @@
 <script setup>
+import { useI18n } from 'vue-i18n'
+
 import FormControlSearch from '@/components/Form/FormControl/FormControlSearch'
 
 const modelValue = defineModel({ type: String, default: '' })
+const { t } = useI18n()
 
 defineProps({
   placeholder: {
     type: String,
-    default: 'Search in filters'
+    default: null
   }
 })
 </script>
 
 <template>
-  <form-control-search v-model="modelValue" :placeholder="placeholder" shadow clear-text class="filters-panel-search" />
+  <form-control-search
+    v-model="modelValue"
+    :placeholder="placeholder ?? t('filtersPanelSearch.placeholder')"
+    shadow
+    clear-text
+    class="filters-panel-search"
+  />
 </template>
