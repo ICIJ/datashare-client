1feb2569d12c673ae742b69367e78513ba5db3ca
Pierre Romera <hello@pirhoo.com>
2025-06-12 16:46:49 +0000

    refactor: use specific button row actions icij/datashare#1719

--- src/components/Search/SearchSavedEntries/SearchSavedEntriesRowActions.vue
+++ src/components/Search/SearchSavedEntries/SearchSavedEntriesRowActions.vue
@@ -4,7 +4,8 @@ import { useI18n } from 'vue-i18n'
 import { useRemoveSavedSearchModal } from '@/composables/useRemoveSavedSearchModal'
 import { useSearchSavingModal } from '@/composables/useSearchSavingModal'
 import PageTableToggleDetailsButton from '@/components/PageTable/PageTableToggleDetailsButton'
-import ButtonRowAction from '@/components/Button/ButtonRowAction/ButtonRowAction'
+import ButtonRowActionEdit from '@/components/Button/ButtonRowAction/ButtonRowActionEdit'
+import ButtonRowActionDelete from '@/components/Button/ButtonRowAction/ButtonRowActionDelete'
 
 const { event } = defineProps({
   event: {
@@ -34,18 +35,8 @@ const confirmRemovalModal = async () => {
 
 <template>
   <div class="d-flex gap-2">
-    <button-row-action
-      hide-tooltip
-      :label="t('searchSavedEntriesRowActions.edit')"
-      icon="pencil"
-      @click="confirmEditModal"
-    />
-    <button-row-action
-      hide-tooltip
-      :label="t('searchSavedEntriesRowActions.delete')"
-      icon="trash"
-      @click="confirmRemovalModal"
-    />
+    <button-row-action-edit :label="t('searchSavedEntriesRowActions.edit')" @click="confirmEditModal" />
+    <button-row-action-delete :label="t('searchSavedEntriesRowActions.delete')" @click="confirmRemovalModal" />
     <page-table-toggle-details-button v-model="detailsShowing" hide-tooltip />
   </div>
 </template>
