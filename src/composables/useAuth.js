7df3c5cdeee59e4fc6d7c1e27f1d3b25dae23953
Pierre Romera <hello@pirhoo.com>
2025-06-13 08:56:12 +0000

    feat: add isAuthendicated computed property to useAuth

--- src/composables/useAuth.js
+++ src/composables/useAuth.js
@@ -1,16 +1,18 @@
-import { ref, onBeforeMount } from 'vue'
+import { computed, ref, onBeforeMount } from 'vue'
 
 import { useCore } from './useCore'
 
 export function useAuth() {
   const { core } = useCore()
+
   const username = ref(null)
+  const isAuthenticated = computed(() => !!username.value)
 
   onBeforeMount(async () => {
     username.value = await core.auth.getUsername()
   })
 
-  return { username }
+  return { username, isAuthenticated }
 }
 
 export default useAuth
