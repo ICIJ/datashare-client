6965403ee1bf51019e9a22add7004e7aaa39c434
Pierre Romera <hello@pirhoo.com>
2025-06-16 14:06:11 +0000

    spec: use mount to test nested component

--- tests/unit/specs/components/Document/DocumentGlobalSearchTermsEntry.spec.js
+++ tests/unit/specs/components/Document/DocumentGlobalSearchTermsEntry.spec.js
@@ -1,4 +1,4 @@
-import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 
 import CoreSetup from '~tests/unit/CoreSetup'
 import DocumentGlobalSearchTermsEntry from '@/components/Document/DocumentGlobalSearchTerms/DocumentGlobalSearchTermsEntry'
@@ -13,7 +13,7 @@ describe('DocumentGlobalSearchTermsEntry', () => {
 
   it('renders without crashing', () => {
     const props = { term: {} }
-    const wrapper = shallowMount(DocumentGlobalSearchTermsEntry, {
+    const wrapper = mount(DocumentGlobalSearchTermsEntry, {
       global: { plugins },
       props
     })
@@ -22,7 +22,7 @@ describe('DocumentGlobalSearchTermsEntry', () => {
 
   it('shows the label', () => {
     const props = { term: { label: 'test' } }
-    const wrapper = shallowMount(DocumentGlobalSearchTermsEntry, {
+    const wrapper = mount(DocumentGlobalSearchTermsEntry, {
       global: { plugins },
       props
     })
@@ -32,7 +32,7 @@ describe('DocumentGlobalSearchTermsEntry', () => {
   it('shows in tags text when there is tags', async () => {
     const props = { term: { label: 'test', count: 0, tags: 0 } }
 
-    const wrapper = shallowMount(DocumentGlobalSearchTermsEntry, {
+    const wrapper = mount(DocumentGlobalSearchTermsEntry, {
       global: { plugins },
       props
     })
@@ -44,24 +44,12 @@ describe('DocumentGlobalSearchTermsEntry', () => {
   it('shows in metadata text when there is metadata', async () => {
     const props = { term: { label: 'test', count: 0, metadata: 0 } }
 
-    const wrapper = shallowMount(DocumentGlobalSearchTermsEntry, {
+    const wrapper = mount(DocumentGlobalSearchTermsEntry, {
       global: { plugins },
       props
     })
     expect(wrapper.find('.document-global-search-terms-entry__metadata').exists()).toBe(false)
     await wrapper.setProps({ term: { label: 'test', count: 0, metadata: 1 } })
     expect(wrapper.find('.document-global-search-terms-entry__metadata').exists()).toBe(true)
-  })
-
-  it('shows counter pill in button icon when count is positive', async () => {
-    const props = { term: { label: 'test', count: 0 } }
-
-    const wrapper = shallowMount(DocumentGlobalSearchTermsEntry, {
-      global: { plugins },
-      props
-    })
-    expect(wrapper.find('button-icon').attributes('counter')).toBeUndefined()
-    await wrapper.setProps({ term: { label: 'test', count: 1, tags: 1 } })
-    expect(wrapper.find('button-icon').attributes('counter')).toBe('1')
   })
 })
