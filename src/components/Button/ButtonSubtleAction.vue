f5315f0888ba49b97f5de523a57192a8737ce1bb
Pierre Romera <hello@pirhoo.com>
2025-06-19 12:40:43 +0000

    feat: add button subtle action icij/datashare#1838

---
+++ src/components/Button/ButtonSubtleAction.vue
@@ -0,0 +1,7 @@
+<script setup>
+import ButtonIcon from '@/components/Button/ButtonIcon'
+</script>
+
+<template>
+  <button-icon variant="link" class="bg-action-subtle text-action-emphasis-subtle p-1" hide-label />
+</template>
---
+++ src/stories/components/Button/ButtonSubtleAction.stories.js
@@ -0,0 +1,14 @@
+import ButtonSubtleAction from '@/components/Button/ButtonSubtleAction'
+
+export default {
+  tags: ['autodocs'],
+  title: 'Components/Button/ButtonSubtleAction',
+  component: ButtonSubtleAction,
+  args: {
+    loading: false,
+    disabled: false,
+    iconLeft: 'arrows-out-simple'
+  }
+}
+
+export const Default = {}
