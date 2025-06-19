35f5bce14d049dc09bf1c1175b1213e94c309209
Pierre Romera <hello@pirhoo.com>
2025-06-19 12:50:56 +0000

    feat: add toggle path tree view button icij/datashare#1838

---
+++ src/components/Button/ButtonTogglePathTreeView.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+import ButtonSubtleAction from '@/components/Button/ButtonSubtleAction'
+
+const active = defineModel('active', { type: Boolean })
+
+const { t } = useI18n()
+
+const toggle = () => {
+  active.value = !active.value
+}
+
+const title = computed(() => {
+  return active.value ? t('buttonTogglePathTreeView.nested') : t('buttonTogglePathTreeView.list')
+})
+
+const icon = computed(() => {
+  return active.value ? PhTreeView : PhListBullets
+})
+</script>
+
+<template>
+  <button-subtle-action :icon-left="icon" :label="title" @click="toggle">
+    {{ title }}
+  </button-subtle-action>
+</template>
---
+++ src/stories/components/Button/ButtonTogglePathTreeView.stories.js
@@ -0,0 +1,12 @@
+import ButtonTogglePathTreeView from '@/components/Button/ButtonTogglePathTreeView'
+
+export default {
+  tags: ['autodocs'],
+  title: 'Components/Button/ButtonTogglePathTreeView',
+  component: ButtonTogglePathTreeView,
+  args: {
+    active: true
+  }
+}
+
+export const Default = {}
