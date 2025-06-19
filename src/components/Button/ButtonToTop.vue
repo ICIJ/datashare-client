dc8d8b2c1b8eef1509de20f4c735b04646a8c396
Pierre Romera <hello@pirhoo.com>
2025-06-19 12:34:11 +0000

    feat: use new design for the back to top button icij/datashare#1837

--- src/components/Button/ButtonToTop.vue
+++ src/components/Button/ButtonToTop.vue
@@ -7,5 +7,31 @@ const { t } = useI18n()
 </script>
 
 <template>
-  <button-icon class="button-to-top" :label="t('buttonToTop.label')" variant="action" :icon-right="PhArrowCircleUp" />
+  <button-icon
+    class="button-to-top rounded-pill"
+    size="lg"
+    :label="t('buttonToTop.label')"
+    variant="primary"
+    :icon-right="PhCaretUp"
+    icon-right-weight="bold"
+    hide-label
+    square
+  />
 </template>
+
+<style lang="scss" scoped>
+.button-to-top {
+  --bs-btn-opacity: 0.5;
+  --bs-btn-color: var(--bs-white);
+  --bs-btn-hover-color: var(--bs-btn-color);
+  --bs-btn-active-color: var(--bs-btn-color);
+  --bs-btn-hover-bg: var(--bs-btn-bg);
+  --bs-btn-box-shadow: var(--bs-box-shadow-lg);
+
+  opacity: var(--bs-btn-opacity);
+
+  &:hover {
+    --bs-btn-opacity: 1;
+  }
+}
+</style>
