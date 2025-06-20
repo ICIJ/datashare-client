a5bd11d2830d5abd80ff3208c65393fbc5e1e15b
Pierre Romera <hello@pirhoo.com>
2025-06-20 12:48:29 +0000

    doc: add missing jsdoc icij/datashare#1841

--- src/router/guards/checkSearchSort.js
+++ src/router/guards/checkSearchSort.js
@@ -6,6 +6,9 @@ import settings from '@/utils/settings'
  * This navigation guard replaces a unique sort query parameter with a pair of sort and order query parameters
  * based on the search properties. This way we can ensure retro-compatibility with
  * the former sort query parameter which used to contain both the sort field and the order.
+ *
+ * @param {Object} to - The target route object.
+ * @returns {Object|null} - Returns a new route object with the updated query parameters.
  */
 export const checkSearchSort = (to) => {
   const { name, params } = to
