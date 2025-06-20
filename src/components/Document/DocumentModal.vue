2d93e24787f28d21335547ecc86edfc4f6506a45
Pierre Romera <hello@pirhoo.com>
2025-06-20 10:44:48 +0000

    feat: enhanced modal display without separator line

--- src/components/Document/DocumentModal.vue
+++ src/components/Document/DocumentModal.vue
@@ -36,7 +36,17 @@ onRouteUpdateNotMatch(route.name, () => hide())
 </script>
 
 <template>
-  <app-modal :id="modalId" v-model="modelValue" body-class="py-0" no-footer no-header no-header-close fullscreen lazy>
+  <app-modal
+    :id="modalId"
+    v-model="modelValue"
+    class="document-modal"
+    body-class="py-0"
+    no-footer
+    no-header
+    no-header-close
+    fullscreen
+    lazy
+  >
     <document-floating class="my-3" no-reduce no-expand>
       <slot>
         <document-view :id="id" :routing="routing" :index="index" :q="q" />
@@ -47,3 +57,21 @@ onRouteUpdateNotMatch(route.name, () => hide())
     </document-floating>
   </app-modal>
 </template>
+
+<style lang="scss">
+.document-modal {
+  &:not(:has(.document-floating--has-floating)) .document-floating__end {
+    padding-left: 0;
+  }
+
+  .document-floating--reached-full-width {
+    .document-floating__start__floating {
+      margin-right: 0;
+    }
+
+    .document-floating__separator-line {
+      display: none;
+    }
+  }
+}
+</style>
