1310f7832e8398c4262ba1b9b9463341f08fcae4
Pierre Romera <hello@pirhoo.com>
2025-06-24 15:47:57 +0000

    doc: add missing level

--- src/stories/components/PathTree/PathTreeView/PathTreeView.stories.js
+++ src/stories/components/PathTree/PathTreeView/PathTreeView.stories.js
@@ -12,6 +12,7 @@ export default {
   args: {
     query: '',
     compact: false,
+    nested: true,
     selectMode: false,
     noLabel: false,
     noSearch: false
@@ -27,29 +28,29 @@ export default {
     setup: () => ({ args }),
     template: `
       <path-tree-view v-bind="args" @update:query="args.query = $event">
-        <path-tree-view-entry name="Flowera" :documents="9104" :directories="3" :size="2110000000">
+        <path-tree-view-entry :nested="args.nested" :level="0" name="Flowera" :documents="9104" :directories="3" :size="2110000000">
           <template #name>
             <div class="text-truncate">
               <phosphor-icon name="circles-three-plus" class="me-1" />
               <project-label project="Flowera" hide-thumbnail />
             </div>
           </template>
-          <path-tree-view-entry name="Contract" :documents="2109" :directories="4" :size="1010000000">
-            <path-tree-view-entry name="2021" :documents="1567" :directories="3" :size="500000000">
-              <path-tree-view-entry name="Note" collapse :documents="678" :directories="4" :size="76000000" />
-              <path-tree-view-entry name="Signature" collapse  :documents="142" :directories="4" :size="378000000" />
-              <path-tree-view-entry name="Others" :documents="747" :directories="4" :size="54000000">
-                <path-tree-view-entry name="Images" collapse :documents="36" :directories="7" :size="8000000" />
-                <path-tree-view-entry name="Attachments" collapse :documents="356" :directories="1" :size="5000000" />
-                <path-tree-view-entry name="ID Cards" collapse :documents="145" :directories="6" :size="4000000" />
-                <path-tree-view-entry name="Permits" collapse :documents="210" :directories="0" :size="7000000" />
-                <path-tree-view-entry name="Balance Sheets" collapse :documents="34" :directories="0" :size="400000" />
-                <path-tree-view-entry-more :total="14" :per-page="5" class="mx-3 my-2" />
+          <path-tree-view-entry :nested="args.nested" :level="1" name="Contract" :documents="2109" :directories="4" :size="1010000000">
+            <path-tree-view-entry :nested="args.nested" :level="2" name="2021" :documents="1567" :directories="3" :size="500000000">
+              <path-tree-view-entry :nested="args.nested" :level="3" name="Note" collapse :documents="678" :directories="4" :size="76000000" />
+              <path-tree-view-entry :nested="args.nested" :level="4" name="Signature" collapse  :documents="142" :directories="4" :size="378000000" />
+              <path-tree-view-entry :nested="args.nested" :level="4" name="Others" :documents="747" :directories="4" :size="54000000">
+                <path-tree-view-entry :nested="args.nested" :level="5" name="Images" collapse :documents="36" :directories="7" :size="8000000" />
+                <path-tree-view-entry :nested="args.nested" :level="5" name="Attachments" collapse :documents="356" :directories="1" :size="5000000" />
+                <path-tree-view-entry :nested="args.nested" :level="5" name="ID Cards" collapse :documents="145" :directories="6" :size="4000000" />
+                <path-tree-view-entry :nested="args.nested" :level="5" name="Permits" collapse :documents="210" :directories="0" :size="7000000" />
+                <path-tree-view-entry :nested="args.nested" :level="5" name="Balance Sheets" collapse :documents="34" :directories="0" :size="400000" />
+                <path-tree-view-entry-more :level="5" :total="14" :per-page="5" class="mx-3 my-2" />
               </path-tree-view-entry>
             </path-tree-view-entry>
-            <path-tree-view-entry name="2020" collapse />
-            <path-tree-view-entry name="2019" collapse />
-            <path-tree-view-entry name="2018" collapse />
+            <path-tree-view-entry :level="2" :nested="args.nested" name="2020" collapse />
+            <path-tree-view-entry :level="2" :nested="args.nested" name="2019" collapse />
+            <path-tree-view-entry :level="2" :nested="args.nested" name="2018" collapse />
           </path-tree-view-entry>
         </path-tree-view-entry>
       </path-tree-view>
