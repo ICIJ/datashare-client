d50e2697e36b2e9330f54862f408e2202ef46fed
Pierre Romera <hello@pirhoo.com>
2025-06-26 09:08:38 +0000

    build: add missing env

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { resolve } from 'path'
 import * as childProcess from 'child_process'
 import Components from 'unplugin-vue-components/vite'
@@ -13,7 +13,8 @@ import { BootstrapVueNextResolver } from 'unplugin-vue-components/resolvers'
 export default ({ mode }) => {
   const VITE_GIT_HASH = childProcess.execSync('git rev-parse HEAD').toString()
   const VITE_CWD  = process.cwd()
-  Object.assign(process.env, { ...process.env, VITE_GIT_HASH, VITE_CWD })
+  const ENV = loadEnv(mode, process.cwd(), '')
+  Object.assign(process.env, { ...ENV, VITE_GIT_HASH, VITE_CWD })
   
   return defineConfig({
     base: process.env.VITE_BASE,
