13a50c2b49ae26cee34722ceea546082c887ab41
Pierre Romera <hello@pirhoo.com>
2025-06-27 09:07:17 +0000

    feat: remove compact mode

--- src/components/Button/ButtonToggleFilters.vue
+++ src/components/Button/ButtonToggleFilters.vue
@@ -3,20 +3,10 @@ import { computed } from 'vue'
 import { useI18n } from 'vue-i18n'
 
 import ButtonIcon from '@/components/Button/ButtonIcon'
-import { useBreakpoints } from '@/composables/useBreakpoints'
 import { VARIANT } from '@/enums/variants.js'
 
 const active = defineModel('active', { type: Boolean })
 
-const props = defineProps({
-  loading: {
-    type: Boolean
-  },
-  compactBreakpoint: {
-    type: String,
-    default: 'sm'
-  }
-})
 const { t } = useI18n()
 
 const toggle = () => {
@@ -26,12 +16,6 @@ const toggle = () => {
 const variant = computed(() => {
   return active.value ? VARIANT.ACTION : VARIANT.OUTLINE_TERTIARY
 })
-
-const { breakpointDown } = useBreakpoints()
-
-const compact = computed(() => {
-  return breakpointDown.value[props.compactBreakpoint]
-})
 </script>
 
 <template>
@@ -39,9 +23,6 @@ const compact = computed(() => {
     class="button-toggle-filters flex-shrink-0"
     :label="t('buttonToggleFilters.label')"
     :icon-left="PhFunnel"
-    :square="compact"
-    :hide-label="compact"
-    :loading="loading"
     :variant="variant"
     @click="toggle"
   />
