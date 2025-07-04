fe81a24e479387711e6618b4b9db237287f33ed4
Pierre Romera <hello@pirhoo.com>
2025-07-04 09:21:48 +0000

    refactor: migrate to composition API

--- src/components/Document/DocumentViewer/DocumentViewerImage.vue
+++ src/components/Document/DocumentViewer/DocumentViewerImage.vue
@@ -1,28 +1,14 @@
-<template>
-  <div class="image-viewer m-auto p-3">
-    <img :src="document.inlineFullUrl" class="d-inline-block" />
-  </div>
-</template>
-
-<script>
-/**
- * Display a preview image of the document.
- */
-export default {
-  name: 'DocumentViewerImage',
-  props: {
-    /**
-     * The selected document
-     */
-    document: {
-      type: Object
-    }
+<script setup>
+defineProps({
+  document: {
+    type: Object,
+    required: true
   }
-}
+})
 </script>
 
-<style>
-.image-viewer img {
-  max-width: 100%;
-}
-</style>
+<template>
+  <div class="image-viewer m-auto p-3">
+    <img :src="document.inlineFullUrl" class="d-inline-block img-fluid" />
+  </div>
+</template>
