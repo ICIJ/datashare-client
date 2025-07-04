49a8ea2d95599384588dd2da4ad134e3d443fe6b
Pierre Romera <hello@pirhoo.com>
2025-07-04 10:18:27 +0000

    feat: implement imaghe rotation controls

--- src/components/Document/DocumentViewer/DocumentViewerImage.vue
+++ src/components/Document/DocumentViewer/DocumentViewerImage.vue
@@ -1,14 +1,88 @@
 <script setup>
-defineProps({
+import { onBeforeMount, ref } from 'vue'
+
+import { useImage } from '@/composables/useImage'
+import { useWait } from '@/composables/useWait'
+import AppSpinner from '@/components/AppSpinner/AppSpinner'
+import AppWait from '@/components/AppWait/AppWait'
+import ButtonRowAction from '@/components/Button/ButtonRowAction/ButtonRowAction'
+
+const props = defineProps({
   document: {
     type: Object,
     required: true
   }
 })
+
+const { rotateBase64Image } = useImage()
+const { waitFor, loaderId } = useWait()
+const imageBase64 = ref(null)
+const imageRotation = ref(0)
+
+async function fetch() {
+  imageBase64.value = await rotateBase64Image(props.document.inlineFullUrl, imageRotation)
+}
+
+async function rotateClockwise() {
+  imageRotation.value = (imageRotation.value + 90) % 360
+  return fetch()
+}
+
+async function rotateCounterClockwise() {
+  imageRotation.value = (imageRotation.value - 90 + 360) % 360
+  return fetch()
+}
+
+onBeforeMount(waitFor(fetch))
 </script>
 
 <template>
-  <div class="image-viewer m-auto p-3">
-    <img :src="document.inlineFullUrl" class="d-inline-block img-fluid" />
-  </div>
+  <app-wait class="image-viewer mx-auto p-3" :for="loaderId">
+    <template #waiting>
+      <app-spinner />
+    </template>
+    <div class="image-viewer__wrapper">
+      <div class="image-viewer__wrapper__controls justify-content-center d-flex gap-1 p-1">
+        <button-row-action
+          class="image-viewer__wrapper__controls__button"
+          label="Rotate 90° to the left"
+          :icon-left="PhArrowCounterClockwise"
+          @click="rotateCounterClockwise()"
+        />
+        <button-row-action
+          class="image-viewer__wrapper__controls__button"
+          label="Rotate 90° to the right"
+          :icon-left="PhArrowClockwise"
+          @click="rotateClockwise()"
+        />
+      </div>
+      <img :src="imageBase64" class="image-viewer__wrapper__image img-fluid" />
+    </div>
+  </app-wait>
 </template>
+
+<style scoped lang="scss">
+.image-viewer {
+  &__wrapper {
+    position: relative;
+
+    &__controls {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      z-index: 10;
+      opacity: 0.25;
+
+      &:hover {
+        opacity: 1;
+      }
+
+      &__button {
+        --bs-btn-bg: var(--bs-btn-hover-bg);
+        --bs-btn-active-shadow: none;
+      }
+    }
+  }
+}
+</style>
