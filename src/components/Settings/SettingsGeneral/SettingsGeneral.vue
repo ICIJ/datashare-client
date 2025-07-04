6a120ebe3a978357caa2a763c4e12859851f5571
Caroline Desprat <cdesprat@icij.org>
2025-07-04 15:50:47 +0000

    style: lint

--- src/components/Settings/SettingsGeneral/SettingsGeneral.vue
+++ src/components/Settings/SettingsGeneral/SettingsGeneral.vue
@@ -1,27 +1,3 @@
-<template>
-  <b-form class="d-flex flex-column gap-1" @submit.prevent="save">
-    <b-form-group
-      v-for="(_, name) in settings"
-      :key="name"
-      :label-cols="12"
-      :label-cols-sm="4"
-      :label-cols-lg="3"
-      :label-for="`input-settings-${name}`"
-    >
-      <template #label>
-        <settings-general-label :name="name" :field-changed="fieldChanged(name)" @restore="restore" />
-      </template>
-      <b-form-input :id="`input-settings-${name}`" v-model="replica[name]" />
-    </b-form-group>
-    <div class="d-flex gap-3 justify-content-end">
-      <button-reset type="reset" :disabled="hasNotChanged" :variant="VARIANT.OUTLINE_TERTIARY" @click="reset" />
-      <b-button type="submit" variant="action" :disabled="hasNotChanged">
-        {{ submitLabel }}
-      </b-button>
-    </div>
-  </b-form>
-</template>
-
 <script setup>
 import { computed, toRaw, reactive } from 'vue'
 import { useI18n } from 'vue-i18n'
@@ -29,7 +5,6 @@ import { isEqual } from 'lodash'
 
 import SettingsGeneralLabel from '@/components/Settings/SettingsGeneral/SettingsGeneralLabel'
 import ButtonReset from '@/components/Button/ButtonReset'
-import { VARIANT } from '@/enums/variants'
 
 const props = defineProps({
   settings: {
@@ -42,7 +17,6 @@ const replica = reactive({ ...props.settings })
 const { t } = useI18n()
 
 const submitLabel = computed(() => t('global.submit'))
-const resetLabel = computed(() => t('global.reset'))
 const hasNotChanged = computed(() => isEqual(props.settings, replica))
 
 const emit = defineEmits(['save'])
@@ -63,3 +37,27 @@ function save() {
   emit('save', toRaw(replica))
 }
 </script>
+
+<template>
+  <b-form class="d-flex flex-column gap-1" @submit.prevent="save">
+    <b-form-group
+      v-for="(_, name) in settings"
+      :key="name"
+      :label-cols="12"
+      :label-cols-sm="4"
+      :label-cols-lg="3"
+      :label-for="`input-settings-${name}`"
+    >
+      <template #label>
+        <settings-general-label :name="name" :field-changed="fieldChanged(name)" @restore="restore" />
+      </template>
+      <b-form-input :id="`input-settings-${name}`" v-model="replica[name]" />
+    </b-form-group>
+    <div class="d-flex gap-3 justify-content-end">
+      <button-reset type="reset" :disabled="hasNotChanged" @click="reset" />
+      <b-button type="submit" variant="action" :disabled="hasNotChanged">
+        {{ submitLabel }}
+      </b-button>
+    </div>
+  </b-form>
+</template>
