0a0ded4fdf51c323d382b37990c11ba3524d2a83
Caroline Desprat <cdesprat@icij.org>
2025-07-04 15:30:11 +0000

    feat: add reset settings button to page settings ICIJ/datashare#1785

---
+++ src/components/Button/ButtonReset.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { useI18n } from 'vue-i18n'
+
+import ButtonIcon from '@/components/Button/ButtonIcon'
+import { VARIANT } from '@/enums/variants'
+defineProps({
+  labelKey: { type: String, default: 'global.reset' },
+  variant: { type: String, default: VARIANT.OUTLINE_SECONDARY }
+})
+const { t } = useI18n()
+</script>
+
+<template>
+  <button-icon icon-left="arrow-counter-clockwise" :variant="variant">{{ t(labelKey) }}</button-icon>
+</template>
