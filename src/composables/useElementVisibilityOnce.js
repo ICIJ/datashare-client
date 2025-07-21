307bffc604c35e3bf24d1c1b991c96bb675d0c34
Pierre Romera <hello@pirhoo.com>
2025-07-21 13:52:57 +0000

    feat: add composable to track visibility once icij/datashare#1857

---
+++ src/composables/useElementVisibilityOnce.js
@@ -0,0 +1,11 @@
+import { ref } from 'vue'
+import { useElementVisibility, whenever } from '@vueuse/core'
+
+export function useElementVisibilityOnce(element) {
+  const isVisible = useElementVisibility(element)
+  const visible = ref(isVisible.value)
+
+  whenever(isVisible, (value) => (visible.value = value), { once: true })
+
+  return visible
+}
