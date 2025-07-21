6152498d546814f0901ab2402b9d1e0b6f12c5c2
Pierre Romera <hello@pirhoo.com>
2025-07-21 16:46:35 +0000

    feat: allow to override hideLabel

--- src/components/Button/ButtonSubtleAction.vue
+++ src/components/Button/ButtonSubtleAction.vue
@@ -1,7 +1,14 @@
 <script setup>
 import { ButtonIcon } from '@icij/murmur-next'
+
+defineProps({
+  hideLabel: {
+    type: Boolean,
+    default: true
+  }
+})
 </script>
 
 <template>
-  <button-icon variant="link" class="bg-action-subtle text-action-emphasis-subtle p-1" hide-label />
+  <button-icon variant="link" class="bg-action-subtle text-action-emphasis-subtle p-1" :hide-label="hideLabel" />
 </template>
