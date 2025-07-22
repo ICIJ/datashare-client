d0e0d1e46662f96a82753cc9caca19a9603a3796
Caroline Desprat <cdesprat@icij.org>
2025-07-22 15:59:12 +0000

    fix: pass empty array to `useRouter` in DocumentViewTabs.spec.js

--- tests/unit/specs/views/Document/DocumentView/DocumentViewTabs/DocumentViewTabs.spec.js
+++ tests/unit/specs/views/Document/DocumentView/DocumentViewTabs/DocumentViewTabs.spec.js
@@ -8,7 +8,7 @@ describe('DocumentViewTabs.vue', () => {
   let core
 
   beforeEach(() => {
-    core = CoreSetup.init().useAll().useRouter()
+    core = CoreSetup.init().useAll().useRouter([])
   })
 
   it('should display the component', async () => {
