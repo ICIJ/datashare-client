94d9379ba3d96c0336d591ebb67b6901b8e7526a
Pierre Romera <hello@pirhoo.com>
2025-07-22 13:00:33 +0000

    fix: ensure page change also redirect with route's query icij/datashare#1866

--- src/views/Search/Search.vue
+++ src/views/Search/Search.vue
@@ -83,9 +83,15 @@ const total = computed(() => parseInt(searchStore.response.total))
 const perPage = computed(() => parseInt(appStore.getSettings('search', 'perPage')))
 const page = useUrlPageFromWithStore({
   perPage: toValue(perPage),
-  // The "from" query parameter is updated to reflect the current state
-  // which while force the route to redirect to "search".
-  to: 'search',
+  // The "to" query parameter is used to ensure that when the page is changed, the route query is updated
+  // to be on the search route. Here we use the store's `toRouteQuery` to ensure that the route query is updated
+  // with the current search parameters. This allow us to navigate from child routes where the query parameters 
+  // are not the same as the search route.
+  to: () => {
+    const name = 'search'
+    const { toRouteQuery: query } = searchStore
+    return { name, query }
+  },
   // The value of the "from" query parameter is directly taken from the store
   get: () => searchStore.from,
   // The value of the "from" query parameter is mutated with the store
