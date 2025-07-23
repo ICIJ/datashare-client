611f7b6d773fdbf2358bb147dc6195207e33a413
Pierre Romera <hello@pirhoo.com>
2025-07-23 13:30:25 +0000

    fix: export  ButtonIcon

--- src/components/Button/ButtonIcon.vue
+++ src/components/Button/ButtonIcon.vue
@@ -1,8 +1,8 @@
-<script setup>
+<script>
 // Ensure retro-compatibility with plugins
 import { defineComponent } from 'vue'
 import { ButtonIcon } from '@icij/murmur-next'
 
 // Re-export the ButtonIcon component. If not using script setup, storybook doc gen will fail during build.
-defineComponent(ButtonIcon)
+export default defineComponent(ButtonIcon)
 </script>
