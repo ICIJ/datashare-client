80fdb1c70117cf832caaa9c9e65d64634c8df92f
Pierre Romera <hello@pirhoo.com>
2025-07-29 11:09:54 +0000

    chore: upgrade @icij/eslint-config to v3

--- eslint.config.js
+++ eslint.config.js
@@ -1,46 +1,64 @@
-
-import vitest from "eslint-plugin-vitest"
-import vitestGlobals from "eslint-plugin-vitest-globals"
+import globals from "globals"
+import icijeslint from "@icij/eslint-config"
 import storybook from 'eslint-plugin-storybook'
 
-import compat from "./bin/eslint/compat.js"
 import { globals as iconsGlobals } from "./bin/icons.js"
 
 export default [
   {
-    ignores: [
-      "dist",
-      "**/*.config.js",
-      "**/config.yml",
-      "public",
-      "node_modules",
-      "storybook-static",
-      ".storybook"
-    ]
+    ignores: [ "**/*.config.js", "public", ".storybook"]
   },
-  ...compat.extends("@icij/eslint-config-icij"),
+  
+  // ICIJ ESLint shared config (includes Vue, TypeScript, Stylistic and Vitest)
+  ...icijeslint.configs.all,
+
+  // Storybook config
   ...storybook.configs['flat/recommended'],
+
+  // Node scripts for common tasks
   {
-    files: ["tests/**"],
+    files: ["bin/**/*.{cjs,mjs,js}"],
     languageOptions: {
-      globals: vitestGlobals.environments.env.globals,
-    },
-     ...vitest.configs.recommended,
+      globals: {
+        ...globals.node,
+      }
+    }
   },
+
+  // Vitest are written for the browser and must include browser globals
+  {
+    files: [
+      "src/**/*.{js,vue}",
+      "tests/**/*.spec.js",
+    ],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      }
+    }
+  },
+
+  // Specific rules for the project
   {
     languageOptions: {
       globals: {
         ...iconsGlobals,
+        process: true
       }
     },
     rules: {
-      "no-unused-vars": ["error", { argsIgnorePattern: "^(Ph[A-Z])|(_)" }],
+      // Legacy rules, might not be needed anymore
       "vue/no-v-model-argument": "off",
       "vue/no-v-for-template-key": "off",
       "vue/no-custom-modifiers-on-v-model": "off",
       "vue/no-multiple-template-root": "off",
       "vue/valid-v-slot": "off",
-      "import/extensions": "off",
+      // We use `require` with vite to make all components available for plugins.
+      // We might want to refactor this in the future with eslint comments.
+      "@typescript-eslint/no-require-imports": "off",
+      // We use `v-html` in some components with vue i18n. 
+      // We might want to refactor this in the future with eslint comments.
+      "vue/no-v-html": "off"
     }
   }
 ]
