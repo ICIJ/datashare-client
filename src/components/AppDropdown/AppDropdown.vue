7ec4b9b109d4792a9e44fe8976b659e62099df94
Pierre Romera <hello@pirhoo.com>
2025-09-05 17:11:52 +0200

    feat: add generic app-dropdown

---
+++ src/components/AppDropdown/AppDropdown.vue
@@ -0,0 +1,70 @@
+<script setup>
+import { VARIANT, variantValidator } from '@/enums/variants'
+
+defineProps({
+  variant: {
+    type: String,
+    validator: variantValidator,
+    default: VARIANT.OUTLINE_SECONDARY
+  },
+  menuClass: {
+    type: String,
+    default: 'app-dropdown__menu'
+  },
+  toggleClass: {
+    type: String,
+    default: 'app-dropdown__toggle'
+  },
+  noCaret: {
+    type: Boolean,
+    default: true
+  },
+  autoClose: {
+    type: [Boolean, String],
+    default: 'outside'
+  },
+  buttonIcon: {
+    type: String,
+    default: PhDotsThreeOutlineVertical
+  }
+})
+</script>
+
+<template>
+  <b-dropdown
+    class="app-dropdown"
+    :menu-class="menuClass"
+    :toggle-class="toggleClass"
+    :variant="variant"
+    :no-caret="noCaret"
+    :auto-close="autoClose"
+  >
+    <template #button-content>
+      <slot name="button-content">
+        <phosphor-icon
+          :name="buttonIcon"
+          fill
+        />
+      </slot>
+    </template>
+    <slot />
+  </b-dropdown>
+</template>
+
+<style lang="scss">
+.app-dropdown {
+  &__toggle {
+    --bs-btn-border-width: 0;
+  }
+
+  &__menu {
+    --bs-dropdown-zindex: #{$zindex-sticky};
+    --bs-dropdown-bg: var(--bs-action-bg-subtle);
+    --bs-dropdown-border-color: var(--bs-action);
+    --bs-dropdown-color: var(--bs-action-text-emphasis);
+    --bs-dropdown-link-color: var(--bs-link-color);
+    --bs-dropdown-link-hover-bg: var(--bs-action);
+    --bs-dropdown-link-hover-color: var(--bs-white);
+  }
+}
+</style>
