3b77f0fd329febd1e4138b881b5fa5acd0e3153c
Pierre Romera <hello@pirhoo.com>
2025-09-08 10:35:30 +0000

    feat: add new form control ocr

---
+++ src/components/Form/FormControl/FormControlOcr.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { computed, ref } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+
+const modelValue = defineModel({ type: Boolean, default: false })
+
+const ocrOptions = computed(() => [
+  { text: t('task.documents.form.extractOcr.options.yes'), value: true },
+  { text: t('task.documents.form.extractOcr.options.no'), value: false }
+])
+
+const disabled = ref(false)
+</script>
+
+<template>
+  <b-form-radio-group
+    v-model="modelValue"
+    name="extract-ocr"
+    :disabled="disabled"
+    :options="ocrOptions"
+    stacked
+  />
+</template>
