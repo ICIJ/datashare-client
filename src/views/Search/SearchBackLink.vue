4021cb34ca77a2a39ac2bf50938a6c6ff3f9dab5
Caroline Desprat <cdesprat@icij.org>
2025-09-11 15:56:22 +0000

    feat: add SearchBackLink component in Search view
    - Introduced a new `SearchBackLink` component for consistent back navigation.
    - Updated the Search view to use the new component and removed inline router-link.
    - Added i18n key `searchBackLink` for back button text.

---
+++ src/views/Search/SearchBackLink.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+import { useSearchNav } from '@/composables/useSearchNav'
+import { useI18n } from 'vue-i18n'
+const { searchRoute } = useSearchNav()
+const { t } = useI18n()
+</script>
+
+<template>
+  <router-link :to="searchRoute">
+    <span class="d-inline-flex gap-1"><phosphor-icon :name="PhCaretLeft" />{{ t('searchBackLink') }}</span>
+  </router-link>
+</template>
+
+<style scoped lang="scss">
+
+</style>
