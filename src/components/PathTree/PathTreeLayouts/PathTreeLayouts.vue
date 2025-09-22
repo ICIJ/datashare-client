0ac132afae021fe0f13c149cbbc596925289cfbd
Pierre Romera <hello@pirhoo.com>
2025-09-22 12:55:36 +0000

    feat: add component to select path tree layout

---
+++ src/components/PathTree/PathTreeLayouts/PathTreeLayouts.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { LAYOUTS, layoutValidator } from '@/enums/pathTree'
+import { PhosphorIcon } from '@icij/murmur-next'
+
+const modelValue = defineModel('modelValue', {
+  type: String,
+  default: LAYOUTS.TREE,
+  validator: layoutValidator
+})
+</script>
+
+<template>
+  <div>
+    <b-form-radio-group
+      v-model="modelValue"
+      name="path-tree-layouts"
+    >
+      <b-form-radio :value="LAYOUTS.TREE">
+        <phosphor-icon name="tree-view" /> {{ $t('pathTreeLayouts.tree') }}
+      </b-form-radio>
+      <b-form-radio :value="LAYOUTS.LIST">
+        <phosphor-icon name="list-bullets" /> {{ $t('pathTreeLayouts.list') }}
+      </b-form-radio>
+      <b-form-radio :value="LAYOUTS.GRID">
+        <phosphor-icon name="folder" /> {{ $t('pathTreeLayouts.grid') }}
+      </b-form-radio>
+    </b-form-radio-group>
+  </div>
+</template>
