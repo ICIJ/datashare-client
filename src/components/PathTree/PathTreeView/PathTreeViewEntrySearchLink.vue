6a74b769a66117e8eeb7592bd63f58f90965dd1f
Pierre Romera <hello@pirhoo.com>
2025-09-22 12:54:07 +0000

    feat: add component to display search link

---
+++ src/components/PathTree/PathTreeView/PathTreeViewEntrySearchLink.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { ButtonIcon } from '@icij/murmur-next'
+
+const props = defineProps({
+  projects: {
+    type: Array,
+    default: () => []
+  },
+  path: {
+    type: String
+  }
+})
+
+const { t } = useI18n()
+
+const to = computed(() => {
+  const indices = props.projects.join(',')
+  const query = { 'f[path]': props.path, indices }
+  return { name: 'search', query }
+})
+</script>
+
+<template>
+  <button-icon
+    :icon-left="PhMagnifyingGlass"
+    :to="to"
+    variant="link"
+    hide-label
+    :label="t('pathTreeViewEntrySearchLink.label')"
+    class="path-tree-view-entry-search-link above-stretched-link"
+  />
+</template>
