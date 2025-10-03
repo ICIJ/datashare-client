78e3d3acc39b50c56d66435dcb42dff7adccef09
Pierre Romera <hello@pirhoo.com>
2025-10-03 12:36:22 +0200

    feat: add back to search button

---
+++ src/components/Button/ButtonBackToSearch.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ButtonIcon } from '@icij/murmur-next'
+import { useI18n } from 'vue-i18n'
+
+import { useSearchNav } from '@/composables/useSearchNav'
+const { searchRoute } = useSearchNav()
+const { t } = useI18n()
+</script>
+
+<template>
+  <button-icon
+    :icon-left="PhCaretLeft"
+    :label="t('buttonBackToSearch.label')"
+    :to="searchRoute"
+    variant="link"
+  />
+</template>
