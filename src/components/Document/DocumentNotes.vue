f6486266a523ea402bb4baf845615ff072b67dbd
Pierre Romera <hello@pirhoo.com>
2025-10-10 15:51:53 +0200

    feat: don't show empty notes

--- src/components/Document/DocumentNotes.vue
+++ src/components/Document/DocumentNotes.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="document-notes d-flex flex-column gap-3">
+  <div
+    v-if="nonEmptyNotes.length"
+    class="document-notes d-flex flex-column gap-3"
+  >
     <b-alert
-      v-for="({ note, variant }, i) in notes"
+      v-for="({ note, variant }, i) in nonEmptyNotes"
       :key="i"
       class="m-0"
       no-fade
@@ -14,12 +17,14 @@
 </template>
 
 <script setup>
-import { ref, onBeforeMount } from 'vue'
+import { isEmpty } from 'lodash'
+import { computed, ref, onBeforeMount } from 'vue'
 
 import { useDocumentNotesStore } from '@/store/modules'
 
 const props = defineProps({ document: Object })
 const notes = ref([])
+const nonEmptyNotes = computed(() => notes.value.filter(({ note }) => !isEmpty(note)))
 const documentNotesStore = useDocumentNotesStore()
 
 onBeforeMount(async () => {
