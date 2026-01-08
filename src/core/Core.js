c4563bc8e1852ce11c45ea45da5b8dc827377a37
Pierre Romera <hello@pirhoo.com>
2026-01-08 21:08:49 +0000

    fix: provide legacy injection keys for murmur-next compatibility

--- src/core/Core.js
+++ src/core/Core.js
@@ -6,8 +6,8 @@ import Murmur from '@icij/murmur-next'
 import VCalendar from 'v-calendar'
 import VueScrollTo from 'vue-scrollto'
 import Vue3Toastify, { toast } from 'vue3-toastify'
-import { createBootstrap } from 'bootstrap-vue-next'
-import { createApp, defineComponent, toValue, reactive, watchEffect, h } from 'vue'
+import { createBootstrap, BApp } from 'bootstrap-vue-next'
+import { createApp, defineComponent, toValue, reactive, ref, watchEffect, h } from 'vue'
 import { createI18n } from 'vue-i18n'
 import { createRouter, createWebHashHistory } from 'vue-router'
 import { iteratee, get } from 'lodash'
@@ -63,7 +63,9 @@ class Core extends Behaviors {
    */
   constructor(api = apiInstance) {
     super()
-    const Root = defineComponent({ template: '<router-view />' })
+    const components = { BApp }
+    const template = '<b-app><router-view /></b-app>'
+    const Root = defineComponent({ components, template })
     this._vue = createApp(Root)
     this._api = api
     this._auth = new Auth()
@@ -92,21 +94,7 @@ class Core extends Behaviors {
   useAll() {
     this.usePinia()
     this.useI18n()
-    this.useBootstrapVue({
-      directives: true,
-      components: {
-        BPopover: {
-          offset: '16px'
-        },
-        BTooltip: {
-          offset: '6px',
-          delay: {
-            show: 0,
-            hide: 0
-          }
-        }
-      }
-    })
+    this.useBootstrapVue()
     this.useCommons()
     this.useCore()
     return this
@@ -158,6 +146,13 @@ class Core extends Behaviors {
   useBootstrapVue() {
     this._bootstrapVue = createBootstrap({ components: true, directives: true })
     this.use(this.bootstrapVue)
+    // Provide legacy injection keys for murmur-next compatibility
+    // murmur-next bundles bootstrap-vue-next 0.30.x which uses different keys
+    const defaultsKeyPlugin = 'BootstrapVueNext__ID__defaults__plugin__' // v0.30.4 (murmur-next)
+    const defaultsKeyRegistry = 'BootstrapVueNext__ID__defaults__registry__' // v0.30.5
+    const defaultsValue = ref({})
+    this.vue.provide(defaultsKeyPlugin, defaultsValue)
+    this.vue.provide(defaultsKeyRegistry, defaultsValue)
     return this
   }
 
