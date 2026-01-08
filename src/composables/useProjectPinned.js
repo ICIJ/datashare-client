1b776aa86967cb10cef38aeea26fa8a9143f3ab3
Pierre Romera <hello@pirhoo.com>
2026-01-08 21:10:39 +0000

    refactor: use toRef instead of toValue for reactive project
    Ensures pinned computed properly reacts to project changes

--- src/composables/useProjectPinned.js
+++ src/composables/useProjectPinned.js
@@ -1,16 +1,19 @@
-import { computed, toValue } from 'vue'
+import { computed, toRef } from 'vue'
 
 import { useAppStore } from '@/store/modules'
 
 export function useProjectPinned(project) {
   const appStore = useAppStore()
-  const { name } = toValue(project)
+  const projectRef = toRef(project)
 
   const pinned = computed({
     get() {
+      const { name } = projectRef.value
       return appStore.isProjectPinned(name)
     },
     set(pinned) {
+      const { name } = projectRef.value
+
       if (pinned) {
         return appStore.pinProject(name)
       }
