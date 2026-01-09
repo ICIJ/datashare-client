4b0904795e6895ab88e17b8bfddc8a0eec072cd7
Caroline Desprat <cdesprat@icij.org>
2026-01-09 08:47:25 +0000

    fix: remove unused method

--- src/api/index.js
+++ src/api/index.js
@@ -396,10 +396,6 @@ export class Api {
     return this.sendActionAsText(`/api/index/search/${projectIds}/_mapping/field/${fields}`, { method: Method.GET })
   }
 
-  getPoliciesByUserId(userId) {
-    return this.sendActionAsText(`/api/policies/userId=${userId}`, { method: Method.GET })
-  }
-
   async sendAction(url, config = {}) {
     try {
       const r = await this.axios?.request({ url: Api.getFullUrl(url), ...config })
