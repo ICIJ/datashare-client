292ea9126ccf10abf9d10ef01b92128e2f8d46ac
Caroline Desprat <cdesprat@icij.org>
2026-01-09 09:06:04 +0000

    feat: add DisplayRoles component to streamline the display of multiple roles

---
+++ src/components/Display/DisplayRoles.vue
@@ -0,0 +1,21 @@
+<script setup>
+import DisplayRole from '@/components/Display/DisplayRole.vue'
+defineProps({
+  roles: {
+    type: Array,
+    required: true
+  }
+})
+
+</script>
+<template>
+  <div
+    v-for="(role,index) in roles"
+    :key="index"
+  >
+    <display-role
+      :value="role"
+      no-icon
+    /><span v-if="index !== roles.length-1">, </span>
+  </div>
+</template>
