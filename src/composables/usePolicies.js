33f9ed777e842243100307eae88b09c144af799e
Pierre Romera <hello@pirhoo.com>
2026-01-15 18:47:18 +0100

    refactor: use shorter syntax

--- src/composables/usePolicies.js
+++ src/composables/usePolicies.js
@@ -3,17 +3,22 @@ import { computed } from 'vue'
 import { camelCase, find, upperFirst } from 'lodash'
 
 const DEFAULT_ROLE = 'VIEWER'
+
 export function usePolicies() {
   const config = useConfig()
   const userPolicies = computed(() => config.get('policies', []))
-  function getRolesByProject(projectName) {
-    return find(userPolicies.value, p => p.projectId === projectName)?.roles ?? [DEFAULT_ROLE]
+
+  function getRolesByProject(projectId) {
+    return find(userPolicies.value, { projectId })?.roles
   }
+
   function formatRole(role) {
     return upperFirst(camelCase(role))
   }
+
   function formatRoles(roles) {
     return roles.map(formatRole).join(', ')
   }
+
   return { getRolesByProject, formatRole, formatRoles }
 }
