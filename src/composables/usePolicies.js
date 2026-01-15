4c87ab292f32700666de355231f2d8df522c8c90
Pierre Romera <hello@pirhoo.com>
2026-01-15 18:54:26 +0100

    fix: restore default value

--- src/composables/usePolicies.js
+++ src/composables/usePolicies.js
@@ -9,7 +9,7 @@ export function usePolicies() {
   const userPolicies = computed(() => config.get('policies', []))
 
   function getRolesByProject(projectId) {
-    return find(userPolicies.value, { projectId })?.roles
+    return find(userPolicies.value, { projectId })?.roles ?? [DEFAULT_ROLE]
   }
 
   function formatRole(role) {
