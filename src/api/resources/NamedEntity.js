2216891fdbcfddc47cf3a122a2ffb6e3639c00d6
Pierre Romera <hello@pirhoo.com>
2026-01-23 13:12:35 +0000

    fix(entities): add constructor to NamedEntity to match EsDocList.instantiate signature

--- src/api/resources/NamedEntity.js
+++ src/api/resources/NamedEntity.js
@@ -5,6 +5,18 @@ import EsDoc from './EsDoc'
 export default class NamedEntity extends EsDoc {
   static esName = 'NamedEntity'
 
+  /**
+   * Creates a NamedEntity instance.
+   * @param {Object} raw - The raw ES document data
+   * @param {Object|null} [parent=null] - Parent document raw data (unused but passed by EsDocList)
+   * @param {Object|null} [root=null] - Root document raw data (unused but passed by EsDocList)
+   * @param {number} [position=0] - Position in search results (unused but passed by EsDocList)
+   * @param {Object} [options={}] - Options passed to EsDoc
+   */
+  constructor(raw, parent = null, root = null, position = 0, options = {}) {
+    super(raw, options)
+  }
+
   get category() {
     return this.source.category.toLowerCase()
   }
