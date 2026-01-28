6190971e9f5fdc10ae82a9a74c8dd91c26d0b443
Pierre Romera <hello@pirhoo.com>
2026-01-28 00:29:53 +0000

    feat(components): add DisplayVersion component

---
+++ src/components/Display/DisplayVersion.vue
@@ -0,0 +1,25 @@
+<script setup>
+defineOptions({ name: 'DisplayVersion' })
+
+defineProps({
+  value: {
+    type: String,
+    default: null
+  }
+})
+</script>
+
+<template>
+  <span
+    v-if="value"
+    class="display-version font-monospace"
+  >
+    {{ value }}
+  </span>
+  <span
+    v-else
+    class="display-version display-version--empty"
+  >
+    -
+  </span>
+</template>
