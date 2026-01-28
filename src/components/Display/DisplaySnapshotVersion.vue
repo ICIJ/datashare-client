0900f289d3ca089c91b47a0908138bcae0255db4
Pierre Romera <hello@pirhoo.com>
2026-01-28 00:30:14 +0000

    feat(components): add DisplaySnapshotVersion component

---
+++ src/components/Display/DisplaySnapshotVersion.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { computed } from 'vue'
+
+import DisplayVersion from '@/components/Display/DisplayVersion'
+import { parseSnapshotName } from '@/composables/useSnapshots'
+
+defineOptions({ name: 'DisplaySnapshotVersion' })
+
+const props = defineProps({
+  value: {
+    type: String,
+    required: true
+  }
+})
+
+const version = computed(() => parseSnapshotName(props.value).version)
+</script>
+
+<template>
+  <display-version
+    class="display-snapshot-version"
+    :value="version"
+  />
+</template>
