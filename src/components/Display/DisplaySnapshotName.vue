ffff6b44e64dafff4a383c4ee99dbda488d35367
Pierre Romera <hello@pirhoo.com>
2026-01-28 00:30:07 +0000

    feat(components): add DisplaySnapshotName component

---
+++ src/components/Display/DisplaySnapshotName.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { computed } from 'vue'
+
+import { parseSnapshotName } from '@/composables/useSnapshots'
+
+defineOptions({ name: 'DisplaySnapshotName' })
+
+const props = defineProps({
+  value: {
+    type: String,
+    required: true
+  }
+})
+
+const name = computed(() => parseSnapshotName(props.value).name)
+</script>
+
+<template>
+  <span class="display-snapshot-name font-monospace">
+    {{ name }}
+  </span>
+</template>
