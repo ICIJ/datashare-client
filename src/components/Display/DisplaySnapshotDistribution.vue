6ac2851669a9fb155946f2c33705c2f68d871dfc
Pierre Romera <hello@pirhoo.com>
2026-01-28 00:30:21 +0000

    feat(components): add DisplaySnapshotDistribution component

---
+++ src/components/Display/DisplaySnapshotDistribution.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { computed } from 'vue'
+
+import DisplayEsDistribution from '@/components/Display/DisplayEsDistribution'
+import { parseSnapshotName } from '@/composables/useSnapshots'
+
+defineOptions({ name: 'DisplaySnapshotDistribution' })
+
+const props = defineProps({
+  value: {
+    type: String,
+    required: true
+  }
+})
+
+const distribution = computed(() => parseSnapshotName(props.value).distribution)
+</script>
+
+<template>
+  <display-es-distribution
+    class="display-snapshot-distribution"
+    :value="distribution"
+  />
+</template>
