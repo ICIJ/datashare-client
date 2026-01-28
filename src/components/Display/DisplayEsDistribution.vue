58a15bf0fc35dfb53f477207b491bd8fa8616472
Pierre Romera <hello@pirhoo.com>
2026-01-28 00:30:00 +0000

    feat(components): add DisplayEsDistribution component

---
+++ src/components/Display/DisplayEsDistribution.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+import { ES_DISTRIBUTION } from '@/enums/esDistributions'
+
+defineOptions({ name: 'DisplayEsDistribution' })
+
+const props = defineProps({
+  value: {
+    type: String,
+    default: ES_DISTRIBUTION.ELASTICSEARCH
+  }
+})
+
+const { t } = useI18n()
+
+const label = computed(() => {
+  return t(`display.esDistribution.${props.value}`, props.value)
+})
+</script>
+
+<template>
+  <span class="display-es-distribution">
+    {{ label }}
+  </span>
+</template>
