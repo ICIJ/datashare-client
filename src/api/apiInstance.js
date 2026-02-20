2300e60a6d6329a61d935a29d81e2991ae7954d6
Pierre Romera <hello@pirhoo.com>
2026-02-20 14:21:33 +0000

    feat(api): configure Axios CSRF token from cookie

--- src/api/apiInstance.js
+++ src/api/apiInstance.js
@@ -4,4 +4,7 @@ import { EventBus } from '@/utils/eventBus'
 import { Api } from '@/api'
 import { elasticsearch } from '@/api/elasticsearch'
 
+axios.defaults.xsrfCookieName = '_ds_csrf_token'
+axios.defaults.xsrfHeaderName = 'X-DS-CSRF-TOKEN'
+
 export const apiInstance = new Api(axios, EventBus, elasticsearch)
