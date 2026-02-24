c8870c1609f93afa9575c78779709685654b9509
Pierre Romera <hello@pirhoo.com>
2026-02-24 15:37:46 +0000

    refactor(DocumentThread): extract DocumentThreadEntryHeaderCreationDate component

---
+++ src/components/Document/DocumentThread/DocumentThreadEntryHeaderCreationDate.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { useI18n } from 'vue-i18n'
+
+defineOptions({ name: 'DocumentThreadEntryHeaderCreationDate' })
+
+defineProps({
+  email: {
+    type: Object,
+    required: true
+  },
+  tooltipDelay: {
+    type: Object,
+    default: () => ({ show: 0, hide: 0 })
+  }
+})
+
+const { d } = useI18n()
+</script>
+
+<template>
+  <router-link
+    v-if="email.creationDate"
+    v-b-tooltip.body="{ delay: tooltipDelay }"
+    :to="{ name: 'document', params: email.routerParams }"
+    class="document-thread-entry-header-creation-date"
+    :title="email.creationDateHuman"
+    @click.stop
+  >
+    {{ d(email.creationDate) }}
+  </router-link>
+</template>
---
+++ tests/unit/specs/components/Document/DocumentThread/DocumentThreadEntryHeaderCreationDate.spec.js
@@ -0,0 +1,61 @@
+import { shallowMount } from '@vue/test-utils'
+import { RouterLinkStub } from '@vue/test-utils'
+
+import CoreSetup from '~tests/unit/CoreSetup'
+import DocumentThreadEntryHeaderCreationDate from '@/components/Document/DocumentThread/DocumentThreadEntryHeaderCreationDate'
+
+describe('DocumentThreadEntryHeaderCreationDate.vue', () => {
+  let core
+
+  const mockEmail = {
+    id: 'email_01',
+    creationDate: new Date('2020-12-04T00:00:01Z'),
+    creationDateHuman: 'December 4, 2020',
+    routerParams: { id: 'email_01', index: 'test-index', routing: 'email_01' }
+  }
+
+  function createWrapper(props = {}) {
+    return shallowMount(DocumentThreadEntryHeaderCreationDate, {
+      global: {
+        plugins: core.plugins,
+        stubs: {
+          RouterLink: RouterLinkStub
+        }
+      },
+      props: {
+        email: mockEmail,
+        ...props
+      }
+    })
+  }
+
+  beforeEach(() => {
+    core = CoreSetup.init().useAll()
+  })
+
+  it('should render a router-link when creationDate exists', () => {
+    const wrapper = createWrapper()
+    expect(wrapper.findComponent(RouterLinkStub).exists()).toBe(true)
+  })
+
+  it('should not render a router-link when creationDate is missing', () => {
+    const wrapper = createWrapper({
+      email: { id: 'email_01', routerParams: {} }
+    })
+    expect(wrapper.findComponent(RouterLinkStub).exists()).toBe(false)
+  })
+
+  it('should display the formatted creation date', () => {
+    const wrapper = createWrapper()
+    expect(wrapper.findComponent(RouterLinkStub).text()).toBeTruthy()
+  })
+
+  it('should link to the document route with correct params', () => {
+    const wrapper = createWrapper()
+    const link = wrapper.findComponent(RouterLinkStub)
+    expect(link.props('to')).toEqual({
+      name: 'document',
+      params: { id: 'email_01', index: 'test-index', routing: 'email_01' }
+    })
+  })
+})
