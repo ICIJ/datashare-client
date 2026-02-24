2ede56e96174db443ce7083a810e2eb985312b98
Pierre Romera <hello@pirhoo.com>
2026-02-24 15:37:40 +0000

    refactor(DocumentThread): extract DocumentThreadEntryHeaderFrom component

---
+++ src/components/Document/DocumentThread/DocumentThreadEntryHeaderFrom.vue
@@ -0,0 +1,21 @@
+<script setup>
+import DisplayEmail from '@/components/Display/DisplayEmail'
+
+defineOptions({ name: 'DocumentThreadEntryHeaderFrom' })
+
+defineProps({
+  email: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="document-thread-entry-header-from flex-grow-1">
+    <display-email
+      :value="email.messageFrom"
+      tag="strong"
+    />
+  </div>
+</template>
