829ffac6df3385734567598fd1bf2963d9128293
pirhoo <hello@pirhoo.com>
2026-03-26 13:47:00 +0100

    refactor(Login): delegate to LoginCard or LoginWelcome based on authFilter

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -1,67 +1,42 @@
 <script setup>
-import { useTemplateRef } from 'vue'
-import { useI18n } from 'vue-i18n'
-import { ButtonIcon } from '@icij/murmur-next'
+import { computed } from 'vue'
 import { whenever } from '@vueuse/core'
 import { useRouter } from 'vue-router'
-import IPhUser from '~icons/ph/user'
 
-import LoginImage from '@/components/Login/LoginImage'
-import I18nLocaleDropdown from '@/components/I18n/I18nLocaleDropdown'
+import LoginCard from '@/components/Login/LoginCard'
+import LoginWelcome from '@/components/Login/LoginWelcome'
 import { useCore } from '@/composables/useCore'
 import { useAuth } from '@/composables/useAuth'
-import settings from '@/utils/settings'
 
-const { t } = useI18n()
+const FORM_AUTH_FILTER = 'org.icij.datashare.session.FormAuthFilter'
+
 const core = useCore()
 const { username } = useAuth()
 const router = useRouter()
 
-const image = useTemplateRef('image')
-const signinUrl = import.meta.env.VITE_DS_AUTH_SIGNIN
-const helpLink = core.vue.config.globalProperties.$config.get('helpLink', settings.helpLink)
+const isFormAuth = computed(() => core.config.get('authFilter') === FORM_AUTH_FILTER)
 
 // This ensures the login page can only be accessed by unauthenticated users
 whenever(username, () => router.push({ name: 'landing' }))
 </script>
 
 <template>
-  <div class="login d-flex flex-column justify-content-between align-items-stretch gap-2 p-4 vh-100">
-    <div class="login__enter d-flex flex-column align-items-center text-action-emphasis p-4">
-      <login-image
-        ref="image"
-        style="width: 240px"
-      />
-      <h3>{{ t('login.welcome') }}</h3>
-      <p>{{ t('login.tagline') }}</p>
-      <button-icon
-        :label="t('login.account')"
-        :href="signinUrl"
-        class="login__enter_link mt-4"
-        :icon-left="IPhUser"
-        variant="action"
-        @click="image.shake()"
-      />
-    </div>
-    <div class="login__assistance d-flex flex-column align-items-center justify-content-end gap-2">
-      <button-icon
-        class="login__assistance__help"
-        :label="t('login.askHelp')"
-        tag="a"
-        :href="helpLink"
-        variant="outline-secondary"
-        @click="image.shake()"
-      />
-      <div class="login__assistance__locale d-flex align-items-center gap-2">
-        {{ t('login.switchLanguage') }}
-        <i18n-locale-dropdown class="px-2" />
-      </div>
-    </div>
+  <div
+    class="login d-flex min-vh-100"
+    :class="{ 'login--form-auth': isFormAuth }"
+  >
+    <login-card v-if="isFormAuth" />
+    <login-welcome v-else />
   </div>
 </template>
 
 <style scoped lang="scss">
 .login {
-  max-height: 630px;
+  &--form-auth {
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 1rem;
+  }
 }
 </style>
