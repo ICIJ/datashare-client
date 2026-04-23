2d5d7e5076eaf5efb3dbaa2d203a163c0756a5a6
pirhoo <hello@pirhoo.com>
2026-04-23 08:49:29 +0000

    feat(FileTypesViewEntry): render individual file type with checkbox

---
+++ src/components/FileTypes/FileTypesView/FileTypesViewEntry.vue
@@ -0,0 +1,19 @@
+<script setup>
+const modelValue = defineModel({ type: Boolean })
+
+defineProps({
+  fileType: {
+    type: Array,
+    required: false,
+    default: () => []
+  }
+})
+</script>
+
+<template>
+  <div class="file-types-view-entry">
+    <b-form-checkbox v-model="modelValue">
+      {{ fileType }}
+    </b-form-checkbox>
+  </div>
+</template>
