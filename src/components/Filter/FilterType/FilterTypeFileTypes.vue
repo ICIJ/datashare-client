d70207eeefb21a1620518bf899ae8e441b6365e0
pirhoo <hello@pirhoo.com>
2026-04-23 11:15:57 +0000

    refactor(FilterTypeFileTypes): compose atomic category subcomponents using useContentTypeCategories

--- src/components/Filter/FilterType/FilterTypeFileTypes.vue
+++ src/components/Filter/FilterType/FilterTypeFileTypes.vue
@@ -1,8 +1,15 @@
 <script setup>
+import { computed, ref, useTemplateRef } from 'vue'
+
 import ButtonToggleFileTypesGrouped from '@/components/Button/ButtonToggleFileTypesGrouped.vue'
 import FileTypesView from '@/components/FileTypes/FileTypesView/FileTypesView.vue'
+import FileTypesViewCategory from '@/components/FileTypes/FileTypesView/FileTypesViewCategory.vue'
+import FileTypesViewCategoryName from '@/components/FileTypes/FileTypesView/FileTypesViewCategoryName.vue'
+import FileTypesViewCategoryEntry from '@/components/FileTypes/FileTypesView/FileTypesViewCategoryEntry.vue'
+import FileTypesViewEntry from '@/components/FileTypes/FileTypesView/FileTypesViewEntry.vue'
 import FilterType from './FilterType.vue'
-import { ref } from 'vue'
+import { useContentTypeCategories } from '@/composables/useContentTypeCategories'
+
 const props = defineProps({
   filter: {
     type: Object,
@@ -15,20 +22,43 @@ const props = defineProps({
 
 const grouped = ref(true)
 
+const filterTypeRef = useTemplateRef('filterTypeRef')
+const contentTypes = computed(() =>
+  (filterTypeRef.value?.entries ?? []).map(entry => entry.item.key)
+)
+
+const { categories } = useContentTypeCategories(contentTypes)
 </script>
 
 <template>
   <filter-type
+    ref="filterTypeRef"
     :filter="props.filter"
     :hide-search="grouped"
   >
-    <template
-      #default="{ entries }"
-    >
-      <file-types-view
-        :content-types="entries.map(entry => entry.item.key)"
-        :expanded="!grouped"
-      />
+    <template #default="{ entries }">
+      <file-types-view>
+        <template v-if="grouped">
+          <file-types-view-category
+            v-for="(types, category) in categories"
+            :key="category"
+          >
+            <file-types-view-category-name :label="category" />
+            <file-types-view-category-entry
+              v-for="fileType in types"
+              :key="fileType"
+              :file-type="fileType"
+            />
+          </file-types-view-category>
+        </template>
+        <template v-else>
+          <file-types-view-entry
+            v-for="entry in entries"
+            :key="entry.item.key"
+            :file-type="entry.item.key"
+          />
+        </template>
+      </file-types-view>
     </template>
 
     <template #actions>
--- tests/unit/specs/components/Filter/FilterType/FilterTypeFileTypes.spec.js
+++ tests/unit/specs/components/Filter/FilterType/FilterTypeFileTypes.spec.js
@@ -1,4 +1,4 @@
-import { mount } from '@vue/test-utils'
+import { flushPromises, mount } from '@vue/test-utils'
 import { removeCookie, setCookie } from 'tiny-cookie'
 
 import CoreSetup from '~tests/unit/CoreSetup'
@@ -7,8 +7,21 @@ import { IndexedDocument, letData } from '~tests/unit/es_utils'
 import FilterType from '@/components/Filter/FilterType/FilterType'
 import FilterTypeFileTypes from '@/components/Filter/FilterType/FilterTypeFileTypes'
 import ButtonToggleFileTypesGrouped from '@/components/Button/ButtonToggleFileTypesGrouped'
-import FiltersPanelSectionFilterEntry from '@/components/FiltersPanel/FiltersPanelSectionFilterEntry'
+import FileTypesViewCategory from '@/components/FileTypes/FileTypesView/FileTypesViewCategory'
+import FileTypesViewCategoryEntry from '@/components/FileTypes/FileTypesView/FileTypesViewCategoryEntry'
+import FileTypesViewEntry from '@/components/FileTypes/FileTypesView/FileTypesViewEntry'
+import { apiInstance as api } from '@/api/apiInstance'
 import { useSearchStore } from '@/store/modules'
+
+vi.mock('@/api/apiInstance', async (importOriginal) => {
+  const { apiInstance } = await importOriginal()
+  return {
+    apiInstance: {
+      ...apiInstance,
+      getContentTypesGrouppedByCategories: vi.fn()
+    }
+  }
+})
 
 describe('FilterTypeFileTypes.vue', () => {
   const { index, es } = esConnectionHelper.build()
@@ -19,6 +32,9 @@ describe('FilterTypeFileTypes.vue', () => {
   })
 
   beforeEach(() => {
+    api.getContentTypesGrouppedByCategories.mockClear()
+    api.getContentTypesGrouppedByCategories.mockResolvedValue({})
+
     core = CoreSetup.init().useAll()
     searchStore = useSearchStore()
     const filter = searchStore.getFilter({ name: 'contentType' })
@@ -35,38 +51,71 @@ describe('FilterTypeFileTypes.vue', () => {
     })
   })
 
-  afterAll(() => removeCookie(process.env.VITE_DS_COOKIE_NAME))
+  afterAll(() => {
+    removeCookie(process.env.VITE_DS_COOKIE_NAME)
+    vi.resetAllMocks()
+  })
 
   it('should render a ButtonToggleFileTypesGrouped in the actions slot', () => {
     expect(wrapper.findComponent(ButtonToggleFileTypesGrouped).exists()).toBe(true)
   })
 
-  it('should have grouped set to false by default', () => {
-    expect(wrapper.findComponent(ButtonToggleFileTypesGrouped).props('grouped')).toBe(false)
-  })
-
-  it('should toggle grouped to true when the button is clicked', async () => {
-    await wrapper.findComponent(ButtonToggleFileTypesGrouped).trigger('click')
+  it('should have grouped set to true by default', () => {
     expect(wrapper.findComponent(ButtonToggleFileTypesGrouped).props('grouped')).toBe(true)
   })
 
-  it('should toggle grouped back to false on second click', async () => {
-    const button = wrapper.findComponent(ButtonToggleFileTypesGrouped)
-    await button.trigger('click')
-    await button.trigger('click')
+  it('should toggle grouped to false when the button is clicked', async () => {
+    await wrapper.findComponent(ButtonToggleFileTypesGrouped).trigger('click')
     expect(wrapper.findComponent(ButtonToggleFileTypesGrouped).props('grouped')).toBe(false)
   })
 
-  it('should display matching entries', async () => {
+  it('should toggle grouped back to true on second click', async () => {
+    const button = wrapper.findComponent(ButtonToggleFileTypesGrouped)
+    await button.trigger('click')
+    await button.trigger('click')
+    expect(wrapper.findComponent(ButtonToggleFileTypesGrouped).props('grouped')).toBe(true)
+  })
+
+  it('should render category groups when entries are aggregated in grouped mode', async () => {
+    api.getContentTypesGrouppedByCategories.mockResolvedValue({
+      Documents: ['application/pdf'],
+      Other: ['text/html', 'other']
+    })
+
     await letData(es).have(new IndexedDocument('document_01', index).withContentType('application/pdf')).commit()
-    await letData(es).have(new IndexedDocument('document_02', index).withContentType('application/pdf')).commit()
-    await letData(es).have(new IndexedDocument('document_03', index).withContentType('text/html')).commit()
-    await letData(es).have(new IndexedDocument('document_04', index).withContentType('other')).commit()
-    await letData(es).have(new IndexedDocument('document_05', index).withContentType('other')).commit()
+    await letData(es).have(new IndexedDocument('document_02', index).withContentType('text/html')).commit()
+    await letData(es).have(new IndexedDocument('document_03', index).withContentType('other')).commit()
 
     await wrapper.findComponent(FilterType).vm.aggregateOver()
+    await flushPromises()
 
-    const entries = wrapper.findAllComponents(FiltersPanelSectionFilterEntry)
-    expect(entries).toHaveLength(4) // Including "All"
+    expect(wrapper.findAllComponents(FileTypesViewCategory)).toHaveLength(2)
+    expect(wrapper.findAllComponents(FileTypesViewCategoryEntry)).toHaveLength(3)
+    expect(wrapper.findAllComponents(FileTypesViewEntry)).toHaveLength(0)
+  })
+
+  it('should render flat entries when grouped is toggled off', async () => {
+    await letData(es).have(new IndexedDocument('document_01', index).withContentType('application/pdf')).commit()
+    await letData(es).have(new IndexedDocument('document_02', index).withContentType('text/html')).commit()
+    await letData(es).have(new IndexedDocument('document_03', index).withContentType('other')).commit()
+
+    await wrapper.findComponent(FilterType).vm.aggregateOver()
+    await flushPromises()
+    await wrapper.findComponent(ButtonToggleFileTypesGrouped).trigger('click')
+
+    expect(wrapper.findAllComponents(FileTypesViewCategory)).toHaveLength(0)
+    expect(wrapper.findAllComponents(FileTypesViewEntry)).toHaveLength(3)
+  })
+
+  it('should pass aggregated content types to the categories API', async () => {
+    await letData(es).have(new IndexedDocument('document_01', index).withContentType('application/pdf')).commit()
+    await letData(es).have(new IndexedDocument('document_02', index).withContentType('text/html')).commit()
+
+    await wrapper.findComponent(FilterType).vm.aggregateOver()
+    await flushPromises()
+
+    expect(api.getContentTypesGrouppedByCategories).toHaveBeenCalledWith(
+      expect.arrayContaining(['application/pdf', 'text/html'])
+    )
   })
 })
