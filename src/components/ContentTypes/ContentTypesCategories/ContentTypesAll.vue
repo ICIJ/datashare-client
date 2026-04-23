73446a490c5ef915d8b5c3b46e2ffbf385c87258
pirhoo <hello@pirhoo.com>
2026-04-23 14:00:40 +0000

    feat(ContentTypesAll): add All row for ContentTypes filter

---
+++ src/components/ContentTypes/ContentTypesCategories/ContentTypesAll.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { useI18n } from 'vue-i18n'
+
+import FiltersPanelSectionFilterEntry from '@/components/FiltersPanel/FiltersPanelSectionFilterEntry'
+
+const modelValue = defineModel({ type: Boolean, default: false })
+
+defineProps({
+  count: {
+    type: Number,
+    default: 0
+  },
+  label: {
+    type: String,
+    default: null
+  }
+})
+
+const { t } = useI18n()
+</script>
+
+<template>
+  <filters-panel-section-filter-entry
+    v-model="modelValue"
+    class="content-types-all"
+    :label="label ?? t('filterTypeAll.label')"
+    :count="count"
+    :disabled="modelValue"
+  />
+</template>
---
+++ src/stories/components/ContentTypes/ContentTypesCategories/ContentTypesAll.stories.js
@@ -0,0 +1,19 @@
+import ContentTypesAll from '@/components/ContentTypes/ContentTypesCategories/ContentTypesAll'
+
+export default {
+  tags: ['autodocs'],
+  title: 'Components/ContentTypes/ContentTypesCategories/ContentTypesAll',
+  component: ContentTypesAll,
+  args: {
+    count: 2344,
+    modelValue: true
+  }
+}
+
+export const Default = {}
+
+export const SomeFilterApplied = {
+  args: {
+    modelValue: false
+  }
+}
