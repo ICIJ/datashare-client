954644538097eab08e7dd5307d99c26177cc044b
pirhoo <hello@pirhoo.com>
2026-04-23 08:49:32 +0000

    feat(FileTypesViewGrouped): render file type category with nested entries

---
+++ src/components/FileTypes/FileTypesView/FileTypesViewGrouped.vue
@@ -0,0 +1,30 @@
+<script setup>
+const modelValue = defineModel({ type: Boolean })
+
+defineProps({
+  category: {
+    type: String,
+    required: true
+  },
+  fileTypes: {
+    type: Array,
+    required: false,
+    default: () => []
+  },
+})
+</script>
+
+<template>
+  <div class="file-types-view-entry">
+    <b-form-checkbox v-model="modelValue">
+      {{ category }}
+    </b-form-checkbox>
+    <div
+      v-for="fileType in fileTypes"
+      :key="fileType"
+      class="file-types-view-entry__file-type"
+    >
+      {{ fileType }}
+    </div>
+  </div>
+</template>
