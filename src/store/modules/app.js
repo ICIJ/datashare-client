3f385f2319977c94b7c02550ee34e46d6db5ef63
Caroline Desprat <cdesprat@icij.org>
2026-04-29 12:46:54 +0000

    chore(app): bump SETTINGS_VERSION to 1

--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -10,7 +10,7 @@ import { SEARCH_OPERATORS } from '@/enums/searchOperators'
  * will require resetting user settings to defaults. This is useful if for instance you add new settings
  * or change the structure of existing settings and want to ensure users get the latest defaults.
  */
-const SETTINGS_VERSION = 0
+const SETTINGS_VERSION = 1
 
 /**
  * Defines the application-wide store for managing UI state and user preferences.
