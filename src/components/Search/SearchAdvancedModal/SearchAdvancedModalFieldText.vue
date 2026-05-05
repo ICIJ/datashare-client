85bbb8fe13060a7810b091b08ac9ba7e8f632345
Pierre Romera <hello@pirhoo.com>
2026-05-05 13:36:01 +0000

    refactor(SearchAdvancedModalFieldText): extract example list into atom

--- src/components/Search/SearchAdvancedModal/SearchAdvancedModalFieldText.vue
+++ src/components/Search/SearchAdvancedModal/SearchAdvancedModalFieldText.vue
@@ -11,31 +11,15 @@
       v-if="examples.length"
       #example
     >
-      <p class="search-advanced-modal__example">
-        <template
-          v-for="(line, index) in examples"
-          :key="index"
-        >
-          <span
-            v-if="index > 0"
-            class="search-advanced-modal__example__break"
-          />
-          <span class="search-advanced-modal__example__prefix">
-            {{ index === 0 ? t('searchAdvancedModal.eg') : t('searchAdvancedModal.or') }}
-          </span>
-          <span class="search-advanced-modal__example__value">
-            {{ line }}
-          </span>
-        </template>
-      </p>
+      <search-advanced-modal-examples :examples="examples" />
     </template>
   </search-advanced-modal-field>
 </template>
 
 <script setup>
 import { useTemplateRef } from 'vue'
-import { useI18n } from 'vue-i18n'
 
+import SearchAdvancedModalExamples from './SearchAdvancedModalExamples.vue'
 import SearchAdvancedModalField from './SearchAdvancedModalField.vue'
 
 defineProps({
@@ -59,7 +43,6 @@ defineProps({
 
 const model = defineModel({ type: String, default: '' })
 
-const { t } = useI18n()
 const input = useTemplateRef('input')
 
 // Exposed so a parent (the modal) can autofocus the first row on open.
