4741696d9b24d38b0b9f7cac0b1e02e9ae52fbb7
Caroline Desprat <cdesprat@icij.org>
2026-05-05 14:05:01 +0000

    test(DocumentViewTabsViewer): replace ES round-trips with RawDocBuilder store setup

--- tests/unit/specs/views/Document/DocumentView/DocumentViewTabs/DocumentViewTabsViewer.spec.js
+++ tests/unit/specs/views/Document/DocumentView/DocumentViewTabs/DocumentViewTabsViewer.spec.js
@@ -1,14 +1,13 @@
 import { shallowMount } from '@vue/test-utils'
 
-import esConnectionHelper from '~tests/unit/specs/utils/esConnectionHelper'
 import CoreSetup from '~tests/unit/CoreSetup'
-import { IndexedDocument, letData } from '~tests/unit/es_utils'
+import RawDocBuilder from '~tests/unit/RawDocBuilder'
 import DocumentViewTabsViewer from '@/views/Document/DocumentView/DocumentViewTabs/DocumentViewTabsViewer'
 import { useDocumentStore } from '@/store/modules'
 
 describe('DocumentViewTabsViewer.vue', () => {
-  const { index, es } = esConnectionHelper.build()
-  const id = 'document'
+  const index = 'test-index'
+  const id = '/home/datashare/data/test.document'
   const disabled = true
   let core, documentStore
 
@@ -25,106 +24,67 @@ describe('DocumentViewTabsViewer.vue', () => {
     documentStore.reset()
   })
 
-  it('should call the DocumentViewerLegacySpreadsheet component for XLSX document', async () => {
-    const document = await letData(es)
-      .have(
-        new IndexedDocument(id, index).withContentType(
-          'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-        )
-      )
-      .commitAndGetLastDocument()
-
-    await documentStore.getDocument({ id, index })
-
+  it('should call the DocumentViewerLegacySpreadsheet component for XLSX document', () => {
+    documentStore.setDocument(
+      RawDocBuilder.build(id, index)
+        .withContentType('application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        .toRaw()
+    )
+    const document = documentStore.document
     const wrapper = shallowMount(DocumentViewTabsViewer, {
-      global: {
-        plugins: core.plugins,
-        renderStubDefaultSlot: true
-      },
+      global: { plugins: core.plugins, renderStubDefaultSlot: true },
       props: { document, disabled }
     })
     expect(wrapper.vm.previewComponent).toBe('DocumentViewerLegacySpreadsheet')
   })
 
-  it('should call the DocumentViewerLegacySpreadsheet component for CSV document', async () => {
-    const document = await letData(es)
-      .have(new IndexedDocument(id, index).withContentType('text/csv'))
-      .commitAndGetLastDocument()
-    await documentStore.getDocument({ id, index })
-
+  it('should call the DocumentViewerLegacySpreadsheet component for CSV document', () => {
+    documentStore.setDocument(RawDocBuilder.build(id, index).withContentType('text/csv').toRaw())
+    const document = documentStore.document
     const wrapper = shallowMount(DocumentViewTabsViewer, {
-      global: {
-        plugins: core.plugins,
-        renderStubDefaultSlot: true
-      },
+      global: { plugins: core.plugins, renderStubDefaultSlot: true },
       props: { document, disabled }
     })
     expect(wrapper.vm.previewComponent).toBe('DocumentViewerLegacySpreadsheet')
   })
 
-  it('should call the DocumentViewerPaginated component for Word document', async () => {
-    const document = await letData(es)
-      .have(new IndexedDocument(id, index).withContentType('application/msword'))
-      .commitAndGetLastDocument()
-    await documentStore.getDocument({ id, index })
-
+  it('should call the DocumentViewerPaginated component for Word document', () => {
+    documentStore.setDocument(RawDocBuilder.build(id, index).withContentType('application/msword').toRaw())
+    const document = documentStore.document
     const wrapper = shallowMount(DocumentViewTabsViewer, {
-      global: {
-        plugins: core.plugins,
-        renderStubDefaultSlot: true
-      },
+      global: { plugins: core.plugins, renderStubDefaultSlot: true },
       props: { document, disabled }
     })
     expect(wrapper.vm.previewComponent).toBe('DocumentViewerPaginated')
   })
 
-  it('should call the DocumentViewerTiff component for TIFF document', async () => {
-    const document = await letData(es)
-      .have(new IndexedDocument(id, index).withContentType('image/tiff'))
-      .commitAndGetLastDocument()
-    await documentStore.getDocument({ id, index })
-
+  it('should call the DocumentViewerTiff component for TIFF document', () => {
+    documentStore.setDocument(RawDocBuilder.build(id, index).withContentType('image/tiff').toRaw())
+    const document = documentStore.document
     const wrapper = shallowMount(DocumentViewTabsViewer, {
-      global: {
-        plugins: core.plugins,
-        renderStubDefaultSlot: true
-      },
+      global: { plugins: core.plugins, renderStubDefaultSlot: true },
       props: { document, disabled }
     })
     expect(wrapper.vm.previewComponent).toBe('DocumentViewerTiff')
   })
 
-  it('should call the DocumentViewerAudio component for audio document', async () => {
-    const document = await letData(es)
-      .have(new IndexedDocument(id, index).withContentType('audio/ogg'))
-      .commitAndGetLastDocument()
-    await documentStore.getDocument({ id, index })
-
+  it('should call the DocumentViewerAudio component for audio document', () => {
+    documentStore.setDocument(RawDocBuilder.build(id, index).withContentType('audio/ogg').toRaw())
+    const document = documentStore.document
     const wrapper = shallowMount(DocumentViewTabsViewer, {
-      global: {
-        plugins: core.plugins,
-        renderStubDefaultSlot: true
-      },
+      global: { plugins: core.plugins, renderStubDefaultSlot: true },
       props: { document, disabled }
     })
-
     expect(wrapper.vm.previewComponent).toBe('DocumentViewerAudio')
   })
 
-  it('should call the DocumentViewerVideo component for video document', async () => {
-    const document = await letData(es)
-      .have(new IndexedDocument(id, index).withContentType('video/mp4'))
-      .commitAndGetLastDocument()
-    await documentStore.getDocument({ id, index })
-
+  it('should call the DocumentViewerVideo component for video document', () => {
+    documentStore.setDocument(RawDocBuilder.build(id, index).withContentType('video/mp4').toRaw())
+    const document = documentStore.document
     const wrapper = shallowMount(DocumentViewTabsViewer, {
-      global: {
-        plugins: core.plugins,
-        renderStubDefaultSlot: true
-      },
+      global: { plugins: core.plugins, renderStubDefaultSlot: true },
       props: { document, disabled }
     })
-
     expect(wrapper.vm.previewComponent).toBe('DocumentViewerVideo')
   })
 })
