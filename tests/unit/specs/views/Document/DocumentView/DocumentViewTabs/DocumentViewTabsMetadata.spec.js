c51624015148b5e0343c931c1cc7623f243633db
Caroline Desprat <cdesprat@icij.org>
2026-05-11 14:35:02 +0000

    test(DocumentViewTabsMetadata): shallowMount + fake timers + prop assertions
    Switch mount→shallowMount and add vi.useFakeTimers to neutralise the 200ms
    useDebouncedRef debounce. Use findAllComponents(DocumentMetadata).props() for
    assertions instead of rendered text from child stubs. Cuts file from ~6.7s to
    ~67ms.

--- tests/unit/specs/views/Document/DocumentView/DocumentViewTabs/DocumentViewTabsMetadata.spec.js
+++ tests/unit/specs/views/Document/DocumentView/DocumentViewTabs/DocumentViewTabsMetadata.spec.js
@@ -1,46 +1,46 @@
-import { mount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 
 import CoreSetup from '~tests/unit/CoreSetup'
 import RawDocBuilder from '~tests/unit/RawDocBuilder'
 import DocumentViewTabsMetadata from '@/views/Document/DocumentView/DocumentViewTabs/DocumentViewTabsMetadata'
+import DocumentMetadata from '@/components/Document/DocumentMetadata/DocumentMetadata'
 import { useDocumentStore } from '@/store/modules'
 
-vi.mock('@/api/apiInstance', async (importOriginal) => {
-  const { apiInstance } = await importOriginal()
-
-  return {
-    apiInstance: {
-      ...apiInstance,
-      getPathBanners: vi.fn().mockResolvedValue([])
-    }
+vi.mock('@/api/apiInstance', () => ({
+  apiInstance: {
+    getPathBanners: vi.fn().mockResolvedValue([])
   }
-})
+}))
 
 describe('DocumentViewTabsMetadata.vue', () => {
   const index = 'test-index'
   const id = '/home/datashare/data/foo.txt'
   let wrapper, core, documentStore
 
-  const stubs = { DocumentViewTabsMetadataLinkedDocumentsCard: true }
-
   beforeAll(() => {
     core = CoreSetup.init().useAll()
+  })
+
+  beforeEach(() => {
+    vi.useFakeTimers()
+    core.createPinia()
     documentStore = useDocumentStore()
+  })
+
+  afterEach(() => {
+    wrapper.unmount()
+    documentStore.reset()
+    vi.useRealTimers()
   })
 
   afterAll(() => {
     vi.resetAllMocks()
   })
 
-  afterEach(() => {
-    wrapper.unmount()
-    documentStore.reset()
-  })
-
   it('should display document with 8 metadata', () => {
     documentStore.setDocument(RawDocBuilder.build(id, index).toRaw())
 
-    wrapper = mount(DocumentViewTabsMetadata, { global: { plugins: core.plugins, stubs } })
+    wrapper = shallowMount(DocumentViewTabsMetadata, { global: { plugins: core.plugins } })
 
     const inputs = wrapper.findAll('.document-view-tabs-metadata__entry')
     expect(inputs).toHaveLength(8)
@@ -49,29 +49,29 @@ describe('DocumentViewTabsMetadata.vue', () => {
   it('should display "File on disk" when extractionLevel metadata is missing', () => {
     documentStore.setDocument(RawDocBuilder.build(id, index).toRaw())
 
-    wrapper = mount(DocumentViewTabsMetadata, { global: { plugins: core.plugins, stubs } })
+    wrapper = shallowMount(DocumentViewTabsMetadata, { global: { plugins: core.plugins } })
 
-    const extractionLevelEntry = wrapper.findAll('.document-view-tabs-metadata__entry')
-      .find(el => el.text().includes('Extraction level'))
-
+    const entries = wrapper.findAllComponents(DocumentMetadata)
+    const extractionLevelEntry = entries.find(c => c.props('name') === 'extractionLevel')
     expect(extractionLevelEntry).toBeDefined()
-    expect(extractionLevelEntry.text()).toContain('File on disk')
+    expect(extractionLevelEntry.props('value')).toBe(0)
   })
 
   it('should display document with 8 metadata (including language)', () => {
     documentStore.setDocument(RawDocBuilder.build(id, index).withLanguage('FRENCH').toRaw())
 
-    wrapper = mount(DocumentViewTabsMetadata, { global: { plugins: core.plugins, stubs } })
+    wrapper = shallowMount(DocumentViewTabsMetadata, { global: { plugins: core.plugins } })
 
-    const inputs = wrapper.findAll('.document-metadata__value')
-    const values = inputs.map(input => input.text())
-    expect(values).toContain('French')
+    const entries = wrapper.findAllComponents(DocumentMetadata)
+    const languageEntry = entries.find(c => c.props('name') === 'language')
+    expect(languageEntry).toBeDefined()
+    expect(languageEntry.props('value')).toBe('FRENCH')
   })
 
   it('should filter the list with a query', async () => {
     documentStore.setDocument(RawDocBuilder.build(id, index).toRaw())
 
-    wrapper = mount(DocumentViewTabsMetadata, { global: { plugins: core.plugins, stubs } })
+    wrapper = shallowMount(DocumentViewTabsMetadata, { global: { plugins: core.plugins } })
 
     wrapper.vm.q = 'language'
     await wrapper.vm.$nextTick()
