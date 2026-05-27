d65b0c954cdd8023556918984ee37ed9cf1a8c3f
pirhoo <hello@pirhoo.com>
2026-05-27 09:42:18 +0000

    feat(search): cancel in-flight async search on leaving the search view

--- tests/unit/specs/views/Search/Search.spec.js
+++ tests/unit/specs/views/Search/Search.spec.js
@@ -2,6 +2,7 @@ import { shallowMount } from '@vue/test-utils'
 
 import CoreSetup from '~tests/unit/CoreSetup'
 import Search from '@/views/Search/Search'
+import { useSearchStore } from '@/store/modules'
 
 vi.mock('@/api/apiInstance', {
   apiInstance: {
@@ -30,5 +31,15 @@ describe('Search.vue', () => {
 
   it('is a Vue instance', () => {
     expect(wrapper.vm).toBeTruthy()
+  })
+
+  it('cancels the active search when the view unmounts', () => {
+    // Same active pinia as the mounted component, so this is the same store instance.
+    const searchStore = useSearchStore()
+    const spy = vi.spyOn(searchStore, 'cancelActiveSearch')
+
+    wrapper.unmount()
+
+    expect(spy).toHaveBeenCalledTimes(1)
   })
 })
