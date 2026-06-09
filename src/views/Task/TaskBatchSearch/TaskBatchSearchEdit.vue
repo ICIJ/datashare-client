b7b3ad08a790199317481f749c9105521d965ef1
Pierre Romera <hello@pirhoo.com>
2026-06-09 12:34:30 +0000

    feat(batch-search): add edit page view

---
+++ src/views/Task/TaskBatchSearch/TaskBatchSearchEdit.vue
@@ -0,0 +1,26 @@
+<script setup>
+import PageContainer from '@/components/PageContainer/PageContainer'
+import PageHeader from '@/components/PageHeader/PageHeader'
+import TaskBatchSearchFormEdit from '@/components/Task/TaskBatchSearch/TaskBatchSearchFormEdit'
+
+defineProps({
+  indices: {
+    type: String,
+    required: true
+  },
+  uuid: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <page-header no-toggle-settings />
+  <page-container fluid>
+    <task-batch-search-form-edit
+      :indices="indices"
+      :uuid="uuid"
+    />
+  </page-container>
+</template>
