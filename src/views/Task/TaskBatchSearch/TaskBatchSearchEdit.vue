484bb43df60c3488b650c1a5300ef66894d0ca9e
Pierre Romera <hello@pirhoo.com>
2026-06-16 16:00:58 +0000

    feat(batch-search): show batch search name in edit breadcrumb

--- src/views/Task/TaskBatchSearch/TaskBatchSearchEdit.vue
+++ src/views/Task/TaskBatchSearch/TaskBatchSearchEdit.vue
@@ -1,9 +1,13 @@
 <script setup>
+import { onBeforeMount, ref } from 'vue'
+
+import AppSpinner from '@/components/AppSpinner/AppSpinner'
 import PageContainer from '@/components/PageContainer/PageContainer'
 import PageHeader from '@/components/PageHeader/PageHeader'
 import TaskBatchSearchFormEdit from '@/components/Task/TaskBatchSearch/TaskBatchSearchFormEdit'
+import { useCore } from '@/composables/useCore'
 
-defineProps({
+const { indices, uuid } = defineProps({
   indices: {
     type: String,
     required: true
@@ -13,10 +17,37 @@ defineProps({
     required: true
   }
 })
+
+const core = useCore()
+const batchSearch = ref(null)
+
+const breadcrumbRoutes = [
+  { name: 'task' },
+  { name: 'task.batch-search.list' },
+  { name: 'task.batch-search-queries.list' },
+  { name: 'task.batch-search.edit' }
+]
+
+async function fetchBatchSearch() {
+  batchSearch.value = await core.api.getBatchSearch(uuid)
+}
+
+onBeforeMount(fetchBatchSearch)
 </script>
 
 <template>
-  <page-header no-toggle-settings />
+  <!-- eslint-disable vue/valid-v-slot -->
+  <page-header
+    no-toggle-settings
+    :breadcrumb-routes="breadcrumbRoutes"
+  >
+    <template #entry-label(task.batch-search-queries.list)>
+      <template v-if="batchSearch">
+        {{ batchSearch.name }}
+      </template>
+      <app-spinner v-else />
+    </template>
+  </page-header>
   <page-container fluid>
     <task-batch-search-form-edit
       :indices="indices"
