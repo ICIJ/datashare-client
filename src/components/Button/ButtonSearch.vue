d5c820b13f0b81d00df4f3dd098cd5431527bef4
Pierre Romera <hello@pirhoo.com>
2026-06-23 12:04:23 +0000

    feat(ButtonSearch): add reusable search button with trailing magnifying glass

---
+++ src/components/Button/ButtonSearch.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { useI18n } from 'vue-i18n'
+import { ButtonIcon } from '@icij/murmur'
+
+import IPhMagnifyingGlass from '~icons/ph/magnifying-glass'
+
+import { VARIANT } from '@/enums/variants'
+defineProps({
+  labelKey: { type: String, default: 'global.search' },
+  variant: { type: String, default: VARIANT.ACTION }
+})
+const { t } = useI18n()
+</script>
+
+<template>
+  <button-icon
+    :icon-right="IPhMagnifyingGlass"
+    :variant="variant"
+  >
+    {{ t(labelKey) }}
+  </button-icon>
+</template>
