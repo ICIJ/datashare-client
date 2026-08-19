e8c24ea72d93a6e063827d37a599799e442c3197
pirhoo <hello@pirhoo.com>
2026-08-19 14:40:46 +0000

    refactor(markdown): share the rendered body styles between both views
    Co-authored-by: Caroline Desprat <cdesprat@icij.org>

--- src/components/Document/DocumentViewer/DocumentViewerMarkdown.vue
+++ src/components/Document/DocumentViewer/DocumentViewerMarkdown.vue
@@ -67,7 +67,7 @@ watch(() => props.document, load, { immediate: true })
     <!-- eslint-disable-next-line vue/no-v-html -->
     <div
       v-else
-      class="markdown-viewer__content shadow-sm border p-3 mx-auto"
+      class="markdown-viewer__content markdown-body shadow-sm border p-3 mx-auto"
       v-html="html"
     />
   </div>
@@ -80,79 +80,6 @@ watch(() => props.document, load, { immediate: true })
 
   &__content {
     max-width: 1012px;
-
-    h1,
-    h2,
-    h3,
-    h4,
-    h5,
-    h6 {
-      margin-top: $spacer-lg;
-      margin-bottom: $spacer;
-      font-weight: $font-weight-semibold;
-      line-height: 1.25;
-    }
-
-    h1 {
-      font-size: 2rem;
-      padding-bottom: 0.3em;
-      border-bottom: 1px solid var(--bs-border-color-translucent);
-    }
-
-    h2 {
-      font-size: 1.5rem;
-      padding-bottom: 0.3em;
-      border-bottom: 1px solid var(--bs-border-color-translucent);
-    }
-
-    h3 {
-      font-size: 1.25rem;
-    }
-
-    h4 {
-      font-size: 1rem;
-    }
-
-    h5 {
-      font-size: 0.875rem;
-    }
-
-    h6 {
-      font-size: 0.85rem;
-      color: var(--bs-secondary-color);
-    }
-
-    table {
-      border-collapse: collapse;
-      margin-bottom: $spacer;
-
-      th,
-      td {
-        border: 1px solid var(--bs-border-color);
-        padding: 6px 13px;
-      }
-
-      th {
-        font-weight: $font-weight-semibold;
-      }
-
-      tr:nth-child(2n) {
-        background-color: var(--bs-tertiary-bg);
-      }
-    }
-
-    pre {
-      padding: $spacer;
-      overflow-x: auto;
-      background: var(--bs-tertiary-bg);
-      border-radius: var(--bs-border-radius);
-    }
-
-    blockquote {
-      margin: 0;
-      padding-left: $spacer;
-      border-left: 4px solid var(--bs-border-color);
-    }
   }
 }
 </style>
